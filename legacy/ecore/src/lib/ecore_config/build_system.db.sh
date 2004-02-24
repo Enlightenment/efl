d2071dcb65dcc22720f26fa14221557fb697662b
handyande <handyande>
2004-02-24 19:54:51 +0000

    Hmm, Moz name changes again
    SVN revision: 9097

--- legacy/ecore/src/lib/ecore_config/build_system.db.sh
+++ legacy/ecore/src/lib/ecore_config/build_system.db.sh
@@ -4,14 +4,14 @@ edb_ed $DB add /e/theme/name str "winter"
 if [ $BROWSER ]; then
 edb_ed $DB add /apps/web/browser str "$BROWSER"
 else
-edb_ed $DB add /apps/web/browser str "`which MozillaFirebird 2>/dev/null || 
+edb_ed $DB add /apps/web/browser str "`which firefox 2>/dev/null || 
                                        which phoenix 2>/dev/null || 
                                        which mozilla 2>/dev/null || 
                                        which opera 2>/dev/null || 
                                        which konqueror 2>/dev/null || 
                                        which epiphany 2>/dev/null`"
 fi
-edb_ed $DB add /apps/web/email str "`which MozillaThunderbird 2>/dev/null || 
+edb_ed $DB add /apps/web/email str "`which thunderbird 2>/dev/null || 
                                      which mozilla 2>/dev/null || 
                                      which kmail 2>/dev/null || 
                                      which sylpheed 2>/dev/null || 
