2a6937cccbf90e16710dc46562249fb3d9405f39
Carsten Haitzler <raster@rasterman.com>
2005-05-22 02:49:48 +0000

    whitespace
    SVN revision: 14888

--- legacy/evas/src/lib/engines/common/evas_font_main.c
+++ legacy/evas/src/lib/engines/common/evas_font_main.c
@@ -34,11 +34,11 @@ evas_common_font_ascent_get(RGBA_Font *fn)
    int val, dv;
    int ret;
    RGBA_Font_Int *fi;
-   
+
    evas_common_font_size_use(fn);
    fi = fn->fonts->data;
    val = (int)fi->src->ft.face->size->metrics.ascender;
-   dv = (fi->src->ft.orig_upem * 2048) / fi->src->ft.face->units_per_EM; 
+   dv = (fi->src->ft.orig_upem * 2048) / fi->src->ft.face->units_per_EM;
    ret = (val * fi->src->ft.face->size->metrics.y_scale) / (dv * dv);
    return ret;
 }
@@ -49,11 +49,11 @@ evas_common_font_descent_get(RGBA_Font *fn)
    int val, dv;
    int ret;
    RGBA_Font_Int *fi;
-   
+
    evas_common_font_size_use(fn);
    fi = fn->fonts->data;
    val = -(int)fi->src->ft.face->size->metrics.descender;
-   dv = (fi->src->ft.orig_upem * 2048) / fi->src->ft.face->units_per_EM; 
+   dv = (fi->src->ft.orig_upem * 2048) / fi->src->ft.face->units_per_EM;
    ret = (val * fi->src->ft.face->size->metrics.y_scale) / (dv * dv);
    return ret;
 }
@@ -64,11 +64,11 @@ evas_common_font_max_ascent_get(RGBA_Font *fn)
    int val, dv;
    int ret;
    RGBA_Font_Int *fi;
-   
+
    evas_common_font_size_use(fn);
    fi = fn->fonts->data;
    val = (int)fi->src->ft.face->bbox.yMax;
-   dv = (fi->src->ft.orig_upem * 2048) / fi->src->ft.face->units_per_EM; 
+   dv = (fi->src->ft.orig_upem * 2048) / fi->src->ft.face->units_per_EM;
    ret = (val * fi->src->ft.face->size->metrics.y_scale) / (dv * dv);
    return ret;
 }
@@ -79,11 +79,11 @@ evas_common_font_max_descent_get(RGBA_Font *fn)
    int val, dv;
    int ret;
    RGBA_Font_Int *fi;
-   
+
    evas_common_font_size_use(fn);
    fi = fn->fonts->data;
    val = -(int)fi->src->ft.face->bbox.yMin;
-   dv = (fi->src->ft.orig_upem * 2048) / fi->src->ft.face->units_per_EM;   
+   dv = (fi->src->ft.orig_upem * 2048) / fi->src->ft.face->units_per_EM;
    ret = (val * fi->src->ft.face->size->metrics.y_scale) / (dv * dv);
    return ret;
 }
@@ -94,11 +94,11 @@ evas_common_font_get_line_advance(RGBA_Font *fn)
    int val, dv;
    int ret;
    RGBA_Font_Int *fi;
-   
+
    evas_common_font_size_use(fn);
    fi = fn->fonts->data;
    val = (int)fi->src->ft.face->size->metrics.height;
-   dv = (fi->src->ft.orig_upem * 2048) / fi->src->ft.face->units_per_EM;   
+   dv = (fi->src->ft.orig_upem * 2048) / fi->src->ft.face->units_per_EM;
    ret = (val * fi->src->ft.face->size->metrics.y_scale) / (dv * dv);
    return ret;
 }
@@ -107,24 +107,24 @@ int
 evas_common_font_utf8_get_next(unsigned char *buf, int *iindex)
 {
    /* Reads UTF8 bytes from @buf, starting at *@index and returns
-    * the decoded code point at iindex offset, and advances iidnex 
+    * the decoded code point at iindex offset, and advances iidnex
     * to the next code point after this.
-    * 
+    *
     * Returns 0 to indicate an error (e.g. invalid UTF8)
-    */   
+    */
    int index = *iindex, r;
    unsigned char d, d2, d3, d4;
-   
+
    d = buf[index++];
    if (!d)
      return 0;
-   if (d < 0x80) 
+   if (d < 0x80)
      {
 	*iindex = index;
-	return d;      
+	return d;
      }
-   if ((d & 0xe0) == 0xc0) 
-     { 
+   if ((d & 0xe0) == 0xc0)
+     {
 	/* 2 byte */
 	if (((d2 = buf[index++]) & 0xc0) != 0x80)
 	  return 0;
@@ -132,8 +132,8 @@ evas_common_font_utf8_get_next(unsigned char *buf, int *iindex)
 	r <<= 6;
 	r |= (d2 & 0x3f); /* copy lower 6 */
      }
-   else if ((d & 0xf0) == 0xe0) 
-     { 
+   else if ((d & 0xf0) == 0xe0)
+     {
 	/* 3 byte */
 	if (((d2 = buf[index++]) & 0xc0) != 0x80 ||
 	   ((d3 = buf[index++]) & 0xc0) != 0x80)
@@ -145,7 +145,7 @@ evas_common_font_utf8_get_next(unsigned char *buf, int *iindex)
 	r |= (d3 & 0x3f);
      }
    else
-     { 
+     {
 	/* 4 byte */
 	if (((d2 = buf[index++]) & 0xc0) != 0x80 ||
 	    ((d3 = buf[index++]) & 0xc0) != 0x80 ||
@@ -157,7 +157,7 @@ evas_common_font_utf8_get_next(unsigned char *buf, int *iindex)
 	r <<= 6;
 	r |= (d3 & 0x3f);
 	r <<= 6;
-	r |= (d4 & 0x3f);	
+	r |= (d4 & 0x3f);
      }
    *iindex = index;
    return r;
@@ -167,21 +167,21 @@ int
 evas_common_font_utf8_get_prev(unsigned char *buf, int *iindex)
 {
    /* Reads UTF8 bytes from @buf, starting at *@index and returns
-    * the decoded code point at iindex offset, and advances iidnex 
+    * the decoded code point at iindex offset, and advances iidnex
     * to the next code point after this.
-    * 
+    *
     * Returns 0 to indicate an error (e.g. invalid UTF8)
-    */   
+    */
    int index = *iindex, r, istart = *iindex;
    unsigned char d, d2, d3, d4;
 
    d = buf[index++];
-   if (d < 0x80) 
+   if (d < 0x80)
      {
 	r = d;
      }
-   else if ((d & 0xe0) == 0xc0) 
-     { 
+   else if ((d & 0xe0) == 0xc0)
+     {
 	/* 2 byte */
 	d2 = buf[index++];
 	if ((d2 & 0xc0) != 0x80)
@@ -190,8 +190,8 @@ evas_common_font_utf8_get_prev(unsigned char *buf, int *iindex)
 	r <<= 6;
 	r |= (d2 & 0x3f); /* copy lower 6 */
      }
-   else if ((d & 0xf0) == 0xe0) 
-     { 
+   else if ((d & 0xf0) == 0xe0)
+     {
 	/* 3 byte */
 	d2 = buf[index++];
 	d3 = buf[index++];
@@ -205,7 +205,7 @@ evas_common_font_utf8_get_prev(unsigned char *buf, int *iindex)
 	r |= (d3 & 0x3f);
      }
    else
-     { 
+     {
 	/* 4 byte */
 	d2 = buf[index++];
 	d3 = buf[index++];
@@ -220,7 +220,7 @@ evas_common_font_utf8_get_prev(unsigned char *buf, int *iindex)
 	r <<= 6;
 	r |= (d3 & 0x3f);
 	r <<= 6;
-	r |= (d4 & 0x3f);	
+	r |= (d4 & 0x3f);
      }
    if (istart > 0)
      {
@@ -255,12 +255,12 @@ evas_common_font_utf8_get_last(unsigned char *buf, int buflen)
    /* jumps to the nul byte at the buffer end and decodes backwards and
     * returns the offset index byte in the buffer where the last character
     * in the buffer begins.
-    * 
+    *
     * Returns -1 to indicate an error
     */
    int index;
    unsigned char d;
-   
+
    if (buflen < 1) return 0;
    index = buflen - 1;
    d = buf[index];
