7523c7699bff74089e596cc83996af8062a7a3c6
sebastid <sebastid>
2005-06-12 11:49:39 +0000

    If the window hasn't any sizehint, reset the struct.
    SVN revision: 15271

--- legacy/ecore/src/lib/ecore_x/ecore_x_icccm.c
+++ legacy/ecore/src/lib/ecore_x/ecore_x_icccm.c
@@ -291,7 +291,10 @@ ecore_x_icccm_size_pos_hints_set(Ecore_X_Window win,
    XSizeHints  hint;
    long        mask;
 
-   XGetWMNormalHints(_ecore_x_disp, win, &hint, &mask);
+   if (!XGetWMNormalHints(_ecore_x_disp, win, &hint, &mask))
+     {
+	memset(&hint, 0, sizeof(XSizeHints));
+     }
 
    hint.flags = 0;
    if (request_pos)
