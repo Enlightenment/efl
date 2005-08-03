7c3c5c1fd0293847c43a5d7125f2d1738d706bfb
Mike Frysinger <vapier@gentoo.org>
2005-08-03 00:34:08 +0000

    make sure we abort if autotools fail
    SVN revision: 16007

--- legacy/edje/autogen.sh
+++ legacy/edje/autogen.sh
@@ -3,11 +3,11 @@
 rm -rf autom4te.cache
 rm -f aclocal.m4
 
-echo "Running aclocal..."; aclocal $ACLOCAL_FLAGS -I m4 \
-&& echo "Running autoheader..."; autoheader \
-&& echo "Running autoconf..."; autoconf \
-&& echo "Running libtoolize..."; (libtoolize --automake || glibtoolize --automake) \
-&& echo "Running automake..."; automake --add-missing --copy --gnu
+echo "Running aclocal..." ; aclocal $ACLOCAL_FLAGS -I m4 || exit 1
+echo "Running autoheader..." ; autoheader || exit 1
+echo "Running autoconf..." ; autoconf || exit 1
+echo "Running libtoolize..." ; (libtoolize --automake || glibtoolize --automake) || exit 1
+echo "Running automake..." ; automake --add-missing --copy --gnu || exit 1
 
 if [ -z "$NOCONFIGURE" ]; then
 	./configure "$@"
