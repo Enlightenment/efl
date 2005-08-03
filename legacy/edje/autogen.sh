aac4224f3cb29f9755d3342534992d70eabc40c7
Mike Frysinger <vapier@gentoo.org>
2005-08-03 00:39:10 +0000

    have libtool copy files, not symlink them
    SVN revision: 16010

--- legacy/edje/autogen.sh
+++ legacy/edje/autogen.sh
@@ -6,7 +6,7 @@ rm -f aclocal.m4
 echo "Running aclocal..." ; aclocal $ACLOCAL_FLAGS -I m4 || exit 1
 echo "Running autoheader..." ; autoheader || exit 1
 echo "Running autoconf..." ; autoconf || exit 1
-echo "Running libtoolize..." ; (libtoolize --automake || glibtoolize --automake) || exit 1
+echo "Running libtoolize..." ; (libtoolize --copy --automake || glibtoolize --automake) || exit 1
 echo "Running automake..." ; automake --add-missing --copy --gnu || exit 1
 
 if [ -z "$NOCONFIGURE" ]; then
