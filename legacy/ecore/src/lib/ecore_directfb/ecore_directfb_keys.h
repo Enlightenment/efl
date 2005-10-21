4667f645da209d5145e869f7a90c65d32e684c0e
sebastid <sebastid>
2005-10-21 07:10:07 +0000

    space
    SVN revision: 17726

--- legacy/ecore/src/lib/ecore_directfb/ecore_directfb_keys.h
+++ legacy/ecore/src/lib/ecore_directfb/ecore_directfb_keys.h
@@ -1,10 +1,11 @@
 typedef struct _Ecore_DirectFB_Key_Symbols Ecore_DirectFB_Key_Symbols;
+
 struct _Ecore_DirectFB_Key_Symbols
 {
 	char *keysymbol;
 	unsigned int keycode;
 } _ecore_directfb_key_symbols[] = {
-	{"BackSpace",DIKS_BACKSPACE},
+	{"BackSpace", DIKS_BACKSPACE},
 	{"Tab", DIKS_TAB},
 	{"Return", DIKS_RETURN},
 	{"Cancel", DIKS_CANCEL},
@@ -100,7 +101,7 @@ struct _Ecore_DirectFB_Key_Symbols
 	{"x", DIKS_SMALL_X },
 	{"y", DIKS_SMALL_Y },
 	{"z", DIKS_SMALL_Z },
-	{"braceleft",DIKS_CURLY_BRACKET_LEFT },
+	{"braceleft", DIKS_CURLY_BRACKET_LEFT },
 	{"bar", DIKS_VERTICAL_BAR },
 	{"braceright", DIKS_CURLY_BRACKET_RIGHT },
 	{"asciitilde", DIKS_TILDE },
@@ -117,14 +118,14 @@ struct _Ecore_DirectFB_Key_Symbols
 	{"Print", DIKS_PRINT},
 	{"Pause", DIKS_PAUSE},
 	/* ok */
-	{"Select",DIKS_SELECT},
+	{"Select", DIKS_SELECT},
 	/* goto */
 	{"Clear", DIKS_CLEAR},
 	/* power */
 	/* power 2 */
 	/* option */
-	{"Menu",DIKS_MENU},
-	{"Help",DIKS_HELP},
+	{"Menu", DIKS_MENU},
+	{"Help", DIKS_HELP},
 	/* info */
 	/* time */
 	/* vendor */
@@ -133,8 +134,8 @@ struct _Ecore_DirectFB_Key_Symbols
 	/* channel */
 	/* favorites */
 	/* hasta next */
-	{"Next",DIKS_NEXT},
-	{"Begin",DIKS_BEGIN},
+	{"Next", DIKS_NEXT},
+	{"Begin", DIKS_BEGIN},
 	/* digits */
 	/* teen */
 	/* twen */
@@ -144,32 +145,32 @@ struct _Ecore_DirectFB_Key_Symbols
 	{"upleftcorner", DIKS_CURSOR_LEFT_UP },
 	{"lowleftcorner", DIKS_CURSOR_LEFT_DOWN },
 	{"uprightcorner", DIKS_CURSOR_UP_RIGHT },
-	{"lowrightcorner",DIKS_CURSOR_DOWN_RIGHT },
-	{"F1",DIKS_F1},
-	{"F2",DIKS_F2},
-	{"F3",DIKS_F3},
-	{"F4",DIKS_F4},
-	{"F5",DIKS_F5},
-	{"F6",DIKS_F6},
-	{"F7",DIKS_F7},
-	{"F8",DIKS_F8},
-	{"F9",DIKS_F9},
-	{"F10",DIKS_F10},
-	{"F11",DIKS_F11},
-	{"F12",DIKS_F12},
+	{"lowrightcorner", DIKS_CURSOR_DOWN_RIGHT },
+	{"F1", DIKS_F1},
+	{"F2", DIKS_F2},
+	{"F3", DIKS_F3},
+	{"F4", DIKS_F4},
+	{"F5", DIKS_F5},
+	{"F6", DIKS_F6},
+	{"F7", DIKS_F7},
+	{"F8", DIKS_F8},
+	{"F9", DIKS_F9},
+	{"F10", DIKS_F10},
+	{"F11", DIKS_F11},
+	{"F12", DIKS_F12},
 	/* this are only mapped to one, not left right */
 	{"Shift_L", DIKS_SHIFT},
-	/*{"Shift_R",0xFFE2},*/
+	/*{"Shift_R", 0xFFE2}, */
 	{"Control_L", DIKS_CONTROL},
-	/*{"Control_R",0xFFE4},*/
+	/*{"Control_R", 0xFFE4}, */
 	{"Meta_L", DIKS_META},
-	/* {"Meta_R",0xFFE8},*/
+	/* {"Meta_R", 0xFFE8}, */
 	{"Alt_L", DIKS_ALT},
 	{"Alt_R", DIKS_ALTGR},
 	{"Super_L", DIKS_SUPER},
-	/*{"Super_R",0xFFEC},*/
+	/*{"Super_R", 0xFFEC}, */
 	{"Hyper_L", DIKS_HYPER},
-	/*{"Hyper_R",0xFFEE},*/
+	/*{"Hyper_R", 0xFFEE}, */
 		
 	{"Caps_Lock", DIKS_CAPS_LOCK},
 	{"Num_Lock", DIKS_NUM_LOCK},
