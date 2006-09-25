3232c1d19b5203e37691d3fd0018d0e2051f4dbe
sebastid <sebastid>
2006-09-25 22:39:08 +0000

    xml parser
    SVN revision: 26152

--- legacy/ecore/src/lib/ecore_desktop/ecore_desktop_private.h
+++ legacy/ecore/src/lib/ecore_desktop/ecore_desktop_private.h
@@ -1,3 +1,6 @@
+/*
+ * vim:ts=8:sw=3:sts=8:noexpandtab:cino=>5n-3f0^-2{2
+ */
 #ifndef _ECORE_DESKTOP_PRIVATE_H
 # define _ECORE_DESKTOP_PRIVATE_H
 
@@ -25,12 +28,35 @@ extern Ecore_List  *ecore_desktop_paths_kde_legacy;
 extern Ecore_List  *ecore_desktop_paths_xsessions;
 extern struct _Ecore_Desktop_Instrumentation instrumentation;
 
+typedef struct _Ecore_Desktop_Xml Ecore_Desktop_Xml;
+typedef struct _Ecore_Desktop_Xml_Attribute Ecore_Desktop_Xml_Attribute;
+
+
+struct _Ecore_Desktop_Xml
+{
+   char                         *tag;
+   Ecore_Desktop_Xml_Attribute **attributes;
+   char                         *text;
+
+   unsigned char done : 1;
+
+   Ecore_Desktop_Xml **children;
+   int                 children_count;
+   int                 children_size;
+};
+
+struct _Ecore_Desktop_Xml_Attribute
+{
+   char *key;
+   char *value;
+};
 
 # ifdef __cplusplus
 extern              "C"
 {
 # endif
-
+   Ecore_Desktop_Xml *ecore_desktop_xml_new(const char *file);
+   void               ecore_desktop_xml_del(Ecore_Desktop_Xml *xml);
 # ifdef __cplusplus
 }
 # endif
