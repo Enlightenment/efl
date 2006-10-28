573a7813eabbd8a6f60e3cde575445df9e0aa35b
Carsten Haitzler <raster@rasterman.com>
2006-10-28 09:18:04 +0000

    remove data
    SVN revision: 26830

--- legacy/edje/data/e_logo.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-exec edje_cc -v -fd ./test/fonts -id ./images src/e_logo.edc e_logo.edj
--- legacy/edje/data/edje_test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-exec edje_cc -v -fd ./test/fonts -id ./images src/edje_test.edc edje_test.eet 
