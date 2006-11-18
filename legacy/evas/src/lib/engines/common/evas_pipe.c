f3e4e0d7ed06b227385a2d927ae01e7b5c1a45ff
Carsten Haitzler <raster@rasterman.com>
2006-11-18 02:51:27 +0000

    threads back on - try this.
    SVN revision: 27162

--- legacy/evas/src/lib/engines/common/evas_pipe.c
+++ legacy/evas/src/lib/engines/common/evas_pipe.c
@@ -132,6 +132,7 @@ evas_common_pipe_begin(RGBA_Image *im)
 	thread_num = cpunum;
 	if (thread_num == 1) return;
 	pthread_barrier_init(&(thbarrier[0]), NULL, thread_num + 1);
+	pthread_barrier_init(&(thbarrier[1]), NULL, thread_num + 1);
 	for (i = 0; i < thread_num; i++)
 	  {
 	     pthread_attr_t attr;
@@ -179,9 +180,9 @@ evas_common_pipe_begin(RGBA_Image *im)
 //	     printf("START %i\n", i);
      }
    /* tell worker threads to start */
-   pthread_barrier_init(&(thbarrier[1]), NULL, thread_num + 1);
+//   pthread_barrier_init(&(thbarrier[1]), NULL, thread_num + 1);
    pthread_barrier_wait(&(thbarrier[0]));
-   pthread_barrier_destroy(&(thbarrier[0]));
+//   pthread_barrier_destroy(&(thbarrier[0]));
 #endif   
 }
 
@@ -197,9 +198,9 @@ evas_common_pipe_flush(RGBA_Image *im)
    if (thread_num > 1)
      {
 	/* sync worker threads */
-	pthread_barrier_init(&(thbarrier[0]), NULL, thread_num + 1);
+//	pthread_barrier_init(&(thbarrier[0]), NULL, thread_num + 1);
 	pthread_barrier_wait(&(thbarrier[1]));
-	pthread_barrier_destroy(&(thbarrier[1]));
+//	pthread_barrier_destroy(&(thbarrier[1]));
 //	printf("DONE\n");
      }
    else
