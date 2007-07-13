7643c66325b8b8aa65d6367f2f010b4cadd51c5d
Gustavo Sverzut Barbieri <barbieri@gmail.com>
2007-07-13 15:19:08 +0000

    Fix compile time CPU detection on ARM
    SVN revision: 30795

--- legacy/evas/src/modules/engines/software_16/evas_soft16.h
+++ legacy/evas/src/modules/engines/software_16/evas_soft16.h
@@ -24,8 +24,30 @@
 #define UNROLL8(op...) UNROLL4(op) UNROLL4(op)
 #define UNROLL16(op...) UNROLL8(op) UNROLL8(op)
 
+#if defined(__ARM_ARCH_3M__) || defined(__ARM_ARCH_4__) || \
+    defined(__ARM_ARCH_4T__)
+# define __ARM_ARCH__ 4
+#endif
 
-#if defined(__ARMEL__)
+#if defined(__ARM_ARCH_5__) || defined(__ARM_ARCH_5T__) || \
+    defined(__ARM_ARCH_5E__) || defined(__ARM_ARCH_5TE__) || \
+    defined(__ARM_ARCH_5TEJ__)
+# define __ARM_ARCH__ 5
+#endif
+
+#if defined(__ARM_ARCH_6__) || defined(__ARM_ARCH_6J__) || \
+    defined(__ARM_ARCH_6K__) || defined(__ARM_ARCH_6Z__) || \
+    defined(__ARM_ARCH_6ZK__) || defined(__ARM_ARCH_6T2__)
+# define __ARM_ARCH__ 6
+#endif
+
+#if defined(__ARM_ARCH_7__) || defined(__ARM_ARCH_7A__) || \
+    defined(__ARM_ARCH_7R__) || defined(__ARM_ARCH_7M__)
+# define __ARM_ARCH__ 7
+#endif
+
+
+#if defined(__ARM_ARCH__) && (__ARM_ARCH__ >= 5)
 /* tested on ARMv6 (arm1136j-s), Nokia N800 CPU */
 #define pld(addr, off)                                                  \
    __asm__("pld [%[address], %[offset]]"::                              \
