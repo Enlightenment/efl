33c1532d5de42ca1120725c9f8772f2778454668
doursse <doursse>
2007-07-16 05:55:27 +0000

    missing declaration function
    SVN revision: 30843

--- legacy/evas/src/modules/engines/software_ddraw/evas_engine.h
+++ legacy/evas/src/modules/engines/software_ddraw/evas_engine.h
@@ -85,6 +85,7 @@ RGBA_Image  *evas_software_ddraw_outbuf_new_region_for_update(Outbuf *buf,
 							      int *ch);
 void         evas_software_ddraw_outbuf_free_region_for_update(Outbuf     *buf,
 							       RGBA_Image *update);
+void         evas_software_ddraw_outbuf_flush(Outbuf *buf);
 void         evas_software_ddraw_outbuf_push_updated_region(Outbuf     *buf,
 							    RGBA_Image *update,
 							    int         x,
