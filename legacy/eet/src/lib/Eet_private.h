c16dd5ae7d9f5cf39092d126df6fc3e8ef978b8c
doursse <doursse>
2007-09-29 09:21:43 +0000

    declare properly alloca when AC_FUNC_ALLOCA is used (see autoconf doc)
    SVN revision: 31864

--- legacy/eet/src/lib/Eet_private.h
+++ legacy/eet/src/lib/Eet_private.h
@@ -12,8 +12,21 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <errno.h>
-#if HAVE_ALLOCA_H
-#include <alloca.h>
+#ifdef HAVE_ALLOCA_H
+# include <alloca.h>
+#elif defined __GNUC__
+# define alloca __builtin_alloca
+#elif defined _AIX
+# define alloca __alloca
+#elif defined _MSC_VER
+# include <malloc.h>
+# define alloca _alloca
+#else
+# include <stddef.h>
+# ifdef  __cplusplus
+extern "C"
+# endif
+void *alloca (size_t);
 #endif
 #include <ctype.h>
 
