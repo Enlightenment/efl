37eed61ee87022ccf39b7be7499749268cf8195b
doursse <doursse>
2008-05-29 05:48:16 +0000

    put unistd.h after sys/types.h. Seems to be needed for vc++. Thanks to Dmitriy Mazovka
    SVN revision: 34691

--- legacy/evas/src/lib/include/evas_common.h
+++ legacy/evas/src/lib/include/evas_common.h
@@ -48,12 +48,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
 #include <signal.h>
 #include <setjmp.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <unistd.h>
 #include <time.h>
 #include <ctype.h>
 #include <stdint.h>
