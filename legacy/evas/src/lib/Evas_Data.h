2679ced73b776d5c1160f2a9ba748ad072aac9c7
doursse <doursse>
2008-07-11 05:42:44 +0000

    move evas_object_list declaration from evas_common.h to Evas_Data.h
    SVN revision: 35061

--- legacy/evas/src/lib/Evas_Data.h
+++ legacy/evas/src/lib/Evas_Data.h
@@ -148,6 +148,18 @@ extern "C" {
 
 
   /*
+   * Evas Object List functions
+   */
+
+   EAPI void *evas_object_list_append               (void *in_list, void *in_item);
+   EAPI void *evas_object_list_prepend              (void *in_list, void *in_item);
+   EAPI void *evas_object_list_append_relative      (void *in_list, void *in_item, void *in_relative);
+   EAPI void *evas_object_list_prepend_relative     (void *in_list, void *in_item, void *in_relative);
+   EAPI void *evas_object_list_remove               (void *in_list, void *in_item);
+   EAPI void *evas_object_list_find                 (void *in_list, void *in_item);
+
+
+  /*
    * Evas Stringshare functions
    */
 
--- legacy/evas/src/lib/include/evas_common.h
+++ legacy/evas/src/lib/include/evas_common.h
@@ -783,13 +783,6 @@ struct _Convert_Pal
 extern "C" {
 #endif
 
-void *evas_object_list_append           (void *in_list, void *in_item);
-void *evas_object_list_prepend          (void *in_list, void *in_item);
-void *evas_object_list_append_relative  (void *in_list, void *in_item, void *in_relative);
-void *evas_object_list_prepend_relative (void *in_list, void *in_item, void *in_relative);
-void *evas_object_list_remove           (void *in_list, void *in_item);
-void *evas_object_list_find             (void *in_list, void *in_item);
-
 /****/
 void evas_common_init                                   (void);
 void evas_common_shutdown                               (void);
