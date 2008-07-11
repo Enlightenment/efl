0fd75a462a571ed1a56d315444536e25924c3889
doursse <doursse>
2008-07-11 08:21:25 +0000

    move pipe function declarations to their own file
    SVN revision: 35064

--- legacy/evas/src/lib/include/evas_common.h
+++ legacy/evas/src/lib/include/evas_common.h
@@ -1015,18 +1015,7 @@ EAPI void               evas_common_draw_context_set_render_op     (RGBA_Draw_Co
 EAPI void               evas_common_draw_context_set_sli           (RGBA_Draw_Context *dc, int y, int h);
 
 /****/
-/* image rendering pipelines... new optional system - non-immediate and
- * threadable
- */
-EAPI void evas_common_pipe_begin(RGBA_Image *im);
-EAPI void evas_common_pipe_flush(RGBA_Image *im);
-EAPI void evas_common_pipe_free(RGBA_Image *im);
-EAPI void evas_common_pipe_rectangle_draw(RGBA_Image *dst, RGBA_Draw_Context *dc, int x, int y, int w, int h);
-EAPI void evas_common_pipe_line_draw(RGBA_Image *dst, RGBA_Draw_Context *dc, int x0, int y0, int x1, int y1);
-EAPI void evas_common_pipe_poly_draw(RGBA_Image *dst, RGBA_Draw_Context *dc, RGBA_Polygon_Point *points);
-EAPI void evas_common_pipe_grad_draw(RGBA_Image *dst, RGBA_Draw_Context *dc, int x, int y, int w, int h, RGBA_Gradient *gr);
-EAPI void evas_common_pipe_text_draw(RGBA_Image *dst, RGBA_Draw_Context *dc, RGBA_Font *fn, int x, int y, const char *text);
-EAPI void evas_common_pipe_image_draw(RGBA_Image *src, RGBA_Image *dst, RGBA_Draw_Context *dc, int smooth, int src_region_x, int src_region_y, int src_region_w, int src_region_h, int dst_region_x, int dst_region_y, int dst_region_w, int dst_region_h);
+#include "../engines/common/evas_pipe.h"
 
 void              evas_font_dir_cache_free(void);
 
