162f31877f9e05e4348a86bdcfe9f6b1e7977737
Cedric BAIL <cedric.bail@free.fr>
2008-07-31 16:55:29 +0000

    Forgotten declaration.
    SVN revision: 35275

--- legacy/eina/src/include/eina_module.h
+++ legacy/eina/src/include/eina_module.h
@@ -15,7 +15,10 @@ typedef int (*Eina_Module_Cb)(Eina_Module *m, void *data);
 
 EAPI Eina_Module * eina_module_new(const char *file);
 EAPI void eina_module_free(Eina_Module *m);
+
+EAPI int eina_module_load(Eina_Module *m);
 EAPI void eina_module_unload(Eina_Module *m);
+
 EAPI char * eina_module_path_get(Eina_Module *m);
 EAPI char * eina_module_name_get(Eina_Module *m);
 EAPI void * eina_module_symbol_get(Eina_Module *m, const char *symbol);
