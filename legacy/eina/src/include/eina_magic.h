bf68fa183cbf28270b854fcf3fff220f1e23c333
Cedric BAIL <cedric.bail@free.fr>
2008-08-01 14:25:20 +0000

    Fix macro.
    SVN revision: 35299

--- legacy/eina/src/include/eina_magic.h
+++ legacy/eina/src/include/eina_magic.h
@@ -3,13 +3,15 @@
 
 #ifdef DEBUG
 
+#include "eina_types.h"
+
 #define EINA_MAGIC_NONE            0x1234fedc
 
 #define EINA_MAGIC	Eina_Magic __magic;
 
 #define EINA_MAGIC_SET(d, m)       (d)->__magic = (m)
 #define EINA_MAGIC_CHECK(d, m)     ((d) && ((d)->__magic == (m)))
-#define EINA_MAGIC_FAIL(d, m, fn)  eina_magic_fail((d), (d) ? (d)->__magic : 0, (m), (__FUNCTION__));
+#define EINA_MAGIC_FAIL(d, m)      eina_magic_fail((d), (d) ? (d)->__magic : 0, (m), __FILE__, __FUNCTION__, __LINE__);
 
 typedef unsigned int               Eina_Magic;
 
@@ -27,11 +29,13 @@ EAPI void eina_magic_fail(void *d, Eina_Magic m, Eina_Magic req_m,
 #define EINA_MAGIC
 #define EINA_MAGIC_SET(d, m)       (void)
 #define EINA_MAGIC_CHECK(d, m)	   (1)
-#define EINA_MAGIC_FAIL(d, m, fn)  (void)
+#define EINA_MAGIC_FAIL(d, m)      (void)
 
 #define eina_magic_string_get(Magic)                  (NULL)
 #define eina_magic_string_set(Magic, Magic_Name)      (void)
 #define eina_magic_fail(d, m, req_m, file, fnx, line) (void)
+#define eina_magic_string_init()   (0)
+#define eina_magic_string_shutdown() (0)
 
 #endif
 
