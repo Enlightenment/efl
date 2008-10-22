1cefba401250348cb675963587dd919e3469bfb6
Carsten Haitzler <raster@rasterman.com>
2008-10-22 04:00:05 +0000

    fix last newline detection - dont keep adding them in!
    SVN revision: 36950

--- legacy/edje/src/lib/edje_entry.c
+++ legacy/edje/src/lib/edje_entry.c
@@ -1182,10 +1182,15 @@ _edje_entry_text_markup_set(Edje_Real_Part *rp, const char *text)
    _sel_clear(en->cursor, rp->object, en);
    evas_object_textblock_text_markup_set(rp->object, text); 
    evas_textblock_cursor_node_last(en->cursor);
-   if ((!evas_textblock_cursor_node_format_get(en->cursor)) ||
-       (!strcmp(evas_textblock_cursor_node_format_get(en->cursor), "\n")) ||
-       (!strcmp(evas_textblock_cursor_node_format_get(en->cursor), "\\n")))
-     evas_textblock_cursor_format_append(en->cursor, "\n");
+   if (!evas_textblock_cursor_node_format_get(en->cursor))
+     {
+	evas_textblock_cursor_format_append(en->cursor, "\n");
+     }
+   else if (!((!strcmp(evas_textblock_cursor_node_format_get(en->cursor), "\n")) ||
+	      (!strcmp(evas_textblock_cursor_node_format_get(en->cursor), "\\n"))))
+     {
+	evas_textblock_cursor_format_append(en->cursor, "\n");
+     }
    _anchors_get(en->cursor, rp->object, en);
    _edje_emit(rp->edje, "entry,changed", rp->part->name);
    _edje_entry_set_cursor_start(rp);
