ccab8e21013477b0fafc9b476663c7e3d1e5b65e
Vincent Torri <vincent.torri@gmail.com>
2008-10-24 06:06:57 +0000

    missing files
    SVN revision: 37041

---
+++ legacy/evil/src/bin/evil_test_environment.c
@@ -0,0 +1,37 @@
+
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif /* HAVE_CONFIG_H */
+
+#include <stdio.h>
+#include <string.h>
+
+#include <Evil.h>
+
+#include "evil_suite.h"
+
+static int
+test_env_tests_run(suite *s)
+{
+   char *val;
+   int   res;
+
+   res = setenv("EVIL_TEST_ENV", "val1", 1);
+   if (res < 0)
+     return 0;
+
+   val = getenv("EVIL_TEST_ENV");
+   printf ("val : %s\n", val);
+   if (strcmp(val, "val1"))
+     return 0;
+
+   return 1;
+}
+
+int
+test_environment(suite *s)
+{
+
+   return test_env_tests_run(s);
+}
---
+++ legacy/evil/src/bin/evil_test_environment.h
@@ -0,0 +1,8 @@
+#ifndef __EVIL_TEST_ENVIRONMENT__
+#define __EVIL_TEST_ENVIRONMENT__
+
+
+int test_environment(suite *s);
+
+
+#endif /* __EVIL_TEST_ENVIRONMENT__ */
