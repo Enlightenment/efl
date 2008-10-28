72ae858dac31c07f0bae7637417266203ab8a61c
Gustavo Sverzut Barbieri <barbieri@gmail.com>
2008-10-28 16:37:32 +0000

    fix _GET macros.
    SVN revision: 37276

--- legacy/eina/src/include/eina_inlist.h
+++ legacy/eina/src/include/eina_inlist.h
@@ -37,7 +37,7 @@ struct _Eina_Inlist
 };
 
 #define EINA_INLIST Eina_Inlist __in_list
-#define EINA_INLIST_GET(Inlist) &(Inlist->__in_list)
+#define EINA_INLIST_GET(Inlist) &((Inlist)->__in_list)
 
 EAPI Eina_Inlist * eina_inlist_append(Eina_Inlist *in_list, Eina_Inlist *in_item);
 EAPI Eina_Inlist * eina_inlist_prepend(Eina_Inlist *in_list, Eina_Inlist *in_item);
