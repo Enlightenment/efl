0b2c10c1c6b203d8782b43d3e7482a46a54bb6c5
Sebastian Dransfeld <sd@tango.flipp.net>
2008-12-15 20:31:26 +0000

    Add function prototypes to header.
    SVN revision: 38154

--- legacy/evas/src/lib/cache/evas_cache.h
+++ legacy/evas/src/lib/cache/evas_cache.h
@@ -153,6 +153,8 @@ EAPI Engine_Image_Entry*      evas_cache_engine_image_size_set(Engine_Image_Entr
 
 EAPI void                     evas_cache_engine_image_load_data(Engine_Image_Entry *eim);
 
+EAPI void                     evas_cache_image_preload_data(Image_Entry *im, const void *target);
+EAPI void                     evas_cache_image_preload_cancel(Image_Entry *im);
 
 #ifdef __cplusplus
 }
