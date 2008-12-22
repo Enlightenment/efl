729bf1a8e52edfa678e7d6670169bfc554a5afe9
dm <dm>
2008-12-22 23:21:49 +0000

    Eet Win32 VS8 project modified
    SVN revision: 38283

--- legacy/eet/win32/set_env.bat
+++ legacy/eet/win32/set_env.bat
@@ -15,11 +15,17 @@ set INCLUDE=%EXT_DIR%\include;%INCLUDE%
 set LIB=%EXT_DIR%\lib;%LIB%
 
 rem Add Evil lib path
-set EvilInclude=%cd%\..\..\..\proto\evil\src\lib
-set EvilCommon=%cd%\..\..\..\proto\evil\win32\common
-set EvilOut=%cd%\..\..\..\proto\evil\win32\%PROJECT_TYPE%\out
+set EvilInclude=%cd%\..\..\proto\evil\src\lib
+set EvilCommon=%cd%\..\..\proto\evil\win32\common
+set EvilOut=%cd%\..\..\proto\evil\win32\%PROJECT_TYPE%\out
+
+rem Add Eina lib path
+set EinaInclude=%cd%\..\..\eina\src\include
+set EinaCommon=%cd%\..\..\eina\win32\common
+set EinaOut=%cd%\..\..\eina\win32\%PROJECT_TYPE%\out
 
 set INCLUDE=%EvilCommon%;%EvilInclude%;%EvilInclude%\dlfcn;%EvilInclude%\mman;%INCLUDE%
-set LIB=%EvilOut%;%LIB%
+set INCLUDE=%EinaCommon%;%EinaInclude%;%INCLUDE%
+set LIB=%EinaOut%;%EvilOut%;%LIB%
 
 :END
