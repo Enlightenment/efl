77dedbca0657a1b5c8484da9d4c82e0c382e5b52
dm <dm>
2008-12-22 23:20:52 +0000

    Eina Win32 VS8 project added
    SVN revision: 38282

---
+++ legacy/eina/win32/set_env.bat
@@ -0,0 +1,27 @@
+@echo off
+
+rem Set external libraries directory.
+set EXT_DIR=%cd%\..\..\..\..\extern
+
+if not exist %EXT_DIR% (
+	set EXT_DIR=
+	echo ERROR: External libs dir is not set.
+	pause
+	goto END
+)
+
+rem Add Evil lib path
+set EvilInclude=%cd%\..\..\proto\evil\src\lib
+set EvilCommon=%cd%\..\..\proto\evil\win32\common
+set EvilOut=%cd%\..\..\proto\evil\win32\%PROJECT_TYPE%\out
+
+set INCLUDE=%EvilCommon%;%EvilInclude%;%EvilInclude%\dlfcn;%EvilInclude%\mman;%INCLUDE%
+set LIB=%EvilOut%;%LIB%
+
+rem Add installation directory pathes.
+set INCLUDE=%EXT_DIR%\include;%INCLUDE%
+set LIB=%EXT_DIR%\lib;%LIB%
+
+set INCLUDE=%cd%\common;%cd%\..\src\include;%INCLUDE%
+
+:END
