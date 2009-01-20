7554db76694c11f36a8673388df53de064b524ab
Vincent Torri <vincent.torri@gmail.com>
2009-01-20 16:14:36 +0000

    too much alloca declarations :-)
    SVN revision: 38665

--- legacy/eina/src/lib/eina_benchmark.c
+++ legacy/eina/src/lib/eina_benchmark.c
@@ -49,23 +49,6 @@ extern "C"
 void *alloca (size_t);
 #endif
 
-#ifdef HAVE_ALLOCA_H
-# include <alloca.h>
-#elif defined __GNUC__
-# define alloca __builtin_alloca
-#elif defined _AIX
-# define alloca __alloca
-#elif defined _MSC_VER
-# include <malloc.h>
-# define alloca _alloca
-#else
-# include <stddef.h>
-# ifdef  __cplusplus
-extern "C"
-# endif
-void *alloca (size_t);
-#endif
-
 #include "eina_benchmark.h"
 #include "eina_inlist.h"
 #include "eina_counter.h"
