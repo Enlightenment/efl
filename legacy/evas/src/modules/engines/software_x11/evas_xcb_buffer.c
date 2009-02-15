860fc287c2876b28129bdd1e72e4c7c1bd190ee5
Vincent Torri <vincent.torri@gmail.com>
2009-02-15 11:14:12 +0000

    free correctly each buffer in XCB engine
    The xcb_image dependency should be removed and replaced
    by direct calls of the request XPutImage
    
    
    SVN revision: 39024

--- legacy/evas/src/modules/engines/software_x11/evas_xcb_buffer.c
+++ legacy/evas/src/modules/engines/software_x11/evas_xcb_buffer.c
@@ -221,6 +221,7 @@ evas_software_xcb_x_output_buffer_free(Xcb_Output_Buffer *xcbob,
                                          xcb_get_input_focus_unchecked(xcbob->connection),
                                          NULL));
 	xcb_shm_detach(xcbob->connection, xcbob->shm_info->shmseg);
+	free(xcbob->image->data);
 	xcb_image_destroy(xcbob->image);
 	shmdt(xcbob->shm_info->shmaddr);
 	shmctl(xcbob->shm_info->shmid, IPC_RMID, 0);
@@ -229,6 +230,7 @@ evas_software_xcb_x_output_buffer_free(Xcb_Output_Buffer *xcbob,
    else
      {
 	if (xcbob->data) xcbob->image->data = NULL;
+	free(xcbob->image->data);
 	xcb_image_destroy(xcbob->image);
      }
    free(xcbob);
