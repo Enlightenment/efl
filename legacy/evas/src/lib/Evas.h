3a774f6971d854c5d567f53d5ce9164e19d04531
Vincent Torri <vincent.torri@gmail.com>
2009-03-20 06:43:44 +0000

    * Re-add EAPI. These macros must be defined specifically for each EFL    for Windows (XP and CE).  * include Eina.h before EAPI so that this latter is defined correctly for Evas
    SVN revision: 39599

--- legacy/evas/src/lib/Evas.h
+++ legacy/evas/src/lib/Evas.h
@@ -1,6 +1,35 @@
 #ifndef _EVAS_H
 #define _EVAS_H
 
+#include <Eina.h>
+
+#ifdef EAPI
+# undef EAPI
+#endif
+
+#ifdef _WIN32
+# ifdef EFL_EVAS_BUILD
+#  ifdef DLL_EXPORT
+#   define EAPI __declspec(dllexport)
+#  else
+#   define EAPI
+#  endif /* ! DLL_EXPORT */
+# else
+#  define EAPI __declspec(dllimport)
+# endif /* ! EFL_EVAS_BUILD */
+#else
+# ifdef __GNUC__
+#  if __GNUC__ >= 4
+#   define EAPI __attribute__ ((visibility("default")))
+#  else
+#   define EAPI
+#  endif
+# else
+#  define EAPI
+# endif
+#endif /* ! _WIN32 */
+
+
 /**
  * @file
  * @brief These routines are used for Evas library interaction.
@@ -10,7 +39,6 @@
  * @todo finish api documentation
  */
 
-#include <Eina.h>
 #include <Evas_Data.h>
 
 typedef enum _Evas_Callback_Type
