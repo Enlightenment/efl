8b7a4fd16ecd5648bbd69c21610bbe977969ba92
Vincent Torri <vincent.torri@gmail.com>
2009-03-24 07:36:46 +0000

    Use an enum for the backend
    SVN revision: 39664

--- legacy/evas/src/modules/engines/software_x11/Evas_Engine_Software_X11.h
+++ legacy/evas/src/modules/engines/software_x11/Evas_Engine_Software_X11.h
@@ -3,11 +3,15 @@
 
 #include <X11/Xlib.h>
 
+typedef enum
+{
+  EVAS_ENGINE_INFO_SOFTWARE_X11_BACKEND_XLIB,
+  EVAS_ENGINE_INFO_SOFTWARE_X11_BACKEND_XCB
+} Evas_Engine_Info_Software_X11_Backend;
+
 typedef struct _Evas_Engine_Info_Software_X11 Evas_Engine_Info_Software_X11;
 
 /*
- * backend: 0 == Xlib, 1 == XCB
- *
  *               Xlib       |      XCB
  * connection | Display *   |  xcb_connection_t *
  * screen     | NULL        |  xcb_screen_t *
@@ -25,23 +29,23 @@ struct _Evas_Engine_Info_Software_X11
 
    /* engine specific data & parameters it needs to set up */
    struct {
-      int          backend;
-      void        *connection;
-      void        *screen;
-      unsigned int drawable;
-      unsigned int mask;
-      void        *visual;
-      unsigned int colormap;
-      int          depth;
-      int          rotation;
+      Evas_Engine_Info_Software_X11_Backend backend;
+      void                                 *connection;
+      void                                 *screen;
+      unsigned int                          drawable;
+      unsigned int                          mask;
+      void                                 *visual;
+      unsigned int                          colormap;
+      int                                   depth;
+      int                                   rotation;
 
-      int          alloc_grayscale    : 1;
-      int          debug              : 1;
-      int          shape_dither       : 1;
-      int          destination_alpha  : 1;
-      int          track_mask_changes : 1;
+      int                                   alloc_grayscale    : 1;
+      int                                   debug              : 1;
+      int                                   shape_dither       : 1;
+      int                                   destination_alpha  : 1;
+      int                                   track_mask_changes : 1;
 
-      int          alloc_colors_max;
+      int                                   alloc_colors_max;
    } info;
    /* engine specific function calls to query stuff about the destination */
    struct {
