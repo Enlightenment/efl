b803f43e91fbe88b9a8cc7dd580f810922c6afd3
Gustavo Sverzut Barbieri <barbieri@gmail.com>
2009-07-16 22:06:40 +0000

    Export EAPI otherwise plugins will not get symbols.
    SVN revision: 41371

--- legacy/ethumb/src/lib/Ethumb_Plugin.h
+++ legacy/ethumb/src/lib/Ethumb_Plugin.h
@@ -14,14 +14,14 @@ struct _Ethumb_Plugin
    void (*generate_thumb)(Ethumb *);
 };
 
-void ethumb_calculate_aspect_from_ratio(Ethumb *e, float ia, int *w, int *h);
-void ethumb_calculate_aspect(Ethumb *e, int iw, int ih, int *w, int *h);
-void ethumb_calculate_fill_from_ratio(Ethumb *e, float ia, int *fx, int *fy, int *fw, int *fh);
-void ethumb_calculate_fill(Ethumb *e, int iw, int ih, int *fx, int *fy, int *fw, int *fh);
-Eina_Bool ethumb_plugin_image_resize(Ethumb *e, int w, int h);
-Eina_Bool ethumb_image_save(Ethumb *e);
-void ethumb_finished_callback_call(Ethumb *e, int result);
-Evas * ethumb_evas_get(const Ethumb *e);
-Ecore_Evas * ethumb_ecore_evas_get(const Ethumb *e);
+EAPI void ethumb_calculate_aspect_from_ratio(Ethumb *e, float ia, int *w, int *h);
+EAPI void ethumb_calculate_aspect(Ethumb *e, int iw, int ih, int *w, int *h);
+EAPI void ethumb_calculate_fill_from_ratio(Ethumb *e, float ia, int *fx, int *fy, int *fw, int *fh);
+EAPI void ethumb_calculate_fill(Ethumb *e, int iw, int ih, int *fx, int *fy, int *fw, int *fh);
+EAPI Eina_Bool ethumb_plugin_image_resize(Ethumb *e, int w, int h);
+EAPI Eina_Bool ethumb_image_save(Ethumb *e);
+EAPI void ethumb_finished_callback_call(Ethumb *e, int result);
+EAPI Evas * ethumb_evas_get(const Ethumb *e);
+EAPI Ecore_Evas * ethumb_ecore_evas_get(const Ethumb *e);
 
 #endif /* _ETHUMB_PLUGIN_H_ */
