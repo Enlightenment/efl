f86aa350c6bfa6ad1ba2dd68cb245ff22d0a9f31
Gustavo Sverzut Barbieri <barbieri@gmail.com>
2009-08-06 22:22:56 +0000

    allow limit tests to run.
    eina_suite TEST1 TEST2 ... is your friend ;-)
    
    
    
    SVN revision: 41624

--- legacy/eina/src/tests/eina_suite.c
+++ legacy/eina/src/tests/eina_suite.c
@@ -22,6 +22,7 @@
 
 #include "eina_suite.h"
 #include "eina_mempool.h"
+#include <stdio.h>
 
 typedef struct _Eina_Test_Case Eina_Test_Case;
 struct _Eina_Test_Case
@@ -53,8 +54,29 @@ static const Eina_Test_Case etc[] = {
   { NULL, NULL }
 };
 
+static void
+_list_tests(void)
+{
+   const Eina_Test_Case *itr = etc;
+   fputs("Available Test Cases:\n", stderr);
+   for (; itr->test_case != NULL; itr++)
+     fprintf(stderr, "\t%s\n", itr->test_case);
+}
+
+static Eina_Bool
+_use_test(int argc, const char **argv, const char *test_case)
+{
+   if (argc < 1)
+     return 1;
+
+   for (; argc > 0; argc--, argv++)
+     if (strcmp(test_case, *argv) == 0)
+       return 1;
+   return 0;
+}
+
 Suite *
-eina_build_suite(void)
+eina_build_suite(int argc, const char **argv)
 {
    TCase *tc;
    Suite *s;
@@ -64,6 +86,7 @@ eina_build_suite(void)
 
    for (i = 0; etc[i].test_case != NULL; ++i)
      {
+	if (!_use_test(argc, argv, etc[i].test_case)) continue;
 	tc = tcase_create(etc[i].test_case);
 
 	etc[i].build(tc);
@@ -94,14 +117,29 @@ static void _mempool_shutdown(void)
 }
 
 int
-main(void)
+main(int argc, char **argv)
 {
    Suite *s;
    SRunner *sr;
-   int failed_count;
+   int i, failed_count;
 
+   for (i = 1; i < argc; i++)
+     if ((strcmp(argv[i], "-h") == 0) ||
+	 (strcmp(argv[i], "--help") == 0))
+       {
+	  fprintf(stderr, "Usage:\n\t%s [test_case1 .. [test_caseN]]\n",
+		  argv[0]);
+	  _list_tests();
+	  return 0;
+       }
+     else if ((strcmp(argv[i], "-l") == 0) ||
+	      (strcmp(argv[i], "--list") == 0))
+       {
+	  _list_tests();
+	  return 0;
+       }
 
-   s = eina_build_suite();
+   s = eina_build_suite(argc - 1, argv + 1);
    sr = srunner_create(s);
 
    _mempool_init();
