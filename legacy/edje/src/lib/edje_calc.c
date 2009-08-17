9abdbe30af12cbcc0cee0c71340924b02e53fe4a
Cedric BAIL <cedric.bail@free.fr>
2009-08-17 15:16:23 +0000

    * edje: Fix tween when comming from same state.
    SVN revision: 41844

--- legacy/edje/src/lib/edje_calc.c
+++ legacy/edje/src/lib/edje_calc.c
@@ -105,7 +105,8 @@ _edje_part_description_apply(Edje *ed, Edje_Real_Part *ep, const char *d1, doubl
 
    epd2 = _edje_part_description_find(ed, ep, d2, v2);
 
-   if (epd2 != NULL && epd1 != epd2)
+   /* There is an animation if both description are different or if description is an image with tweens */
+   if (epd2 != NULL && (epd1 != epd2 || (ep->part->type == EDJE_PART_TYPE_IMAGE && epd2->image.tween_list)))
      {
 	if (!ep->param2)
 	  ep->param2 = eina_mempool_malloc(_edje_real_part_state_mp, sizeof (Edje_Real_Part_State));
