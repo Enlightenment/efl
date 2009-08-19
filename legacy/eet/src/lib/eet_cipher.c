5c8b90d23c70904f57c45540ec2c3e14248241eb
Vincent Torri <vincent.torri@gmail.com>
2009-08-19 10:58:38 +0000

    add alloca() declaration
    SVN revision: 41874

--- legacy/eet/src/lib/eet_cipher.c
+++ legacy/eet/src/lib/eet_cipher.c
@@ -2,6 +2,23 @@
 # include <config.h>
 #endif
 
+#ifdef HAVE_ALLOCA_H
+# include <alloca.h>
+#elif defined __GNUC__
+# define alloca __builtin_alloca
+#elif defined _AIX
+# define alloca __alloca
+#elif defined _MSC_VER
+# include <malloc.h>
+# define alloca _alloca
+#else
+# include <stddef.h>
+# ifdef  __cplusplus
+extern "C"
+# endif
+void *alloca (size_t);
+#endif
+
 #include <stdio.h>
 #include <string.h>
 #include <sys/types.h>
