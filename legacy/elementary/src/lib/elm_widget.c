158f95cd3707aa569a2cd1702f99de3a04243c87
Christopher Michael <cpmichael1@comcast.net>
2009-08-26 16:52:53 +0000

    Simplier check for disabled state.
    SVN revision: 42022

--- legacy/elementary/src/lib/elm_widget.c
+++ legacy/elementary/src/lib/elm_widget.c
@@ -645,8 +645,7 @@ EAPI void
 elm_widget_disabled_set(Evas_Object *obj, int disabled)
 {
    API_ENTRY return;
-   if (((sd->disabled) && (disabled)) ||
-       ((!sd->disabled) && (!disabled))) return;
+   if (sd->disabled == disabled) return;
    sd->disabled = disabled;
    if (sd->focused)
      {
