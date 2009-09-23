32d0ba3d65cecb67767af33a19163c3ace85cde9
Vincent Torri <vincent.torri@gmail.com>
2009-09-23 17:17:59 +0000

    fix common configuration of VS
    SVN revision: 42651

--- legacy/evas/win32/set_env.bat
+++ legacy/evas/win32/set_env.bat
@@ -1,7 +1,7 @@
 @echo off
 
 rem Set external libraries directory.
-set EXT_DIR=%cd%\..\..\..\..\extern
+set EXT_DIR=%cd%\..\..\..\extern
 
 if not exist %EXT_DIR% (
 	set EXT_DIR=
@@ -11,12 +11,12 @@ if not exist %EXT_DIR% (
 )
 
 rem Add installation directory pathes.
-set INCLUDE=%EXT_DIR%\include;%INCLUDE%
+set INCLUDE=%EXT_DIR%\include;%EXT_DIR%\include\freetype2;%INCLUDE%
 set LIB=%EXT_DIR%\lib;%LIB%
 
 rem Add Evas lib path
 set EvasPath=%cd%\..\src\lib
-set INCLUDE=%EvasPath%\..\modules\engines\gl_common;%EvasPath%;%EvasPath%\include;%INCLUDE%
+set INCLUDE=%EvasPath%\..\modules\engines\gl_common;%EvasPath%;%EvasPath%\include;%EvasPath%\cserve;%EvasPath%\file;%INCLUDE%
 
 if "%DXSDK_DIR%" == "" (
 	echo WARNING: Microsoft DirectX SDK is not installed.
@@ -29,9 +29,9 @@ set INCLUDE="%DXSDK_DIR%Include";%INCLUDE%
 set LIB="%DXSDK_DIR%Lib\x86";%LIB%
 
 rem Add Evil lib path
-set EvilInclude=%cd%\..\..\PROTO\evil\src\lib
-set EvilCommon=%cd%\..\..\PROTO\evil\win32\common
-set EvilOut=%cd%\..\..\PROTO\evil\win32\%PROJECT_TYPE%\out
+set EvilInclude=%cd%\..\..\evil\src\lib
+set EvilCommon=%cd%\..\..\evil\win32\common
+set EvilOut=%cd%\..\..\evil\win32\%PROJECT_TYPE%\out
 
 rem Add Eet lib path
 set EetInclude=%cd%\..\..\eet\src\lib
