dd5b63045f9dab9e6122591154e2a388ebe48a3b
Christopher Michael <cpmichael1@comcast.net>
2009-09-26 17:39:29 +0000

    Formatting.
    SVN revision: 42715

--- legacy/elementary/src/lib/elm_button.c
+++ legacy/elementary/src/lib/elm_button.c
@@ -12,8 +12,7 @@ typedef struct _Widget_Data Widget_Data;
 
 struct _Widget_Data
 {
-   Evas_Object *btn;
-   Evas_Object *icon;
+   Evas_Object *btn, *icon;
    const char *label;
 };
 
@@ -70,9 +69,9 @@ static void
 _sizing_eval(Evas_Object *obj)
 {
    Widget_Data *wd = elm_widget_data_get(obj);
-   if (!wd) return;
    Evas_Coord minw = -1, minh = -1, maxw = -1, maxh = -1;
 
+   if (!wd) return;
    elm_coords_finger_size_adjust(1, &minw, 1, &minh);
    edje_object_size_min_restricted_calc(wd->btn, &minw, &minh, minw, minh);
    elm_coords_finger_size_adjust(1, &minw, 1, &minh);
@@ -94,13 +93,13 @@ static void
 _sub_del(void *data, Evas_Object *obj, void *event_info)
 {
    Widget_Data *wd = elm_widget_data_get(obj);
-   if (!wd) return;
    Evas_Object *sub = event_info;
+   if (!wd) return;
    if (sub == wd->icon)
      {
 	edje_object_signal_emit(wd->btn, "elm,state,icon,hidden", "elm");
-	evas_object_event_callback_del
-	  (sub, EVAS_CALLBACK_CHANGED_SIZE_HINTS, _changed_size_hints);
+	evas_object_event_callback_del(sub, EVAS_CALLBACK_CHANGED_SIZE_HINTS, 
+                                       _changed_size_hints);
 	wd->icon = NULL;
 	edje_object_message_signal_process(wd->btn);
 	_sizing_eval(obj);
@@ -141,7 +140,8 @@ elm_button_add(Evas_Object *parent)
 
    wd->btn = edje_object_add(e);
    _elm_theme_set(wd->btn, "button", "base", "default");
-   edje_object_signal_callback_add(wd->btn, "elm,action,click", "", _signal_clicked, obj);
+   edje_object_signal_callback_add(wd->btn, "elm,action,click", "", 
+                                   _signal_clicked, obj);
    elm_widget_resize_object_set(obj, wd->btn);
 
    evas_object_smart_callback_add(obj, "sub-object-del", _sub_del, obj);
@@ -162,22 +162,21 @@ EAPI void
 elm_button_label_set(Evas_Object *obj, const char *label)
 {
    Widget_Data *wd = elm_widget_data_get(obj);
-   if (!wd) return;
    Evas_Coord mw, mh;
 
+   if (!wd) return;
    if (wd->label) eina_stringshare_del(wd->label);
    if (label)
      {
 	wd->label = eina_stringshare_add(label);
 	edje_object_signal_emit(wd->btn, "elm,state,text,visible", "elm");
-	edje_object_message_signal_process(wd->btn);
      }
    else
      {
 	wd->label = NULL;
 	edje_object_signal_emit(wd->btn, "elm,state,text,hidden", "elm");
-	edje_object_message_signal_process(wd->btn);
      }
+   edje_object_message_signal_process(wd->btn);
    edje_object_part_text_set(wd->btn, "elm.text", label);
    _sizing_eval(obj);
 }
@@ -187,7 +186,6 @@ elm_button_label_get(Evas_Object *obj)
 {
    Widget_Data *wd = elm_widget_data_get(obj);
    if (!wd) return NULL;
-
    return wd->label;
 }
 
