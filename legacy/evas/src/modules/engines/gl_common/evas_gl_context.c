e31d7799ee04c5b33830be88374f778ba0805cdd
Carsten Haitzler <raster@rasterman.com>
2009-10-13 00:37:16 +0000

    set tex min/mag filter always
    SVN revision: 43044

--- legacy/evas/src/modules/engines/gl_common/evas_gl_context.c
+++ legacy/evas/src/modules/engines/gl_common/evas_gl_context.c
@@ -514,6 +514,8 @@ shader_array_flush(Evas_GL_Context *gc)
 #endif
              glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
              glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
           }
         else
           {
@@ -522,6 +524,10 @@ shader_array_flush(Evas_GL_Context *gc)
 #endif
              glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
              glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+//             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+//             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
           }
      }
 
