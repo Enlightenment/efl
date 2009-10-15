1d7020335aa96322e442d4a2e8e220daeb2fb617
Vincent Torri <vincent.torri@gmail.com>
2009-10-15 06:26:01 +0000

    * add vi header  * include Ecore.h after stdlib.h so that pid_t is defined on windows
    SVN revision: 43092

--- legacy/ecore/src/lib/ecore/ecore_glib.c
+++ legacy/ecore/src/lib/ecore/ecore_glib.c
@@ -1,9 +1,14 @@
+/*
+ * vim:ts=8:sw=3:sts=8:noexpandtab:cino=>5n-3f0^-2{2
+ */
+
 #ifdef HAVE_CONFIG_H
 # include <config.h>
 #endif
 
-#include "Ecore.h"
 #include <stdio.h>
+
+#include "Ecore.h"
 
 #ifdef HAVE_GLIB
 #include <glib.h>
