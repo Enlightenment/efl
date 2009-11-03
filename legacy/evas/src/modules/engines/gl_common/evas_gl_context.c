7526d0751ef48a3b1473597705c032ae8c792cbb
Carsten Haitzler <raster@rasterman.com>
2009-11-03 10:33:37 +0000

    add a new npot extn string
    SVN revision: 43430

--- legacy/evas/src/modules/engines/gl_common/evas_gl_context.c
+++ legacy/evas/src/modules/engines/gl_common/evas_gl_context.c
@@ -121,7 +121,8 @@ evas_gl_common_context_new(void)
           {
              fprintf(stderr, "EXT:\n%s\n", ext);
              if ((strstr(ext, "GL_ARB_texture_non_power_of_two")) ||
-                 (strstr(ext, "OES_texture_npot")))
+                 (strstr(ext, "OES_texture_npot")) ||
+                 (strstr(ext, "GL_IMG_texture_npot")))
                shared->info.tex_npo2 = 1;
              if ((strstr(ext, "GL_NV_texture_rectangle")) ||
                  (strstr(ext, "GL_EXT_texture_rectangle")) ||
