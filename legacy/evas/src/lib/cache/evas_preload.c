d03271bf4a69d4a7503c45d2e24af5e48f7820b8
Christopher Michael <cpmichael1@comcast.net>
2010-01-10 17:59:53 +0000

    Fix compiler warning about discarding pointer type.
    SVN revision: 45021

--- legacy/evas/src/lib/cache/evas_preload.c
+++ legacy/evas/src/lib/cache/evas_preload.c
@@ -49,7 +49,7 @@ _evas_preload_thread_end(Evas_Preload_Pthread_Data *pth)
 {
    Evas_Preload_Pthread_Data *p;
 
-   if (pthread_join(pth->thread, (void**) &p) != 0)
+   if (pthread_join(pth->thread, (void **) &p) != 0)
      return ;
 
    _evas_preload_thread = eina_list_remove(_evas_preload_thread, pth);
@@ -65,11 +65,11 @@ _evas_preload_thread_done(void *target, Evas_Callback_Type type, void *event_inf
    if (work->cancel)
      {
 	if (work->func_cancel)
-	  work->func_cancel((void*) work->data);
+	  work->func_cancel((void *) work->data);
      }
    else
      {
-	work->func_end((void*) work->data);
+	work->func_end((void *) work->data);
      }
 
    free(work);
@@ -100,7 +100,7 @@ _evas_preload_thread_worker(Evas_Preload_Pthread_Data *pth)
 
 	LKU(_mutex);
 
-	work->func_heavy((void*) work->data);
+	work->func_heavy((void *) work->data);
 
 	evas_async_events_put(pth, 0, work, _evas_preload_thread_done);
      }
@@ -120,7 +120,7 @@ _evas_preload_thread_worker(Evas_Preload_Pthread_Data *pth)
 
    work->data = pth;
    work->func_heavy = NULL;
-   work->func_end = (void*) _evas_preload_thread_end;
+   work->func_end = (void *) _evas_preload_thread_end;
    work->func_cancel = NULL;
    work->cancel = EINA_FALSE;
 
@@ -154,7 +154,7 @@ _evas_preload_thread_shutdown(void)
    EINA_LIST_FREE(_evas_preload_thread_data, work)
      {
 	if (work->func_cancel)
-	  work->func_cancel((void*)work->data);
+	  work->func_cancel((void *)work->data);
 	free(work);
      }
 
@@ -183,7 +183,7 @@ evas_preload_thread_run(void (*func_heavy)(void *data),
    work = malloc(sizeof (Evas_Preload_Pthread_Worker));
    if (!work)
      {
-	func_cancel(data);
+	func_cancel((void *)data);
 	return NULL;
      }
 
@@ -209,7 +209,7 @@ evas_preload_thread_run(void (*func_heavy)(void *data),
    if (!pth)
      goto on_error;
 
-   if (pthread_create(&pth->thread, NULL, (void*) _evas_preload_thread_worker, pth) == 0)
+   if (pthread_create(&pth->thread, NULL, (void *) _evas_preload_thread_worker, pth) == 0)
      {
 	LKL(_mutex);
 	_evas_preload_thread_count++;
@@ -221,7 +221,7 @@ evas_preload_thread_run(void (*func_heavy)(void *data),
    if (_evas_preload_thread_count == 0)
      {
 	if (work->func_cancel)
-	  work->func_cancel((void*) work->data);
+	  work->func_cancel((void *) work->data);
 	free(work);
      }
    return NULL;
@@ -230,8 +230,8 @@ evas_preload_thread_run(void (*func_heavy)(void *data),
      If no thread and as we don't want to break app that rely on this
      facility, we will lock the interface until we are done.
     */
-   func_heavy((void*) data);
-   func_end((void*) data);
+   func_heavy((void *) data);
+   func_end((void *) data);
 
    return EINA_TRUE;
 #endif
@@ -247,14 +247,14 @@ evas_preload_thread_cancel(Evas_Preload_Pthread *thread)
    LKL(_mutex);
 
    EINA_LIST_FOREACH(_evas_preload_thread_data, l, work)
-     if ((void*) work == (void*) thread)
+     if ((void *) work == (void *) thread)
        {
 	  _evas_preload_thread_data = eina_list_remove_list(_evas_preload_thread_data, l);
 
 	  LKU(_mutex);
 
 	  if (work->func_cancel)
-	    work->func_cancel((void*) work->data);
+	    work->func_cancel((void *) work->data);
 	  free(work);
 
 	  return EINA_TRUE;
