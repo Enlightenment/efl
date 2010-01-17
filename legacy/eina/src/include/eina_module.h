4f7ae8764ade94e01105cd486c20a05324512b58
Mikhail Gusarov <dottedmag@dottedmag.net>
2010-01-17 06:01:20 +0000

    eina: fix eina_module_list_get prototype: 'path' argument can be NULL
    SVN revision: 45246

--- legacy/eina/src/include/eina_module.h
+++ legacy/eina/src/include/eina_module.h
@@ -66,7 +66,7 @@ EAPI const char * eina_module_file_get(const Eina_Module *m) EINA_PURE EINA_WARN
 EAPI char *eina_module_symbol_path_get(const void *symbol, const char *sub_dir) EINA_PURE EINA_MALLOC EINA_ARG_NONNULL(1, 2);
 EAPI char *eina_module_environment_path_get(const char *env, const char *sub_dir) EINA_PURE EINA_MALLOC EINA_ARG_NONNULL(1, 2);
 
-EAPI Eina_Array * eina_module_list_get(Eina_Array *array, const char *path, unsigned int recursive, Eina_Module_Cb cb, void *data) EINA_MALLOC EINA_ARG_NONNULL(2) EINA_WARN_UNUSED_RESULT;
+EAPI Eina_Array * eina_module_list_get(Eina_Array *array, const char *path, unsigned int recursive, Eina_Module_Cb cb, void *data) EINA_MALLOC EINA_WARN_UNUSED_RESULT;
 EAPI void eina_module_list_load(Eina_Array *list) EINA_ARG_NONNULL(1);
 EAPI void eina_module_list_unload(Eina_Array *list) EINA_ARG_NONNULL(1);
 EAPI void eina_module_list_free(Eina_Array *list) EINA_ARG_NONNULL(1);
