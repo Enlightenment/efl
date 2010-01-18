cad002c759ee87d7f662bb8a4562d237b9d09e3b
Vincent Torri <vincent.torri@gmail.com>
2010-01-18 18:16:50 +0000

    add stub Windows CE ecore_exe. Will be filled later
    SVN revision: 45296

---
+++ legacy/ecore/src/lib/ecore/ecore_exe_wince.c
@@ -0,0 +1,24 @@
+/*
+ * vim:ts=8:sw=3:sts=8:noexpandtab:cino=>5n-3f0^-2{2
+ */
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#ifdef HAVE_EVIL
+# include <Evil.h>
+#endif
+
+#include "Ecore.h"
+#include "ecore_private.h"
+
+void
+_ecore_exe_init(void)
+{
+}
+
+void
+_ecore_exe_shutdown(void)
+{
+}
