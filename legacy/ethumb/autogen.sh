e7e2fb4bc5c19e343a54561724eb529702f669ff
Daniel Kolesa <quaker66@gmail.com>
2010-01-20 19:13:22 +0000

    Using autoreconf in autogen in ethumb is bad. With it, libtool m4 stuff is included inside dist tarball and we dont want that.
    SVN revision: 45360

--- legacy/ethumb/autogen.sh
+++ legacy/ethumb/autogen.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-autoreconf -f -i
+rm -rf autom4te.cache
+rm -f aclocal.m4 ltmain.sh
+
+echo "Running aclocal..." ; aclocal $ACLOCAL_FLAGS -I m4 || exit 1
+echo "Running autoheader..." ; autoheader || exit 1
+echo "Running autoconf..." ; autoconf || exit 1
+echo "Running libtoolize..." ; (libtoolize --copy --automake || glibtoolize --automake) || exit 1
+echo "Running automake..." ; automake --add-missing --copy --gnu || exit 1
 
 if [ -z "$NOCONFIGURE" ]; then
 	./configure "$@"
