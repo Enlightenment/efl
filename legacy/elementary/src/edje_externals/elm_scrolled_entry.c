1f9f8800370876a504801b450d33e3454b2f579c
Iván Briano <sachieru@gmail.com>
2010-01-29 15:16:22 +0000

    Don't set text in entries for now. It has big issues with state changes.
    SVN revision: 45704

--- legacy/elementary/src/edje_externals/elm_scrolled_entry.c
+++ legacy/elementary/src/edje_externals/elm_scrolled_entry.c
@@ -2,7 +2,7 @@
 
 typedef struct _Elm_Params_Entry
 {
-   const char *text;
+   //const char *text;
    int single, password;
 } Elm_Params_Entry;
 
@@ -16,13 +16,13 @@ external_scrolled_entry_state_set(void *data, Evas_Object *obj, const void *from
 
    if (!p2)
      {
-	elm_scrolled_entry_entry_set(obj, p1->text);
+	//elm_scrolled_entry_entry_set(obj, p1->text);
 	elm_scrolled_entry_single_line_set(obj, p1->single);
 	elm_scrolled_entry_password_set(obj, p1->password);
 	return;
      }
 
-   elm_scrolled_entry_entry_set(obj, p2->text);
+   //elm_scrolled_entry_entry_set(obj, p2->text);
    elm_scrolled_entry_single_line_set(obj, p2->single);
    elm_scrolled_entry_password_set(obj, p2->password);
 }
@@ -40,9 +40,9 @@ external_scrolled_entry_params_parse(void *data, Evas_Object *obj, const Eina_Li
 
    EINA_LIST_FOREACH(params, l, param)
      {
-	if (!strcmp(param->name, "text"))
+	/*if (!strcmp(param->name, "text"))
 	  mem->text = eina_stringshare_add(param->s);
-	else if (!strcmp(param->name, "single line"))
+	else*/ if (!strcmp(param->name, "single line"))
 	  mem->single = param->i;
 	else if (!strcmp(param->name, "password"))
 	  mem->password = param->i;
@@ -56,13 +56,13 @@ external_scrolled_entry_params_free(void *params)
 {
    Elm_Params_Entry *mem = params;
 
-   if (mem->text)
-     eina_stringshare_del(mem->text);
+//   if (mem->text)
+//     eina_stringshare_del(mem->text);
    free(mem);
 }
 
 static Edje_External_Param_Info external_scrolled_entry_params[] = {
-   EDJE_EXTERNAL_PARAM_INFO_STRING_DEFAULT("text", "some text"),
+//   EDJE_EXTERNAL_PARAM_INFO_STRING_DEFAULT("text", "some text"),
    EDJE_EXTERNAL_PARAM_INFO_BOOL("single line"),
    EDJE_EXTERNAL_PARAM_INFO_BOOL("password"),
    EDJE_EXTERNAL_PARAM_INFO_SENTINEL
