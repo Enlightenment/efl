ac60497dbf068693b5e6d77de2bcb8fcd750b7a2
Sebastian Dransfeld <sd@tango.flipp.net>
2010-02-06 20:41:32 +0000

    EINA_ARG_NONNULL
    SVN revision: 45942

--- legacy/eina/src/include/eina_strbuf.h
+++ legacy/eina/src/include/eina_strbuf.h
@@ -8,19 +8,19 @@
 typedef struct _Eina_Strbuf Eina_Strbuf;
 
 EAPI Eina_Strbuf *eina_strbuf_new(void);
-EAPI void eina_strbuf_free(Eina_Strbuf *buf);
-EAPI void eina_strbuf_append(Eina_Strbuf *buf, const char *str);
-EAPI void eina_strbuf_append_char(Eina_Strbuf *buf, char c);
+EAPI void eina_strbuf_free(Eina_Strbuf *buf) EINA_ARG_NONNULL(1);
+EAPI void eina_strbuf_append(Eina_Strbuf *buf, const char *str) EINA_ARG_NONNULL(1, 2);
+EAPI void eina_strbuf_append_char(Eina_Strbuf *buf, char c) EINA_ARG_NONNULL(1);
 EAPI void eina_strbuf_insert(Eina_Strbuf *buf, const char *str, 
-                              size_t pos);
+                              size_t pos) EINA_ARG_NONNULL(1, 2);
 #define eina_strbuf_prepend(buf, str) eina_strbuf_insert(buf, str, 0)
-EAPI const char *eina_strbuf_string_get(Eina_Strbuf *buf);
-EAPI size_t eina_strbuf_length_get(Eina_Strbuf *buf);
+EAPI const char *eina_strbuf_string_get(Eina_Strbuf *buf) EINA_ARG_NONNULL(1);
+EAPI size_t eina_strbuf_length_get(Eina_Strbuf *buf) EINA_ARG_NONNULL(1);
 EAPI int eina_strbuf_replace(Eina_Strbuf *buf, const char *str, 
-                                 const char *with, unsigned int n);
+                                 const char *with, unsigned int n) EINA_ARG_NONNULL(1, 2, 3);
 #define eina_strbuf_replace_first(buf, str, with) \
 	eina_strbuf_replace(buf, str, with, 1)
 EAPI int eina_strbuf_replace_all(Eina_Strbuf *buf, const char *str,
-                                  const char *with);
+                                  const char *with) EINA_ARG_NONNULL(1, 2, 3);
 
 #endif /* EINA_STRBUF_H */
