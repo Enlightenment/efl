25f169fc21356d6b356b8eaf58d88d2bf84ce6da
Carsten Haitzler <raster@rasterman.com>
2010-02-18 07:21:17 +0000

    dont need these with shaders.
    SVN revision: 46267

--- legacy/evas/src/modules/engines/gl_common/evas_gl_context.c
+++ legacy/evas/src/modules/engines/gl_common/evas_gl_context.c
@@ -1076,16 +1076,16 @@ shader_array_flush(Evas_GL_Context *gc)
 
    if (gc->shader.cur_tex != gc->shader.current.cur_tex)
      {
-        if (gc->shader.cur_tex)
-          {
-             glEnable(GL_TEXTURE_2D);
-             GLERR(__FUNCTION__, __FILE__, __LINE__, "");
-          }
-        else
-          {
-             glDisable(GL_TEXTURE_2D);
-             GLERR(__FUNCTION__, __FILE__, __LINE__, "");
-          }
+//        if (gc->shader.cur_tex)
+//          {
+//             glEnable(GL_TEXTURE_2D);
+//             GLERR(__FUNCTION__, __FILE__, __LINE__, "");
+//          }
+//        else
+//          {
+//             glDisable(GL_TEXTURE_2D);
+//             GLERR(__FUNCTION__, __FILE__, __LINE__, "");
+//          }
         glActiveTexture(GL_TEXTURE0);
         GLERR(__FUNCTION__, __FILE__, __LINE__, "");
         glBindTexture(GL_TEXTURE_2D, gc->shader.cur_tex);
