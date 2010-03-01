bf8b3ce2a389339e3c499a8a511418296109d9e9
Vincent Torri <vincent.torri@gmail.com>
2010-03-01 21:05:17 +0000

    add unit test and coverage framework, and trivial tests
    SVN revision: 46742

---
+++ legacy/efreet/src/tests/efreet_test_efreet.c
@@ -0,0 +1,25 @@
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include <Efreet.h>
+
+#include "efreet_suite.h"
+
+
+START_TEST(efreet_test_efreet_init)
+{
+   int ret;
+
+   ret = efreet_init();
+   fail_if(ret != 1);
+
+   ret = efreet_shutdown();
+   fail_if(ret != 0);
+}
+END_TEST
+
+void efreet_test_efreet(TCase *tc)
+{
+   tcase_add_test(tc, efreet_test_efreet_init);
+}
