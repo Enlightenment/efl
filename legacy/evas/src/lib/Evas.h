f883c94804198d89179bc3bd8b7df56d6aa997b6
Christopher Michael <cpmichael1@comcast.net>
2010-03-08 08:36:08 +0000

    Add macros (actually defines like hint_fill_set) for expand_set to make it easier for people to know that weight_set handles expansion.
    SVN revision: 46992

--- legacy/evas/src/lib/Evas.h
+++ legacy/evas/src/lib/Evas.h
@@ -497,10 +497,12 @@ struct _Evas_Native_Surface
 #define EVAS_TEXTURE_RESTRICT_REPEAT    4 /**< tiling clamps and any range offset repeats */
 #define EVAS_TEXTURE_PAD                5 /**< tiling extends with end values */
 
-#define EVAS_HINT_EXPAND  1.0 /**< Use with evas_object_size_hint_weight_set(), evas_object_size_hint_weight_get() */
+#define EVAS_HINT_EXPAND  1.0 /**< Use with evas_object_size_hint_weight_set(), evas_object_size_hint_weight_get(), evas_object_size_hint_expand_set(), evas_object_size_hint_expand_get() */
 #define EVAS_HINT_FILL   -1.0 /**< Use with evas_object_size_hint_align_set(), evas_object_size_hint_align_get(), evas_object_size_hint_fill_set(), evas_object_size_hint_fill_get() */
 #define evas_object_size_hint_fill_set evas_object_size_hint_align_set /**< Convenience macro to make it easier to understand that align is also used for fill properties (as fill is mutually exclusive to align) */
 #define evas_object_size_hint_fill_get evas_object_size_hint_align_get /**< Convenience macro to make it easier to understand that align is also used for fill properties (as fill is mutually exclusive to align) */
+#define evas_object_size_hint_expand_set evas_object_size_hint_weight_set /**< Convenience macro to make it easier to understand that weight is also used for expand properties */
+#define evas_object_size_hint_expand_get evas_object_size_hint_weight_get /**< Convenience macro to make it easier to understand that weight is also used for expand properties */
 
 typedef enum _Evas_Render_Op
 {
