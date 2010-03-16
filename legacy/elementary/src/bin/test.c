5328f8ff8e7b5fb92595828e62607334b37a3ba8
Carsten Haitzler <raster@rasterman.com>
2010-03-16 12:42:24 +0000

    add new test.
    SVN revision: 47289

--- legacy/elementary/src/bin/test.c
+++ legacy/elementary/src/bin/test.c
@@ -43,6 +43,7 @@ void test_genlist6(void *data, Evas_Object *obj, void *event_info);
 void test_table(void *data, Evas_Object *obj, void *event_info);
 void test_pager(void *data, Evas_Object *obj, void *event_info);
 void test_win_state(void *data, Evas_Object *obj, void *event_info);
+void test_win_state2(void *data, Evas_Object *obj, void *event_info);
 void test_progressbar(void *data, Evas_Object *obj, void *event_info);
 void test_fileselector(void *data, Evas_Object *obj, void *event_info);
 void test_separator(void *data, Evas_Object *obj, void *event_info);
@@ -230,6 +231,7 @@ my_win_main(void)
    ADD_TEST("Radios", test_radio);
    ADD_TEST("Pager", test_pager);
    ADD_TEST("Window States", test_win_state);
+   ADD_TEST("Window States 2", test_win_state2);
    ADD_TEST("Progressbar", test_progressbar);
    ADD_TEST("File Selector", test_fileselector);
    ADD_TEST("Separator", test_separator);
--- legacy/elementary/src/bin/test_win_state.c
+++ legacy/elementary/src/bin/test_win_state.c
@@ -101,6 +101,12 @@ _win_foc_out(void *data, Evas *e, void *event_info)
    printf("FOC OUT\n");
 }
 
+static void
+_close_win(void *data, Evas *e, void *event_info)
+{
+   evas_object_del(data);
+}
+
 void
 test_win_state(void *data, Evas_Object *obj, void *event_info)
 {
@@ -222,6 +228,139 @@ test_win_state(void *data, Evas_Object *obj, void *event_info)
    evas_object_show(bx2);
 
    evas_object_resize(win, 280, 400);
+   evas_object_show(win);
+}
+
+void
+test_win_state2(void *data, Evas_Object *obj, void *event_info)
+{
+   Evas_Object *win, *bg, *sl, *bx, *bx2, *bt, *ck;
+   char buf[PATH_MAX];
+   
+   win = elm_win_add(NULL, "window-state2", ELM_WIN_BASIC);
+   elm_win_override_set(win, 1);
+   evas_object_smart_callback_add(win, "moved", my_win_move, NULL);
+   evas_object_event_callback_add(win, EVAS_CALLBACK_RESIZE, _win_resize, NULL);
+   elm_win_title_set(win, "Window States 2");
+   elm_win_autodel_set(win, 1);
+
+   bg = elm_bg_add(win);
+   snprintf(buf, sizeof(buf), "%s/images/sky_02.jpg", PACKAGE_DATA_DIR);
+   elm_bg_file_set(bg, buf, NULL);
+   elm_win_resize_object_add(win, bg);
+   evas_object_size_hint_weight_set(bg, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+   evas_object_show(bg);
+   evas_object_data_set(win, "bg", bg);
+
+   bx = elm_box_add(win);
+   evas_object_size_hint_weight_set(bx, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+   elm_win_resize_object_add(win, bx);
+   evas_object_show(bx);
+
+   bx2 = elm_box_add(win);
+   elm_box_horizontal_set(bx2, 1);
+   evas_object_size_hint_weight_set(bx2, EVAS_HINT_EXPAND, 0.0);
+   evas_object_size_hint_fill_set(bx2, EVAS_HINT_FILL, EVAS_HINT_FILL);
+
+   bt = elm_button_add(win);
+   elm_button_label_set(bt, "Alpha On");
+   evas_object_smart_callback_add(bt, "clicked", my_bt_38_alpha_on, win);
+   evas_object_size_hint_fill_set(bt, EVAS_HINT_FILL, EVAS_HINT_FILL);
+   evas_object_size_hint_weight_set(bt, 0.0, 0.0);
+   elm_box_pack_end(bx2, bt);
+   evas_object_show(bt);
+
+   bt = elm_button_add(win);
+   elm_button_label_set(bt, "Alpha Off");
+   evas_object_smart_callback_add(bt, "clicked", my_bt_38_alpha_off, win);
+   evas_object_size_hint_fill_set(bt, EVAS_HINT_FILL, EVAS_HINT_FILL);
+   evas_object_size_hint_weight_set(bt, 0.0, 0.0);
+   elm_box_pack_end(bx2, bt);
+   evas_object_show(bt);
+
+   bt = elm_button_add(win);
+   elm_button_label_set(bt, "Close");
+   evas_object_smart_callback_add(bt, "clicked", _close_win, win);
+   evas_object_size_hint_fill_set(bt, EVAS_HINT_FILL, EVAS_HINT_FILL);
+   evas_object_size_hint_weight_set(bt, EVAS_HINT_EXPAND, 0.0);
+   elm_box_pack_end(bx2, bt);
+   evas_object_show(bt);
+
+   elm_box_pack_end(bx, bx2);
+   evas_object_show(bx2);
+
+   bx2 = elm_box_add(win);
+   elm_box_horizontal_set(bx2, 1);
+   elm_box_homogenous_set(bx2, 1);
+   evas_object_size_hint_weight_set(bx2, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+   evas_object_size_hint_fill_set(bx2, EVAS_HINT_FILL, EVAS_HINT_FILL);
+
+   sl = elm_slider_add(win);
+   elm_slider_label_set(sl, "Override Redirect");
+   elm_slider_span_size_set(sl, 100);
+   evas_object_size_hint_align_set(sl, 0.5, 0.5);
+   evas_object_size_hint_weight_set(sl, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+   elm_slider_indicator_format_set(sl, "%3.0f");
+   elm_slider_min_max_set(sl, 50, 150);
+   elm_slider_value_set(sl, 50);
+   elm_slider_inverted_set(sl, 1);
+   elm_box_pack_end(bx2, sl);
+   evas_object_show(sl);
+
+   elm_box_pack_end(bx, bx2);
+   evas_object_show(bx2);
+
+   ck = elm_check_add(win);
+   elm_check_label_set(ck, "resize");
+   elm_check_state_set(ck, rotate_with_resize);
+   evas_object_smart_callback_add(ck, "changed", my_ck_38_resize, win);
+   evas_object_size_hint_weight_set(ck, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+   evas_object_size_hint_align_set(ck, 0.02, 0.99);
+   evas_object_show(ck); 
+   elm_box_pack_end(bx, ck);
+
+   bx2 = elm_box_add(win);
+   elm_box_horizontal_set(bx2, 1);
+   elm_box_homogenous_set(bx2, 1);
+   evas_object_size_hint_weight_set(bx2, EVAS_HINT_EXPAND, 0.0);
+   evas_object_size_hint_fill_set(bx2, EVAS_HINT_FILL, EVAS_HINT_FILL);
+
+   bt = elm_button_add(win);
+   elm_button_label_set(bt, "Rot 0");
+   evas_object_smart_callback_add(bt, "clicked", my_bt_38_rot_0, win);
+   evas_object_size_hint_fill_set(bt, EVAS_HINT_FILL, EVAS_HINT_FILL);
+   evas_object_size_hint_weight_set(bt, EVAS_HINT_EXPAND, 0.0);
+   elm_box_pack_end(bx2, bt);
+   evas_object_show(bt);
+
+   bt = elm_button_add(win);
+   elm_button_label_set(bt, "Rot 90");
+   evas_object_smart_callback_add(bt, "clicked", my_bt_38_rot_90, win);
+   evas_object_size_hint_fill_set(bt, EVAS_HINT_FILL, EVAS_HINT_FILL);
+   evas_object_size_hint_weight_set(bt, EVAS_HINT_EXPAND, 0.0);
+   elm_box_pack_end(bx2, bt);
+   evas_object_show(bt);
+
+   bt = elm_button_add(win);
+   elm_button_label_set(bt, "Rot 180");
+   evas_object_smart_callback_add(bt, "clicked", my_bt_38_rot_180, win);
+   evas_object_size_hint_fill_set(bt, EVAS_HINT_FILL, EVAS_HINT_FILL);
+   evas_object_size_hint_weight_set(bt, EVAS_HINT_EXPAND, 0.0);
+   elm_box_pack_end(bx2, bt);
+   evas_object_show(bt);
+
+   bt = elm_button_add(win);
+   elm_button_label_set(bt, "Rot 270");
+   evas_object_smart_callback_add(bt, "clicked", my_bt_38_rot_270, win);
+   evas_object_size_hint_fill_set(bt, EVAS_HINT_FILL, EVAS_HINT_FILL);
+   evas_object_size_hint_weight_set(bt, EVAS_HINT_EXPAND, 0.0);
+   elm_box_pack_end(bx2, bt);
+   evas_object_show(bt);
+
+   elm_box_pack_end(bx, bx2);
+   evas_object_show(bx2);
+
+   evas_object_resize(win, 320, 480);
    evas_object_show(win);
 }
 #endif
