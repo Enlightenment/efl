dbb9cf4df7e869231dbe2dd2850feff872ecef67
Vincent Torri <vincent.torri@gmail.com>
2010-03-28 08:36:55 +0000

    add guards around the inclusion of config.h
    SVN revision: 47527

--- legacy/embryo/src/bin/embryo_cc_scvars.c
+++ legacy/embryo/src/bin/embryo_cc_scvars.c
@@ -27,7 +27,10 @@
  * vim:ts=8:sw=3:sts=8:noexpandtab:cino=>5n-3f0^-2{2
  */
 
-#include <config.h>		/* for PATH_MAX */
+#ifdef HAVE_CONFIG_H
+# include <config.h>		/* for PATH_MAX */
+#endif
+
 #include "embryo_cc_sc.h"
 
 /*  global variables
