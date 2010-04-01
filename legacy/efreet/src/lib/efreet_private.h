3b546516c3b238d4a77bdbb2114856a218d18a7b
Sebastian Dransfeld <sd@tango.flipp.net>
2010-04-01 19:32:41 +0000

    efreet: fancy alloca include in common header
    SVN revision: 47664

--- legacy/efreet/src/lib/efreet_private.h
+++ legacy/efreet/src/lib/efreet_private.h
@@ -4,6 +4,24 @@
 
 #include <Eet.h>
 
+#ifdef HAVE_ALLOCA_H
+# include <alloca.h>
+#elif defined __GNUC__
+# define alloca __builtin_alloca
+#elif defined _AIX
+# define alloca __alloca
+#elif defined _MSC_VER
+# include <malloc.h>
+# define alloca _alloca
+#else
+# include <stddef.h>
+# ifdef  __cplusplus
+extern "C"
+# endif
+void *alloca (size_t);
+#endif
+
+
 /**
  * @file efreet_private.h
  * @brief Contains methods and defines that are private to the Efreet
