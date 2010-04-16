74d6e04becd18ebae72da04fdf41f3b54d033d3d
Vincent Torri <vincent.torri@gmail.com>
2010-04-16 06:24:49 +0000

    add Visual Studio 9 solution and project files
    SVN revision: 48048

---
+++ legacy/evas/win32/vs9/start.bat
@@ -0,0 +1,35 @@
+@echo off
+rem Warning: environment is set for Win32 platform.
+
+set PROJECT_TYPE=vs8
+
+pushd ..
+call set_env.bat
+popd
+
+if "%EXT_DIR%" == "" goto END
+
+rem Setup common Win32 environment variables
+
+set SolutionDirectory=%cd%
+set DebugOutputDirectory=%SolutionDirectory%\out
+set ReleaseOutputDirectory=%SolutionDirectory%\out
+set DebugLibraryDirectory=%SolutionDirectory%\out
+set ReleaseLibraryDirectory=%SolutionDirectory%\out
+set TemporaryDirectory=%SolutionDirectory%\temp
+
+set LIB=%DebugLibraryDirectory%;%ReleaseLibraryDirectory%;%LIB%
+
+rem Check for basic requirements
+if "%VS80COMNTOOLS%" == "" (
+	echo ERROR: Microsoft Visual Studio 2005 is not installed.
+	pause
+	goto END
+)
+
+rem Setting environment for using Microsoft Visual Studio 2005 x86 tools.
+call "%VS80COMNTOOLS%vsvars32.bat"
+
+evas.sln
+
+:END
