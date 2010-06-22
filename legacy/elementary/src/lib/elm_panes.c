f3b076fba2bafebc505f614d2d35eae8254a1ee2
Jonathan Atton <jonathan.atton@gmail.com>
2010-06-22 17:27:19 +0000

    remove useless calcul
    SVN revision: 49795

--- legacy/elementary/src/lib/elm_panes.c
+++ legacy/elementary/src/lib/elm_panes.c
@@ -62,9 +62,6 @@ _sizing_eval(Evas_Object *obj)
    Widget_Data *wd = elm_widget_data_get(obj);
    Evas_Coord minw = -1, minh = -1;
    if (!wd) return;
-   edje_object_size_min_calc(wd->panes, &minw, &minh);
-   evas_object_size_hint_min_set(obj, minw, minh);
-   evas_object_size_hint_max_set(obj, minw, minh);
 }
 
 static void
