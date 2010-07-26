dd2dc39bffdb2329502227d1fa97a38629744374
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2010-07-26 05:09:20 +0000

    this was supposed to go with the last commit...
    SVN revision: 50502

--- legacy/eina/src/include/eina_stringshare.h
+++ legacy/eina/src/include/eina_stringshare.h
@@ -68,6 +68,7 @@
 EAPI const char *eina_stringshare_add_length(const char *str, unsigned int slen) EINA_WARN_UNUSED_RESULT;
 EAPI const char *eina_stringshare_add(const char *str) EINA_WARN_UNUSED_RESULT;
 EAPI const char *eina_stringshare_printf(const char *fmt, ...) EINA_WARN_UNUSED_RESULT;
+EAPI const char *eina_stringshare_vprintf(const char *fmt, va_list args) EINA_WARN_UNUSED_RESULT;
 EAPI const char *eina_stringshare_nprintf(unsigned int len, const char *fmt, ...) EINA_WARN_UNUSED_RESULT;
 EAPI const char *eina_stringshare_ref(const char *str);
 EAPI void eina_stringshare_del(const char *str);
