32932b4135c7a06e1ef244aa66b67cad88a8df08
Vincent Torri <vincent.torri@gmail.com>
2010-08-17 05:44:11 +0000

    alloca() declaration
    SVN revision: 51240

--- legacy/eina/src/lib/eina_stringshare.c
+++ legacy/eina/src/lib/eina_stringshare.c
@@ -34,6 +34,24 @@
 #endif
 
 #define _GNU_SOURCE
+
+#ifdef HAVE_ALLOCA_H
+# include <alloca.h>
+#elif defined __GNUC__
+# define alloca __builtin_alloca
+#elif defined _AIX
+# define alloca __alloca
+#elif defined _MSC_VER
+# include <malloc.h>
+# define alloca _alloca
+#else
+# include <stddef.h>
+# ifdef  __cplusplus
+extern "C"
+# endif
+void *alloca (size_t);
+#endif
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
