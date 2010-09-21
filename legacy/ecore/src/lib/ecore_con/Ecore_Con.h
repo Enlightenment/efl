0929a7a9ce2a421b5e1ed3fdf15fe8c092f19712
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2010-09-21 03:14:46 +0000

    -EAPI int               ecore_con_url_url_set(Ecore_Con_Url *url_con, +EAPI Eina_Bool         ecore_con_url_url_set(Ecore_Con_Url *url_con, -EAPI int               ecore_con_url_httpauth_set(Ecore_Con_Url *url_con, +EAPI Eina_Bool         ecore_con_url_httpauth_set(Ecore_Con_Url *url_con, -EAPI int               ecore_con_url_send(Ecore_Con_Url *url_con, +EAPI Eina_Bool         ecore_con_url_send(Ecore_Con_Url *url_con, -EAPI int               ecore_con_url_ftp_upload(Ecore_Con_Url *url_con, +EAPI Eina_Bool         ecore_con_url_ftp_upload(Ecore_Con_Url *url_con,
    SVN revision: 52531

--- legacy/ecore/src/lib/ecore_con/Ecore_Con.h
+++ legacy/ecore/src/lib/ecore_con/Ecore_Con.h
@@ -408,15 +408,15 @@ EAPI void              ecore_con_url_additional_header_add(Ecore_Con_Url *url_co
                                                            const char *value);
 EAPI void              ecore_con_url_additional_headers_clear(Ecore_Con_Url *url_con);
 EAPI const Eina_List * ecore_con_url_response_headers_get(Ecore_Con_Url *url_con);
-EAPI int               ecore_con_url_url_set(Ecore_Con_Url *url_con,
+EAPI Eina_Bool         ecore_con_url_url_set(Ecore_Con_Url *url_con,
                                              const char *url);
 EAPI void              ecore_con_url_fd_set(Ecore_Con_Url *url_con, int fd);
 EAPI int               ecore_con_url_received_bytes_get(Ecore_Con_Url *url_con);
-EAPI int               ecore_con_url_httpauth_set(Ecore_Con_Url *url_con,
+EAPI Eina_Bool         ecore_con_url_httpauth_set(Ecore_Con_Url *url_con,
                                                   const char *username,
                                                   const char *password,
                                                   Eina_Bool safe);
-EAPI int               ecore_con_url_send(Ecore_Con_Url *url_con,
+EAPI Eina_Bool         ecore_con_url_send(Ecore_Con_Url *url_con,
                                           const void *data, size_t length,
                                           const char *content_type);
 EAPI void              ecore_con_url_time(Ecore_Con_Url *url_con,
@@ -427,7 +427,7 @@ EAPI Eina_Bool         ecore_con_lookup(const char *name,
                                         Ecore_Con_Dns_Cb done_cb,
                                         const void *data);
 
-EAPI int               ecore_con_url_ftp_upload(Ecore_Con_Url *url_con,
+EAPI Eina_Bool         ecore_con_url_ftp_upload(Ecore_Con_Url *url_con,
                                                 const char *filename,
                                                 const char *user,
                                                 const char *pass,
@@ -436,7 +436,7 @@ EAPI void              ecore_con_url_verbose_set(Ecore_Con_Url *url_con,
                                                  int verbose);
 EAPI void              ecore_con_url_ftp_use_epsv_set(Ecore_Con_Url *url_con,
                                                       int use_epsv);
-EAPI int               ecore_con_url_http_post_send(Ecore_Con_Url *url_con,
+EAPI Eina_Bool         ecore_con_url_http_post_send(Ecore_Con_Url *url_con,
                                                     void *curl_httppost);
 
 #ifdef __cplusplus
