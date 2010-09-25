42dba1a3ea40fc0b0ad60261171c8119c656f542
Vincent Torri <vincent.torri@gmail.com>
2010-09-25 06:10:18 +0000

    formatting
    SVN revision: 52726

--- legacy/ecore/src/lib/ecore_sdl/Ecore_Sdl.h
+++ legacy/ecore/src/lib/ecore_sdl/Ecore_Sdl.h
@@ -50,63 +50,63 @@ EAPI extern int ECORE_SDL_EVENT_EXPOSE;
 typedef struct _Ecore_Sdl_Event_Key_Down Ecore_Sdl_Event_Key_Down;
 struct _Ecore_Sdl_Event_Key_Down /** SDL Key Down event */
 {
-        const char      *keyname; /**< The name of the key that was pressed */
-        const char      *keycompose; /**< The UTF-8 string conversion if any */
-        unsigned int    time;
+   const char      *keyname; /**< The name of the key that was pressed */
+   const char      *keycompose; /**< The UTF-8 string conversion if any */
+   unsigned int    time;
 };
 
 typedef struct _Ecore_Sdl_Event_Key_Up Ecore_Sdl_Event_Key_Up;
 struct _Ecore_Sdl_Event_Key_Up /** SDL Key Up event */
 {
-        const char      *keyname; /**< The name of the key that was released */
-        const char      *keycompose; /**< The UTF-8 string conversion if any */
-        unsigned int    time;
+   const char      *keyname; /**< The name of the key that was released */
+   const char      *keycompose; /**< The UTF-8 string conversion if any */
+   unsigned int    time;
 };
 
 typedef struct _Ecore_Sdl_Event_Mouse_Button_Down Ecore_Sdl_Event_Mouse_Button_Down;
 struct _Ecore_Sdl_Event_Mouse_Button_Down /** SDL Mouse Down event */
 {
-	int             button; /**< Mouse button that was pressed (1 - 32) */
-	int             x; /**< Mouse co-ordinates when mouse button was pressed */
-	int             y; /**< Mouse co-ordinates when mouse button was pressed */
-	int             double_click : 1; /**< Set if click was a double click */
-	int             triple_click : 1; /**< Set if click was a triple click  */
-        unsigned int    time;
+   int             button; /**< Mouse button that was pressed (1 - 32) */
+   int             x; /**< Mouse co-ordinates when mouse button was pressed */
+   int             y; /**< Mouse co-ordinates when mouse button was pressed */
+   int             double_click : 1; /**< Set if click was a double click */
+   int             triple_click : 1; /**< Set if click was a triple click  */
+   unsigned int    time;
 };
 
 typedef struct _Ecore_Sdl_Event_Mouse_Button_Up Ecore_Sdl_Event_Mouse_Button_Up;
 struct _Ecore_Sdl_Event_Mouse_Button_Up /** SDL Mouse Up event */
 {
-	int             button; /**< Mouse button that was released (1 - 32) */
-	int             x; /**< Mouse co-ordinates when mouse button was raised */
-	int             y; /**< Mouse co-ordinates when mouse button was raised */
-	int             double_click : 1; /**< Set if click was a double click */
-	int             triple_click : 1; /**< Set if click was a triple click  */
-        unsigned int    time;
+   int             button; /**< Mouse button that was released (1 - 32) */
+   int             x; /**< Mouse co-ordinates when mouse button was raised */
+   int             y; /**< Mouse co-ordinates when mouse button was raised */
+   int             double_click : 1; /**< Set if click was a double click */
+   int             triple_click : 1; /**< Set if click was a triple click  */
+   unsigned int    time;
 };
 
 typedef struct _Ecore_Sdl_Event_Mouse_Move Ecore_Sdl_Event_Mouse_Move;
 struct _Ecore_Sdl_Event_Mouse_Move /** SDL Mouse Move event */ 
 {
-	int             x; /**< Mouse co-ordinates where the mouse cursor moved to */
-	int             y; /**< Mouse co-ordinates where the mouse cursor moved to */
-        unsigned int    time;
+   int             x; /**< Mouse co-ordinates where the mouse cursor moved to */
+   int             y; /**< Mouse co-ordinates where the mouse cursor moved to */
+   unsigned int    time;
 };
 
 typedef struct _Ecore_Sdl_Event_Mouse_Wheel Ecore_Sdl_Event_Mouse_Wheel;
 struct _Ecore_Sdl_Event_Mouse_Wheel /** SDL Mouse Wheel event */
 {
-	int             x,y;
-	int             direction; /* 0 = vertical, 1 = horizontal */
-	int             wheel; /* value 1 (left/up), -1 (right/down) */
-        unsigned int    time;
+   int             x,y;
+   int             direction; /* 0 = vertical, 1 = horizontal */
+   int             wheel; /* value 1 (left/up), -1 (right/down) */
+   unsigned int    time;
 };
 
 typedef struct _Ecore_Sdl_Event_Video_Resize Ecore_Sdl_Event_Video_Resize;
 struct _Ecore_Sdl_Event_Video_Resize
 {
-        int             w;
-        int             h;
+   int             w;
+   int             h;
 };
 
 EAPI int        ecore_sdl_init(const char *name);
