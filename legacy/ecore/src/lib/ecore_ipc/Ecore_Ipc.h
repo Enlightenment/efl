1f3091dbb3d182801597c05ad81ae321c18ba1e2
Vincent Torri <vincent.torri@gmail.com>
2010-09-25 06:03:38 +0000

    formatting
    SVN revision: 52725

--- legacy/ecore/src/lib/ecore_ipc/Ecore_Ipc.h
+++ legacy/ecore/src/lib/ecore_ipc/Ecore_Ipc.h
@@ -40,8 +40,8 @@ extern "C" {
 /**
  * Macros used for generic data packing
  */
-EAPI unsigned short _ecore_ipc_swap_16(unsigned short v);
-EAPI unsigned int _ecore_ipc_swap_32(unsigned int v);
+EAPI unsigned short     _ecore_ipc_swap_16(unsigned short v);
+EAPI unsigned int       _ecore_ipc_swap_32(unsigned int v);
 EAPI unsigned long long _ecore_ipc_swap_64(unsigned long long v);
 
 #ifdef WORDS_BIGENDIAN
@@ -216,108 +216,108 @@ EAPI unsigned long long _ecore_ipc_swap_64(unsigned long long v);
       } \
    return d
 
-   typedef enum _Ecore_Ipc_Type
-     {
-	ECORE_IPC_LOCAL_USER,
-	  ECORE_IPC_LOCAL_SYSTEM,
-	  ECORE_IPC_REMOTE_SYSTEM,
-          ECORE_IPC_USE_SSL = 16
-     } Ecore_Ipc_Type;
+typedef enum _Ecore_Ipc_Type
+{
+   ECORE_IPC_LOCAL_USER,
+   ECORE_IPC_LOCAL_SYSTEM,
+   ECORE_IPC_REMOTE_SYSTEM,
+   ECORE_IPC_USE_SSL = 16
+} Ecore_Ipc_Type;
    
-   typedef struct _Ecore_Ipc_Event_Client_Add  Ecore_Ipc_Event_Client_Add;
-   typedef struct _Ecore_Ipc_Event_Client_Del  Ecore_Ipc_Event_Client_Del;
-   typedef struct _Ecore_Ipc_Event_Server_Add  Ecore_Ipc_Event_Server_Add;
-   typedef struct _Ecore_Ipc_Event_Server_Del  Ecore_Ipc_Event_Server_Del;
-   typedef struct _Ecore_Ipc_Event_Client_Data Ecore_Ipc_Event_Client_Data;
-   typedef struct _Ecore_Ipc_Event_Server_Data Ecore_Ipc_Event_Server_Data;
+typedef struct _Ecore_Ipc_Event_Client_Add  Ecore_Ipc_Event_Client_Add;
+typedef struct _Ecore_Ipc_Event_Client_Del  Ecore_Ipc_Event_Client_Del;
+typedef struct _Ecore_Ipc_Event_Server_Add  Ecore_Ipc_Event_Server_Add;
+typedef struct _Ecore_Ipc_Event_Server_Del  Ecore_Ipc_Event_Server_Del;
+typedef struct _Ecore_Ipc_Event_Client_Data Ecore_Ipc_Event_Client_Data;
+typedef struct _Ecore_Ipc_Event_Server_Data Ecore_Ipc_Event_Server_Data;
+
+struct _Ecore_Ipc_Event_Client_Add
+{
+   Ecore_Ipc_Client *client;
+};
+
+struct _Ecore_Ipc_Event_Client_Del
+{
+   Ecore_Ipc_Client *client;
+};
+
+struct _Ecore_Ipc_Event_Server_Add
+{
+   Ecore_Ipc_Server *server;
+};
+
+struct _Ecore_Ipc_Event_Server_Del
+{
+   Ecore_Ipc_Server *server;
+};
    
-   struct _Ecore_Ipc_Event_Client_Add
-     {
-	Ecore_Ipc_Client *client;
-     };
-   
-   struct _Ecore_Ipc_Event_Client_Del
-     {
-	Ecore_Ipc_Client *client;
-     };
-   
-   struct _Ecore_Ipc_Event_Server_Add
-     {
-	Ecore_Ipc_Server *server;
-     };
-   
-   struct _Ecore_Ipc_Event_Server_Del
-     {
-	Ecore_Ipc_Server *server;
-     };
-   
-   struct _Ecore_Ipc_Event_Client_Data
-     {
-	Ecore_Ipc_Client *client;
-	/* FIXME: this needs to become an ipc message */
-	int               major;
-	int               minor;
-	int               ref;
-	int               ref_to;
-	int               response;
-	void             *data;
-	int               size;
-     };
-   
-   struct _Ecore_Ipc_Event_Server_Data
-     {
-	Ecore_Ipc_Server *server;
-	/* FIXME: this needs to become an ipc message */
-	int               major;
-	int               minor;
-	int               ref;
-	int               ref_to;
-	int               response;
-	void             *data;
-	int               size;
-     };
-   
-   EAPI extern int ECORE_IPC_EVENT_CLIENT_ADD;
-   EAPI extern int ECORE_IPC_EVENT_CLIENT_DEL;
-   EAPI extern int ECORE_IPC_EVENT_SERVER_ADD;
-   EAPI extern int ECORE_IPC_EVENT_SERVER_DEL;
-   EAPI extern int ECORE_IPC_EVENT_CLIENT_DATA;
-   EAPI extern int ECORE_IPC_EVENT_SERVER_DATA;
-   
-   EAPI int               ecore_ipc_init(void);
-   EAPI int               ecore_ipc_shutdown(void);
-   
-   /* FIXME: need to add protocol type parameter */
-   EAPI Ecore_Ipc_Server *ecore_ipc_server_add(Ecore_Ipc_Type type, const char *name, int port, const void *data);
-   
-   /* FIXME: need to add protocol type parameter */
-   EAPI Ecore_Ipc_Server *ecore_ipc_server_connect(Ecore_Ipc_Type type, char *name, int port, const void *data);
-   EAPI void             *ecore_ipc_server_del(Ecore_Ipc_Server *svr);
-   EAPI void             *ecore_ipc_server_data_get(Ecore_Ipc_Server *svr);
-   EAPI Eina_Bool         ecore_ipc_server_connected_get(Ecore_Ipc_Server *svr);
-   EAPI Eina_List        *ecore_ipc_server_clients_get(Ecore_Ipc_Server *svr);
+struct _Ecore_Ipc_Event_Client_Data
+{
+   Ecore_Ipc_Client *client;
    /* FIXME: this needs to become an ipc message */
-   EAPI int               ecore_ipc_server_send(Ecore_Ipc_Server *svr, int major, int minor, int ref, int ref_to, int response, const void *data, int size);
-   EAPI void              ecore_ipc_server_client_limit_set(Ecore_Ipc_Server *svr, int client_limit, char reject_excess_clients);
-   EAPI void              ecore_ipc_server_data_size_max_set(Ecore_Ipc_Server *srv, int size);
-   EAPI int               ecore_ipc_server_data_size_max_get(Ecore_Ipc_Server *srv);
-   EAPI const char       *ecore_ipc_server_ip_get(Ecore_Ipc_Server *svr);
-   EAPI void              ecore_ipc_server_flush(Ecore_Ipc_Server *svr);
-       
-   /* FIXME: this needs to become an ipc message */
-   EAPI int               ecore_ipc_client_send(Ecore_Ipc_Client *cl, int major, int minor, int ref, int ref_to, int response, const void *data, int size);
-   EAPI Ecore_Ipc_Server *ecore_ipc_client_server_get(Ecore_Ipc_Client *cl);
-   EAPI void             *ecore_ipc_client_del(Ecore_Ipc_Client *cl);
-   EAPI void              ecore_ipc_client_data_set(Ecore_Ipc_Client *cl, const void *data);
-   EAPI void             *ecore_ipc_client_data_get(Ecore_Ipc_Client *cl);
-   EAPI void              ecore_ipc_client_data_size_max_set(Ecore_Ipc_Client *cl, int size);
-   EAPI int               ecore_ipc_client_data_size_max_get(Ecore_Ipc_Client *cl);
-   EAPI const char       *ecore_ipc_client_ip_get(Ecore_Ipc_Client *cl);
-   EAPI void              ecore_ipc_client_flush(Ecore_Ipc_Client *cl);
+   int               major;
+   int               minor;
+   int               ref;
+   int               ref_to;
+   int               response;
+   void             *data;
+   int               size;
+};
    
-   EAPI int               ecore_ipc_ssl_available_get(void);
-   /* FIXME: need to add a callback to "ok" large ipc messages greater than */
-   /*        a certain size (seurity/DOS attack safety) */
+struct _Ecore_Ipc_Event_Server_Data
+{
+   Ecore_Ipc_Server *server;
+   /* FIXME: this needs to become an ipc message */
+   int               major;
+   int               minor;
+   int               ref;
+   int               ref_to;
+   int               response;
+   void             *data;
+   int               size;
+};
+   
+EAPI extern int ECORE_IPC_EVENT_CLIENT_ADD;
+EAPI extern int ECORE_IPC_EVENT_CLIENT_DEL;
+EAPI extern int ECORE_IPC_EVENT_SERVER_ADD;
+EAPI extern int ECORE_IPC_EVENT_SERVER_DEL;
+EAPI extern int ECORE_IPC_EVENT_CLIENT_DATA;
+EAPI extern int ECORE_IPC_EVENT_SERVER_DATA;
+
+EAPI int               ecore_ipc_init(void);
+EAPI int               ecore_ipc_shutdown(void);
+
+/* FIXME: need to add protocol type parameter */
+EAPI Ecore_Ipc_Server *ecore_ipc_server_add(Ecore_Ipc_Type type, const char *name, int port, const void *data);
+
+/* FIXME: need to add protocol type parameter */
+EAPI Ecore_Ipc_Server *ecore_ipc_server_connect(Ecore_Ipc_Type type, char *name, int port, const void *data);
+EAPI void             *ecore_ipc_server_del(Ecore_Ipc_Server *svr);
+EAPI void             *ecore_ipc_server_data_get(Ecore_Ipc_Server *svr);
+EAPI Eina_Bool         ecore_ipc_server_connected_get(Ecore_Ipc_Server *svr);
+EAPI Eina_List        *ecore_ipc_server_clients_get(Ecore_Ipc_Server *svr);
+/* FIXME: this needs to become an ipc message */
+EAPI int               ecore_ipc_server_send(Ecore_Ipc_Server *svr, int major, int minor, int ref, int ref_to, int response, const void *data, int size);
+EAPI void              ecore_ipc_server_client_limit_set(Ecore_Ipc_Server *svr, int client_limit, char reject_excess_clients);
+EAPI void              ecore_ipc_server_data_size_max_set(Ecore_Ipc_Server *srv, int size);
+EAPI int               ecore_ipc_server_data_size_max_get(Ecore_Ipc_Server *srv);
+EAPI const char       *ecore_ipc_server_ip_get(Ecore_Ipc_Server *svr);
+EAPI void              ecore_ipc_server_flush(Ecore_Ipc_Server *svr);
+    
+/* FIXME: this needs to become an ipc message */
+EAPI int               ecore_ipc_client_send(Ecore_Ipc_Client *cl, int major, int minor, int ref, int ref_to, int response, const void *data, int size);
+EAPI Ecore_Ipc_Server *ecore_ipc_client_server_get(Ecore_Ipc_Client *cl);
+EAPI void             *ecore_ipc_client_del(Ecore_Ipc_Client *cl);
+EAPI void              ecore_ipc_client_data_set(Ecore_Ipc_Client *cl, const void *data);
+EAPI void             *ecore_ipc_client_data_get(Ecore_Ipc_Client *cl);
+EAPI void              ecore_ipc_client_data_size_max_set(Ecore_Ipc_Client *cl, int size);
+EAPI int               ecore_ipc_client_data_size_max_get(Ecore_Ipc_Client *cl);
+EAPI const char       *ecore_ipc_client_ip_get(Ecore_Ipc_Client *cl);
+EAPI void              ecore_ipc_client_flush(Ecore_Ipc_Client *cl);
+
+EAPI int               ecore_ipc_ssl_available_get(void);
+/* FIXME: need to add a callback to "ok" large ipc messages greater than */
+/*        a certain size (seurity/DOS attack safety) */
    
 #ifdef __cplusplus
 }
