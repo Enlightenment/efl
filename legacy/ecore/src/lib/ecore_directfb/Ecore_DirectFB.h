2de55203a42f6677d1851f9c6aa76ed847f37d7a
Vincent Torri <vincent.torri@gmail.com>
2010-09-25 05:08:30 +0000

    directfb does not exists on Windows, so simplify EAPI
    SVN revision: 52718

--- legacy/ecore/src/lib/ecore_directfb/Ecore_DirectFB.h
+++ legacy/ecore/src/lib/ecore_directfb/Ecore_DirectFB.h
@@ -7,23 +7,15 @@
 # undef EAPI
 #endif /* ifdef EAPI */
 
-#ifdef _MSC_VER
-# ifdef BUILDING_DLL
-#  define EAPI __declspec(dllexport)
-# else /* ifdef BUILDING_DLL */
-#  define EAPI __declspec(dllimport)
-# endif /* ifdef BUILDING_DLL */
-#else /* ifdef _MSC_VER */
-# ifdef __GNUC__
-#  if __GNUC__ >= 4
-#   define EAPI __attribute__ ((visibility("default")))
-#  else /* if __GNUC__ >= 4 */
-#   define EAPI
-#  endif /* if __GNUC__ >= 4 */
-# else /* ifdef __GNUC__ */
+#ifdef __GNUC__
+# if __GNUC__ >= 4
+#  define EAPI __attribute__ ((visibility("default")))
+# else /* if __GNUC__ >= 4 */
 #  define EAPI
-# endif /* ifdef __GNUC__ */
-#endif /* ifdef _MSC_VER */
+# endif /* if __GNUC__ >= 4 */
+#else /* ifdef __GNUC__ */
+# define EAPI
+#endif /* ifdef __GNUC__ */
 
 #ifdef __cplusplus
 extern "C" {
