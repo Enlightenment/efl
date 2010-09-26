ec9741575fc8a8bfbdc1904bb45b22cedd343a2a
Gustavo Sverzut Barbieri <barbieri@gmail.com>
2010-09-26 23:19:15 +0000

    less yet faster and less error-prone code by using Widget_Data instead.
    instead of refetching and revalidating Widget_Data everytime, just
    handle it. Although this will save some instructions, the goal is to
    make code shorter and less error prone.
    
    
    
    SVN revision: 52790

--- legacy/elementary/src/lib/elm_layout.c
+++ legacy/elementary/src/lib/elm_layout.c
@@ -15,6 +15,7 @@ typedef struct _Subinfo Subinfo;
 
 struct _Widget_Data
 {
+   Evas_Object *obj;
    Evas_Object *lay;
    Eina_List *subs;
    Eina_Bool needs_size_calc:1;
@@ -29,7 +30,7 @@ struct _Subinfo
 static const char *widtype = NULL;
 static void _del_hook(Evas_Object *obj);
 static void _theme_hook(Evas_Object *obj);
-static void _sizing_eval(Evas_Object *obj);
+static void _sizing_eval(Widget_Data *wd);
 static void _changed_size_hints(void *data, Evas *e, Evas_Object *obj, void *event_info);
 static void _sub_del(void *data, Evas_Object *obj, void *event_info);
 
@@ -54,7 +55,7 @@ _theme_hook(Evas_Object *obj)
    if (!wd) return;
    edje_object_scale_set(wd->lay, elm_widget_scale_get(obj) *
                          _elm_config->scale);
-   _sizing_eval(obj);
+   _sizing_eval(wd);
 }
 
 static void
@@ -64,7 +65,7 @@ _changed_hook(Evas_Object *obj)
    if (!wd) return;
    if (wd->needs_size_calc)
      {
-	_sizing_eval(obj);
+	_sizing_eval(wd);
 	wd->needs_size_calc = 0;
      }
 }
@@ -112,24 +113,20 @@ _elm_layout_focus_cycle_hook(Evas_Object *obj, Elm_Focus_Direction dir, Eina_Boo
 }
 
 static void
-_sizing_eval(Evas_Object *obj)
+_sizing_eval(Widget_Data *wd)
 {
-   Widget_Data *wd = elm_widget_data_get(obj);
    Evas_Coord minw = -1, minh = -1;
-   if (!wd) return;
    edje_object_size_min_calc(wd->lay, &minw, &minh);
-   evas_object_size_hint_min_set(obj, minw, minh);
-   evas_object_size_hint_max_set(obj, -1, -1);
+   evas_object_size_hint_min_set(wd->obj, minw, minh);
+   evas_object_size_hint_max_set(wd->obj, -1, -1);
 }
 
 static void
-_request_sizing_eval(Evas_Object *obj)
+_request_sizing_eval(Widget_Data *wd)
 {
-   Widget_Data *wd = elm_widget_data_get(obj);
-   if (!wd) return;
    if (wd->needs_size_calc) return;
    wd->needs_size_calc = 1;
-   evas_object_smart_changed(obj);
+   evas_object_smart_changed(wd->obj);
 }
 
 static void
@@ -153,7 +150,7 @@ _sub_del(void *data __UNUSED__, Evas_Object *obj, void *event_info)
 	     evas_object_event_callback_del_full(sub,
                                             EVAS_CALLBACK_CHANGED_SIZE_HINTS,
                                             _changed_size_hints,
-                                            obj);
+                                            wd);
 	     wd->subs = eina_list_remove_list(wd->subs, l);
 	     eina_stringshare_del(si->swallow);
 	     free(si);
@@ -185,7 +182,7 @@ elm_layout_add(Evas_Object *parent)
 
    wd = ELM_NEW(Widget_Data);
    e = evas_object_evas_get(parent);
-   obj = elm_widget_add(e);
+   wd->obj = obj = elm_widget_add(e);
    ELM_SET_WIDTYPE(widtype, "layout");
    elm_widget_type_set(obj, "layout");
    elm_widget_sub_object_add(parent, obj);
@@ -199,11 +196,11 @@ elm_layout_add(Evas_Object *parent)
    wd->lay = edje_object_add(e);
    elm_widget_resize_object_set(obj, wd->lay);
    edje_object_signal_callback_add(wd->lay, "size,eval", "elm",
-                                   _signal_size_eval, obj);
+                                   _signal_size_eval, wd);
    
    evas_object_smart_callback_add(obj, "sub-object-del", _sub_del, obj);
 
-   _request_sizing_eval(obj);
+   _request_sizing_eval(wd);
    return obj;
 }
 
@@ -225,7 +222,7 @@ elm_layout_file_set(Evas_Object *obj, const char *file, const char *group)
    Widget_Data *wd = elm_widget_data_get(obj);
    if (!wd) return EINA_FALSE;
    Eina_Bool ret = edje_object_file_set(wd->lay, file, group);
-   if (ret) _request_sizing_eval(obj);
+   if (ret) _request_sizing_eval(wd);
    return ret;
 }
 
@@ -248,7 +245,7 @@ elm_layout_theme_set(Evas_Object *obj, const char *clas, const char *group, cons
    Widget_Data *wd = elm_widget_data_get(obj);
    if (!wd) return EINA_FALSE;
    Eina_Bool ret = _elm_theme_object_set(obj, wd->lay, clas, group, style);
-   if (ret) _request_sizing_eval(obj);
+   if (ret) _request_sizing_eval(wd);
    return ret;
 }
 
@@ -287,14 +284,14 @@ elm_layout_content_set(Evas_Object *obj, const char *swallow, Evas_Object *conte
 	elm_widget_sub_object_add(obj, content);
 	evas_object_event_callback_add(content,
                                        EVAS_CALLBACK_CHANGED_SIZE_HINTS,
-				       _changed_size_hints, obj);
+				       _changed_size_hints, wd);
 	edje_object_part_swallow(wd->lay, swallow, content);
 	si = ELM_NEW(Subinfo);
 	si->swallow = eina_stringshare_add(swallow);
 	si->obj = content;
 	wd->subs = eina_list_append(wd->subs, si);
      }
-   _request_sizing_eval(obj);
+   _request_sizing_eval(wd);
 }
 
 /**
@@ -372,5 +369,7 @@ EAPI void
 elm_layout_sizing_eval(Evas_Object *obj)
 {
    ELM_CHECK_WIDTYPE(obj, widtype);
-   _request_sizing_eval(obj);
+   Widget_Data *wd = elm_widget_data_get(obj);
+   EINA_SAFETY_ON_NULL_RETURN(wd);
+   _request_sizing_eval(wd);
 }
