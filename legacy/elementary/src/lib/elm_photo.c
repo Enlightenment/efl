2b34e155b2f2441fbc8ab4af901208adfc7db37f
Christopher Michael <cpmichael1@comcast.net>
2010-10-12 03:53:03 +0000

    Add UNUSED where missing. Only calculate scale once here and reuse it. Fix formatting.
    SVN revision: 53286

--- legacy/elementary/src/lib/elm_photo.c
+++ legacy/elementary/src/lib/elm_photo.c
@@ -46,9 +46,11 @@ _theme_hook(Evas_Object *obj)
 {
    Widget_Data *wd = elm_widget_data_get(obj);
    if (!wd) return;
-   _elm_theme_object_set(obj, wd->frm, "photo", "base", elm_widget_style_get(obj));
+   _elm_theme_object_set(obj, wd->frm, "photo", "base", 
+                         elm_widget_style_get(obj));
    edje_object_part_swallow(wd->frm, "elm.swallow.content", wd->img);
-   edje_object_scale_set(wd->frm, elm_widget_scale_get(obj) * _elm_config->scale);
+   edje_object_scale_set(wd->frm, elm_widget_scale_get(obj) * 
+                         _elm_config->scale);
    _sizing_eval(obj);
 }
 
@@ -57,40 +59,45 @@ _sizing_eval(Evas_Object *obj)
 {
    Widget_Data *wd = elm_widget_data_get(obj);
    Evas_Coord minw = -1, minh = -1, maxw = -1, maxh = -1;
-   if (!wd) return;
 
-   if(wd->size > 0) {
-	   evas_object_size_hint_min_set(wd->img,
-			   wd->size * elm_widget_scale_get(obj) * _elm_config->scale,
-			   wd->size * elm_widget_scale_get(obj) * _elm_config->scale);
-	   edje_object_part_swallow(wd->frm, "elm.swallow.content", wd->img);
-	   elm_coords_finger_size_adjust(1, &minw, 1, &minh);
-	   edje_object_size_min_restricted_calc(wd->frm, &minw, &minh, minw, minh);
-	   elm_coords_finger_size_adjust(1, &minw, 1, &minh);
-	   maxw = minw;
-	   maxh = minh;
-	   evas_object_size_hint_min_set(obj, minw, minh);
-	   evas_object_size_hint_max_set(obj, maxw, maxh);
-   }
+   if (!wd) return;
+   if (wd->size > 0) 
+     {
+        double scale = 0.0;
+
+        scale = (wd->size * elm_widget_scale_get(obj) * _elm_config->scale);
+        evas_object_size_hint_min_set(wd->img, scale, scale);
+        edje_object_part_swallow(wd->frm, "elm.swallow.content", wd->img);
+        elm_coords_finger_size_adjust(1, &minw, 1, &minh);
+        edje_object_size_min_restricted_calc(wd->frm, &minw, &minh, minw, minh);
+        elm_coords_finger_size_adjust(1, &minw, 1, &minh);
+        maxw = minw;
+        maxh = minh;
+        evas_object_size_hint_min_set(obj, minw, minh);
+        evas_object_size_hint_max_set(obj, maxw, maxh);
+     }
 }
 
 static void
 _icon_move_resize(void *data, Evas *e __UNUSED__, Evas_Object *obj __UNUSED__, void *event_info __UNUSED__)
 {
-	Evas_Coord w, h;
-	Widget_Data *wd = elm_widget_data_get(data);
-	if (!wd) return;
+   Evas_Coord w, h;
+   Widget_Data *wd = elm_widget_data_get(data);
 
-	if(wd->fill) {
-		Evas_Object *icon = _els_smart_icon_object_get(wd->img);
-		evas_object_geometry_get(icon, NULL, NULL, &w, &h);
-		Edje_Message_Int_Set *msg = alloca(sizeof(Edje_Message_Int_Set) + (sizeof(int)));
-		msg->count=2;
-		msg->val[0] = (int)w;
-		msg->val[1] = (int)h;
+   if (!wd) return;
+   if (wd->fill) 
+     {
+        Edje_Message_Int_Set *msg;
+        Evas_Object *icon = _els_smart_icon_object_get(wd->img);
 
-		edje_object_message_send(wd->frm, EDJE_MESSAGE_INT_SET, 0, msg);
-	}
+        evas_object_geometry_get(icon, NULL, NULL, &w, &h);
+        msg = alloca(sizeof(Edje_Message_Int_Set) + (sizeof(int)));
+        msg->count=2;
+        msg->val[0] = (int)w;
+        msg->val[1] = (int)h;
+
+        edje_object_message_send(wd->frm, EDJE_MESSAGE_INT_SET, 0, msg);
+     }
 }
 
 
@@ -109,7 +116,7 @@ _longpress(void *objv)
    const char *file;
    char *buf;
 
-   printf("Long press: start drag!\n");
+   DBG("Long press: start drag!");
    wd->longtimer = NULL; /* clear: must return NULL now */
    evas_object_event_callback_del(objv, EVAS_CALLBACK_MOUSE_MOVE, _mouse_move);
 
@@ -156,16 +163,16 @@ _mouse_move(void *data, Evas *e __UNUSED__, Evas_Object *obj, void *event)
 }
 
 static void
-_mouse_down(void *data, Evas *e __UNUSED__, Evas_Object *obj __UNUSED__, void *event_info)
+_mouse_down(void *data, Evas *e __UNUSED__, Evas_Object *obj __UNUSED__, void *event_info __UNUSED__)
 {
    Widget_Data *wd = elm_widget_data_get(data);
 
    if (wd->longtimer) ecore_timer_del(wd->longtimer);
 
    /* FIXME: Hard coded timeout */
-   wd->longtimer = ecore_timer_add(0.7,_longpress, data);
-   evas_object_event_callback_add(obj,EVAS_CALLBACK_MOUSE_MOVE,
-                                  _mouse_move,data);
+   wd->longtimer = ecore_timer_add(0.7, _longpress, data);
+   evas_object_event_callback_add(obj, EVAS_CALLBACK_MOUSE_MOVE,
+                                  _mouse_move, data);
 }
 
 static void
@@ -221,7 +228,8 @@ elm_photo_add(Evas_Object *parent)
    _els_smart_icon_fill_inside_set(wd->img, 0);
    _els_smart_icon_scale_size_set(wd->img, 0);
    wd->fill = EINA_FALSE;
-   _els_smart_icon_scale_set(wd->img, elm_widget_scale_get(obj) * _elm_config->scale);
+   _els_smart_icon_scale_set(wd->img, 
+                             elm_widget_scale_get(obj) * _elm_config->scale);
    evas_object_event_callback_add(wd->img, EVAS_CALLBACK_MOUSE_UP,
 				  _mouse_up, obj);
    evas_object_event_callback_add(wd->img, EVAS_CALLBACK_MOUSE_DOWN,
@@ -279,8 +287,8 @@ elm_photo_size_set(Evas_Object *obj, int size)
 {
    ELM_CHECK_WIDTYPE(obj, widtype);
    Widget_Data *wd = elm_widget_data_get(obj);
-   if (!wd) return;
 
+   if (!wd) return;
    wd->size = (size > 0) ? size : 0;
 
    _els_smart_icon_scale_size_set(wd->img, wd->size);
@@ -301,6 +309,7 @@ elm_photo_fill_inside_set(Evas_Object *obj, Eina_Bool fill)
 {
    ELM_CHECK_WIDTYPE(obj, widtype);
    Widget_Data *wd = elm_widget_data_get(obj);
+
    if (!wd) return;
    _els_smart_icon_fill_inside_set(wd->img, fill);
    wd->fill = fill;
