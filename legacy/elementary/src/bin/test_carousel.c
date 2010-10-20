3970b9f9d3f80e7c6d40bc7614c5a62606d80ad3
Bruno Dilly <bdilly@profusion.mobi>
2010-10-20 17:41:03 +0000

    Add carousel to carousel test =P
    But it's not working anyway.
    
    
    
    SVN revision: 53684

--- legacy/elementary/src/bin/test_carousel.c
+++ legacy/elementary/src/bin/test_carousel.c
@@ -6,7 +6,7 @@
 void
 test_carousel(void *data __UNUSED__, Evas_Object *obj __UNUSED__, void *event_info __UNUSED__)
 {
-   Evas_Object *win, *bg;
+   Evas_Object *win, *bg, *car;
 
    win = elm_win_add(NULL, "carousel", ELM_WIN_BASIC);
    elm_win_title_set(win, "Carousel");
@@ -16,6 +16,15 @@ test_carousel(void *data __UNUSED__, Evas_Object *obj __UNUSED__, void *event_in
    elm_win_resize_object_add(win, bg);
    evas_object_size_hint_weight_set(bg, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
    evas_object_show(bg);
+
+   car = elm_carousel_add(win);
+   elm_win_resize_object_add(win, car);
+   evas_object_size_hint_weight_set(car, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+   elm_carousel_item_add(car, NULL, "Item 1", NULL, NULL);
+   elm_carousel_item_add(car, NULL, "Item 2", NULL, NULL);
+   elm_carousel_item_add(car, NULL, "Tinga", NULL, NULL);
+   elm_carousel_item_add(car, NULL, "Item 4", NULL, NULL);
+   evas_object_show(car);
 
    evas_object_resize(win, 320, 240);
    evas_object_show(win);
