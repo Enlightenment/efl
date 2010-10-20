75ca8388681470a944a92b3bb28325b65da18c4e
Gustavo Lima Chaves <glima@profusion.mobi>
2010-10-20 12:38:46 +0000

    Elm list external file needed love WRT to new/changed exported properties.
    SVN revision: 53662

--- legacy/elementary/src/edje_externals/elm_list.c
+++ legacy/elementary/src/edje_externals/elm_list.c
@@ -7,26 +7,29 @@ typedef struct _Elm_Params_List
    const char *policy_h;
    const char *policy_v;
    const char *mode;
+   Eina_Bool h_mode : 1;
+   Eina_Bool h_mode_exists : 1;
    Eina_Bool multi : 1;
    Eina_Bool multi_exists : 1;
    Eina_Bool always_select : 1;
    Eina_Bool always_select_exists : 1;
 } Elm_Params_List;
 
-#define CHOICE_GET(CHOICES, STR)		\
-  unsigned int i;				\
-  for (i = 0; i < sizeof(CHOICES); i++)		\
-    if (strcmp(STR, CHOICES[i]) == 0)		\
+#define CHOICE_GET(CHOICES, STR)                \
+  unsigned int i;                               \
+  for (i = 0; i < sizeof(CHOICES); i++)         \
+    if (strcmp(STR, CHOICES[i]) == 0)           \
       return i
 
 static const char *scroller_policy_choices[] = {"auto", "on", "off", NULL};
-static const char *list_mode_choices[] = {"compress", "scroll", "limit", NULL};
+static const char *list_mode_choices[] = {"compress", "scroll", "limit",
+                                          "expand", NULL};
 
 static Elm_Scroller_Policy
 _scroller_policy_choices_setting_get(const char *policy_str)
 {
    assert(sizeof(scroller_policy_choices)/
-	  sizeof(scroller_policy_choices[0]) == ELM_SCROLLER_POLICY_LAST + 1);
+          sizeof(scroller_policy_choices[0]) == ELM_SCROLLER_POLICY_LAST + 1);
    CHOICE_GET(scroller_policy_choices, policy_str);
    return ELM_SCROLLER_POLICY_LAST;
 }
@@ -40,7 +43,6 @@ _list_mode_setting_get(const char *mode_str)
    return ELM_LIST_LAST;
 }
 
-
 static void
 external_list_state_set(void *data __UNUSED__, Evas_Object *obj, const void *from_params, const void *to_params, float pos __UNUSED__)
 {
@@ -53,33 +55,35 @@ external_list_state_set(void *data __UNUSED__, Evas_Object *obj, const void *fro
 
    if (p->mode)
      {
-	Elm_List_Mode set = _list_mode_setting_get(p->mode);
+        Elm_List_Mode set = _list_mode_setting_get(p->mode);
 
-	if (set != ELM_LIST_LAST)
-	   elm_list_mode_set(obj, set);
+        if (set != ELM_LIST_LAST)
+          elm_list_mode_set(obj, set);
      }
 
    if ((p->policy_h) && (p->policy_v))
      {
-	policy_h = _scroller_policy_choices_setting_get(p->policy_h);
-	policy_v = _scroller_policy_choices_setting_get(p->policy_v);
-	elm_list_scroller_policy_set(obj, policy_h, policy_v);
+        policy_h = _scroller_policy_choices_setting_get(p->policy_h);
+        policy_v = _scroller_policy_choices_setting_get(p->policy_v);
+        elm_list_scroller_policy_set(obj, policy_h, policy_v);
      }
    else if ((p->policy_h) || (p->policy_v))
      {
-	elm_list_scroller_policy_get(obj, &policy_h, &policy_v);
-	if (p->policy_h)
-	  {
-	     policy_h = _scroller_policy_choices_setting_get(p->policy_h);
-	     elm_list_scroller_policy_set(obj, policy_h, policy_v);
-	  }
-	else
-	  {
-	     policy_v = _scroller_policy_choices_setting_get(p->policy_v);
-	     elm_list_scroller_policy_set(obj, policy_h, policy_v);
-	  }
+        elm_list_scroller_policy_get(obj, &policy_h, &policy_v);
+        if (p->policy_h)
+          {
+             policy_h = _scroller_policy_choices_setting_get(p->policy_h);
+             elm_list_scroller_policy_set(obj, policy_h, policy_v);
+          }
+        else
+          {
+             policy_v = _scroller_policy_choices_setting_get(p->policy_v);
+             elm_list_scroller_policy_set(obj, policy_h, policy_v);
+          }
      }
 
+   if (p->h_mode_exists)
+     elm_list_horizontal_set(obj, p->h_mode);
    if (p->multi_exists)
      elm_list_multi_select_set(obj, p->multi);
    if (p->always_select_exists)
@@ -89,55 +93,63 @@ external_list_state_set(void *data __UNUSED__, Evas_Object *obj, const void *fro
 static Eina_Bool
 external_list_param_set(void *data __UNUSED__, Evas_Object *obj, const Edje_External_Param *param)
 {
-   if (!strcmp(param->name, "horizontal mode"))
+   if (!strcmp(param->name, "list mode"))
      {
-	if (param->type == EDJE_EXTERNAL_PARAM_TYPE_CHOICE)
-	  {
-	     Elm_List_Mode set = _list_mode_setting_get(param->s);
-	     if (set == ELM_LIST_LAST) return EINA_FALSE;
-	     elm_list_mode_set(obj, set);
-	     return EINA_TRUE;
-	  }
+        if (param->type == EDJE_EXTERNAL_PARAM_TYPE_CHOICE)
+          {
+             Elm_List_Mode set = _list_mode_setting_get(param->s);
+             if (set == ELM_LIST_LAST) return EINA_FALSE;
+             elm_list_mode_set(obj, set);
+             return EINA_TRUE;
+          }
      }
-   else if (!strcmp(param->name, "scroll horizontal"))
+   else if (!strcmp(param->name, "horizontal scroll"))
      {
-	if (param->type == EDJE_EXTERNAL_PARAM_TYPE_CHOICE)
-	  {
-	     Elm_Scroller_Policy h, v;
-	     elm_list_scroller_policy_get(obj, &h, &v);
-	     h = _scroller_policy_choices_setting_get(param->s);
-	     if (h == ELM_SCROLLER_POLICY_LAST) return EINA_FALSE;
-	     elm_list_scroller_policy_set(obj, h, v);
-	     return EINA_TRUE;
-	  }
+        if (param->type == EDJE_EXTERNAL_PARAM_TYPE_CHOICE)
+          {
+             Elm_Scroller_Policy h, v;
+             elm_list_scroller_policy_get(obj, &h, &v);
+             h = _scroller_policy_choices_setting_get(param->s);
+             if (h == ELM_SCROLLER_POLICY_LAST) return EINA_FALSE;
+             elm_list_scroller_policy_set(obj, h, v);
+             return EINA_TRUE;
+          }
      }
-   else if (!strcmp(param->name, "scroll vertical"))
+   else if (!strcmp(param->name, "vertical scroll"))
      {
-	if (param->type == EDJE_EXTERNAL_PARAM_TYPE_CHOICE)
-	  {
-	     Elm_Scroller_Policy h, v;
-	     elm_list_scroller_policy_get(obj, &h, &v);
-	     v = _scroller_policy_choices_setting_get(param->s);
-	     if (v == ELM_SCROLLER_POLICY_LAST) return EINA_FALSE;
-	     elm_list_scroller_policy_set(obj, h, v);
-	     return EINA_TRUE;
-	  }
+        if (param->type == EDJE_EXTERNAL_PARAM_TYPE_CHOICE)
+          {
+             Elm_Scroller_Policy h, v;
+             elm_list_scroller_policy_get(obj, &h, &v);
+             v = _scroller_policy_choices_setting_get(param->s);
+             if (v == ELM_SCROLLER_POLICY_LAST) return EINA_FALSE;
+             elm_list_scroller_policy_set(obj, h, v);
+             return EINA_TRUE;
+          }
      }
-   else  if (!strcmp(param->name, "multi"))
+   else if (!strcmp(param->name, "horizontal mode"))
      {
-	if (param->type == EDJE_EXTERNAL_PARAM_TYPE_BOOL)
-	  {
-	     elm_list_multi_select_set(obj, param->i);
-	     return EINA_TRUE;
-	  }
+        if (param->type == EDJE_EXTERNAL_PARAM_TYPE_BOOL)
+          {
+             elm_list_horizontal_set(obj, param->i);
+             return EINA_TRUE;
+          }
      }
-   else if (!strcmp(param->name, "always_select"))
+   else if (!strcmp(param->name, "multi-select mode"))
      {
-	if (param->type == EDJE_EXTERNAL_PARAM_TYPE_BOOL)
-	  {
-	     elm_list_always_select_mode_set(obj, param->i);
-	     return EINA_TRUE;
-	  }
+        if (param->type == EDJE_EXTERNAL_PARAM_TYPE_BOOL)
+          {
+             elm_list_multi_select_set(obj, param->i);
+             return EINA_TRUE;
+          }
+     }
+   else if (!strcmp(param->name, "always-select mode"))
+     {
+        if (param->type == EDJE_EXTERNAL_PARAM_TYPE_BOOL)
+          {
+             elm_list_always_select_mode_set(obj, param->i);
+             return EINA_TRUE;
+          }
      }
 
    ERR("unknown parameter '%s' of type '%s'",
@@ -149,56 +161,64 @@ external_list_param_set(void *data __UNUSED__, Evas_Object *obj, const Edje_Exte
 static Eina_Bool
 external_list_param_get(void *data __UNUSED__, const Evas_Object *obj, Edje_External_Param *param)
 {
-   if (!strcmp(param->name, "multi"))
+   if (!strcmp(param->name, "horizontal mode"))
      {
-	if (param->type == EDJE_EXTERNAL_PARAM_TYPE_BOOL)
-	  {
-	     param->i = elm_list_multi_select_get(obj);
-	     return EINA_TRUE;
-	  }
+        if (param->type == EDJE_EXTERNAL_PARAM_TYPE_BOOL)
+          {
+             param->i = elm_list_horizontal_get(obj);
+             return EINA_TRUE;
+          }
      }
-   else if (!strcmp(param->name, "always_select"))
+   else if (!strcmp(param->name, "multi-select mode"))
      {
-	if (param->type == EDJE_EXTERNAL_PARAM_TYPE_BOOL)
-	  {
-	     param->i = elm_list_always_select_mode_get(obj);
-	     return EINA_TRUE;
-	  }
+        if (param->type == EDJE_EXTERNAL_PARAM_TYPE_BOOL)
+          {
+             param->i = elm_list_multi_select_get(obj);
+             return EINA_TRUE;
+          }
      }
-   else if (!strcmp(param->name, "scroll horizontal"))
+   else if (!strcmp(param->name, "always-select mode"))
      {
-	if (param->type == EDJE_EXTERNAL_PARAM_TYPE_CHOICE)
-	  {
-	     Elm_Scroller_Policy h, v;
-	     elm_list_scroller_policy_get(obj, &h, &v);
+        if (param->type == EDJE_EXTERNAL_PARAM_TYPE_BOOL)
+          {
+             param->i = elm_list_always_select_mode_get(obj);
+             return EINA_TRUE;
+          }
+     }
+   else if (!strcmp(param->name, "horizontal scroll"))
+     {
+        if (param->type == EDJE_EXTERNAL_PARAM_TYPE_CHOICE)
+          {
+             Elm_Scroller_Policy h, v;
+             elm_list_scroller_policy_get(obj, &h, &v);
 
-	     param->s = scroller_policy_choices[h];
-	     return EINA_TRUE;
-	  }
+             param->s = scroller_policy_choices[h];
+             return EINA_TRUE;
+          }
      }
-   else if (!strcmp(param->name, "scroll vertical"))
+   else if (!strcmp(param->name, "vertical scroll"))
      {
-	if (param->type == EDJE_EXTERNAL_PARAM_TYPE_CHOICE)
-	  {
-	     Elm_Scroller_Policy h, v;
-	     elm_list_scroller_policy_get(obj, &h, &v);
+        if (param->type == EDJE_EXTERNAL_PARAM_TYPE_CHOICE)
+          {
+             Elm_Scroller_Policy h, v;
+             elm_list_scroller_policy_get(obj, &h, &v);
 
-	     param->s = scroller_policy_choices[v];
-	     return EINA_TRUE;
-	  }
+             param->s = scroller_policy_choices[v];
+             return EINA_TRUE;
+          }
      }
    else if (!strcmp(param->name, "list mode"))
      {
-	if (param->type == EDJE_EXTERNAL_PARAM_TYPE_CHOICE)
-	  {
-	     Elm_List_Mode m = elm_list_mode_get(obj);
+        if (param->type == EDJE_EXTERNAL_PARAM_TYPE_CHOICE)
+          {
+             Elm_List_Mode m = elm_list_mode_get(obj);
 
-	     if (m == ELM_LIST_LAST)
-	       return EINA_FALSE;
+             if (m == ELM_LIST_LAST)
+               return EINA_FALSE;
 
-	     param->s = list_mode_choices[m];
-	     return EINA_TRUE;
-	  }
+             param->s = list_mode_choices[m];
+             return EINA_TRUE;
+          }
      }
 
    ERR("unknown parameter '%s' of type '%s'",
@@ -220,31 +240,35 @@ external_list_params_parse(void *data __UNUSED__, Evas_Object *obj __UNUSED__, c
 
    EINA_LIST_FOREACH(params, l, param)
      {
-	if (!strcmp(param->name, "multi"))
-	  {
-	     mem->multi = param->i;
-	     mem->multi_exists = EINA_TRUE;
-	  }
-	else if (!strcmp(param->name, "always_select"))
-	  {
-	     mem->always_select = param->i;
-	     mem->always_select_exists = EINA_TRUE;
-	  }
-	else if (!strcmp(param->name, "scroll horizontal"))
-	  mem->policy_h = eina_stringshare_add(param->s);
-	else if (!strcmp(param->name, "scroll vertical"))
-	  mem->policy_v = eina_stringshare_add(param->s);
-	else if (!strcmp(param->name, "list mode"))
-	  mem->mode = eina_stringshare_add(param->s);
+        if (!strcmp(param->name, "horizontal mode"))
+          {
+             mem->h_mode = param->i;
+             mem->h_mode_exists = EINA_TRUE;
+          }
+        else if (!strcmp(param->name, "multi-select mode"))
+          {
+             mem->multi = param->i;
+             mem->multi_exists = EINA_TRUE;
+          }
+        else if (!strcmp(param->name, "always-select mode"))
+          {
+             mem->always_select = param->i;
+             mem->always_select_exists = EINA_TRUE;
+          }
+        else if (!strcmp(param->name, "horizontal scroll"))
+          mem->policy_h = eina_stringshare_add(param->s);
+        else if (!strcmp(param->name, "vertical scroll"))
+          mem->policy_v = eina_stringshare_add(param->s);
+        else if (!strcmp(param->name, "list mode"))
+          mem->mode = eina_stringshare_add(param->s);
      }
    return mem;
 }
 
-static Evas_Object *external_list_content_get(void *data __UNUSED__,
-		const Evas_Object *obj __UNUSED__, const char *content __UNUSED__)
+static Evas_Object *external_list_content_get(void *data __UNUSED__, const Evas_Object *obj __UNUSED__, const char *content __UNUSED__)
 {
-	ERR("so content");
-	return NULL;
+   ERR("so content");
+   return NULL;
 }
 
 static void
@@ -265,12 +289,13 @@ external_list_params_free(void *params)
 static Edje_External_Param_Info external_list_params[] = {
   EDJE_EXTERNAL_PARAM_INFO_CHOICE_FULL("list mode", "scroll",
                                        list_mode_choices),
-  EDJE_EXTERNAL_PARAM_INFO_CHOICE_FULL("scroll horizontal", "auto",
-				       scroller_policy_choices),
-  EDJE_EXTERNAL_PARAM_INFO_CHOICE_FULL("scroll vertical", "auto",
-				       scroller_policy_choices),
-  EDJE_EXTERNAL_PARAM_INFO_BOOL("multi"),
-  EDJE_EXTERNAL_PARAM_INFO_BOOL("always_select"),
+  EDJE_EXTERNAL_PARAM_INFO_CHOICE_FULL("horizontal scroll", "auto",
+                                       scroller_policy_choices),
+  EDJE_EXTERNAL_PARAM_INFO_CHOICE_FULL("vertical scroll", "auto",
+                                       scroller_policy_choices),
+  EDJE_EXTERNAL_PARAM_INFO_BOOL("horizontal mode"),
+  EDJE_EXTERNAL_PARAM_INFO_BOOL("multi-select mode"),
+  EDJE_EXTERNAL_PARAM_INFO_BOOL("always-select mode"),
   EDJE_EXTERNAL_PARAM_INFO_SENTINEL
 };
 
