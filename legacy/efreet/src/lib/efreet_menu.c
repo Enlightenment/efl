42f57e9fc35e12248328828c826d5a8fa561935f
Vincent Torri <vincent.torri@gmail.com>
2010-11-25 23:48:59 +0000

    put alloca declaration after config.h
    SVN revision: 55001

--- legacy/efreet/src/lib/efreet_menu.c
+++ legacy/efreet/src/lib/efreet_menu.c
@@ -4,6 +4,24 @@
 # include <config.h>
 #endif
 
+#undef alloca
+#ifdef HAVE_ALLOCA_H
+# include <alloca.h>
+#elif defined __GNUC__
+# define alloca __builtin_alloca
+#elif defined _AIX
+# define alloca __alloca
+#elif defined _MSC_VER
+# include <malloc.h>
+# define alloca _alloca
+#else
+# include <stddef.h>
+# ifdef  __cplusplus
+extern "C"
+# endif
+void *alloca (size_t);
+#endif
+
 #include <stdio.h>
 #include <string.h>
 #include <sys/types.h>
--- legacy/efreet/src/lib/efreet_private.h
+++ legacy/efreet/src/lib/efreet_private.h
@@ -5,24 +5,6 @@
 #include <Ecore.h>
 #include <Ecore_File.h>
 
-#undef alloca
-#ifdef HAVE_ALLOCA_H
-# include <alloca.h>
-#elif defined __GNUC__
-# define alloca __builtin_alloca
-#elif defined _AIX
-# define alloca __alloca
-#elif defined _MSC_VER
-# include <malloc.h>
-# define alloca _alloca
-#else
-# include <stddef.h>
-# ifdef  __cplusplus
-extern "C"
-# endif
-void *alloca (size_t);
-#endif
-
 
 /**
  * @file efreet_private.h
