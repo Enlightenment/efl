652f7487609ec0507cca4e20aeb0587f39f6eb0b
Sebastian Dransfeld <sd@tango.flipp.net>
2010-12-03 14:53:21 +0000

    more decimals
    SVN revision: 55195

--- legacy/efreet/src/tests/ef_cache.c
+++ legacy/efreet/src/tests/ef_cache.c
@@ -23,45 +23,45 @@ check(void)
     id = efreet_util_path_to_file_id("/usr/share/applications/gnome-panel.desktop");
     if (id)
     {
-        printf("efreet_util_path_to_file_id(/usr/share/applications/gnome-panel.desktop): %s %.3f\n", id, (ecore_time_get() - start));
+        printf("efreet_util_path_to_file_id(/usr/share/applications/gnome-panel.desktop): %s %.6f\n", id, (ecore_time_get() - start));
     }
     else
-        printf("efreet_util_path_to_file_id(/usr/share/applications/gnome-panel.desktop): NULL %.3f\n", (ecore_time_get() - start));
+        printf("efreet_util_path_to_file_id(/usr/share/applications/gnome-panel.desktop): NULL %.6f\n", (ecore_time_get() - start));
 
     //EAPI Efreet_Desktop *efreet_util_desktop_name_find(const char *name);
     start = ecore_time_get();
     desktop = efreet_util_desktop_name_find("Evolution");
     if (desktop)
-        printf("efreet_util_desktop_name_find(Evolution): %s %.3f\n", desktop->orig_path, (ecore_time_get() - start));
+        printf("efreet_util_desktop_name_find(Evolution): %s %.6f\n", desktop->orig_path, (ecore_time_get() - start));
     else
-        printf("efreet_util_desktop_name_find(Evolution): NULL %.3f\n", (ecore_time_get() - start));
+        printf("efreet_util_desktop_name_find(Evolution): NULL %.6f\n", (ecore_time_get() - start));
     efreet_desktop_free(desktop);
 
     //EAPI Efreet_Desktop *efreet_util_desktop_generic_name_find(const char *generic_name);
     start = ecore_time_get();
     desktop = efreet_util_desktop_generic_name_find("Spreadsheet");
     if (desktop)
-        printf("efreet_util_desktop_generic_name_find(Spreadsheet): %s %.3f\n", desktop->orig_path, (ecore_time_get() - start));
+        printf("efreet_util_desktop_generic_name_find(Spreadsheet): %s %.6f\n", desktop->orig_path, (ecore_time_get() - start));
     else
-        printf("efreet_util_desktop_generic_name_find(Spreadsheet): NULL %.3f\n", (ecore_time_get() - start));
+        printf("efreet_util_desktop_generic_name_find(Spreadsheet): NULL %.6f\n", (ecore_time_get() - start));
     efreet_desktop_free(desktop);
 
     //EAPI Efreet_Desktop *efreet_util_desktop_wm_class_find(const char *wmname, const char *wmclass);
     start = ecore_time_get();
     desktop = efreet_util_desktop_wm_class_find("Firefox", NULL);
     if (desktop)
-        printf("efreet_util_desktop_wm_class_find(Firefox): %s %.3f\n", desktop->orig_path, (ecore_time_get() - start));
+        printf("efreet_util_desktop_wm_class_find(Firefox): %s %.6f\n", desktop->orig_path, (ecore_time_get() - start));
     else
-        printf("efreet_util_desktop_wm_class_find(Firefox): NULL %.3f\n", (ecore_time_get() - start));
+        printf("efreet_util_desktop_wm_class_find(Firefox): NULL %.6f\n", (ecore_time_get() - start));
     efreet_desktop_free(desktop);
 
     //EAPI Efreet_Desktop *efreet_util_desktop_exec_find(const char *exec);
     start = ecore_time_get();
     desktop = efreet_util_desktop_exec_find("/usr/bin/update-manager");
     if (desktop)
-        printf("efreet_util_desktop_exec_find(update-manager): %s %.3f\n", desktop->orig_path, (ecore_time_get() - start));
+        printf("efreet_util_desktop_exec_find(update-manager): %s %.6f\n", desktop->orig_path, (ecore_time_get() - start));
     else
-        printf("efreet_util_desktop_exec_find(update-manager): NULL %.3f\n", (ecore_time_get() - start));
+        printf("efreet_util_desktop_exec_find(update-manager): NULL %.6f\n", (ecore_time_get() - start));
     efreet_desktop_free(desktop);
 
     //EAPI Eina_List *efreet_util_desktop_name_glob_list(const char *glob);
@@ -75,7 +75,7 @@ check(void)
             efreet_desktop_free(desktop);
         }
     }
-    printf("time: %.3f\n", (ecore_time_get() - start));
+    printf("time: %.6f\n", (ecore_time_get() - start));
 
     //EAPI Eina_List *efreet_util_desktop_mime_list(const char *mime);
     start = ecore_time_get();
@@ -88,7 +88,7 @@ check(void)
             efreet_desktop_free(desktop);
         }
     }
-    printf("time: %.3f\n", (ecore_time_get() - start));
+    printf("time: %.6f\n", (ecore_time_get() - start));
 
     //EAPI Eina_List *efreet_util_desktop_exec_glob_list(const char *glob);
     start = ecore_time_get();
@@ -101,7 +101,7 @@ check(void)
             efreet_desktop_free(desktop);
         }
     }
-    printf("time: %.3f\n", (ecore_time_get() - start));
+    printf("time: %.6f\n", (ecore_time_get() - start));
 
     //EAPI Eina_List *efreet_util_desktop_categories_list(void);
     start = ecore_time_get();
@@ -113,7 +113,7 @@ check(void)
             printf("efreet_util_desktop_categories_list(): %s\n", id);
         }
     }
-    printf("time: %.3f\n", (ecore_time_get() - start));
+    printf("time: %.6f\n", (ecore_time_get() - start));
 
     //EAPI Eina_List *efreet_util_desktop_category_list(const char *category);
     start = ecore_time_get();
@@ -126,7 +126,7 @@ check(void)
             efreet_desktop_free(desktop);
         }
     }
-    printf("time: %.3f\n", (ecore_time_get() - start));
+    printf("time: %.6f\n", (ecore_time_get() - start));
 
     desktop = efreet_desktop_get("/opt/google/chrome/google-chrome.desktop");
     if (desktop) efreet_desktop_free(desktop);
