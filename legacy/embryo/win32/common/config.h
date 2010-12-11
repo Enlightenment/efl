3a7c89cdf7a03637b1cb012ed2098a56bfb2e29a
Vincent Torri <vincent.torri@gmail.com>
2010-12-11 07:52:08 +0000

    add config.h for Windows, to define VMAJ and al.
    SVN revision: 55489

---
+++ legacy/embryo/win32/common/config.h
@@ -0,0 +1,28 @@
+/* EINA - EFL data type library
+ * Copyright (C) 2008 Cedric Bail
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library;
+ * if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef EMBRYO_CONFIG_H_
+#define EMBRYO_CONFIG_H_
+
+
+#define VMAJ 1
+#define VMIN 0
+#define VMIC 0
+#define VREV 0
+
+#endif /* EMBRYO_CONFIG_H_ */
