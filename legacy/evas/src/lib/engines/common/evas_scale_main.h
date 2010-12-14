bd3ae0dc9d5126eb8c763059890d7fe640580c43
Carsten Haitzler <raster@rasterman.com>
2010-12-14 03:09:01 +0000

    fix warning.
    SVN revision: 55542

--- legacy/evas/src/lib/engines/common/evas_scale_main.h
+++ legacy/evas/src/lib/engines/common/evas_scale_main.h
@@ -8,5 +8,6 @@ EAPI void evas_common_scale_rgba_in_to_out_clip_smooth      (RGBA_Image *src, RG
 
 EAPI void evas_common_scale_rgba_in_to_out_clip_sample      (RGBA_Image *src, RGBA_Image *dst, RGBA_Draw_Context *dc, int src_region_x, int src_region_y, int src_region_w, int src_region_h, int dst_region_x, int dst_region_y, int dst_region_w, int dst_region_h);
 
+EAPI void evas_common_rgba_image_scalecache_dump(void);
 
 #endif /* _EVAS_SCALE_MAIN_H */
