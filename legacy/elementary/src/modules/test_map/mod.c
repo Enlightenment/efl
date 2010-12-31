1465ef7735dad7ca01838d9b7b8fdb2da0d6ec2c
Christopher Michael <cpmichael1@comcast.net>
2010-12-31 01:13:36 +0000

    Add UNUSED so we get a clean compile.
    SVN revision: 55786

--- legacy/elementary/src/modules/test_map/mod.c
+++ legacy/elementary/src/modules/test_map/mod.c
@@ -4,43 +4,43 @@
 #endif
 
 EAPI int 
-elm_modapi_init(void *m) 
+elm_modapi_init(void *m __UNUSED__) 
 {   
    return 1; // succeed always
 }
 
 EAPI int 
-elm_modapi_shutdown(void *m) 
+elm_modapi_shutdown(void *m __UNUSED__) 
 {
    return 1; // succeed always
 }
 
 EAPI Eina_Bool
-obj_hook(Evas_Object *obj)
+obj_hook(Evas_Object *obj __UNUSED__)
 {
    return EINA_TRUE;
 }
 
 EAPI Eina_Bool
-obj_unhook(Evas_Object *obj)
+obj_unhook(Evas_Object *obj __UNUSED__)
 { 
    return EINA_TRUE;
 }
 
 EAPI Eina_Bool 
-obj_convert_geo_into_coord(const Evas_Object *obj, int zoom, double lon, double lat, int size, int *x, int *y)
+obj_convert_geo_into_coord(const Evas_Object *obj __UNUSED__, int zoom __UNUSED__, double lon __UNUSED__, double lat __UNUSED__, int size __UNUSED__, int *x __UNUSED__, int *y __UNUSED__)
 {
    return EINA_FALSE;
 }
 
 EAPI Eina_Bool
-obj_convert_coord_into_geo(const Evas_Object *obj, int zoom, int x, int y, int size, double *lon, double *lat)
+obj_convert_coord_into_geo(const Evas_Object *obj __UNUSED__, int zoom __UNUSED__, int x __UNUSED__, int y __UNUSED__, int size __UNUSED__, double *lon __UNUSED__, double *lat __UNUSED__)
 {
    return EINA_FALSE;
 }
 
 EAPI char*
-obj_url_request(Evas_Object *obj, int x, int y, int zoom)
+obj_url_request(Evas_Object *obj __UNUSED__, int x, int y, int zoom)
 {
    char buf[PATH_MAX];
    snprintf(buf, sizeof(buf), "http://tile.openstreetmap.org/%d/%d/%d.png",
