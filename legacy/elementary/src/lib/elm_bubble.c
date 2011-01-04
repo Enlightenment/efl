f99cf1fa9462388c52f2f6f053832a4cbd7ce96f
Sohyun Kim <anna1014.kim@samsung.com>
2011-01-04 08:42:55 +0000

    From: Sohyun Kim <anna1014.kim@samsung.com> Subject: [E-devel] [Patch] elm_bubble theme patch
    I'm Sohyun Kim. Nice to meet you.
    
    This is a patch for bubble theme.
    
    The bubble has a region for icon, label and info text. 
    However, it has the region, if icon, label and info text are not set.
    I modified theme and added edje signals.
    
    1. add edje signal for label and info text state
    2. remove text size for label and info text default state
       -> set it on "visible" state
    3. add base image state for info text only
       
    
    
    SVN revision: 55839

--- legacy/elementary/src/lib/elm_bubble.c
+++ legacy/elementary/src/lib/elm_bubble.c
@@ -54,7 +54,11 @@ _theme_hook(Evas_Object *obj)
    _elm_theme_object_set(obj, wd->bbl, "bubble", wd->corner,
                          elm_widget_style_get(obj));
    edje_object_part_text_set(wd->bbl, "elm.text", wd->label);
+   if (wd->label) edje_object_signal_emit(wd->bbl, "elm,state,text,visible", "elm");
+   else edje_object_signal_emit(wd->bbl, "elm,state,text,hidden", "elm");
    edje_object_part_text_set(wd->bbl, "elm.info", wd->info);
+   if (wd->info) edje_object_signal_emit(wd->bbl, "elm,state,info,visible", "elm");
+   else edje_object_signal_emit(wd->bbl, "elm,state,info,hidden", "elm");
    if (wd->content)
      {
         edje_object_part_swallow(wd->bbl, "elm.swallow.content", wd->content);
@@ -197,6 +201,8 @@ elm_bubble_label_set(Evas_Object *obj, const char *label)
    if (!wd) return;
    eina_stringshare_replace(&wd->label, label);
    edje_object_part_text_set(wd->bbl, "elm.text", label);
+   if (label) edje_object_signal_emit(wd->bbl, "elm,state,text,visible", "elm");
+   else edje_object_signal_emit(wd->bbl, "elm,state,text,hidden", "elm");
    _sizing_eval(obj);
 }
 
@@ -241,6 +247,8 @@ elm_bubble_info_set(Evas_Object *obj, const char *info)
    if (!wd) return;
    eina_stringshare_replace(&wd->info, info);
    edje_object_part_text_set(wd->bbl, "elm.info", info);
+   if (info) edje_object_signal_emit(wd->bbl, "elm,state,info,visible", "elm");
+   else edje_object_signal_emit(wd->bbl, "elm,state,info,hidden", "elm");
    _sizing_eval(obj);
 }
 
