4897c19265f7cf847c79a6977f3c265049dd5b52
Bruno Dilly <bdilly@profusion.mobi>
2011-01-05 14:23:39 +0000

    Fix forgotten attribution of max year on elm calendar widget
    This bug was always setting the displayed year to min year
    when the function elm_calendar_min_max_set is called.
    
    
    
    SVN revision: 55881

--- legacy/elementary/src/lib/elm_calendar.c
+++ legacy/elementary/src/lib/elm_calendar.c
@@ -901,6 +901,7 @@ elm_calendar_min_max_year_set(Evas_Object *obj, int min, int max)
    max -= 1900;
    if ((wd->year_min == min) && (wd->year_max == max)) return;
    wd->year_min = min > 2 ? min : 2;
+   wd->year_max = max;
    if (wd->selected_time.tm_year > wd->year_max)
      wd->selected_time.tm_year = wd->year_max;
    if (wd->selected_time.tm_year < wd->year_min)
