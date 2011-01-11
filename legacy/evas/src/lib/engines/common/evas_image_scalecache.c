da9bf3353ff9df7c6e1a39d346bf070a1f42b7c9
Carsten Haitzler <raster@rasterman.com>
2011-01-11 08:12:32 +0000

    oooh oops. all calls to cache_prune already lock for it.. so deadlock! fix fix! sorry!
    SVN revision: 56043

--- legacy/evas/src/lib/engines/common/evas_image_scalecache.c
+++ legacy/evas/src/lib/engines/common/evas_image_scalecache.c
@@ -311,7 +311,6 @@ _cache_prune(Scaleitem *notsci, Eina_Bool copies_only)
 #endif
         if (sci->im)
           {
-             LKL(cache_lock);
              evas_common_rgba_image_free(&sci->im->cache_entry);
              sci->im = NULL;
              sci->usage = 0;
@@ -324,7 +323,6 @@ _cache_prune(Scaleitem *notsci, Eina_Bool copies_only)
 //             INF(" 2- %i", sci->dst_w * sci->dst_h * 4);
              cache_list = eina_inlist_remove(cache_list, (Eina_Inlist *)sci);
              memset(sci, 0, sizeof(Eina_Inlist));
-             LKU(cache_lock);
           }
 #ifdef EVAS_FRAME_QUEUING
         RWLKU(sci->lock);
