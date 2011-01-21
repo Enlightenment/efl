bdf92b1ed73df759f561f74915a47e0c1f6ea23e
ChunEon Park <chuneon.park@samsung.com>
2011-01-21 01:01:12 +0000

    Fix invalid pointer ref
    Patch by: ChunEon Park <chuneon.park@samsung.com>
    
    
    
    SVN revision: 56249

--- legacy/eet/src/lib/eet_lib.c
+++ legacy/eet/src/lib/eet_lib.c
@@ -1725,11 +1725,12 @@ EAPI Eet_Error
 eet_identity_set(Eet_File *ef,
                  Eet_Key  *key)
 {
-   Eet_Key *tmp = ef->key;
+   Eet_Key *tmp;
 
    if (!ef)
       return EET_ERROR_BAD_OBJECT;
 
+   tmp = ef->key;
    ef->key = key;
    eet_identity_ref(ef->key);
    eet_identity_unref(tmp);
