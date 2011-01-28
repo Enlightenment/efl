5459ecbcee3c564d51a9571776ee28dd8ff9e5b4
Carsten Haitzler <raster@rasterman.com>
2011-01-28 03:00:48 +0000

    fix documentation so vtorri can write a correct example :)
    SVN revision: 56340

--- legacy/ecore/src/lib/ecore_file/ecore_file_download.c
+++ legacy/ecore/src/lib/ecore_file/ecore_file_download.c
@@ -104,12 +104,13 @@ ecore_file_download_shutdown(void)
  * will be filled with the status of the download (200, 404,...). The
  * @p progress_cb is called during the download operation, each time a
  * packet is received or when CURL wants. It can be used to display the
- * percentage of the downloaded file. The only operations that can be
- * aborted are those with protocol 'http' or 'ftp'. In that case @p job_ret
- * can be filled. It can be used with ecore_file_download_abort() or
- * ecore_file_download_abort_all() to respectively abort one or all
- * download operations. This function returns EINA_TRUE if the
- * download starts, EINA_FALSE otherwise.
+ * percentage of the downloaded file. Return 0 from this callback if provided
+ * to continue the operation or anything else to abort the download. The only
+ * operations that can be aborted are those with protocol 'http' or 'ftp'. In 
+ * that case @p job_ret can be filled. It can be used with
+ * ecore_file_download_abort() or ecore_file_download_abort_all() to
+ * respectively abort one or all download operations. This function returns
+ * EINA_TRUE if the download starts, EINA_FALSE otherwise.
  */
 EAPI Eina_Bool
 ecore_file_download(const char *url,
