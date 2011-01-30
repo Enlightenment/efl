379eb8f660c7a1b8b218a10cbf8e6924a101a1a1
Tom Hacohen <tom@stosb.com>
2011-01-30 10:43:50 +0000

    Evas font-engine: Fixed ot face loading when loading fonts from memory.
    SVN revision: 56523

--- legacy/evas/src/lib/engines/common/evas_font_load.c
+++ legacy/evas/src/lib/engines/common/evas_font_load.c
@@ -138,6 +138,12 @@ evas_common_font_source_memory_load(const char *name, const void *data, int data
       free(fs);
       return NULL;
     }
+#ifdef OT_SUPPORT
+   if (evas_common_font_ot_is_enabled())
+     {
+        evas_common_font_ot_load_face(fs);
+     }
+#endif
    FTUNLOCK();
    fs->ft.orig_upem = fs->ft.face->units_per_EM;
    fs->references = 1;
