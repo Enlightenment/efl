04c2eda67716f441b9bb8b54541f0d1a24fb46e4
Tom Hacohen <tom@stosb.com>
2011-02-10 08:29:24 +0000

    Elementary ui-mirroring: move elm_widget_mirrored_* functions to elm_widget.h. and created elm_object_mirrored wrappers for them.
    SVN revision: 56893

--- legacy/elementary/src/lib/elm_main.c
+++ legacy/elementary/src/lib/elm_main.c
@@ -1059,6 +1059,72 @@ elm_policy_get(unsigned int policy)
 }
 
 /**
+ * @defgroup UI-Mirroring Selective Widget mirroring
+ *
+ * These functions allow you to set ui-miroring on specific widgets or whe
+ * whole interface. Widgets can be in one of two modes, automatic and manual.
+ * Automatic means they'll be changed according to the system mirroring mode
+ * and manual means only explicit changes will matter. You are not supposed to
+ * change mirroring state of a widget set to automatic, will mostly work, but
+ * the behavior is not really defined.
+ */
+
+/**
+ * Returns the widget's mirrored mode.
+ *
+ * @param obj The widget.
+ * @return mirrored mode of the object.
+ *
+ **/
+EAPI Eina_Bool
+elm_object_mirrored_get(const Evas_Object *obj)
+{
+   EINA_SAFETY_ON_NULL_RETURN_VAL(obj, EINA_FALSE);
+   return elm_widget_mirrored_get(obj);
+}
+
+/**
+ * Sets the widget's mirrored mode.
+ *
+ * @param obj The widget.
+ * @param mirrored EINA_TRUE to set mirrored mode. EINA_FALSE to unset.
+ */
+EAPI void
+elm_object_mirrored_set(Evas_Object *obj, Eina_Bool mirrored)
+{
+   EINA_SAFETY_ON_NULL_RETURN(obj);
+   elm_widget_mirrored_set(obj, mirrored);
+}
+
+/**
+ * Returns the widget's mirrored mode setting.
+ *
+ * @param obj The widget.
+ * @return mirrored mode setting of the object.
+ *
+ **/
+EAPI Eina_Bool
+elm_object_mirrored_automatic_get(const Evas_Object *obj)
+{
+   EINA_SAFETY_ON_NULL_RETURN_VAL(obj, EINA_FALSE);
+   return elm_widget_mirrored_automatic_get(obj);
+}
+
+/**
+ * Sets the widget's mirrored mode setting.
+ * When widget in automatic mode, it follows the system mirrored mode set by
+ * elm_mirrored_set().
+ * @param obj The widget.
+ * @param automatic EINA_TRUE for auto mirrored mode. EINA_FALSE for manual.
+ */
+EAPI void
+elm_object_mirrored_automatic_set(Evas_Object *obj, Eina_Bool automatic)
+{
+   EINA_SAFETY_ON_NULL_RETURN(obj);
+   elm_widget_mirrored_automatic_set(obj, automatic);
+}
+
+/**
  * @defgroup Scaling Selective Widget Scaling
  *
  * Different widgets can be scaled independently. These functions allow you to
