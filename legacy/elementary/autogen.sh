89faf3c22bce2383972d2cf43bc195d47a1117ac
Cedric BAIL <cedric.bail@free.fr>
2011-02-21 14:56:49 +0000

    efl: turn on cached result, that will reduce your configure time a lot !
    SVN revision: 57213

--- legacy/elementary/autogen.sh
+++ legacy/elementary/autogen.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-rm -rf autom4te.cache
-rm -f aclocal.m4 ltmain.sh
-
-touch README
-
 echo "Running autopoint..." ; autopoint -f || :
 echo "Running aclocal..." ; aclocal $ACLOCAL_FLAGS -I m4 || exit 1
 echo "Running autoheader..." ; autoheader || exit 1
@@ -13,5 +8,5 @@ echo "Running libtoolize..." ; (libtoolize --copy --automake || glibtoolize --au
 echo "Running automake..." ; automake --add-missing --copy --gnu || exit 1
 
 if [ -z "$NOCONFIGURE" ]; then
-	./configure "$@"
+	./configure -C "$@"
 fi
