57934d16f164ca1e2791ce78c3e16b42d0e7084a
Tom Hacohen <tom@stosb.com>
2011-02-22 16:33:18 +0000

    Elementary test-bubble: Modified the test to include a 'no-icon' style.
    This exposes an issue introduced in a patch I'll revert in a moment.
    
    SVN revision: 57243

--- legacy/elementary/src/bin/test_bubble.c
+++ legacy/elementary/src/bin/test_bubble.c
@@ -54,23 +54,15 @@ test_bubble(void *data __UNUSED__, Evas_Object *obj __UNUSED__, void *event_info
    elm_box_pack_end(bx, bb);
    evas_object_show(bb);
 
-   ic = elm_icon_add(win);
-   snprintf(buf, sizeof(buf), "%s/images/logo_small.png", PACKAGE_DATA_DIR);
-   elm_icon_file_set(ic, buf, NULL);
-   elm_icon_scale_set(ic, 0, 0);
-   evas_object_size_hint_aspect_set(ic, EVAS_ASPECT_CONTROL_HORIZONTAL, 1, 1);
-
    bb = elm_bubble_add(win);
    elm_bubble_label_set(bb, "Message 2");
    elm_bubble_info_set(bb, "10:32 4/11/2008");
-   elm_bubble_icon_set(bb, ic);
    evas_object_smart_callback_add(bb, "clicked", _print_clicked, NULL);
-   evas_object_show(ic);
    evas_object_size_hint_weight_set(bb, EVAS_HINT_EXPAND, 0.0);
    evas_object_size_hint_align_set(bb, EVAS_HINT_FILL, EVAS_HINT_FILL);
 
    ct = elm_label_add(win);
-   elm_label_label_set(ct, "Corner: base (top-left)");
+   elm_label_label_set(ct, "Corner: base (top-left) - no icon");
    elm_bubble_content_set(bb, ct);
 
    elm_box_pack_end(bx, bb);
