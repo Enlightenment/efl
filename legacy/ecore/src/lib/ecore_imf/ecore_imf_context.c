674efd2730425a6b969e02f77ff69090cea93713
Jihoon Kim <jihoon48.kim@samsung.com>
2011-03-10 09:15:36 +0000

    From: Jihoon Kim <jihoon48.kim@samsung.com> Subject: [E-devel] [PATCH] patch for describing ecore_imf APIs more detail
    This patch will supplement the documentation about
    ecore_imf_context_preedit_start_event_add,
    ecore_imf_context_preedit_end_event_add and 
    ecore_imf_context_delete_surrounding_event_add API.
    
    
    
    SVN revision: 57659

--- legacy/ecore/src/lib/ecore_imf/ecore_imf_context.c
+++ legacy/ecore/src/lib/ecore_imf/ecore_imf_context.c
@@ -740,6 +740,8 @@ _ecore_imf_event_free_preedit(void *data __UNUSED__, void *event)
 /**
  * Adds ECORE_IMF_EVENT_PREEDIT_START to the event queue.
  *
+ * ECORE_IMF_EVENT_PREEDIT_START should be added when a new preedit sequence starts.
+ *
  * @param ctx An #Ecore_IMF_Context.
  * @ingroup Ecore_IMF_Context_Module_Group
  */
@@ -763,6 +765,8 @@ ecore_imf_context_preedit_start_event_add(Ecore_IMF_Context *ctx)
 
 /**
  * Adds ECORE_IMF_EVENT_PREEDIT_END to the event queue.
+ *
+ * ECORE_IMF_EVENT_PREEDIT_END should be added when a new preedit sequence has been completed or canceled.
  *
  * @param ctx An #Ecore_IMF_Context.
  * @ingroup Ecore_IMF_Context_Module_Group
@@ -854,6 +858,10 @@ _ecore_imf_event_free_delete_surrounding(void *data __UNUSED__, void *event)
 
 /**
  * Adds ECORE_IMF_EVENT_DELETE_SURROUNDING to the event queue.
+ *
+ * Asks the widget that the input context is attached to to delete characters around the cursor position 
+ * by adding the ECORE_IMF_EVENT_DELETE_SURROUNDING to the event queue.
+ * Note that offset and n_chars are in characters not in bytes.
  *
  * @param ctx An #Ecore_IMF_Context.
  * @param offset The start offset of surrounding to be deleted.
