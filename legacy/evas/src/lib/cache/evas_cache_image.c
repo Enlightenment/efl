25f1cf2d2fbf744bdf33a1d30dc2b41b56017b1c
Cedric BAIL <cedric.bail@free.fr>
2011-03-22 14:29:25 +0000

    evas: fix build with uclibc.
    SVN revision: 57991

--- legacy/evas/src/lib/cache/evas_cache_image.c
+++ legacy/evas/src/lib/cache/evas_cache_image.c
@@ -288,11 +288,11 @@ _timestamp_compare(Image_Timestamp *tstamp, struct stat *st)
    if (tstamp->size != st->st_size) return EINA_FALSE;
    if (tstamp->ino != st->st_ino) return EINA_FALSE;
 #ifdef _STAT_VER_LINUX
-#ifdef __USE_MISC   
+#if (defined __USE_MISC && defined st_mtime)
    if (tstamp->mtime_nsec != (unsigned long int)st->st_mtim.tv_nsec) return EINA_FALSE;
-#else   
+#else
    if (tstamp->mtime_nsec != (unsigned long int)st->st_mtimensec) return EINA_FALSE;
-#endif   
+#endif
 #endif
    return EINA_TRUE;
 }
@@ -304,12 +304,12 @@ _timestamp_build(Image_Timestamp *tstamp, struct stat *st)
    tstamp->size = st->st_size;
    tstamp->ino = st->st_ino;
 #ifdef _STAT_VER_LINUX
-#ifdef __USE_MISC   
+#if (defined __USE_MISC && defined st_mtime)
    tstamp->mtime_nsec = (unsigned long int)st->st_mtim.tv_nsec;
-#else   
+#else
    tstamp->mtime_nsec = (unsigned long int)st->st_mtimensec;
 #endif
-#endif   
+#endif
 }
 
 static Image_Entry *
