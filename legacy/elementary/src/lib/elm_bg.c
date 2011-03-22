11365c74453fc6526d7aa5ce75543bfaabace66e
ChunEon Park <hermet@hermet.pe.kr>
2011-03-22 07:51:17 +0000

    fixed white trailing
    SVN revision: 57962

--- legacy/elementary/src/lib/elm_bg.c
+++ legacy/elementary/src/lib/elm_bg.c
@@ -415,7 +415,7 @@ elm_bg_overlay_unset(Evas_Object *obj)
 EAPI void
 elm_bg_load_size_set(Evas_Object *obj, Evas_Coord w, Evas_Coord h)
 {
-   ELM_CHECK_WIDTYPE(obj, widtype);   
+   ELM_CHECK_WIDTYPE(obj, widtype);
    Widget_Data *wd = elm_widget_data_get(obj);
    const char *p;
    if (!wd) return;
