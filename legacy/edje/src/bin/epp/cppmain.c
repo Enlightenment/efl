49bc9d8ee185a013fc55dc40c0997f34fc557801
Christopher Michael <cpmichael1@comcast.net>
2011-03-24 20:25:17 +0000

    Edje:EPP: Comment out unused goto line. Fix (some) formatting (but needs more fixing as the formatting in this is just horrible).
    SVN revision: 58074

--- legacy/edje/src/bin/epp/cppmain.c
+++ legacy/edje/src/bin/epp/cppmain.c
@@ -123,15 +123,17 @@ main(int argc, char **argv)
 #endif
 	if (!opts->no_output)
 	  {
-	     size_t              n;
+	     size_t n;
 
 	     n = CPP_WRITTEN(&parse_in);
 	     if (fwrite(parse_in.token_buffer, 1, n, stdout) != n)
 		exit(FATAL_EXIT_CODE);
 	  }
+        /*
       next:
 	parse_in.limit = parse_in.token_buffer;
 	got_text = 0;
+         */
      }
 
  done:
