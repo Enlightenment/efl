daacf8fd7b673477cae142bb98cfcc7be14c4019
Sangho Park <gouache95@gmail.com>
2011-04-27 06:04:04 +0000

    elm_map: fix pinch zoom bug
    SVN revision: 58946

--- legacy/elementary/src/lib/elm_map.c
+++ legacy/elementary/src/lib/elm_map.c
@@ -1595,15 +1595,13 @@ _mouse_multi_down(void *data, Evas *evas __UNUSED__, Evas_Object *obj, void *eve
    elm_smart_scroller_hold_set(wd->scr, 1);
    elm_smart_scroller_freeze_set(wd->scr, 1);
 
-   if ((wd->multi_count + 1) > 1) goto done;
-   else wd->multi_count++;
-
    ev = create_event_object(data, obj, down->device);
    if (!ev)
      {
         DBG("Failed : create_event_object");
         goto done;
      }
+   wd->multi_count++;
 
    ev->hold_timer = NULL;
    ev->start.x = ev->prev.x = down->output.x;
@@ -1638,7 +1636,7 @@ _mouse_multi_move(void *data, Evas *evas __UNUSED__, Evas_Object *obj __UNUSED__
    ev0 = get_event_object(data, 0);
    if (!ev0) return;
 
-   if (wd->multi_count == 1)
+   if (wd->multi_count >= 1)
      {
         Evas_Coord x, y, w, h;
         float half_w, half_h;
