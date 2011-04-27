5dc8b2be5e5b620c254c8ca82b221d0910eb3d2d
Tom Hacohen <tom@stosb.com>
2011-04-27 07:41:06 +0000

    Elementary gettext: Don't set default textdomain, elm is a lib...
    SVN revision: 58949

--- legacy/elementary/src/lib/elm_config.c
+++ legacy/elementary/src/lib/elm_config.c
@@ -1540,8 +1540,7 @@ _env_get(void)
 
    /* Get RTL orientation from system */
    setlocale(LC_ALL, "");
-   bindtextdomain("elementary", LOCALE_DIR);
-   textdomain("elementary");
+   bindtextdomain(PACKAGE, LOCALE_DIR);
    _elm_config->is_mirrored = !strcmp(E_("default:LTR"), "default:RTL");
 
    s = getenv("ELM_TOOLTIP_DELAY");
