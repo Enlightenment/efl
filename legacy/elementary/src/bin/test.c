66b0ee3cd39f85685885e1db0b1b9a20fcb57072
Carsten Haitzler <raster@rasterman.com>
2011-05-25 11:44:10 +0000

    add table test case.
    SVN revision: 59664

--- legacy/elementary/src/bin/test.c
+++ legacy/elementary/src/bin/test.c
@@ -70,6 +70,7 @@ void test_genlist9(void *data, Evas_Object *obj, void *event_info);
 void test_genlist10(void *data, Evas_Object *obj, void *event_info);
 void test_table(void *data, Evas_Object *obj, void *event_info);
 void test_table2(void *data, Evas_Object *obj, void *event_info);
+void test_table3(void *data, Evas_Object *obj, void *event_info);
 void test_gengrid(void *data, Evas_Object *obj, void *event_info);
 void test_gengrid2(void *data, Evas_Object *obj, void *event_info);
 void test_pager(void *data, Evas_Object *obj, void *event_info);
@@ -287,6 +288,7 @@ my_win_main(char *autorun)
    ADD_TEST("Toggles", test_toggle);
    ADD_TEST("Table", test_table);
    ADD_TEST("Table Homogeneous", test_table2);
+   ADD_TEST("Table 3", test_table3);
    ADD_TEST("Clock", test_clock);
    ADD_TEST("Clock 2", test_clock2);
    ADD_TEST("Layout", test_layout);
