04a24dc1f3acace9bf28db2e1c7d11f809fb5a6d
Christopher Michael <cpmichael1@comcast.net>
2011-05-27 01:13:41 +0000

    Evas: Fix shadow declaration of variable 'index'
    SVN revision: 59710

--- legacy/evas/src/lib/canvas/evas_object_box.c
+++ legacy/evas/src/lib/canvas/evas_object_box.c
@@ -91,11 +91,11 @@ _evas_object_box_iterator_free(Evas_Object_Box_Iterator *it)
 }
 
 static Eina_Bool
-_evas_object_box_accessor_get_at(Evas_Object_Box_Accessor *it, unsigned int index, void **data)
+_evas_object_box_accessor_get_at(Evas_Object_Box_Accessor *it, unsigned int idx, void **data)
 {
    Evas_Object_Box_Option *opt = NULL;
 
-   if (!eina_accessor_data_get(it->real_accessor, index, (void *)&opt))
+   if (!eina_accessor_data_get(it->real_accessor, idx, (void *)&opt))
      return EINA_FALSE;
    if (data) *data = opt->obj;
    return EINA_TRUE;
