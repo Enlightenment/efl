14e66e3fb863702620f2ddfc310efe2564c64325
ChunEon Park <hermet@hermet.pe.kr>
2011-06-16 10:03:37 +0000

    evas/canvas - ok. it works fine. modified to use the standard computation again.
    SVN revision: 60386

--- legacy/evas/src/lib/canvas/evas_map.c
+++ legacy/evas/src/lib/canvas/evas_map.c
@@ -715,8 +715,8 @@ evas_map_util_rotate(Evas_Map *m, double degrees, Evas_Coord cx, Evas_Coord cy)
 
         xx = x * cos(r);
         yy = x * sin(r);
-        x = xx + (y * cos(r + M_PI_2));
-        y = yy + (y * sin(r + M_PI_2));
+        x = xx - (y * sin(r));
+        y = yy + (y * cos(r));
 
         p->px = p->x = x + cx;
         p->py = p->y = y + cy;
@@ -772,24 +772,24 @@ evas_map_util_3d_rotate(Evas_Map *m, double dx, double dy, double dz,
           {
              xx = x * cos(rz);
              yy = x * sin(rz);
-             x = xx + (y * cos(rz + M_PI_2));
-             y = yy + (y * sin(rz + M_PI_2));
+             x = xx - (y * sin(rz));
+             y = yy + (y * cos(rz));
           }
 
         if (ry != 0.0)
           {
              xx = x * cos(ry);
              zz = x * sin(ry);
-             x = xx + (z * cos(ry + M_PI_2));
-             z = zz + (z * sin(ry + M_PI_2));
+             x = xx - (z * sin(ry));
+             z = zz + (z * cos(ry));
           }
 
         if (rx != 0.0)
           {
              zz = z * cos(rx);
              yy = z * sin(rx);
-             z = zz + (y * cos(rx + M_PI_2));
-             y = yy + (y * sin(rx + M_PI_2));
+             z = zz - (y * sin(rx));
+             y = yy + (y * cos(rx));
           }
 
         p->px = p->x = x + cx;
