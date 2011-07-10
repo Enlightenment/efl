2e130b0de9be80a67bf387bf91c1cf3e0aff8c1a
Vincent Torri <vincent.torri@gmail.com>
2011-07-10 09:00:57 +0000

    add first part of the ecore_con support. elm_map.c must be fixed.
    SVN revision: 61193

--- legacy/elementary/src/lib/elm_map.c
+++ legacy/elementary/src/lib/elm_map.c
@@ -1,4 +1,11 @@
-#include <Ecore_Con.h>
+#ifdef HAVE_CONFIG
+# include <elementary_config.h>
+#endif
+
+#ifdef HAVE_ECORE_CON
+# include <Ecore_Con.h>
+#endif
+
 #include "Elementary.h"
 #include "elm_priv.h"
 
