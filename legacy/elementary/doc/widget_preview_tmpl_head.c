4c54cb95abf18f5720eca09e86ba23491aa8bb2d
Jonas M. Gastal <jgastal@profusion.mobi>
2011-07-20 17:55:15 +0000

    Elementary: eps doesn't support transparency.
    SVN revision: 61535

--- legacy/elementary/doc/widget_preview_tmpl_head.c
+++ legacy/elementary/doc/widget_preview_tmpl_head.c
@@ -3,12 +3,13 @@
 EAPI int
 elm_main(int argc, char **argv)
 {
-   Evas_Object *win;
+   Evas_Object *win, *bg;
    Evas_Coord w, h;
    
    w = atoi(argv[1]);
    h = atoi(argv[2]);
    
    win = elm_win_add(NULL, "main", ELM_WIN_BASIC);
-   elm_win_alpha_set(win, EINA_TRUE);
-   
+   bg = elm_bg_add(win);
+   elm_win_resize_object_add(win, bg);
+   evas_object_show(bg);
