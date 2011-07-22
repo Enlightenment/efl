63d312c13b56e0967ab26345e2da9d59f30d1c21
Daniel Juyung Seo <seojuyung2@gmail.com>
2011-07-22 05:46:47 +0000

    elm widget_preview_actionslider.c: Fixed to use elm_object_text_part_set rather than elm_actionslider_labels_set.
    SVN revision: 61578

--- legacy/elementary/doc/widget_preview_actionslider.c
+++ legacy/elementary/doc/widget_preview_actionslider.c
@@ -5,7 +5,9 @@ evas_object_size_hint_weight_set(o, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
 elm_win_resize_object_add(win, o);
 evas_object_show(o);
 
-elm_actionslider_labels_set(o, "left", "center", "right");
+elm_object_text_part_set(o, "left", "left");
+elm_object_text_part_set(o, "center", "center");
+elm_object_text_part_set(o, "right", "right");
 elm_actionslider_indicator_pos_set(o, ELM_ACTIONSLIDER_CENTER);
 
 #include "widget_preview_tmpl_foot.c"
