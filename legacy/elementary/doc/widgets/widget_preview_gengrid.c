94ea6662711eb168b106ab9a2a03bd8e0ff47da9
Gustavo Lima Chaves <glima@profusion.mobi>
2011-07-25 19:52:47 +0000

    [elementary] Index preview image for gengrid.
    SVN revision: 61707

---
+++ legacy/elementary/doc/widgets/widget_preview_gengrid.c
@@ -0,0 +1,47 @@
+#include <Elementary.h>
+
+static char *
+_grid_label_get(void        *data,
+                Evas_Object *obj,
+                const char  *part )
+{
+   return strdup("label");
+}
+
+static Evas_Object *
+_grid_icon_get(void        *data,
+               Evas_Object *obj,
+               const char  *part)
+{
+   if (!strcmp(part, "elm.swallow.icon"))
+     {
+        Evas_Object *icon = elm_icon_add(obj);
+        elm_icon_standard_set(icon, "home");
+        evas_object_size_hint_aspect_set(icon, EVAS_ASPECT_CONTROL_VERTICAL, 1,
+                                         1);
+        evas_object_show(icon);
+        return icon;
+     }
+
+   return NULL;
+}
+
+#include "widget_preview_tmpl_head.c"
+
+Evas_Object *gengrid = elm_gengrid_add(win);
+evas_object_size_hint_weight_set(gengrid, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+elm_win_resize_object_add(win, gengrid);
+evas_object_show(gengrid);
+
+Elm_Gengrid_Item_Class gic;
+gic.item_style = "default";
+gic.func.label_get = _grid_label_get;
+gic.func.icon_get = _grid_icon_get;
+gic.func.state_get = NULL;
+gic.func.del = NULL;
+
+elm_gengrid_item_append(gengrid, &gic, NULL, NULL, NULL);
+elm_gengrid_item_append(gengrid, &gic, NULL, NULL, NULL);
+elm_gengrid_item_append(gengrid, &gic, NULL, NULL, NULL);
+
+#include "widget_preview_tmpl_foot.c"
