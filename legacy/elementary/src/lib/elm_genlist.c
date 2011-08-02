1c6f5c265afbe8f7dfd01c58a2b2a4cecb0a8333
Daniel Juyung Seo <seojuyung2@gmail.com>
2011-08-02 06:29:54 +0000

    elm genlist: Fixed comment typo.
    SVN revision: 61979

--- legacy/elementary/src/lib/elm_genlist.c
+++ legacy/elementary/src/lib/elm_genlist.c
@@ -3596,7 +3596,7 @@ elm_genlist_item_insert_after(Evas_Object                  *obj,
    Elm_Genlist_Item *it = _item_new(wd, itc, data, parent, flags, func,
                                     func_data);
    if (!it) return NULL;
-   /* It make no sense to insert after in an empty list with after != NULL, something really bad is happening in your app. */
+   /* It makes no sense to insert after in an empty list with after != NULL, something really bad is happening in your app. */
    EINA_SAFETY_ON_NULL_RETURN_VAL(wd->items, NULL);
 
    wd->items = eina_inlist_append_relative(wd->items, EINA_INLIST_GET(it),
