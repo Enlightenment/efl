610facb5bed08df90e0f6d71c9802f9b6f0eed24
Jonas M. Gastal <jgastal@profusion.mobi>
2011-08-04 20:44:05 +0000

    Eina: adding image to eina_stringshare.
    SVN revision: 62104

--- legacy/eina/src/include/eina_stringshare.h
+++ legacy/eina/src/include/eina_stringshare.h
@@ -126,6 +126,12 @@
  * string creation/destruction speed, reduces memory use and decreases
  * memory fragmentation, so a win all-around.
  *
+ * The following diagram gives an idea of what happens as you create strings
+ * with eina_stringshare_add():
+ *
+ * @image html eina_stringshare.png
+ * @image latex eina_stringshare.eps width=\textwidth
+ *
  * For more information, see @ref eina_stringshare_example_01_page
  * "this example".
  */
