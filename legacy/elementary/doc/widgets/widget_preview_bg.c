a2ffa3ababecfe954cd62a1a61ddd500e88cb6f1
Carsten Haitzler <raster@rasterman.com>
2011-08-09 08:01:42 +0000

    i think bg widfget should maybe have a solid wallpaper image as an example.. not a transparent icon :)
    SVN revision: 62230

--- legacy/elementary/doc/widgets/widget_preview_bg.c
+++ legacy/elementary/doc/widgets/widget_preview_bg.c
@@ -4,7 +4,7 @@ Evas_Object *o = elm_bg_add(win);
 evas_object_size_hint_weight_set(o, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
 elm_win_resize_object_add(win, o);
 
-elm_bg_file_set(o, PACKAGE_DATA_DIR"/images/logo_small.png", NULL);
+elm_bg_file_set(o, PACKAGE_DATA_DIR"/images/sky_03.jpg", NULL);
 evas_object_show(o);
 
 #include "widget_preview_tmpl_foot.c"
