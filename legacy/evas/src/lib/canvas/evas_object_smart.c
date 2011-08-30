6694437c15e27be4d193a0911d57ddc3bb5df5b7
Carsten Haitzler <raster@rasterman.com>
2011-08-30 15:33:46 +0000

    lets up the max recalc count a bit now that things re-schedule themselves more.
    SVN revision: 62994

--- legacy/evas/src/lib/canvas/evas_object_smart.c
+++ legacy/evas/src/lib/canvas/evas_object_smart.c
@@ -585,15 +585,13 @@ evas_object_smart_need_recalculate_set(Evas_Object *obj, Eina_Bool value)
    
    if (o->need_recalculate == value) return;
 
-   if (obj->recalculate_cycle > 64)
+   if (obj->recalculate_cycle > 256)
      {
         ERR("Object %p is not stable during recalc loop", obj);
         return;
      }
    if (obj->layer->evas->in_smart_calc) obj->recalculate_cycle++;
    o->need_recalculate = value;
-
-   if (!obj->smart.smart->smart_class->calculate) return;
 }
 
 EAPI Eina_Bool
