95d188941d3155034862e369eb2c62d0eae2fb91
Daniel Juyung Seo <seojuyung2@gmail.com>
2011-09-13 16:59:25 +0000

    elm elm_widget.c: Fixed indentation.
    SVN revision: 63361

--- legacy/elementary/src/lib/elm_widget.c
+++ legacy/elementary/src/lib/elm_widget.c
@@ -3400,30 +3400,30 @@ static void
 _smart_init(void)
 {
    if (_e_smart) return;
-   {
-      static const Evas_Smart_Class sc =
-      {
-         SMART_NAME,
-         EVAS_SMART_CLASS_VERSION,
-         _smart_add,
-         _smart_del,
-         _smart_move,
-         _smart_resize,
-         _smart_show,
-         _smart_hide,
-         _smart_color_set,
-         _smart_clip_set,
-         _smart_clip_unset,
-         _smart_calculate,
-         NULL,
-         NULL,
-         NULL,
-         NULL,
-         NULL,
-         NULL
-      };
-      _e_smart = evas_smart_class_new(&sc);
-   }
+     {
+        static const Evas_Smart_Class sc =
+          {
+             SMART_NAME,
+             EVAS_SMART_CLASS_VERSION,
+             _smart_add,
+             _smart_del,
+             _smart_move,
+             _smart_resize,
+             _smart_show,
+             _smart_hide,
+             _smart_color_set,
+             _smart_clip_set,
+             _smart_clip_unset,
+             _smart_calculate,
+             NULL,
+             NULL,
+             NULL,
+             NULL,
+             NULL,
+             NULL
+          };
+        _e_smart = evas_smart_class_new(&sc);
+     }
 }
 
 /* happy debug functions */
