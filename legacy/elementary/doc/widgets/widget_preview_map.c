b40cc78bac3c3459010a52e36820f56e1cc957c0
Vincent Torri <vincent.torri@gmail.com>
2011-09-18 17:23:41 +0000

    fix compilation on Windows
    SVN revision: 63458

--- legacy/elementary/doc/widgets/widget_preview_map.c
+++ legacy/elementary/doc/widgets/widget_preview_map.c
@@ -1,9 +1,10 @@
-#include <Elementary.h>
 #ifdef HAVE_CONFIG_H
 # include "elementary_config.h"
 #else
 # define __UNUSED__
 #endif
+
+#include <Elementary.h>
 
 /* FIXME: it shouldn't be required. For unknown reason map won't call
  * pan_calculate until shot delay time, but then it will take a screenshot
@@ -18,11 +19,12 @@ _nasty_hack(void *data)
    return ECORE_CALLBACK_CANCEL;
 }
 
-EAPI int
+EAPI_MAIN int
 elm_main(int argc __UNUSED__, char **argv)
 {
    Evas_Object *win, *bg;
    Evas_Coord w, h;
+   Evas_Object *o;
 
    w = atoi(argv[1]);
    h = atoi(argv[2]);
@@ -32,13 +34,13 @@ elm_main(int argc __UNUSED__, char **argv)
    elm_win_resize_object_add(win, bg);
    evas_object_show(bg);
 
-evas_object_size_hint_weight_set(bg, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+   evas_object_size_hint_weight_set(bg, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
 
-Evas_Object *o = elm_map_add(win);
-evas_object_size_hint_weight_set(o, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-elm_win_resize_object_add(win, o);
-evas_object_show(o);
+   o = elm_map_add(win);
+   evas_object_size_hint_weight_set(o, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+   elm_win_resize_object_add(win, o);
+   evas_object_show(o);
 
-ecore_timer_add(0.5, _nasty_hack, win);
+   ecore_timer_add(0.5, _nasty_hack, win);
 
 #include "widget_preview_tmpl_foot.c"
