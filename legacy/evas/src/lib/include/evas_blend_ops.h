993fa90cb361eb6fa30b42aa70a5d58815a1a63f
Vincent Torri <vincent.torri@gmail.com>
2011-09-30 04:04:28 +0000

    unneeded checks
    SVN revision: 63708

--- legacy/evas/src/lib/include/evas_blend_ops.h
+++ legacy/evas/src/lib/include/evas_blend_ops.h
@@ -9,14 +9,6 @@
 #include <immintrin.h>
 #endif
 
-#ifndef always_inline
-#if defined(__GNUC__) && (__GNUC__ > 3 || __GNUC__ == 3 && __GNUC_MINOR__ > 0)
-#    define always_inline __attribute__((always_inline)) inline
-#else
-#    define always_inline inline
-#endif
-#endif
-
 
 /* src pixel flags: */
 
