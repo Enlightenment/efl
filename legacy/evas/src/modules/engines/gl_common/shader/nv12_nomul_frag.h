82b843d222bd8749f49cfe7402e2e592ba7b2879
Carsten Haitzler <raster@rasterman.com>
2011-10-06 13:25:18 +0000

    only use highp *IF* compiler supports it.
    SVN revision: 63880

--- legacy/evas/src/modules/engines/gl_common/shader/nv12_nomul_frag.h
+++ legacy/evas/src/modules/engines/gl_common/shader/nv12_nomul_frag.h
@@ -1,5 +1,9 @@
 "#ifdef GL_ES\n"
+"#ifdef GL_FRAGMENT_PRECISION_HIGH\n"
 "precision highp float;\n"
+"#else\n"
+"precision mediump float;\n"
+"#endif\n"
 "#endif\n"
 "uniform sampler2D tex, texuv;\n"
 "varying vec2 tex_c, tex_cuv;\n"
@@ -9,18 +13,19 @@
 "  y=texture2D(tex,tex_c).g;\n"
 "  u=texture2D(texuv,tex_cuv).g;\n"
 "  v=texture2D(texuv,tex_cuv).a;\n"
-
+"\n"
 "  u=u-0.5;\n"
 "  v=v-0.5;\n"
 "  vmu=v*0.813+u*0.391;\n"
 "  u=u*2.018;\n"
 "  v=v*1.596;\n"
 "  y=(y-0.062)*1.164;\n"
-
+"\n"
 "  r=y+v;\n"
 "  g=y-vmu;\n"
 "  b=y+u;\n"
-
+"\n"
 "  gl_FragColor=vec4(r,g,b,1.0);\n"
 "}\n"
-
+"\n"
+"\n"
