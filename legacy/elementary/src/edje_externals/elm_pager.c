a34438d5fca14fb1244f397f1a80cf0afd4acbfd
Mike McCormack <mj.mccormack@samsung.com>
2011-10-11 06:27:47 +0000

    elementary: Intentiness
    Signed-off-by: Mike McCormack <mj.mccormack@samsung.com>
    
    SVN revision: 63983

--- legacy/elementary/src/edje_externals/elm_pager.c
+++ legacy/elementary/src/edje_externals/elm_pager.c
@@ -10,7 +10,9 @@ typedef struct _Elm_Params_Pager
 
 
 static void
-external_pager_state_set(void *data __UNUSED__, Evas_Object *obj, const void *from_params, const void *to_params, float pos __UNUSED__)
+external_pager_state_set(void *data __UNUSED__, Evas_Object *obj,
+                         const void *from_params, const void *to_params,
+                         float pos __UNUSED__)
 {
    const Elm_Params_Pager *p;
 
@@ -18,19 +20,21 @@ external_pager_state_set(void *data __UNUSED__, Evas_Object *obj, const void *fr
    else if (from_params) p = from_params;
    else return;
 
-  if(p->disable_animation_exists) elm_pager_animation_disabled_set(obj, p->disable_animation);
+   if (p->disable_animation_exists)
+     elm_pager_animation_disabled_set(obj, p->disable_animation);
 }
 
 static Eina_Bool
-external_pager_param_set(void *data __UNUSED__, Evas_Object *obj, const Edje_External_Param *param)
+external_pager_param_set(void *data __UNUSED__, Evas_Object *obj,
+                         const Edje_External_Param *param)
 {
    if (!strcmp(param->name, "disable animation"))
      {
-	if (param->type == EDJE_EXTERNAL_PARAM_TYPE_BOOL)
-	  {
-	     elm_pager_animation_disabled_set(obj, param->i);
-	     return EINA_TRUE;
-	  }
+        if (param->type == EDJE_EXTERNAL_PARAM_TYPE_BOOL)
+          {
+             elm_pager_animation_disabled_set(obj, param->i);
+             return EINA_TRUE;
+          }
      }
    ERR("unknown parameter '%s' of type '%s'",
        param->name, edje_external_param_type_str(param->type));
@@ -39,7 +43,9 @@ external_pager_param_set(void *data __UNUSED__, Evas_Object *obj, const Edje_Ext
 }
 
 static Eina_Bool
-external_pager_param_get(void *data __UNUSED__, const Evas_Object *obj __UNUSED__, Edje_External_Param *param)
+external_pager_param_get(void *data __UNUSED__,
+                         const Evas_Object *obj __UNUSED__,
+                         Edje_External_Param *param)
 {
    ERR("unknown parameter '%s' of type '%s'",
        param->name, edje_external_param_type_str(param->type));
@@ -48,7 +54,9 @@ external_pager_param_get(void *data __UNUSED__, const Evas_Object *obj __UNUSED_
 }
 
 static void *
-external_pager_params_parse(void *data __UNUSED__, Evas_Object *obj __UNUSED__, const Eina_List *params)
+external_pager_params_parse(void *data __UNUSED__,
+                            Evas_Object *obj __UNUSED__,
+                            const Eina_List *params)
 {
    Elm_Params_Pager *mem;
    Edje_External_Param *param;
@@ -60,21 +68,23 @@ external_pager_params_parse(void *data __UNUSED__, Evas_Object *obj __UNUSED__, 
 
    EINA_LIST_FOREACH(params, l, param)
      {
-	if (!strcmp(param->name, "disable animation"))
-		{
-	     mem->disable_animation = !!param->i;
-		  mem->disable_animation_exists = EINA_TRUE;
-		}
+        if (!strcmp(param->name, "disable animation"))
+          {
+             mem->disable_animation = !!param->i;
+             mem->disable_animation_exists = EINA_TRUE;
+          }
      }
 
    return mem;
 }
 
-static Evas_Object *external_pager_content_get(void *data __UNUSED__,
-		const Evas_Object *obj __UNUSED__, const char *content __UNUSED__)
+static Evas_Object *
+external_pager_content_get(void *data __UNUSED__,
+                           const Evas_Object *obj __UNUSED__,
+                           const char *content __UNUSED__)
 {
-	ERR("so content");
-	return NULL;
+   ERR("so content");
+   return NULL;
 }
 
 static void
