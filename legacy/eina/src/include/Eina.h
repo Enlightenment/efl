3e0c2d16dce17350c86b1ca6eef14a10afb9d816
Mike McCormack <mj.mccormack@samsung.com>
2011-10-13 07:30:34 +0000

    eina: Hook clist into the top level documentation
    Signed-off-by: Mike McCormack <mj.mccormack@samsung.com>
    
    SVN revision: 64039

--- legacy/eina/src/include/Eina.h
+++ legacy/eina/src/include/Eina.h
@@ -62,6 +62,7 @@
  * @li @ref Eina_Array_Group standard array of @c void* data.
  * @li @ref Eina_Hash_Group standard hash of @c void* data.
  * @li @ref Eina_Inline_List_Group list with nodes inlined into user type.
+ * @li @ref Eina_CList_Group compact inlined list.
  * @li @ref Eina_List_Group standard list of @c void* data.
  * @li @ref Eina_Iterator_Group Iterator functions.
  * @li @ref Eina_Matrixsparse_Group sparse matrix of @c void* data.
