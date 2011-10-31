70af6eb78353fd32ae0a5fa9f4fa0dc1e96879c6
Bruno Dilly <bdilly@profusion.mobi>
2011-10-31 19:00:09 +0000

    Elementary Index Test: avoid leak
    Avoid leak caused by not updated signal name.
    Btw, Mr. Valgrind is sad with elm developers.
    Lots of warnings regarding uninitialised values.
    
    
    SVN revision: 64582

--- legacy/elementary/src/bin/test_index.c
+++ legacy/elementary/src/bin/test_index.c
@@ -202,7 +202,7 @@ test_index2(void *data __UNUSED__, Evas_Object *obj __UNUSED__, void *event_info
 
    win = elm_win_add(NULL, "sorted-index-list", ELM_WIN_BASIC);
    elm_win_title_set(win, "Sorted Index and List");
-   evas_object_smart_callback_add(win, "delete-request", test_index2_del, gui);
+   evas_object_smart_callback_add(win, "delete,request", test_index2_del, gui);
    elm_win_autodel_set(win, EINA_TRUE);
 
    bg = elm_bg_add(win);
