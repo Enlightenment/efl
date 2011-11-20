0d7ca9bff52539f7c882cb56c39240f7ecff89bb
Vincent Torri <vincent.torri@gmail.com>
2011-11-20 07:09:31 +0000

    Ecore_Win32: Resolving the issue of "mouse-down inside and mouse-up outside"
    Patch by Kim Shinwoo
    
    SVN revision: 65425

--- legacy/ecore/src/lib/ecore_win32/ecore_win32.c
+++ legacy/ecore/src/lib/ecore_win32/ecore_win32.c
@@ -107,6 +107,7 @@ _ecore_win32_window_procedure(HWND   window,
        /* Mouse input notifications */
      case WM_LBUTTONDOWN:
        INF("left button down message");
+       SetCapture(window);
        _ecore_win32_event_handle_button_press(data, 1);
        return 0;
      case WM_MBUTTONDOWN:
@@ -123,10 +124,10 @@ _ecore_win32_window_procedure(HWND   window,
 
           INF("left button up message");
 
+          ReleaseCapture();
           w = (Ecore_Win32_Window *)GetWindowLongPtr(window, GWLP_USERDATA);
           if (w->drag.dragging)
             {
-               ReleaseCapture();
                w->drag.dragging = 0;
                return 0;
             }
