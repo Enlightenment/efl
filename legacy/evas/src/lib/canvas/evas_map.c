b0ddea750875e3bec36bd30d359e88c5c2cb0ba9
Carsten Haitzler <raster@rasterman.com>
2011-12-12 06:25:14 +0000

    only lround 2 times instead of 4 for map - minor bit of cleaner code.
    SVN revision: 66108

--- legacy/evas/src/lib/canvas/evas_map.c
+++ legacy/evas/src/lib/canvas/evas_map.c
@@ -81,10 +81,8 @@ _evas_map_calc_map_geometry(Evas_Object *obj)
 
    p = obj->cur.map->points;
    p_end = p + obj->cur.map->count;
-   x1 = lround(p->x);
-   x2 = lround(p->x);
-   y1 = lround(p->y);
-   y2 = lround(p->y);
+   x1 = x2 = lround(p->x);
+   y1 = y2 = lround(p->y);
    p++;
    for (; p < p_end; p++)
      {
