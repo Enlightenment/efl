77d0c1f015d44f469f55439e7463b6c2b34db33e
Fabiano Fidêncio <fabianoo@fidencio.org>
2011-12-25 02:36:00 +0000

    elm toolbar: call the correct function to set a item del callback
        We don't want a recursive loop, eh?
        And Merry X-Mas to all!
    
    
    SVN revision: 66494

--- legacy/elementary/src/lib/elm_toolbar.c
+++ legacy/elementary/src/lib/elm_toolbar.c
@@ -1470,7 +1470,7 @@ EAPI void
 elm_toolbar_item_del_cb_set(Elm_Object_Item *it, Evas_Smart_Cb func)
 {
    ELM_OBJ_ITEM_CHECK_OR_RETURN(it);
-   elm_toolbar_item_del_cb_set(it, func);
+   elm_widget_item_del_cb_set(it, func);
 }
 
 EAPI Eina_Bool
