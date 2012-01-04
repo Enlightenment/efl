f909faa92785beb289263cd91bffe8c6a2fabcb5
Carsten Haitzler <raster@rasterman.com>
2012-01-04 03:33:24 +0000

    add invalid panel back.
    SVN revision: 66835

--- legacy/elementary/src/lib/elm_entry.h
+++ legacy/elementary/src/lib/elm_entry.h
@@ -26,6 +26,7 @@ typedef enum
    ELM_INPUT_PANEL_LAYOUT_IP, /**< IP layout */
    ELM_INPUT_PANEL_LAYOUT_MONTH, /**< Month layout */
    ELM_INPUT_PANEL_LAYOUT_NUMBERONLY, /**< Number Only layout */
+   ELM_INPUT_PANEL_LAYOUT_INVALID, /**< Never use this */
    ELM_INPUT_PANEL_LAYOUT_HEX, /**< Hexadecimal layout */
    ELM_INPUT_PANEL_LAYOUT_TERMINAL, /**< Command-line terminal layout */
    ELM_INPUT_PANEL_LAYOUT_PASSWORD /**< Like normal, but no auto-correct, no auto-capitalization etc. */
