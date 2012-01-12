ed2183c1469d27d476a93c12d6307ff8d0dc1e54
Gustavo Sverzut Barbieri <barbieri@gmail.com>
2012-01-12 02:56:56 +0000

    missing file.
    my bad...
    
    
    SVN revision: 67108

---
+++ legacy/eina/src/tests/cxx_compile_test.cxx
@@ -0,0 +1,34 @@
+/* EINA - EFL data type library
+ * Copyright (C) 2012 ProFUSION embedded systems
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library;
+ * if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include "Eina.h"
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   eina_init();
+   cout << "Eina compiles with C++!";
+   eina_shutdown();
+   return 0;
+}
