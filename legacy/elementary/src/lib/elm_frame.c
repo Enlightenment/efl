06010704037707c840eadf295183e6911f4cd54b
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2012-01-14 04:12:45 +0000

    add elm_frame_collapse_go() to trigger frame animations, elm_frame_collapse_set() now bypasses them
    SVN revision: 67207

--- legacy/elementary/src/lib/elm_frame.c
+++ legacy/elementary/src/lib/elm_frame.c
@@ -293,6 +293,22 @@ elm_frame_collapse_set(Evas_Object *obj, Eina_Bool enable)
    if (!wd) return;
    enable = !!enable;
    if (wd->collapsed == enable) return;
+   edje_object_signal_emit(wd->frm, "elm,action,switch", "elm");
+   edje_object_message_signal_process(wd->frm);
+   wd->collapsed = enable;
+   wd->anim = EINA_FALSE;
+   _sizing_eval(obj);
+}
+
+EAPI void
+elm_frame_collapse_go(Evas_Object *obj, Eina_Bool enable)
+{
+   Widget_Data *wd;
+   ELM_CHECK_WIDTYPE(obj, widtype);
+   wd = elm_widget_data_get(obj);
+   if (!wd) return;
+   enable = !!enable;
+   if (wd->collapsed == enable) return;
    edje_object_signal_emit(wd->frm, "elm,action,toggle", "elm");
    evas_object_smart_callback_add(wd->frm, "recalc", (Evas_Smart_Cb)_recalc, obj);
    wd->collapsed = enable;
