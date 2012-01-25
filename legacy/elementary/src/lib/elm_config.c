2d5761d30b223d11840e7ee10cb2e5b999d85ea8
Iván Briano <sachieru@gmail.com>
2012-01-25 17:15:43 +0000

    Whine if the engine in ELM_ENGINE is not known so users are aware of it
    SVN revision: 67540

--- legacy/elementary/src/lib/elm_config.c
+++ legacy/elementary/src/lib/elm_config.c
@@ -1205,6 +1205,8 @@ _env_get(void)
           eina_stringshare_replace(&_elm_config->engine, ELM_WAYLAND_SHM);
         else if ((!strcasecmp(s, "wayland_egl")))
           eina_stringshare_replace(&_elm_config->engine, ELM_WAYLAND_EGL);
+        else
+          ERR("Unknown engine '%s'.", s);
      }
 
    s = getenv("ELM_VSYNC");
