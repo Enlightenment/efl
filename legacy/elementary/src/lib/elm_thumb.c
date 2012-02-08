61ea7edcd0830ad18c20180071ba51ad2bde3b3d
Jihoon Kim <imfine98@gmail.com>
2012-02-08 01:45:26 +0000

    elm_thumb.c: fix build warning in case without ethumb
    SVN revision: 67744

--- legacy/elementary/src/lib/elm_thumb.c
+++ legacy/elementary/src/lib/elm_thumb.c
@@ -628,6 +628,8 @@ elm_thumb_file_set(Evas_Object *obj, const char *file, const char *key)
 #ifdef HAVE_ELEMENTARY_ETHUMB
    if (((file_replaced) || (key_replaced)) && (evas_object_visible_get(obj)))
      _thumb_show(wd);
+#else
+   (void)key_replaced;
 #endif
 }
 
