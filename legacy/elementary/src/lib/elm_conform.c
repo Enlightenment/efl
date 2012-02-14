5caa46d931fc6d4999e0c3bdd7d05971931f360b
ChunEon Park <hermet@hermet.pe.kr>
2012-02-14 08:18:03 +0000

    elementary/conform - BAD! Missing more contents hookings.
    SVN revision: 67901

--- legacy/elementary/src/lib/elm_conform.c
+++ legacy/elementary/src/lib/elm_conform.c
@@ -597,6 +597,8 @@ elm_conformant_add(Evas_Object *parent)
    elm_widget_theme_hook_set(obj, _theme_hook);
    elm_widget_can_focus_set(obj, EINA_FALSE);
    elm_widget_content_set_hook_set(obj, _content_set_hook);
+   elm_widget_content_get_hook_set(obj, _content_get_hook);
+   elm_widget_content_unset_hook_set(obj, _content_unset_hook);
 
    wd->base = edje_object_add(e);
    _elm_theme_object_set(obj, wd->base, "conformant", "base", "default");
