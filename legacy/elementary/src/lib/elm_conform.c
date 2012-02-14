4c3ccfcd60047f100007783370c5f129328f4cf0
ChunEon Park <hermet@hermet.pe.kr>
2012-02-14 08:00:20 +0000

    elementary/conform - fixed a missing content hooking.
    Thanks Jihoon for reporting. 
    
    
    
    SVN revision: 67900

--- legacy/elementary/src/lib/elm_conform.c
+++ legacy/elementary/src/lib/elm_conform.c
@@ -596,6 +596,7 @@ elm_conformant_add(Evas_Object *parent)
    elm_widget_del_hook_set(obj, _del_hook);
    elm_widget_theme_hook_set(obj, _theme_hook);
    elm_widget_can_focus_set(obj, EINA_FALSE);
+   elm_widget_content_set_hook_set(obj, _content_set_hook);
 
    wd->base = edje_object_add(e);
    _elm_theme_object_set(obj, wd->base, "conformant", "base", "default");
