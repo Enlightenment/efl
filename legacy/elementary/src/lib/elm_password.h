c456a0ae6c508ebdb1ab7048f715092f0b011ada
Sanjeev BA <iamsanjeev@gmail.com>
2012-02-15 05:30:34 +0000

    Use consistent name for the show-last feature.
    Signed-off-by: Sanjeev BA <iamsanjeev@gmail.com>
    
    SVN revision: 67958

--- legacy/elementary/src/lib/elm_password.h
+++ legacy/elementary/src/lib/elm_password.h
@@ -1,7 +1,7 @@
 /**
- * @defgroup Password_last_show Password last input show
+ * @defgroup Password_last_show Password show last
  *
- * Last show feature of password mode enables user to view
+ * Show last feature of password mode enables user to view
  * the last input entered for few seconds before masking it.
  * These functions allow to set this feature in password mode
  * of entry widget and also allow to manipulate the duration
@@ -11,13 +11,14 @@
  */
 
 /**
- * Get show last setting of password mode.
+ * Get the "show last" setting of password mode.
  *
- * This gets the show last input setting of password mode which might be
+ * This gets the "show last" setting of password mode which might be
  * enabled or disabled.
  *
- * @return @c EINA_TRUE, if the last input show setting is enabled, @c EINA_FALSE
- *            if it's disabled.
+ * @return @c EINA_TRUE, if the "show last" setting is enabled, 
+ * @c EINA_FALSE  if it's disabled.
+ *
  * @ingroup Password_last_show
  */
 EAPI Eina_Bool elm_password_show_last_get(void);
@@ -27,25 +28,25 @@ EAPI Eina_Bool elm_password_show_last_get(void);
  *
  * This enables or disables show last setting of password mode.
  *
- * @param password_show_last If EINA_TRUE enables last input show in password mode.
+ * @param password_show_last If EINA_TRUE enables "show last" in password mode.
  * @see elm_password_show_last_timeout_set()
  * @ingroup Password_last_show
  */
 EAPI void      elm_password_show_last_set(Eina_Bool password_show_last);
 
 /**
- * Gets the timeout value in last show password mode.
+ * Gets the timeout value in "show last" password mode.
  *
  * This gets the time out value for which the last input entered in password
  * mode will be visible.
  *
- * @return The timeout value of last show password mode.
+ * @return The timeout value of "show last" password mode.
  * @ingroup Password_last_show
  */
 EAPI double    elm_password_show_last_timeout_get(void);
 
 /**
- * Set's the timeout value in last show password mode.
+ * Set's the timeout value in "show last" password mode.
  *
  * This sets the time out value for which the last input entered in password
  * mode will be visible.
