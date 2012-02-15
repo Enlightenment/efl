e34ee4854f72944f7a2db4427d116725ca4a87a6
ChunEon Park <hermet@hermet.pe.kr>
2012-02-15 04:56:16 +0000

    elementary/factory - api reviewd
    SVN revision: 67956

--- legacy/elementary/src/lib/elm_factory.h
+++ legacy/elementary/src/lib/elm_factory.h
@@ -1,4 +1,5 @@
 EAPI Evas_Object                 *elm_factory_add(Evas_Object *parent);
+//XXX: api name sounds uncertain.
 EAPI void                         elm_factory_maxmin_mode_set(Evas_Object *obj, Eina_Bool enabled);
 EAPI Eina_Bool                    elm_factory_maxmin_mode_get(const Evas_Object *obj);
 EAPI void                         elm_factory_maxmin_reset_set(Evas_Object *obj);
