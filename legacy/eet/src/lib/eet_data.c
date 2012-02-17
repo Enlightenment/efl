026f3439ed9dcaa6993985b3e9d9425ccd6c3fbf
Vincent Torri <vincent.torri@gmail.com>
2012-02-17 21:08:15 +0000

    Eet: remove ugly FMT_* compatibility strings as Evil has now POSIX printf family functions
    SVN revision: 68091

--- legacy/eet/src/lib/eet_data.c
+++ legacy/eet/src/lib/eet_data.c
@@ -21,18 +21,6 @@
 #include "Eet.h"
 #include "Eet_private.h"
 
-#ifdef _WIN32
-# define FMT_CHAR       "%c"
-# define FMT_UCHAR      "%c"
-# define FMT_LONG_LONG  "%I64i"
-# define FMT_ULONG_LONG "%I64u"
-#else
-# define FMT_CHAR       "%hhi"
-# define FMT_UCHAR      "%hhu"
-# define FMT_LONG_LONG  "%lli"
-# define FMT_ULONG_LONG "%llu"
-#endif
-
 /*
  * routines for doing data -> struct and struct -> data conversion
  *
@@ -3108,7 +3096,7 @@ _eet_data_dump_parse(Eet_Dictionary *ed,
                                             if      (!strcmp(tok3, "char:"))
                                               {
                                                  n->type = EET_T_CHAR;
-                                                 sscanf(tok4, FMT_CHAR,
+                                                 sscanf(tok4, "%hhi",
                                                         &(n->data.value.c));
                                               }
                                             else if (!strcmp(tok3, "short:"))
@@ -3126,7 +3114,7 @@ _eet_data_dump_parse(Eet_Dictionary *ed,
                                             else if (!strcmp(tok3, "long_long:"))
                                               {
                                                  n->type = EET_T_LONG_LONG;
-                                                 sscanf(tok4, FMT_LONG_LONG,
+                                                 sscanf(tok4, "%lli",
                                                         &(n->data.value.l));
                                               }
                                             else if (!strcmp(tok3, "float:"))
@@ -3144,7 +3132,7 @@ _eet_data_dump_parse(Eet_Dictionary *ed,
                                             else if (!strcmp(tok3, "uchar:"))
                                               {
                                                  n->type = EET_T_UCHAR;
-                                                 sscanf(tok4, FMT_UCHAR,
+                                                 sscanf(tok4, "%hhu",
                                                         &(n->data.value.uc));
                                               }
                                             else if (!strcmp(tok3, "ushort:"))
@@ -3162,7 +3150,7 @@ _eet_data_dump_parse(Eet_Dictionary *ed,
                                             else if (!strcmp(tok3, "ulong_long:"))
                                               {
                                                  n->type = EET_T_ULONG_LONG;
-                                                 sscanf(tok4, FMT_ULONG_LONG,
+                                                 sscanf(tok4, "%llu",
                                                         &(n->data.value.ul));
                                               }
                                             else if (!strcmp(tok3, "string:"))
@@ -4996,4 +4984,3 @@ eet_data_xattr_cipher_set(const char          *filename,
 
    return ret;
 }
-
