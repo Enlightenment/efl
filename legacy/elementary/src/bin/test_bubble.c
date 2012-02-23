a97767638da8dc7a2db3312b642d7478c6899ff1
Aharon Hillel <a.hillel@partner.samsung.com>
2012-02-23 13:58:12 +0000

    elementary: Updated test_bubble.c to do API testing
    Signed-off-by: Aharon Hillel <a.hillel@partner.samsung.com>
    
    SVN revision: 68323

--- legacy/elementary/src/bin/test_bubble.c
+++ legacy/elementary/src/bin/test_bubble.c
@@ -1,4 +1,5 @@
 #include <Elementary.h>
+#include "test.h"
 #ifdef HAVE_CONFIG_H
 # include "elementary_config.h"
 #endif
@@ -10,25 +11,133 @@ _print_clicked(void *data __UNUSED__, Evas_Object *obj __UNUSED__, void *event_i
    printf("bubble clicked\n");
 }
 
+struct _api_data
+{
+   unsigned int state;  /* What state we are testing       */
+   Evas_Object *win;    /* Parent Window of widgets        */
+   void *box;           /* Use this to get box content     */
+};
+typedef struct _api_data api_data;
+
+enum _api_state
+{
+   BUBBLE_SET_CORNER_1,
+   BUBBLE_SET_CORNER_2,
+   BUBBLE_SET_ICON_CONTENT,
+   API_STATE_LAST
+};
+typedef enum _api_state api_state;
+
+static void
+set_api_state(api_data *api)
+{
+   const Eina_List *items = elm_box_children_get(api->box);
+   if(!eina_list_count(items))
+     return;
+
+   switch(api->state)
+     { /* Put all api-changes under switch */
+      case BUBBLE_SET_CORNER_1:
+         elm_bubble_corner_set(eina_list_nth(items, 0), "bottom_left");
+         elm_object_text_set(elm_object_content_get(eina_list_nth(items, 0)),
+                  "Corner: base (bottom-left) - with icon");
+         elm_bubble_corner_set(eina_list_nth(items, 1), "top_right");
+         elm_object_text_set(elm_object_content_get(eina_list_nth(items, 1)),
+                  "Corner: base (top-right) - no icon");
+         break;
+
+      case BUBBLE_SET_CORNER_2:
+         elm_bubble_corner_set(eina_list_nth(items, 0), "top_right");
+         elm_object_text_set(elm_object_content_get(eina_list_nth(items, 0)),
+                  "Corner: base (top-right) - with icon");
+         elm_bubble_corner_set(eina_list_nth(items, 1), "bottom_left");
+         elm_object_text_set(elm_object_content_get(eina_list_nth(items, 1)),
+                  "Corner: base (bottom-left) - no icon");
+         break;
+
+      case BUBBLE_SET_ICON_CONTENT:
+           {
+              char buf[PATH_MAX];
+              Evas_Object *ct, *ic = elm_icon_add(api->win);
+
+              snprintf(buf, sizeof(buf), "%s/images/logo_small.png", elm_app_data_dir_get());
+              elm_icon_file_set(ic, buf, NULL);
+              elm_icon_scale_set(ic, 0, 0);
+              elm_object_content_set(eina_list_nth(items, 0), ic);
+              ct = elm_label_add(api->win);
+              elm_object_text_set(ct, "Using icon as top-bubble content");
+              elm_object_content_set(eina_list_nth(items, 1), ct);
+              evas_object_size_hint_align_set(ic, 0.5, 0.5);
+              evas_object_show(ic);
+           }
+         break;
+
+      case API_STATE_LAST:
+
+         break;
+      default:
+         return;
+     }
+}
+
+static void
+_api_bt_clicked(void *data, Evas_Object *obj, void *event_info __UNUSED__)
+{  /* Will add here a SWITCH command containing code to modify test-object */
+   /* in accordance a->state value. */
+   api_data *a = data;
+   char str[128];
+
+   printf("clicked event on API Button: api_state=<%d>\n", a->state);
+   set_api_state(a);
+   a->state++;
+   sprintf(str, "Next API function (%u)", a->state);
+   elm_object_text_set(obj, str);
+   elm_object_disabled_set(obj, a->state == API_STATE_LAST);
+}
+
+static void
+_cleanup_cb(void *data, Evas *e __UNUSED__, Evas_Object *obj __UNUSED__, void *event_info __UNUSED__)
+{
+   free(data);
+}
+
+
 void
 test_bubble(void *data __UNUSED__, Evas_Object *obj __UNUSED__, void *event_info __UNUSED__)
 {
-   Evas_Object *win, *bg, *bx, *ic, *bb, *ct;
+   Evas_Object *win, *bg, *bx, *ic, *bb, *ct, *bxx, *bt;
    char buf[PATH_MAX];
+   api_data *api = calloc(1, sizeof(api_data));
 
    win = elm_win_add(NULL, "box-vert", ELM_WIN_BASIC);
+   api->win = win;
    elm_win_title_set(win, "Bubble");
    elm_win_autodel_set(win, EINA_TRUE);
+   evas_object_event_callback_add(win, EVAS_CALLBACK_FREE, _cleanup_cb, api);
 
    bg = elm_bg_add(win);
    elm_win_resize_object_add(win, bg);
    evas_object_size_hint_weight_set(bg, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
    evas_object_show(bg);
 
+   bxx = elm_box_add(win);
+   elm_win_resize_object_add(win, bxx);
+   evas_object_size_hint_weight_set(bxx, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+   evas_object_show(bxx);
+
    bx = elm_box_add(win);
-   elm_win_resize_object_add(win, bx);
    evas_object_size_hint_weight_set(bx, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+   api->box = bx;
    evas_object_show(bx);
+
+   bt = elm_button_add(win);
+   elm_object_text_set(bt, "Next API function");
+   evas_object_smart_callback_add(bt, "clicked", _api_bt_clicked, (void *) api);
+   elm_box_pack_end(bxx, bt);
+   elm_object_disabled_set(bt, api->state == API_STATE_LAST);
+   evas_object_show(bt);
+
+   elm_box_pack_end(bxx, bx);
 
    ic = elm_icon_add(win);
    snprintf(buf, sizeof(buf), "%s/images/logo_small.png", elm_app_data_dir_get());
