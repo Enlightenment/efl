a45eee11ee8e9a95dade8714fff4fa0a7382cbc8
Daniel Juyung Seo <juyung.seo@samsung.com>
2012-03-06 10:59:33 +0000

    elm elm_gen_common.h: Use proper doxygen keywords.
    Signed-off-by: Daniel Juyung Seo <juyung.seo@samsung.com>
    
    SVN revision: 68804

--- legacy/elementary/src/lib/elm_gen_common.h
+++ legacy/elementary/src/lib/elm_gen_common.h
@@ -45,7 +45,7 @@ struct Elm_Gen_Item
    Ecore_Timer              *long_timer;
    int                       relcount;
    int                       walking;
-   int                       generation; /* a generation of an item. when the item is created, this value is set to the value of genlist generation. this value will be decreased when the item is going to be deleted */
+   int                       generation; /**< a generation of an item. when the item is created, this value is set to the value of genlist generation. this value will be decreased when the item is going to be deleted */
    const char               *mouse_cursor;
 
    struct
@@ -66,12 +66,12 @@ struct Elm_Gen_Item
    Eina_Bool                 realized : 1;
    Eina_Bool                 selected : 1;
    Eina_Bool                 highlighted : 1;
-   Eina_Bool                 dragging : 1; /* this is set true when an item is being dragged. this is set false on multidown/mouseup/mousedown. when this is true, the item should not be unrealized. or evas mouse down/up event will be corrupted. */
+   Eina_Bool                 dragging : 1; /**< this is set true when an item is being dragged. this is set false on multidown/mouseup/mousedown. when this is true, the item should not be unrealized. or evas mouse down/up event will be corrupted. */
    Eina_Bool                 down : 1;
    Eina_Bool                 group : 1;
    Eina_Bool                 reorder : 1;
-   Eina_Bool                 mode_set : 1; /* item uses style mode for highlight/select */
-   Eina_Bool                 flipped : 1; /* a flag that shows the flip status of the item. */
+   Eina_Bool                 mode_set : 1; /**< item uses style mode for highlight/select */
+   Eina_Bool                 flipped : 1; /**< a flag that shows the flip status of the item. */
 };
 
 typedef struct _Pan Pan;
@@ -85,15 +85,15 @@ struct _Pan
 struct _Widget_Data
 {
    Eina_Inlist_Sorted_State      *state;
-   Evas_Object                   *obj; /* the genlist object */
-   Evas_Object                   *scr; /* a smart scroller object which is used internally in genlist */
-   Evas_Object                   *pan_smart; /* "elm_genlist_pan" evas smart object. this is an extern pan of smart scroller(scr). */
-   Eina_List                     *selected; /* a list of selected items */
-   Eina_List                     *group_items; /* a list of groups index items */
-   Eina_Inlist                   *items; /* an inlist of all items */
-   Elm_Gen_Item                  *reorder_it; /* item currently being repositioned */
+   Evas_Object                   *obj; /**< the genlist object */
+   Evas_Object                   *scr; /**< a smart scroller object which is used internally in genlist */
+   Evas_Object                   *pan_smart; /**< "elm_genlist_pan" evas smart object. this is an extern pan of smart scroller(scr). */
+   Eina_List                     *selected; /**< a list of selected items */
+   Eina_List                     *group_items; /**< a list of groups index items */
+   Eina_Inlist                   *items; /**< an inlist of all items */
+   Elm_Gen_Item                  *reorder_it; /**< item currently being repositioned */
    Elm_Object_Item               *last_selected_item;
-   Pan                           *pan; /* pan_smart object's smart data */
+   Pan                           *pan; /**< pan_smart object's smart data */
    Ecore_Job                     *calc_job;
    int                            walking;
    int                            item_width, item_height;
@@ -101,28 +101,28 @@ struct _Widget_Data
    int                            minw, minh;
    unsigned int                   item_count;
    Evas_Coord                     pan_x, pan_y;
-   Eina_Bool                      reorder_mode : 1; /* a flag for reorder mode enable/disable */
+   Eina_Bool                      reorder_mode : 1; /**< a flag for reorder mode enable/disable */
    Eina_Bool                      on_hold : 1;
-   Eina_Bool                      multi : 1; /* a flag for item multi selection */
+   Eina_Bool                      multi : 1; /**< a flag for item multi selection */
    Eina_Bool                      no_select : 1;
    Eina_Bool                      wasselected : 1;
    Eina_Bool                      always_select : 1;
-   Eina_Bool                      clear_me : 1; /* a flag whether genlist is marked as to be cleared or not. if this flag is true, genlist clear was already deferred.  */
+   Eina_Bool                      clear_me : 1; /**< a flag whether genlist is marked as to be cleared or not. if this flag is true, genlist clear was already deferred.  */
    Eina_Bool                      h_bounce : 1;
    Eina_Bool                      v_bounce : 1;
    Ecore_Cb                       del_cb, calc_cb, sizing_cb;
    Ecore_Cb                       clear_cb;
    ////////////////////////////////////
-   Eina_Inlist                   *blocks; /* an inlist of all blocks. a block consists of a certain number of items. maximum number of items in a block is 'max_items_per_block'. */
+   Eina_Inlist                   *blocks; /**< an inlist of all blocks. a block consists of a certain number of items. maximum number of items in a block is 'max_items_per_block'. */
    Evas_Coord                     reorder_old_pan_y, w, h, realminw, prev_viewport_w;
    Ecore_Job                     *update_job;
    Ecore_Idle_Enterer            *queue_idle_enterer;
    Ecore_Idler                   *must_recalc_idler;
    Eina_List                     *queue;
    Elm_Gen_Item                  *show_item, *anchor_item, *mode_item, *reorder_rel, *expanded_item;
-   Eina_Inlist                   *item_cache; /* an inlist of edje object it cache. */
+   Eina_Inlist                   *item_cache; /**< an inlist of edje object it cache. */
    Evas_Coord                     anchor_y;
-   Evas_Coord                     reorder_start_y; /* reorder it's initial y coordinate in the pan. */
+   Evas_Coord                     reorder_start_y; /**< reorder it's initial y coordinate in the pan. */
    Elm_List_Mode                  mode;
    Ecore_Timer                   *multi_timer, *scr_hold_timer;
    Ecore_Animator                *reorder_move_animator;
@@ -135,7 +135,7 @@ struct _Widget_Data
    Eina_Bool                      multi_timeout : 1;
    Eina_Bool                      multitouched : 1;
    Eina_Bool                      longpressed : 1;
-   Eina_Bool                      bring_in : 1; /* a flag to describe the scroll animation. (show, bring in) */
+   Eina_Bool                      bring_in : 1; /**< a flag to describe the scroll animation. (show, bring in) */
    Eina_Bool                      compress : 1;
    Eina_Bool                      height_for_width : 1;
    Eina_Bool                      homogeneous : 1;
@@ -144,22 +144,22 @@ struct _Widget_Data
    Eina_Bool                      reorder_pan_move : 1;
    Eina_Bool                      auto_scroll_enabled : 1;
    Eina_Bool                      pan_changed : 1;
-   Eina_Bool                      requeued : 1; /* this is set to EINA_TRUE when the item is re-queued. this happens when the item is un-queued but the rel item is still in the queue. this item will be processed later. */
-   Eina_Bool                      check_scroll : 1; /* this flag means genlist is supposed to be scrolled. if this flag is set to EINA_TRUE, genlist checks whether it's ok to scroll genlist now or not. */
+   Eina_Bool                      requeued : 1; /**< this is set to EINA_TRUE when the item is re-queued. this happens when the item is un-queued but the rel item is still in the queue. this item will be processed later. */
+   Eina_Bool                      check_scroll : 1; /**< this flag means genlist is supposed to be scrolled. if this flag is set to EINA_TRUE, genlist checks whether it's ok to scroll genlist now or not. */
    struct
      {
         Evas_Coord x, y;
      } history[SWIPE_MOVES];
    int                            multi_device;
    int                            item_cache_count;
-   int                            item_cache_max; /* maximum number of cached items */
+   int                            item_cache_max; /**< maximum number of cached items */
    int                            movements;
-   int                            max_items_per_block; /* maximum number of items per block */
-   double                         longpress_timeout; /* longpress timeout. this value comes from _elm_config by default. this can be changed by elm_genlist_longpress_timeout_set() */
-   int                            generation; /* a generation of genlist. when genlist is cleared, this value will be increased and a new generation will start */
+   int                            max_items_per_block; /**< maximum number of items per block */
+   double                         longpress_timeout; /**< longpress timeout. this value comes from _elm_config by default. this can be changed by elm_genlist_longpress_timeout_set() */
+   int                            generation; /**< a generation of genlist. when genlist is cleared, this value will be increased and a new generation will start */
    Eina_Compare_Cb                item_compare_cb;
    Eina_Compare_Cb                item_compare_data_cb;
-   Elm_Genlist_Item_Scrollto_Type scrollto_type; /* a scrollto type which remembers where to scroll ex) in, top, middle */
+   Elm_Genlist_Item_Scrollto_Type scrollto_type; /**< a scrollto type which remembers where to scroll ex) in, top, middle */
 
    /* The stuff below directly come from gengrid without any thinking */
    unsigned int                   nmax;
