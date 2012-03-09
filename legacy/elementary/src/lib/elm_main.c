2e62935285d5d7be651973164753aa2108bae0df
Jiyoun Park <jijibe99@gmail.com>
2012-03-09 11:16:58 +0000

    remove deprecated function related with elm_object_text_part_XX
    SVN revision: 69088

--- legacy/elementary/src/lib/elm_main.c
+++ legacy/elementary/src/lib/elm_main.c
@@ -934,20 +934,6 @@ elm_object_scale_get(const Evas_Object *obj)
 }
 
 EAPI void
-elm_object_text_part_set(Evas_Object *obj, const char *part, const char *label)
-{
-   EINA_SAFETY_ON_NULL_RETURN(obj);
-   elm_widget_text_part_set(obj, part, label);
-}
-
-EAPI const char *
-elm_object_text_part_get(const Evas_Object *obj, const char *part)
-{
-   EINA_SAFETY_ON_NULL_RETURN_VAL(obj, NULL);
-   return elm_widget_text_part_get(obj, part);
-}
-
-EAPI void
 elm_object_part_text_set(Evas_Object *obj, const char *part, const char *label)
 {
    EINA_SAFETY_ON_NULL_RETURN(obj);
