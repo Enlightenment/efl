d829bf73ce4fdec34f338a162d962b2cb8079969
Jonas M. Gastal <jgastal@profusion.mobi>
2012-03-09 17:31:47 +0000

    Ecore idlers doc bump.
    SVN revision: 69112

--- legacy/ecore/src/lib/ecore/Ecore.h
+++ legacy/ecore/src/lib/ecore/Ecore.h
@@ -1271,14 +1271,15 @@ EAPI char *ecore_timer_dump(void);
 /**
  * @defgroup Ecore_Idle_Group Ecore Idle functions
  *
- * Callbacks that are called when the program enters or exits an
- * idle state.
+ * The idler functionality in Ecore allows for callbacks to be called when the
+ * program isn't handling @ref Ecore_Event_Group "events", @ref Ecore_Time_Group
+ * "timers" or @ref Ecore_FD_Handler_Group "fd handlers".
  *
- * The ecore main loop enters an idle state when it is waiting for
- * timers to time out, data to come in on a file descriptor or any
- * other event to occur.  You can set callbacks to be called when
- * the main loop enters an idle state, during an idle state or just
- * after the program wakes up.
+ * There are three types of idlers: Enterers, Idlers(proper) and Exiters. They
+ * are called, respectively, when the program is about to enter an idle state,
+ * when the program is in an idle state and when the program has just left an
+ * idle state and will begin processing @ref Ecore_Event_Group "events", @ref
+ * Ecore_Time_Group "timers" or @ref Ecore_FD_Handler_Group "fd handlers".
  *
  * Enterer callbacks are good for updating your program's state, if
  * it has a state engine.  Once all of the enterer handlers are
@@ -1288,13 +1289,12 @@ EAPI char *ecore_timer_dump(void);
  * enterer handlers.  They are useful for interfaces that require
  * polling and timers would be too slow to use.
  *
+ * Exiter callbacks are called when the main loop wakes up from an idle state.
+ *
  * If no idler callbacks are specified, then the process literally
  * goes to sleep.  Otherwise, the idler callbacks are called
  * continuously while the loop is "idle", using as much CPU as is
  * available to the process.
- *
- * Exiter callbacks are called when the main loop wakes up from an
- * idle state.
  *
  * @note Idle state doesn't mean that the @b program is idle, but
  * that the <b>main loop</b> is idle. It doesn't have any timers,
