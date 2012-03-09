37a079ce852cf9b31e46ed4aa6e6a2dd44d29c81
Daniel Juyung Seo <juyung.seo@samsung.com>
2012-03-09 11:39:01 +0000

    elm elm_gen.h: Updated documentation and fixed formatting.
    Signed-off-by: Daniel Juyung Seo <juyung.seo@samsung.com>
    
    SVN revision: 69093

--- legacy/elementary/src/lib/elm_gen.h
+++ legacy/elementary/src/lib/elm_gen.h
@@ -44,18 +44,19 @@ typedef void                          (*Elm_Gen_Item_Del_Cb)(void *data, Evas_Ob
 
 struct _Elm_Gen_Item_Class
 {
-   int           version;  /**< Set by elementary if you alloc an item class using elm_gengrid_item_class_new() or elm_genlist_item_class_new(), or if you set your own class (must be const) then set it to ELM_GENGRID_ITEM_CLASS_VERSION */
+   int           version;  /**< Set by elementary if you alloc an item class using elm_genlist/gengrid_item_class_new(), or if you set your own class (must be const) then set it to ELM_GENLIST/GENGRID_ITEM_CLASS_VERSION */
    unsigned int  refcount; /**< Set it to 0 if you use your own const class, or its managed for you by class ref/unref calls */
    Eina_Bool     delete_me : 1; /**< Leave this alone - set it to 0 if you have a const class of your own */
    const char   *item_style; /**< Name of the visual style to use for this item. If you don't know use "default" */
    const char   *mode_item_style; /**< Style used if item is set to a specific mode. @see elm_genlist_item_mode_set() or NULL if you don't care. currently it's used only in genlist. */
    const char   *edit_item_style; /**< Style to use when in edit mode, or NULL if you don't care. currently it's used only in genlist. */
-   struct {
-      Elm_Gen_Item_Text_Get_Cb    text_get; /**< Text fetching class function for gengrid/list item classes.*/
-      Elm_Gen_Item_Content_Get_Cb content_get; /**< Content fetching class function for gengrid/list item classes. */
-      Elm_Gen_Item_State_Get_Cb   state_get; /**< State fetching class function for gengrid/list item classes. */
-      Elm_Gen_Item_Del_Cb         del; /**< Deletion class function for gengrid/list item classes. */
-   } func;
+   struct
+     {
+        Elm_Gen_Item_Text_Get_Cb    text_get; /**< Text fetching class function for genlist/gengrid item classes.*/
+        Elm_Gen_Item_Content_Get_Cb content_get; /**< Content fetching class function for genlist/gengrid item classes. */
+        Elm_Gen_Item_State_Get_Cb   state_get; /**< State fetching class function for genlist/gengrid item classes. */
+        Elm_Gen_Item_Del_Cb         del; /**< Deletion class function for genlist/gengrid item classes. */
+     } func;
 }; /**< #Elm_Gen_Item_Class member definitions */
 
 #define ELM_GEN_ITEM_CLASS_VERSION 2
