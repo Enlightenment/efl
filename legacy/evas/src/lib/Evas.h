dc6c19e47e8d09b7f6c0a094567a86a274efb696
Jonas M. Gastal <jgastal@profusion.mobi>
2012-03-22 16:57:16 +0000

    Those are not currently implemented, so documenting them is confusing to users.
    SVN revision: 69568

--- legacy/evas/src/lib/Evas.h
+++ legacy/evas/src/lib/Evas.h
@@ -810,10 +810,10 @@ typedef enum _Evas_Render_Op
    EVAS_RENDER_BLEND_REL = 1, /**< d = d*(1 - sa) + s*da */
    EVAS_RENDER_COPY = 2, /**< d = s */
    EVAS_RENDER_COPY_REL = 3, /**< d = s*da */
-   EVAS_RENDER_ADD = 4, /**< d = d + s */
+   EVAS_RENDER_ADD = 4, /* d = d + s */
    EVAS_RENDER_ADD_REL = 5, /**< d = d + s*da */
    EVAS_RENDER_SUB = 6, /**< d = d - s */
-   EVAS_RENDER_SUB_REL = 7, /**< d = d - s*da */
+   EVAS_RENDER_SUB_REL = 7, /* d = d - s*da */
    EVAS_RENDER_TINT = 8, /**< d = d*s + d*(1 - sa) + s*(1 - da) */
    EVAS_RENDER_TINT_REL = 9, /**< d = d*(1 - sa + s) */
    EVAS_RENDER_MASK = 10, /**< d = d*sa */
