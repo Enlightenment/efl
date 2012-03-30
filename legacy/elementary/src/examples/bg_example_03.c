13eaab301ff00afdf48d8e7c4a73f18f4f95afcc
Daniel Juyung Seo <seojuyung2@gmail.com>
2012-03-30 06:33:57 +0000

    elm bg_example_03.c: Fixed window size to see the contents properly.
    SVN revision: 69781

--- legacy/elementary/src/examples/bg_example_03.c
+++ legacy/elementary/src/examples/bg_example_03.c
@@ -162,7 +162,7 @@ elm_main(int argc, char **argv)
 
    evas_object_size_hint_min_set(bg, 160, 160);
    evas_object_size_hint_max_set(bg, 640, 640);
-   evas_object_resize(win, 320, 320);
+   evas_object_resize(win, 460, 320);
    evas_object_show(win);
 
    elm_run();
