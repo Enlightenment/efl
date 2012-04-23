e8348bf8020587a4be2c99d0ab4b7964507cbbb6
Tom Hacohen <tom@stosb.com>
2012-04-23 08:10:00 +0000

    Eobj: Fixed windows support.
    DAMN, Windows is broken...
    Thanks to vtorri.
    
    SVN revision: 70409

--- legacy/eobj/lib/Eobj.h
+++ legacy/eobj/lib/Eobj.h
@@ -4,6 +4,32 @@
 #include <stdarg.h>
 #include <Eina.h>
 
+#ifdef EAPI
+# undef EAPI
+#endif
+
+#ifdef _WIN32
+# ifdef _EOBJ_BUILD
+#  ifdef DLL_EXPORT
+#   define EAPI __declspec(dllexport)
+#  else
+#   define EAPI
+#  endif /* ! DLL_EXPORT */
+# else
+#  define EAPI __declspec(dllimport)
+# endif /* ! _EOBJ_BUILD */
+#else
+# ifdef __GNUC__
+#  if __GNUC__ >= 4
+#   define EAPI __attribute__ ((visibility("default")))
+#  else
+#   define EAPI
+#  endif
+# else
+#  define EAPI
+# endif
+#endif /* ! _WIN32 */
+
 /**
  * @var _eobj_class_creation_lock
  * This variable is used for locking purposes in the class_get function
