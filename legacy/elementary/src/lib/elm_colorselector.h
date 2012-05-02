93acd828383880aae117ae10cf2b1173cc08b815
Gustavo Lima Chaves <glima@profusion.mobi>
2012-05-02 17:01:53 +0000

    [Elm] Colorselector now a layout. There are issues with its theme, though. Later on I'll get back to it.
    SVN revision: 70666

--- legacy/elementary/src/lib/elm_colorselector.h
+++ legacy/elementary/src/lib/elm_colorselector.h
@@ -5,19 +5,26 @@
  * @image html img/widget/colorselector/preview-00.png
  * @image latex img/widget/colorselector/preview-00.eps
  *
- * A ColorSelector is a color selection widget. It allows application to set a
- * series of colors.It also allows to load/save colors from/to config with a
- * unique identifier, by default, the colors are loaded/saved from/to config
- * using "default" identifier.
- * The colors can be picked by user from the color set by clicking on individual color
- * item on the palette or by selecting it from selector.
+ * A ColorSelector is a color selection widget. It allows application
+ * to set a series of colors.It also allows to load/save colors
+ * from/to config with a unique identifier, by default, the colors are
+ * loaded/saved from/to config using "default" identifier. The colors
+ * can be picked by user from the color set by clicking on individual
+ * color item on the palette or by selecting it from selector.
  *
- * Signals that you can add callbacks for are:
- * - "changed" - When the color value changes on selector(event_info is NULL).
- * - "color,item,selected" - When user clicks on color item. The event_info parameter
- * of the callback will be the selected color item.
- * - "color,item,longpressed" - When user long presses on color item. The event info
- * parameter of the callback contains selected color item.
+ * This widget inherits from the @ref Layout one, so that all the
+ * functions acting on it also work for check objects.
+ *
+ * This widget emits the following signals, besides the ones sent from
+ * @ref Layout:
+ * - @c "changed" - When the color value changes on selector
+ *   event_info is NULL.
+ * - @c "color,item,selected" - When user clicks on color item. The
+ *   event_info parameter of the callback will be the selected color
+ *   item.
+ * - @c "color,item,longpressed" - When user long presses on color
+ *   item. The event info parameter of the callback contains selected
+ *   color item.
  *
  * See @ref tutorial_colorselector.
  * @{
