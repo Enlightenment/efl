cf09d45c3b8bcbb50c251c1ff9b8859976279621
Tom Hacohen <tom@stosb.com>
2012-05-13 08:37:09 +0000

    Elm ctxpopup: remove useless code until we actually support mirroring.
    Currently there's semi-ready code (1 liner) that does nothing. Hermet, you
    promised you'll add ui-mirroring support AGES ago when this widget got in.
    
    SVN revision: 70960

--- legacy/elementary/src/lib/elc_ctxpopup.c
+++ legacy/elementary/src/lib/elc_ctxpopup.c
@@ -738,13 +738,11 @@ static void
 _theme_hook(Evas_Object *obj)
 {
    Widget_Data *wd;
-   Eina_Bool rtl;
 
    wd = elm_widget_data_get(obj);
    if (!wd) return;
 
    _elm_widget_mirrored_reload(obj);
-   rtl = elm_widget_mirrored_get(obj);
 
    _elm_theme_object_set(obj, wd->bg, "ctxpopup", "bg",
                          elm_widget_style_get(obj));
