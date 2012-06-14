f5d9c7bf2f7558b150b8f2510cec6cf2feb3bc87
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2012-06-14 07:28:23 +0000

    +efreet_dirs_reset()
    SVN revision: 72104

--- legacy/efreet/src/lib/efreet_base.c
+++ legacy/efreet/src/lib/efreet_base.c
@@ -187,6 +187,22 @@ efreet_hostname_get(void)
     return hostname;
 }
 
+EAPI void
+efreet_dirs_reset(void)
+{
+   const char *s;
+   eina_stringshare_replace(&hostname, NULL);
+   eina_stringshare_replace(&xdg_desktop_dir, NULL);
+   eina_stringshare_replace(&xdg_cache_home, NULL);
+   eina_stringshare_replace(&xdg_config_home, NULL);
+   eina_stringshare_replace(&xdg_data_home, NULL);
+   eina_stringshare_replace(&efreet_home_dir, NULL);
+   EINA_LIST_FREE(xdg_data_dirs, s)
+     eina_stringshare_del(s);
+   EINA_LIST_FREE(xdg_config_dirs, s)
+     eina_stringshare_del(s);
+}
+
 /**
  * @internal
  * @param key The environemnt key to lookup
