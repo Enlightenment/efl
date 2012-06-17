2256af3421833945a78503a42b7341f1adf553f9
Carsten Haitzler <raster@rasterman.com>
2012-06-17 02:23:49 +0000

    fix gl engine bitmap fonts :)
    SVN revision: 72256

--- legacy/evas/src/modules/engines/gl_common/evas_gl_font.c
+++ legacy/evas/src/modules/engines/gl_common/evas_gl_font.c
@@ -23,7 +23,8 @@ evas_gl_font_texture_new(void *context, RGBA_Font_Glyph *fg)
    nw = ((w + 3) / 4) * 4;
    ndata = alloca(nw *h);
    if (!ndata) return NULL;
-   if (fg->glyph_out->bitmap.num_grays == 256)
+   if ((fg->glyph_out->bitmap.num_grays == 256) &&
+       (fg->glyph_out->bitmap.pixel_mode == FT_PIXEL_MODE_GRAY))
      {
 	int x, y;
 	DATA8 *p1, *p2;
@@ -40,7 +41,7 @@ evas_gl_font_texture_new(void *context, RGBA_Font_Glyph *fg)
 	       }
 	  }
      }
-   else if (fg->glyph_out->bitmap.num_grays == 0)
+   else
      {
 	DATA8 *tmpbuf = NULL, *dp, *tp, bits;
 	int bi, bj, end;
