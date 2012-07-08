317f074fc1a497b9906bcd1f11535c727f42372f
Tom Hacohen <tom@stosb.com>
2012-07-08 10:31:34 +0000

    Elm smart/pan: Check obj != NULL before fetching smart data.
    Patch by Daniel Zaoui.
    
    SVN revision: 73464

--- legacy/elementary/src/lib/els_pan.c
+++ legacy/elementary/src/lib/els_pan.c
@@ -2,7 +2,7 @@
 #include "elm_priv.h"
 
 #define SMART_NAME "elm_pan"
-#define API_ENTRY Smart_Data *sd; sd = evas_object_smart_data_get(obj); if ((!obj) || (!sd) || (evas_object_type_get(obj) && strcmp(evas_object_type_get(obj), SMART_NAME)))
+#define API_ENTRY Smart_Data *sd = NULL; if ((!obj) || (!(sd = evas_object_smart_data_get(obj))) || (evas_object_type_get(obj) && strcmp(evas_object_type_get(obj), SMART_NAME)))
 #define INTERNAL_ENTRY Smart_Data *sd; sd = evas_object_smart_data_get(obj); if (!sd) return;
 typedef struct _Smart_Data Smart_Data;
 
