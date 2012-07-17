4e0345f4eeaeb68d9a045154c304cb5e112a4c77
Carsten Haitzler <raster@rasterman.com>
2012-07-17 08:42:25 +0000

    hell no. printf. not eina-log. this is test exampels, not debug from an app. they printf as OUTPUT. alkso u broke build:
    /home/raster/C/elm/src/bin/test_genlist.c:2862: undefined reference to
    _elm_log_dom'
    
    of course there is no symbol exposed that is an INTERNAL LOG DOMAIN to
    elm! it's for elm not for outside.
    
    
    
    SVN revision: 73957

--- legacy/elementary/src/bin/test.h
+++ legacy/elementary/src/bin/test.h
@@ -18,9 +18,8 @@
    } while(0)
 #endif
 
-extern int _elm_log_dom;
-#define CRITICAL(...) EINA_LOG_DOM_CRIT(_elm_log_dom, __VA_ARGS__)
-#define ERR(...)      EINA_LOG_DOM_ERR (_elm_log_dom, __VA_ARGS__)
-#define WRN(...)      EINA_LOG_DOM_WARN(_elm_log_dom, __VA_ARGS__)
-#define INF(...)      EINA_LOG_DOM_INFO(_elm_log_dom, __VA_ARGS__)
-#define DBG(...)      EINA_LOG_DOM_DBG (_elm_log_dom, __VA_ARGS__)
+#define CRITICAL(...) printf(__VA_ARGS__)
+#define ERR(...)      printf(__VA_ARGS__)
+#define WRN(...)      printf(__VA_ARGS__)
+#define INF(...)      printf(__VA_ARGS__)
+#define DBG(...)      printf(__VA_ARGS__)
