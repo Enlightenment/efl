fc15eee33d8c8aca2ee10c05426a6ef0792f74a8
Christopher Michael <cpmichael1@comcast.net>
2012-07-31 06:04:52 +0000

    Elm: Update label example to move labels so they do not overlap and actually call evas_object_move on one that was missing it.
    SVN revision: 74614

--- legacy/elementary/src/examples/label_example_01.c
+++ legacy/elementary/src/examples/label_example_01.c
@@ -24,6 +24,7 @@ elm_main(int argc, char **argv)
    elm_label_slide_duration_set(label, 3);
    elm_label_slide_set(label, EINA_TRUE);
    elm_object_style_set(label, "slide_bounce");
+   evas_object_move(label, 0, 10);
    evas_object_resize(label, 200, 15);
    evas_object_show(label);
 
@@ -33,7 +34,7 @@ elm_main(int argc, char **argv)
                                "too long, but maybe not.");
    elm_label_ellipsis_set(label2, EINA_TRUE);
    evas_object_resize(label2, 200, 15);
-   evas_object_move(label2, 0, 15);
+   evas_object_move(label2, 0, 30);
    evas_object_show(label2);
 
    label3 = elm_label_add(win);
@@ -42,7 +43,7 @@ elm_main(int argc, char **argv)
    elm_label_line_wrap_set(label3, ELM_WRAP_CHAR);
    elm_label_ellipsis_set(label3, EINA_TRUE);
    evas_object_resize(label3, 200, 15);
-   evas_object_move(label3, 0, 30);
+   evas_object_move(label3, 0, 50);
    evas_object_show(label3);
 
    label4 = elm_label_add(win);
@@ -50,15 +51,15 @@ elm_main(int argc, char **argv)
                                "for no reason other than that we can.");
    elm_label_line_wrap_set(label4, ELM_WRAP_CHAR);
    evas_object_resize(label4, 200, 30);
-   evas_object_move(label4, 0, 45);
+   evas_object_move(label4, 0, 80);
    evas_object_show(label4);
 
    label5 = elm_label_add(win);
    elm_object_text_set(label5, "And for this label we choose a different text, "
                                "for no reason other than that we can.");
    elm_label_line_wrap_set(label5, ELM_WRAP_WORD);
-   evas_object_resize(label5, 200, 30);
-   evas_object_move(label5, 0, 75);
+   evas_object_resize(label5, 200, 40);
+   evas_object_move(label5, 0, 110);
    evas_object_show(label5);
 
    label6 = elm_label_add(win);
@@ -66,10 +67,10 @@ elm_main(int argc, char **argv)
    elm_object_style_set(label6, "marker");
    evas_object_color_set(label6, 255, 0, 0, 255);
    evas_object_resize(label6, 200, 15);
-   evas_object_move(label6, 0, 105);
+   evas_object_move(label6, 0, 140);
    evas_object_show(label6);
 
-   evas_object_resize(win, 200, 125);
+   evas_object_resize(win, 200, 170);
    evas_object_show(win);
 
    elm_run();
