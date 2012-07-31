81ca96c96afde033369f5e9627fb97ed98d4aa41
Gustavo Lima Chaves <glima@profusion.mobi>
2012-07-31 22:06:47 +0000

    [elm] Separator now inheritable.
    SVN revision: 74690

--- legacy/elementary/src/lib/elm_separator.c
+++ legacy/elementary/src/lib/elm_separator.c
@@ -1,47 +1,11 @@
 #include <Elementary.h>
 #include "elm_priv.h"
-#include "elm_widget_layout.h"
+#include "elm_widget_separator.h"
 
-static const char SEPARATOR_SMART_NAME[] = "elm_separator";
+EAPI const char ELM_SEPARATOR_SMART_NAME[] = "elm_separator";
 
-typedef struct _Elm_Separator_Smart_Data Elm_Separator_Smart_Data;
-
-struct _Elm_Separator_Smart_Data
-{
-   Elm_Layout_Smart_Data base;
-
-   Eina_Bool             horizontal : 1;
-};
-
-#define ELM_SEPARATOR_DATA_GET(o, sd) \
-  Elm_Separator_Smart_Data * sd = evas_object_smart_data_get(o)
-
-#define ELM_SEPARATOR_DATA_GET_OR_RETURN(o, ptr)     \
-  ELM_SEPARATOR_DATA_GET(o, ptr);                    \
-  if (!ptr)                                          \
-    {                                                \
-       CRITICAL("No widget data for object %p (%s)", \
-                o, evas_object_type_get(o));         \
-       return;                                       \
-    }
-
-#define ELM_SEPARATOR_DATA_GET_OR_RETURN_VAL(o, ptr, val) \
-  ELM_SEPARATOR_DATA_GET(o, ptr);                         \
-  if (!ptr)                                               \
-    {                                                     \
-       CRITICAL("No widget data for object %p (%s)",      \
-                o, evas_object_type_get(o));              \
-       return val;                                        \
-    }
-
-#define ELM_SEPARATOR_CHECK(obj)                                             \
-  if (!obj || !elm_widget_type_check((obj), SEPARATOR_SMART_NAME, __func__)) \
-    return
-
-/* Inheriting from elm_layout. Besides, we need no more than what is
- * there */
 EVAS_SMART_SUBCLASS_NEW
-  (SEPARATOR_SMART_NAME, _elm_separator, Elm_Layout_Smart_Class,
+  (ELM_SEPARATOR_SMART_NAME, _elm_separator, Elm_Separator_Smart_Class,
   Elm_Layout_Smart_Class, elm_layout_smart_class_get, NULL);
 
 static Eina_Bool
@@ -54,7 +18,8 @@ _elm_separator_smart_theme(Evas_Object *obj)
    else
      eina_stringshare_replace(&(ELM_LAYOUT_DATA(sd)->group), "vertical");
 
-   if (!ELM_WIDGET_CLASS(_elm_separator_parent_sc)->theme(obj)) return EINA_FALSE;
+   if (!ELM_WIDGET_CLASS(_elm_separator_parent_sc)->theme(obj))
+     return EINA_FALSE;
 
    return EINA_TRUE;
 }
@@ -90,7 +55,7 @@ _elm_separator_smart_add(Evas_Object *obj)
 }
 
 static void
-_elm_separator_smart_set_user(Elm_Layout_Smart_Class *sc)
+_elm_separator_smart_set_user(Elm_Separator_Smart_Class *sc)
 {
    ELM_WIDGET_CLASS(sc)->base.add = _elm_separator_smart_add;
 
@@ -100,7 +65,23 @@ _elm_separator_smart_set_user(Elm_Layout_Smart_Class *sc)
    ELM_WIDGET_CLASS(sc)->focus_next = NULL;
    ELM_WIDGET_CLASS(sc)->focus_direction = NULL;
 
-   sc->sizing_eval = _elm_separator_smart_sizing_eval;
+   ELM_LAYOUT_CLASS(sc)->sizing_eval = _elm_separator_smart_sizing_eval;
+}
+
+EAPI const Elm_Separator_Smart_Class *
+elm_separator_smart_class_get(void)
+{
+   static Elm_Separator_Smart_Class _sc =
+     ELM_SEPARATOR_SMART_CLASS_INIT_NAME_VERSION(ELM_SEPARATOR_SMART_NAME);
+   static const Elm_Separator_Smart_Class *class = NULL;
+
+   if (class)
+     return class;
+
+   _elm_separator_smart_set(&_sc);
+   class = &_sc;
+
+   return class;
 }
 
 EAPI Evas_Object *
