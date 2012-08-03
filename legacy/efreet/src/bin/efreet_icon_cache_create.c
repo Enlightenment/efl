493fd82f35e98b913b79135d9df83de6ef558a14
Sebastian Dransfeld <sd@tango.flipp.net>
2012-08-03 20:21:47 +0000

    efreet: always need to revalidate themes
    Themes read from cache are not valid until a valid index file has been
    read.
    
    SVN revision: 74865

--- legacy/efreet/src/bin/efreet_icon_cache_create.c
+++ legacy/efreet/src/bin/efreet_icon_cache_create.c
@@ -893,6 +893,7 @@ main(int argc, char **argv)
         {
             if (!strncmp(keys[i], "__efreet", 8)) continue;
             theme = eet_data_read(theme_ef, theme_edd, keys[i]);
+            theme->valid = 0;
             if (theme) eina_hash_direct_add(icon_themes, theme->theme.name.internal, theme);
         }
         free(keys);
