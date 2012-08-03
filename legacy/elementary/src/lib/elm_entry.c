aaa627e23f86622884d58b24647c4ec6c9f24c1b
Carsten Haitzler <raster@rasterman.com>
2012-08-03 12:04:08 +0000

    fix anchor click in desktop mode.
    SVN revision: 74844

--- legacy/elementary/src/lib/elm_entry.c
+++ legacy/elementary/src/lib/elm_entry.c
@@ -1891,9 +1891,7 @@ _signal_anchor_clicked(void *data, Evas_Object *obj __UNUSED__, const char *emis
    if (!wd->disabled)
      {
         evas_object_smart_callback_call(data, SIG_ANCHOR_CLICKED, &ei);
-
-        if (!_elm_config->desktop_entry)
-          _entry_hover_anchor_clicked(data, data, &ei);
+        _entry_hover_anchor_clicked(data, data, &ei);
      }
 }
 
