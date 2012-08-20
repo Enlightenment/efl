b66f460e840b7e87a9bce17799fb71273d76e0c9
Bruno Dilly <bdilly@profusion.mobi>
2012-08-20 22:16:02 +0000

    EPhysics: test torque forces
    SVN revision: 75477

--- legacy/ephysics/src/bin/test_rotating_forever.c
+++ legacy/ephysics/src/bin/test_rotating_forever.c
@@ -16,6 +16,32 @@ _rotate_cb(void *data)
    return EINA_TRUE;
 }
 
+static Eina_Bool
+_increase_torque_cb(void *data)
+{
+   EPhysics_Body *body = data;
+   Evas_Object *obj;
+
+   ephysics_body_torque_apply(body, 2);
+   obj = ephysics_body_evas_object_get(body);
+   evas_object_data_set(obj, "increase_timer", NULL);
+
+   return EINA_FALSE;
+}
+
+static Eina_Bool
+_stop_torque_cb(void *data)
+{
+   EPhysics_Body *body = data;
+   Evas_Object *obj;
+
+   ephysics_body_forces_clear(body);
+   obj = ephysics_body_evas_object_get(body);
+   evas_object_data_set(obj, "stop_timer", NULL);
+
+   return EINA_FALSE;
+}
+
 static void
 _del_cb(void *data, EPhysics_Body *body __UNUSED__, void *event_info __UNUSED__)
 {
@@ -23,15 +49,32 @@ _del_cb(void *data, EPhysics_Body *body __UNUSED__, void *event_info __UNUSED__)
 }
 
 static void
+_del_torque_cb(void *data, EPhysics_Body *body __UNUSED__, void *event_info __UNUSED__)
+{
+   Evas_Object *obj = data;
+   Ecore_Timer *timer;
+
+   timer = evas_object_data_get(obj, "increase_timer");
+   if (timer)
+     ecore_timer_del(timer);
+
+   timer = evas_object_data_get(obj, "stop_timer");
+   if (timer)
+     ecore_timer_del(timer);
+}
+
+static void
 _update_object_cb(void *data __UNUSED__, EPhysics_Body *body, void *event_info __UNUSED__)
 {
-   double rot, vrot;
+   double rot, vrot, torque;
 
    rot = ephysics_body_rotation_get(body);
    vrot = ephysics_body_angular_velocity_get(body);
+   ephysics_body_forces_get(body, NULL, NULL, &torque);
+
    ephysics_body_evas_object_update(body);
 
-   DBG("body: %p, rot: %lf, vrot :%lf", body, rot, vrot);
+   DBG("body: %p, rot: %lf, vrot: %lf, torque: %lf", body, rot, vrot, torque);
 }
 
 static void
@@ -95,6 +138,31 @@ _world_populate(Test_Data *test_data)
    ephysics_body_event_callback_add(body,
                                     EPHYSICS_CALLBACK_BODY_DEL,
                                     _del_cb, timer);
+
+   cube = elm_image_add(test_data->win);
+   elm_image_file_set(
+      cube, PACKAGE_DATA_DIR "/" EPHYSICS_TEST_THEME ".edj", "blue-cube");
+   evas_object_move(cube, WIDTH * 2 / 3, 60);
+   evas_object_resize(cube, 70, 70);
+   evas_object_show(cube);
+   test_data->evas_objs = eina_list_append(test_data->evas_objs, cube);
+
+   body = ephysics_body_box_add(test_data->world);
+   ephysics_body_evas_object_set(body, cube, EINA_TRUE);
+   test_data->bodies = eina_list_append(test_data->bodies, body);
+   ephysics_body_event_callback_add(body,
+                                    EPHYSICS_CALLBACK_BODY_UPDATE,
+                                    _update_object_cb, NULL);
+
+   ephysics_body_torque_apply(body, 2);
+   ephysics_body_event_callback_add(body,
+                                    EPHYSICS_CALLBACK_BODY_DEL,
+                                    _del_torque_cb, cube);
+
+   timer = ecore_timer_add(3, _increase_torque_cb, body);
+   evas_object_data_set(cube, "increase_timer", timer);
+   timer = ecore_timer_add(5, _stop_torque_cb, body);
+   evas_object_data_set(cube, "stop_timer", timer);
 }
 
 static void
