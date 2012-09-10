32c68acf3c6061290127c65a022121c6bd1d7b46
Christopher Michael <cpmichael1@comcast.net>
2012-09-10 12:28:17 +0000

    Ecore_X: Actually, we can still retrieve crtcs without returning Count of them. Fix (very very minor, imo not worthy of a changelog entry).
    SVN revision: 76400

--- legacy/ecore/src/lib/ecore_x/xlib/ecore_x_randr_12.c
+++ legacy/ecore/src/lib/ecore_x/xlib/ecore_x_randr_12.c
@@ -527,7 +527,7 @@ ecore_x_randr_crtcs_get(Ecore_X_Window root,
    XRRScreenResources *res = NULL;
    Ecore_X_Randr_Crtc *ret = NULL;
 
-   if (num && root &&
+   if (root &&
        (res = _ecore_x_randr_get_screen_resources (_ecore_x_disp, root)))
      {
         if ((ret = malloc(sizeof(Ecore_X_Randr_Crtc) * res->ncrtc)))
