94e46bb26cd8e7153f17acff3365922923f422e0
Gustavo Lima Chaves <glima@profusion.mobi>
2012-09-10 13:52:36 +0000

    [elm] Check for scrollable, not scroller, on these elm_scroller.c calls.
    SVN revision: 76406

--- legacy/elementary/src/lib/elm_scroller.c
+++ legacy/elementary/src/lib/elm_scroller.c
@@ -798,7 +798,6 @@ elm_scroller_page_relative_get(const Evas_Object *obj,
                                double *h_pagerel,
                                double *v_pagerel)
 {
-   ELM_SCROLLER_CHECK(obj);
    ELM_SCROLLER_DATA_GET(obj, sd);
 
    sd->s_iface->paging_get(obj, h_pagerel, v_pagerel, NULL, NULL);
@@ -824,7 +823,6 @@ elm_scroller_page_size_get(const Evas_Object *obj,
                            Evas_Coord *h_pagesize,
                            Evas_Coord *v_pagesize)
 {
-   ELM_SCROLLER_CHECK(obj);
    ELM_SCROLLER_DATA_GET(obj, sd);
 
    sd->s_iface->paging_get(obj, NULL, NULL, h_pagesize, v_pagesize);
