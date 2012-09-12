0ef27080028620299b308c759f29313fbc7360d1
Gustavo Lima Chaves <glima@profusion.mobi>
2012-09-12 17:01:34 +0000

    [elm] Fix missing icon on Genlist 'double_label' theme for consistency.
    Patch by João Paulo Fernandes Ventura.
    
    
    
    SVN revision: 76549

--- legacy/elementary/src/examples/genlist_example_03.c
+++ legacy/elementary/src/examples/genlist_example_03.c
@@ -35,7 +35,6 @@ _item_content_get(void *data, Evas_Object *obj, const char *part)
    if (!strcmp(part, "elm.swallow.icon"))
      elm_icon_standard_set(ic, "clock");
 
-   evas_object_size_hint_aspect_set(ic, EVAS_ASPECT_CONTROL_VERTICAL, 1, 1);
    return ic;
 }
 
