8ca073ed14d4c7f0f96ba265d81d7b46fbc5c276
Stefan Schmidt <stefan@datenfreihafen.org>
2012-09-28 09:10:40 +0000

    eeze/sensor: Update eeze sensor docs.
    Mostly typo fixes and hopefully clarify some things.
    
    SVN revision: 77189

--- legacy/eeze/src/lib/Eeze_Sensor.h
+++ legacy/eeze/src/lib/Eeze_Sensor.h
@@ -35,8 +35,7 @@
  * @since 1.8
  *
  * All sensor types known by Eeze Sensor. This list of types include real physical types like
- * proximity or light as well as "aggregated" types like putting a device down on the dsiplay side
- * (facedown).
+ * proximity or light as well as "aggregated" types like facedown or doubletap.
  */
 typedef enum
 {
@@ -61,7 +60,7 @@ typedef enum
  * @defgroup Sensor_Events Available eeze sensor events
  * @brief Sensor events that are emitted from the library as ecore events
  *
- * Event types used to regoister ecore_event_handler on. These events are used for
+ * Event types used to register ecore_event_handler on. These events are used for
  * #eeze_sensor_async_read to deliver read out data. It is also used for generated events like
  * facedown or shake.
  * @since 1.8
@@ -99,9 +98,9 @@ typedef struct _Eeze_Sensor
  * @since 1.8
  *
  * Object for a sensor type. Keeps information about the type and holds the data for the accessor
- * functions. As this information gets also updated by async reads it might be a good idea to check
- * the timestamp value to see when the data has been updated. The timestamp is given as unix epoch
- * (seconds since 00:00:00 UTC on 1 January 1970).
+ * functions. As this information gets also updated by asynchronous reads it might be a good idea
+ * to check the timestamp value to see when the data has been updated. The timestamp is given in
+ * microseconds.
  */
 typedef struct _Eeze_Sensor_Obj
 {
@@ -140,10 +139,10 @@ EAPI void eeze_sensor_free(Eeze_Sensor_Obj *sens);
 /**
  * @brief Get accuracy from sensor object.
  * @param sens Sensor object to operate on.
- * @param accuracy Pointer to write accurancy value into.
+ * @param accuracy Pointer to write accuracy value into.
  * @return EINA_TRUE for success and EINA_FALSE for failure
  *
- * Accessor function to get the accurancy property from the sensor object.
+ * Access function to get the accuracy property from the sensor object.
  * @since 1.8
  */
 EAPI Eina_Bool eeze_sensor_accuracy_get(Eeze_Sensor_Obj *sens, int *accuracy);
@@ -156,8 +155,8 @@ EAPI Eina_Bool eeze_sensor_accuracy_get(Eeze_Sensor_Obj *sens, int *accuracy);
  * @param z Pointer to write third data property value into.
  * @return EINA_TRUE for success and EINA_FALSE for failure
  *
- * Accessor function to get all three data properties from the sensor object. This is used for sensor
- * types that offer all three values. Like acceleromter and magnetic.
+ * Access function to get all three data properties from the sensor object. This is used for sensor
+ * types that offer all three values. Like accelerometer and magnetic.
  * @since 1.8
  */
 EAPI Eina_Bool eeze_sensor_xyz_get(Eeze_Sensor_Obj *sens, float *x, float *y, float *z);
@@ -169,7 +168,7 @@ EAPI Eina_Bool eeze_sensor_xyz_get(Eeze_Sensor_Obj *sens, float *x, float *y, fl
  * @param y Pointer to write second data property value into.
  * @return EINA_TRUE for success and EINA_FALSE for failure
  *
- * Accessor function to get the first two data properties from the sensor object. This is used for sensor
+ * Access function to get the first two data properties from the sensor object. This is used for sensor
  * types that offer two values. Like panning.
  * @since 1.8
  */
@@ -181,7 +180,7 @@ EAPI Eina_Bool eeze_sensor_xy_get(Eeze_Sensor_Obj *sens, float *x, float *y);
  * @param x Pointer to write first data property value into.
  * @return EINA_TRUE for success and EINA_FALSE for failure
  *
- * Accessor function to get the first data property from the sensor object. This is used for sensor
+ * Access function to get the first data property from the sensor object. This is used for sensor
  * types that only offer one value. Like light or proximity.
  * @since 1.8
  */
@@ -193,7 +192,7 @@ EAPI Eina_Bool eeze_sensor_x_get(Eeze_Sensor_Obj *sens, float *x);
  * @param timestamp Pointer to write timestamp value into.
  * @return EINA_TRUE for success and EINA_FALSE for failure
  *
- * Accessor function to get the timestamp property from the sensor object.
+ * Access function to get the timestamp property from the sensor object.
  * @since 1.8
  */
 EAPI Eina_Bool eeze_sensor_timestamp_get(Eeze_Sensor_Obj *sens, unsigned long long *timestamp);
@@ -204,7 +203,7 @@ EAPI Eina_Bool eeze_sensor_timestamp_get(Eeze_Sensor_Obj *sens, unsigned long lo
  * @return EINA_TRUE for success and EINA_FALSE for failure
  *
  * This function reads sensor data from the device and fills the sensor object with the data. This
- * call is synchronuos and blocks until the data is read out and updated in the sensor object.
+ * call is synchronous and blocks until the data is read out and updated in the sensor object.
  * For simple applications this is fine and the easiest way to use the API.
  * @since 1.8
  */
