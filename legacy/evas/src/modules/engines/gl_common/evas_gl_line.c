77e2edbedd3eaf0449f43c5438d8c022e6409dfa
ChunEon Park <hermet@hermet.pe.kr>
2012-10-16 05:31:28 +0000

    evas/line - fix the line vertical drawing on gl backened.
    SVN revision: 78034

--- legacy/evas/src/modules/engines/gl_common/evas_gl_line.c
+++ legacy/evas/src/modules/engines/gl_common/evas_gl_line.c
@@ -27,6 +27,21 @@ evas_gl_common_line_draw(Evas_Engine_GL_Context *gc, int x1, int y1, int x2, int
    cx = gc->dc->clip.x; cy = gc->dc->clip.y;
    cw = gc->dc->clip.w; ch = gc->dc->clip.h;
 
+   //Increment pixels since the gl line origin position is slightly different
+   if (x1 == x2)
+     {
+        if (gc->rot == 0)
+          x1++;
+     }
+
+   if (y1 == y2)
+     {
+        if ((gc->rot == 90) || (gc->rot == 180))
+          y1++;
+     }
+
+   x2++; y2++;
+
    evas_gl_common_context_line_push(gc, x1, y1, x2, y2,
                                     c, cx, cy, cw, ch,
                                     r, g, b, a);
