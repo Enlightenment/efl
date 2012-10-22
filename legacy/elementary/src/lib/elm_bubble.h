4a59db31f854791fe041ac78db204121577a617b
Daniel Juyung Seo <seojuyung2@gmail.com>
2012-10-22 00:47:10 +0000

    elm elm_bubble.h: Added documentation for bubble widget.
    SVN revision: 78310

--- legacy/elementary/src/lib/elm_bubble.h
+++ legacy/elementary/src/lib/elm_bubble.h
@@ -67,11 +67,11 @@
  */
 typedef enum
 {
-  ELM_BUBBLE_POS_INVALID = -1,
-  ELM_BUBBLE_POS_TOP_LEFT,
-  ELM_BUBBLE_POS_TOP_RIGHT,
-  ELM_BUBBLE_POS_BOTTOM_LEFT,
-  ELM_BUBBLE_POS_BOTTOM_RIGHT,
+  ELM_BUBBLE_POS_INVALID = -1, /**< invalid corner */
+  ELM_BUBBLE_POS_TOP_LEFT, /**< the arrow of the bubble points to the top left corner. */
+  ELM_BUBBLE_POS_TOP_RIGHT, /**< the arrow of the bubble points to the top right corner. */
+  ELM_BUBBLE_POS_BOTTOM_LEFT, /**< the arrow of the bubble points to the bottom left corner. */
+  ELM_BUBBLE_POS_BOTTOM_RIGHT, /**< the arrow of the bubble points to the bottom right corner. */
 } Elm_Bubble_Pos;
 
 /**
