d0126e755b025536abd4039e3c960afef57f9226
Leandro Dorileo <dorileo@profusion.mobi>
2012-10-30 18:31:58 +0000

    EPhysics: the z axis is inverted
    The z axis is inverted so the greater the lower it is, the smaller
    the above it is.
    
    
    Patch by: Leandro Dorileo <dorileo@profusion.mobi>
    
    
    
    SVN revision: 78682

--- legacy/ephysics/src/lib/ephysics_body.cpp
+++ legacy/ephysics/src/lib/ephysics_body.cpp
@@ -362,11 +362,11 @@ _ephysics_body_evas_stacking_sort_cb(const void *d1, const void *d2)
    stacking1 = (const EPhysics_Body_Evas_Stacking *)d1;
    stacking2 = (const EPhysics_Body_Evas_Stacking *)d2;
 
-   if (!stacking1) return -1;
-   if (!stacking2) return 1;
+   if (!stacking1) return 1;
+   if (!stacking2) return -1;
 
-   if (stacking1->stacking < stacking2->stacking) return 1;
-   if (stacking2->stacking > stacking2->stacking) return -1;
+   if (stacking1->stacking < stacking2->stacking) return -1;
+   if (stacking2->stacking > stacking2->stacking) return 1;
 
    return 0;
 }
