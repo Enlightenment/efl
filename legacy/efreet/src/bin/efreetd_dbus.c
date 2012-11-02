9262991a456c2648b96d19731749b12b9ee5a0dc
Sebastian Dransfeld <sd@tango.flipp.net>
2012-11-02 01:47:00 +0000

    efreet: iface registration before name request
    SVN revision: 78812

--- legacy/efreet/src/bin/efreetd_dbus.c
+++ legacy/efreet/src/bin/efreetd_dbus.c
@@ -151,8 +151,6 @@ on_name_request(void *data __UNUSED__, const EDBus_Message *msg, EDBus_Pending *
         ERR("error name already in use");
         return;
      }
-
-   iface = edbus_service_interface_register(conn, PATH, &desc);
 }
 
 /* external */
@@ -176,6 +174,7 @@ dbus_init(void)
    conn = edbus_connection_get(EDBUS_CONNECTION_TYPE_SESSION);
    if (!conn) goto conn_error;
 
+   iface = edbus_service_interface_register(conn, PATH, &desc);
    edbus_name_request(conn, BUS, EDBUS_NAME_REQUEST_FLAG_DO_NOT_QUEUE,
                       on_name_request, NULL);
 
