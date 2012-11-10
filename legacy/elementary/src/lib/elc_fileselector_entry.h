577f90e9a431873a04f9d3d0072ec8a9ad93a028
ChunEon Park <hermet@hermet.pe.kr>
2012-11-10 09:21:25 +0000

    elementary/elc_fileselector_entry -  [E-devel] E-devel] [PATCH] [ELEMENTARY] elm_fileselector_entry_inwin_mode_set doc fix
    Hi all,
    
    I had prepare some doc fix for elm_fileselector_entry_inwin_mode_set.
    
    By mistake docs said it returns EINA TRUE in both cases.
    
    Please verify attached file.
    
    Regards,
    
    Patrick
    
    Signed-Off-By: Patryk Kaczmarek<patryk.k@samsung.com>
    
    
    
    SVN revision: 79089

--- legacy/elementary/src/lib/elc_fileselector_entry.h
+++ legacy/elementary/src/lib/elc_fileselector_entry.h
@@ -295,7 +295,7 @@ EAPI Eina_Bool                   elm_fileselector_entry_is_save_get(const Evas_O
  *
  * @param obj The file selector entry widget
  * @param value @c EINA_TRUE to make it use an inner window, @c
- * EINA_TRUE to make it use a dedicated window
+ * EINA_FALSE to make it use a dedicated window
  *
  * @see elm_win_inwin_add() for more information on inner windows
  * @see elm_fileselector_entry_inwin_mode_get()
