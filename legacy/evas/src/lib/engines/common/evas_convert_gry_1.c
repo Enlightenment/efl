cd926b304dc8f072d99d55f90ef4bd6ad9d3a62f
Carsten Haitzler <raster@rasterman.com>
2012-11-13 23:52:18 +0000

    move libs already in EFL into... IN-EFL - this will stop/prevent/limit thnigs like people using them and patching them, etc.
    SVN revision: 79255

--- legacy/evas/src/lib/engines/common/evas_convert_gry_1.c
+++
@@ -1,6 +0,0 @@
-#include "evas_common.h"
-#include "evas_convert_gry_1.h"
-
-void evas_common_convert_rgba_to_1bpp_gry_1_dith(DATA32 *src __UNUSED__, DATA8 *dst __UNUSED__, int src_jump __UNUSED__, int dst_jump __UNUSED__, int w __UNUSED__, int h __UNUSED__, int dith_x __UNUSED__, int dith_y __UNUSED__, DATA8 *pal __UNUSED__)
-{
-}
