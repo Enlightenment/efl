47a3d97885b80f1e703b55ecd491aabf287fc1dc
Vincent Torri <vincent.torri@gmail.com>
2012-11-23 22:38:23 +0000

    Ecore: fix alloca declaration
    SVN revision: 79613

--- legacy/ecore/src/lib/ecore_x/xlib/ecore_x_selection.c
+++ legacy/ecore/src/lib/ecore_x/xlib/ecore_x_selection.c
@@ -2,6 +2,32 @@
 # include <config.h>
 #endif /* ifdef HAVE_CONFIG_H */
 
+#ifdef STDC_HEADERS
+# include <stdlib.h>
+# include <stddef.h>
+#else
+# ifdef HAVE_STDLIB_H
+#  include <stdlib.h>
+# endif
+#endif
+#ifdef HAVE_ALLOCA_H
+# include <alloca.h>
+#elif !defined alloca
+# ifdef __GNUC__
+#  define alloca __builtin_alloca
+# elif defined _AIX
+#  define alloca __alloca
+# elif defined _MSC_VER
+#  include <malloc.h>
+#  define alloca _alloca
+# elif !defined HAVE_ALLOCA
+#  ifdef  __cplusplus
+extern "C"
+#  endif
+void *alloca (size_t);
+# endif
+#endif
+
 #include <stdlib.h>
 #include <string.h>
 
