e8610822712253b96392c8d35c4d21626d695ff9
Vincent Torri <vincent.torri@gmail.com>
2012-12-03 07:01:02 +0000

    ecore: backport #79818
    SVN revision: 80010

--- src/lib/ecore_evas/ecore_evas_wayland_common.c
+++ src/lib/ecore_evas/ecore_evas_wayland_common.c
@@ -43,13 +43,11 @@ _ecore_evas_wl_common_cb_mouse_in(void *data EINA_UNUSED, int type EINA_UNUSED, 
    ee = ecore_event_window_match(ev->window);
    if ((!ee) || (ee->ignore_events)) return ECORE_CALLBACK_PASS_ON;
    if (ev->window != ee->prop.window) return ECORE_CALLBACK_PASS_ON;
-   if (!ee->in)
-     {
-        if (ee->func.fn_mouse_in) ee->func.fn_mouse_in(ee);
-        ecore_event_evas_modifier_lock_update(ee->evas, ev->modifiers);
-        evas_event_feed_mouse_in(ee->evas, ev->timestamp, NULL);
-        ee->in = EINA_TRUE;
-     }
+   if (ee->func.fn_mouse_in) ee->func.fn_mouse_in(ee);
+   ecore_event_evas_modifier_lock_update(ee->evas, ev->modifiers);
+   evas_event_feed_mouse_in(ee->evas, ev->timestamp, NULL);
+   _ecore_evas_mouse_move_process(ee, ev->x, ev->y, ev->timestamp);
+   ee->in = EINA_TRUE;
    return ECORE_CALLBACK_PASS_ON;
 }
 
