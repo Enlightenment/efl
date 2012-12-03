128074a2bd09fa5c1f467b95c1c0446c68052d99
Vincent Torri <vincent.torri@gmail.com>
2012-12-03 18:29:51 +0000

    Ecore: merge unit tests, ignore++
    SVN revision: 80090

---
+++ src/tests/ecore/ecore_suite.h
@@ -0,0 +1,11 @@
+#ifndef _ECORE_SUITE_H
+#define _ECORE_SUITE_H
+
+#include <check.h>
+
+void ecore_test_ecore(TCase *tc);
+void ecore_test_ecore_con(TCase *tc);
+void ecore_test_ecore_x(TCase *tc);
+
+
+#endif /* _ECORE_SUITE_H */
---
+++ src/tests/ecore/ecore_test_ecore_x.c
@@ -0,0 +1,60 @@
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include <Ecore_X.h>
+
+#include "ecore_suite.h"
+
+/* FIXME: Currently disable these tests. They are useless ATM and they just
+ * make buildbot complain. Once we add useful tests here we'll also bother
+ * with getting X on the server. */
+#undef HAVE_ECORE_X_XLIB
+
+/* TODO: change to HAVE_ECORE_X when xcb implementation is done */
+#ifdef HAVE_ECORE_X_XLIB
+
+START_TEST(ecore_test_ecore_x_init)
+{
+   int ret;
+
+   ret = ecore_x_init(NULL);
+   fail_if(ret != 1);
+
+   ret = ecore_x_shutdown();
+   fail_if(ret != 0);
+}
+END_TEST
+
+START_TEST(ecore_test_ecore_x_bell)
+{
+   int i;
+   int ret;
+
+   ret = ecore_x_init(NULL);
+   fail_if(ret != 1);
+
+   printf("You should hear 3 beeps now.\n");
+   for (i = 0; i < 3; i++)
+     {
+	ret = ecore_x_bell(0);
+	fail_if(ret != EINA_TRUE);
+	ecore_x_sync();
+	sleep(1);
+     }
+
+   ecore_x_shutdown();
+}
+END_TEST
+
+#endif
+
+void ecore_test_ecore_x(TCase *tc EINA_UNUSED)
+{
+
+/* TODO: change to HAVE_ECORE_X when xcb implementation is done */
+#ifdef HAVE_ECORE_X_XLIB
+   tcase_add_test(tc, ecore_test_ecore_x_init);
+   tcase_add_test(tc, ecore_test_ecore_x_bell);
+#endif
+}
