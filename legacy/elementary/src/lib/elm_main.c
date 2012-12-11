b470aaa0f2977c682c3824f09e01d21fa1c7dd50
Flavio Vinicius Alvares Ceolin <flavio.ceolin@profusion.mobi>
2012-12-11 14:05:48 +0000

    adding support for elocation
    SVN revision: 80657

--- legacy/elementary/src/lib/elm_main.c
+++ legacy/elementary/src/lib/elm_main.c
@@ -411,6 +411,32 @@ _elm_unneed_edbus(void)
 #endif
 }
 
+#ifdef ELM_ELOCATION
+static Eina_Bool _elm_need_elocation = EINA_FALSE;
+#endif
+EAPI Eina_Bool
+elm_need_elocation(void)
+{
+#ifdef ELM_ELOCATION
+   if (_elm_need_elocation) return EINA_TRUE;
+   _elm_need_elocation = EINA_TRUE;
+   elocation_init();
+   return EINA_TRUE;
+#else
+   return EINA_FALSE;
+#endif
+}
+
+static void
+_elm_unneed_elocation(void)
+{
+#ifdef ELM_ELOCATION
+   if (!_elm_need_elocation) return;
+   _elm_need_elocation = EINA_FALSE;
+   edbus_shutdown();
+#endif
+}
+
 #ifdef ELM_EFREET
 static Eina_Bool _elm_need_efreet = EINA_FALSE;
 #endif
@@ -627,6 +653,7 @@ elm_quicklaunch_shutdown(void)
    _elm_unneed_efreet();
    _elm_unneed_e_dbus();
    _elm_unneed_edbus();
+   _elm_unneed_elocation();
    _elm_unneed_ethumb();
    _elm_unneed_web();
    ecore_file_shutdown();
--- legacy/elementary/src/lib/elm_need.h
+++ legacy/elementary/src/lib/elm_need.h
@@ -57,6 +57,21 @@ EAPI Eina_Bool elm_need_e_dbus(void) EINA_DEPRECATED;
 EAPI Eina_Bool elm_need_edbus(void);
 
 /**
+ * Request that your elementary application needs elocation
+ *
+ * This initializes the elocation library when called and if
+ * support exists it returns EINA_TRUE, otherwise returns
+ * EINA_FALSE. This must be called before any elocation usage.
+ *
+ * @return EINA_TRUE if support exists and initialization succeeded.
+ *
+ * @since 1.8.0
+ *
+ * @ingroup edbus
+ */
+EAPI Eina_Bool elm_need_elocation(void);
+
+/**
  * Request that your elementary application needs ethumb
  *
  * This initializes the Ethumb library when called and if support exists
