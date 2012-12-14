90b5d6aeb2e0650d4bb9173105015391d6b589fb
Stefan Schmidt <stefan@datenfreihafen.org>
2012-12-14 14:25:26 +0000

    Revert "efl/embryo: Keep room for the terminating '\0' we may add to the path."
    After consulting with cedric and Sachiel I have to agree that there is no issue.
    
    SVN revision: 80946

--- src/bin/embryo/embryo_cc_sc1.c
+++ src/bin/embryo/embryo_cc_sc1.c
@@ -651,7 +651,7 @@ setopt(int argc, char **argv, char *iname, char *oname,
 static void
 setconfig(char *root)
 {
-   char                path[PATH_MAX - 1];
+   char                path[PATH_MAX];
    char               *ptr;
    int                 len;
 
