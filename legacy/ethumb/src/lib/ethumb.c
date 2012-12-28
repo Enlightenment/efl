fa1e49a6424ea05ea12ba73d60dddbc9074125af
Cedric BAIL <cedric.bail@free.fr>
2012-12-28 07:30:33 +0000

    ethumb: fix compilation on windows.
    Patch by Vincent Torri.
    
    
    SVN revision: 81783

--- legacy/ethumb/src/lib/ethumb.c
+++ legacy/ethumb/src/lib/ethumb.c
@@ -23,21 +23,30 @@
 #include "config.h"
 #endif
 
+#ifdef STDC_HEADERS
+# include <stdlib.h>
+# include <stddef.h>
+#else
+# ifdef HAVE_STDLIB_H
+#  include <stdlib.h>
+# endif
+#endif
 #ifdef HAVE_ALLOCA_H
 # include <alloca.h>
-#elif defined __GNUC__
-# define alloca __builtin_alloca
-#elif defined _AIX
-# define alloca __alloca
-#elif defined _MSC_VER
-# include <malloc.h>
-# define alloca _alloca
-#else
-# include <stddef.h>
-# ifdef  __cplusplus
+#elif !defined alloca
+# ifdef __GNUC__
+#  define alloca __builtin_alloca
+# elif defined _AIX
+#  define alloca __alloca
+# elif defined _MSC_VER
+#  include <malloc.h>
+#  define alloca _alloca
+# elif !defined HAVE_ALLOCA
+#  ifdef  __cplusplus
 extern "C"
-# endif
+#  endif
 void *alloca (size_t);
+# endif
 #endif
 
 #include <stdio.h>
@@ -60,6 +69,14 @@ void *alloca (size_t);
 # define PATH_MAX 4096
 #endif
 
+#ifdef HAVE_LIBEXIF
+  #include <libexif/exif-data.h>
+#endif
+
+#ifdef HAVE_EVIL
+# include <Evil.h>
+#endif
+
 #include <Eina.h>
 #include <eina_safety_checks.h>
 #include <Evas.h>
@@ -72,10 +89,6 @@ void *alloca (size_t);
 #include "ethumb_private.h"
 #include "Ethumb_Plugin.h"
 #include "md5.h"
-
-#ifdef HAVE_LIBEXIF
-  #include <libexif/exif-data.h>
-#endif
 
 static Ethumb_Version _version = { VMAJ, VMIN, VMIC, VREV };
 EAPI Ethumb_Version *ethumb_version = &_version;
