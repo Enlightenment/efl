86727a6b8ba117192ae5ad534487f577a1223f86
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2012-12-29 15:57:08 +0000

    same netwm tooltip fix
    SVN revision: 81876

--- legacy/elementary/src/lib/els_tooltip.c
+++ legacy/elementary/src/lib/els_tooltip.c
@@ -131,7 +131,7 @@ _elm_tooltip_show(Elm_Tooltip *tt)
      }
    if (tt->free_size)
      {
-        tt->tt_win = elm_win_add(NULL, "tooltip", ELM_WIN_BASIC);
+        tt->tt_win = elm_win_add(NULL, "tooltip", ELM_WIN_TOOLTIP);
         elm_win_override_set(tt->tt_win, EINA_TRUE);
         tt->tt_evas = evas_object_evas_get(tt->tt_win);
         tt->tooltip = edje_object_add(tt->tt_evas);
