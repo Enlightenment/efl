4d8dffc411f3e0389284b0d2a8f8f0ec382ccd88
Cedric BAIL <cedric.bail@free.fr>
2013-01-22 09:58:36 +0000

    efl: remove useless code.
    SVN revision: 83065

--- src/lib/eina/eina_abi.c
+++ src/lib/eina/eina_abi.c
@@ -25,18 +25,6 @@
 #include "eina_unicode.h"
 #include "eina_safety_checks.h"
 
-#if EINA_SIZEOF_WCHAR_T >= 4
-# include <wchar.h>
-typedef wchar_t Eina_Unicode;
-#else
-# include <inttypes.h>
-typedef uint32_t Eina_Unicode;
-#endif
-
-#define ERROR_REPLACEMENT_BASE  0xDC80
-#define EINA_IS_INVALID_BYTE(x)      ((x == 192) || (x == 193) || (x >= 245))
-#define EINA_IS_CONTINUATION_BYTE(x) ((x & 0xC0) == 0x80)
-
 EAPI Eina_Unicode eina_unicode_utf8_get_next(const char *buf, int *iindex)
 {
    return eina_unicode_utf8_next_get(buf, iindex);
