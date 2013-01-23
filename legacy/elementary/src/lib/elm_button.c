76548cabbcf663f0f032b3c5b5dc23134768a3e3
Shinwoo Kim <kimcinoo.efl@gmail.com>
2013-01-23 04:04:46 +0000

    [access] does not activate button if button is disabled.
    SVN revision: 83118

--- legacy/elementary/src/lib/elm_button.c
+++ legacy/elementary/src/lib/elm_button.c
@@ -82,8 +82,12 @@ _elm_button_smart_activate(Eo *obj, void *_pd EINA_UNUSED, va_list *list)
 
    if (act != ELM_ACTIVATE_DEFAULT) return;
 
-   evas_object_smart_callback_call(obj, SIG_CLICKED, NULL);
-   elm_layout_signal_emit(obj, "elm,anim,activate", "elm");
+   if (!elm_widget_disabled_get(obj) &&
+       !evas_object_freeze_events_get(obj))
+     {
+        evas_object_smart_callback_call(obj, SIG_CLICKED, NULL);
+        elm_layout_signal_emit(obj, "elm,anim,activate", "elm");
+     }
 
    if (ret) *ret = EINA_TRUE;
 }
