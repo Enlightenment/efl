3642599e8fe7c95d0d3218bbc4780965e9c83741
Mike McCormack <mike@atratus.org>
2013-01-25 21:30:48 +0000

    efl: Fix unused parameter warning
    Signed-off-by: Mike McCormack <mike@atratus.org>
    
    SVN revision: 83334

--- src/lib/ecore_evas/ecore_evas_util.c
+++ src/lib/ecore_evas/ecore_evas_util.c
@@ -117,7 +117,7 @@ _ecore_evas_obj_callback_hide(void *data, Evas *e EINA_UNUSED, Evas_Object *obj 
 }
 
 static void
-_ecore_evas_obj_callback_resize(void *data, Evas *e, Evas_Object *obj, void *event_info EINA_UNUSED)
+_ecore_evas_obj_callback_resize(void *data, Evas *e EINA_UNUSED, Evas_Object *obj, void *event_info EINA_UNUSED)
 {
    Ecore_Evas *ee = data;
    Evas_Coord ow, oh;
