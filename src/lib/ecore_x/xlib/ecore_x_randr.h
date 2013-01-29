3888089a7fe633bd04c4522c767f7e6c6c566026
Christopher Michael <cp.michael@samsung.com>
2013-01-29 14:48:08 +0000

    Remove old randr code.
    Signed-off-by: Christopher Michael <cp.michael@samsung.com>
    
    SVN revision: 83437

--- src/lib/ecore_x/xlib/ecore_x_randr.h
+++
@@ -1,7 +0,0 @@
-#ifndef ECORE_X_INLINE_X
-#define ECORE_X_INLINE_X
-Eina_Bool _ecore_x_randr_root_validate(Ecore_X_Window root);
-Eina_Bool _ecore_x_randr_output_validate(Ecore_X_Window root,
-                                         Ecore_X_Randr_Output
-                                         output);
-#endif
