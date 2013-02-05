232ea5b875f7b173fbf88ef53f54ceb10b8fae3a
Christopher Michael <cp.michael@samsung.com>
2013-02-05 14:16:00 +0000

    Fix function prototypes to take resize edges into account.
    Signed-off-by: Christopher Michael <cp.michael@samsung.com>

--- src/modules/evas/engines/wayland_shm/evas_swapbuf.h
+++ src/modules/evas/engines/wayland_shm/evas_swapbuf.h
@@ -3,9 +3,9 @@
 
 # include "evas_engine.h"
 
-Outbuf *evas_swapbuf_setup(int w, int h, unsigned int rotation, Outbuf_Depth depth, Eina_Bool alpha, struct wl_shm *wl_shm, struct wl_surface *wl_surface);
+Outbuf *evas_swapbuf_setup(int x, int y, int w, int h, unsigned int rotation, Outbuf_Depth depth, Eina_Bool alpha, struct wl_shm *wl_shm, struct wl_surface *wl_surface);
 void evas_swapbuf_free(Outbuf *ob);
-void evas_swapbuf_reconfigure(Outbuf *ob, int w, int h, unsigned int rotation, Outbuf_Depth depth, Eina_Bool alpha);
+void evas_swapbuf_reconfigure(Outbuf *ob, int x, int y, int w, int h, unsigned int rotation, Outbuf_Depth depth, Eina_Bool alpha);
 RGBA_Image *evas_swapbuf_update_region_new(Outbuf *ob, int x, int y, int w, int h, int *cx, int *cy, int *cw, int *ch);
 void evas_swapbuf_update_region_push(Outbuf *ob, RGBA_Image *update, int x, int y, int w, int h);
 void evas_swapbuf_update_region_free(Outbuf *ob, RGBA_Image *update);
