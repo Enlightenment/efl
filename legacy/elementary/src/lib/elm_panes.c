fd804d6153a6896efbf2e02e700c1a69984f965d
Daniel Juyung Seo <seojuyung2@gmail.com>
2013-02-16 06:07:22 +0000

    elm elm_panes.c: fixed wrong eo description.
    SVN revision: 83969

--- legacy/elementary/src/lib/elm_panes.c
+++ legacy/elementary/src/lib/elm_panes.c
@@ -485,7 +485,7 @@ static const Eo_Op_Description op_desc[] = {
      EO_OP_DESCRIPTION(ELM_OBJ_PANES_SUB_ID_CONTENT_RIGHT_SIZE_SET, "Set the size proportion of panes widget's right side."),
      EO_OP_DESCRIPTION(ELM_OBJ_PANES_SUB_ID_HORIZONTAL_SET, "Set the orientation of a given panes widget."),
      EO_OP_DESCRIPTION(ELM_OBJ_PANES_SUB_ID_HORIZONTAL_GET, "Retrieve the orientation of a given panes widget."),
-     EO_OP_DESCRIPTION(ELM_OBJ_PANES_SUB_ID_FIXED_SET, "Set whether the left and right panes resize homogeneously or not."),
+     EO_OP_DESCRIPTION(ELM_OBJ_PANES_SUB_ID_FIXED_SET, "Set whether the left and right panes can be resized by user interaction"),
      EO_OP_DESCRIPTION(ELM_OBJ_PANES_SUB_ID_FIXED_GET, "Retrieve the resize mode for the panes of a given panes widget."),
      EO_OP_DESCRIPTION_SENTINEL
 };
