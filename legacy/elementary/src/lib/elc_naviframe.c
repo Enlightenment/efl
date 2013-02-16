46f7abc6df821e427ddc9f4c06afe5db6c85a0f2
ChunEon Park <hermet@hermet.pe.kr>
2013-02-16 12:33:52 +0000

    elementary/naviframe - don't call the parent smart_theme. Actually navifram itself is nothing to do about theme.
    SVN revision: 83986

--- legacy/elementary/src/lib/elc_naviframe.c
+++ legacy/elementary/src/lib/elc_naviframe.c
@@ -282,10 +282,7 @@ _elm_naviframe_smart_theme(Eo *obj, void *_pd, va_list *list)
    Elm_Naviframe_Item *it;
    Elm_Naviframe_Smart_Data *sd = _pd;
    Eina_Bool *ret = va_arg(*list, Eina_Bool *);
-   Eina_Bool int_ret = EINA_FALSE;
    const char *style, *sstyle;
-   eo_do_super(obj, elm_wdg_theme(&int_ret));
-   if (!int_ret) return;
 
    eo_do(obj, elm_wdg_style_get(&style));
 
