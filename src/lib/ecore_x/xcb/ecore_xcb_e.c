bc78f49d718cb7e70acc870d59478fa2659efd3c
Jiyoun Park <jy0703.park@samsung.com>
2013-03-05 13:46:14 +0900

    ecore_x : just fix typo

--- src/lib/ecore_x/xcb/ecore_xcb_e.c
+++ src/lib/ecore_x/xcb/ecore_xcb_e.c
@@ -1132,7 +1132,7 @@ _ecore_xcb_e_clipboard_state_get(Ecore_X_Atom atom)
    if (atom == ECORE_X_ATOM_E_ILLUME_CLIPBOARD_OFF)
      return ECORE_X_ILLUME_CLIPBOARD_STATE_OFF;
 
-   return ECORE_X_ILLUME_INDICATOR_STATE_UNKNOWN;
+   return ECORE_X_ILLUME_CLIPBOARD_STATE_UNKNOWN;
 }
 
 EAPI void
