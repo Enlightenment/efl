a35926215ddf9f58abcc0453cbb7f1b3f0822d30
Daniel Juyung Seo <seojuyung@gmail.com>
2013-03-10 00:47:26 +0900

    elm_focus.h: more informative documentation.

--- legacy/elementary/src/lib/elm_focus.h
+++ legacy/elementary/src/lib/elm_focus.h
@@ -70,8 +70,8 @@ EAPI Eina_Bool            elm_object_focus_get(const Evas_Object *obj);
  * the focus from @p obj, passing it back to the previous element in the
  * focus chain list.
  *
- * @warning Only visible object can get a focus. Call evas_object_show(o), if
- * you want to give a focus to the evas object.
+ * @warning Only visible object can get a focus. Call evas_object_show(o) before
+ * calling this API, if you want to give a focus to the evas object.
  *
  * @see elm_object_focus_get(), elm_object_focus_custom_chain_get()
  *
