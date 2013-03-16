42d407eab53f28b38548b0bbfce25a71caa5bfa1
Cedric Bail <cedric.bail@free.fr>
2013-03-16 15:50:01 +0900

    ecore_evas: killing Glew and Direct3d, because someone already killed Kenny.

--- src/lib/ecore_evas/ecore_evas_deprecated.c
+++ src/lib/ecore_evas/ecore_evas_deprecated.c
@@ -65,6 +65,26 @@ ecore_evas_software_wince_gdi_new(Ecore_WinCE_Window *parent EINA_UNUSED,
    return NULL;
 }
 
+EAPI Ecore_Evas *
+ecore_evas_direct3d_new(Ecore_Win32_Window *parent,
+			int                 x,
+			int                 y,
+			int                 width,
+			int                 height)
+{
+   return NULL;
+}
+
+EAPI Ecore_Evas *
+ecore_evas_gl_glew_new(Ecore_Win32_Window *parent,
+		       int                 x,
+		       int                 y,
+		       int                 width,
+		       int                 height)
+{
+   return NULL;
+}
+
 EAPI Ecore_WinCE_Window *
 ecore_evas_software_wince_window_get(const Ecore_Evas *ee EINA_UNUSED)
 {
