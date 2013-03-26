2155bb0fef8c35453e4854e7143aa70198559369
Daniel Juyung Seo <seojuyung@gmail.com>
2013-03-27 01:17:18 +0900

    elm_genlist.c: fixed formatting.

--- legacy/elementary/src/lib/elm_genlist.c
+++ legacy/elementary/src/lib/elm_genlist.c
@@ -3981,8 +3981,8 @@ _access_activate_cb(void *data __UNUSED__,
              Elm_Gen_Item *it2;
 
              EINA_LIST_FOREACH_SAFE(sd->selected, l, l_next, it2)
-                if (it2 != it)
-                  _item_unselect(it2);
+               if (it2 != it)
+                 _item_unselect(it2);
           }
         _item_highlight(it);
         it->sel_cb(it);
@@ -4141,8 +4141,8 @@ _item_mouse_up_cb(void *data,
              Elm_Gen_Item *it2;
 
              EINA_LIST_FOREACH_SAFE(sd->selected, l, l_next, it2)
-                if (it2 != it)
-                  _item_unselect(it2);
+               if (it2 != it)
+                 _item_unselect(it2);
           }
         _item_highlight(it);
         it->sel_cb(it);
