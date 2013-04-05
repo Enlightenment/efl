359d7034ba780492e90d5396323390172da90384
Cedric BAIL <cedric.bail@samsung.com>
2013-04-05 12:20:37 +0900

    eet: enable more debug information regarding memory allocation.

--- src/lib/eet/eet_lib.c
+++ src/lib/eet/eet_lib.c
@@ -943,6 +943,9 @@ eet_internal_read2(Eet_File *ef)
         if (eet_test_close(!ef->ed, ef))
           return NULL;
 
+        INF("loading dictionnary for '%s' with %lu entries of size %i",
+            ef->path, num_dictionary_entries, sizeof(Eet_String));
+
         ef->ed->all = calloc(1, num_dictionary_entries * sizeof(Eet_String));
         if (eet_test_close(!ef->ed->all, ef))
           return NULL;
