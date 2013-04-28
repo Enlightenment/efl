961fc60e3d9d3ac75dcd77d44d7cf0295fcf1817
Daniel Zaoui <daniel.zaoui@samsung.com>
2013-04-28 08:41:17 +0300

    Elementary: Ctx Popup header split.

---
+++ legacy/elementary/src/lib/elc_ctxpopup_common.h
@@ -0,0 +1,9 @@
+typedef enum
+{
+   ELM_CTXPOPUP_DIRECTION_DOWN, /**< ctxpopup show appear below clicked area */
+   ELM_CTXPOPUP_DIRECTION_RIGHT, /**< ctxpopup show appear to the right of the clicked area */
+   ELM_CTXPOPUP_DIRECTION_LEFT, /**< ctxpopup show appear to the left of the clicked area */
+   ELM_CTXPOPUP_DIRECTION_UP, /**< ctxpopup show appear above the clicked area */
+   ELM_CTXPOPUP_DIRECTION_UNKNOWN, /**< ctxpopup does not determine it's direction yet*/
+} Elm_Ctxpopup_Direction; /**< Direction in which to show the popup */
+
