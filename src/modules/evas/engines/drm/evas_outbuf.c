248c2f8233e6ecbe6054aa56589831ca41eb9b9e
Chris Michael <cp.michael@samsung.com>
2013-04-30 07:12:47 +0100

    Add initial file for Outbuf
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

---
+++ src/modules/evas/engines/drm/evas_outbuf.c
@@ -0,0 +1,15 @@
+#include "evas_common.h"
+#include "evas_private.h"
+#include "evas_engine.h"
+
+Outbuf *
+evas_outbuf_setup(int w, int h, unsigned int rotation, unsigned int depth, Eina_Bool alpha)
+{
+   return NULL;
+}
+
+void 
+evas_outbuf_free(Outbuf *ob)
+{
+
+}
