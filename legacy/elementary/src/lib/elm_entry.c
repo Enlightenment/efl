350fa0366a27d18485216f15cdfab12068998ede
WooHyun Jung <wh0705.jung@samsung.com>
2013-05-06 15:51:54 +0900

    elementary/elm_entry.c : Filter_accept should consider meanless tags and should not remove it. Thanks YoungBok Shin.

--- legacy/elementary/src/lib/elm_entry.c
+++ legacy/elementary/src/lib/elm_entry.c
@@ -4331,13 +4331,20 @@ elm_entry_filter_accept_set(void *data,
                   if (read_char == ';')
                     {
                        char *tag;
+                       int utf8;
                        tag = malloc(read_idx - last_read_idx + 1);
                        if (tag)
                          {
                             strncpy(tag, (*text) + last_read_idx, read_idx - last_read_idx);
-                            tag[read_idx - last_read_idx] = 0;
-                            read_char = *(elm_entry_markup_to_utf8(tag));
+                            tag[read_idx - last_read_idx] = 0;                            
+                            utf8 = *(elm_entry_markup_to_utf8(tag));
                             free(tag);
+                            if (!utf8)
+                              {
+                                 in_set = EINA_FALSE;
+                                 goto inserting;
+                              }
+                            read_char = utf8;
                          }
                     }
                }
