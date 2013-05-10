fd8cb707b987fe4146af2d2b5fd915ea1eec7ba6
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2013-05-10 13:35:02 +0900

    free strings in dri/drm swapper so they don't leak.

--- src/modules/evas/engines/software_x11/evas_xlib_swapper.c
+++ src/modules/evas/engines/software_x11/evas_xlib_swapper.c
@@ -556,6 +556,8 @@ _drm_init(Display *disp, int scr)
         if (swap_debug) ERR("DRM bufmgr init failed");
         goto err;
      }
+   if (drv_name) XFree(drv_name);
+   if (dev_name) XFree(dev_name);
    return EINA_TRUE;
 err:
    if (drm_fd >= 0)
@@ -583,6 +585,8 @@ err:
         dlclose(xfixes_lib);
         xfixes_lib = NULL;
      }
+   if (drv_name) XFree(drv_name);
+   if (dev_name) XFree(dev_name);
    return EINA_FALSE;
 }
 
