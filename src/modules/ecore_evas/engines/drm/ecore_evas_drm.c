817284fb9655869a26bbd6171d8e94cd4db60672
Chris Michael <cp.michael@samsung.com>
2013-05-14 08:16:45 +0100

    Add start of ecore_evas_drm code.
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

---
+++ src/modules/ecore_evas/engines/drm/ecore_evas_drm.c
@@ -0,0 +1,14 @@
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include <stdlib.h>
+#include <string.h>
+
+#include <Eina.h>
+#include <Ecore.h>
+#include "ecore_private.h"
+#include <Ecore_Input.h>
+#include <Ecore_Input_Evas.h>
+#include <Ecore_Evas.h>
+#include "ecore_evas_private.h"
