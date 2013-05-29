416981aecc34f1bf08c5b7c1633407d61ff0d1eb
Daniel Juyung Seo <seojuyung2@gmail.com>
2013-05-29 21:12:22 +0900

    elu_ews_wm.c: ELM_SAFE_FREE adoption.
    I splited ELM_SAFE_FREE refactoring patches. One commit per each file as recommended.
    For the detail, please refer 3072dab12f12fe83fb5a628d15efd5cded11787f.

--- legacy/elementary/src/lib/elu_ews_wm.c
+++ legacy/elementary/src/lib/elu_ews_wm.c
@@ -33,7 +33,7 @@ _elm_ews_wm_border_del(void *data)
 
    if (_ews_border_mover_obj == deco)
      {
-        ELM_FREE_FUNC(_ews_border_mover, ecore_animator_del);
+        ELM_SAFE_FREE(_ews_border_mover, ecore_animator_del);
         _ews_border_mover_obj = NULL;
      }
 }
@@ -276,7 +276,7 @@ _elm_ews_border_sig_move_start(void *data, Evas_Object *o __UNUSED__, const char
    Evas_Object *bs_o = ecore_evas_ews_backing_store_get(ee);
    int x, y, ox, oy;
 
-   ELM_FREE_FUNC(_ews_border_mover, ecore_animator_del);
+   ELM_SAFE_FREE(_ews_border_mover, ecore_animator_del);
 
    evas_pointer_output_xy_get(evas_object_evas_get(bs_o), &x, &y);
    evas_object_geometry_get(bs_o, &ox, &oy, NULL, NULL);
@@ -290,7 +290,7 @@ static void
 _elm_ews_border_sig_move_stop(void *data __UNUSED__, Evas_Object *o __UNUSED__, const char *sig __UNUSED__, const char *source __UNUSED__)
 {
    if (!_ews_border_mover) return;
-   ELM_FREE_FUNC(_ews_border_mover, ecore_animator_del);
+   ELM_SAFE_FREE(_ews_border_mover, ecore_animator_del);
    _ews_border_mover_obj = NULL;
 }
 
@@ -523,11 +523,11 @@ _elm_ews_wm_shutdown(void)
 {
    Ecore_Event_Handler *eh;
 
-   ELM_FREE_FUNC(_ews_border_mover, ecore_animator_del);
+   ELM_SAFE_FREE(_ews_border_mover, ecore_animator_del);
    _ews_border_mover_obj = NULL;
 
    EINA_LIST_FREE(_ews_ev_handlers, eh) ecore_event_handler_del(eh);
-   ELM_FREE_FUNC(_ews_borders, eina_hash_free);
-   ELM_FREE_FUNC(_ews_borders_geo, ecore_animator_del);
+   ELM_SAFE_FREE(_ews_borders, eina_hash_free);
+   ELM_SAFE_FREE(_ews_borders_geo, ecore_animator_del);
    _ews_bg = NULL;
 }
