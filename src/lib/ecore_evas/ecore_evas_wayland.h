97a425b52d39afa250ed2fb46419dcfef032ce61
Chris Michael <devilhorns@comcast.net>
2013-06-06 04:29:42 +0100

    Fix formatting.
    Signed-off-by: Chris Michael <devilhorns@comcast.net>

--- src/lib/ecore_evas/ecore_evas_wayland.h
+++ src/lib/ecore_evas/ecore_evas_wayland.h
@@ -3,13 +3,14 @@
 
 typedef struct _Ecore_Evas_Interface_Wayland Ecore_Evas_Interface_Wayland;
 
-struct _Ecore_Evas_Interface_Wayland {
+struct _Ecore_Evas_Interface_Wayland 
+{
    Ecore_Evas_Interface base;
 
-   void             (*resize)(Ecore_Evas *ee, int location);
-   void             (*move)(Ecore_Evas *ee, int x, int y);
-   void             (*pointer_set)(Ecore_Evas *ee, int hot_x, int hot_y);
-   void             (*type_set)(Ecore_Evas *ee, int type);
+   void (*resize)(Ecore_Evas *ee, int location);
+   void (*move)(Ecore_Evas *ee, int x, int y);
+   void (*pointer_set)(Ecore_Evas *ee, int hot_x, int hot_y);
+   void (*type_set)(Ecore_Evas *ee, int type);
    Ecore_Wl_Window* (*window_get)(const Ecore_Evas *ee);
 };
 
