d2609352700472bc3237a31d9a85a666fb5b4cfe
ChunEon Park <hermet@hermet.pe.kr>
2013-06-08 18:13:26 +0900

    elementary/naviframe - don't ignore the subtitle case

--- legacy/elementary/src/lib/elc_naviframe.c
+++ legacy/elementary/src/lib/elc_naviframe.c
@@ -368,7 +368,7 @@ _access_obj_process(Elm_Naviframe_Item *it, Eina_Bool is_access)
         ao = ((Elm_Widget_Item *)it)->access_obj;
         if (!ao) return;
 
-        if (it->title_label)
+        if (it->title_label || it->subtitle_label)
           _elm_access_edje_object_part_object_unregister
              (WIDGET(it), elm_layout_edje_get(VIEW(it)), TITLE_ACCESS_PART);
         evas_object_del(ao);
