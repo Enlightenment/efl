c44bd02f0e88b008d5662487902f6208372093ea
Stefan Schmidt <s.schmidt@samsung.com>
2013-06-13 16:25:14 +0100

    elm_gesture_layer: Fix potential NULL pointer dereference.
    If _pointer_even_make is false pe stays NULL and might get dereferenced
    later. Instead we return now if _pointer_event_make fails.
    
    Thanks to Tom for consulting with the gesture layer code.

--- legacy/elementary/src/lib/elm_gesture_layer.c
+++ legacy/elementary/src/lib/elm_gesture_layer.c
@@ -1340,8 +1340,10 @@ _event_process(void *data,
    ELM_GESTURE_LAYER_DATA_GET(data, sd);
 
    /* Start testing candidate gesture from here */
-   if (_pointer_event_make(data, event_info, event_type, &_pe))
-     pe = &_pe;
+   if (!_pointer_event_make(data, event_info, event_type, &_pe))
+     return;
+
+   pe = &_pe;
 
    /* Test all the gestures */
    {
