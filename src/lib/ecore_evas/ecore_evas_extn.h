09376dae70bba43db6020084b6b29745c1d74c46
Chris Michael <devilhorns@comcast.net>
2013-06-17 07:07:22 +0100

    Fix formatting
    Signed-off-by: Chris Michael <devilhorns@comcast.net>

--- src/lib/ecore_evas/ecore_evas_extn.h
+++ src/lib/ecore_evas/ecore_evas_extn.h
@@ -3,13 +3,14 @@
 
 typedef struct _Ecore_Evas_Interface_Extn Ecore_Evas_Interface_Extn;
 
-struct _Ecore_Evas_Interface_Extn {
+struct _Ecore_Evas_Interface_Extn 
+{
    Ecore_Evas_Interface base;
 
-    void            (*data_lock)(Ecore_Evas *ee);
-    void            (*data_unlock)(Ecore_Evas *ee);
-    Eina_Bool       (*connect)(Ecore_Evas *ee, const char *svcname, int svcnum, Eina_Bool svcsys);
-    Eina_Bool       (*listen)(Ecore_Evas *ee, const char *svcname, int svcnum, Eina_Bool svcsys);
+   void            (*data_lock)(Ecore_Evas *ee);
+   void            (*data_unlock)(Ecore_Evas *ee);
+   Eina_Bool       (*connect)(Ecore_Evas *ee, const char *svcname, int svcnum, Eina_Bool svcsys);
+   Eina_Bool       (*listen)(Ecore_Evas *ee, const char *svcname, int svcnum, Eina_Bool svcsys);
 };
 
 #endif
