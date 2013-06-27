7927c0f9fa742c6b57e97ce3ad348633db369844
Daniel Willmann <d.willmann@samsung.com>
2013-06-27 17:53:48 +0100

    edje_test: Test geometry of a more complex layout
    It seems someone thought it was fun to have rel2.offset behave
    differently than rel1.offset...
    A rel2.offset of 0 actually means the size is increased by one pixel.
    
    Acocunt for that in the tests.
    
    Signed-off-by: Daniel Willmann <d.willmann@samsung.com>

--- src/tests/edje/edje_test_edje.c
+++ src/tests/edje/edje_test_edje.c
@@ -84,6 +84,8 @@ START_TEST(edje_test_simple_layout_geometry)
    obj = edje_object_add(evas);
    fail_unless(edje_object_file_set(obj, test_layout_get("test_layout.edj"), "test_group"));
 
+   /* rel1.relative: 0.0 0.0;
+    * rel2.relative: 1.0 1.0; */
    evas_object_resize(obj, 1000, 1000);
    edje_object_part_geometry_get(obj, "background", &x, &y, &w, &h);
 
@@ -100,10 +102,61 @@ START_TEST(edje_test_simple_layout_geometry)
 }
 END_TEST
 
+START_TEST(edje_test_complex_layout)
+{
+   int x, y, w, h;
+   Evas *evas = EDJE_TEST_INIT_EVAS();
+   Evas_Object *obj;
+
+   obj = edje_object_add(evas);
+   fail_unless(edje_object_file_set(obj, test_layout_get("complex_layout.edj"), "test_group"));
+   evas_object_resize(obj, 1000, 1000);
+
+   /* Apparently rel2 offsets are retarded and you have to introduce off-by-one
+    * madness (rel2.offset: -1 -1; acts like there is zero offset...)
+    * Take that into account when checking w and h */
+
+   /* rel1.relative: 0.0 0.0; rel1.offset: 0 0;
+    * rel2.relative: 1.0 1.0; rel2.offset -1 -1; */
+   edje_object_part_geometry_get(obj, "background", &x, &y, &w, &h);
+   fail_if(x != 0 || y != 0);
+   fail_if(w != 1000-1 + 1 || h != 1000-1 + 1);
+
+   /* rel1.relative: 0.0 0.0; rel1.offset: 1 1;
+    * rel2.relative: 1.0 1.0; rel2.offset 0 0; */
+   edje_object_part_geometry_get(obj, "background2", &x, &y, &w, &h);
+   fail_if(x != 1 || y != 1);
+   fail_if(w != 1000-1 + 1 || h != 1000-1 + 1);
+
+   /* rel1.relative: 0.0 0.0; rel1.offset: 5 5;
+    * rel2.relative: 0.5 0.5; rel2.offset -2 -2; */
+   edje_object_part_geometry_get(obj, "ul", &x, &y, &w, &h);
+   fail_if(x != 5 || y != 5);
+   fail_if(w != 500-5-2 + 1 || h != 500-5-2 + 1);
+
+   /* rel1.relative: 0.5 0.0; rel1.offset: 2 5;
+    * rel2.relative: 1.0 0.5; rel2.offset -5 -2; */
+   edje_object_part_geometry_get(obj, "ur", &x, &y, &w, &h);
+   fail_if(x != 500+2 || y != 5);
+   fail_if(w != 500-5-2 + 1 || h != 500-5-2 + 1);
+
+   /* rel1.relative: 0.0 0.5; rel1.offset: 5 2;
+    * rel2.relative: 1.0 1.0; rel2.offset -5 -5; */
+   edje_object_part_geometry_get(obj, "l", &x, &y, &w, &h);
+   fail_if(x != 5 || y != 500+2);
+   fail_if(w != 1000-5-5 + 1 || h != 500-5-2 + 1);
+
+
+
+   EDJE_TEST_FREE_EVAS();
+}
+END_TEST
+
 void edje_test_edje(TCase *tc)
 {    
    tcase_add_test(tc, edje_test_edje_init);
    tcase_add_test(tc,edje_test_load_simple_layout);
    tcase_add_test(tc, edje_test_edje_load);
    tcase_add_test(tc, edje_test_simple_layout_geometry);
+   tcase_add_test(tc, edje_test_complex_layout);
 }
