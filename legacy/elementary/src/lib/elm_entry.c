74474779819f72e262ba179d0f81c0a58628da75
WooHyun Jung <wh0705.jung@samsung.com>
2013-06-27 16:30:54 +0900

    elementar/elm_entry.c : Just fixed memory leak by elm_entry_utf8_to_markup.

--- legacy/elementary/src/lib/elm_entry.c
+++ legacy/elementary/src/lib/elm_entry.c
@@ -4529,14 +4529,21 @@ elm_entry_filter_accept_set(void *data,
                   if (read_char == ';')
                     {
                        char *tag;
-                       int utf8;
+                       int utf8 = 0;
                        tag = malloc(read_idx - last_read_idx + 1);
                        if (tag)
                          {
+                            char *markup;
                             strncpy(tag, (*text) + last_read_idx, read_idx - last_read_idx);
-                            tag[read_idx - last_read_idx] = 0;                            
-                            utf8 = *(elm_entry_markup_to_utf8(tag));
+                            tag[read_idx - last_read_idx] = 0;
+                            markup = elm_entry_markup_to_utf8(tag);
+                            printf("[%s : %d] %s \n", __func__, __LINE__, markup);
                             free(tag);
+                            if (markup)
+                              {
+                                 utf8 = *markup;
+                                 free(markup);
+                              }
                             if (!utf8)
                               {
                                  in_set = EINA_FALSE;
