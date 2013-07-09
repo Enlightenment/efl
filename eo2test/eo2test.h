1d04d7f20193019b6824b57be886b06032912cf2
Jérémy Zurcher <jeremy@asynk.ch>
2013-07-09 14:11:05 +0200

    eo2: EO_FUNC -> EO_FUNC_BODY: build the function body instead of fct in the header

--- eo2test/eo2test.h
+++ eo2test/eo2test.h
@@ -11,7 +11,7 @@ enum {
      TEST_SUB_ID_LAST
 };
 
-static inline EO_FUNC(inst_func, int, TEST_ID, func(objid, a), 0, int a)
+EAPI int inst_func(eo2_a, int a);
 
 EAPI const Eo_Class *test_class_get(void);
 #define TEST_CLASS test_class_get()
