94e3e45deded835f93104def222a1140d5b60f49
Tom Hacohen <tom@stosb.com>
2013-07-09 15:48:59 +0100

    eo2: Fixed compilation.

--- eo2test/make.sh
+++ eo2test/make.sh
@@ -6,6 +6,6 @@ D="-DHAVE_CONFIG_H=1"
 L=`pkg-config --cflags --libs eina`
 
 
-gcc $CFLAGS $I $D $L $S -std=c99 eo2test.c -o eo2test && ./eo2test
+gcc $CFLAGS eo2test.c $S $I $D $L -std=c99 -o eo2test && ./eo2test
 
-gcc $CFLAGS $I $D $L $S -std=gnu99 eo2test-bench.c -lrt -o eo2test-bench && ./eo2test-bench
+gcc $CFLAGS eo2test-bench.c $S $I $D $L -std=gnu99 -lrt -o eo2test-bench && ./eo2test-bench
