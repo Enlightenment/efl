4966711754572df645fdb2a9cf29f73cb0bafbe8
Jérémy Zurcher <jeremy@asynk.ch>
2013-07-10 21:51:40 +0200

    eo2: hacky proof of concept to bypass the use of OPID and Eo_Op_Func_Description

--- eo2test/eo2test.c
+++ eo2test/eo2test.c
@@ -55,25 +55,41 @@ _inst_func_get(Eo *objid EINA_UNUSED, void *obj_data)
    return data->a;
 }
 
+/* should use macro and replace op_desc[], can't be const because of OP*/
+Eo2_Op_Description op_descs [] = {
+       { _inst_func_get, inst_func_get, EO_NOOP, EO_OP_TYPE_REGULAR, "Set properties"},
+       { _inst_func_set, inst_func_set, EO_NOOP, EO_OP_TYPE_REGULAR, "Get property A"},
+       { NULL, NULL, 0, EO_OP_TYPE_INVALID, NULL}
+};
+
 static void
 _class_constructor(Eo_Class *klass)
 {
-   const Eo_Op_Func_Description func_desc[] = {
-        EO_OP_FUNC(TEST_ID(inst_func_set), (void *) _inst_func_set),
-        EO_OP_FUNC(TEST_ID(inst_func_get), (void *) _inst_func_get),
-        EO_OP_FUNC_SENTINEL
-   };
+   eo2_class_funcs_set(klass, op_descs, OP_DESC_SIZE(op_descs));
+   // now op_descs is sorted by api_func, and class _dich is feed
 
-   eo_class_funcs_set(klass, func_desc);
 }
 
+/*********************************************************************************/
+/* can be removed when Eo2_Op_Description is used instead of Eo_Op_Description   */
+/* Eo_Op_Func_Description could also be removed                                  */
 EAPI Eo_Op TEST_BASE_ID = 0;
 
+enum {
+     TEST_SUB_ID_inst_func_set,
+     TEST_SUB_ID_inst_func_get,
+     TEST_SUB_ID_LAST
+};
+
+#define TEST_ID(func_name) (TEST_BASE_ID + (TEST_SUB_ID_ ## func_name))
+
+/* this should be replaced by Eo2_Op_Description */
 static const Eo_Op_Description op_desc[] = {
      EO_OP_DESCRIPTION(TEST_SUB_ID_inst_func_set, "Set"),
      EO_OP_DESCRIPTION(TEST_SUB_ID_inst_func_get, "Get"),
      EO_OP_DESCRIPTION_SENTINEL
 };
+/*********************************************************************************/
 
 static const Eo_Class_Description class_desc = {
      EO_VERSION,
@@ -89,4 +105,4 @@ static const Eo_Class_Description class_desc = {
 EO_DEFINE_CLASS(test_class_get, &class_desc, EO_BASE_CLASS, NULL)
 
 /* fct_sym, ret_type, OP_ID, fct_call, default_ret_val, arguments… */
-EAPI EO_FUNC_BODYV(inst_func_set, int, TEST_ID, EO_FUNC_CALLV(a, b), 0, int a, int b)
+EAPI EO_FUNC_BODYV(inst_func_set, int, EO_FUNC_CALLV(a, b), 0, int a, int b)
--- eo2test/eo2test.h
+++ eo2test/eo2test.h
@@ -3,18 +3,13 @@
 
 #include "Eo.h"
 
-extern EAPI Eo_Op TEST_BASE_ID;
-#define TEST_ID(func_name) (TEST_BASE_ID + (TEST_SUB_ID_ ## func_name))
-
-enum {
-     TEST_SUB_ID_inst_func_set,
-     TEST_SUB_ID_inst_func_get,
-     TEST_SUB_ID_LAST
-};
+// hack as it's needed by EO_FUNC_BODY
+// and does not belong to Eo_Class_Description for now
+extern Eo2_Op_Description op_descs[];
 
 EAPI int inst_func_set(eo2_a, int a, int b);
 
-static inline EO_FUNC_BODY(inst_func_get, int, TEST_ID, EO_FUNC_CALL(), -1)
+static inline EO_FUNC_BODY(inst_func_get, int, EO_FUNC_CALL(), -1)
 
 EAPI const Eo_Class *test_class_get(void);
 #define TEST_CLASS test_class_get()
