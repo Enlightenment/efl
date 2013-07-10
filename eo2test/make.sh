7da355591b518dacdaffc7366a5657d80b247288
Jérémy Zurcher <jeremy@asynk.ch>
2013-07-10 09:15:49 +0200

    eo2: make.sh use env CC

--- eo2test/make.sh
+++ eo2test/make.sh
@@ -5,7 +5,8 @@ I="-I../src/lib/eo -I../build -I../ -Ibuild"
 D="-DHAVE_CONFIG_H=1"
 L=`pkg-config --cflags --libs eina`
 
+CC=${CC:-gcc}
 
-gcc $CFLAGS eo2test.c $S $I $D $L -std=c99 -o eo2test && ./eo2test
+$CC $CFLAGS eo2test.c $S $I $D $L -std=c99 -o eo2test && ./eo2test
 
-gcc $CFLAGS eo_simple.c eo2test.c eo2-bench.c -DNOMAIN $S $I $D $L -std=gnu99 -lrt -o eo2test-bench && ./eo2test-bench
+$CC $CFLAGS eo_simple.c eo2test.c eo2-bench.c -DNOMAIN $S $I $D $L -std=gnu99 -lrt -o eo2test-bench && ./eo2test-bench
