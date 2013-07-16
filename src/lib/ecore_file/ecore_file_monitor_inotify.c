445162530113e94741c90de74881b85b2e32e7b4
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2013-07-16 20:00:43 +0900

    fix clang sizeof complaint

--- src/lib/ecore_file/ecore_file_monitor_inotify.c
+++ src/lib/ecore_file/ecore_file_monitor_inotify.c
@@ -112,7 +112,7 @@ ecore_file_monitor_backend_add(const char *path,
         ecore_file_monitor_backend_init();
      }
 
-   em = calloc(1, sizeof(Ecore_File_Monitor_Inotify));
+   em = (Ecore_File_Monitor *)calloc(1, sizeof(Ecore_File_Monitor_Inotify));
    if (!em) return NULL;
 
    em->func = func;
