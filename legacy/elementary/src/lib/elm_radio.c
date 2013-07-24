ac17ecd0b6b1be2a27a9bf789edba90bf50802bf
Daniel Juyung Seo <seojuyung2@gmail.com>
2013-07-24 20:48:22 +0900

    elm_radio.c: remove unused callback. elm,action,radio,on.
    elm,action,radio,on was not used since elm 1.0 so this is not a theme api break.

--- legacy/elementary/src/lib/elm_radio.c
+++ legacy/elementary/src/lib/elm_radio.c
@@ -279,8 +279,6 @@ _elm_radio_smart_add(Eo *obj, void *_pd, va_list *list EINA_UNUSED)
      CRITICAL("Failed to set layout!");
 
    elm_layout_signal_callback_add
-     (obj, "elm,action,radio,on", "", _radio_on_cb, obj);
-   elm_layout_signal_callback_add
      (obj, "elm,action,radio,toggle", "", _radio_on_cb, obj);
 
    priv->group = calloc(1, sizeof(Group));
