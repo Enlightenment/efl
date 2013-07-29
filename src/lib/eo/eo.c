e82c0f6bf770ff9bd7286102cdfe2b9d5896442d
Jérémy Zurcher <jeremy@asynk.ch>
2013-07-29 23:52:46 +0200

    eo2: fix indent

--- src/lib/eo/eo.c
+++ src/lib/eo/eo.c
@@ -918,7 +918,7 @@ static void
 _eo_class_constructor(_Eo_Class *klass)
 {
    if (klass->constructed)
-      return;
+     return;
 
    klass->constructed = EINA_TRUE;
 
@@ -926,7 +926,7 @@ _eo_class_constructor(_Eo_Class *klass)
      _eo2_class_funcs_set(klass);
 
    if (klass->desc->class_constructor)
-      klass->desc->class_constructor(_eo_class_id_get(klass));
+     klass->desc->class_constructor(_eo_class_id_get(klass));
 }
 
 EAPI void
