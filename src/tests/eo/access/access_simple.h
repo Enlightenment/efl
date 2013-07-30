cee4680a5f3d6deea2c0424d5eecbcaff347ea8e
Tom Hacohen <tom@stosb.com>
2013-07-30 16:08:25 +0100

    eo2: Updated the access test to use eo2.

--- src/tests/eo/access/access_simple.h
+++ src/tests/eo/access/access_simple.h
@@ -1,21 +1,12 @@
 #ifndef SIMPLE_H
 #define SIMPLE_H
 
-extern EAPI Eo_Op SIMPLE_BASE_ID;
-
-enum {
-     SIMPLE_SUB_ID_A_SET,
-     SIMPLE_SUB_ID_LAST
-};
-
 typedef struct
 {
    int public_x2;
 } Simple_Public_Data;
 
-#define SIMPLE_ID(sub_id) (SIMPLE_BASE_ID + sub_id)
-
-#define simple_a_set(a) SIMPLE_ID(SIMPLE_SUB_ID_A_SET), EO_TYPECHECK(int, a)
+EAPI void simple_a_set(int a);
 
 extern const Eo_Event_Description _EV_A_CHANGED;
 #define EV_A_CHANGED (&(_EV_A_CHANGED))
