5f45e57b89b2bc3ffa904fd33ced8fe718c6e0dc
Tom Hacohen <tom@stosb.com>
2013-07-30 14:12:03 +0100

    eo2: revert "eo2_add accepts non-defauld constructors"
    We want to have normal functions as non-default constructors, not va_arg
    ones. What we should do is split the object creation to two parts again.
    The creation, the constructing (changes using the macro) and the
    verification/end part that checks the constructor has been called.
    
    This reverts commit 2ff2ce1894f173b306a896bda595e1a7768c074d.

--- src/lib/eo/eo.c
+++ src/lib/eo/eo.c
@@ -1472,22 +1472,8 @@ eo_add_internal(const char *file, int line, const Eo_Class *klass_id, Eo *parent
 
    /* Run the relevant do stuff. */
    if (klass->desc->version == EO2_VERSION)
-     {
-        eo2_constructor_type constr;
-        va_list p_list;
-        if(eo2_do_start((Eo *)obj_id, EINA_FALSE))
-          {
-             va_start(p_list, parent_id);
-             while ((constr = va_arg(p_list, eo2_constructor_type)))
-               constr();
-             va_end(p_list);
-             eo2_do_end(NULL);
-             do_err = EINA_FALSE;
-          }
-        else
-          do_err = EINA_TRUE;
-     }
-   else
+     eo2_do((Eo *)obj_id, eo2_constructor());
+   /* Run the relevant do stuff. */
      {
         va_list p_list;
         va_start(p_list, parent_id);
