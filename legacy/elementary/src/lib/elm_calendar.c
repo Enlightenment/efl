bd5299d1a178cc9d13a4b6d6f201744703b93d9b
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2013-08-07 16:49:23 +0900

    fix finger size calc for calendar

--- legacy/elementary/src/lib/elm_calendar.c
+++ legacy/elementary/src/lib/elm_calendar.c
@@ -69,8 +69,9 @@ _elm_calendar_smart_sizing_eval(Eo *obj, void *_pd EINA_UNUSED, va_list *list EI
    Evas_Coord minw = -1, minh = -1;
 
    Elm_Widget_Smart_Data *wd = eo_data_scope_get(obj, ELM_OBJ_WIDGET_CLASS);
-
-   elm_coords_finger_size_adjust(8, &minw, ELM_DAY_LAST, &minh);
+   
+   // 7x8 (1 month+year, days, 6 dates.)
+   elm_coords_finger_size_adjust(7, &minw, 8, &minh);
    edje_object_size_min_restricted_calc
      (wd->resize_obj, &minw, &minh, minw, minh);
    evas_object_size_hint_min_set(obj, minw, minh);
