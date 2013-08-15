4e1989433e3c8b2af2bf5e5aa7ff44888186d8c2
Daniel Juyung Seo <seojuyung@gmail.com>
2013-08-15 22:42:09 +0900

    elm_win.c: formatting fix before working on this file.

--- legacy/elementary/src/lib/elm_win.c
+++ legacy/elementary/src/lib/elm_win.c
@@ -2117,7 +2117,7 @@ _elm_win_focus_highlight_init(Elm_Win_Smart_Data *sd)
                            _elm_win_object_focus_out, sd->obj);
 
    sd->focus_highlight.cur.target = evas_focus_get(sd->evas);
-   sd->focus_highlight.fobj= edje_object_add(sd->evas);
+   sd->focus_highlight.fobj = edje_object_add(sd->evas);
    sd->focus_highlight.theme_changed = EINA_TRUE;
 
    edje_object_signal_callback_add(sd->focus_highlight.fobj,
