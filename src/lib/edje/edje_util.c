88cc896f06ef8975e24c6d8403aa259925339a79
Michaël Bouchaud (yoz) <yoz@efl.so>
2013-09-04 12:06:03 +0200

    edje: be recursive on edje_object_scale_set

--- src/lib/edje/edje_util.c
+++ src/lib/edje/edje_util.c
@@ -363,7 +363,7 @@ _scale_set(Eo *obj, void *_pd, va_list *list)
    double scale = va_arg(*list, double);
    Eina_Bool *ret = va_arg(*list, Eina_Bool *);
    if (ret) *ret = EINA_TRUE;
-   Edje *ed, *ged;
+   Edje *ed;
    Evas_Object *o;
    Eina_List *l;
    unsigned int i;
@@ -371,12 +371,10 @@ _scale_set(Eo *obj, void *_pd, va_list *list)
    ed = _pd;
    if (ed->scale == scale) return;
    ed->scale = FROM_DOUBLE(scale);
-   EINA_LIST_FOREACH(ed->groups, l, ged)
-     ged->scale = ed->scale;
    EINA_LIST_FOREACH(ed->subobjs, l, o)
      {
         const char *s = evas_object_type_get(o);
-        if ((s) && (!strcmp(s, "edje"))) edje_object_calc_force(o);
+        if ((s) && (!strcmp(s, "edje"))) edje_object_scale_set(o, scale);
      }
    for (i = 0; i < ed->table_parts_size; i++)
      {
