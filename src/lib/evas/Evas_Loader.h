fdea402d32810b3fd4384a177714d4f0c2e74a6a
Cedric Bail <cedric.bail@samsung.com>
2013-10-01 17:24:37 +0900

    evas: try to set EAPI correctly.

--- src/lib/evas/Evas_Loader.h
+++ src/lib/evas/Evas_Loader.h
@@ -3,6 +3,36 @@
 
 #include "Eina.h"
 
+#ifdef EAPI
+# undef EAPI
+#endif
+
+#ifdef _WIN32
+# ifdef EFL_EVAS_BUILD
+#  ifdef DLL_EXPORT
+#   define EAPI __declspec(dllexport)
+#  else
+#   define EAPI
+#  endif /* ! DLL_EXPORT */
+# else
+#  define EAPI __declspec(dllimport)
+# endif /* ! EFL_EVAS_BUILD */
+#else
+# ifdef __GNUC__
+#  if __GNUC__ >= 4
+#   define EAPI __attribute__ ((visibility("default")))
+#  else
+#   define EAPI
+#  endif
+# else
+#  define EAPI
+# endif
+#endif /* ! _WIN32 */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* the module api version */
 #define EVAS_MODULE_API_VERSION 2
 
@@ -163,5 +193,9 @@ EAPI Eina_Bool    evas_module_unregister (const Evas_Module_Api *module, Evas_Mo
 #define EVAS_EINA_MODULE_DEFINE(Tn, Name)	\
   EINA_MODULE_INIT(evas_##Tn##_##Name##_init);	\
   EINA_MODULE_SHUTDOWN(evas_##Tn##_##Name##_shutdown);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
