4eb7b8192533451e216b00f6b32ca4340c035550
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2013-11-03 20:33:05 +0900

    evas - re-fix to use first, last or if item is list head on text recomp

--- src/lib/evas/canvas/evas_object_text.c
+++ src/lib/evas/canvas/evas_object_text.c
@@ -169,7 +169,9 @@ _evas_object_text_item_del(Evas_Object_Text *o, Evas_Object_Text_Item *it)
    else if (o->last_computed.ellipsis_end == it)
      o->last_computed.ellipsis_end = NULL;
 
-   if (EINA_INLIST_GET(it)->last)
+   if ((EINA_INLIST_GET(it)->next) ||
+       (EINA_INLIST_GET(it)->prev) ||
+       (o->items == (EINA_INLIST_GET(it))))
      o->items = (Evas_Object_Text_Item *)eina_inlist_remove
      (EINA_INLIST_GET(o->items), EINA_INLIST_GET(it));
    _evas_object_text_item_clean(it);
