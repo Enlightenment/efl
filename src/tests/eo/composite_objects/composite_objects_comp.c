14cb50f7939cfbcc6cf3decad87340d8c09410ad
Tom Hacohen <tom@stosb.com>
2013-11-07 15:28:55 +0000

    eo2: Migrated the composite test to eo2.

--- src/tests/eo/composite_objects/composite_objects_comp.c
+++ src/tests/eo/composite_objects/composite_objects_comp.c
@@ -8,58 +8,51 @@
 
 #include "../eunit_tests.h"
 
-EAPI Eo_Op COMP_BASE_ID = 0;
-
 #define MY_CLASS COMP_CLASS
 
-static void
-_a_get(Eo *obj, void *class_data EINA_UNUSED, va_list *list)
+static int
+_a_get(Eo *obj, void *class_data EINA_UNUSED)
 {
-   int *a;
-   a = va_arg(*list, int *);
-   eo_do_super(obj, MY_CLASS, simple_a_get(a));
+   int a;
+   eo2_do_super(obj, MY_CLASS, a = simple_a_get());
+
+   return a;
 }
 
 static void
-_constructor(Eo *obj, void *class_data EINA_UNUSED, va_list *list EINA_UNUSED)
+_constructor(Eo *obj, void *class_data EINA_UNUSED)
 {
-   eo_do_super(obj, MY_CLASS, eo_constructor());
+   eo2_do_super(obj, MY_CLASS, eo2_constructor());
 
-   Eo *simple = eo_add(SIMPLE_CLASS, obj);
+   Eo *simple = eo2_add(SIMPLE_CLASS, obj);
    eo_composite_attach(simple, obj);
-   eo_do(simple, eo_event_callback_forwarder_add(EV_A_CHANGED, obj));
+   eo2_do(simple, eo2_event_callback_forwarder_add(EV_A_CHANGED, obj));
 
    fail_if(eo_composite_is(obj));
    fail_if(!eo_composite_is(simple));
 
-   eo_do(obj, eo_base_data_set("simple-obj", simple, NULL));
+   eo2_do(obj, eo2_base_data_set("simple-obj", simple, NULL));
 
    eo_unref(simple);
 }
 
-static void
-_class_constructor(Eo_Class *klass)
-{
-   const Eo_Op_Func_Description func_desc[] = {
-        EO_OP_FUNC(EO_BASE_ID(EO_BASE_SUB_ID_CONSTRUCTOR), _constructor),
-        EO_OP_FUNC(SIMPLE_ID(SIMPLE_SUB_ID_A_GET), _a_get),
-        EO_OP_FUNC_SENTINEL
-   };
-
-   eo_class_funcs_set(klass, func_desc);
-}
+static Eo2_Op_Description op_descs[] = {
+     EO2_OP_FUNC_OVERRIDE(_constructor, eo2_constructor),
+     EO2_OP_FUNC_OVERRIDE(_a_get, simple_a_get),
+     EO2_OP_SENTINEL
+};
 
 static const Eo_Class_Description class_desc = {
-     EO_VERSION,
+     EO2_VERSION,
      "Comp",
      EO_CLASS_TYPE_REGULAR,
-     EO_CLASS_DESCRIPTION_OPS(NULL, NULL, 0),
+     EO2_CLASS_DESCRIPTION_OPS(op_descs),
      NULL,
      0,
-     _class_constructor,
+     NULL,
      NULL
 };
 
-EO_DEFINE_CLASS(comp_class_get, &class_desc, EO_BASE_CLASS,
+EO_DEFINE_CLASS(comp_class_get, &class_desc, EO2_BASE_CLASS,
       SIMPLE_CLASS, NULL);
 
--- src/tests/eo/composite_objects/composite_objects_simple.h
+++ src/tests/eo/composite_objects/composite_objects_simple.h
@@ -1,23 +1,13 @@
 #ifndef SIMPLE_H
 #define SIMPLE_H
 
-extern EAPI Eo_Op SIMPLE_BASE_ID;
-
-enum {
-     SIMPLE_SUB_ID_A_SET,
-     SIMPLE_SUB_ID_A_GET,
-     SIMPLE_SUB_ID_LAST
-};
-
 typedef struct
 {
    int a;
 } Simple_Public_Data;
 
-#define SIMPLE_ID(sub_id) (SIMPLE_BASE_ID + sub_id)
-
-#define simple_a_set(a) SIMPLE_ID(SIMPLE_SUB_ID_A_SET), EO_TYPECHECK(int, a)
-#define simple_a_get(a) SIMPLE_ID(SIMPLE_SUB_ID_A_GET), EO_TYPECHECK(int *, a)
+EAPI void simple_a_set(int a);
+EAPI int simple_a_get(void);
 
 extern const Eo_Event_Description _EV_A_CHANGED;
 #define EV_A_CHANGED (&(_EV_A_CHANGED))
