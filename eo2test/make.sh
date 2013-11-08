b56423a486657b52bdcbe25fd6847bfc0c8cc690
Tom Hacohen <tom@stosb.com>
2013-11-08 14:58:08 +0000

    eo2: Fixed eo2test/make.sh.

--- eo2test/make.sh
+++ eo2test/make.sh
@@ -7,6 +7,6 @@ L=`pkg-config --cflags --libs eina`
 
 CC=${CC:-gcc}
 
-$CC $CFLAGS eo_simple.c eo2_simple.c eo2-bench.c -DNOMAIN $S $I $D $L -std=gnu99 -lrt -o eo2-bench && ./eo2-bench || exit 1
+$CC $CFLAGS eo_simple.c eo2_simple.c eo2-bench.c -DEFL_BETA_API_SUPPORT -DNOMAIN $S $I $D $L -std=gnu99 -lrt -o eo2-bench && ./eo2-bench || exit 1
 
 g++ $CFLAGS --std=c++0x simplesignal.cc `pkg-config --cflags --libs eina eo` -o simplesignal && ./simplesignal || exit 1
