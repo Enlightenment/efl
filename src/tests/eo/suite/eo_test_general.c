e0f89ca8d5f250d6397e03021b59822f7db3cd75
Tom Hacohen <tom@stosb.com>
2013-11-08 14:57:47 +0000

    Eo2 test-suite: Fixed a compliation issue.

--- src/tests/eo/suite/eo_test_general.c
+++ src/tests/eo/suite/eo_test_general.c
@@ -622,7 +622,7 @@ START_TEST(eo_magic_checks)
 
         eo2_do((Eo *) buf,
 	      eo2_wref_add(&wref),
-	      eo2_parent_get(&parent));
+	      parent = eo2_parent_get());
         fail_if(wref);
         fail_if(parent);
 
