c20c53752809452c8b8df971d11572e70ba22d70
Tom Hacohen <tom@stosb.com>
2013-11-08 15:02:06 +0000

    eo2: use EO2_CLASS_DESCRIPTION_NOOPS everywhere.

--- src/lib/eo/eo2_class_class.c
+++ src/lib/eo/eo2_class_class.c
@@ -4,15 +4,11 @@
 
 #include "Eo.h"
 
-static Eo2_Op_Description op_descs [] = {
-     EO2_OP_SENTINEL
-};
-
 static const Eo_Class_Description class_desc = {
      EO2_VERSION,
      "Eo_Abstract_Class",
      EO_CLASS_TYPE_REGULAR_NO_INSTANT,
-     EO2_CLASS_DESCRIPTION_OPS(op_descs),
+     EO2_CLASS_DESCRIPTION_NOOPS(),
      NULL,
      0,
      NULL,
--- src/tests/eo/function_overrides/function_overrides_inherit.c
+++ src/tests/eo/function_overrides/function_overrides_inherit.c
@@ -12,7 +12,7 @@ static const Eo_Class_Description class_desc = {
      EO2_VERSION,
      "Inherit",
      EO_CLASS_TYPE_REGULAR,
-     EO_CLASS_DESCRIPTION_OPS(NULL, NULL, 0),
+     EO2_CLASS_DESCRIPTION_NOOPS(),
      NULL,
      0,
      NULL,
