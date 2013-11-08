67c1b3f724283fd542c61f8e590f88ea4d9e7644
Tom Hacohen <tom@stosb.com>
2013-11-08 10:48:55 +0000

    Eo2: Migrated mixin test to eo2.

--- src/tests/eo/mixin/mixin_mixin.h
+++ src/tests/eo/mixin/mixin_mixin.h
@@ -1,22 +1,7 @@
 #ifndef MIXIN_H
 #define MIXIN_H
 
-extern EAPI Eo_Op MIXIN_BASE_ID;
-
-enum {
-     MIXIN_SUB_ID_AB_SUM_GET,
-     MIXIN_SUB_ID_LAST
-};
-
-#define MIXIN_ID(sub_id) (MIXIN_BASE_ID + sub_id)
-
-
-/**
- * @def mixin_ab_sum_get(sum)
- * @brief Get sum of a,b integer elements
- * @param[out] sum integer pointer to sum - value
- */
-#define mixin_ab_sum_get(sum) MIXIN_ID(MIXIN_SUB_ID_AB_SUM_GET), EO_TYPECHECK(int *, sum)
+EAPI int mixin_ab_sum_get(void);
 
 #define MIXIN_CLASS mixin_class_get()
 const Eo_Class *mixin_class_get(void);
