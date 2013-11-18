ba30060ff971a3a9201bdc8a789dc56f382695cd
Cedric Bail <cedric.bail@samsung.com>
2013-11-18 16:01:27 +0900

    evas: try to clarify licensing for people that are going to make their own Evas loader.

--- src/lib/evas/Evas_Loader.h
+++ src/lib/evas/Evas_Loader.h
@@ -1,3 +1,38 @@
+/* EVAS - EFL Scene Graph
+ * Copyright (C) 2013 Enlightenment Developers:
+ *           Cedric Bail <cedric.bail@free.fr>
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *    2. Redistributions in binary form must reproduce the above copyright 
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+/**
+ * What does it means for you ?
+ *    Evas Loader are dynamically linked to Evas at run time. You can use anything
+ * you want here as long as the license of the module you add to your system match
+ * the application you are using it in. Be aware that if you make a module that use
+ * a GPL library, it may turn the license of your binary to GPL. Evas doesn't ship
+ * with any GPL module.
+ */
+
 #ifndef _EVAS_LOADER_H
 #define _EVAS_LOADER_H
 
