58b578c9b07497496fedd51f3aa3bd434a0fa305
Jérémy Zurcher <jeremy@asynk.ch>
2013-11-20 10:19:16 +0100

    eina: fix a possible race condition during eina_file_close.
    replay 7e8fb93 without the breakage

--- src/lib/eina/eina_file_common.c
+++ src/lib/eina/eina_file_common.c
@@ -451,17 +451,20 @@ eina_file_close(Eina_File *file)
 
    EINA_SAFETY_ON_NULL_RETURN(file);
 
+   eina_lock_take(&_eina_file_lock_cache);
+
    eina_lock_take(&file->lock);
    file->refcount--;
    if (file->refcount == 0) leave = EINA_FALSE;
    eina_lock_release(&file->lock);
-   if (leave) return;
-
-   eina_lock_take(&_eina_file_lock_cache);
+   if (leave) goto end;
 
    eina_hash_del(_eina_file_cache, file->filename, file);
+
+   // Backend specific file resource close
    eina_file_real_close(file);
 
+ end:
    eina_lock_release(&_eina_file_lock_cache);
 }
 
