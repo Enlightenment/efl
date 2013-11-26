df983469388dc34e5c95161bbcc3a318c86b8891
Chris Michael <cp.michael@samsung.com>
2013-11-26 14:13:50 +0000

    Add start of ecore_drm_launch code
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

---
+++ src/bin/ecore_drm/ecore_drm_launch.c
@@ -0,0 +1,16 @@
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+
+int 
+main(int argc, char **argv)
+{
+   printf("Ecore_Drm_Launch Started\n");
+
+   return 0;
+}
