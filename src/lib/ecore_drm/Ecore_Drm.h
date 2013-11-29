e4d203762a2feff2a8fd57059bcb6334e6f7ccd2
Chris Michael <cp.michael@samsung.com>
2013-11-29 11:07:13 +0000

    Add operation support to ecore_drm (types and enums for message passing)
    This adds a structure to be used for passing messages between
    ecore_drm library and the ecore_drm_launch slave process. Also added
    here is an enum for Operations, and an enum for Operation Results.
    
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

--- src/lib/ecore_drm/Ecore_Drm.h
+++ src/lib/ecore_drm/Ecore_Drm.h
@@ -23,6 +23,30 @@
 # endif // ifdef __GNUC__
 #endif // ifdef _MSC_VER
 
+typedef enum _Ecore_Drm_Op
+{
+   ECORE_DRM_OP_READ_FD_SET,
+   ECORE_DRM_OP_WRITE_FD_SET,
+   ECORE_DRM_OP_OPEN_FD,
+   ECORE_DRM_OP_CLOSE_FD
+} Ecore_Drm_Op;
+
+typedef enum _Ecore_Drm_Op_Result 
+{
+   ECORE_DRM_OP_SUCCESS,
+   ECORE_DRM_OP_FAILURE
+} Ecore_Drm_Op_Result;
+
+/* structure for message passing */
+typedef struct _Ecore_Drm_Message 
+{
+   int opcode, size;
+   void *data;
+} Ecore_Drm_Message;
+
+/* opaque structure to represent a drm device */
+typedef struct _Ecore_Drm_Device Ecore_Drm_Device;
+
 /**
  * @file
  * @brief Ecore functions for dealing with drm, virtual terminals
@@ -33,10 +57,13 @@
  * Ecore_Drm provides a wrapper and functions for using libdrm
  * 
  * @li @ref Ecore_Drm_Init_Group
+ * @li @ref Ecore_Drm_Device_Group
  * 
  */
 
 EAPI int ecore_drm_init(void);
 EAPI int ecore_drm_shutdown(void);
+
+EAPI Ecore_Drm_Device *ecore_drm_device_find(const char *name);
 
 #endif
