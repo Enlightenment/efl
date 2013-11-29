cda78b1b12c72adf2b0097e21af25ea35819cdbd
Chris Michael <cp.michael@samsung.com>
2013-11-29 11:09:47 +0000

    Add additional internal headers we need for various functions
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

--- src/lib/ecore_drm/ecore_drm_private.h
+++ src/lib/ecore_drm/ecore_drm_private.h
@@ -9,9 +9,13 @@
 # include <stdlib.h>
 # include <string.h>
 # include <unistd.h>
+# include <fcntl.h>
 # include <sys/types.h>
+# include <sys/socket.h>
+# include <sys/wait.h>
 # include <sys/stat.h>
 # include <sys/ioctl.h>
+# include <errno.h>
 
 # include <Ecore_Drm.h>
 
