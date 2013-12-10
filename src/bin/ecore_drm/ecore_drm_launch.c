4a242ac7240d75ad66256ce276cc08a4c25f65cd
Chris Michael <cp.michael@samsung.com>
2013-12-10 13:04:17 +0000

    Add code to setup tty when it is opened. Add function to close an FD.
    After we open a tty, we need to send some ioctl()s to put it into
    graphics mode. Also, add code to actually close an FD. This can close
    a tty, drm device fd, etc, etc.
    
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

--- src/bin/ecore_drm/ecore_drm_launch.c
+++ src/bin/ecore_drm/ecore_drm_launch.c
@@ -9,12 +9,20 @@
 #include <limits.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <signal.h>
+#include <poll.h>
+#include <linux/kd.h>
+#include <linux/vt.h>
 #include <linux/major.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/signalfd.h>
 #include <sys/stat.h>
-#include <poll.h>
+#include <sys/ioctl.h>
+
+/* #include <xf86drm.h> */
+/* #include <xf86drmMode.h> */
+/* #include <drm_fourcc.h> */
 
 #include <Eina.h>
 #include <Ecore_Drm.h>
@@ -92,31 +100,88 @@ _open_device(const char *file)
 static int 
 _open_tty(const char *file)
 {
-   int fd = -1;
+   int fd = -1, mode = 0;
    int ret = ECORE_DRM_OP_SUCCESS;
    struct stat st;
+   struct vt_mode vtmode;
 
    fprintf(stderr, "Open Tty: %s\n", file);
 
    if (!strncmp(file, "/dev/tty0", 9))
      fd = dup(0);
-   else if ((fd = open(file, (O_RDWR | O_CLOEXEC))) < 0)
+   else if ((fd = open(file, (O_RDWR | O_NOCTTY))) < 0)
      {
         fprintf(stderr, "\tFailed to open: %m\n");
         ret = ECORE_DRM_OP_FAILURE;
+        goto mode_err;
      }
 
    if ((fstat(fd, &st) < 0) || 
-       (major(st.st_rdev) != TTY_MAJOR) || 
-       (minor(st.st_rdev) == 0))
+       (major(st.st_rdev) != TTY_MAJOR) || (minor(st.st_rdev) == 0))
      {
         fprintf(stderr, "\t%s not a tty\n", file);
-        ret = ECORE_DRM_OP_FAILURE;
-        close(fd);
-        fd = -1;
+        goto mode_err;
      }
 
+   /* try to get the current mode of this vt */
+   if (ioctl(fd, KDGETMODE, &mode))
+     {
+        fprintf(stderr, "\tCould not get vt mode: %m\n");
+        goto mode_err;
+     }
+
+   /* if we are in text mode, switch to graphics mode */
+   if (mode == KD_TEXT)
+     {
+        /* try to set graphics mode */
+        if (ioctl(fd, KDSETMODE, KD_GRAPHICS) < 0)
+          {
+             fprintf(stderr, "\tCould not set graphics mode: %m\n");
+             goto mode_err;
+          }
+     }
+
+   /* setup vt mode signals */
+   vtmode.mode = VT_PROCESS;
+   vtmode.waitv = 0;
+   vtmode.relsig = SIGUSR1;
+   vtmode.acqsig = SIGUSR2;
+
+   /* try to set mode */
+   if (ioctl(fd, VT_SETMODE, &vtmode) < 0)
+     {
+        fprintf(stderr, "\tCould not set mode signals: %m\n");
+        /* reset to text mode */
+        if (mode == KD_TEXT) ioctl(fd, KDSETMODE, KD_TEXT);
+        goto mode_err;
+     }
+
+   fprintf(stderr, "\tOpened Tty: %d\n", fd);
+
    _send_msg(ECORE_DRM_OP_TTY_OPEN, fd, &ret, sizeof(int));
+
+   return ret;
+
+mode_err:
+   if (fd > 0) close(fd);
+   fd = -1;
+   ret = ECORE_DRM_OP_FAILURE;
+   _send_msg(ECORE_DRM_OP_TTY_OPEN, fd, &ret, sizeof(int));
+
+   return ret;
+}
+
+static int 
+_close_fd(int opcode, int fd)
+{
+   int ret = ECORE_DRM_OP_SUCCESS;
+
+   fprintf(stderr, "Close FD: %d\n", fd);
+
+   if (fd >= 0) close(fd);
+   fd = -1;
+
+   _send_msg(opcode, fd, &ret, sizeof(int));
 
    return ret;
 }
@@ -221,7 +286,6 @@ _read_msg(void)
         fprintf(stderr, "Ecore_Drm_Operation_Write_FD_Set\n");
         _drm_write_fd = *((int *)CMSG_DATA(cmsg));
         if (_drm_write_fd >= 0) ret = 1;
-        fprintf(stderr, "\tWrite FD: %d\n", _drm_write_fd);
         break;
       case ECORE_DRM_OP_DEVICE_OPEN:
         fprintf(stderr, "Ecore_Drm_Operation_Device_Open: %s\n", (char *)data);
@@ -229,7 +293,7 @@ _read_msg(void)
         break;
       case ECORE_DRM_OP_DEVICE_CLOSE:
         fprintf(stderr, "Ecore_Drm_Operation_Device_Close\n");
-        ret = 1;
+        ret = _close_fd(ECORE_DRM_OP_DEVICE_CLOSE, *((int *)data));
         break;
       case ECORE_DRM_OP_TTY_OPEN:
         fprintf(stderr, "Ecore_Drm_Operation_Tty_Open: %s\n", (char *)data);
@@ -237,7 +301,7 @@ _read_msg(void)
         break;
       case ECORE_DRM_OP_TTY_CLOSE:
         fprintf(stderr, "Ecore_Drm_Operation_Tty_Close\n");
-        ret = 1;
+        ret = _close_fd(ECORE_DRM_OP_TTY_CLOSE, *((int *)data));
         break;
       default:
         fprintf(stderr, "Uknown operation: %d\n", dmsg.opcode);
@@ -260,13 +324,13 @@ main(int argc EINA_UNUSED, char **argv EINA_UNUSED)
 
    fprintf(stderr, "\tGot Fd: %d\n", _drm_read_fd);
 
-   if ((_drm_write_fd = _write_fd_get()) < 0)
-     {
-        fprintf(stderr, "\tCould not Get FD\n");
-        return EXIT_FAILURE;
-     }
+   /* if ((_drm_write_fd = _write_fd_get()) < 0) */
+   /*   { */
+   /*      fprintf(stderr, "\tCould not Get FD\n"); */
+   /*      return EXIT_FAILURE; */
+   /*   } */
 
-   fprintf(stderr, "\tGot Fd: %d\n", _drm_write_fd);
+   /* fprintf(stderr, "\tGot Fd: %d\n", _drm_write_fd); */
 
    while (1)
      {
