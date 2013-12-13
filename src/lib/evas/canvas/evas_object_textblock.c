Enlightenment/efl

commit 3f3ad67616d491c40e4c7c34d3927896f80b8caa

Evas textblock: Fixed a bug with deletion of ranges.
Fixed a bug with deletion of ranges that end just before a visual format.
Thanks to WooHyun Jung for reporting.

src/lib/evas/canvas/evas_object_textblock.c
@@ -7545,6 +7545,11 @@ _evas_textblock_node_text_adjust_offsets_to_start(Evas_Object_Textblock *o,
              last_node->offset -= delta;
              break;
           }
+        else if (use_end && itr && (pos + itr->offset >= (size_t) end) &&
+              itr->visible)
+          {
+             break;
+          }
 
         delta = orig_end - pos;
         if (!first)

src/tests/evas/evas_test_textblock.c
@@ -1218,6 +1218,13 @@ START_TEST(evas_textblock_format_removal)
    fnode = evas_textblock_node_format_first_get(tb);
    fail_if (fnode);
 
+   /* Range delete with empty paragraphs. */
+   evas_object_textblock_text_markup_set(tb, "<ps/><ps/><ps/><ps/><ps/>");
+   evas_textblock_cursor_pos_set(cur, 2);
+   evas_textblock_cursor_pos_set(main_cur, 3);
+   evas_textblock_cursor_range_delete(cur, main_cur);
+   ck_assert_str_eq(evas_object_textblock_text_markup_get(tb), "<ps/><ps/><ps/><ps/>");
+
    /* Verify fmt position and REP_CHAR positions are the same */
    evas_object_textblock_text_markup_set(tb,
          "This is<ps/>an <item absize=93x152 vsize=ascent></>a.");

commit 7141c1dbb2fd5ca0a12284cea7e6e1b7fb4af0e0

Evas textblock: Fixed deletion of ranges ending with visual formats.
This is a regression introduced in:
3f3ad67616d491c40e4c7c34d3927896f80b8caa

src/lib/evas/canvas/evas_object_textblock.c
@@ -7830,7 +7830,7 @@ _evas_textblock_node_text_adjust_offsets_to_start(Evas_Object_Textblock *o,
              last_node->offset -= delta;
              break;
           }
-        else if (use_end && itr && (pos + itr->offset >= (size_t) end) &&
+        else if (use_end && itr && (pos + itr->offset > (size_t) end) &&
               itr->visible)
           {
              break;