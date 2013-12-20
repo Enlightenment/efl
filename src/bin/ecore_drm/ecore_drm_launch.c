edf9eee50049eba4784db79631a7d43b9181bb7e
Chris Michael <cp.michael@samsung.com>
2013-12-20 11:34:11 +0000

    Rework ecore_drm slave code:   - This reworks setuid slave process to pass the fd's credentials to the parent process.   - Use epoll instead of normal poll() .. (faster)
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

--- src/bin/ecore_drm/ecore_drm_launch.c
+++ src/bin/ecore_drm/ecore_drm_launch.c
@@ -2,189 +2,43 @@
 # include "config.h"
 #endif
 
+/* standard headers */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <limits.h>
-#include <fcntl.h>
 #include <errno.h>
 #include <signal.h>
-#include <poll.h>
-#include <linux/kd.h>
-#include <linux/vt.h>
-#include <linux/major.h>
-#include <sys/socket.h>
+#include <fcntl.h>
 #include <sys/types.h>
-#include <sys/signalfd.h>
-#include <sys/stat.h>
-#include <sys/ioctl.h>
-
-/* #include <xf86drm.h> */
-/* #include <xf86drmMode.h> */
-/* #include <drm_fourcc.h> */
+#include <sys/socket.h>
+#include <sys/epoll.h>
 
 #include <Eina.h>
 #include <Ecore_Drm.h>
 
+static struct cmsghdr *cmsgptr = NULL;
+static int _read_fd = -1;
+static int _write_fd = -1;
+static int _epoll_fd = -1;
+
+#if defined(SCM_CREDS) // Bsd
+# define CRED_STRUCT cmsgcred
+# define SCM_CREDTYPE SCM_CREDS
+#elif defined(SCM_CREDENTIALS) // Linux (3.2.0 ?)
+# define CRED_STRUCT ucred
+# define SCM_CREDTYPE SCM_CREDENTIALS
+# define CRED_OPT SO_PASSCRED
+#endif
+
+#define RIGHTS_LEN CMSG_LEN(sizeof(int))
+#define CREDS_LEN CMSG_LEN(sizeof(struct CRED_STRUCT))
+#define CONTROL_LEN (RIGHTS_LEN + CREDS_LEN)
+
 #define IOVSET(_iov, _addr, _len) \
    (_iov)->iov_base = (void *)(_addr); \
    (_iov)->iov_len = (_len);
-
-static int _drm_read_fd = -1;
-static int _drm_write_fd = -1;
-
-static int 
-_send_msg(int opcode, int fd, void *data, size_t bytes)
-{
-   Ecore_Drm_Message dmsg;
-   struct iovec iov[2];
-   char ctrl[CMSG_SPACE(sizeof(int))];
-   struct msghdr msg;
-   struct cmsghdr *cmsg;
-   ssize_t size;
-
-   IOVSET(iov + 0, &dmsg, sizeof(dmsg));
-   IOVSET(iov + 1, data, bytes);
-
-   dmsg.opcode = opcode;
-   dmsg.size = bytes;
-
-   memset(&msg, 0, sizeof(struct msghdr));
-   memset(ctrl, 0, CMSG_SPACE(sizeof(int)));
-
-   msg.msg_name = NULL;
-   msg.msg_namelen = 0;
-   msg.msg_iov = iov;
-   msg.msg_iovlen = 2;
-   msg.msg_controllen = CMSG_SPACE(sizeof(int));
-   msg.msg_control = ctrl;
-
-   cmsg = CMSG_FIRSTHDR(&msg);
-   cmsg->cmsg_level = SOL_SOCKET;
-   cmsg->cmsg_type = SCM_RIGHTS;
-   cmsg->cmsg_len = CMSG_LEN(sizeof(int));
-
-   *((int *)CMSG_DATA(cmsg)) = fd;
-
-   errno = 0;
-   size = sendmsg(_drm_write_fd, &msg, 0);
-   if (errno != 0)
-     {
-        fprintf(stderr, "Failed to send message: %d\n", errno);
-        fprintf(stderr, "\t%s\n", strerror(errno));
-     }
-
-   return size;
-}
-
-static int 
-_open_device(const char *file)
-{
-   int fd = -1;
-   int ret = ECORE_DRM_OP_SUCCESS;
-
-   fprintf(stderr, "Open Device: %s\n", file);
-
-   if ((fd = open(file, O_RDWR)) < 0)
-     {
-        fprintf(stderr, "\tFailed to open: %m\n");
-        ret = ECORE_DRM_OP_FAILURE;
-     }
-
-   _send_msg(ECORE_DRM_OP_DEVICE_OPEN, fd, &ret, sizeof(int));
-
-   return ret;
-}
-
-static int 
-_open_tty(const char *file)
-{
-   int fd = -1, mode = 0;
-   int ret = ECORE_DRM_OP_SUCCESS;
-   struct stat st;
-   struct vt_mode vtmode;
-
-   fprintf(stderr, "Open Tty: %s\n", file);
-
-   if (!strncmp(file, "/dev/tty0", 9))
-     fd = dup(0);
-   else if ((fd = open(file, (O_RDWR | O_NOCTTY))) < 0)
-     {
-        fprintf(stderr, "\tFailed to open: %m\n");
-        ret = ECORE_DRM_OP_FAILURE;
-        goto mode_err;
-     }
-
-   if ((fstat(fd, &st) < 0) || 
-       (major(st.st_rdev) != TTY_MAJOR) || (minor(st.st_rdev) == 0))
-     {
-        fprintf(stderr, "\t%s not a tty\n", file);
-        goto mode_err;
-     }
-
-   /* try to get the current mode of this vt */
-   if (ioctl(fd, KDGETMODE, &mode))
-     {
-        fprintf(stderr, "\tCould not get vt mode: %m\n");
-        goto mode_err;
-     }
-
-   /* if we are in text mode, switch to graphics mode */
-   if (mode == KD_TEXT)
-     {
-        /* try to set graphics mode */
-        if (ioctl(fd, KDSETMODE, KD_GRAPHICS) < 0)
-          {
-             fprintf(stderr, "\tCould not set graphics mode: %m\n");
-             goto mode_err;
-          }
-     }
-
-   /* setup vt mode signals */
-   vtmode.mode = VT_PROCESS;
-   vtmode.waitv = 0;
-   vtmode.relsig = SIGUSR1;
-   vtmode.acqsig = SIGUSR2;
-
-   /* try to set mode */
-   if (ioctl(fd, VT_SETMODE, &vtmode) < 0)
-     {
-        fprintf(stderr, "\tCould not set mode signals: %m\n");
-        /* reset to text mode */
-        if (mode == KD_TEXT) ioctl(fd, KDSETMODE, KD_TEXT);
-        goto mode_err;
-     }
-
-   fprintf(stderr, "\tOpened Tty: %d\n", fd);
-
-   _send_msg(ECORE_DRM_OP_TTY_OPEN, fd, &ret, sizeof(int));
-
-   return ret;
-
-mode_err:
-   if (fd > 0) close(fd);
-   fd = -1;
-   ret = ECORE_DRM_OP_FAILURE;
-   _send_msg(ECORE_DRM_OP_TTY_OPEN, fd, &ret, sizeof(int));
-
-   return ret;
-}
-
-static int 
-_close_fd(int opcode, int fd)
-{
-   int ret = ECORE_DRM_OP_SUCCESS;
-
-   fprintf(stderr, "Close FD: %d\n", fd);
-
-   if (fd >= 0) close(fd);
-   fd = -1;
-
-   _send_msg(opcode, fd, &ret, sizeof(int));
-
-   return ret;
-}
 
 static int 
 _read_fd_get(void)
@@ -192,16 +46,14 @@ _read_fd_get(void)
    char *ev, *end;
    int fd = -1, flags = -1;
 
-   if (!(ev = getenv("ECORE_DRM_LAUNCHER_READ_SOCKET")))
+   if (!(ev = getenv("ECORE_DRM_LAUNCHER_SOCKET_READ")))
      return -1;
 
    fd = strtol(ev, &end, 0);
    if (*end != '\0') return -1;
 
    flags = fcntl(fd, F_GETFD);
-   if (flags == -1) return -1;
-
-   /* fcntl(fd, F_SETFD, flags | FD_CLOEXEC); */
+   if (flags < 0) return -1;
 
    return fd;
 }
@@ -212,150 +64,204 @@ _write_fd_get(void)
    char *ev, *end;
    int fd = -1, flags = -1;
 
-   if (!(ev = getenv("ECORE_DRM_LAUNCHER_WRITE_SOCKET")))
+   if (!(ev = getenv("ECORE_DRM_LAUNCHER_SOCKET_WRITE")))
      return -1;
 
    fd = strtol(ev, &end, 0);
    if (*end != '\0') return -1;
 
    flags = fcntl(fd, F_GETFD);
-   if (flags == -1) return -1;
-
-   /* fcntl(fd, F_SETFD, flags | FD_CLOEXEC); */
+   if (flags < 0) return -1;
 
    return fd;
 }
 
 static int 
-_read_msg(void)
+_send_msg(int opcode, int fd, void *data, size_t bytes)
 {
-   int ret = -1;
    Ecore_Drm_Message dmsg;
-   char data[BUFSIZ];
+   struct CRED_STRUCT *creds;
+   struct cmsghdr *cmsg;
    struct iovec iov[2];
-   char ctrl[CMSG_SPACE(sizeof(int))];
+   struct msghdr msg;
+   ssize_t size;
+
+   /* send a message to the calling process */
+   /* 'fd' is the fd to send */
+
+   IOVSET(iov + 0, &dmsg, sizeof(dmsg));
+   IOVSET(iov + 1, data, bytes);
+
+   dmsg.opcode = opcode;
+   dmsg.size = bytes;
+
+   msg.msg_iov = iov;
+   msg.msg_iovlen = 2;
+   msg.msg_name = NULL;
+   msg.msg_namelen = 0;
+   msg.msg_flags = 0;
+
+   if ((!cmsgptr) && (!(cmsgptr = malloc(CONTROL_LEN))))
+     return -1;
+
+   msg.msg_control = cmsgptr;
+   msg.msg_controllen = CONTROL_LEN;
+
+   cmsg = cmsgptr;
+
+   cmsg->cmsg_level = SOL_SOCKET;
+   cmsg->cmsg_type = SCM_RIGHTS;
+   cmsg->cmsg_len = RIGHTS_LEN;
+   *((int *)CMSG_DATA(cmsg)) = fd;
+
+   cmsg = CMSG_NXTHDR(&msg, cmsg);
+   cmsg->cmsg_level = SOL_SOCKET;
+   cmsg->cmsg_type = SCM_CREDTYPE;
+   cmsg->cmsg_len = CREDS_LEN;
+   creds = (struct CRED_STRUCT *)CMSG_DATA(cmsg);
+
+#if defined(SCM_CREDENTIALS)
+   creds->uid = geteuid();
+   creds->gid = getegid();
+   creds->pid = getpid();
+#endif
+
+   errno = 0;
+   size = sendmsg(_write_fd, &msg, MSG_EOR);
+   if (errno != 0)
+     {
+        fprintf(stderr, "Failed to send message: %s", strerror(errno));
+        return -1;
+     }
+
+   return size;
+}
+
+static int 
+_recv_msg(void)
+{
+   /* int ret = -1; */
+   Ecore_Drm_Message dmsg;
+   struct iovec iov[2];
    struct msghdr msg;
    struct cmsghdr *cmsg;
+   char data[BUFSIZ];
    ssize_t size;
+
+/* #if defined(CRED_OPT) */
+/*    struct CRED_STRUCT *creds; */
+/*    if (getsockopt(_read_fd, SOL_SOCKET, CRED_OPT,  */
+/*                   &creds, sizeof(struct CRED_STRUCT)) < 0) */
+/*      return -1; */
+/* #endif */
+
+   fprintf(stderr, "Received Message\n");
 
    IOVSET(iov + 0, &dmsg, sizeof(dmsg));
    IOVSET(iov + 1, &data, sizeof(data));
 
-   memset(&msg, 0, sizeof(msg));
-
-   msg.msg_name = NULL;
-   msg.msg_namelen = 0;
    msg.msg_iov = iov;
    msg.msg_iovlen = 2;
-   msg.msg_controllen = CMSG_SPACE(sizeof(int));
-   msg.msg_control = ctrl;
+   msg.msg_name = NULL;
+   msg.msg_namelen = 0;
+
+   if ((!cmsgptr) && (!(cmsgptr = malloc(CONTROL_LEN))))
+     return -1;
+
+   msg.msg_control = cmsgptr;
+   msg.msg_controllen = CONTROL_LEN;
 
    errno = 0;
-   size = recvmsg(_drm_read_fd, &msg, 0);//MSG_CMSG_CLOEXEC);
-
+   size = recvmsg(_read_fd, &msg, MSG_ERRQUEUE);
    if (errno != 0)
      {
-        fprintf(stderr, "Recvd %li\n", size);
-        fprintf(stderr, "Recv Err: %d\n", errno);
-        fprintf(stderr, "Recv Err: %s\n", strerror(errno));
+        fprintf(stderr, "Failed to receive message: %m\n");
+        return -1;
      }
 
-   cmsg = CMSG_FIRSTHDR(&msg);
-   if ((cmsg) && (cmsg->cmsg_len == CMSG_LEN(sizeof(int))))
+   fprintf(stderr, "\tReceived %d bytes\n", (int)size);
+
+   for (cmsg = CMSG_FIRSTHDR(&msg); cmsg != NULL; 
+        cmsg = CMSG_NXTHDR(&msg, cmsg))
      {
         if (cmsg->cmsg_level != SOL_SOCKET)
+          continue;
+
+        switch (cmsg->cmsg_type)
           {
-             fprintf(stderr, "\tInvalid cmsg level\n");
+           case SCM_RIGHTS:
+             switch (dmsg.opcode)
+               {
+                case ECORE_DRM_OP_DEVICE_OPEN:
+                  fprintf(stderr, "Open Device: %s\n", (char *)data);
+                  break;
+                default:
+                  fprintf(stderr, "Unhandled Opcode: %d\n", dmsg.opcode);
+                  break;
+               }
+             break;
+           /* case SCM_CREDTYPE: */
+           /*   creds = (struct CRED_STRUCT *)CMSG_DATA(cmsg); */
+           /*   break; */
+           default:
+             fprintf(stderr, "Unhandled message type: %d\n", cmsg->cmsg_type);
              return -1;
-          }
-        if (cmsg->cmsg_type != SCM_RIGHTS)
-          {
-             fprintf(stderr, "\tInvalid cmsg type\n");
-             return -1;
+             break;
           }
      }
 
-   switch (dmsg.opcode)
-     {
-      case ECORE_DRM_OP_READ_FD_SET:
-        fprintf(stderr, "Ecore_Drm_Operation_Read_FD_Set\n");
-        _drm_read_fd = *((int *)CMSG_DATA(cmsg));
-        if (_drm_read_fd >= 0) ret = 1;
-        break;
-      case ECORE_DRM_OP_WRITE_FD_SET:
-        fprintf(stderr, "Ecore_Drm_Operation_Write_FD_Set\n");
-        _drm_write_fd = *((int *)CMSG_DATA(cmsg));
-        if (_drm_write_fd >= 0) ret = 1;
-        break;
-      case ECORE_DRM_OP_DEVICE_OPEN:
-        fprintf(stderr, "Ecore_Drm_Operation_Device_Open: %s\n", (char *)data);
-        ret = _open_device((char *)data);
-        break;
-      case ECORE_DRM_OP_DEVICE_CLOSE:
-        fprintf(stderr, "Ecore_Drm_Operation_Device_Close\n");
-        ret = _close_fd(ECORE_DRM_OP_DEVICE_CLOSE, *((int *)data));
-        break;
-      case ECORE_DRM_OP_TTY_OPEN:
-        fprintf(stderr, "Ecore_Drm_Operation_Tty_Open: %s\n", (char *)data);
-        ret = _open_tty((char *)data);
-        break;
-      case ECORE_DRM_OP_TTY_CLOSE:
-        fprintf(stderr, "Ecore_Drm_Operation_Tty_Close\n");
-        ret = _close_fd(ECORE_DRM_OP_TTY_CLOSE, *((int *)data));
-        break;
-      default:
-        fprintf(stderr, "Uknown operation: %d\n", dmsg.opcode);
-        break;
-     }
-
-   return ret;
+   return size;
 }
 
 int 
 main(int argc EINA_UNUSED, char **argv EINA_UNUSED)
 {
-   fprintf(stderr, "Ecore_Drm_Launch Started\n");
+   struct epoll_event ev, events[1];
+   int ret, i;
 
-   if ((_drm_read_fd = _read_fd_get()) < 0)
+   fprintf(stderr, "Spartacus Is Alive\n");
+
+   _read_fd = _read_fd_get();
+   if (_read_fd < 0) return EXIT_FAILURE;
+
+   _write_fd = _write_fd_get();
+   if (_write_fd < 0) return EXIT_FAILURE;
+
+   _epoll_fd = epoll_create(1);
+
+   memset(&ev, 0, sizeof(ev));
+   ev.events = EPOLLIN;
+   ev.data.fd = _read_fd;
+
+   if (epoll_ctl(_epoll_fd, EPOLL_CTL_ADD, _read_fd, &ev) < 0)
      {
-        fprintf(stderr, "\tCould not Get FD\n");
         return EXIT_FAILURE;
      }
 
-   fprintf(stderr, "\tGot Fd: %d\n", _drm_read_fd);
+   memset(&events, 0, sizeof(events));
 
-   /* if ((_drm_write_fd = _write_fd_get()) < 0) */
-   /*   { */
-   /*      fprintf(stderr, "\tCould not Get FD\n"); */
-   /*      return EXIT_FAILURE; */
-   /*   } */
-
-   /* fprintf(stderr, "\tGot Fd: %d\n", _drm_write_fd); */
-
-   while (1)
+   for (;;)
      {
-        struct pollfd pfd[1];
-        int n = 0;
-
-        fprintf(stderr, "Checking for Messages on FD %d...\n", _drm_read_fd);
-
-        pfd[0].fd = _drm_read_fd;
-        pfd[0].events = (POLLIN | POLLPRI | POLLNVAL);
-
-        if ((n = poll(pfd, 1, -1)) < 0)
+        ret = epoll_wait(_epoll_fd, events, sizeof(events) / sizeof(struct epoll_event), 0);
+        if (ret < 0)
           {
-             fprintf(stderr, "\tPoll Failed\n");
-             break;
+             fprintf(stderr, "Epoll Failed: %m\n");
+             return EXIT_FAILURE;
           }
 
-        if (pfd[0].revents & POLLIN)
+        for (i = 0; i < ret; i++)
           {
-             _read_msg();
-          }
-        else if (pfd[0].revents & POLLNVAL)
-          {
-             fprintf(stderr, "\tInvalid FD to Poll\n");
-             break;
+             if (events[i].data.fd != _read_fd) continue;
+
+             if (events[i].events & EPOLLIN)
+               {
+                  fprintf(stderr, "Epoll Data In\n");
+                  _recv_msg();
+               }
+             else if (events[i].events & EPOLLERR)
+               {
+                  fprintf(stderr, "Epoll Data Error\n");
+               }
           }
      }
 
