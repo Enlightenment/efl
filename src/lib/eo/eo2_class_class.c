705e459811f94f69ce9bba6eb8dc909e3236f1b6
Jérémy Zurcher <jeremy@asynk.ch>
2013-12-27 00:33:49 +0100

    eo2: add EO2_CLASS_CLASS

---
+++ src/lib/eo/eo2_class_class.c
@@ -0,0 +1,20 @@
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include "Eo.h"
+
+static Eo2_Op_Description op_descs [] = {};
+
+static const Eo_Class_Description class_desc = {
+     EO2_VERSION,
+     "Eo Abstract Class",
+     EO_CLASS_TYPE_REGULAR_NO_INSTANT,
+     EO2_CLASS_DESCRIPTION_OPS(op_descs),
+     NULL,
+     0,
+     NULL,
+     NULL
+};
+
+EO_DEFINE_CLASS(eo2_class_class_get, &class_desc, NULL, NULL)
