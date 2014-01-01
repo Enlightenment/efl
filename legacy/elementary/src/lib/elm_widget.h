722be35238df04f7341b37513b6f3a65e86db759
Daniel Juyung Seo <seojuyung2@gmail.com>
2014-01-02 08:49:26 +0900

    elm_widget: Partial update for widget smart data comments.

--- legacy/elementary/src/lib/elm_widget.h
+++ legacy/elementary/src/lib/elm_widget.h
@@ -434,8 +434,8 @@ typedef struct _Elm_Widget_Smart_Data
    Eina_Bool                     can_access : 1;
    Eina_Bool                     highlighted : 1;
    Eina_Bool                     highlight_root : 1;
-   Eina_Bool                     on_translate : 1;
-   Eina_Bool                     on_create : 1;
+   Eina_Bool                     on_translate : 1; /*<< This is true when any types of elm translate function is being called. */
+   Eina_Bool                     on_create : 1; /*<< This is true when the widget is on creation(general widget constructor). */
 } Elm_Widget_Smart_Data;
 
 /**
