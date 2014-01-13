Enlightenment/efl

commit fb59ac34a1ec65680ca4ff3fc62d1fc42aa72f5a

ecore_evas - use eina_bool instead of the char for interal data.

src/modules/ecore_evas/engines/win32/ecore_evas_win32.c
@@ -861,7 +861,7 @@ _ecore_evas_win32_iconified_set(Ecore_Evas *ee, Eina_Bool on)
 }
 
 static void
-_ecore_evas_win32_borderless_set(Ecore_Evas *ee, int on)
+_ecore_evas_win32_borderless_set(Ecore_Evas *ee, Eina_Bool on)
 {
    if (((ee->prop.borderless) && (on)) ||
        ((!ee->prop.borderless) && (!on))) return;
@@ -890,7 +890,7 @@ _ecore_evas_win32_borderless_set(Ecore_Evas *ee, int on)
 }
 
 static void
-_ecore_evas_win32_override_set(Ecore_Evas *ee, int on)
+_ecore_evas_win32_override_set(Ecore_Evas *ee, Eina_Bool on)
 {
    struct _Ecore_Win32_Window *window;
 
@@ -908,7 +908,7 @@ _ecore_evas_win32_override_set(Ecore_Evas *ee, int on)
 }
 
 static void
-_ecore_evas_win32_fullscreen_set(Ecore_Evas *ee, int on)
+_ecore_evas_win32_fullscreen_set(Ecore_Evas *ee, Eina_Boo on)
 {
    struct _Ecore_Win32_Window *window;
    Ecore_Evas_Engine_Data_Win32 *wdata = ee->engine.data;
@@ -1272,8 +1272,8 @@ _ecore_evas_win32_new_internal(int (*_ecore_evas_engine_backend_init)(Ecore_Evas
    ee->prop.max.w = 32767;
    ee->prop.max.h = 32767;
    ee->prop.layer = 4;
-   ee->prop.request_pos = 0;
-   ee->prop.sticky = 0;
+   ee->prop.request_pos = EINA_FALSE;
+   ee->prop.sticky = EINA_FALSE;
    /* FIXME: sticky to add */
    ee->prop.window = 0;
 

src/lib/ecore_evas/ecore_evas.c
@@ -2023,7 +2023,7 @@ ecore_evas_withdrawn_get(const Ecore_Evas *ee)
 }
 
 EAPI void
-ecore_evas_sticky_set(Ecore_Evas *ee, Eina_Bool sticky)
+ecore_evas_sticky_set(Ecore_Evas *ee, Eina_Bool on)
 {
    if (!ECORE_MAGIC_CHECK(ee, ECORE_MAGIC_EVAS))
      {
@@ -2032,7 +2032,7 @@ ecore_evas_sticky_set(Ecore_Evas *ee, Eina_Bool sticky)
         return;
      }
 
-   IFC(ee, fn_sticky_set) (ee, sticky);
+   IFC(ee, fn_sticky_set) (ee, on);
    IFE;
 }
 
@@ -2101,7 +2101,7 @@ ecore_evas_aspect_get(const Ecore_Evas *ee)
 }
 
 EAPI void
-ecore_evas_urgent_set(Ecore_Evas *ee, Eina_Bool urgent)
+ecore_evas_urgent_set(Ecore_Evas *ee, Eina_Bool on)
 {
    if (!ECORE_MAGIC_CHECK(ee, ECORE_MAGIC_EVAS))
      {
@@ -2110,7 +2110,7 @@ ecore_evas_urgent_set(Ecore_Evas *ee, Eina_Bool urgent)
         return;
      }
 
-   IFC(ee, fn_urgent_set) (ee, urgent);
+   IFC(ee, fn_urgent_set) (ee, on);
    IFE;
 }
 
@@ -2127,7 +2127,7 @@ ecore_evas_urgent_get(const Ecore_Evas *ee)
 }
 
 EAPI void
-ecore_evas_modal_set(Ecore_Evas *ee, Eina_Bool modal)
+ecore_evas_modal_set(Ecore_Evas *ee, Eina_Bool on)
 {
    if (!ECORE_MAGIC_CHECK(ee, ECORE_MAGIC_EVAS))
      {
@@ -2136,7 +2136,7 @@ ecore_evas_modal_set(Ecore_Evas *ee, Eina_Bool modal)
         return;
      }
 
-   IFC(ee, fn_modal_set) (ee, modal);
+   IFC(ee, fn_modal_set) (ee, on);
    IFE;
 }
 
@@ -2153,7 +2153,7 @@ ecore_evas_modal_get(const Ecore_Evas *ee)
 }
 
 EAPI void
-ecore_evas_demand_attention_set(Ecore_Evas *ee, Eina_Bool demand)
+ecore_evas_demand_attention_set(Ecore_Evas *ee, Eina_Bool on)
 {
    if (!ECORE_MAGIC_CHECK(ee, ECORE_MAGIC_EVAS))
      {
@@ -2162,7 +2162,7 @@ ecore_evas_demand_attention_set(Ecore_Evas *ee, Eina_Bool demand)
         return;
      }
 
-   IFC(ee, fn_demands_attention_set) (ee, demand);
+   IFC(ee, fn_demands_attention_set) (ee, on);
    IFE;
 }
 
@@ -2179,7 +2179,7 @@ ecore_evas_demand_attention_get(const Ecore_Evas *ee)
 }
 
 EAPI void
-ecore_evas_focus_skip_set(Ecore_Evas *ee, Eina_Bool skip)
+ecore_evas_focus_skip_set(Ecore_Evas *ee, Eina_Bool on)
 {
    if (!ECORE_MAGIC_CHECK(ee, ECORE_MAGIC_EVAS))
      {
@@ -2188,7 +2188,7 @@ ecore_evas_focus_skip_set(Ecore_Evas *ee, Eina_Bool skip)
         return;
      }
 
-   IFC(ee, fn_focus_skip_set) (ee, skip);
+   IFC(ee, fn_focus_skip_set) (ee, on);
    IFE;
 }
 

src/lib/ecore_evas/ecore_evas_buffer.c
@@ -647,12 +647,12 @@ ecore_evas_buffer_allocfunc_new(int w, int h,
    ee->prop.max.h = 0;
    ee->prop.layer = 0;
    ee->prop.focused = EINA_TRUE;
-   ee->prop.borderless = 1;
-   ee->prop.override = 1;
-   ee->prop.maximized = 1;
-   ee->prop.fullscreen = 0;
-   ee->prop.withdrawn = 0;
-   ee->prop.sticky = 0;
+   ee->prop.borderless = EINA_TRUE;
+   ee->prop.override = EINA_TRUE;
+   ee->prop.maximized = EINA_TRUE;
+   ee->prop.fullscreen = EINA_FALSE;
+   ee->prop.withdrawn = EINA_FALSE;
+   ee->prop.sticky = EINA_FALSE;
 
    /* init evas here */
    ee->evas = evas_new();
@@ -775,12 +775,12 @@ ecore_evas_object_image_new(Ecore_Evas *ee_target)
    ee->prop.max.h = 0;
    ee->prop.layer = 0;
    ee->prop.focused = EINA_FALSE;
-   ee->prop.borderless = 1;
-   ee->prop.override = 1;
-   ee->prop.maximized = 0;
-   ee->prop.fullscreen = 0;
-   ee->prop.withdrawn = 0;
-   ee->prop.sticky = 0;
+   ee->prop.borderless = EINA_TRUE;
+   ee->prop.override = EINA_TRUE;
+   ee->prop.maximized = EINA_FALSE;
+   ee->prop.fullscreen = EINA_FALSE;
+   ee->prop.withdrawn = EINA_FALSE;
+   ee->prop.sticky = EINA_FALSE;
 
    /* init evas here */
    ee->evas = evas_new();

src/lib/ecore_evas/ecore_evas_ews.c
@@ -483,39 +483,39 @@ _ecore_evas_ews_iconified_set(Ecore_Evas *ee, Eina_Bool on)
 }
 
 static void
-_ecore_evas_ews_borderless_set(Ecore_Evas *ee, int val)
+_ecore_evas_ews_borderless_set(Ecore_Evas *ee, Eina_Bool on)
 {
-   if (ee->prop.borderless == val) return;
-   ee->prop.borderless = val;
+   if (ee->prop.borderless == on) return;
+   ee->prop.borderless = on;
    _ecore_evas_ews_event(ee, ECORE_EVAS_EWS_EVENT_CONFIG_CHANGE);
 }
 
 static void
-_ecore_evas_ews_override_set(Ecore_Evas *ee, int val)
+_ecore_evas_ews_override_set(Ecore_Evas *ee, Eina_Bool on)
 {
-   if (ee->prop.override == val) return;
+   if (ee->prop.override == on) return;
    if (ee->visible) evas_object_show(ee->engine.ews.image);
    if (ee->prop.focused) evas_object_focus_set(ee->engine.ews.image, EINA_TRUE);
-   ee->prop.override = val;
+   ee->prop.override = on;
    _ecore_evas_ews_event(ee, ECORE_EVAS_EWS_EVENT_CONFIG_CHANGE);
 }
 
 static void
-_ecore_evas_ews_maximized_set(Ecore_Evas *ee, int val)
+_ecore_evas_ews_maximized_set(Ecore_Evas *ee, Eina_Bool on)
 {
-   if (ee->prop.maximized == val) return;
-   ee->prop.maximized = val;
-   if (val) evas_object_show(ee->engine.ews.image);
+   if (ee->prop.maximized == on) return;
+   ee->prop.maximized = on;
+   if (on) evas_object_show(ee->engine.ews.image);
    _ecore_evas_ews_event(ee, ECORE_EVAS_EWS_EVENT_MAXIMIZED_CHANGE);
 }
 
 static void
-_ecore_evas_ews_fullscreen_set(Ecore_Evas *ee, int val)
+_ecore_evas_ews_fullscreen_set(Ecore_Evas *ee, Eina_Bool on)
 {
-   if (ee->prop.fullscreen == val) return;
-   ee->prop.fullscreen = val;
+   if (ee->prop.fullscreen == on) return;
+   ee->prop.fullscreen = on;
 
-   if (!val)
+   if (!on)
      {
         evas_object_move(ee->engine.ews.image, ee->x, ee->y);
         evas_object_resize(ee->engine.ews.image, ee->w, ee->h);
@@ -546,20 +546,20 @@ _ecore_evas_ews_avoid_damage_set(Ecore_Evas *ee, int val)
 }
 
 static void
-_ecore_evas_ews_withdrawn_set(Ecore_Evas *ee, int val)
+_ecore_evas_ews_withdrawn_set(Ecore_Evas *ee, Eina_Bool on)
 {
-   if (ee->prop.withdrawn == val) return;
-   ee->prop.withdrawn = val;
+   if (ee->prop.withdrawn == on) return;
+   ee->prop.withdrawn = on;
    _ecore_evas_ews_event(ee, ECORE_EVAS_EWS_EVENT_CONFIG_CHANGE);
 }
 
 static void
-_ecore_evas_ews_sticky_set(Ecore_Evas *ee, int val)
+_ecore_evas_ews_sticky_set(Ecore_Evas *ee, Eina_Bool on)
 {
-   if (ee->prop.sticky == val) return;
-   ee->prop.sticky = val;
-   if ((val) && (ee->func.fn_sticky)) ee->func.fn_sticky(ee);
-   else if ((!val) && (ee->func.fn_unsticky)) ee->func.fn_unsticky(ee);
+   if (ee->prop.sticky == on) return;
+   ee->prop.sticky = on;
+   if ((on) && (ee->func.fn_sticky)) ee->func.fn_sticky(ee);
+   else if ((!on) && (ee->func.fn_unsticky)) ee->func.fn_unsticky(ee);
    _ecore_evas_ews_event(ee, ECORE_EVAS_EWS_EVENT_CONFIG_CHANGE);
 }
 

src/lib/ecore_evas/ecore_evas_private.h
@@ -83,13 +83,13 @@ struct _Ecore_Evas_Engine_Func
    void (*fn_layer_set) (Ecore_Evas *ee, int layer);
    void (*fn_focus_set) (Ecore_Evas *ee, Eina_Bool on);
    void (*fn_iconified_set) (Ecore_Evas *ee, Eina_Bool on);
-   void (*fn_borderless_set) (Ecore_Evas *ee, int on);
-   void (*fn_override_set) (Ecore_Evas *ee, int on);
-   void (*fn_maximized_set) (Ecore_Evas *ee, int on);
-   void (*fn_fullscreen_set) (Ecore_Evas *ee, int on);
+   void (*fn_borderless_set) (Ecore_Evas *ee, Eina_Bool on);
+   void (*fn_override_set) (Ecore_Evas *ee, Eina_Bool on);
+   void (*fn_maximized_set) (Ecore_Evas *ee, Eina_Bool on);
+   void (*fn_fullscreen_set) (Ecore_Evas *ee, Eina_Bool on);
    void (*fn_avoid_damage_set) (Ecore_Evas *ee, int on);
-   void (*fn_withdrawn_set) (Ecore_Evas *ee, int withdrawn);
-   void (*fn_sticky_set) (Ecore_Evas *ee, int sticky);
+   void (*fn_withdrawn_set) (Ecore_Evas *ee, Eina_Bool on);
+   void (*fn_sticky_set) (Ecore_Evas *ee, Eina_Bool on);
    void (*fn_ignore_events_set) (Ecore_Evas *ee, int ignore);
    void (*fn_alpha_set) (Ecore_Evas *ee, int alpha);
    void (*fn_transparent_set) (Ecore_Evas *ee, int transparent);
@@ -98,10 +98,10 @@ struct _Ecore_Evas_Engine_Func
 
    void (*fn_window_group_set) (Ecore_Evas *ee, const Ecore_Evas *ee_group);
    void (*fn_aspect_set) (Ecore_Evas *ee, double aspect);
-   void (*fn_urgent_set) (Ecore_Evas *ee, int urgent);
-   void (*fn_modal_set) (Ecore_Evas *ee, int modal);
-   void (*fn_demands_attention_set) (Ecore_Evas *ee, int demand);
-   void (*fn_focus_skip_set) (Ecore_Evas *ee, int skip);
+   void (*fn_urgent_set) (Ecore_Evas *ee, Eina_Bool on);
+   void (*fn_modal_set) (Ecore_Evas *ee, Eina_Bool on);
+   void (*fn_demands_attention_set) (Ecore_Evas *ee, Eina_Bool on);
+   void (*fn_focus_skip_set) (Ecore_Evas *ee, Eina_Bool on);
 
    int (*fn_render) (Ecore_Evas *ee);
    void (*fn_screen_geometry_get) (const Ecore_Evas *ee, int *x, int *y, int *w, int *h);
@@ -191,20 +191,19 @@ struct _Ecore_Evas
       double          aspect;
       Eina_Bool       focused      : 1;
       Eina_Bool       iconified    : 1;
-//FIXME: keep going change to Eina_Bool
-      char            borderless   : 1;
-      char            override     : 1;
-      char            maximized    : 1;
-      char            fullscreen   : 1;
-      char            withdrawn    : 1;
-      char            sticky       : 1;
-      char            request_pos  : 1;
-      char            draw_frame   : 1;
-      char            hwsurface    : 1;
-      char            urgent           : 1;
-      char            modal            : 1;
-      char            demand_attention : 1;
-      char            focus_skip       : 1;
+      Eina_Bool       borderless   : 1;
+      Eina_Bool       override     : 1;
+      Eina_Bool       maximized    : 1;
+      Eina_Bool       fullscreen   : 1;
+      Eina_Bool       withdrawn    : 1;
+      Eina_Bool       sticky       : 1;
+      Eina_Bool       request_pos  : 1;
+      Eina_Bool       draw_frame   : 1;
+      Eina_Bool       hwsurface    : 1;
+      Eina_Bool       urgent           : 1;
+      Eina_Bool       modal            : 1;
+      Eina_Bool       demand_attention : 1;
+      Eina_Bool       focus_skip       : 1;
   } prop;
 
    struct {

src/modules/ecore_evas/engines/cocoa/ecore_evas_cocoa.c
@@ -516,8 +516,8 @@ ecore_evas_cocoa_new_internal(Ecore_Cocoa_Window *parent, int x, int y, int w, i
   ee->prop.max.w = 32767;
   ee->prop.max.h = 32767;
   ee->prop.layer = 4;
-  ee->prop.request_pos = 0;
-  ee->prop.sticky = 0;
+  ee->prop.request_pos = EINA_FALSE;
+  ee->prop.sticky = EINA_FALSE;
   ee->prop.window = 0;
 
   printf("Create New Evas\n");

src/modules/ecore_evas/engines/drm/ecore_evas_drm.c
@@ -72,13 +72,13 @@ static Ecore_Evas_Engine_Func _ecore_evas_drm_engine_func =
    NULL, //void (*fn_layer_set) (Ecore_Evas *ee, int layer);
    NULL, //void (*fn_focus_set) (Ecore_Evas *ee, Eina_Bool on);
    NULL, //void (*fn_iconified_set) (Ecore_Evas *ee, Eina_Bool on);
-   NULL, //void (*fn_borderless_set) (Ecore_Evas *ee, int on);
-   NULL, //void (*fn_override_set) (Ecore_Evas *ee, int on);
-   NULL, //void (*fn_maximized_set) (Ecore_Evas *ee, int on);
-   NULL, //void (*fn_fullscreen_set) (Ecore_Evas *ee, int on);
+   NULL, //void (*fn_borderless_set) (Ecore_Evas *ee, Eina_Bool on);
+   NULL, //void (*fn_override_set) (Ecore_Evas *ee, Eina_Bool on);
+   NULL, //void (*fn_maximized_set) (Ecore_Evas *ee, Eina_Bool on);
+   NULL, //void (*fn_fullscreen_set) (Ecore_Evas *ee, Eina_Bool on);
    NULL, //void (*fn_avoid_damage_set) (Ecore_Evas *ee, int on);
-   NULL, //void (*fn_withdrawn_set) (Ecore_Evas *ee, int withdrawn);
-   NULL, //void (*fn_sticky_set) (Ecore_Evas *ee, int sticky);
+   NULL, //void (*fn_withdrawn_set) (Ecore_Evas *ee, Eina_Bool on);
+   NULL, //void (*fn_sticky_set) (Ecore_Evas *ee, Eina_Bool on);
    NULL, //void (*fn_ignore_events_set) (Ecore_Evas *ee, int ignore);
    NULL, //void (*fn_alpha_set) (Ecore_Evas *ee, int alpha);
    NULL, //void (*fn_transparent_set) (Ecore_Evas *ee, int transparent);
@@ -87,10 +87,10 @@ static Ecore_Evas_Engine_Func _ecore_evas_drm_engine_func =
    
    NULL, //void (*fn_window_group_set) (Ecore_Evas *ee, const Ecore_Evas *ee_group);
    NULL, //void (*fn_aspect_set) (Ecore_Evas *ee, double aspect);
-   NULL, //void (*fn_urgent_set) (Ecore_Evas *ee, int urgent);
-   NULL, //void (*fn_modal_set) (Ecore_Evas *ee, int modal);
-   NULL, //void (*fn_demands_attention_set) (Ecore_Evas *ee, int demand);
-   NULL, //void (*fn_focus_skip_set) (Ecore_Evas *ee, int skip);
+   NULL, //void (*fn_urgent_set) (Ecore_Evas *ee, Eina_Bool on);
+   NULL, //void (*fn_modal_set) (Ecore_Evas *ee, Eina_Bool on);
+   NULL, //void (*fn_demands_attention_set) (Ecore_Evas *ee, Eina_Bool on);
+   NULL, //void (*fn_focus_skip_set) (Ecore_Evas *ee, Eina_Bool on);
    
    NULL, //int (*fn_render) (Ecore_Evas *ee);
    NULL, //void (*fn_screen_geometry_get) (const Ecore_Evas *ee, int *x, int *y, int *w, int *h);

src/modules/ecore_evas/engines/extn/ecore_evas_extn.c
@@ -1158,12 +1158,12 @@ ecore_evas_extn_plug_new_internal(Ecore_Evas *ee_target)
    ee->prop.max.h = 0;
    ee->prop.layer = 0;
    ee->prop.focused = EINA_FALSE;
-   ee->prop.borderless = 1;
-   ee->prop.override = 1;
-   ee->prop.maximized = 0;
-   ee->prop.fullscreen = 0;
-   ee->prop.withdrawn = 0;
-   ee->prop.sticky = 0;
+   ee->prop.borderless = EINA_TRUE;
+   ee->prop.override = EINA_TRUE;
+   ee->prop.maximized = EINA_FALSE;
+   ee->prop.fullscreen = EINA_FALSE;
+   ee->prop.withdrawn = EINA_FALSE;
+   ee->prop.sticky = EINA_FALSE;
 
    bdata->image = o;
    evas_object_data_set(bdata->image, "Ecore_Evas", ee);
@@ -2042,12 +2042,12 @@ ecore_evas_extn_socket_new_internal(int w, int h)
    ee->prop.max.h = 0;
    ee->prop.layer = 0;
    ee->prop.focused = EINA_FALSE;
-   ee->prop.borderless = 1;
-   ee->prop.override = 1;
-   ee->prop.maximized = 0;
-   ee->prop.fullscreen = 0;
-   ee->prop.withdrawn = 0;
-   ee->prop.sticky = 0;
+   ee->prop.borderless = EINA_TRUE;
+   ee->prop.override = EINA_TRUE;
+   ee->prop.maximized = EINA_FALSE;
+   ee->prop.fullscreen = EINA_FALSE;
+   ee->prop.withdrawn = EINA_FALSE;
+   ee->prop.sticky = EINA_FALSE;
 
    /* init evas here */
    ee->evas = evas_new();

commit 384783a896eb315cfa99125ca497f971312c38f0

ecore_evas_win32: Fix build regression in ecore evas win32 engine
Regression introduced with fb59ac34a1ec65680ca4ff3fc62d1fc42aa72f5a
Simple typo during Eina_Bool convertion.

src/modules/ecore_evas/engines/win32/ecore_evas_win32.c
@@ -908,7 +908,7 @@ _ecore_evas_win32_override_set(Ecore_Evas *ee, Eina_Bool on)
 }
 
 static void
-_ecore_evas_win32_fullscreen_set(Ecore_Evas *ee, Eina_Boo on)
+_ecore_evas_win32_fullscreen_set(Ecore_Evas *ee, Eina_Bool on)
 {
    struct _Ecore_Win32_Window *window;
    Ecore_Evas_Engine_Data_Win32 *wdata = ee->engine.data;