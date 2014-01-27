325bf9ab78a9d8d35d5c048656888626d35843a1
Jihoon Kim <jihoon48.kim@samsung.com>
2014-01-27 21:34:09 +0900

    entry: clear selection before selecting new one

--- src/lib/edje/edje_entry.c
+++ src/lib/edje/edje_entry.c
@@ -4212,12 +4212,11 @@ _edje_entry_imf_event_selection_set_cb(void *data, Ecore_IMF_Context *ctx EINA_U
        (rp->part->entry_mode < EDJE_ENTRY_EDIT_MODE_SELECTABLE))
      return;
 
+   _sel_clear(ed, en->cursor, rp->object, en);
    evas_textblock_cursor_pos_set(en->cursor, ev->start);
-
+   _sel_enable(ed, en->cursor, rp->object, en);
    _sel_start(en->cursor, rp->object, en);
-
    evas_textblock_cursor_pos_set(en->cursor, ev->end);
-
    _sel_extend(ed, en->cursor, rp->object, en);
 }
 
