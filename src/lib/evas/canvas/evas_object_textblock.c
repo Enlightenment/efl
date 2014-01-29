Enlightenment/efl

commit 548e548632333f5ef533079a7aaa54aadd20ef8d

Evas textblock: Fixed a selection issue.
The issue happens when selecting in strings that have both bidi and different
scripts in the same bidi run. E.g: "עבריתenglishрусскийעברית".

SVN revision: 83786

src/lib/evas/canvas/evas_object_textblock.c
@@ -9574,7 +9574,10 @@ _evas_textblock_cursor_range_in_line_geometry_get(
 
         while (it && (it != it2))
           {
-             max_x = it->x + it->adv;
+             if ((start <= it->text_pos) && (it->text_pos <= end))
+               {
+                  max_x = it->x + it->adv;
+               }
              it = (Evas_Object_Textblock_Item *) EINA_INLIST_GET(it)->next;
           }
         if (min_x != max_x)

src/tests/evas/evas_test_textblock.c
@@ -1381,6 +1381,11 @@ START_TEST(evas_textblock_geometries)
    fail_if((tr->x != tr2->x) || (tr->y != tr2->y) || (tr->w != tr2->w) ||
          (tr->h != tr2->h));
 
+   EINA_LIST_FREE(rects, tr)
+      free(tr);
+   EINA_LIST_FREE(rects2, tr2)
+      free(tr2);
+
    /* Multiline range */
    evas_textblock_cursor_pos_set(cur, 0);
    evas_textblock_cursor_pos_set(main_cur, 14);
@@ -1413,6 +1418,23 @@ START_TEST(evas_textblock_geometries)
    tr = eina_list_data_get(rects);
    tr2 = eina_list_data_get(eina_list_next(rects));
    fail_if(tr->y >= tr2->y);
+
+   EINA_LIST_FREE(rects, tr)
+      free(tr);
+   EINA_LIST_FREE(rects2, tr2)
+      free(tr2);
+
+   /* Same run different scripts */
+   evas_object_textblock_text_markup_set(tb, "עבריתenglishрусскийעברית");
+
+   evas_textblock_cursor_pos_set(cur, 3);
+   evas_textblock_cursor_pos_set(main_cur, 7);
+   rects = evas_textblock_cursor_range_geometry_get(cur, main_cur);
+
+   fail_if(eina_list_count(rects) != 2);
+
+   EINA_LIST_FREE(rects, tr)
+      free(tr);
 
    END_TB_TEST();
 }

commit 22813f5b5218a02273fefca3f61fde970ce47d75

Evas textblock: Fix selection with bidi text.
This is a regression introduced in
548e548632333f5ef533079a7aaa54aadd20ef8d.

This is really bad, and essentially broke selection geometry for bidi
text. Very serious.

The problematic code assumed that the range comparison for the items
assumed the item marked with 1 is always logically before the item marked
with 2, which is just not true.

src/lib/evas/canvas/evas_object_textblock.c
@@ -10136,7 +10136,8 @@ _evas_textblock_cursor_range_in_line_geometry_get(
 
         while (it && (it != it2))
           {
-             if ((it1->text_pos <= it->text_pos) && (it->text_pos <= it2->text_pos))
+             if (((it1->text_pos <= it->text_pos) && (it->text_pos <= it2->text_pos)) ||
+                   ((it2->text_pos <= it->text_pos) && (it->text_pos <= it1->text_pos)))
                {
                   max_x = it->x + it->adv;
                }