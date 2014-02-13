c7aa6039c804abd01d15f6691237a4e3b14b4bbd
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2014-02-13 19:05:43 +0900

    evas filter - fix uninitialized pointer in evas
    fixes CID 1174077

--- src/lib/evas/filters/evas_filter_parser.c
+++ src/lib/evas/filters/evas_filter_parser.c
@@ -2168,7 +2168,7 @@ _instr2cmd_curve(Evas_Filter_Context *ctx, Evas_Filter_Program *pgm,
    const char *src, *dst, *points_str, *interpolation, *channel_name;
    DATA8 values[256] = {0}, points[512];
    int cmdid, point_count = 0;
-   char *token, *copy = NULL, *saveptr;
+   char *token, *copy = NULL, *saveptr = NULL;
    Buffer *in, *out;
    Eina_Bool parse_ok = EINA_FALSE;
 
