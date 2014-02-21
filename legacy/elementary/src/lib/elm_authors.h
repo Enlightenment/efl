aba69c4c5f496ff53a8ed075126e7ab82d44991c
Daniel Juyung Seo <seojuyung2@gmail.com>
2014-02-22 06:49:50 +0900

    elm_authors.h: Sync with AUTHORS.

--- legacy/elementary/src/lib/elm_authors.h
+++ legacy/elementary/src/lib/elm_authors.h
@@ -1,6 +1,6 @@
 /**
  * @page authors Authors
- * @author Carsten Haitzler <raster@@rasterman.com>
+ * @author The Rasterman (Carsten Haitzler) <raster@@rasterman.com>
  * @author Gustavo Sverzut Barbieri <barbieri@@profusion.mobi>
  * @author Cedric Bail <cedric.bail@@free.fr>
  * @author Vincent Torri <vtorri@@univ-evry.fr>
@@ -18,7 +18,7 @@
  * @author Brett Nash <nash@@nash.id.au>
  * @author Bruno Dilly <bdilly@@profusion.mobi>
  * @author Rafael Fonseca <rfonseca@@profusion.mobi>
- * @author Chuneon Park <hermet@@hermet.pe.kr>
+ * @author Hermet (Chuneon Park) <hermet@@hermet.pe.kr>
  * @author Woohyun Jung <wh0705.jung@@samsung.com>
  * @author Jaehwan Kim <jae.hwan.kim@@samsung.com>
  * @author Wonguk Jeong <wonguk.jeong@@samsung.com>
@@ -38,18 +38,17 @@
  * @author Jeonghyun Yun (arosis) <jh0506.yun@@samsung.com>
  * @author Tom Hacohen <tom@@stosb.com>
  * @author Aharon Hillel <a.hillel@@samsung.com>
- * @author Jonathan Atton (Watchwolf) <jonathan.atton@@gmail.com>
  * @author Shinwoo Kim <kimcinoo@@gmail.com>
  * @author Govindaraju SM <govi.sm@@samsung.com> <govism@@gmail.com>
  * @author Prince Kumar Dubey <prince.dubey@@samsung.com> <prince.dubey@@gmail.com>
  * @author Sung W. Park <sungwoo@@gmail.com>
  * @author Thierry el Borgi <thierry@@substantiel.fr>
  * @author Shilpa Singh <shilpa.singh@@samsung.com> <shilpasingh.o@@gmail.com>
- * @author Chanwook Jung <joey.jung@@samsung.com>
+ * @author Chanwook Jung <joey.jung@@samsung.com> <jchanwook@gmail.com>
  * @author Hyoyoung Chang <hyoyoung.chang@@samsung.com>
  * @author Guillaume "Kuri" Friloux <guillaume.friloux@@asp64.com>
  * @author Kim Yunhan <spbear@@gmail.com>
- * @author Bluezery <ohpowel@@gmail.com>
+ * @author Tae-Hwan Kim (Bluezery) <the81.kim@samsung.com> <ohpowel@@gmail.com>
  * @author Nicolas Aguirre <aguirre.nicolas@@gmail.com>
  * @author Sanjeev BA <iamsanjeev@@gmail.com>
  * @author Hyunsil Park <hyunsil.park@@samsung.com>
@@ -74,7 +73,7 @@
  * @author Murilo Belluzzo <murilo.belluzzo@@profusion.mobi>
  * @author Ricardo de Almeida Gonzaga <ricardo@@profusion.mobi>
  * @author Gwanglim Lee <gl77.lee@@samsung.com> <gwanglim@@gmail.com>
- * @author JaeHyun Jo <jae_hyun_cho@@naver.com>
+ * @author JaeHyun Cho <jae_hyun_cho@@naver.com>
  * @author Bora Hwang <bora1.hwang@@samsung.com>
  * @author Jiyoung Choi <jychoi7.choi@@samsung.com>
  * @author Arvind R <arvino55@@gmail.com>
@@ -91,6 +90,68 @@
  * @author Michal Jagiello <m.jagiello@@samsung.com>
  * @author Chinmaya Panigrahi <c.panigrahi@@samsung.com>
  * @author Mohammad Irfan <mohammad.i@@samsung.com>
+ * @author ajwillia.ms (Andrew Williams) <andy@@andywilliams.me>
+ * @author Iván Briano <sachieru@@gmail.com>
+ * @author Jonas M. Gastal <jgastal@@profusion.mobi>
+ * @author Rafael Antognolli <antognolli@@gmail.com>
+ * @author Mike McCormack <mike@@atratus.org>
+ * @author Massimo Maiurana <maiurana@@gmail.com>
+ * @author Henrique Dante de Almeida <hdante@@profusion.mobi>
+ * @author Lucas De Marchi <lucas.demarchi@@profusion.mobi>
+ * @author Boris Faure <billiob@@gmail.com>
+ * @author Sebastian Dransfeld <sd@@tango.flipp.net>
+ * @author Daniel Willmann <daniel@@totalueberwachung.de>
+ * @author maxerba <maiurana@@gmail.com>
+ * @author Youness Alaoui <kakaroto@@kakaroto.homelinux.net>
+ * @author Rui Seabra <rms@@1407.org>
+ * @author Andreas Volz <linux@@brachttal.net>
+ * @author Jaeun Choi <jaeun12.choi@@samsung.com>
+ * @author Doug Newgard <scimmia22@@outlook.com>
+ * @author José Roberto de Souza <zehortigoza@@profusion.mobi>
+ * @author Luis Felipe Strano Moraes <lfelipe@@profusion.mobi>
+ * @author Thiago Thamada <tiba@@profusion.mobi>
+ * @author U. Artie Eoff <ullysses.a.eoff@@intel.com>
+ * @author Daniel Hirt <daniel.hirt@@samsung.com>
+ * @author Eduardo Lima <eduardo.lima@@intel.com>
+ * @author Leif Middelschulte <leif.middelschulte@@gmail.com>
+ * @author Lukasz Stanislawski <l.stanislaws@@samsung.com>
+ * @author Stephen Houston <smhouston88@@gmail.com>
+ * @author Andrii Kroitor <an.kroitor@@samsung.com>
+ * @author Joao Paulo Fernandes Ventura <ventura@@profusion.mobi>
+ * @author Pau Espin Pedrol <pespin.shar@@gmail.com>
+ * @author Jérémy Zurcher <jeremy@@asynk.ch>
+ * @author Kai Huuhko <kai.huuhko@@gmail.com>
+ * @author Leandro Dorileo <dorileo@@profusion.mobi>
+ * @author Michael Jennings <mej@@kainx.org>
+ * @author Myungjae Lee <mjae.lee@@samsung.com>
+ * @author Tristan Lelong <tristan.lelong@@blunderer.org>
+ * @author Yossi Kantor <yossi.kantor@@samsung.com>
+ * @author cabelitos <guilherme.iscaro@@intel.com>
+ * @author Alex Wu <zhiwen.wu@@linux.intel.com>
+ * @author Alex-P. Natsios <drakevr@@linuxteam.teilar.gr>
+ * @author Aron Xu <happyaron.xu@@gmail.com>
+ * @author Chidambar Zinnoury <illogict@@online.fr>
+ * @author Christophe Sadoine <chris@@indefini.org>
+ * @author Daniel Vieira Franzolin <daniel@@profusion.mobi>
+ * @author Daniele Ricci <daniele.athome@@gmail.com>
+ * @author David Walter Seikel <onefang@@gmail.com>
+ * @author Dennis Schridde <devurandom@@gmx.net>
+ * @author Hannes Janetzek <hannes.janetzek@@gmail.com>
+ * @author Lionel Landwerlin <llandwerlin@@gmail.com>
+ * @author Mariusz Bialonczyk <manio@@skyboo.net>
+ * @author Martin Jansa <Martin.Jansa@@gmail.com>
+ * @author Michael Lauer <mlauer@@vanille-media.de>
+ * @author Mike Frysinger <vapier@@gentoo.org>
+ * @author Rodrigo Cesar Lopes Belem <rclbelem@@gmail.com>
+ * @author Seong-ho Cho <darkcircle.0426@@gmail.com>
+ * @author Seunghun Lee <shiin.lee@@samsung.com>
+ * @author Thanatermesis <thanatermesis.ecvs@@gmail.com>
+ * @author Viacheslav Lvov <v.lvov@@samsung.com>
+ * @author Deasung Kim <deasung.kim@@samsung.com>
+ * @author Jeonghoon Park <jh1979.park@@samsung.com>
+ * @author Prashant <pb.ingale@@samsung.com>
+ * @author suxia li <suxia.li@@samsung.com>
+ * @author yan.wang <yan.wang@@linux.intel.com>
  *
  * Please contact <enlightenment-devel@lists.sourceforge.net> to get in
  * contact with the developers and maintainers.
