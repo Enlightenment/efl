c409347e85d64e4cc3b9a0d99ec48a1f36668e9a
Anand <anand.km@samsung.com>
2014-03-10 16:25:23 +0900

    Entry test code: Added example to show the last character of entered password
    Summary: Added example to show the last character of entered password
    
    Test Plan: elementary_test <search scrolled entry>
    
    Reviewers: singh.amitesh, seoz
    
    CC: seoz
    
    Differential Revision: https://phab.enlightenment.org/D610

--- legacy/elementary/src/bin/test_entry.c
+++ legacy/elementary/src/bin/test_entry.c
@@ -303,7 +303,7 @@ _end_hide_cb(void *data, Evas_Object *obj EINA_UNUSED, void *event_info EINA_UNU
 void
 test_entry_scrolled(void *data EINA_UNUSED, Evas_Object *obj EINA_UNUSED, void *event_info EINA_UNUSED)
 {
-   Evas_Object *win, *bx, *bx2, *bx3, *bt, *en, *en_p, *sp, *entry;
+   Evas_Object *win, *bx, *bx2, *bx3, *bx4, *bt, *en, *en_p, *sp, *entry, *label;
    static Elm_Entry_Filter_Accept_Set digits_filter_data, digits_filter_data2;
    static Elm_Entry_Filter_Limit_Size limit_filter_data, limit_filter_data2;
 
@@ -462,7 +462,33 @@ test_entry_scrolled(void *data EINA_UNUSED, Evas_Object *obj EINA_UNUSED, void *
    evas_object_show(en_p);
    elm_box_pack_end(bx, en_p);
 
-   /* entry with icon/end widgets*/
+   /* Last password show entry */
+   bx4 = elm_box_add(win);
+   evas_object_size_hint_weight_set(bx4, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+   evas_object_show(bx4);
+
+   label = elm_label_add(bx4);
+   elm_object_text_set(label, "<b>Last password show entry</b>");
+   evas_object_size_hint_align_set(label, EVAS_HINT_FILL, EVAS_HINT_FILL);
+   elm_box_pack_end(bx4, label);
+   evas_object_show(label);
+
+   en = elm_entry_add(bx4);
+   elm_entry_scrollable_set(en, EINA_TRUE);
+   evas_object_size_hint_weight_set(en, EVAS_HINT_EXPAND, 0.0);
+   evas_object_size_hint_align_set(en, EVAS_HINT_FILL, 0.5);
+   elm_scroller_policy_set(en, ELM_SCROLLER_POLICY_OFF, ELM_SCROLLER_POLICY_OFF);
+   elm_entry_password_set(en, EINA_TRUE);
+   elm_object_text_set(en, "Last password show");
+   elm_entry_single_line_set(en, EINA_TRUE);
+   edje_password_show_last_set(EINA_TRUE);
+   edje_password_show_last_timeout_set(-1);
+   evas_object_show(en);
+
+   elm_box_pack_end(bx4, en);
+   elm_box_pack_end(bx, bx4);
+
+   /* entry with icon/end widgets */
    entry = elm_entry_add(win);
    elm_entry_scrollable_set(entry, EINA_TRUE);
    elm_scroller_policy_set(entry, ELM_SCROLLER_POLICY_OFF, ELM_SCROLLER_POLICY_OFF);
