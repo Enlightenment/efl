7100cfea40b249d23e6914065f9c62ae53852584
Daniel Juyung Seo <seojuyung2@gmail.com>
2014-03-16 02:39:09 +0900

    list: Reset prev_focused_item when prev_focused_item is deleted.
    This is inspired by a2c680d684601d8df725e8293616c880f07d3ca2.

--- legacy/elementary/src/lib/elm_list.c
+++ legacy/elementary/src/lib/elm_list.c
@@ -1674,6 +1674,8 @@ _item_del_pre_hook(Elm_Object_Item *it)
      sd->focused_item = NULL;
    if (sd->last_focused_item == (Elm_Object_Item *)it)
      sd->last_focused_item = NULL;
+   if (sd->prev_focused_item == (Elm_Object_Item *)it)
+     sd->prev_focused_item = NULL;
    if (sd->last_selected_item == (Elm_Object_Item *)it)
      sd->last_selected_item = NULL;
 
