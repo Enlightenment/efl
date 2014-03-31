44c96c3efd0a53942945c4245a77c3502027906f
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2014-03-31 20:39:16 +0900

    fix left over soft cursors in elm
    @fix - this fixes T893

--- legacy/elementary/src/lib/els_cursor.c
+++ legacy/elementary/src/lib/els_cursor.c
@@ -225,6 +225,14 @@ _elm_cursor_set(Elm_Cursor *cur)
      }
    else
      {
+        if (cur->obj)
+          {
+             evas_object_del(cur->obj);
+             cur->obj = NULL;
+          }
+        ecore_evas_object_cursor_set(cur->ee, NULL,
+                                     ELM_OBJECT_LAYER_CURSOR, cur->hot_x,
+                                     cur->hot_y);
 #ifdef HAVE_ELEMENTARY_X
         if (cur->x.win)
           ecore_x_window_cursor_set(cur->x.win, cur->x.cursor);
