4ed35ccc04c63d727af4af7176601c5a1f64f665
Yossi Kantor <yossi.kantor@samsung.com>
2014-04-09 14:59:37 +0300

    Eolian: Eo header clean and legacy integration for:
      Elementary Background
      Elementary Box
      Elementary Bubble
      Elementary Button
      Elementary Calendar
      Elementary Check
      Elementary Clock
      Elementary Color Selector
      Elementary Ctxpopup

--- legacy/elementary/src/lib/elm_bg_legacy.h
+++ legacy/elementary/src/lib/elm_bg_legacy.h
@@ -9,81 +9,6 @@
 EAPI Evas_Object                 *elm_bg_add(Evas_Object *parent);
 
 /**
- * Set the file (image or edje collection) to give life for the
- * background
- *
- * @param obj The background object handle
- * @param file The file path
- * @param group Optional key (group in Edje) within the file
- * @return @c EINA_TRUE on success, @c EINA_FALSE otherwise
- *
- * This sets the image file used in the background object. If the
- * image comes from an Edje group, it will be stretched to completely
- * fill the background object. If it comes from a traditional image file, it
- * will by default be centered in this widget's are (thus retaining
- * its aspect), what could lead to some parts being not visible. You
- * may change the mode of exhibition for a real image file with
- * elm_bg_option_set().
- *
- * @note Once the image of @p obj is set, a previously set one will be
- * deleted, even if @p file is @c NULL.
- *
- * @note This will only affect the contents of one of the background's
- * swallow spots, namely @c "elm.swallow.background". If you want to
- * achieve the @c Layout's file setting behavior, you'll have to call
- * that method on this object.
- *
- * @ingroup Bg
- */
-EAPI Eina_Bool                    elm_bg_file_set(Evas_Object *obj, const char *file, const char *group);
-
-/**
- * Get the file (image or edje collection) set on a given background
- * widget
- *
- * @param obj The background object handle
- * @param file Where to store the requested file's path
- * @param group Where to store the optional key within @a file, @b if
- * it's an Edje file
- *
- * @note Use @c NULL pointers on the file components you're not
- * interested in: they'll be ignored by the function.
- *
- * @ingroup Bg
- */
-EAPI void                         elm_bg_file_get(const Evas_Object *obj, const char **file, const char **group);
-
-/**
- * Set the mode of display for a given background widget's image
- *
- * @param obj The background object handle
- * @param option The desired background option (see #Elm_Bg_Option)
- *
- * This sets how the background widget will display its image. This
- * will only work if the elm_bg_file_set() was previously called with
- * an image file on @a obj. The image can be display tiled, scaled,
- * centered or stretched.
- *
- * @see elm_bg_option_get()
- *
- * @ingroup Bg
- */
-EAPI void                         elm_bg_option_set(Evas_Object *obj, Elm_Bg_Option option);
-
-/**
- * Get the mode of display for a given background widget's image
- *
- * @param obj The background object handle
- * @return The image displaying mode in use for @a obj or #ELM_BG_OPTION_LAST,
- * on errors.
- *
- * @see elm_bg_option_set() for more details
- *
- * @ingroup Bg
- */
-EAPI Elm_Bg_Option                elm_bg_option_get(const Evas_Object *obj);
-
-/**
  * Set the color on a given background widget
  *
  * @param obj The background object handle
@@ -123,27 +48,4 @@ EAPI void                         elm_bg_color_set(Evas_Object *obj, int r, int 
  */
 EAPI void                         elm_bg_color_get(const Evas_Object *obj, int *r, int *g, int *b);
 
-/**
- * Set the size of the pixmap representation of the image set on a
- * given background widget.
- *
- * @param obj The background object handle
- * @param w The new width of the image pixmap representation.
- * @param h The new height of the image pixmap representation.
- *
- * @warning This function just makes sense if an image file was set on
- * @p obj, with elm_bg_file_set().
- *
- * This function sets a new size for pixmap representation of the
- * given bg image. It allows for the image to be loaded already in the
- * specified size, reducing the memory usage and load time (for
- * example, when loading a big image file with its load size set to a
- * smaller size)
- *
- * @note This is just a hint for the underlying system. The real size
- * of the pixmap may differ depending on the type of image being
- * loaded, being bigger than requested.
- *
- * @ingroup Bg
- */
-EAPI void                         elm_bg_load_size_set(Evas_Object *obj, Evas_Coord w, Evas_Coord h);
+#include "elm_bg.eo.legacy.h"
--- legacy/elementary/src/lib/elm_bubble_legacy.h
+++ legacy/elementary/src/lib/elm_bubble_legacy.h
@@ -10,29 +10,4 @@
  */
 EAPI Evas_Object                 *elm_bubble_add(Evas_Object *parent);
 
-/**
- * Set the corner of the bubble
- *
- * @param obj The bubble object.
- * @param pos The given corner for the bubble.
- *
- * This function sets the corner of the bubble. The corner will be used to
- * determine where the arrow in the frame points to and where label, icon and
- * info are shown.
- *
- *
- * @ingroup Bubble
- */
-EAPI void  elm_bubble_pos_set(Evas_Object *obj, Elm_Bubble_Pos pos);
-
-/**
- * Get the corner of the bubble
- *
- * @param obj The bubble object.
- * @return The given corner for the bubble.
- *
- * This function gets the selected corner of the bubble.
- *
- * @ingroup Bubble
- */
-EAPI Elm_Bubble_Pos elm_bubble_pos_get(const Evas_Object *obj);
+#include "elm_bubble.eo.legacy.h"
