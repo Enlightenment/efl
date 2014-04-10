9c5ac1dd0022b21528c1525db46f38b7a3a4414e
Yossi Kantor <yossi.kantor@samsung.com>
2014-04-10 12:31:53 +0300

    Eolian: Eo header clean.
      Elementary Conform
      Elementary Widget
      Elementary Widget Container

--- legacy/elementary/src/lib/elm_conform_eo.h
+++ legacy/elementary/src/lib/elm_conform_eo.h
@@ -3,19 +3,9 @@
  *
  * @{
  */
+
 #include "elm_conformant.eo.h"
-#if 0
-#define ELM_OBJ_CONFORMANT_CLASS elm_obj_conformant_class_get()
 
-const Eo_Class *elm_obj_conformant_class_get(void) EINA_CONST;
-
-extern EAPI Eo_Op ELM_OBJ_CONFORMANT_BASE_ID;
-
-enum
-{
-   ELM_OBJ_CONFORMANT_SUB_ID_LAST
-};
-#endif
 /**
  * @}
  */
