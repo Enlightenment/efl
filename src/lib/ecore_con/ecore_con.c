b53a68d4c68f1ab47dc78c1d867244e61c1d073c
Stefan Schmidt <s.schmidt@samsung.com>
2014-04-11 11:26:18 +0200

    ecore_con: Remove superfluous >= 0 check for unsigned
    Since num was changed from (signed) int to (unsigned)  size_t in
    16d7b981ebc22df86e4dc7719ae63677bff8b1e1 this check no longer
    makes sense as it will always be true.

--- src/lib/ecore_con/ecore_con.c
+++ src/lib/ecore_con/ecore_con.c
@@ -2467,7 +2467,7 @@ _ecore_con_client_flush(Ecore_Con_Client *cl)
         if (cl->fd_handler)
           ecore_main_fd_handler_active_set(cl->fd_handler, ECORE_FD_READ);
      }
-   else if (cl->fd_handler && (num >= 0))
+   else if (cl->fd_handler)
      ecore_main_fd_handler_active_set(cl->fd_handler, ECORE_FD_WRITE);
 }
 
