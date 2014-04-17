d9ea423b91c372e0a50ec90efeed53d14995be97
Stefan Schmidt <s.schmidt@samsung.com>
2014-04-17 17:01:45 +0200

    ecore_wl: Actually watch out for ECORE_FD_ERROR

--- src/lib/ecore_wayland/ecore_wl.c
+++ src/lib/ecore_wayland/ecore_wl.c
@@ -183,7 +183,7 @@ ecore_wl_init(const char *name)
    _ecore_wl_disp->fd = wl_display_get_fd(_ecore_wl_disp->wl.display);
 
    _ecore_wl_disp->fd_hdl =
-     ecore_main_fd_handler_add(_ecore_wl_disp->fd, ECORE_FD_READ | ECORE_FD_WRITE,
+     ecore_main_fd_handler_add(_ecore_wl_disp->fd, ECORE_FD_READ | ECORE_FD_WRITE | ECORE_FD_ERROR,
                                _ecore_wl_cb_handle_data, _ecore_wl_disp,
                                NULL, NULL);
 
