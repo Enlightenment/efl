845008da8c7a5022ca5720b746904c2f697649dc
Mike Blumenkrantz <zmike@samsung.com>
2014-04-20 23:30:13 -0400

    edje programs now provide slightly more verbose errors when disabling due to recursion limits

--- src/lib/edje/edje_program.c
+++ src/lib/edje/edje_program.c
@@ -454,8 +454,10 @@ _edje_program_run(Edje *ed, Edje_Program *pr, Eina_Bool force, const char *ssig,
      }
    if ((recursions >= 64) || (recursion_limit))
      {
-        ERR("Programs recursing up to recursion limit of %i in '%s' with '%s', '%s' from '%s', '%s'. Disabled.",
+        ERR("Programs recursing up to recursion limit of %i in '%s' with sig='%s', src='%s' from '%s', '%s'. Disabled.",
             64, pr->name, ssig, ssrc, ed->path, ed->group);
+        if (pr->action == EDJE_ACTION_TYPE_STATE_SET && ((pr->tween.time > ZERO) && (!ed->no_anim)))
+          ERR("Possible solution: try adding transition time to prevent Schrödinger's part state");
         recursion_limit = 1;
         return;
      }
