8056a854285afe1f52ec2ab000046586d28f5ac7
Daniel Kolesa <quaker66@gmail.com>
2014-04-28 14:41:58 +0100

    elua: support for default domain in gettext module

--- src/bin/elua/core/gettext.lua
+++ src/bin/elua/core/gettext.lua
@@ -15,6 +15,8 @@ end
 
 local domains = {}
 
+local default_domain
+
 M.register_domain = function(dom, dir)
     local d, err = bind_textdomain(dom, dir)
     if not d then
@@ -28,10 +30,20 @@ M.get_domain = function(dom)
     return domains[dom]
 end
 
+M.set_default_domain = function(dom)
+    if not domains[dom] then return false end
+    default_domain = dom
+    return true
+end
+
 local cast, ffistr = ffi.cast, ffi.string
 
 if dgettext then
     M.gettext = function(dom, msgid)
+        if not msgid then
+            msgid = dom
+            dom   = default_domain
+        end
         if not domains[dom] or not msgid then return msgid end
         local cmsgid = cast("const char*", msgid)
         local lmsgid = dgettext(dom, cmsgid)
