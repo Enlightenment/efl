a0fc68ee1196c65b8ee359a2e686828a609dacd4
Jean-Philippe Andre <jp.andre@samsung.com>
2014-04-29 09:02:49 +0900

    Evas GL: Fix invalid definitions of ETC2 GLenum
    Bad copy and paste.

--- src/lib/evas/Evas_GL.h
+++ src/lib/evas/Evas_GL.h
@@ -1058,8 +1058,8 @@ typedef signed long int  GLsizeiptr;   // Changed khronos_ssize_t
 
 /* The following are OpenGL ES 3.0 definitions for ETC2
  * Note that RGB8_ETC2 is a superset of GL_OES_compressed_ETC1_RGB8_texture */
-#define GL_COMPRESSED_RGB8_ETC2                                 0x8D64
-#define GL_COMPRESSED_RGBA8_ETC2_EAC                            0x8D64
+#define GL_COMPRESSED_RGB8_ETC2                                 0x9274
+#define GL_COMPRESSED_RGBA8_ETC2_EAC                            0x9278
 
 /* GL_OES_compressed_paletted_texture */
 #define GL_PALETTE4_RGB8_OES                                    0x8B90
