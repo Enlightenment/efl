8e45bb869eece7c6d7313d6f95dfe2de5572bb33
Jean-Philippe Andre <jp.andre@samsung.com>
2014-05-20 12:27:29 +0900

    elm_test: Set PACKAGE_LIB_DIR to fix windows runtime

--- legacy/elementary/src/bin/test.c
+++ legacy/elementary/src/bin/test.c
@@ -890,6 +890,7 @@ elm_main(int argc, char **argv)
 
    /* tell elm about our app so it can figure out where to get files */
    elm_app_compile_bin_dir_set(PACKAGE_BIN_DIR);
+   elm_app_compile_lib_dir_set(PACKAGE_LIB_DIR);
    elm_app_compile_data_dir_set(PACKAGE_DATA_DIR);
    elm_app_info_set(elm_main, "elementary", "images/logo.png");
 
