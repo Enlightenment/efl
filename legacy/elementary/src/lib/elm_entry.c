ccc06de678e088adc548f4ab7a19e95367f91e01
Andy Williams <andy@andywilliams.me>
2014-05-22 22:33:46 +0100

    Correctly emit the scroll signal when scrolling an entry @fix T1274.
    As the Elm_Entry has replaced the _scroll_cb we need to emit the same signal
    (that was the only function of elm_scroller's _scroll_cb)

--- legacy/elementary/src/lib/elm_entry.c
+++ legacy/elementary/src/lib/elm_entry.c
@@ -4505,6 +4505,9 @@ static void
 _scroll_cb(Evas_Object *obj, void *data EINA_UNUSED)
 {
    ELM_ENTRY_DATA_GET(obj, sd);
+   /* here we need to emit the signal that the elm_scroller would have done */
+   evas_object_smart_callback_call(obj, "scroll", NULL);
+
    if (sd->have_selection)
      _update_selection_handler(obj);
 }
