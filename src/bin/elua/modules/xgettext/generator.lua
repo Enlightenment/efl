a0b8f72ae95c802a67daa5e6257cdea06e093482
Daniel Kolesa <d.kolesa@samsung.com>
2014-05-30 14:53:54 +0100

    elua: respect msgstr prefix and suffix in xgettext

--- src/bin/elua/modules/xgettext/generator.lua
+++ src/bin/elua/modules/xgettext/generator.lua
@@ -22,6 +22,14 @@ local gen_message = function(str)
     return table.concat(ret, "\n")
 end
 
+local gen_msgstr = function(str, prefix, suffix)
+    if not prefix and not suffix then
+        return '""'
+    end
+    return gen_message(table.concat(prefix
+        and { prefix, str, suffix } or { str, suffix }))
+end
+
 return { init  = function(chunkname, input, keywords, flags, opts)
     local ps   = parser.init(chunkname, input, keywords, flags, opts)
     local msg  = ps()
@@ -38,11 +46,13 @@ return { init  = function(chunkname, input, keywords, flags, opts)
             ret[#ret + 1] = "msgctxt " .. gen_message(msg.context)
         end
         ret[#ret + 1] = "msgid " .. gen_message(msg[1])
+        local spf, ssf = opts["m"], opts["M"]
         if msg[2] then
             ret[#ret + 1] = "msgid_plural " .. gen_message(msg[2])
-            ret[#ret + 1] = "msgstr[0] \"\"\nmsgstr[1] \"\""
+            ret[#ret + 1] = "msgstr[0] " .. gen_msgstr(msg[1], spf, ssf)
+            ret[#ret + 1] = "msgstr[1] " .. gen_msgstr(msg[2], spf, ssf)
         else
-            ret[#ret + 1] = "msgstr \"\""
+            ret[#ret + 1] = "msgstr " .. gen_msgstr(msg[1], spf, ssf)
         end
         rets[#rets + 1] = table.concat(ret, "\n")
         msg             = ps()
