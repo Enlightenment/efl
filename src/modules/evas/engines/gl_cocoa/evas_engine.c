d29aac6fe7a2cbcca09363fa11bc7a30c8054d73
Raoul Hecky <raoul.hecky@gmail.com>
2014-06-08 21:54:10 +0200

    evas_cocoa: remove eng_image_mask_create

--- src/modules/evas/engines/gl_cocoa/evas_engine.c
+++ src/modules/evas/engines/gl_cocoa/evas_engine.c
@@ -407,20 +407,6 @@ eng_image_colorspace_get(void *data EINA_UNUSED, void *image)
    return im->cs.space;
 }
 
-static void
-eng_image_mask_create(void *data EINA_UNUSED, void *image)
-{
-   Evas_GL_Image *im;
-
-   if (!image) return;
-   im = image;
-   if (!im->im->image.data)
-      evas_cache_image_load_data(&im->im->cache_entry);
-   if (!im->tex)
-      im->tex = evas_gl_common_texture_new(im->gc, im->im);
-}
-
-
 static void *
 eng_image_alpha_set(void *data, void *image, int has_alpha)
 {
@@ -1404,7 +1390,6 @@ module_open(Evas_Module *em)
    ORD(image_format_get);
    ORD(image_colorspace_set);
    ORD(image_colorspace_get);
-   ORD(image_mask_create);
    //   ORD(image_native_set);
    //   ORD(image_native_get);
    
