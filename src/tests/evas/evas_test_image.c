573f477272fb842f2ed3f1c951fd1c77d6936b59
Wonguk Jeong <wonguk.jeong@samsung.com>
2014-06-29 13:04:16 +0200

    jpeg laoder: exif orientation test added
    Summary:
    currently, normal orientation tests are only added.
    I'm going to add flipped orientation tests as well after I put related code in jpeg loader (currently it's not supported)
    
    Reviewers: raster, cedric, jpeg
    
    CC: seoz, cedric
    
    Differential Revision: https://phab.enlightenment.org/D1098

--- src/tests/evas/evas_test_image.c
+++ src/tests/evas/evas_test_image.c
@@ -9,13 +9,15 @@
 #include "Evas.h"
 #include "evas_tests_helpers.h"
 
+#define TESTS_IMG_DIR TESTS_SRC_DIR"/images"
+
 #if 0
 static const char *
 _test_image_get(const char *name)
 {
    static char filename[PATH_MAX];
 
-   snprintf(filename, PATH_MAX, TESTS_SRC_DIR"/images/%s", name);
+   snprintf(filename, PATH_MAX, TESTS_IMG_DIR"/%s", name);
 
    return filename;
 }
@@ -30,7 +32,7 @@ START_TEST(evas_object_image_loader)
 
    o = evas_object_image_add(e);
 
-   it = eina_file_direct_ls(TESTS_SRC_DIR"/images/");
+   it = eina_file_direct_ls(TESTS_IMG_DIR);
    EINA_ITERATOR_FOREACH(it, file)
      {
         int w, h;
@@ -49,7 +51,120 @@ START_TEST(evas_object_image_loader)
 }
 END_TEST
 
+typedef struct _orientation_Test_Res {
+   const char *img;
+   int flag;
+   int (*compare_func)(const uint32_t *d1, const uint32_t *d2, int w2, int h2);
+} Orientation_Test_Res;
+
+static int _compare_img(const uint32_t *d1, const uint32_t *d2, int w2, int h2)
+{
+   return memcmp(d1, d2, w2 * h2 * 4);
+}
+
+static int _compare_img_90(const uint32_t *d1, const uint32_t *d2, int w2, int h2)
+{
+   int x, y;
+   int r;
+
+   for (x = w2 - 1; x >= 0; x--)
+     {
+        for (y = 0; y < h2; y++)
+          {
+             r = *d1 - *(d2 + x + y * w2);
+             if (r != 0) return r;
+             d1++;
+          }
+     }
+
+   return 0;
+}
+
+static int _compare_img_180(const uint32_t *d1, const uint32_t *d2, int w2, int h2)
+{
+   int x, y;
+   int r;
+
+   for (y = h2 - 1; y >= 0; y--)
+     {
+        for (x = w2 - 1; x >= 0; x--)
+          {
+             r = *d1 - *(d2 + x + y * w2);
+             if (r != 0) return r;
+             d1++;
+          }
+     }
+
+   return 0;
+}
+
+static int _compare_img_270(const uint32_t *d1, const uint32_t *d2, int w2, int h2)
+{
+   int x, y;
+   int r;
+
+   for (x = 0; x < w2; x++)
+     {
+        for (y = h2 - 1; y >= 0; y--)
+          {
+             r = *d1 - *(d2 + x + y * w2);
+             if (r != 0) return r;
+             d1++;
+          }
+     }
+
+   return 0;
+}
+
+/* TODO: flip test code for 2, 4, 5, 7 */
+START_TEST(evas_object_image_loader_orientation)
+{
+   Evas *e = _setup_evas();
+   Evas_Object *orig, *rot;
+   Orientation_Test_Res res[] = {
+       {TESTS_IMG_DIR"/Light_exif.jpg", 1, _compare_img},
+       {TESTS_IMG_DIR"/Light_exif_90.jpg", 6, _compare_img_90},
+       {TESTS_IMG_DIR"/Light_exif_180.jpg", 3, _compare_img_180},
+       {TESTS_IMG_DIR"/Light_exif_270.jpg", 8, _compare_img_270},
+       {NULL, 0, NULL}
+   };
+   int w, h, r_w, r_h;
+   const uint32_t *d, *r_d;
+   int i;
+
+   orig = evas_object_image_add(e);
+   evas_object_image_file_set(orig, TESTS_IMG_DIR"/Light.jpg", NULL);
+   fail_if(evas_object_image_load_error_get(orig) != EVAS_LOAD_ERROR_NONE);
+   evas_object_image_size_get(orig, &w, &h);
+   fail_if(w == 0 || h == 0);
+   d = evas_object_image_data_get(orig, EINA_FALSE);
+
+   rot = evas_object_image_add(e);
+   evas_object_image_load_orientation_set(rot, EINA_TRUE);
+
+   for (i = 0; res[i].img; i++)
+     {
+        evas_object_image_file_set(rot, res[i].img, NULL);
+        fail_if(evas_object_image_load_error_get(rot) != EVAS_LOAD_ERROR_NONE);
+        evas_object_image_size_get(rot, &r_w, &r_h);
+	fail_if(w * h != r_w * r_h);
+
+        r_d = evas_object_image_data_get(rot, EINA_FALSE);
+
+        fail_if(res[i].compare_func(d, r_d, r_w, r_h),
+                "Image orientation test failed: exif orientation flag: %d\n", res[i].flag);
+     }
+
+   evas_object_del(orig);
+   evas_object_del(rot);
+
+   evas_free(e);
+   evas_shutdown();
+}
+END_TEST
+
 void evas_test_image_object(TCase *tc)
 {
    tcase_add_test(tc, evas_object_image_loader);
+   tcase_add_test(tc, evas_object_image_loader_orientation);
 }
