12dc40fa3b98600fe23280c901bab324a1184623
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2014-07-01 19:57:04 +0900

    formatting - convert tabs to whitespace

--- src/lib/evas/common/evas_image_save.c
+++ src/lib/evas/common/evas_image_save.c
@@ -19,17 +19,17 @@ evas_common_save_image_to_file(RGBA_Image *im, const char *file, const char *key
    p = strrchr(file, '.');
    if (p)
      {
-	p++;
+        p++;
 
-	if (!strcasecmp(p, "png"))
+        if (!strcasecmp(p, "png"))
           saver = "png";
-	if ((!strcasecmp(p, "jpg")) || (!strcasecmp(p, "jpeg")) ||
-	    (!strcasecmp(p, "jfif")))
+        if ((!strcasecmp(p, "jpg")) || (!strcasecmp(p, "jpeg")) ||
+            (!strcasecmp(p, "jfif")))
           saver = "jpeg";
-	if ((!strcasecmp(p, "eet")) || (!strcasecmp(p, "edj")) ||
+        if ((!strcasecmp(p, "eet")) || (!strcasecmp(p, "edj")) ||
             (!strcasecmp(p, "eap")))
           saver = "eet";
-	if (!strcasecmp(p, "webp"))
+        if (!strcasecmp(p, "webp"))
           saver = "webp";
         if (!strcasecmp(p, "tgv"))
           saver = "tgv";
@@ -39,17 +39,17 @@ evas_common_save_image_to_file(RGBA_Image *im, const char *file, const char *key
      {
         Evas_Module *em;
 
-	em = evas_module_find_type(EVAS_MODULE_TYPE_IMAGE_SAVER, saver);
-	if (em)
-	  {
-	     evas_module_use(em);
-	     if (evas_module_load(em))
-	       {
-		  evas_image_save_func = em->functions;
+        em = evas_module_find_type(EVAS_MODULE_TYPE_IMAGE_SAVER, saver);
+        if (em)
+          {
+             evas_module_use(em);
+             if (evas_module_load(em))
+               {
+                  evas_image_save_func = em->functions;
                   return evas_image_save_func->image_save(im, file, key, quality,
                                                           compress, encoding);
-	       }
-	  }
+               }
+          }
      }
    return 0;
 }
