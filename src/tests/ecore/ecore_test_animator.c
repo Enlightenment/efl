a7dfaef21522ef239ed802eae125d655f537c757
Jérémy Zurcher <jeremy@asynk.ch>
2014-07-03 23:05:57 +0200

    Revert "ecore: animator use eo_add() instead of eo_add_custom()"
    mmhhmm, missing @ Constructor tag, bad for the bindings,
    maybe we must split animator and timeline into 2 classes,
    maybe support callback hot swaping ...
    
    This reverts commit ec4ffb86d6bc9a3d4e4e81b80359abd7bbdb8f22.

--- src/tests/ecore/ecore_test_animator.c
+++ src/tests/ecore/ecore_test_animator.c
@@ -29,18 +29,15 @@ START_TEST(ecore_test_animators)
    fail_if(!ecore_init(), "ERROR: Cannot init Ecore!\n");
 
    ecore_animator_frametime_set(interval1);
+   animator = eo_add_custom(ECORE_ANIMATOR_CLASS, NULL, ecore_animator_timeline_constructor(1, _anim_cb, &interval1));
 
-   animator = eo_add(ECORE_ANIMATOR_CLASS, NULL);
-   fail_if(animator);
-
-   animator = eo_add(ECORE_ANIMATOR_CLASS, NULL, ecore_obj_animator_timeline_init(1, _anim_cb, &interval1));
    fail_if(!animator);
 
    ecore_main_loop_begin();
 
    ecore_animator_frametime_set(interval2);
    prev = 0;
-   animator = eo_add(ECORE_ANIMATOR_CLASS, NULL, ecore_obj_animator_timeline_init(1, _anim_cb, &interval2));
+   animator = eo_add_custom(ECORE_ANIMATOR_CLASS, NULL, ecore_animator_timeline_constructor(1, _anim_cb, &interval2));
    fail_if(!animator);
 
    ecore_main_loop_begin();
