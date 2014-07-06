e3192c374dcfae6abc16096de394c57451022f66
Gwanglim Lee <gl77.lee@samsung.com>
2014-07-06 20:19:28 +0200

    evas: gl_x11 - add missing function pointer glsym_eglSwapBuffersWithDamage
    Summary: added missing global function pointer to gl_x11
    
    Test Plan: N/A
    
    Reviewers: cedric, raster, devilhorns
    
    CC: cedric
    
    Differential Revision: https://phab.enlightenment.org/D1128
    
    Signed-off-by: Cedric BAIL <c.bail@partner.samsung.com>

--- src/modules/evas/engines/gl_x11/evas_engine.c
+++ src/modules/evas/engines/gl_x11/evas_engine.c
@@ -78,6 +78,7 @@ void     (*glsym_eglDestroyImage)              (EGLDisplay a, void *b) = NULL;
 void     (*glsym_glEGLImageTargetTexture2DOES) (int a, void *b)  = NULL;
 void          *(*glsym_eglMapImageSEC)         (void *a, void *b, int c, int d) = NULL;
 unsigned int   (*glsym_eglUnmapImageSEC)       (void *a, void *b, int c) = NULL;
+unsigned int   (*glsym_eglSwapBuffersWithDamage) (EGLDisplay a, void *b, const EGLint *d, EGLint c) = NULL;
 
 #else
 
