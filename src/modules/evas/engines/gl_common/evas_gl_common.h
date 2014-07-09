ec9ffd0ae85988679c4f029c8783259b0544abf0
Jean-Philippe Andre <jp.andre@samsung.com>
2014-07-09 12:36:22 +0900

    Evas gl: Fix invalid number of atlasses
    Left-over from a local experiment where ETC1+Alpha had its own
    texture pool. Not required.

--- src/modules/evas/engines/gl_common/evas_gl_common.h
+++ src/modules/evas/engines/gl_common/evas_gl_common.h
@@ -416,7 +416,7 @@ struct _Evas_GL_Shared
 #define MAX_ATLAS_H            512
 #define DEF_ATLAS_H                 512
 
-#define ATLAS_FORMATS_COUNT    11
+#define ATLAS_FORMATS_COUNT    10
 
       Eina_List *cspaces; // depend on the values of etc1, etc2 and st3c
 
