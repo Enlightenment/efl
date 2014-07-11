a9171a89f9aabcfc18edad450665f548de5d0566
Daniel Kolesa <d.kolesa@samsung.com>
2014-07-11 11:33:26 +0100

    eolian: remove parser dump for now (doesn't include all info, when it's needed again it can be re-added)

--- src/lib/eolian/eo_parser.h
+++ src/lib/eolian/eo_parser.h
@@ -3,7 +3,6 @@
 
 #include "eo_lexer.h"
 
-void      eo_parser_dump         (Eo_Lexer *ls);
 Eina_Bool eo_parser_walk         (Eo_Lexer *ls, Eina_Bool eot);
 Eina_Bool eo_parser_database_fill(const char *filename, Eina_Bool eot);
 
