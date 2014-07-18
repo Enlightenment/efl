b0b533fcb1beff3c01501258bb4af4a6533e6884
Tom Hacohen <tom@stosb.com>
2014-07-18 13:38:25 +0100

    Efl: Create libefl.
    Create libefl.so and the interal package for dependencies.

---
+++ src/lib/efl/Efl.h
@@ -0,0 +1,9 @@
+#ifndef _EFL_H
+#define _EFL_H
+
+#include <Eo.h>
+
+/* Interfaces */
+#include "interfaces/efl_interface_file.eo.h"
+
+#endif
---
+++ src/lib/efl/interfaces/efl_interfaces_main.c
@@ -0,0 +1,7 @@
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include <Efl.h>
+
+#include "interfaces/efl_interface_file.eo.c"
