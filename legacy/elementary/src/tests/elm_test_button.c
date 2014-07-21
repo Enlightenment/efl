18ca7d8eb545c3023f8cf2c8cb49c2122c1fab20
Zbigniew Kosinski <z.kosinski@samsung.com>
2014-07-21 18:07:10 +0200

    tests: check required interfaces implementation by widgets
    Reviewers: stanluk, raster
    
    Subscribers: raster
    
    Differential Revision: https://phab.enlightenment.org/D944

--- legacy/elementary/src/tests/elm_test_button.c
+++ legacy/elementary/src/tests/elm_test_button.c
@@ -9,6 +9,12 @@
 #include "elm_interface_atspi_accessible.h"
 #include "elm_interface_atspi_accessible.eo.h"
 
+#define ELM_INTERFACE_ATSPI_COMPONENT_PROTECTED
+#include "elm_interface_atspi_component.eo.h"
+
+#define ELM_INTERFACE_ATSPI_ACTION_PROTECTED
+#include "elm_interface_atspi_action.eo.h"
+
 
 START_TEST (elm_atspi_role_get)
 {
@@ -27,7 +33,25 @@ START_TEST (elm_atspi_role_get)
 }
 END_TEST
 
+START_TEST (elm_atspi_interfaces_check)
+{
+   Evas_Object *win, *button;
+
+   elm_init(1, NULL);
+   win = elm_win_add(NULL, "button", ELM_WIN_BASIC);
+
+   button = elm_button_add(win);
+
+   ck_assert(eo_isa(button, ELM_INTERFACE_ATSPI_ACCESSIBLE_MIXIN));
+   ck_assert(eo_isa(button, ELM_INTERFACE_ATSPI_COMPONENT_MIXIN));
+   ck_assert(eo_isa(button, ELM_INTERFACE_ATSPI_ACTION_MIXIN));
+
+   elm_shutdown();
+}
+END_TEST
+
 void elm_test_button(TCase *tc)
 {
  tcase_add_test(tc, elm_atspi_role_get);
+ tcase_add_test(tc, elm_atspi_interfaces_check);
 }
