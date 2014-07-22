0daff5e524372003f4c0b6f05db284fccfb8ea3a
Cedric BAIL <c.bail@partner.samsung.com>
2014-07-22 16:10:19 +0200

    evas: do check visibility flag early to not propagate show/hide when not needed.
    This should be a proper fix instead of D1217

--- src/lib/evas/canvas/evas_object_main.c
+++ src/lib/evas/canvas/evas_object_main.c
@@ -1148,14 +1148,14 @@ _show(Evas_Object *eo_obj, Evas_Object_Protected_Data *obj)
 {
    if (!obj->layer) return;
    if (obj->delete_me) return;
+   if (obj->cur->visible)
+     {
+        return;
+     }
    if (evas_object_intercept_call_show(eo_obj, obj)) return;
    if (obj->is_smart)
      {
         eo_do(eo_obj, evas_obj_smart_show());
-     }
-   if (obj->cur->visible)
-     {
-        return;
      }
    EINA_COW_STATE_WRITE_BEGIN(obj, state_write, cur)
      {
@@ -1197,16 +1197,16 @@ _hide(Evas_Object *eo_obj, Evas_Object_Protected_Data *obj)
    MAGIC_CHECK_END();
    if (!obj->layer) return;
    if (obj->delete_me) return;
-   if (evas_object_intercept_call_hide(eo_obj, obj)) return;
-   if (obj->is_smart)
-     {
-        eo_do(eo_obj, evas_obj_smart_hide());
-     }
    if (!obj->cur->visible)
      {
         return;
      }
-
+  if (evas_object_intercept_call_hide(eo_obj, obj)) return;
+   if (obj->is_smart)
+     {
+        eo_do(eo_obj, evas_obj_smart_hide());
+     }
+ 
    EINA_COW_STATE_WRITE_BEGIN(obj, state_write, cur)
      {
         state_write->visible = 0;
