ab38ffd6edf2f61917bd90ecbc82c59df12904a0
Tom Hacohen <tom@stosb.com>
2014-07-24 15:01:36 +0100

    Eo base: Fix marking of eolian functions with EOLIAN.

--- src/lib/eo/eo_base_class.c
+++ src/lib/eo/eo_base_class.c
@@ -913,7 +913,7 @@ _eo_base_constructor(Eo *obj, Eo_Base_Data *pd EINA_UNUSED)
    _eo_condtor_done(obj);
 }
 
-static void
+EOLIAN static void
 _eo_base_destructor(Eo *obj, Eo_Base_Data *pd)
 {
    Eo *child;
@@ -930,20 +930,20 @@ _eo_base_destructor(Eo *obj, Eo_Base_Data *pd)
    _eo_condtor_done(obj);
 }
 
-static Eo *
+EOLIAN static Eo *
 _eo_base_finalize(Eo *obj, Eo_Base_Data *pd EINA_UNUSED)
 {
    return _eo_add_internal_end(obj);
 }
 
-static void
+EOLIAN static void
 _eo_base_class_constructor(Eo_Class *klass EINA_UNUSED)
 {
    event_freeze_count = 0;
    _legacy_events_hash = eina_hash_stringshared_new(_legacy_events_hash_free_cb);
 }
 
-static void
+EOLIAN static void
 _eo_base_class_destructor(Eo_Class *klass EINA_UNUSED)
 {
    eina_hash_free(_legacy_events_hash);
