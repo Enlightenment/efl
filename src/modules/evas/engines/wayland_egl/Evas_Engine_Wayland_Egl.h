e2c08ffec29fc8b70d0a1625667d1261789ca2fb
Chris Michael <cp.michael@samsung.com>
2014-08-06 14:19:15 -0400

    evas-wayland-egl: Add support for SWAP_MODE_QUADRUPLE and use Eina_Bool for boolean values
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

--- src/modules/evas/engines/wayland_egl/Evas_Engine_Wayland_Egl.h
+++ src/modules/evas/engines/wayland_egl/Evas_Engine_Wayland_Egl.h
@@ -11,7 +11,8 @@ typedef enum _Evas_Engine_Info_Wayland_Egl_Swap_Mode
    EVAS_ENGINE_WAYLAND_EGL_SWAP_MODE_FULL = 1,
    EVAS_ENGINE_WAYLAND_EGL_SWAP_MODE_COPY = 2,
    EVAS_ENGINE_WAYLAND_EGL_SWAP_MODE_DOUBLE = 3,
-   EVAS_ENGINE_WAYLAND_EGL_SWAP_MODE_TRIPLE = 4
+   EVAS_ENGINE_WAYLAND_EGL_SWAP_MODE_TRIPLE = 4,
+   EVAS_ENGINE_WAYLAND_EGL_SWAP_MODE_QUADRUPLE = 5
 } Evas_Engine_Info_Wayland_Egl_Swap_Mode;
 
 struct _Evas_Engine_Info_Wayland_Egl
@@ -25,6 +26,7 @@ struct _Evas_Engine_Info_Wayland_Egl
      {
         struct wl_display *display;
         struct wl_surface *surface;
+        struct wl_egl_window *win;
         int depth, screen, rotation, edges;
         unsigned int destination_alpha : 1;
      } info;
@@ -39,8 +41,8 @@ struct _Evas_Engine_Info_Wayland_Egl
    /* non-blocking or blocking mode */
    Evas_Engine_Render_Mode render_mode;
 
-   unsigned char vsync : 1;
-   unsigned char indirect : 1;
+   Eina_Bool vsync : 1;
+   Eina_Bool indirect : 1;
    unsigned char swap_mode : 4;
 };
 
