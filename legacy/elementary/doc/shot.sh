c27476de4f68a892e98315dabac7f363d5c94f80
Stefan Schmidt <s.schmidt@samsung.com>
2014-08-07 09:18:50 +0200

    shot.sh: Set ELM_DISPLAY to bring the elm shot engine back to life
    As a small fallout from e7dbe063c9c044f072bacafd31ab872f6210788b we now
    need to set ELM_DISPLAY with our system has something like $DISPLAY or
    $WAYLAND_DISPLAY set. Elm uses that now to find out which window system
    to use.
    
    In the case of daing make doc we want it forced to shot even if we run
    under X. With ELM_DISPLAY we do that. Thanks goes to raster for the
    explanantion and git bisect for pinning it down.

--- legacy/elementary/doc/shot.sh
+++ legacy/elementary/doc/shot.sh
@@ -8,6 +8,7 @@ shift
 export ELM_THEME=default
 export ELM_SCALE=1.0
 export ELM_ENGINE=shot:delay=3:file=$DIR/$FILE
+export ELM_DISPLAY=shot
 export ILLUME_KBD=0,0,240,123
 export ILLUME_IND=0,0,240,32
 export ILLUME_STK=0,288,240,32
