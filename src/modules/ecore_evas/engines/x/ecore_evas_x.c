cf86cf5588e3114f43e058d16c87e8ded4c397c6
Cedric BAIL <cedric@osg.samsung.com>
2014-08-21 12:31:07 +0200

    ecore_evas: Ecore_Evas_X - make sure that ecore evas post render is always called.
    This fix a case where when doing asynchronous rendering and no update was done,
    the post render evas event would never be triggered and so no call to ecore evas
    post render callback were made. Now the async X backend follow all other backend
    behavior.
    
    @fix

--- src/modules/ecore_evas/engines/x/ecore_evas_x.c
+++ src/modules/ecore_evas/engines/x/ecore_evas_x.c
@@ -712,6 +712,7 @@ _ecore_evas_x_render(Ecore_Evas *ee)
         ee->in_async_render = EINA_TRUE;
         rend = 1;
      }
+   else if (ee->func.fn_post_render) ee->func.fn_post_render(ee);
 
    return rend;
 }
