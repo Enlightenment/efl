41ec0da2aa3be54b117601d9ab3863eff8efe0f1
Tom Hacohen <tom@stosb.com>
2014-08-22 14:51:35 +0100

    Revert "ecore_getopt  - handle if arg_val is null"
    This completely breaks getopt. Try running terminology --help.
    
    This reverts commit 1bb6a8a4e4055c6fce2391012aae3c6984aa2302.

--- src/lib/ecore/ecore_getopt.c
+++ src/lib/ecore/ecore_getopt.c
@@ -1788,9 +1788,7 @@ _ecore_getopt_parse_arg_short(const Ecore_Getopt *parser,
 
         desc_idx = desc - parser->descs;
         value = values + desc_idx;
-        if (arg_val)
-          ret = _ecore_getopt_desc_handle(parser, desc, value, arg_val);
-        else ret = EINA_FALSE;
+        ret = _ecore_getopt_desc_handle(parser, desc, value, arg_val);
         if ((!ret) && parser->strict)
           return EINA_FALSE;
      }
