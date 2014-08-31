ca27474779c7610410644dd93221844a4c1fabc4
Daniel Juyung Seo <seojuyung2@gmail.com>
2014-09-01 00:40:20 +0900

    actionslider: Added descriptions for Elm_Actionslider_Pos and added # for enum.

--- legacy/elementary/src/lib/elm_actionslider_common.h
+++ legacy/elementary/src/lib/elm_actionslider_common.h
@@ -1,8 +1,13 @@
+/**
+ * A position for indicators, magnets, and enabled items.
+ *
+ * @ingroup Actionslider
+ */
 typedef enum
 {
-   ELM_ACTIONSLIDER_NONE = 0,
-   ELM_ACTIONSLIDER_LEFT = 1 << 0,
-   ELM_ACTIONSLIDER_CENTER = 1 << 1,
-   ELM_ACTIONSLIDER_RIGHT = 1 << 2,
-   ELM_ACTIONSLIDER_ALL = (1 << 3) - 1
+   ELM_ACTIONSLIDER_NONE = 0, /**< no position is set */
+   ELM_ACTIONSLIDER_LEFT = 1 << 0, /**< left position */
+   ELM_ACTIONSLIDER_CENTER = 1 << 1, /**< center position */
+   ELM_ACTIONSLIDER_RIGHT = 1 << 2, /**< right position */
+   ELM_ACTIONSLIDER_ALL = (1 << 3) - 1 /**< all positions for left/center/right */
 } Elm_Actionslider_Pos;
