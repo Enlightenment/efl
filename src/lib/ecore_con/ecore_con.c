fdc8e455e460beb08350e3031da3967edd5339ef
Stefan Schmidt <s.schmidt@samsung.com>
2014-09-04 10:57:19 +0200

    ecore_con: Make sure host_server is not NUL before dereferencing
    This was intended to get fixed in f53683f76a8325f964051a4acfa2c74e460d5d8f
    
    CID 1232731

--- src/lib/ecore_con/ecore_con.c
+++ src/lib/ecore_con/ecore_con.c
@@ -805,7 +805,7 @@ _ecore_con_client_ecore_con_base_send(Eo *obj EINA_UNUSED, Ecore_Con_Client_Data
              cl->buf = eina_binbuf_new();
              EINA_SAFETY_ON_NULL_RETURN_VAL(cl->buf, 0);
 #ifdef TCP_CORK
-             if ((cl->fd >= 0) && (host_server->type) &&
+             if ((cl->fd >= 0) && (host_server) &&
                  ((host_server->type & ECORE_CON_TYPE) == ECORE_CON_REMOTE_CORK))
                {
                   int state = 1;
