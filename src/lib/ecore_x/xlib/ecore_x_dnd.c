Enlightenment/efl

commit 45a2296218b0f29781265394f2a97aa1cd167fea

Fix memleak reported by Coverity and replace an XFree call
NB: Fixes Coverity CID1039276

Signed-off-by: Chris Michael <cp.michael@samsung.com>

src/lib/ecore_x/xlib/ecore_x_dnd.c
@@ -228,7 +228,10 @@ ecore_x_dnd_type_isset(Ecore_X_Window win,
    LOGFN(__FILE__, __LINE__, __FUNCTION__);
    if (!ecore_x_window_prop_property_get(win, ECORE_X_ATOM_XDND_TYPE_LIST,
                                          XA_ATOM, 32, &data, &num))
-     return ret;
+     {
+        if (data) free(data);
+        return ret;
+     }
 
    atom = ecore_x_atom_get(type);
    atoms = (Ecore_X_Atom *)data;
@@ -242,7 +245,7 @@ ecore_x_dnd_type_isset(Ecore_X_Window win,
           }
      }
 
-   XFree(data);
+   if (data) free(data);
    return ret;
 }
 

commit 7a02b1057324cdc8665e0672369d624a4cf2c49a

ecore_x: No need to free NULL value.
NB: This reintroduces the Coverity Resouce Leak detected with CID1039276

Summary:
When ecore_x_window_prop_property_get returns 0, data is already NULL hence
no need to free NULL value. This fault was introduced in 45a2296218b0f297
while fixing coverity defects.

Reviewers: seoz, cedric, raster, devilhorns

Subscribers: cedric, seoz

Differential Revision: https://phab.enlightenment.org/D1473

src/lib/ecore_x/xlib/ecore_x_dnd.c
@@ -230,10 +230,7 @@ ecore_x_dnd_type_isset(Ecore_X_Window win,
    LOGFN(__FILE__, __LINE__, __FUNCTION__);
    if (!ecore_x_window_prop_property_get(win, ECORE_X_ATOM_XDND_TYPE_LIST,
                                          XA_ATOM, 32, &data, &num))
-     {
-        if (data) free(data);
-        return ret;
-     }
+     return ret;
 
    atom = ecore_x_atom_get(type);
    atoms = (Ecore_X_Atom *)data;