c59546a100101d5d59361162c7464a7b12b559b4
Cedric BAIL <cedric@osg.samsung.com>
2014-10-08 17:08:02 +0200

    evas: let's fix all Tasn borkage.

--- src/examples/evas/evas-3d-pick.c
+++ src/examples/evas/evas-3d-pick.c
@@ -376,7 +376,6 @@ main(void)
 
    /* Add evas objects. */
    background = eo_add(EVAS_RECTANGLE_CLASS, evas);
-   eo_unref(background);
    eo_do(background,
          evas_obj_color_set(0, 0, 0, 255),
          evas_obj_size_set(WIDTH, HEIGHT),
