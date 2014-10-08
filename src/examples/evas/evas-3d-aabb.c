1a83fc51a48a00074e4d8554cb75462f59c32cc1
Tom Hacohen <tom@stosb.com>
2014-10-08 09:14:48 +0100

    Evas examples: Fix 3d aabb example.
    This fixes evas-3d-aabb.c after the changes to eo_add.
    
    Fixes T1719.

--- src/examples/evas/evas-3d-aabb.c
+++ src/examples/evas/evas-3d-aabb.c
@@ -285,7 +285,6 @@ main(void)
          evas_3d_scene_size_set(WIDTH, HEIGHT));
 
    background = eo_add(EVAS_RECTANGLE_CLASS, evas);
-   eo_unref(background);
    eo_do(background,
          evas_obj_color_set(0, 0, 0, 255),
          evas_obj_size_set(WIDTH, HEIGHT),
