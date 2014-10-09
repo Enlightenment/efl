8862fc38b451fb8a598876111871336fe0749387
Cedric BAIL <cedric@osg.samsung.com>
2014-10-09 12:20:53 +0200

    evas: GL_X11 context need to always be with alpha or it will fail to change.
    @fix

--- src/modules/evas/engines/gl_x11/evas_engine.c
+++ src/modules/evas/engines/gl_x11/evas_engine.c
@@ -938,6 +938,7 @@ eng_setup(Evas *eo_e, void *in)
                     {
                        inc = 1;
                        eng_window_free(re->win);
+                       re->win->gl_context->references++;
                        gl_wins--;
                     }
                   re->w = e->output.w;
