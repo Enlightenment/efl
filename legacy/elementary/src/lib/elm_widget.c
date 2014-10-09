f7686dfd0eda94a70c3bd14811b313486291d79f
Lukasz Stanislawski <l.stanislaws@samsung.com>
2014-10-09 11:37:35 +0200

    atspi: add text tags filtering

--- legacy/elementary/src/lib/elm_widget.c
+++ legacy/elementary/src/lib/elm_widget.c
@@ -5558,6 +5558,9 @@ _elm_widget_elm_interface_atspi_accessible_name_get(Eo *obj EINA_UNUSED, Elm_Wid
 {
    const char *ret;
    ret = elm_object_text_get(obj);
+   if (!ret) return NULL;
+
+   ret = _elm_util_mkup_to_text(ret);
    return ret ? strdup(ret) : NULL;
 }
 
