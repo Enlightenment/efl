f4ecdc3b8d02332e468234ad931f31c67de8529b
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2014-10-10 12:15:36 +0200

    eeze: Add documentation for Eeze_Version structure

--- src/lib/eeze/Eeze.h
+++ src/lib/eeze/Eeze.h
@@ -289,13 +289,16 @@ typedef struct Eeze_Udev_Watch Eeze_Udev_Watch;
 
 #define EEZE_VERSION_MAJOR EFL_VERSION_MAJOR
 #define EEZE_VERSION_MINOR EFL_VERSION_MINOR
-
+  /**
+   * @typedef Eeeze_Version
+   * Represents the current version of Eeze
+   */
    typedef struct _Eeze_Version
      {
-        int major;
-        int minor;
-        int micro;
-        int revision;
+        int major; /** < major (binary or source incompatible changes) */
+        int minor; /** < minor (new features, bugfixes, major improvements version) */
+        int micro; /** < micro (bugfix, internal improvements, no new features version) */
+        int revision; /** < git revision (0 if a proper release or the git revision number Eeze is built from) */
      } Eeze_Version;
 
    EAPI extern Eeze_Version *eeze_version;
