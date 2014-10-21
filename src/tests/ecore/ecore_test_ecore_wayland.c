46d3d38e3cc59ee7ed731a26040306a9fb95472b
Srivardhan Hebbar <sri.hebbar@samsung.com>
2014-10-21 08:34:28 -0400

    ecore_wayland: Added test case to check ecore_wl_init by passing socket name.
    Summary:
    Added a test case for ecore_wayland to test ecore_wl_init by passing the socket name. Wayland display is created and a socket is added to the display, then this socket is passed to ecore_wl_init to connect. It should successfully connect. Then
    ecore_wl_shutdown is called to verify if it closes.
    
    Signed-off-by: Srivardhan Hebbar <sri.hebbar@samsung.com>
    
    Reviewers: devilhorns
    
    Subscribers: cedric
    
    Differential Revision: https://phab.enlightenment.org/D1560

--- src/tests/ecore/ecore_test_ecore_wayland.c
+++ src/tests/ecore/ecore_test_ecore_wayland.c
@@ -8,8 +8,42 @@
 #include <Ecore_Wayland.h>
 
 #include "ecore_suite.h"
+#include "wayland-server.h"
 
 #define MAX_ITER 15
+
+static char test_socket[] = "test1";
+
+START_TEST(ecore_test_ecore_wl_init_name)
+{
+   struct wl_display *test_display = NULL;
+   int ret = 0, i, j;
+
+   test_display = wl_display_create();
+   fprintf(stderr, "Creating display\n");
+   fail_if(test_display == NULL);
+
+   ret = wl_display_add_socket(test_display, test_socket);
+   fprintf(stderr, "Connecting socket to display\n");
+   fail_if(ret != 0);
+
+   for (i = 1; i <= MAX_ITER; i++)
+     {
+        ret = ecore_wl_init(test_socket);
+        fprintf(stderr, "Created %d ecore wayland instance.\n", i);
+        fail_if(ret != i);
+     }
+
+   for (j = MAX_ITER - 1; j >= 0; j--)
+     {
+        ret = ecore_wl_shutdown();
+        fprintf(stderr, "Deleted %d ecore wayland instance.\n", MAX_ITER - j);
+        fail_if(ret != j);
+     }
+
+   wl_display_destroy(test_display);
+}
+END_TEST
 
 START_TEST(ecore_test_ecore_wl_init)
 {
@@ -34,4 +68,5 @@ END_TEST
 void ecore_test_ecore_wayland(TCase *tc)
 {
    tcase_add_test(tc, ecore_test_ecore_wl_init);
+   tcase_add_test(tc, ecore_test_ecore_wl_init_name);
 }
