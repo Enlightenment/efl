e43e1a94bfcbf912c583afe54e8c527fefd1b790
Felipe Magno de Almeida <felipe@expertisesolutions.com.br>
2014-10-23 19:04:57 -0200

    Fixed wrongful merge

--- src/bindings/eina_cxx/eina_list.hh
+++ src/bindings/eina_cxx/eina_list.hh
@@ -449,8 +449,6 @@ public:
 
   typedef typename _base_type::native_handle_type native_handle_type;
 
-  range_list& operator=(range_list&& other) = default;
-
   using _base_type::_base_type;
   using _base_type::size;
   using _base_type::empty;
