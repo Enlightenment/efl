cb534366f347e324bed9f3206c32383dd18a3f65
Jean-Philippe Andre <jp.andre@samsung.com>
2014-10-24 20:48:08 +0900

    Evas GL: Pass calls to getProcAddress down to the engines
    Later we need to add a whitelist of supported functions.

--- src/modules/evas/engines/gl_generic/evas_engine.c
+++ src/modules/evas/engines/gl_generic/evas_engine.c
@@ -1202,10 +1202,15 @@ eng_gl_string_query(void *data, int name)
    return evgl_string_query(name);
 }
 
-// Need to deprecate this function..
 static void *
-eng_gl_proc_address_get(void *data EINA_UNUSED, const char *name EINA_UNUSED)
+eng_gl_proc_address_get(void *data, const char *name)
 {
+   Render_Engine_GL_Generic *re = data;
+   EVGLINIT(re, NULL);
+
+   if (re->evgl_funcs && re->evgl_funcs->proc_address_get)
+     return re->evgl_funcs->proc_address_get(name);
+
    return NULL;
 }
 
