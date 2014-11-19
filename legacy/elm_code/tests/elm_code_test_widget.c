e3610e160267c07f1163cd070798f03c7f01dc7d
Andy Williams <andy@andywilliams.me>
2014-11-19 23:20:23 +0000

    Fix a test missed in move to mem-mapping

--- legacy/elm_code/tests/elm_code_test_widget.c
+++ legacy/elm_code/tests/elm_code_test_widget.c
@@ -4,9 +4,9 @@
 
 #include "elm_code_suite.h"
 
-static void _assert_cell_type(Evas_Textgrid_Cell cell, Elm_Code_Token_Type type)
+static void _assert_cell_type(Evas_Textgrid_Cell cell, Elm_Code_Token_Type type, int id)
 {
-   ck_assert(cell.fg == type);
+   ck_assert_msg(cell.fg == type, "Wrong type for cell %d", id);
 }
 
 START_TEST (elm_code_widget_token_render_simple_test)
@@ -22,18 +22,18 @@ START_TEST (elm_code_widget_token_render_simple_test)
    file = elm_code_file_new(code);
    elm_code_file_line_append(file, "some \"test content\", 45", 23);
    line = elm_code_file_line_get(file, 1);
-   length = strlen(line->content);
+   length = line->length;
 
-   elm_code_file_line_token_add(file, 1, 6+1, 18+1, ELM_CODE_TOKEN_TYPE_COMMENT);
-   elm_code_file_line_token_add(file, 1, 22+1, 23+1, ELM_CODE_TOKEN_TYPE_COMMENT);
+   elm_code_file_line_token_add(file, 1, 6+1, 17+1, ELM_CODE_TOKEN_TYPE_COMMENT);
+   elm_code_file_line_token_add(file, 1, 21+1, 22+1, ELM_CODE_TOKEN_TYPE_COMMENT);
 
-   elm_code_widget_fill_line_tokens(cells, length, line);
-   _assert_cell_type(cells[1], ELM_CODE_TOKEN_TYPE_DEFAULT);
-   _assert_cell_type(cells[4], ELM_CODE_TOKEN_TYPE_DEFAULT);
-   _assert_cell_type(cells[6], ELM_CODE_TOKEN_TYPE_DEFAULT);
-   _assert_cell_type(cells[16], ELM_CODE_TOKEN_TYPE_COMMENT);
-   _assert_cell_type(cells[20], ELM_CODE_TOKEN_TYPE_DEFAULT);
-   _assert_cell_type(cells[23], ELM_CODE_TOKEN_TYPE_COMMENT);
+   elm_code_widget_fill_line_tokens(cells, length+1, line);
+   _assert_cell_type(cells[1], ELM_CODE_TOKEN_TYPE_DEFAULT, 1);
+   _assert_cell_type(cells[4], ELM_CODE_TOKEN_TYPE_DEFAULT, 4);
+   _assert_cell_type(cells[6], ELM_CODE_TOKEN_TYPE_DEFAULT, 6);
+   _assert_cell_type(cells[16], ELM_CODE_TOKEN_TYPE_COMMENT, 16);
+   _assert_cell_type(cells[20], ELM_CODE_TOKEN_TYPE_DEFAULT, 20);
+   _assert_cell_type(cells[23], ELM_CODE_TOKEN_TYPE_COMMENT, 23);
 
    elm_code_free(code);
 }
