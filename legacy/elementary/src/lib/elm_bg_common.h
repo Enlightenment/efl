0b820975b6df85c7adff8c77ac9a31772e9bead6
Daniel Juyung Seo <seojuyung2@gmail.com>
2014-11-20 23:08:57 +0900

    actionslider, app, bg: Fix and clean up doxygen documentation.

--- legacy/elementary/src/lib/elm_bg_common.h
+++ legacy/elementary/src/lib/elm_bg_common.h
@@ -1,11 +1,15 @@
 /**
+ * @addtogroup Bg
+ *
+ * @{
+ */
+
+/**
  * Identifiers on how a background widget is to display its image --
  * if it was set to use an image file.
  *
  * @see elm_bg_option_set()
  * @see elm_bg_option_get()
- *
- * @ingroup Bg
  */
 typedef enum
 {
@@ -16,3 +20,7 @@ typedef enum
    ELM_BG_OPTION_LAST /**< sentinel value, also used to indicate errors */
 } Elm_Bg_Option;
 
+
+/**
+ * @}
+ */
