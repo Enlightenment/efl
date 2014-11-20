dcfce03791889e71fdef283e60cb1d874b9511c9
Jaeun Choi <jaeun12.choi@samsung.com>
2014-11-20 14:56:36 +0900

    Evas masking: Implement support for polygon draw (SW)

--- src/lib/evas/common/evas_polygon.h
+++ src/lib/evas/common/evas_polygon.h
@@ -8,7 +8,7 @@ EAPI RGBA_Polygon_Point *evas_common_polygon_point_add    (RGBA_Polygon_Point *p
 EAPI RGBA_Polygon_Point *evas_common_polygon_points_clear (RGBA_Polygon_Point *points);
 EAPI void                evas_common_polygon_draw         (RGBA_Image *dst, RGBA_Draw_Context *dc, RGBA_Polygon_Point *points, int x, int y);
 
-EAPI void                evas_common_polygon_rgba_draw    (RGBA_Image *dst, int ext_x, int ext_y, int ext_w, int ext_h, DATA32 col, int render_op, RGBA_Polygon_Point *points, int x, int y);
+EAPI void                evas_common_polygon_rgba_draw    (RGBA_Image *dst, int ext_x, int ext_y, int ext_w, int ext_h, DATA32 col, int render_op, RGBA_Polygon_Point *points, int x, int y, RGBA_Image *mask_ie, int mask_x, int mask_y);
 
 #endif /* _EVAS_POLYGON_H */
 
