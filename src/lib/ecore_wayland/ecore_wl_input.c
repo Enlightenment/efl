9be84a1ce7518389329219769fbdd038514ee6f0
vivek <vivek.ellur@samsung.com>
2014-12-01 10:00:19 -0500

    ecore-wayland: Check if system has mouse in ecore_wl_input
    Summary:
    Added a condition to check if system has mouse before setting
    the cursor on surface.
    
    @fix
    
    Signed-off-by: vivek <vivek.ellur@samsung.com>
    
    Reviewers: devilhorns
    
    Subscribers: cedric
    
    Differential Revision: https://phab.enlightenment.org/D1707

--- src/lib/ecore_wayland/ecore_wl_input.c
+++ src/lib/ecore_wayland/ecore_wl_input.c
@@ -919,8 +919,9 @@ _ecore_wl_input_cb_pointer_enter(void *data, struct wl_pointer *pointer EINA_UNU
    input->display->serial = serial;
    input->pointer_enter_serial = serial;
 
+   if (input->pointer)
    /* The cursor on the surface is undefined until we set it */
-   ecore_wl_input_cursor_from_name_set(input, "left_ptr");
+     ecore_wl_input_cursor_from_name_set(input, "left_ptr");
 
    if ((win = ecore_wl_window_surface_find(surface)))
      {
