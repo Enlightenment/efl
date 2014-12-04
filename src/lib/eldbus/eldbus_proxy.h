14332f5d0dd503c10c284357a9b9183567644c4f
Chris Michael <cp.michael@samsung.com>
2014-12-04 09:45:51 -0500

    eldbus: Add API function declaration for eldbus_proxy_send_and_block
    Summary: This adds a public facing API function to make dbus calls
    which will block and wait for a reply. This is needed for a couple of
    use cases in our Wayland efforts (libinput, etc).
    
    @feature
    
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

--- src/lib/eldbus/eldbus_proxy.h
+++ src/lib/eldbus/eldbus_proxy.h
@@ -117,6 +117,20 @@ EAPI Eldbus_Message        *eldbus_proxy_method_call_new(Eldbus_Proxy *proxy, co
 EAPI Eldbus_Pending        *eldbus_proxy_send(Eldbus_Proxy *proxy, Eldbus_Message *msg, Eldbus_Message_Cb cb, const void *cb_data, double timeout) EINA_ARG_NONNULL(1, 2);
 
 /**
+ * @brief Send a message and block while waiting for the reply
+ *
+ * @param proxy the msg will be send in connection that proxy belongs
+ * @param msg message that will be send
+ * @param cb if msg is a method call a callback should be passed
+ * @param cb_data data passed to callback
+ *
+ * @return A Eldbus_Pending object on the sent message.
+ *
+ * @since 1.13
+ */
+EAPI Eldbus_Message        *eldbus_proxy_send_and_block(Eldbus_Proxy *proxy, Eldbus_Message *msg) EINA_ARG_NONNULL(1, 2);
+
+/**
  * @brief Call a method in proxy.
  * Send a method call to interface that proxy belong with data.
  *
