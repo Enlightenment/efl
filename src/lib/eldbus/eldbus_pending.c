dfb60c0bfe8cc9ed7a9ed84582e85c56bbdeb1e4
José Roberto de Souza <zehortigoza@gmail.com>
2014-12-05 01:04:20 -0200

    eldbus: Use the timeout parameter on eldbus_proxy_send_and_block()
    Sorry by bad I had forgot to use the new parameter.

--- src/lib/eldbus/eldbus_pending.c
+++ src/lib/eldbus/eldbus_pending.c
@@ -170,7 +170,7 @@ _eldbus_connection_send_and_block(Eldbus_Connection *conn, Eldbus_Message *msg, 
 
    dbus_error_init(&err);
    dbus_msg = dbus_connection_send_with_reply_and_block(conn->dbus_conn,
-                                               msg->dbus_msg, -1, &err);
+                                               msg->dbus_msg, timeout, &err);
    EINA_SAFETY_ON_TRUE_GOTO(dbus_error_is_set(&err), dbus_error_set);
    dbus_error_free(&err);
 
