a138716746cf97df32b219d72a5ba238d9503c71
Chris Michael <cp.michael@samsung.com>
2014-12-10 07:59:10 -0500

    ecore-drm: Fix formatting.
    Summary: No functional changes, just formatting fixes
    
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

--- src/lib/ecore_drm/ecore_drm_logind.h
+++ src/lib/ecore_drm/ecore_drm_logind.h
@@ -5,8 +5,8 @@
 #  include "config.h"
 # endif
 
-#ifdef HAVE_SYSTEMD_LOGIN
-# include <systemd/sd-login.h>
+# ifdef HAVE_SYSTEMD_LOGIN
+#  include <systemd/sd-login.h>
 
 Eina_Bool _ecore_drm_logind_connect(Ecore_Drm_Device *dev);
 void _ecore_drm_logind_disconnect(Ecore_Drm_Device *dev);
@@ -20,36 +20,36 @@ int _ecore_drm_dbus_device_take(uint32_t major, uint32_t minor, Ecore_Drm_Open_C
 int _ecore_drm_dbus_device_take_no_pending(uint32_t major, uint32_t minor, Eina_Bool *paused_out, double timeout);
 void _ecore_drm_dbus_device_release(uint32_t major, uint32_t minor);
 
-#else
+# else
 static inline Eina_Bool
 _ecore_drm_logind_connect(Ecore_Drm_Device *dev EINA_UNUSED)
 {
-      return EINA_FALSE;
+   return EINA_FALSE;
 }
 
 static inline void
 _ecore_drm_logind_disconnect(Ecore_Drm_Device *dev EINA_UNUSED)
 {
-      return;
+   return;
 }
 
 static inline Eina_Bool
 _ecore_drm_logind_device_open(const char *device EINA_UNUSED, Ecore_Drm_Open_Cb callback EINA_UNUSED, void *data EINA_UNUSED)
 {
-      return EINA_FALSE;
+   return EINA_FALSE;
 }
 
 static inline int
 _ecore_drm_logind_device_open_no_pending(const char *device EINA_UNUSED)
 {
-      return -1;
+   return -1;
 }
 
 static inline void
 _ecore_drm_logind_device_close(const char *device EINA_UNUSED)
 {
-      return;
+   return;
 }
-#endif
+# endif
 
 #endif
