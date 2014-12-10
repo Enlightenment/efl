ce473d88a127e624fe22046865b3aff161c66f77
Chris Michael <cp.michael@samsung.com>
2014-12-10 10:16:24 -0500

    ecore-drm: Add internal function to set the output size in the input device structure
    Summary: This is used to update all input devices as to the new output
    size so that input device events can get their coordinates transformed
    into output coords
    
    @fix
    
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

--- src/lib/ecore_drm/ecore_drm_inputs.c
+++ src/lib/ecore_drm/ecore_drm_inputs.c
@@ -194,6 +194,23 @@ const struct libinput_interface _input_interface =
    _cb_close_restricted,
 };
 
+void 
+_ecore_drm_inputs_update_output(Ecore_Drm_Device *dev, int w, int h)
+{
+   Ecore_Drm_Seat *seat;
+   Ecore_Drm_Evdev *edev;
+   Eina_List *l, *ll;
+
+   EINA_LIST_FOREACH(dev->seats, l, seat)
+     {
+        EINA_LIST_FOREACH(seat->devices, ll, edev)
+          {
+             edev->output.w = w;
+             edev->output.h = h;
+          }
+     }
+}
+
 /* public functions */
 EAPI Eina_Bool 
 ecore_drm_inputs_create(Ecore_Drm_Device *dev)
