82eb0a210ce8841e89757b80437592d6d7f2dbf3
Daniel Juyung Seo <seojuyung2@gmail.com>
2014-12-19 02:36:16 +0900

    image: Fix formatting for the latest commit 283988.

--- legacy/elementary/src/lib/elm_image.c
+++ legacy/elementary/src/lib/elm_image.c
@@ -184,8 +184,8 @@ _elm_image_internal_sizing_eval(Evas_Object *obj, Elm_Image_Data *sd)
         evas_object_move(sd->img, x, y);
         evas_object_image_fill_set(sd->img, 0, 0, w, h);
 
-        if (x < 0) w+=x;
-        if (y < 0) h+=y;
+        if (x < 0) w += x;
+        if (y < 0) h += y;
 
         evas_object_resize(sd->img, w, h);
      }
