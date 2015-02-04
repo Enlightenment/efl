cf9f64cfc6f5c7a14d315553dacd94c653a04f3d
Chris Michael <cp.michael@samsung.com>
2015-02-04 11:23:24 -0500

    ecore-x: Deprecate misspelled screensaver API function
    Summary: This API function is misspelled, so after speaking with
    Stefan, we've decided to deprecate this function add add a properly
    spelled one.
    
    @fix
    
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

--- src/lib/ecore_x/Ecore_X.h
+++ src/lib/ecore_x/Ecore_X.h
@@ -1986,10 +1986,11 @@ EAPI int                             ecore_x_screensaver_interval_get(void);
 EAPI void                            ecore_x_screensaver_event_listen_set(Eina_Bool on);
 EAPI Eina_Bool                       ecore_x_screensaver_custom_blanking_enable(void); /** @since 1.7 */
 EAPI Eina_Bool                       ecore_x_screensaver_custom_blanking_disable(void); /** @since 1.7 */
-EAPI void                            ecore_x_screensaver_supend(void); /** @since 1.11 */
+EINA_DEPRECATED EAPI void            ecore_x_screensaver_supend(void); /** @since 1.11 */
 EAPI void                            ecore_x_screensaver_resume(void); /** @since 1.11 */
 EAPI void                            ecore_x_screensaver_reset(void); /** @since 1.11 */
 EAPI void                            ecore_x_screensaver_activate(void); /** @since 1.11 */
+EAPI void                            ecore_x_screensaver_suspend(void); /** @since 1.13 */
 
 /* FIXME: these funcs need categorising */
 
