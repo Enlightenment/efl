70d9ed501bd041a5d514f04917c4828eeba7c09d
Minkyoung Kim <mer.kim@samsung.com>
2015-02-23 21:31:41 +0900

    Evas GL:Add evasgl extension macro '_EVASGL_EXT_DRVNAME_PRIVATE'.
    Summary:
    To distinguish supported extension name from not supported.
    This patch can be solution to the problem, glGetString() returns non-supported extention name.
    
    Test Plan: Local tests
    
    Reviewers: raster, jpeg, Hermet, cedric
    
    Subscribers: cedric, spacegrapher, wonsik
    
    Differential Revision: https://phab.enlightenment.org/D1981
    
    Signed-off-by: Jean-Philippe Andre <jp.andre@samsung.com>

--- src/modules/evas/engines/gl_common/evas_gl_api_ext.h
+++ src/modules/evas/engines/gl_common/evas_gl_api_ext.h
@@ -24,6 +24,7 @@
 #define _EVASGL_EXT_BEGIN(name)
 #define _EVASGL_EXT_END()
 #define _EVASGL_EXT_DRVNAME(name)
+#define _EVASGL_EXT_DRVNAME_PRIVATE(name)
 #define _EVASGL_EXT_DRVNAME_DESKTOP(deskname)
 #define _EVASGL_EXT_FUNCTION_BEGIN(ret, name, param) extern ret (*gl_ext_sym_##name) param;
 #define _EVASGL_EXT_FUNCTION_END()
@@ -39,6 +40,7 @@
 #undef _EVASGL_EXT_BEGIN
 #undef _EVASGL_EXT_END
 #undef _EVASGL_EXT_DRVNAME
+#undef _EVASGL_EXT_DRVNAME_PRIVATE
 #undef _EVASGL_EXT_DRVNAME_DESKTOP
 #undef _EVASGL_EXT_FUNCTION_BEGIN
 #undef _EVASGL_EXT_FUNCTION_END
@@ -58,6 +60,7 @@
 #define _EVASGL_EXT_BEGIN(name) extern int _gl_ext_support_##name;
 #define _EVASGL_EXT_END()
 #define _EVASGL_EXT_DRVNAME(name)
+#define _EVASGL_EXT_DRVNAME_PRIVATE(name)
 #define _EVASGL_EXT_DRVNAME_DESKTOP(deskname)
 #define _EVASGL_EXT_FUNCTION_BEGIN(ret, name, param)
 #define _EVASGL_EXT_FUNCTION_END()
@@ -73,6 +76,7 @@
 #undef _EVASGL_EXT_BEGIN
 #undef _EVASGL_EXT_END
 #undef _EVASGL_EXT_DRVNAME
+#undef _EVASGL_EXT_DRVNAME_PRIVATE
 #undef _EVASGL_EXT_DRVNAME_DESKTOP
 #undef _EVASGL_EXT_FUNCTION_BEGIN
 #undef _EVASGL_EXT_FUNCTION_END
