ae9670c137fc813d9c18b768642e6e4c4e056416
Chris Michael <cp.michael@samsung.com>
2015-03-02 10:46:38 -0500

    ecore-imf: Add check for environment variable to enable show/hide of input panel
    Summary: Allow ecore_imf input panel to be shown/hidden via the master
    'enabled' switch or via an environment variable. This is required for
    Tizen in order that 3rd party apps can still show input_panel even if
    master switch is disabled (as per discussion with Carsten).
    
    @fix
    
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

--- src/lib/ecore_imf/ecore_imf_context.c
+++ src/lib/ecore_imf/ecore_imf_context.c
@@ -878,7 +878,8 @@ ecore_imf_context_input_panel_show(Ecore_IMF_Context *ctx)
      }
 
    show_req_ctx = ctx;
-   if (ctx->input_panel_enabled)
+   if ((ctx->input_panel_enabled) || 
+       (getenv("ECORE_IMF_INPUT_PANEL_ENABLED")))
      {
         if (ctx->klass->show) ctx->klass->show(ctx);
      }
@@ -894,7 +895,8 @@ ecore_imf_context_input_panel_hide(Ecore_IMF_Context *ctx)
         return;
      }
 
-   if (ctx->input_panel_enabled)
+   if ((ctx->input_panel_enabled) || 
+       (getenv("ECORE_IMF_INPUT_PANEL_ENABLED")))
      {
         if (ctx->klass->hide) ctx->klass->hide(ctx);
      }
