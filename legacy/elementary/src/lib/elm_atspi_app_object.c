c49d674d81bb1b9c6a73cd62ff21bd96ac5acff5
Daniel Juyung Seo <seojuyung2@gmail.com>
2015-03-10 22:51:17 +0900

    atspi: Remove unnecessary null check.
    win could not be null due to EINA_LIST_FOREACH design.

--- legacy/elementary/src/lib/elm_atspi_app_object.c
+++ legacy/elementary/src/lib/elm_atspi_app_object.c
@@ -33,7 +33,6 @@ _elm_atspi_app_object_elm_interface_atspi_accessible_children_get(Eo *obj EINA_U
 
    EINA_LIST_FOREACH(_elm_win_list, l, win)
      {
-        if (!win) continue;
         if (eo_isa(win, ELM_INTERFACE_ATSPI_ACCESSIBLE_MIXIN))
           accs = eina_list_append(accs, win);
      }
