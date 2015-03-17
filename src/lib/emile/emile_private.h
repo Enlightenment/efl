6e59828f74290432166de13731728d547619ac19
Cedric BAIL <cedric@osg.samsung.com>
2015-03-17 08:50:43 +0100

    emile: cleanup private header.

--- src/lib/emile/emile_private.h
+++ src/lib/emile/emile_private.h
@@ -5,23 +5,23 @@ extern int _emile_log_dom_global;
 
 #ifdef ERR
 # undef ERR
-#endif /* ifdef ERR */
+#endif
 #define ERR(...)  EINA_LOG_DOM_ERR(_emile_log_dom_global, __VA_ARGS__)
 #ifdef DBG
 # undef DBG
-#endif /* ifdef DBG */
+#endif
 #define DBG(...)  EINA_LOG_DOM_DBG(_emile_log_dom_global, __VA_ARGS__)
 #ifdef INF
 # undef INF
-#endif /* ifdef INF */
+#endif
 #define INF(...)  EINA_LOG_DOM_INFO(_emile_log_dom_global, __VA_ARGS__)
 #ifdef WRN
 # undef WRN
-#endif /* ifdef WRN */
+#endif
 #define WRN(...)  EINA_LOG_DOM_WARN(_emile_log_dom_global, __VA_ARGS__)
 #ifdef CRI
 # undef CRI
-#endif /* ifdef CRI */
+#endif
 #define CRI(...) EINA_LOG_DOM_CRIT(_emile_log_dom_global, __VA_ARGS__)
 
 typedef enum
@@ -43,4 +43,4 @@ emile_pbkdf2_sha1(const char *key,
                   unsigned char *res,
                   unsigned int res_len);
 
-#endif /* EMILE_PRIVATE_H_ */
+#endif
