b72fdea48895bf4b5f53e877e62025ec6c68c834
Andy Williams <andy@andywilliams.me>
2015-03-27 21:45:43 +0000

    elm_code file: test windows with new eina fixes
    Should pass from eina 1.13.99

--- legacy/elm_code/src/tests/elm_code_file_test_load.c
+++ legacy/elm_code/src/tests/elm_code_file_test_load.c
@@ -52,6 +52,21 @@ START_TEST (elm_code_file_load_blank_lines)
 }
 END_TEST
 
+START_TEST (elm_code_file_load_windows)
+{
+   char *path = TESTS_DIR "testfile-windows.txt";
+   Elm_Code_File *file;
+   Elm_Code *code;
+
+   code = elm_code_create();
+   file = elm_code_file_open(code, path);
+
+   ck_assert_uint_eq(4, elm_code_file_lines_get(file));
+   elm_code_file_close(file);
+   elm_code_free(code);
+}
+END_TEST
+
 static void _assert_line_content_eq(const char *content, Elm_Code_Line *line)
 {
    int length;
@@ -123,6 +138,7 @@ void elm_code_file_test_load(TCase *tc)
    tcase_add_test(tc, elm_code_file_load);
    tcase_add_test(tc, elm_code_file_load_lines);
    tcase_add_test(tc, elm_code_file_load_blank_lines);
+   tcase_add_test(tc, elm_code_file_load_windows);
    tcase_add_test(tc, elm_code_file_load_content);
    tcase_add_test(tc, elm_code_file_line_ending_unix);
    tcase_add_test(tc, elm_code_file_line_ending_windows);
