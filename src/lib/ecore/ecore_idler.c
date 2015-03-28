bc9265f16d5a69c47fce3f2162a080ca5ecaf424
ChunEon Park <chuneon.park@samsung.com>
2015-03-28 15:43:50 +0900

    ecore/idler - simpler checking.

--- src/lib/ecore/ecore_idler.c
+++ src/lib/ecore/ecore_idler.c
@@ -87,7 +87,7 @@ static void *
 _ecore_idler_del(Ecore_Idler *obj)
 {
    Ecore_Idler_Data *idler = eo_data_scope_get(obj, MY_CLASS);
-   EINA_SAFETY_ON_NULL_RETURN_VAL(idler, NULL);
+   if (!idler) return NULL;
    EINA_SAFETY_ON_TRUE_RETURN_VAL(idler->delete_me, NULL);
    idler->delete_me = 1;
    idlers_delete_me = 1;
