0510ea90bd8b25cff1e21eea48ec5efd13238993
Cedric BAIL <cedric@osg.samsung.com>
2015-04-03 16:14:58 +0200

    ector: add initial interface for Surface and Renderer.

--- src/lib/ector/Ector.h
+++ src/lib/ector/Ector.h
@@ -3,6 +3,7 @@
 
 #include <Eina.h>
 #include <Eo.h>
+#include <Evas.h>
 
 #ifdef EAPI
 # undef EAPI
@@ -103,6 +104,55 @@ typedef Eo Ector_Surface;
  */
 typedef Eo Ector_Renderer;
 
+/**
+ * @typedef Ector_Colorspace
+ * The definiton of colorspace.
+ */
+typedef Evas_Colorspace Ector_Colorspace;
+
+/**
+ * Raster operations at pixel level
+ */
+typedef enum _Ector_Rop
+{
+  ECTOR_ROP_BLEND, /**< D = S + D(1 - Sa) */
+  ECTOR_ROP_COPY, /**< D = S */
+  ECTOR_ROP_LAST
+} Ector_Rop;
+
+/**
+ * Quality values
+ */
+typedef enum _Ector_Quality
+{
+  ECTOR_QUALITY_BEST, /**< Best quality */
+  ECTOR_QUALITY_GOOD, /**< Good quality */
+  ECTOR_QUALITY_FAST, /**< Lower quality, fastest */
+  ECTOR_QUALITY_LAST
+} Ector_Quality;
+
+/**
+ * Priorities
+ */
+typedef enum _Ector_Priority
+{
+  ECTOR_PRIORITY_NONE = 0,
+  ECTOR_PRIORITY_MARGINAL = 64,
+  ECTOR_PRIORITY_SECONDARY = 128,
+  ECTOR_PRIORITY_PRIMARY = 256,
+} Ector_Priority;
+
+/**
+ * What kind of update is being pushed
+ */
+typedef enum _Ector_Update_Type
+{
+  ECTOR_UPDATE_BACKGROUND = 1, /* All the previous state in that area is reset to the new updated profile */
+  ECTOR_UPDATE_EMPTY = 2, /* Pushing empty area (no visible pixels at all, no need to read this surface to render it) */
+  ECTOR_UPDATE_ALPHA = 4, /* Pushing some transparent pixels (this impact the under layer and will require to read back the surface where this surface is blitted) */
+  ECTOR_UPDATE_OPAQUE = 8 /* Pushing some opaque pixels (this means that their is no need to read the under layer when blitting this surface) */
+} Ector_Update_Type;
+
 #ifdef EFL_BETA_API_SUPPORT
 
 /**
@@ -120,6 +170,9 @@ EAPI int ector_init(void);
  * @see ector_init()
  */
 EAPI int ector_shutdown(void);
+
+#include "ector_surface.h"
+#include "ector_renderer.h"
 
 #endif
 
---
+++ src/lib/ector/ector_renderer.h
@@ -0,0 +1,6 @@
+#ifndef ECTOR_RENDERER_H
+#define ECTOR_RENDERER_H
+
+#include "ector_renderer.eo.h"
+
+#endif
