ea9330bfe8a251423c85a2e6db2fb6b50a072c1d
Cedric BAIL <cedric@osg.samsung.com>
2015-04-03 16:14:46 +0200

    evas: declare internal evas_vg helper.

--- src/lib/evas/canvas/evas_vg_private.h
+++ src/lib/evas/canvas/evas_vg_private.h
@@ -7,4 +7,8 @@ struct _Evas_VG_Container_Data
    Eina_List *children;
 };
 
+Eina_Bool
+evas_vg_path_dup(Evas_VG_Path_Command **out_cmd, double **out_pts,
+                 const Evas_VG_Path_Command *in_cmd, const double *in_pts);
+
 #endif
