2ca2a43916087e801b3f7891fdc421106cfea4c6
Cedric BAIL <cedric@osg.samsung.com>
2015-04-03 16:23:18 +0200

    ecore_evas: fix with new naming.

--- src/bin/ecore_evas/ecore_evas_svg.c
+++ src/bin/ecore_evas/ecore_evas_svg.c
@@ -157,13 +157,13 @@ main(int argc, char *argv[])
 
 
    r = eo_add(EVAS_RECTANGLE_CLASS, e,
-              evas_obj_color_set(color[0], color[1], color[2], alpha ? 0 : 255),
-              evas_obj_visibility_set(EINA_TRUE));
+              efl_gfx_color_set(color[0], color[1], color[2], alpha ? 0 : 255),
+              efl_gfx_visibility_set(EINA_TRUE));
    ecore_evas_object_associate(ee, r, ECORE_EVAS_OBJECT_ASSOCIATE_BASE);
 
    vg = eo_add(EVAS_VG_CLASS, e,
                efl_file_set(argv[arg_index], NULL),
-               evas_obj_visibility_set(EINA_TRUE));
+               efl_gfx_visibility_set(EINA_TRUE));
    ecore_evas_object_associate(ee, vg, ECORE_EVAS_OBJECT_ASSOCIATE_BASE);
 
    if (display)
