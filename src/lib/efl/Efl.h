dd587216133fd3938c44cc6c77ef525a5e077341
Subhransu Sekhar Mohanty <sub.mohanty@samsung.com>
2015-04-03 16:30:35 +0200

    efl: add documentation for Efl.Gfx enums.

--- src/lib/efl/Efl.h
+++ src/lib/efl/Efl.h
@@ -35,85 +35,97 @@ extern "C"
 #endif /* ! _WIN32 */
 
 /**
- * Path command enum.
+ * These values determine how the points are interpreted in a stream of points.
  *
- * @since 1.13
- * @ingroup Efl_Gfx_Shape
+ * @since 1.14
  */
 typedef enum _Efl_Gfx_Path_Command
 {
-  EFL_GFX_PATH_COMMAND_TYPE_END = 0, /**< End of the stream of command */
-  EFL_GFX_PATH_COMMAND_TYPE_MOVE_TO, /**< A move command type */
-  EFL_GFX_PATH_COMMAND_TYPE_LINE_TO, /**< A line command type */
-  EFL_GFX_PATH_COMMAND_TYPE_CUBIC_TO, /**< A cubic command type */
-  EFL_GFX_PATH_COMMAND_TYPE_CLOSE, /**< A close command type */
+  EFL_GFX_PATH_COMMAND_TYPE_END = 0, /**< The end of stream , no more points to process. */
+  EFL_GFX_PATH_COMMAND_TYPE_MOVE_TO, /**< The next point is the start point of a sub path */
+  EFL_GFX_PATH_COMMAND_TYPE_LINE_TO, /**< The next point is used to draw a line from current point */
+  EFL_GFX_PATH_COMMAND_TYPE_CUBIC_TO, /**< The next three point is used to draw a cubic bezier curve from current point */
+  EFL_GFX_PATH_COMMAND_TYPE_CLOSE, /**< Close the curent subpath by drawing a line between current point and the first point of current subpath */
   EFL_GFX_PATH_COMMAND_TYPE_LAST, /**< Not a valid command, but last one according to this version header */
 } Efl_Gfx_Path_Command;
 
 /**
  * Type describing dash
- * @since 1.13
+ *
+ * @see efl_gfx_shape_stroke_dash_set()
+ *
+ * @since 1.14
  */
 typedef struct _Efl_Gfx_Dash Efl_Gfx_Dash;
 struct _Efl_Gfx_Dash
 {
-   double length;
-   double gap;
+   double length; /**< dash drawing length */
+   double gap; /**< distance bettwen two dashes */
 };
 
 /**
- * Type defining how a line end.
- * @since 1.13
+ * These values determine how the end of opened sub-paths are rendered in a
+ * stroke.
+ *
+ * @see efl_gfx_shape_stroke_cap_set()
+ *
+ * @since 1.14
  */
 typedef enum _Efl_Gfx_Cap
 {
-  EFL_GFX_CAP_BUTT,
-  EFL_GFX_CAP_ROUND,
-  EFL_GFX_CAP_SQUARE,
-  EFL_GFX_CAP_LAST
+  EFL_GFX_CAP_BUTT = 0, /**< The end of lines is rendered as a full stop on the last point itself */
+  EFL_GFX_CAP_ROUND, /**< The end of lines is rendered as a half-circle around the last point */
+  EFL_GFX_CAP_SQUARE, /**< The end of lines is rendered as a square around the last point */
+  EFL_GFX_CAP_LAST /**< End of enum value */
 } Efl_Gfx_Cap;
 
 /**
- * Type defining how join between path are drawn.
- * @since 1.13
+ * These values determine how two joining lines are rendered in a stroker.
+ *
+ * @see efl_gfx_shape_stroke_join_set()
+ *
+ * @since 1.14
  */
 typedef enum _Efl_Gfx_Join
 {
-  EFL_GFX_JOIN_MITER,
-  EFL_GFX_JOIN_ROUND,
-  EFL_GFX_JOIN_BEVEL,
-  EFL_GFX_JOIN_LAST
+  EFL_GFX_JOIN_MITER = 0, /**< Used to render rounded line joins. Circular arcs are used to join two lines smoothly. */
+  EFL_GFX_JOIN_ROUND, /**< Used to render beveled line joins. The outer corner of the joined lines is filled by enclosing the triangular region of the corner with a straight line between the outer corners of each stroke. */
+  EFL_GFX_JOIN_BEVEL, /**< Used to render mitered line joins. The intersection of the strokes is clipped at a line perpendicular to the bisector of the angle between the strokes, at the distance from the intersection of the segments equal to the product of the miter limit value and the border radius.  This prevents long spikes being created. */
+  EFL_GFX_JOIN_LAST /**< End of enum value */
 } Efl_Gfx_Join;
 
 /**
  * Type defining gradient stop.
- * @since 1.13
+ * @since 1.14
  */
 typedef struct _Efl_Gfx_Gradient_Stop Efl_Gfx_Gradient_Stop;
 struct _Efl_Gfx_Gradient_Stop
 {
-   double offset;
-   int r;
-   int g;
-   int b;
-   int a;
+   double offset; /**< The location of the gradient stop within the gradient vector*/
+   int r; /**< The component R color of the gradient stop */
+   int g; /**< The component G color of the gradient stop */
+   int b; /**< The component B color of the graident stop */
+   int a; /**< The component A color of the graident stop */
 };
 
 /**
- * Type defining how the gradient spread after its limit.
- * @since 1.13
+ * Specifies how the area outside the gradient area should be filled.
+ *
+ * @see efl_gfx_gradient_spread_set()
+ *
+ * @since 1.14
  */
 typedef enum _Efl_Gfx_Gradient_Spread
 {
-  EFL_GFX_GRADIENT_SPREAD_PAD,
-  EFL_GFX_GRADIENT_SPREAD_REFLECT,
-  EFL_GFX_GRADIENT_SPREAD_REPEAT,
-  EFL_GFX_GRADIENT_SPREAD_LAST
+  EFL_GFX_GRADIENT_SPREAD_PAD, /**< The area is filled with the closest stop color. This is the default. */
+  EFL_GFX_GRADIENT_SPREAD_REFLECT, /**< The gradient is reflected outside the gradient area. */
+  EFL_GFX_GRADIENT_SPREAD_REPEAT, /**< The gradient is repeated outside the gradient area. */
+  EFL_GFX_GRADIENT_SPREAD_LAST /**< End of enum value */
 } Efl_Gfx_Gradient_Spread;
 
 /**
  * Type defining how an image content get filled.
- * @since 1.13
+ * @since 1.14
  */
 typedef enum _Efl_Gfx_Fill_Spread
 {
