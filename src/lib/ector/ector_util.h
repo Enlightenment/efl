1d0ce5bc05fb6658f2ede82c20f62687a4dc8d3e
Subhransu Sekhar Mohanty <sub.mohanty@samsung.com>
2015-04-03 16:33:49 +0200

    ector: fix ector_color_multiply() function

--- src/lib/ector/ector_util.h
+++ src/lib/ector/ector_util.h
@@ -6,7 +6,7 @@ ector_color_multiply(unsigned int c1, unsigned int c2)
 {
    return ( ((((((c1) >> 16) & 0xff00) * (((c2) >> 16) & 0xff00)) + 0xff0000) & 0xff000000) +
             ((((((c1) >> 8) & 0xff00) * (((c2) >> 16) & 0xff)) + 0xff00) & 0xff0000) +
-            ((((((c1) & 0xff00) * ((c2) & 0xff00)) + 0xff00) >> 16) & 0xff00) +
+            ((((((c1) & 0xff00) * ((c2) & 0xff00)) + 0xff0000) >> 16) & 0xff00) +
             (((((c1) & 0xff) * ((c2) & 0xff)) + 0xff) >> 8) );
 }
 
