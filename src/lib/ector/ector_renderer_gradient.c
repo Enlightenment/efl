32de8d9e5f8d5a306eee10fbfb4a9d10ebcfcb75
Cedric BAIL <cedric@osg.samsung.com>
2015-04-03 16:30:35 +0200

    efl: move Efl.Gfx.Gradient_* to Efl.Gfx.Gradient.* much nicer for bindings.

--- src/lib/ector/ector_renderer_gradient.c
+++ src/lib/ector/ector_renderer_gradient.c
@@ -8,10 +8,10 @@
 #include "ector_private.h"
 
 static void
-_ector_renderer_generic_gradient_efl_gfx_gradient_stop_set(Eo *obj,
-                                                           Ector_Renderer_Generic_Gradient_Data *pd,
-                                                           const Efl_Gfx_Gradient_Stop *colors,
-                                                           unsigned int length)
+_ector_renderer_generic_gradient_efl_gfx_gradient_base_stop_set(Eo *obj EINA_UNUSED,
+                                                                Ector_Renderer_Generic_Gradient_Data *pd,
+                                                                const Efl_Gfx_Gradient_Stop *colors,
+                                                                unsigned int length)
 {
    pd->colors = realloc(pd->colors, length * sizeof(Efl_Gfx_Gradient_Stop));
    if (!pd->colors)
@@ -25,26 +25,26 @@ _ector_renderer_generic_gradient_efl_gfx_gradient_stop_set(Eo *obj,
 }
 
 static void
-_ector_renderer_generic_gradient_efl_gfx_gradient_stop_get(Eo *obj,
-                                                           Ector_Renderer_Generic_Gradient_Data *pd,
-                                                           const Efl_Gfx_Gradient_Stop **colors,
-                                                           unsigned int *length)
+_ector_renderer_generic_gradient_efl_gfx_gradient_base_stop_get(Eo *obj EINA_UNUSED,
+                                                                Ector_Renderer_Generic_Gradient_Data *pd,
+                                                                const Efl_Gfx_Gradient_Stop **colors,
+                                                                unsigned int *length)
 {
    if (colors) *colors = pd->colors;
    if (length) *length = pd->colors_count;
 }
 
 static void
-_ector_renderer_generic_gradient_efl_gfx_gradient_spread_set(Eo *obj,
-                                                             Ector_Renderer_Generic_Gradient_Data *pd,
-                                                             Efl_Gfx_Gradient_Spread s)
+_ector_renderer_generic_gradient_efl_gfx_gradient_base_spread_set(Eo *obj EINA_UNUSED,
+                                                                  Ector_Renderer_Generic_Gradient_Data *pd,
+                                                                  Efl_Gfx_Gradient_Spread s)
 {
    pd->s = s;
 }
 
 static Efl_Gfx_Gradient_Spread
-_ector_renderer_generic_gradient_efl_gfx_gradient_spread_get(Eo *obj,
-                                                             Ector_Renderer_Generic_Gradient_Data *pd)
+_ector_renderer_generic_gradient_efl_gfx_gradient_base_spread_get(Eo *obj EINA_UNUSED,
+                                                                  Ector_Renderer_Generic_Gradient_Data *pd)
 {
    return pd->s;
 }
--- src/lib/evas/canvas/evas_vg_gradient.c
+++ src/lib/evas/canvas/evas_vg_gradient.c
@@ -6,10 +6,10 @@
 #include <strings.h>
 
 static void
-_evas_vg_gradient_efl_gfx_gradient_stop_set(Eo *obj EINA_UNUSED,
-                                            Evas_VG_Gradient_Data *pd,
-                                            const Efl_Gfx_Gradient_Stop *colors,
-                                            unsigned int length)
+_evas_vg_gradient_efl_gfx_gradient_base_stop_set(Eo *obj EINA_UNUSED,
+                                                 Evas_VG_Gradient_Data *pd,
+                                                 const Efl_Gfx_Gradient_Stop *colors,
+                                                 unsigned int length)
 {
    pd->colors = realloc(pd->colors, length * sizeof(Efl_Gfx_Gradient_Stop));
    if (!pd->colors)
@@ -23,26 +23,26 @@ _evas_vg_gradient_efl_gfx_gradient_stop_set(Eo *obj EINA_UNUSED,
 }
 
 static void
-_evas_vg_gradient_efl_gfx_gradient_stop_get(Eo *obj EINA_UNUSED,
-                                            Evas_VG_Gradient_Data *pd,
-                                            const Efl_Gfx_Gradient_Stop **colors,
-                                            unsigned int *length)
+_evas_vg_gradient_efl_gfx_gradient_base_stop_get(Eo *obj EINA_UNUSED,
+                                                 Evas_VG_Gradient_Data *pd,
+                                                 const Efl_Gfx_Gradient_Stop **colors,
+                                                 unsigned int *length)
 {
    if (colors) *colors = pd->colors;
    if (length) *length = pd->colors_count;
 }
 
 static void
-_evas_vg_gradient_efl_gfx_gradient_spread_set(Eo *obj EINA_UNUSED,
-                                              Evas_VG_Gradient_Data *pd,
-                                              Efl_Gfx_Gradient_Spread s)
+_evas_vg_gradient_efl_gfx_gradient_base_spread_set(Eo *obj EINA_UNUSED,
+                                                   Evas_VG_Gradient_Data *pd,
+                                                   Efl_Gfx_Gradient_Spread s)
 {
    pd->s = s;
 }
 
 static Efl_Gfx_Gradient_Spread
-_evas_vg_gradient_efl_gfx_gradient_spread_get(Eo *obj EINA_UNUSED,
-                                              Evas_VG_Gradient_Data *pd)
+_evas_vg_gradient_efl_gfx_gradient_base_spread_get(Eo *obj EINA_UNUSED,
+                                                   Evas_VG_Gradient_Data *pd)
 {
    return pd->s;
 }
