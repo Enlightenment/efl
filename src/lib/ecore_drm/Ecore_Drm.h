1a38dc0819defdcd2938bf1a3da74c98ce086ebf
Chris Michael <cp.michael@samsung.com>
2015-04-07 11:18:18 -0400

    ecore-drm: Fix formatting
    Summary: NO functional changes, just formatting.
    
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

--- src/lib/ecore_drm/Ecore_Drm.h
+++ src/lib/ecore_drm/Ecore_Drm.h
@@ -5,26 +5,26 @@
 #  undef EAPI
 # endif
 
-#ifdef _MSC_VER
-# ifdef BUILDING_DLL
-#  define EAPI __declspec(dllexport)
-# else // ifdef BUILDING_DLL
-#  define EAPI __declspec(dllimport)
-# endif // ifdef BUILDING_DLL
-#else // ifdef _MSC_VER
-# ifdef __GNUC__
-#  if __GNUC__ >= 4
-#   define EAPI __attribute__ ((visibility("default")))
-#  else // if __GNUC__ >= 4
+# ifdef _MSC_VER
+#  ifdef BUILDING_DLL
+#   define EAPI __declspec(dllexport)
+#  else // ifdef BUILDING_DLL
+#   define EAPI __declspec(dllimport)
+#  endif // ifdef BUILDING_DLL
+# else // ifdef _MSC_VER
+#  ifdef __GNUC__
+#   if __GNUC__ >= 4
+#    define EAPI __attribute__ ((visibility("default")))
+#   else // if __GNUC__ >= 4
+#    define EAPI
+#   endif // if __GNUC__ >= 4
+#  else // ifdef __GNUC__
 #   define EAPI
-#  endif // if __GNUC__ >= 4
-# else // ifdef __GNUC__
-#  define EAPI
-# endif // ifdef __GNUC__
-#endif // ifdef _MSC_VER
+#  endif // ifdef __GNUC__
+# endif // ifdef _MSC_VER
 
-#include <Ecore.h>
-#include <Eeze.h>
+# include <Ecore.h>
+# include <Eeze.h>
 
 typedef enum _Ecore_Drm_Evdev_Capabilities
 {
