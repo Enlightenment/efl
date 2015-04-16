75dd60d62d90efde12e5067d08df816e13de203a
Daniel Kolesa <d.kolesa@osg.samsung.com>
2015-04-16 15:08:02 +0100

    elua lib: test elua_state_dirs_fill

--- src/tests/elua/elua_lib.c
+++ src/tests/elua/elua_lib.c
@@ -19,8 +19,17 @@ START_TEST(elua_api)
     st = elua_state_new("test");
     fail_if(!st);
 
-    elua_state_dirs_set(st, ELUA_CORE_DIR, ELUA_MODULES_DIR, ELUA_APPS_DIR);
+    /* test env vars */
+    setenv("ELUA_CORE_DIR", "foo", 1);
+    setenv("ELUA_MODULES_DIR", "bar", 1);
+    setenv("ELUA_APPS_DIR", "baz", 1);
+    elua_state_dirs_fill(st, EINA_FALSE);
+    fail_if(strcmp(elua_state_core_dir_get(st), "foo"));
+    fail_if(strcmp(elua_state_mod_dir_get(st), "bar"));
+    fail_if(strcmp(elua_state_apps_dir_get(st), "baz"));
 
+    /* now fill it properly */
+    elua_state_dirs_set(st, ELUA_CORE_DIR, ELUA_MODULES_DIR, ELUA_APPS_DIR);
     fail_if(strcmp(elua_state_core_dir_get(st), ELUA_CORE_DIR));
     fail_if(strcmp(elua_state_mod_dir_get(st), ELUA_MODULES_DIR));
     fail_if(strcmp(elua_state_apps_dir_get(st), ELUA_APPS_DIR));
