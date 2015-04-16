dbe34d803c37a1e9b85b787fba4f6e015c66c134
Daniel Kolesa <d.kolesa@samsung.com>
2015-04-16 10:55:27 +0100

    elua lib: add test suite

---
+++ src/tests/elua/elua_lib.c
@@ -0,0 +1,21 @@
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include<Eina.h>
+#include "Elua.h"
+#include "elua_suite.h"
+
+/*START_TEST(test_name)
+{
+}
+END_TEST*/
+
+void elua_lib_test(TCase *tc)
+{
+   //tcase_add_test(tc, test_name);
+}
+
