963fe509ca1acc5bef1585bb65e8264a5036697a
kabeer khan <kabeer.khan@samsung.com>
2015-04-17 12:06:43 +0200

    ecore_file: less confusing documentation of ecore_file_download API
    Summary: Signed-off-by: kabeer khan <kabeer.khan@samsung.com>
    
    Reviewers: cedric
    
    Subscribers: cedric
    
    Differential Revision: https://phab.enlightenment.org/D2373
    
    Signed-off-by: Cedric BAIL <cedric@osg.samsung.com>

--- src/lib/ecore_file/ecore_file_download.c
+++ src/lib/ecore_file/ecore_file_download.c
@@ -177,10 +177,10 @@ _ecore_file_download(const char *url,
  * percentage of the downloaded file. Return 0 from this callback, if provided,
  * to continue the operation or anything else to abort the download. The only
  * operations that can be aborted are those with protocol 'http' or 'ftp'. In
- * that case @p job_ret can be filled. It can be used with
- * ecore_file_download_abort() or ecore_file_download_abort_all() to
- * respectively abort one or all download operations. This function returns
- * @c EINA_TRUE if the download starts, @c EINA_FALSE otherwise.
+ * that case @p job_ret can be passed to ecore_file_download_abort() to abort
+ * that download job. Similarly ecore_file_download_abort_all() can be used to
+ * abort all download operations. This function returns @c EINA_TRUE if the
+ * download starts, @c EINA_FALSE otherwise.
  */
 EAPI Eina_Bool
 ecore_file_download(const char *url,
