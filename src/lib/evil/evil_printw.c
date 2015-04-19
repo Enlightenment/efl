d1722eb394216ff304ee8aa83146641057d99e72
Vincent Torri <vincent.torri@gmail.com>
2015-04-19 08:28:56 +0200

    evil: remove unused files (evil_p*) and uneeded evil_uuid.c file (managed by mingw-w64)
    Just keeping up with latest mingw-w64.

--- src/lib/evil/evil_printw.c
+++
@@ -1,4 +0,0 @@
-
-#define __BUILD_WIDEAPI 1
-
-#include "evil_printa.c"
--- src/lib/evil/evil_uuid.c
+++
@@ -1,9 +0,0 @@
-/*
- * Defines the windows UUID IID_IPersistFile used for links in
- * evil. This is here since uuid.lib is a static only library and
- * libtool does not allow you to link a DLL against a static library.
- */
-
-# define INITGUID
-# include <basetyps.h>
-DEFINE_OLEGUID(IID_IPersistFile, 0x0000010BL, 0, 0);
