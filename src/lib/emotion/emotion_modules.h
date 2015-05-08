7485c41cf45903c2653058180a500e69948440aa
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2015-05-08 14:12:29 +0900

    efl - emotion modules - unbreak due to EAPI changes
    emotion relied on Emotion.h's EAPI defs... now they have been removed
    fix!

--- src/lib/emotion/emotion_modules.h
+++ src/lib/emotion/emotion_modules.h
@@ -3,6 +3,32 @@
 
 #include "Emotion.h"
 
+#ifdef EAPI
+# undef EAPI
+#endif
+
+#ifdef _WIN32
+# ifdef EFL_EMOTION_BUILD
+#  ifdef DLL_EXPORT
+#   define EAPI __declspec(dllexport)
+#  else
+#   define EAPI
+#  endif /* ! DLL_EXPORT */
+# else
+#  define EAPI __declspec(dllimport)
+# endif /* ! EFL_EMOTION_BUILD */
+#else
+# ifdef __GNUC__
+#  if __GNUC__ >= 4
+#   define EAPI __attribute__ ((visibility("default")))
+#  else
+#   define EAPI
+#  endif
+# else
+#  define EAPI
+# endif
+#endif /* ! _WIN32 */
+
 #define META_TRACK_TITLE 1
 #define META_TRACK_ARTIST 2
 #define META_TRACK_GENRE 3
