d3750c504fa4279c9def9aae1fc2dd95c9cb6be0
Yakov Goldberg <yakov.g@samsung.com>
2015-05-13 17:27:04 +0300

    elm_panel: Move enums to elm_panel.eo
    Move enums from elm_panel_common.h to elm_panel.eo in order to make them
    more accessible for bindings.

--- legacy/elementary/src/lib/elm_panel_common.h
+++ legacy/elementary/src/lib/elm_panel_common.h
@@ -5,17 +5,6 @@
  */
 
 /**
- * Panel orientation mode
- */
-typedef enum
-{
-   ELM_PANEL_ORIENT_TOP, /**< Panel (dis)appears from the top */
-   ELM_PANEL_ORIENT_BOTTOM, /**< Panel (dis)appears from the bottom */
-   ELM_PANEL_ORIENT_LEFT, /**< Panel (dis)appears from the left */
-   ELM_PANEL_ORIENT_RIGHT, /**< Panel (dis)appears from the right */
-} Elm_Panel_Orient;
-
-/**
  * Panel scroll information
  *
  * @since 1.11
