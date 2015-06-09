03acea0a8a174d5a25a917d2831fa38335fedcfa
Chris Michael <cp.michael@samsung.com>
2015-06-09 09:03:54 -0400

    ecore-x: Fix compiler warning of unused variable
    Summary: XIDeviceEvent variable is only used if we have support for XI2_2.
    
    @fix
    
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

--- src/lib/ecore_x/xlib/ecore_x_xi2.c
+++ src/lib/ecore_x/xlib/ecore_x_xi2.c
@@ -387,13 +387,13 @@ _ecore_x_input_multi_handler(XEvent *xevent)
 {
 #ifdef ECORE_XI2
    if (xevent->type != GenericEvent) return;
-   XIDeviceEvent *evd = (XIDeviceEvent *)(xevent->xcookie.data);
 
    switch (xevent->xcookie.evtype)
      {
 #ifdef ECORE_XI2_2
       case XI_TouchUpdate:
           {
+             XIDeviceEvent *evd = (XIDeviceEvent *)(xevent->xcookie.data);
              int devid = evd->deviceid;
              int i = _ecore_x_input_touch_index_get(devid, evd->detail, XI_TouchUpdate);
              if ((i == 0) && (evd->flags & XITouchEmulatingPointer)) return;
@@ -416,6 +416,7 @@ _ecore_x_input_multi_handler(XEvent *xevent)
 
       case XI_TouchBegin:
           {
+             XIDeviceEvent *evd = (XIDeviceEvent *)(xevent->xcookie.data);
              int devid = evd->deviceid;
              int i = _ecore_x_input_touch_index_get(devid, evd->detail, XI_TouchBegin);
              if ((i == 0) && (evd->flags & XITouchEmulatingPointer)) return;
@@ -440,6 +441,7 @@ _ecore_x_input_multi_handler(XEvent *xevent)
 
       case XI_TouchEnd:
           {
+             XIDeviceEvent *evd = (XIDeviceEvent *)(xevent->xcookie.data);
              int devid = evd->deviceid;
              int i = _ecore_x_input_touch_index_get(devid, evd->detail, XI_TouchEnd);
              if ((i == 0) && (evd->flags & XITouchEmulatingPointer))
