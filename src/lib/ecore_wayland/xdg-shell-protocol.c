590b597e6e2ac6231b44076faf37f4d121f22941
Chris Michael <cp.michael@samsung.com>
2015-06-25 15:14:56 -0400

    Revert "ecore-wayland: Update xdg shell protocol file"
    This reverts commit 5793a62fc839af489263c918b78adb7765f8566d.
    
    Reverted due to wrong version being pushed

--- src/lib/ecore_wayland/xdg-shell-protocol.c
+++ src/lib/ecore_wayland/xdg-shell-protocol.c
@@ -50,8 +50,7 @@ static const struct wl_interface *types[] = {
 	NULL,
 	NULL,
 	NULL,
-	NULL,
-	&wl_surface_interface,
+	&xdg_surface_interface,
 	&wl_seat_interface,
 	NULL,
 	NULL,
@@ -65,9 +64,10 @@ static const struct wl_interface *types[] = {
 };
 
 static const struct wl_message xdg_shell_requests[] = {
+	{ "destroy", "", types + 0 },
 	{ "use_unstable_version", "i", types + 0 },
 	{ "get_xdg_surface", "no", types + 4 },
-	{ "get_xdg_popup", "nooouiiu", types + 6 },
+	{ "get_xdg_popup", "nooouii", types + 6 },
 	{ "pong", "u", types + 0 },
 };
 
@@ -77,23 +77,23 @@ static const struct wl_message xdg_shell_events[] = {
 
 WL_EXPORT const struct wl_interface xdg_shell_interface = {
 	"xdg_shell", 1,
-	4, xdg_shell_requests,
+	5, xdg_shell_requests,
 	1, xdg_shell_events,
 };
 
 static const struct wl_message xdg_surface_requests[] = {
 	{ "destroy", "", types + 0 },
-	{ "set_parent", "?o", types + 14 },
+	{ "set_parent", "?o", types + 13 },
 	{ "set_title", "s", types + 0 },
 	{ "set_app_id", "s", types + 0 },
-	{ "show_window_menu", "ouii", types + 15 },
-	{ "move", "ou", types + 19 },
-	{ "resize", "ouu", types + 21 },
+	{ "show_window_menu", "ouii", types + 14 },
+	{ "move", "ou", types + 18 },
+	{ "resize", "ouu", types + 20 },
 	{ "ack_configure", "u", types + 0 },
 	{ "set_window_geometry", "iiii", types + 0 },
 	{ "set_maximized", "", types + 0 },
 	{ "unset_maximized", "", types + 0 },
-	{ "set_fullscreen", "?o", types + 24 },
+	{ "set_fullscreen", "?o", types + 23 },
 	{ "unset_fullscreen", "", types + 0 },
 	{ "set_minimized", "", types + 0 },
 };
@@ -114,7 +114,7 @@ static const struct wl_message xdg_popup_requests[] = {
 };
 
 static const struct wl_message xdg_popup_events[] = {
-	{ "popup_done", "u", types + 0 },
+	{ "popup_done", "", types + 0 },
 };
 
 WL_EXPORT const struct wl_interface xdg_popup_interface = {
