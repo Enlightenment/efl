ad51d699a3274b03d363ff567259c6a05b6e3340
Yakov Goldberg <yakov.g@samsung.com>
2015-07-07 14:09:03 +0300

    elm_colorselector: Move enums to elm_colorselector.eo
    Move enums from elm_colorselector_common.h to elm_colorselector.eo in order to make them
    more accessible for bindings.

--- legacy/elementary/src/lib/elm_colorselector_common.h
+++ legacy/elementary/src/lib/elm_colorselector_common.h
@@ -13,30 +13,12 @@ struct _Elm_Color_RGBA
    unsigned int a;
 };
 
-
 typedef struct _Elm_Custom_Palette Elm_Custom_Palette;
 struct _Elm_Custom_Palette
 {
    const char *palette_name;
    Eina_List  *color_list;
 };
-
-/**
- * @enum Elm_Colorselector_Mode
- *
- * Different modes supported by Colorselector
- *
- * @see elm_colorselector_mode_set()
- * @see elm_colorselector_mode_get()
- */
-typedef enum
-{
-   ELM_COLORSELECTOR_PALETTE = 0, /**< only color palette is displayed */
-   ELM_COLORSELECTOR_COMPONENTS, /**< only color selector is displayed */
-   ELM_COLORSELECTOR_BOTH, /**< Both Palette and selector is displayed, default */
-   ELM_COLORSELECTOR_PICKER, /**< only color picker is displayed */
-   ELM_COLORSELECTOR_ALL /**< all possible color selector is displayed */
-} Elm_Colorselector_Mode;
 
 EAPI void elm_colorselector_palette_item_color_get(const Elm_Object_Item *it, int *r, int *g, int *b, int *a);
 
