5beb47aa4d65d0be9d75550511dd184e9e69b4d8
Daniel Kolesa <d.kolesa@osg.samsung.com>
2015-07-15 20:05:23 +0100

    image_savers/jpeg: actually use normal jmp_buf/setjmp/longjmp

--- src/modules/evas/image_savers/jpeg/evas_image_save_jpeg.c
+++ src/modules/evas/image_savers/jpeg/evas_image_save_jpeg.c
@@ -14,8 +14,8 @@ static Evas_Image_Save_Func evas_image_save_jpeg_func =
 
 struct _JPEG_error_mgr
 {
-   struct     jpeg_error_mgr pub;
-   sigjmp_buf setjmp_buffer;
+   struct jpeg_error_mgr pub;
+   jmp_buf setjmp_buffer;
 };
 typedef struct _JPEG_error_mgr *emptr;
 
@@ -26,7 +26,7 @@ _JPEGFatalErrorHandler(j_common_ptr cinfo)
    emptr errmgr;
 
    errmgr = (emptr) cinfo->err;
-   siglongjmp(errmgr->setjmp_buffer, 1);
+   longjmp(errmgr->setjmp_buffer, 1);
    return;
 }
 
@@ -73,7 +73,7 @@ save_image_jpeg(RGBA_Image *im, const char *file, int quality)
    jerr.pub.error_exit = _JPEGFatalErrorHandler;
    jerr.pub.emit_message = _JPEGErrorHandler2;
    jerr.pub.output_message = _JPEGErrorHandler;
-   if (sigsetjmp(jerr.setjmp_buffer, 1))
+   if (sigjmp(jerr.setjmp_buffer))
      {
 	jpeg_destroy_compress(&cinfo);
 	fclose(f);
