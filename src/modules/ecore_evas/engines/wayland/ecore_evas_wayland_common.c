82de5a66d7da2dbd63e8950fdfc6e469bae16276
Chris Michael <cp.michael@samsung.com>
2015-07-30 15:17:57 -0400

    ecore-evas-wayland: Remove call to ecore_wl_window_update_location
    Summary: This function should really not be called here as it triggers
    an xdg_surface_set_window_geometry call which (in turn) should only be
    getting called when the window geometry (meaning visible region)
    itself has changed.
    
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

--- src/modules/ecore_evas/engines/wayland/ecore_evas_wayland_common.c
+++ src/modules/ecore_evas/engines/wayland/ecore_evas_wayland_common.c
@@ -732,8 +732,6 @@ _ecore_evas_wl_common_move(Ecore_Evas *ee, int x, int y)
      {
         ee->x = x;
         ee->y = y;
-        if (wdata->win)
-          ecore_wl_window_update_location(wdata->win, x, y);
         if (ee->func.fn_move) ee->func.fn_move(ee);
      }
 }
