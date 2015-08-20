69bf2f0c7cbfbb141865181883fcf07d37066e72
Chris Michael <cp.michael@samsung.com>
2015-08-20 09:42:04 -0400

    ecore-wl2: Remove unused wl_registry field in display structure
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

--- src/lib/ecore_wl2/ecore_wl2_private.h
+++ src/lib/ecore_wl2/ecore_wl2_private.h
@@ -46,7 +46,6 @@ struct _Ecore_Wl2_Display
    struct
      {
         struct wl_display *display;
-        struct wl_registry *registry;
         struct wl_compositor *compositor;
         struct wl_subcompositor *subcompositor;
         struct wl_data_device_manager *data_device_manager;
