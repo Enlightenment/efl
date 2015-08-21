Enlightenment/efl

commit 024812c1a76286991f292c3191936778ec219ff8

evil: fix incorrect function call.
_localtime64_s() requires _time64_t as one argument but here we passes time_s.
Proper api is localtime_s().

src/lib/evil/evil_time.c
@@ -13,7 +13,7 @@
 struct tm *
 evil_localtime_r(const time_t *timep, struct tm *result)
 {
-   _localtime64_s(result, timep);
+   localtime_s(result, timep);
 
    return result;
 }

commit c73ccef4a753bc29e3d6589a38d1e65f462183fd

Evil: fix compilation when MSVC is not available.
localtime_s is not defined in msvcrt.dll but rather is defined in
Microsoft libc when Visual Studio or other stuff is installed.

Issue introduced in:024812c1a76286991f292c3191936778ec219ff8

Fixes T2681

@fix

src/lib/evil/evil_time.c
@@ -13,7 +13,9 @@
 struct tm *
 evil_localtime_r(const time_t *timep, struct tm *result)
 {
-   localtime_s(result, timep);
+   __time64_t t = *timep;
+
+   _localtime64_s(result, &t);
 
    return result;
 }