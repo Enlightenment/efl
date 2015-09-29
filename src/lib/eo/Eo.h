Enlightenment/efl

commit 37f84b7e966372384e2dfe5d191a6f907a17962e

Eo: Drop doc field from ops and events.
This hasn't been used for a while. Since we are going to break Eo a bit anyway
it's a good opportunity to drop this.

This may cause a slight performance issues with legacy events, such as
smart callbacks. This shouldn't really be a problem as we've migrated away from
them. If it does, we need to migrate the remaining parts. Only relevant
for callbacks that are added before the classes are created, which
shouldn't be possible except for smart, only for old evas callbacks.

src/lib/eo/Eo.h
@@ -242,24 +242,22 @@ typedef unsigned int Eo_Op;
  */
 
 /**
- * @def EO_EVENT_DESCRIPTION(name, doc)
+ * @def EO_EVENT_DESCRIPTION(name)
  * An helper macro to help populating #Eo_Event_Description
  * @param name The name of the event.
- * @param doc Additional doc for the event.
  * @see Eo_Event_Description
  */
-#define EO_EVENT_DESCRIPTION(name, doc) { name, doc, EINA_FALSE }
+#define EO_EVENT_DESCRIPTION(name) { name, EINA_FALSE }
 
 /**
- * @def EO_HOT_EVENT_DESCRIPTION(name, doc)
+ * @def EO_HOT_EVENT_DESCRIPTION(name)
  * An helper macro to help populating #Eo_Event_Description and make
  * the event impossible to freeze.
  * @param name The name of the event.
- * @param doc Additional doc for the event.
  * @see Eo_Event_Description
  * @see EO_EVENT_DESCRIPTION
  */
-#define EO_HOT_EVENT_DESCRIPTION(name, doc) { name, doc, EINA_TRUE }
+#define EO_HOT_EVENT_DESCRIPTION(name) { name, EINA_TRUE }
 
 
 
@@ -351,7 +349,6 @@ typedef struct _Eo_Op_Description
    void *func;             /**< The static function to call for the op. */
    Eo_Op op;               /**< The op. */
    Eo_Op_Type op_type;     /**< The type of the Op. */
-   const char *doc;        /**< Explanation about the Op. */
 } Eo_Op_Description;
 
 /**
@@ -540,11 +537,11 @@ EAPI extern Eo_Hook_Call eo_hook_call_post;
 # define _EO_OP_API_ENTRY(a) a, #a
 #endif
 
-#define EO_OP_FUNC(_api, _private, _doc) { _EO_OP_API_ENTRY(_api), _private, EO_NOOP, EO_OP_TYPE_REGULAR, _doc }
-#define EO_OP_CLASS_FUNC(_api, _private, _doc) { _EO_OP_API_ENTRY(_api), _private, EO_NOOP, EO_OP_TYPE_CLASS, _doc }
-#define EO_OP_FUNC_OVERRIDE(_api, _private) { _EO_OP_API_ENTRY(_api), _private, EO_OP_OVERRIDE, EO_OP_TYPE_REGULAR, NULL }
-#define EO_OP_CLASS_FUNC_OVERRIDE(_api, _private) { _EO_OP_API_ENTRY(_api), _private, EO_OP_OVERRIDE, EO_OP_TYPE_CLASS, NULL }
-#define EO_OP_SENTINEL { _EO_OP_API_ENTRY(NULL), NULL, 0, EO_OP_TYPE_INVALID, NULL }
+#define EO_OP_FUNC(_api, _private) { _EO_OP_API_ENTRY(_api), _private, EO_NOOP, EO_OP_TYPE_REGULAR }
+#define EO_OP_CLASS_FUNC(_api, _private) { _EO_OP_API_ENTRY(_api), _private, EO_NOOP, EO_OP_TYPE_CLASS }
+#define EO_OP_FUNC_OVERRIDE(_api, _private) { _EO_OP_API_ENTRY(_api), _private, EO_OP_OVERRIDE, EO_OP_TYPE_REGULAR }
+#define EO_OP_CLASS_FUNC_OVERRIDE(_api, _private) { _EO_OP_API_ENTRY(_api), _private, EO_OP_OVERRIDE, EO_OP_TYPE_CLASS }
+#define EO_OP_SENTINEL { _EO_OP_API_ENTRY(NULL), NULL, 0, EO_OP_TYPE_INVALID }
 
 // returns the OP id corresponding to the given api_func
 EAPI Eo_Op _eo_api_op_id_get(const void *api_func, Eina_Bool is_main_loop, const char *file, int line);

src/lib/eo/eo_base_class.c
@@ -401,7 +401,6 @@ _wref_destruct(Eo_Base_Data *pd)
 
 /* XXX: Legacy support, remove when legacy is dead. */
 static Eina_Hash *_legacy_events_hash = NULL;
-static const char *_legacy_event_desc = "Dynamically generated legacy event";
 
 EAPI const Eo_Event_Description *
 eo_base_legacy_only_event_description_get(const char *_event_name)
@@ -412,7 +411,6 @@ eo_base_legacy_only_event_description_get(const char *_event_name)
      {
         event_desc = calloc(1, sizeof(Eo_Event_Description));
         event_desc->name = event_name;
-        event_desc->doc = _legacy_event_desc;
         eina_hash_add(_legacy_events_hash, event_name, event_desc);
      }
    else
@@ -643,22 +641,13 @@ _cb_desc_match(const Eo_Event_Description *a, const Eo_Event_Description *b)
    if (!a)
       return EINA_FALSE;
 
-   /* If either is legacy, fallback to string comparison. */
-   if ((a->doc == _legacy_event_desc) || (b->doc == _legacy_event_desc))
+   if (a == b)
      {
-        /* Take stringshare shortcut if both are legacy */
-        if (a->doc == b->doc)
-          {
-             return (a->name == b->name);
-          }
-        else
-          {
-             return !strcmp(a->name, b->name);
-          }
+        return EINA_TRUE;
      }
    else
      {
-        return (a == b);
+        return !strcmp(a->name, b->name);
      }
 }
 

src/tests/eo/suite/eo_test_general.c
@@ -722,8 +722,8 @@ EO_FUNC_BODY(multi_a_print, Eina_Bool, EINA_FALSE);
 EO_FUNC_BODY(multi_class_hi_print, Eina_Bool, EINA_FALSE);
 
 static Eo_Op_Description _multi_do_op_descs[] = {
-     EO_OP_FUNC(multi_a_print, _a_print, "Print property a"),
-     EO_OP_FUNC(multi_class_hi_print, _class_hi_print, "Print Hi"),
+     EO_OP_FUNC(multi_a_print, _a_print),
+     EO_OP_FUNC(multi_class_hi_print, _class_hi_print),
      EO_OP_SENTINEL
 };
 

src/bin/eolian/common_funcs.c
@@ -155,20 +155,3 @@ _startline(char *str, char *pos)
 
    return ret;
 }
-
-char*
-_source_desc_get(const char *str)
-{
-   Eina_Strbuf *part = eina_strbuf_new();
-   if (str)
-     {
-        const char *p = strchr(str, '\n');
-        size_t offs = (p) ? (size_t)(p - str) : strlen(str);
-        eina_strbuf_append_n(part, str, offs);
-        eina_strbuf_replace_all(part, "\\", "\\\\");
-        eina_strbuf_replace_all(part, "\"", "\\\"");
-     }
-   char *ret = eina_strbuf_string_steal(part);
-   eina_strbuf_free(part);
-   return ret;
-}

src/bin/eolian/common_funcs.h
@@ -63,8 +63,6 @@ char *_nextline(char *str, unsigned int lines);
 
 char *_startline(char *str, char *pos);
 
-char *_source_desc_get(const char *str);
-
 void _class_env_create(const Eolian_Class *class, const char *over_classname, _eolian_class_vars *env);
 
 void _class_func_env_create(const Eolian_Class *class, const char *funcname, Eolian_Function_Type ftype EINA_UNUSED, _eolian_class_func_vars *env);

src/bin/eolian/eo_generator.c
@@ -586,7 +586,7 @@ eo_bind_func_generate(const Eolian_Class *class, const Eolian_Function *funcid, 
 
 static Eina_Bool
 eo_op_desc_generate(const Eolian_Class *class, const Eolian_Function *fid, Eolian_Function_Type ftype,
-      const char *desc, Eina_Strbuf *buf)
+                    Eina_Strbuf *buf)
 {
    _eolian_class_func_vars func_env;
    const char *funcname = eolian_function_name_get(fid);
@@ -603,12 +603,12 @@ eo_op_desc_generate(const Eolian_Class *class, const Eolian_Function *fid, Eolia
    if (!is_virtual_pure)
      {
         Eolian_Function_Type ftype2 = (Eolian_Function_Type) eina_hash_find(_funcs_params_init, funcname);
-        eina_strbuf_append_printf(buf, "%s_%s_%s%s, \"%s\"),",
+        eina_strbuf_append_printf(buf, "%s_%s_%s%s),",
               ftype == ftype2?"__eolian":"",
-              class_env.lower_classname, funcname, suffix, desc);
+              class_env.lower_classname, funcname, suffix);
      }
    else
-      eina_strbuf_append_printf(buf, "NULL, \"%s\"),", desc);
+      eina_strbuf_append_printf(buf, "NULL),");
 
    return EINA_TRUE;
 }
@@ -625,16 +625,11 @@ eo_source_beginning_generate(const Eolian_Class *class, Eina_Strbuf *buf)
    EINA_ITERATOR_FOREACH(itr, event)
      {
         Eina_Stringshare *evname = eolian_event_c_name_get(event);
-        const char *evdesc = NULL;
-        const Eolian_Documentation *doc = eolian_event_documentation_get(event);
-        if (doc) evdesc = eolian_documentation_summary_get(doc);
-        char *evdesc_line1 = _source_desc_get(evdesc);
 
         eina_strbuf_append_printf(tmpbuf,
-                                  "EOAPI const Eo_Event_Description _%s =\n   EO_EVENT_DESCRIPTION(\"%s\", \"%s\");\n",
-                                  evname, eolian_event_name_get(event), evdesc_line1);
+                                  "EOAPI const Eo_Event_Description _%s =\n   EO_EVENT_DESCRIPTION(\"%s\");\n",
+                                  evname, eolian_event_name_get(event));
         eina_stringshare_del(evname);
-        free(evdesc_line1);
      }
    eina_iterator_free(itr);
 
@@ -652,14 +647,8 @@ _desc_generate(const Eolian_Class *class, const Eolian_Function *fid, Eolian_Fun
    snprintf(tmpstr, sizeof(tmpstr), "%s%s", funcname, (ftype == EOLIAN_PROP_SET)
      ? "_set" : ((ftype == EOLIAN_PROP_GET) ? "_get" : ""));
 
-   const char *opdesc = NULL;
-   const Eolian_Documentation *doc = eolian_function_documentation_get(fid, ftype);
-   if (doc) opdesc = eolian_documentation_summary_get(doc);
-
-   char *desc = _source_desc_get(opdesc);
-   eo_op_desc_generate(class, fid, ftype, desc, tmpbuf);
+   eo_op_desc_generate(class, fid, ftype, tmpbuf);
    eina_strbuf_append(str_op, eina_strbuf_string_get(tmpbuf));
-   free(desc);
 }
 
 static Eina_Bool

src/bindings/eo_cxx/eo_inherit_bindings.hh
@@ -138,13 +138,11 @@ Eo_Class const* create_class(eina::index_sequence<S...>)
      );
    op_descs[detail::operation_description_size<E...>::value].op = EO_NOOP;
    op_descs[detail::operation_description_size<E...>::value].op_type = EO_OP_TYPE_REGULAR;
-   op_descs[detail::operation_description_size<E...>::value].doc = NULL;
 
    op_descs[detail::operation_description_size<E...>::value+1].func = 0;
    op_descs[detail::operation_description_size<E...>::value+1].api_func = 0;
    op_descs[detail::operation_description_size<E...>::value+1].op = 0;
    op_descs[detail::operation_description_size<E...>::value+1].op_type = EO_OP_TYPE_INVALID;
-   op_descs[detail::operation_description_size<E...>::value+1].doc = NULL;
 
    typedef inherit<D, E...> inherit_type;
    using namespace detail;

src/examples/eo/evas/evas_elw_button.c
@@ -11,7 +11,7 @@
 EAPI Eo_Op ELW_BUTTON_BASE_ID = 0;
 
 EAPI const Eo_Event_Description _EV_CLICKED =
-        EO_EVENT_DESCRIPTION("clicked", "Called when there was a click.");
+        EO_EVENT_DESCRIPTION("clicked");
 
 typedef struct
 {

commit d889da6b12e6123073edb54d766c9af1fc6db822

Eo legacy events: Fix broken callback comparison
Commit 37f84b7e966372384e2dfe5d191a6f907a17962e introduced a few changes
to the callback matching mechanism that made it so sometimes callbacks
would be triggered for the wrong events. The problem was there because
of the support for legacy events that forces to do string comparison
instead of the usual pointer comparison. We should only do string
comparison when we are certain one of the callbacks is a legacy
generated one.

Regression tests will follow tomorrow. Way too late here for that.

Thanks to cedric for reporting.

src/lib/eo/Eo.h
@@ -919,6 +919,8 @@ typedef void (*eo_key_data_free_func)(void *);
 
 /**
  * Don't use.
+ * The values of the returned event structure are also internal, don't assume
+ * anything about them.
  * @internal
  */
 EAPI const Eo_Event_Description *eo_base_legacy_only_event_description_get(const char *_event_name);

src/lib/eo/eo_base_class.c
@@ -401,11 +401,16 @@ _wref_destruct(Eo_Base_Data *pd)
 
 /* XXX: Legacy support, remove when legacy is dead. */
 static Eina_Hash *_legacy_events_hash = NULL;
+#define _LEGACY_EVENT_FIRST_CHAR 1
 
 EAPI const Eo_Event_Description *
 eo_base_legacy_only_event_description_get(const char *_event_name)
 {
-   Eina_Stringshare *event_name = eina_stringshare_add(_event_name);
+   char buf[1024];
+   buf[0] = _LEGACY_EVENT_FIRST_CHAR; /* Encode it's a legacy event */
+   strncpy(buf + 1, _event_name, sizeof(buf) - 1);
+   buf[sizeof(buf) - 1] = '\0';
+   Eina_Stringshare *event_name = eina_stringshare_add(buf);
    Eo_Event_Description *event_desc = eina_hash_find(_legacy_events_hash, event_name);
    if (!event_desc)
      {
@@ -419,6 +424,26 @@ eo_base_legacy_only_event_description_get(const char *_event_name)
      }
 
    return event_desc;
+}
+
+static Eina_Bool
+_legacy_event_desc_is(const Eo_Event_Description *desc)
+{
+   return (desc->name[0] == _LEGACY_EVENT_FIRST_CHAR);
+}
+
+/* Also supports non legacy. */
+static const char *
+_legacy_event_desc_name_get(const Eo_Event_Description *desc)
+{
+   if (_legacy_event_desc_is(desc))
+     {
+        return desc->name + 1;
+     }
+   else
+     {
+        return desc->name;
+     }
 }
 
 static void
@@ -641,13 +666,19 @@ _cb_desc_match(const Eo_Event_Description *a, const Eo_Event_Description *b)
    if (!a)
       return EINA_FALSE;
 
-   if (a == b)
+   if (_legacy_event_desc_is(a) && _legacy_event_desc_is(b))
      {
-        return EINA_TRUE;
+        return (a->name == b->name);
+     }
+   else if (_legacy_event_desc_is(a) || _legacy_event_desc_is(b))
+     {
+        const char *aname = _legacy_event_desc_name_get(a);
+        const char *bname = _legacy_event_desc_name_get(b);
+        return !strcmp(aname, bname);
      }
    else
      {
-        return !strcmp(a->name, b->name);
+        return (a == b);
      }
 }
 

src/tests/eo/suite/eo_test_general.c
@@ -125,12 +125,12 @@ START_TEST(eo_signals)
      {
         const Eo_Event_Description *a_desc = eo_base_legacy_only_event_description_get("a,changed");
         fail_if(!a_desc);
-        ck_assert_str_eq(a_desc->name, "a,changed");
+        ck_assert_str_eq(a_desc->name, "\x01" "a,changed");
         fail_if(a_desc == EV_A_CHANGED);
 
         const Eo_Event_Description *bad_desc = eo_base_legacy_only_event_description_get("bad");
         fail_if(!bad_desc);
-        ck_assert_str_eq(bad_desc->name, "bad");
+        ck_assert_str_eq(bad_desc->name, "\x01" "bad");
 
         /* Call Eo event with legacy and non-legacy callbacks. */
         _eo_signals_cb_current = 0;