Enlightenment/efl

commit 21c43528234a315a1cab7df00014ab62dca92ced

evas image object - dont update just if image is dirty
marking images as dirty is only meant to have the pixle get callback
called IF rendered. you ADD update regions to get it to be called.
this is how it has always meant to work, but evas image objects where
adding updates just if dirty. this ends up with e as a compositor
redrawing ENTIRE eindows if even a single blinking cursor is blinking.
oh so bad.

@fix

src/lib/evas/canvas/evas_object_image.c
@@ -3747,11 +3747,6 @@ evas_object_image_render_pre(Evas_Object *eo_obj,
              evas_object_render_pre_prev_cur_add(&e->clip_changes, eo_obj, obj);
              if (!o->pixels->pixel_updates) goto done;
           }
-        if (o->dirty_pixels)
-          {
-             evas_object_render_pre_prev_cur_add(&e->clip_changes, eo_obj, obj);
-             if (!o->pixels->pixel_updates) goto done;
-          }
         if (o->cur->frame != o->prev->frame)
           {
              evas_object_render_pre_prev_cur_add(&e->clip_changes, eo_obj, obj);

commit b38f8a3f39008845ac42ee04a83cf76c23b281c0

Evas GL: Fix render: force pixel_get if dirty
All examples and docs point to using only the dirty flag in
order to trigger a redraw of an Evas GL surface. The commit
21c43528234 broke this behaviour (for a good reason, but not
related to Evas GL).

This is a compatibility fix.

src/lib/evas/canvas/evas_object_image.c
@@ -3751,6 +3751,19 @@ evas_object_image_render_pre(Evas_Object *eo_obj,
              evas_object_render_pre_prev_cur_add(&e->clip_changes, eo_obj, obj);
              if (!o->pixels->pixel_updates) goto done;
           }
+        if (o->dirty_pixels && ENFN->image_native_get)
+          {
+             /* Evas GL surfaces have historically required only the dirty
+              * pixel to trigger a redraw (call to pixels_get). Other kinds
+              * of surfaces must add data update regions. */
+             Evas_Native_Surface *ns;
+             ns = ENFN->image_native_get(ENDT, o->engine_data);
+             if (ns && (ns->type == EVAS_NATIVE_SURFACE_EVASGL))
+               {
+                  evas_object_render_pre_prev_cur_add(&e->clip_changes, eo_obj, obj);
+                  if (!o->pixels->pixel_updates) goto done;
+               }
+          }
         if (o->cur->frame != o->prev->frame)
           {
              evas_object_render_pre_prev_cur_add(&e->clip_changes, eo_obj, obj);