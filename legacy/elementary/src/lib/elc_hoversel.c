bcc0cd758ae1e6472c1d1959b691bd9c986f167b
Chris Michael <cp.michael@samsung.com>
2015-10-05 09:11:25 -0400

    elementary: Comment out (#if 0) unused functions
    Summary: Unsure whether these are left-over, or are going to be
    implemented in the near future...but for now, #if 0 out these 2
    functions as they are currently unused.
    
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

--- legacy/elementary/src/lib/elc_hoversel.c
+++ legacy/elementary/src/lib/elc_hoversel.c
@@ -440,6 +440,7 @@ _on_parent_del(void *data,
    elm_hoversel_hover_parent_set(data, NULL);
 }
 
+#if 0
 static void
 _on_table_del(void *data,
               Evas *e EINA_UNUSED,
@@ -462,6 +463,7 @@ _size_hints_changed_cb(void *data,
 {
    elm_layout_sizing_eval(data);
 }
+#endif
 
 static void
 _elm_hoversel_item_elm_widget_item_part_text_set(Eo *eo_it EINA_UNUSED,
