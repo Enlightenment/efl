bf1eef174bb596cbab291f823c9f3644e1dc4bb3
Chris Michael <cp.michael@samsung.com>
2015-10-07 11:00:46 -0400

    evas-wayland-shm: Fix to add support for MERGE_MODE Smart
    Summary: Seems we were never setting any merge_mode for the
    wayland-shm engine. This fix implements setting merge_mode
    
    @fix
    
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

--- src/modules/evas/engines/wayland_shm/evas_engine.c
+++ src/modules/evas/engines/wayland_shm/evas_engine.c
@@ -27,6 +27,8 @@ _render_engine_swapbuf_setup(int w, int h, unsigned int rotation, unsigned int d
 {
    Render_Engine *re;
    Outbuf *ob;
+   Render_Engine_Merge_Mode merge_mode = MERGE_SMART;
+   const char *s;
 
    LOGFN(__FILE__, __LINE__, __FUNCTION__);
 
@@ -51,6 +53,19 @@ _render_engine_swapbuf_setup(int w, int h, unsigned int rotation, unsigned int d
      goto err;
 
    re->outbuf_reconfigure = _evas_outbuf_reconfigure;
+
+   s = getenv("EVAS_WAYLAND_PARTIAL_MERGE");
+   if (s)
+     {
+        if ((!strcmp(s, "bounding")) || (!strcmp(s, "b")))
+          merge_mode = MERGE_BOUNDING;
+        else if ((!strcmp(s, "full")) || (!strcmp(s, "f")))
+          merge_mode = MERGE_FULL;
+        else if ((!strcmp(s, "smart")) || (!strcmp(s, "s")))
+          merge_mode = MERGE_SMART;
+     }
+
+   evas_render_engine_software_generic_merge_mode_set(&re->generic, merge_mode);
 
    /* return allocated render engine */
    return re;
