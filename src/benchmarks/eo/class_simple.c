862372ed2316d23e657f3562a4d5c77d05bd8849
Tom Hacohen <tom@stosb.com>
2015-10-14 10:54:02 +0100

    Eo benchmarks: Improve benchmarks.
    Add more cases, and tune the number of times we test.

--- src/benchmarks/eo/class_simple.c
+++ src/benchmarks/eo/class_simple.c
@@ -8,6 +8,17 @@
 #define MY_CLASS SIMPLE_CLASS
 
 static void
+_other_call(Eo *obj EINA_UNUSED, void *class_data EINA_UNUSED, Eo *other, int times)
+{
+   if (times > 0)
+     {
+        eo_do(other, simple_other_call(obj, times-1));
+     }
+}
+
+EAPI EO_VOID_FUNC_BODYV(simple_other_call, EO_FUNC_CALL(other, times), Eo *other, int times);
+
+static void
 _a_set(Eo *obj EINA_UNUSED, void *class_data, int a)
 {
    Simple_Public_Data *pd = class_data;
@@ -18,6 +29,7 @@ EAPI EO_VOID_FUNC_BODYV(simple_a_set, EO_FUNC_CALL(a), int a);
 
 static Eo_Op_Description op_desc[] = {
      EO_OP_FUNC(simple_a_set, _a_set),
+     EO_OP_FUNC(simple_other_call, _other_call),
 };
 
 static const Eo_Class_Description class_desc = {
--- src/benchmarks/eo/class_simple.h
+++ src/benchmarks/eo/class_simple.h
@@ -7,6 +7,9 @@ typedef struct
 } Simple_Public_Data;
 
 EAPI void simple_a_set(int a);
+/* Calls simple_other_call(other, obj) and then simple_other_call(obj, other)
+ * for 'times' times in order to grow the call stack on other objects. */
+EAPI void simple_other_call(Eo *other, int times);
 
 #define SIMPLE_CLASS simple_class_get()
 const Eo_Class *simple_class_get(void);
