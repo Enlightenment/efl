30ad41cf999ce8f46308252735c1010a32468939
Srivardhan Hebbar <sri.hebbar@samsung.com>
2015-10-21 14:01:38 -0700

    ecore_x: remove useless assignment.
    Summary: Signed-off-by: Srivardhan Hebbar <sri.hebbar@samsung.com>
    
    Reviewers: cedric
    
    Differential Revision: https://phab.enlightenment.org/D3206
    
    Signed-off-by: Cedric BAIL <cedric@osg.samsung.com>

--- src/lib/ecore_x/xlib/ecore_x_randr.c
+++ src/lib/ecore_x/xlib/ecore_x_randr.c
@@ -1954,7 +1954,6 @@ ecore_x_randr_crtc_info_free(Ecore_X_Randr_Crtc_Info *info)
              if (info->outputs) free(info->outputs);
              if (info->possible) free(info->possible);
              free(info);
-             info = NULL;
           }
      }
 #endif
