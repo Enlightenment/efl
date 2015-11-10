705b08ed745512797ac2ce44c10623f3acf5c2af
Srivardhan Hebbar <sri.hebbar@samsung.com>
2015-11-10 15:04:19 -0800

    eina: updated example to include eina_strbuf_substr_get.
    Summary:
    Depends on D3224
    
    Signed-off-by: Srivardhan Hebbar <sri.hebbar@samsung.com>
    
    Reviewers: cedric
    
    Differential Revision: https://phab.enlightenment.org/D3226
    
    Signed-off-by: Cedric BAIL <cedric@osg.samsung.com>

--- src/examples/eina/eina_strbuf_01.c
+++ src/examples/eina/eina_strbuf_01.c
@@ -6,7 +6,7 @@
 
 int main(int argc EINA_UNUSED, char **argv EINA_UNUSED)
 {
-   Eina_Strbuf *buf;
+   Eina_Strbuf *buf, *substr;
 
    eina_init();
 
@@ -18,6 +18,10 @@ int main(int argc EINA_UNUSED, char **argv EINA_UNUSED)
 
    eina_strbuf_tolower(buf);
    printf("%s\n", eina_strbuf_string_get(buf));
+
+   substr = eina_strbuf_substr_get(buf, 3, 2);
+   printf("%s\n", eina_strbuf_string_get(substr));
+   eina_strbuf_free(substr);
 
    eina_strbuf_insert_escaped(buf, "my ", 0);
    printf("%s\n", eina_strbuf_string_get(buf));
