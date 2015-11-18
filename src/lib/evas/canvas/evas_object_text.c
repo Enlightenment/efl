Enlightenment/efl

commit 392df9479f816a641fabbf333326bdab4b9cfc28

Evas text: Fix memory leak
Summary:
Fix memory leak

Position is getting returned without
freeing the temporary logical item list.

@fix

Test Plan: NA

Reviewers: tasn, raster

Reviewed By: raster

Subscribers: raster, cedric

Differential Revision: https://phab.enlightenment.org/D3345

src/lib/evas/canvas/evas_object_text.c
@@ -271,6 +271,7 @@ _evas_object_text_last_up_to_pos(const Evas_Object *eo_obj,
 {
    Evas_Object_Protected_Data *obj = eo_data_scope_get(eo_obj, EVAS_OBJECT_CLASS);
    Evas_Object_Text_Item *it;
+   int pos = 0;
 
 #ifdef BIDI_SUPPORT
    /*FIXME: not very efficient, sort the items arrays. */
@@ -291,15 +292,17 @@ _evas_object_text_last_up_to_pos(const Evas_Object *eo_obj,
           {
              if ((x <= cx) && (cx < x + it->adv))
                {
-                  return it->text_pos + ENFN->font_last_up_to_pos(ENDT,
+                  pos = it->text_pos + ENFN->font_last_up_to_pos(ENDT,
                         o->font,
                         &it->text_props,
                         cx - x,
                         cy);
+                  break;
                }
              x += it->adv;
           }
         eina_list_free(logical_it);
+        return pos;
      }
    else
 #endif

commit 1e1e50ca062abe2381c983a5976f77c4cfc35a81

Evas text: Fix last up to pos error return value.
This commit also simplifies the code a (tiny) bit.

This fixes the bug introduced in:
392df9479f816a641fabbf333326bdab4b9cfc28

src/lib/evas/canvas/evas_object_text.c
@@ -271,7 +271,7 @@ _evas_object_text_last_up_to_pos(const Evas_Object *eo_obj,
 {
    Evas_Object_Protected_Data *obj = eo_data_scope_get(eo_obj, EVAS_OBJECT_CLASS);
    Evas_Object_Text_Item *it;
-   int pos = 0;
+   int pos = -1;
 
 #ifdef BIDI_SUPPORT
    /* Reorder if it's a bidi text */
@@ -301,7 +301,6 @@ _evas_object_text_last_up_to_pos(const Evas_Object *eo_obj,
              x += it->adv;
           }
         eina_list_free(logical_it);
-        return pos;
      }
    else
 #endif
@@ -318,7 +317,7 @@ _evas_object_text_last_up_to_pos(const Evas_Object *eo_obj,
                }
           }
      }
-   return -1;
+   return pos;
 }
 
 static int