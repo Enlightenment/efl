f8e7f0d2accb7dfbb785e0ae8514cbe31160e16b
se.osadchy <se.osadchy@samsung.com>
2015-12-02 14:14:17 -0800

    evas: fix bug with logic with eina_matrix in evas_3d_utils
    Summary: Also need to fix logically dead code in coverity.
    
    Reviewers: raster, Hermet, cedric
    
    Subscribers: jpeg
    
    Maniphest Tasks: T2832
    
    Differential Revision: https://phab.enlightenment.org/D3346
    
    Signed-off-by: Cedric BAIL <cedric@osg.samsung.com>

---
+++ src/tests/evas/evas_test_matrix.c
@@ -0,0 +1,41 @@
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#include "../../lib/evas/include/evas_common_private.h"
+#include "../../lib/evas/include/evas_private.h"
+#include "evas_suite.h"
+#include "Evas.h"
+#include "evas_tests_helpers.h"
+
+START_TEST(evas_matrix)
+{
+   Evas_Vec3 position;
+   Evas_Vec4 orientation;
+   Evas_Vec3 scale;
+   Eina_Matrix4 mat;
+
+   evas_vec3_set(&position, 5.0, 3.0, 2.0);
+   evas_vec4_set(&orientation, 30.0, 1.0, 0.0, 0.0);
+   evas_vec3_set(&scale, 1.0, 2.0, 1.0);
+
+   evas_mat4_build(&mat, &position, &orientation, &scale);
+   fail_if((mat.xx != -1) || (mat.xy != 60) ||
+           (mat.wx != 5) || (mat.wy != 3) ||
+           (mat.wz != 2) || (mat.ww != 1));
+
+   evas_mat4_inverse_build(&mat, &position, &orientation, &scale);
+   fail_if((mat.xx - 0.99 < DBL_EPSILON) || (mat.xy - 0.0 < DBL_EPSILON) ||
+           (mat.yx -0.0 < DBL_EPSILON) || (mat.yy -0.49 < DBL_EPSILON));
+}
+END_TEST
+
+void evas_test_matrix(TCase *tc)
+{
+   tcase_add_test(tc, evas_matrix);
+}
