e1881b05f43bea21494b398b0e01003ae349c30c
Vitor Sousa <vitorsousasilva@gmail.com>
2016-01-19 18:45:45 -0200

    elm_calendar,elm_datetime: move struct tm typedefs to .eo files
    Move elm_calendar and elm_datetime struct tm typedefs to theirs
    respective .eo files.
    Use Ecore.Time, an alias to struct tm, to refer to this type in the
    .eo files.
    
    Use namespaced names for these typedefs, in order to follow Eolian
    standards.

--- legacy/elementary/src/lib/elm_calendar_common.h
+++ legacy/elementary/src/lib/elm_calendar_common.h
@@ -15,8 +15,6 @@
  */
 typedef char * (*Elm_Calendar_Format_Cb)(struct tm *stime);
 
-/* temporary until better solution is found: is here because of eolian */
-typedef struct tm Elm_Calendar_Time;
 
 /**
  * @}
--- legacy/elementary/src/lib/elm_datetime_common.h
+++ legacy/elementary/src/lib/elm_datetime_common.h
@@ -3,8 +3,6 @@
  *
  * @{
  */
-/* temporary until better solution is found: is here because of eolian */
-typedef struct tm Elm_Datetime_Time;
 
 /**
  * @}
