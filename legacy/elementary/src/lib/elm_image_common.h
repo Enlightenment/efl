7b5e8cb0c34c683fc4f5477b9ad063907109687e
Larry <larry.olj@gmail.com>
2016-02-04 18:08:14 -0200

    Elm_Image: moved undefined types to eolian file

--- legacy/elementary/src/lib/elm_image_common.h
+++ legacy/elementary/src/lib/elm_image_common.h
@@ -8,8 +8,6 @@
  * Using Evas_Image_Orient enums
  * @since 1.14
  */
-typedef Evas_Image_Orient Elm_Image_Orient;
-
 #define ELM_IMAGE_ORIENT_NONE     EVAS_IMAGE_ORIENT_NONE
 #define ELM_IMAGE_ORIENT_0        EVAS_IMAGE_ORIENT_0
 #define ELM_IMAGE_ROTATE_90       EVAS_IMAGE_ORIENT_90
@@ -22,35 +20,6 @@ typedef Evas_Image_Orient Elm_Image_Orient;
 #define ELM_IMAGE_FLIP_VERTICAL   EVAS_IMAGE_FLIP_VERTICAL
 #define ELM_IMAGE_FLIP_TRANSPOSE  EVAS_IMAGE_FLIP_TRANSPOSE
 #define ELM_IMAGE_FLIP_TRANSVERSE EVAS_IMAGE_FLIP_TRANSVERSE
-
-/**
- * Structure associated with smart callback 'download,progress'.
- * @since 1.8
- */
-typedef struct _Elm_Image_Progress Elm_Image_Progress;
-/**
- * Structure associated with smart callback 'download,progress'.
- */
-struct _Elm_Image_Progress
-{
-   double now;
-   double total;
-};
-
-/**
- * Structre associated with smart callback 'download,error'
- * @since 1.8
- */
-typedef struct _Elm_Image_Error Elm_Image_Error;
-/**
- * Structure associated with smart callback 'download,progress'.
- */
-struct _Elm_Image_Error
-{
-   int status;
-
-   Eina_Bool open_error;
-};
 
 /**
  * @}
