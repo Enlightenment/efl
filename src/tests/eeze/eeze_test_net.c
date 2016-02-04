92ff735c110abac6fcb5cc9089d5c960d843888f
Vincent Torri <vincent dot torri at gmail dot com>
2016-02-04 14:35:53 +0100

    Test rework #7: Eeze
    eeze_suite.c has also been splitted

---
+++ src/tests/eeze/eeze_test_net.c
@@ -0,0 +1,116 @@
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include <Eeze.h>
+#include <Eeze_Net.h>
+
+#include "eeze_suite.h"
+
+START_TEST(eeze_test_net_list)
+{
+   int ret;
+   Eina_List *list = NULL;
+
+   ret = eeze_init();
+   fail_if(ret != 1);
+
+   list = eeze_net_list();
+   fail_if(list == NULL);
+
+   ret = eeze_shutdown();
+   fail_if(ret != 0);
+}
+END_TEST
+
+START_TEST(eeze_test_net_attr)
+{
+   int ret;
+   int idx = 0;
+   Eeze_Net *net = NULL;
+   const char *tmp = NULL;
+
+   ret = eeze_init();
+   fail_if(ret != 1);
+
+   net = eeze_net_new("lo");
+   fail_if(net == NULL);
+
+   tmp = eeze_net_mac_get(net);
+   fail_if(tmp == NULL);
+   tmp = NULL;
+
+   idx = eeze_net_idx_get(net);
+   fail_if(!idx);
+
+   eeze_net_scan(net);
+
+   tmp = eeze_net_addr_get(net, EEZE_NET_ADDR_TYPE_IP);
+   fail_if(tmp == NULL);
+   tmp = NULL;
+
+   tmp = eeze_net_addr_get(net, EEZE_NET_ADDR_TYPE_BROADCAST);
+   fail_if(tmp == NULL);
+   tmp = NULL;
+
+   tmp = eeze_net_addr_get(net, EEZE_NET_ADDR_TYPE_NETMASK);
+   fail_if(tmp == NULL);
+   tmp = NULL;
+
+   tmp = eeze_net_attribute_get(net, "carrier");
+   fail_if(tmp == NULL);
+   tmp = NULL;
+
+   tmp = eeze_net_syspath_get(net);
+   fail_if(tmp == NULL);
+
+   eeze_net_free(net);
+
+   ret = eeze_shutdown();
+   fail_if(ret != 0);
+}
+END_TEST
+
+/*
+#ifdef HAVE_IPV6
+START_TEST(eeze_test_net_attr_ipv6)
+{
+   int ret;
+   Eeze_Net *net = NULL;
+   const char *tmp = NULL;
+
+   ret = eeze_init();
+   fail_if(ret != 1);
+
+   net = eeze_net_new("lo");
+   fail_if(net == NULL);
+
+   tmp = eeze_net_addr_get(net, EEZE_NET_ADDR_TYPE_IP6);
+   fail_if(tmp == NULL);
+   tmp = NULL;
+
+   tmp = eeze_net_addr_get(net, EEZE_NET_ADDR_TYPE_BROADCAST6);
+   fail_if(tmp == NULL);
+   tmp = NULL;
+
+   tmp = eeze_net_addr_get(net, EEZE_NET_ADDR_TYPE_NETMASK6);
+   fail_if(tmp == NULL);
+
+   eeze_net_free(net);
+
+   ret = eeze_shutdown();
+   fail_if(ret != 0);
+}
+END_TEST
+#endif
+*/
+
+void eeze_test_net(TCase *tc)
+{
+   tcase_add_test(tc, eeze_test_net_list);
+   tcase_add_test(tc, eeze_test_net_attr);
+#ifdef HAVE_IPV6
+   //FIXME Figure out why we fail for the ipv6 tests here (code or test)
+   //tcase_add_test(tc, eeze_test_net_attr_ipv6);
+#endif
+}
---
+++ src/tests/eeze/eeze_test_sensor.c
@@ -0,0 +1,280 @@
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include <Ecore.h>
+#include <Eeze.h>
+#include <Eeze_Sensor.h>
+
+#include "eeze_suite.h"
+#include "eeze_sensor_private.h"
+
+static int cb_count = 0;
+
+static Eina_Bool
+event_cb(void *data EINA_UNUSED, int ev_type EINA_UNUSED, void *event)
+{
+   Eeze_Sensor_Obj *sens = NULL;
+   int acc;
+   double timestamp;
+   Eina_Bool rc = EINA_FALSE;
+
+   sens = event;
+   fail_if(sens == NULL);
+   rc = eeze_sensor_accuracy_get(sens, &acc);
+   fail_if(rc == EINA_FALSE);
+   rc = eeze_sensor_timestamp_get(sens, &timestamp);
+   fail_if(rc == EINA_FALSE);
+
+   /* We expect 5 callbacks from async reads right now */
+   if (++cb_count == 5)
+     ecore_main_loop_quit();
+
+   return ECORE_CALLBACK_DONE;
+}
+
+START_TEST(eeze_test_sensor_read)
+{
+   Eeze_Sensor_Obj *sens = NULL;
+   int ret = 0;
+   float x, y, z;
+   int acc;
+   double timestamp;
+   Eina_Bool rc = EINA_FALSE;
+
+   ecore_init();
+   ret = eeze_init();
+   fail_if(ret != 1);
+
+   sens = eeze_sensor_new(EEZE_SENSOR_TYPE_ACCELEROMETER);
+   fail_if(sens == NULL);
+   rc = eeze_sensor_read(sens);
+   fail_if(rc == EINA_FALSE);
+   rc = eeze_sensor_accuracy_get(sens, &acc);
+   fail_if(rc == EINA_FALSE);
+   rc = eeze_sensor_timestamp_get(sens, &timestamp);
+   fail_if(rc == EINA_FALSE);
+   rc = eeze_sensor_xyz_get(sens, &x, &y, &z);
+   fail_if(rc == EINA_FALSE);
+
+   sens = eeze_sensor_new(EEZE_SENSOR_TYPE_MAGNETIC);
+   fail_if(sens == NULL);
+   rc = eeze_sensor_read(sens);
+   fail_if(rc == EINA_FALSE);
+   rc = eeze_sensor_accuracy_get(sens, &acc);
+   fail_if(rc == EINA_FALSE);
+   rc = eeze_sensor_timestamp_get(sens, &timestamp);
+   fail_if(rc == EINA_FALSE);
+   rc = eeze_sensor_xyz_get(sens, &x, &y, &z);
+   fail_if(rc == EINA_FALSE);
+
+   sens = eeze_sensor_new(EEZE_SENSOR_TYPE_ORIENTATION);
+   fail_if(sens == NULL);
+   rc = eeze_sensor_read(sens);
+   fail_if(rc == EINA_FALSE);
+   rc = eeze_sensor_accuracy_get(sens, &acc);
+   fail_if(rc == EINA_FALSE);
+   rc = eeze_sensor_timestamp_get(sens, &timestamp);
+   fail_if(rc == EINA_FALSE);
+   rc = eeze_sensor_xyz_get(sens, &x, &y, &z);
+   fail_if(rc == EINA_FALSE);
+
+   sens = eeze_sensor_new(EEZE_SENSOR_TYPE_GYROSCOPE);
+   fail_if(sens == NULL);
+   rc = eeze_sensor_read(sens);
+   fail_if(rc == EINA_FALSE);
+   rc = eeze_sensor_accuracy_get(sens, &acc);
+   fail_if(rc == EINA_FALSE);
+   rc = eeze_sensor_timestamp_get(sens, &timestamp);
+   fail_if(rc == EINA_FALSE);
+   rc = eeze_sensor_xyz_get(sens, &x, &y, &z);
+   fail_if(rc == EINA_FALSE);
+
+   /* Use gyro with xy here even if it offers xzy */
+   sens = eeze_sensor_new(EEZE_SENSOR_TYPE_GYROSCOPE);
+   fail_if(sens == NULL);
+   rc = eeze_sensor_read(sens);
+   fail_if(rc == EINA_FALSE);
+   rc = eeze_sensor_accuracy_get(sens, &acc);
+   fail_if(rc == EINA_FALSE);
+   rc = eeze_sensor_timestamp_get(sens, &timestamp);
+   fail_if(rc == EINA_FALSE);
+   rc = eeze_sensor_xy_get(sens, &x, &y);
+   fail_if(rc == EINA_FALSE);
+
+   sens = eeze_sensor_new(EEZE_SENSOR_TYPE_PROXIMITY);
+   fail_if(sens == NULL);
+   rc = eeze_sensor_read(sens);
+   fail_if(rc == EINA_FALSE);
+   rc = eeze_sensor_accuracy_get(sens, &acc);
+   fail_if(rc == EINA_FALSE);
+   rc = eeze_sensor_timestamp_get(sens, &timestamp);
+   fail_if(rc == EINA_FALSE);
+   rc = eeze_sensor_x_get(sens, &x);
+   fail_if(rc == EINA_FALSE);
+
+   sens = eeze_sensor_new(EEZE_SENSOR_TYPE_LIGHT);
+   fail_if(sens == NULL);
+   rc = eeze_sensor_read(sens);
+   fail_if(rc == EINA_FALSE);
+   rc = eeze_sensor_accuracy_get(sens, &acc);
+   fail_if(rc == EINA_FALSE);
+   rc = eeze_sensor_timestamp_get(sens, &timestamp);
+   fail_if(rc == EINA_FALSE);
+   rc = eeze_sensor_x_get(sens, &x);
+   fail_if(rc == EINA_FALSE);
+
+   /* Call non existing type */
+   sens = eeze_sensor_new(EEZE_SENSOR_TYPE_LAST + 1);
+   fail_if(sens != NULL);
+
+   /* Give NULL as sensor object */
+   rc = eeze_sensor_read(NULL);
+   fail_if(rc != EINA_FALSE);
+
+   /* Change sensor type after creation but before read */
+   sens = eeze_sensor_new(EEZE_SENSOR_TYPE_LIGHT);
+   fail_if(sens == NULL);
+   sens->type = EEZE_SENSOR_TYPE_LAST + 1;
+   rc = eeze_sensor_read(sens);
+   fail_if(rc != EINA_FALSE);
+
+   /* Try to read from a type you can't read from */
+   sens = eeze_sensor_new(EEZE_SENSOR_TYPE_LIGHT);
+   fail_if(sens == NULL);
+   sens->type = EEZE_SENSOR_TYPE_LAST;
+   rc = eeze_sensor_read(sens);
+   fail_if(rc != EINA_FALSE);
+
+   /* Try all getter functions with a NULL sensor object */
+   rc = eeze_sensor_xyz_get(NULL, &x, &y, &z);
+   fail_if(rc != EINA_FALSE);
+   rc = eeze_sensor_xy_get(NULL, &x, &y);
+   fail_if(rc != EINA_FALSE);
+   rc = eeze_sensor_x_get(NULL, &x);
+   fail_if(rc != EINA_FALSE);
+   rc = eeze_sensor_accuracy_get(NULL, &acc);
+   fail_if(rc != EINA_FALSE);
+   rc = eeze_sensor_timestamp_get(NULL, &timestamp);
+   fail_if(rc != EINA_FALSE);
+
+   eeze_sensor_free(sens);
+
+   /* Try free on NULL */
+   eeze_sensor_free(NULL);
+
+   ret = eeze_shutdown();
+   fail_if(ret != 0);
+}
+END_TEST
+
+START_TEST(eeze_test_sensor_async_read)
+{
+   Ecore_Event_Handler *handler;
+
+   Eeze_Sensor_Obj *sens = NULL;
+   int ret = 0;
+   Eina_Bool rc = EINA_FALSE;
+
+   ecore_init();
+   ret = eeze_init();
+   fail_if(ret != 1);
+
+   handler = ecore_event_handler_add(EEZE_SENSOR_EVENT_ACCELEROMETER, event_cb, NULL);
+   fail_if(handler == NULL);
+   sens = eeze_sensor_new(EEZE_SENSOR_TYPE_ACCELEROMETER);
+   fail_if(sens == NULL);
+   rc = eeze_sensor_async_read(sens, NULL);
+   fail_if(rc == EINA_FALSE);
+
+   ecore_event_handler_add(EEZE_SENSOR_EVENT_MAGNETIC, event_cb, NULL);
+   sens = eeze_sensor_new(EEZE_SENSOR_TYPE_MAGNETIC);
+   fail_if(sens == NULL);
+   rc = eeze_sensor_async_read(sens, NULL);
+   fail_if(rc == EINA_FALSE);
+
+   ecore_event_handler_add(EEZE_SENSOR_EVENT_ORIENTATION, event_cb, NULL);
+   sens = eeze_sensor_new(EEZE_SENSOR_TYPE_ORIENTATION);
+   fail_if(sens == NULL);
+   rc = eeze_sensor_async_read(sens, NULL);
+   fail_if(rc == EINA_FALSE);
+
+   ecore_event_handler_add(EEZE_SENSOR_EVENT_GYROSCOPE, event_cb, NULL);
+   sens = eeze_sensor_new(EEZE_SENSOR_TYPE_GYROSCOPE);
+   fail_if(sens == NULL);
+   rc = eeze_sensor_async_read(sens, NULL);
+   fail_if(rc == EINA_FALSE);
+
+   ecore_event_handler_add(EEZE_SENSOR_EVENT_PROXIMITY, event_cb, NULL);
+   sens = eeze_sensor_new(EEZE_SENSOR_TYPE_PROXIMITY);
+   fail_if(sens == NULL);
+   rc = eeze_sensor_async_read(sens, NULL);
+   fail_if(rc == EINA_FALSE);
+
+   ecore_event_handler_add(EEZE_SENSOR_EVENT_LIGHT, event_cb, NULL);
+   sens = eeze_sensor_new(EEZE_SENSOR_TYPE_LIGHT);
+   fail_if(sens == NULL);
+   rc = eeze_sensor_async_read(sens, NULL);
+   fail_if(rc == EINA_FALSE);
+
+   /* Error case */
+   sens = eeze_sensor_new(EEZE_SENSOR_TYPE_LAST + 1);
+   fail_if(sens != NULL);
+   rc = eeze_sensor_async_read(NULL, NULL);
+   fail_if(rc != EINA_FALSE);
+
+   /* Change sensor type after creation but before read */
+   sens = eeze_sensor_new(EEZE_SENSOR_TYPE_LIGHT);
+   fail_if(sens == NULL);
+   sens->type = EEZE_SENSOR_TYPE_LAST + 1;
+   rc = eeze_sensor_async_read(sens, NULL);
+   fail_if(rc != EINA_FALSE);
+
+   /* Try to read from a type you can't read from */
+   sens = eeze_sensor_new(EEZE_SENSOR_TYPE_LIGHT);
+   fail_if(sens == NULL);
+   sens->type = EEZE_SENSOR_TYPE_LAST;
+   rc = eeze_sensor_async_read(sens, NULL);
+   fail_if(rc != EINA_FALSE);
+
+   ecore_main_loop_begin();
+
+   ret = eeze_shutdown();
+   fail_if(ret != 0);
+}
+END_TEST
+
+START_TEST(eeze_test_sensor_obj_get)
+{
+   Eeze_Sensor_Obj *obj = NULL, *obj_tmp = NULL;
+   int ret = 0;
+
+   ecore_init();
+   ret = eeze_init();
+   fail_if(ret != 1);
+
+   obj = calloc(1, sizeof(Eeze_Sensor_Obj));
+   fail_if(obj == NULL);
+
+   obj_tmp = obj;
+
+   obj = eeze_sensor_obj_get(EEZE_SENSOR_TYPE_ACCELEROMETER);
+   fail_if(obj == obj_tmp);
+
+   free(obj);
+
+   /* Try to get non existing obj */
+   obj_tmp = eeze_sensor_obj_get(EEZE_SENSOR_TYPE_LAST + 1);
+   fail_if(obj_tmp != NULL);
+
+   ret = eeze_shutdown();
+   fail_if(ret != 0);
+}
+END_TEST
+
+void eeze_test_sensor(TCase *tc)
+{
+   tcase_add_test(tc, eeze_test_sensor_read);
+   tcase_add_test(tc, eeze_test_sensor_async_read);
+   tcase_add_test(tc, eeze_test_sensor_obj_get);
+}
