563f616b7c0cf67ee546f0b2939e81f19fbea27a
Vincent Torri <vincent dot torri at gmail dot com>
2016-02-04 14:17:31 +0100

    Test rework #3: Ecore_Cxx

---
+++ src/tests/ecore_cxx/ecore_cxx_suite.h
@@ -0,0 +1,11 @@
+#ifndef _ECORE_CXX_SUITE_H
+#define _ECORE_CXX_SUITE_H
+
+#include <cassert>
+#include <algorithm>
+
+#include <check.h>
+
+void ecore_cxx_test_safe_call(TCase* tc);
+
+#endif /* _ECORE_CXX_SUITE_H */
