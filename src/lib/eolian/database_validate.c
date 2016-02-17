56a21ab5f22a4bddcefd4ea7bd00582ba74c651d
Cedric BAIL <cedric@osg.samsung.com>
2016-02-17 14:34:20 -0800

    Revert "eolian: strict validation for EFL (errors by default)"
    This reverts commit 21a2a8007dc53e11842526191abab9f393b3d777.
    
    Please run make check from time to time.

--- src/lib/eolian/database_validate.c
+++ src/lib/eolian/database_validate.c
@@ -174,10 +174,9 @@ _ef_map_cb(const Eina_Hash *hash EINA_UNUSED, const void *key EINA_UNUSED,
 static Eina_Bool
 _type_error(const Validator *vs, const Eolian_Type *tp, const char *msg)
 {
-   Eina_Bool weak = !getenv("EOLIAN_VALIDATE_STRICT");
    if (vs->silent_types)
-     return weak;
-   if (!weak)
+     return EINA_FALSE;
+   if (getenv("EOLIAN_VERBOSE_TYPES"))
      {
         fprintf(stderr, "eolian:%s:%d:%d: %s\n", tp->base.file, tp->base.line,
                 tp->base.column, msg);
@@ -185,7 +184,7 @@ _type_error(const Validator *vs, const Eolian_Type *tp, const char *msg)
      }
    eina_log_print(_eolian_log_dom, EINA_LOG_LEVEL_WARN, tp->base.file, "",
                   tp->base.line, "%s at column %d", msg, tp->base.column);
-   return EINA_TRUE;
+   return EINA_FALSE;
 }
 
 static Eina_Bool
@@ -213,7 +212,8 @@ _validate_type(const Validator *vs, const Eolian_Type *tp)
              {
                 char buf[256];
                 snprintf(buf, sizeof(buf), "undefined type %s", tp->full_name);
-                return _type_error(vs, tp, buf);
+                _type_error(vs, tp, buf);
+                return EINA_TRUE; /* for now only warn */
              }
            return _validate_type(vs, tpp);
         }
@@ -246,7 +246,7 @@ _validate_type(const Validator *vs, const Eolian_Type *tp)
                 char buf[256];
                 snprintf(buf, sizeof(buf), "undefined class %s "
                          "(likely wrong namespacing)", tp->full_name);
-                return _type_error(vs, tp, buf);
+                _type_error(vs, tp, buf);
              }
            return EINA_TRUE;
         }
