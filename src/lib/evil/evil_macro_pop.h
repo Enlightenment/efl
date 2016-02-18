2636fd6559a83a574e83f4977709357e1df6b912
Vincent Torri <vincent dot torri at gmail dot com>
2016-02-18 13:20:00 -0800

    Evil: remove useless defines
    Summary: Those defines are already defined in mingw-w64 header files
    
    Test Plan: makE
    
    Reviewers: cedric, jpeg
    
    Differential Revision: https://phab.enlightenment.org/D3713
    
    Signed-off-by: Cedric BAIL <cedric@osg.samsung.com>

--- src/lib/evil/evil_macro_pop.h
+++ src/lib/evil/evil_macro_pop.h
@@ -1,69 +1,6 @@
 #ifndef __EVIL_MACRO_POP_H__
 #define __EVIL_MACRO_POP_H__
 
-/* Redefine to GNU specific PRI... and SCN... macros.  */
-
-#define PRId64 "lld"
-#define PRIdLEAST64 "lld"
-#define PRIdFAST64 "lld"
-#define PRIdMAX "lld"
-#define PRIi64 "lli"
-#define PRIiLEAST64 "lli"
-#define PRIiFAST64 "lli"
-#define PRIiMAX "lli"
-#define PRIo64 "llo"
-#define PRIoLEAST64 "llo"
-#define PRIoFAST64 "llo"
-#define PRIoMAX "llo"
-#define PRIu64 "llu"
-#define PRIuLEAST64 "llu"
-#define PRIuFAST64 "llu"
-#define PRIuMAX "llu"
-#define PRIx64 "llx"
-#define PRIxLEAST64 "llx"
-#define PRIxFAST64 "llx"
-#define PRIxMAX "llx"
-#define PRIX64 "llX"
-#define PRIXLEAST64 "llX"
-#define PRIXFAST64 "llX"
-#define PRIXMAX "llX"
-
-#define SCNd64 "lld"
-#define SCNdLEAST64 "lld"
-#define SCNdFAST64 "lld"
-#define SCNdMAX "lld"
-#define SCNi64 "lli"
-#define SCNiLEAST64 "lli"
-#define SCNiFAST64 "lli"
-#define SCNiMAX "lli"
-#define SCNo64 "llo"
-#define SCNoLEAST64 "llo"
-#define SCNoFAST64 "llo"
-#define SCNoMAX "llo"
-#define SCNx64 "llx"
-#define SCNxLEAST64 "llx"
-#define SCNxFAST64 "llx"
-#define SCNxMAX "llx"
-#define SCNu64 "llu"
-#define SCNuLEAST64 "llu"
-#define SCNuFAST64 "llu"
-#define SCNuMAX "llu"
-
-#ifdef _WIN64
-#define PRIdPTR "lld"
-#define PRIiPTR "lli"
-#define PRIoPTR "llo"
-#define PRIuPTR "llu"
-#define PRIxPTR "llx"
-#define PRIXPTR "llX"
-
-#define SCNdPTR "lld"
-#define SCNiPTR "lli"
-#define SCNoPTR "llo"
-#define SCNxPTR "llx"
-#define SCNuPTR "llu"
-#endif /* _WIN64 */
-
 #undef EAPI
 #define EAPI
 
