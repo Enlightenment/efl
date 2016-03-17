9dd69c849e49421c323f5cff3c72c5137370869d
Ji-Youn Park <jy0703.park@samsung.com>
2016-03-17 17:38:52 +0830

    Elm_Glveiw: add legacy API to .h file

--- legacy/elementary/src/lib/elm_glview_legacy.h
+++ legacy/elementary/src/lib/elm_glview_legacy.h
@@ -25,8 +25,68 @@ EAPI Evas_Object *elm_glview_version_add(Evas_Object *parent, Evas_GL_Context_Ve
  *
  * @param obj The GLView object
  *
- * @ingroup GLView
+ * @ingroup Elm_GLView
  */
 EAPI void elm_glview_changed_set(Evas_Object *obj);
 
+/**
+ * Gets the size of the GLView.
+ *
+ * @param obj The GLView object
+ * @param w pointer of int width
+ * @param h pointer of int height
+ *
+ * @ingroup Elm_GLView
+ */
+EAPI void elm_glview_size_get(const Elm_Glview *obj, int *w, int *h);
+
+/**
+ * Sets the size of the GLView.
+ *
+ * @param obj The GLView object
+ * @param w width of GLView
+ * @param h height of GLView
+ *
+ * @ingroup Elm_GLView
+ */
+EAPI void elm_glview_size_set(Elm_Glview *obj, int w, int h);
+
+/**
+ * Set the init function that runs once in the main loop.
+ * @param obj The GLView object
+ * @param func The callback function
+ *
+ * @ingroup GLView
+ */
+EAPI void elm_glview_init_func_set(Elm_Glview *obj, Elm_GLView_Func_Cb func);
+
+/**
+ * Set the delete function that runs in the main loop.
+ *
+ * @param obj The GLView object
+ * @param func The callback function
+ *
+ * @ingroup Elm_GLView
+ */
+EAPI void elm_glview_del_func_set(Elm_Glview *obj, Elm_GLView_Func_Cb func);
+
+/**
+ * Set the resize function that gets called when resize happens.
+ *
+ * @param obj The GLView object
+ * @param func The callback function
+ *
+ * @ingroup Elm_GLView
+ */
+EAPI void elm_glview_resize_func_set(Elm_Glview *obj, Elm_GLView_Func_Cb func);
+
+/**
+ * Set the render function that runs in the main loop.
+ *
+ * @param obj The GLView object
+ * @param func The callback function
+ *
+ * @ingroup Elm_GLView
+ */
+EAPI void elm_glview_render_func_set(Elm_Glview *obj, Elm_GLView_Func_Cb func);
 #include "elm_glview.eo.legacy.h"
