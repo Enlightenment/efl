4cf98237eec434592e6eccbcc8519ba1a2393e86
Amitesh Singh <amitesh.sh@samsung.com>
2016-04-08 09:09:59 +0530

    doc: add elm nstate preview

---
+++ doc/widgets/widget_preview_nstate.c
@@ -0,0 +1,10 @@
+#include "widget_preview_tmpl_head.c"
+
+Evas_Object *o = eo_add(ELM_NSTATE_CLASS, win);
+evas_object_size_hint_weight_set(o, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+elm_win_resize_object_add(win, o);
+evas_object_show(o);
+
+elm_object_text_set(o, "Nstate");
+
+#include "widget_preview_tmpl_foot.c"
