b4da02cac26326a7c9bf0beeffcd8db00c0835f5
Tom Hacohen <tom@stosb.com>
2016-04-12 15:39:36 +0100

    Elm test web: Correctly disable the web tests.
    This gets rid of some clang warnings.

--- src/tests/elementary/elm_test_web.c
+++ src/tests/elementary/elm_test_web.c
@@ -9,6 +9,7 @@
 
 START_TEST (elm_atspi_role_get)
 {
+#if 0
    Evas_Object *win, *web;
    Elm_Atspi_Role role;
 
@@ -21,10 +22,11 @@ START_TEST (elm_atspi_role_get)
    ck_assert(role == ELM_ATSPI_ROLE_HTML_CONTAINER);
 
    elm_shutdown();
+#endif
 }
 END_TEST
 
 void elm_test_web(TCase *tc)
 {
- /* tcase_add_test(tc, elm_atspi_role_get); */
+   tcase_add_test(tc, elm_atspi_role_get);
 }
