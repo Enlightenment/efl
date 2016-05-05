6124039c8f11936f78c08fd9b8dfd9c128c05ea2
Felipe Magno de Almeida <felipe@expertisesolutions.com.br>
2016-05-04 21:13:48 -0300

    eolian-cxx: Added test for the removal of the .Base class requirement
    Test creates class with the same name as the namespace of another class

--- src/tests/eolian_cxx/name_name.c
+++ src/tests/eolian_cxx/name_name.c
@@ -4,5 +4,15 @@
 struct Name_Name_Data {};
 typedef struct Name_Name_Data Name_Name_Data;
 
+struct Ns_Name_Data {};
+typedef struct Ns_Name_Data Ns_Name_Data;
+
+struct Ns_Name_Other_Data {};
+typedef struct Ns_Name_Other_Data Ns_Name_Other_Data;
+
 #include "name_name.eo.h"
 #include "name_name.eo.c"
+#include "ns_name.eo.h"
+#include "ns_name.eo.c"
+#include "ns_name_other.eo.h"
+#include "ns_name_other.eo.c"
--- src/tests/eolian_cxx/name_name_cxx.cc
+++ src/tests/eolian_cxx/name_name_cxx.cc
@@ -3,4 +3,8 @@
 
 #include "name_name.eo.h"
 #include "name_name.eo.hh"
+#include "ns_name.eo.h"
+#include "ns_name.eo.hh"
+#include "ns_name_other.eo.h"
+#include "ns_name_other.eo.hh"
 
