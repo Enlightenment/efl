e01ac62e5f36614e3a5195105f28bf5d9b05be08
Chris Michael <cpmichael@osg.samsung.com>
2016-05-31 09:49:43 -0400

    ecore-evas-drm: Disable direct input callback
    For ecore_evas drm engine(s), disable setting of
    ecore_event_window_direct_callback as this Completely Breaks all input
    when running Enlightenment Wayland.
    
    NB: This can likely be re-enabled at some point, when the jpeg
    breakage is over ;)
    
    @fix
    
    Signed-off-by: Chris Michael <cpmichael@osg.samsung.com>

--- src/modules/ecore_evas/engines/drm/ecore_evas_drm.c
+++ src/modules/ecore_evas/engines/drm/ecore_evas_drm.c
@@ -770,7 +770,7 @@ ecore_evas_drm_new_internal(const char *device, unsigned int parent EINA_UNUSED,
                                (Ecore_Event_Multi_Move_Cb)_ecore_evas_mouse_multi_move_process,
                                (Ecore_Event_Multi_Down_Cb)_ecore_evas_mouse_multi_down_process,
                                (Ecore_Event_Multi_Up_Cb)_ecore_evas_mouse_multi_up_process);
-   _ecore_event_window_direct_cb_set(ee->prop.window, _ecore_evas_input_direct_cb);
+   /* _ecore_event_window_direct_cb_set(ee->prop.window, _ecore_evas_input_direct_cb); */
 
    ecore_drm2_output_crtc_size_get(edata->output, &mw, &mh);
 
@@ -906,7 +906,7 @@ ecore_evas_gl_drm_new_internal(const char *device, unsigned int parent EINA_UNUS
                                (Ecore_Event_Multi_Move_Cb)_ecore_evas_mouse_multi_move_process,
                                (Ecore_Event_Multi_Down_Cb)_ecore_evas_mouse_multi_down_process,
                                (Ecore_Event_Multi_Up_Cb)_ecore_evas_mouse_multi_up_process);
-   _ecore_event_window_direct_cb_set(ee->prop.window, _ecore_evas_input_direct_cb);
+   /* _ecore_event_window_direct_cb_set(ee->prop.window, _ecore_evas_input_direct_cb); */
 
    ecore_drm2_output_crtc_size_get(edata->output, &mw, &mh);
 
