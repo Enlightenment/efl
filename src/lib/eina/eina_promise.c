Enlightenment/efl

commit dc73f6644e0ed8fdbd2c6c8187591fb7eb8027c7

Eina promise: Simplify code and remove shadow variable.
There was no need for the extra variable. This also fixes a shadow
warning.

src/lib/eina/eina_promise.c
@@ -280,8 +280,7 @@ _eina_promise_pointer_value_get(_Eina_Promise_Default const* p)
    if (p->has_finished && !p->has_errored)
      {
         char const* buffer = promise->value;
-        void** p = (void**)buffer;
-        return *p;
+        return (void**)buffer;
      }
    else
      {

commit 4c3b7fb2481a7e6c971e9606c09a5a271ecf5c06

Eina promise: Fix broken return value.
I broke it in dc73f6644e0ed8fdbd2c6c8187591fb7eb8027c7.
Excuse of the day: I'm not working from my usual spot and there's screen glare
here that made me not see the *. Good excuse?

src/lib/eina/eina_promise.c
@@ -280,7 +280,7 @@ _eina_promise_pointer_value_get(_Eina_Promise_Default const* p)
    if (p->has_finished && !p->has_errored)
      {
         char const* buffer = promise->value;
-        return (void**)buffer;
+        return *((void**)buffer);
      }
    else
      {