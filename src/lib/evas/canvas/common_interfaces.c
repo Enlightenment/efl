1dbff7d2b80ab54f6bdde306888da3a0c5a208f4
Jean-Philippe Andre <jp.andre@samsung.com>
2016-06-10 17:08:05 +0900

    Evas: Remove Evas.Signal_Interface
    It was basically empty.

--- src/lib/evas/canvas/common_interfaces.c
+++ src/lib/evas/canvas/common_interfaces.c
@@ -7,16 +7,6 @@ static const Eo_Event_Description *_evas_scrollable_interface_event_desc[];
 static const Eo_Event_Description *_evas_selectable_interface_event_desc[];
 static const Eo_Event_Description *_evas_zoomable_interface_event_desc[];
 
-static void
-_evas_signal_interface_class_constructor(Eo_Class *klass EINA_UNUSED)
-{
-}
-
-static void
-_evas_signal_interface_class_destructor(Eo_Class *klass EINA_UNUSED)
-{
-}
-
 #include "canvas/evas_signal_interface.eo.c"
 #include "canvas/evas_draggable_interface.eo.c"
 #include "canvas/evas_clickable_interface.eo.c"
