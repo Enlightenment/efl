fa19b3745850457728fc8573c127c1818a564d35
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2016-06-15 14:04:28 +0900

    shader gen - make this use plain sh not bash for portability
    this should fix T3793

--- src/modules/evas/engines/gl_common/shader/gen_shaders.sh
+++ src/modules/evas/engines/gl_common/shader/gen_shaders.sh
@@ -1,38 +1,39 @@
-#!/bin/bash
+#!/bin/sh
 
 # This script will generate a C file containing all the shaders used by Evas
 
 DIR=`dirname $0`
 
-OUTPUT=${DIR}/evas_gl_shaders.x
+OUTPUT="$DIR/evas_gl_shaders.x"
 
 # Skip generation if there is no diff (or no git)
 if ! git rev-parse 2>> /dev/null >> /dev/null ; then exit 0 ; fi
 if git diff --quiet --exit-code -- "$DIR"
 then
-  touch "${OUTPUT}"
+  touch "$OUTPUT"
   exit 0
 fi
 
 exec 1<&-
-exec 1>${OUTPUT}
+exec 1>"$OUTPUT"
 
 # Write header
 printf "/* DO NOT MODIFY THIS FILE AS IT IS AUTO-GENERATED */\n"
 printf "/* IF IT IS CHANGED PLEASE COMMIT THE CHANGES */\n\n"
 
-for shd in fragment vertex ; do
-  m4 ${DIR}/include.glsl ${DIR}/${shd}.glsl > ${shd}.tmp
+for SHD in fragment vertex ; do
+  m4 "$DIR/include.glsl" "$DIR/$SHD.glsl" > "$SHD.tmp"
 
-  OIFS=$IFS
-  IFS=$'\n'
-  printf "static const char ${shd}_glsl[] ="
-  for line in `cat ${shd}.tmp` ; do
-      printf "\n   \"${line}\\\n\""
+  OIFS="$IFS"
+  IFS=`printf '\n+'`
+  IFS=${IFS%+}
+  printf "static const char "$SHD"_glsl[] ="
+  for LINE in `cat "$SHD.tmp"` ; do
+      printf "\n   \"$LINE\\\n\""
   done
   printf ";\n\n"
-  IFS=${OIFS}
+  IFS="$OIFS"
 
-  rm ${shd}.tmp
+  rm "$SHD.tmp"
 done
 
--- src/modules/evas/engines/gl_common/shader_3d/gen_shaders_3d.sh
+++ src/modules/evas/engines/gl_common/shader_3d/gen_shaders_3d.sh
@@ -1,60 +1,61 @@
-#!/bin/bash
+#!/bin/sh
 
 # This script will generate a C file containing all the shaders used by Evas_3D
 
 DIR=`dirname $0`
 
-OUTPUT=${DIR}/evas_gl_3d_shaders.x
+OUTPUT="$DIR/evas_gl_3d_shaders.x"
 
 # Skip generation if there is no diff (or no git)
 if ! git rev-parse 2>> /dev/null >> /dev/null ; then exit 0 ; fi
 if git diff --quiet --exit-code -- "$DIR"
 then
-  touch "${OUTPUT}"
+  touch "$OUTPUT"
   exit 0
 fi
 
 exec 1<&-
-exec 1>${OUTPUT}
+exec 1>"$OUTPUT"
 
-SHADERS="$@"
-vert_shaders_source=""
-frag_shaders_source=""
+SHADERS=$@
+VERT_SHADERS_SOURCE=""
+FRAG_SHADERS_SOURCE=""
 
 # Write header
 printf "/* DO NOT MODIFY THIS FILE AS IT IS AUTO-GENERATED */\n"
 printf "/* IF IT IS CHANGED PLEASE COMMIT THE CHANGES */\n\n"
 
-for shd in ${SHADERS} ; do
-  lname=`basename ${shd} .shd`
+for SHD in $SHADERS ; do
+  LNAME=`basename "$SHD" .shd`
 
-  if echo ${lname} |grep _vert 2>&1 >> /dev/null ; then
-    vert_shaders_source="${vert_shaders_source}    ${lname}_glsl,\n"
+  if echo "$LNAME" | grep _vert 2>&1 >> /dev/null ; then
+    VERT_SHADERS_SOURCE="$VERT_SHADERS_SOURCE    "$LNAME"_glsl,\n"
   fi
-  if echo ${lname} |grep _frag 2>&1 >> /dev/null ; then
-    frag_shaders_source="${frag_shaders_source}    ${lname}_glsl,\n"
+  if echo "$LNAME" | grep _frag 2>&1 >> /dev/null ; then
+    FRAG_SHADERS_SOURCE="$FRAG_SHADERS_SOURCE    "$LNAME"_glsl,\n"
   fi
 
-  m4 ${DIR}/include.shd ${shd} > ${shd}.tmp
+  m4 "$DIR/include.shd" "$SHD" > "$SHD.tmp"
 
-  OIFS=$IFS
-  IFS=$'\n'
-  printf "static const char ${lname}_glsl[] ="
-  for line in `cat ${shd}.tmp` ; do
-      printf "\n   \"${line}\\\n\""
+  OIFS="$IFS"
+  IFS=`printf '\n+'`
+  IFS=${IFS%+}
+  printf "static const char "$LNAME"_glsl[] ="
+  for LINE in `cat "$SHD.tmp"` ; do
+      printf "\n   \"$LINE\\\n\""
   done
   printf ";\n\n"
-  IFS=${OIFS}
+  IFS="$OIFS"
 
-  rm ${shd}.tmp
+  rm "$SHD.tmp"
 done
 
 printf "static const char *vertex_shaders[] =
 {\n"
-      printf "${vert_shaders_source}"
+      printf "$VERT_SHADERS_SOURCE"
 printf "};\n\n"
 
 printf "static const char *fragment_shaders[] =
 {\n"
-      printf "${frag_shaders_source}"
+      printf "$FRAG_SHADERS_SOURCE"
 printf "};\n"
