33bb4583b86408450cbb2be6efb7b7d63f11b11e
Jean Guyomarc'h <jean@guyomarch.bzh>
2016-06-15 22:33:39 +0200

    eina: fix compiling of safepointers on OSX
    This is a reccurring compatibility issue...
    MAP_ANONYMOUS is not defined on OSX, but MAP_ANON is.
    I know MAP_ANON is marked as deprecated in the Linux man
    pages, but it has the benefit of being more portable.

--- src/lib/eina/eina_safepointer.c
+++ src/lib/eina/eina_safepointer.c
@@ -75,7 +75,7 @@ _eina_safepointer_calloc(int number, size_t size)
      * MEM_PAGE_SIZE;
 
    header = mmap(NULL, newsize, PROT_READ | PROT_WRITE,
-                 MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+                 MAP_PRIVATE | MAP_ANON, -1, 0);
    if (header == MAP_FAILED)
      {
         ERR("mmap of Eina_Safepointer table region failed.");
