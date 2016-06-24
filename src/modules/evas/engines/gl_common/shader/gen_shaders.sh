e1d5d5e4a1817e148e51a8e1ae15969f826f4b1d
Jean-Philippe Andre <jp.andre@samsung.com>
2016-06-24 14:36:10 +0900

    evas: (Try to) fix shaders gen without breaking everything
    So... adding the .x files to BUILT_SOURCES means they would
    get deleted during clean. This was bad, but turned to be awful
    when this made the check for writability fail.
    
    See:
    8a273158788d630e56b4dc14cdd44f0eaae6b7b5
    2b8912462e0ec81efb8e30a63383b811f52e16f5
    
    This commit reverts:
    0b69356f1bbceab3261835bc2d4632f8f4669206
    
    Notes:
    Please revert if this breaks the build for anyone.
    I'm pretty sure the use of top_srcdir is not recommended the
    way I've done it.
    
    I hate autofoo.

--- src/modules/evas/engines/gl_common/shader/gen_shaders.sh
+++ src/modules/evas/engines/gl_common/shader/gen_shaders.sh
@@ -3,8 +3,15 @@
 # This script will generate a C file containing all the shaders used by Evas
 
 DIR=`dirname $0`
+cd $DIR/../../../../../
 
 OUTPUT="$DIR/evas_gl_shaders.x"
+
+# Skip generation during make distcheck
+if [ "${top_distdir}" != "" ] ; then exit 0; fi
+
+# Skip generation if file can not be written to
+if [ -e ${OUTPUT} ] && [ ! -w ${OUTPUT} ] ; then exit 0; fi
 
 # Skip generation if there is no diff (or no git)
 if ! git rev-parse 2>> /dev/null >> /dev/null ; then exit 0 ; fi
