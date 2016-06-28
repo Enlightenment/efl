ee33978dc33df995bda44a95bc3acc40cc715d05
Cedric BAIL <cedric@osg.samsung.com>
2016-06-28 11:15:50 -0700

    elementary: try to keep const type in config test.

--- src/tests/elementary/elm_test_config.c
+++ src/tests/elementary/elm_test_config.c
@@ -166,10 +166,10 @@ START_TEST (elm_config_win)
 END_TEST
 
 static inline Eina_Bool
-_eina_list_string_has(Eina_List *list, const char *str)
+_eina_list_string_has(const Eina_List *list, const char *str)
 {
    const char *s;
-   Eina_List *l;
+   const Eina_List *l;
 
    EINA_LIST_FOREACH(list, l, s)
      if (eina_streq(str, s))
