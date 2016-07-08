b32a8d3a0f237d783dd7b43823f1ae524276a978
Jean-Philippe Andre <jp.andre@samsung.com>
2016-07-08 12:24:42 +0900

    ecore: Fix warning about err_no
    Double warning, yay:
     explicitly assigning value of variable of type 'int' to itself
     variable 'err_no' is uninitialized when used here
    
    See 1abbfdd1f7b09ba9931657d57ffc4172c662c2eb

--- src/lib/ecore/ecore_main.c
+++ src/lib/ecore/ecore_main.c
@@ -1766,7 +1766,7 @@ _ecore_main_select(double timeout)
 
    eina_evlog("!SLEEP", NULL, 0.0, t ? "timeout" : "forever");
    ret = main_loop_select(max_fd + 1, &rfds, &wfds, &exfds, t);
-   err_no = err_no;
+   err_no = errno;
    eina_evlog("!WAKE", NULL, 0.0, NULL);
 
    _ecore_time_loop_time = ecore_time_get();
