b33b2ddc3934289af3c8c6f323e4b83056d52261
Cedric Bail <cedric@osg.samsung.com>
2016-07-19 09:51:13 -0700

    elementary: as screen get bigger, we need more cache to store uncompressed image.

--- src/bin/elementary/config.c
+++ src/bin/elementary/config.c
@@ -4106,7 +4106,7 @@ _status_config_caches(Evas_Object *win,
    elm_slider_span_size_set(sl, 120);
    elm_slider_unit_format_set(sl, "%1.0f MiB");
    elm_slider_indicator_format_set(sl, "%1.0f");
-   elm_slider_min_max_set(sl, 0, 32);
+   elm_slider_min_max_set(sl, 0, 512);
    elm_slider_value_set(sl, (double)elm_config_cache_image_cache_size_get() / 1024.0);
    elm_box_pack_end(bx, sl);
    evas_object_show(sl);
