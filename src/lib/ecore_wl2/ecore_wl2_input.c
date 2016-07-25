f94d349f523905623124e685e8de8ac64f24d18e
Marcel Hollerbach <marcel-hollerbach@t-online.de>
2016-07-25 17:08:05 +0200

    ecore_wl2: also add the del control code
    otherwise we are sending the controlcode for delete, which is wrong.
    
    This fixes the backspace on delete problem.

--- src/lib/ecore_wl2/ecore_wl2_input.c
+++ src/lib/ecore_wl2/ecore_wl2_input.c
@@ -421,7 +421,9 @@ _ecore_wl2_input_symbol_rep_find(xkb_keysym_t keysym, char *buffer, int size, un
     n = xkb_keysym_to_utf8(keysym, buffer, size);
 
     /* check if we are a control code */
-    if (n > 0 && !(buffer[0] > 0x0 && buffer[0] < 0x20))
+    if (n > 0 && !(
+        (buffer[0] > 0x0 && buffer[0] < 0x20) || /* others 0x0 to 0x1F control codes */
+        buffer[0] == 0x7F)) /*delete control code */
       return;
 
     if (xkb_keysym_get_name(keysym, buffer, size) != 0)
