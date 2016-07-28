f7522886666b785430d9a2af477cb953b97c3f61
Stefan Schmidt <stefan@osg.samsung.com>
2016-07-28 16:00:31 +0200

    eldbus: make sure eldbus model is hidden behind the BETA API flag
    Make sure we keep this in beta for now until our EO based interfaces are ready.

--- src/lib/eldbus/Eldbus_Model.h
+++ src/lib/eldbus/Eldbus_Model.h
@@ -9,12 +9,16 @@
 extern "C" {
 #endif
 
+#ifdef EFL_BETA_API_SUPPORT
+
 #include <eldbus_model_connection.eo.h>
 #include <eldbus_model_object.eo.h>
 #include <eldbus_model_proxy.eo.h>
 #include <eldbus_model_arguments.eo.h>
 #include <eldbus_model_method.eo.h>
 #include <eldbus_model_signal.eo.h>
+
+#endif
 
 #ifdef __cplusplus
 }
