fb6ffc6ac52f92de67c2602975df2418e3af0d62
InHong Han <inhong1.han@samsung.com>
2016-08-03 16:40:24 +0900

    elm_entry: Add voice input panel layout
    @feature
    
    Change-Id: I41502d2446b95a4be31fc60a8d995b9d37930844
    Signed-off-by: InHong Han <inhong1.han@samsung.com>

--- src/lib/ecore_imf/Ecore_IMF.h
+++ src/lib/ecore_imf/Ecore_IMF.h
@@ -310,7 +310,8 @@ typedef enum
    ECORE_IMF_INPUT_PANEL_LAYOUT_TERMINAL,        /**< Command-line terminal layout including ESC, Alt, Ctrl key, so on (no auto-correct, no auto-capitalization) @since 1.2 */
    ECORE_IMF_INPUT_PANEL_LAYOUT_PASSWORD,        /**< Like normal, but no auto-correct, no auto-capitalization etc. @since 1.2 */
    ECORE_IMF_INPUT_PANEL_LAYOUT_DATETIME,        /**< Date and time layout @since 1.8 */
-   ECORE_IMF_INPUT_PANEL_LAYOUT_EMOTICON         /**< Emoticon layout @since 1.10 */
+   ECORE_IMF_INPUT_PANEL_LAYOUT_EMOTICON,        /**< Emoticon layout @since 1.10 */
+   ECORE_IMF_INPUT_PANEL_LAYOUT_VOICE            /**< Voice layout, but if the IME does not support voice layout, then normal layout will be shown @since 1.19 */
 } Ecore_IMF_Input_Panel_Layout;
 
 /**
