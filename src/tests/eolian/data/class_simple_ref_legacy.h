96be95d958f1007baf6cc70f3d7873c9be66d77d
Daniel Kolesa <d.kolesa@osg.samsung.com>
2016-08-15 12:50:52 +0100

    eolian tests: fix reference file after beta in legacy change

--- src/tests/eolian/data/class_simple_ref_legacy.h
+++ src/tests/eolian/data/class_simple_ref_legacy.h
@@ -17,43 +17,7 @@ typedef Eo Class_Simple;
  *
  * @ingroup Class_Simple
  */
-
-/**
- * @brief Common desc for a
- *
- * comment a.set
- *
- * @param[in] value Value description
- *
- * @return comment for property set return
- *
- * @ingroup Class_Simple
- */
-EAPI Eina_Bool evas_object_simple_a_set(Class_Simple *obj, int value);
-
-/**
- * @brief Common desc for a
- *
- * @return Value description
- *
- * @ingroup Class_Simple
- */
-EAPI int evas_object_simple_a_get(const Class_Simple *obj);
 EAPI void evas_object_simple_b_set(Class_Simple *obj);
-
-/**
- * @brief comment foo
- *
- * @param[in] a a
- * @param[in,out] b
- * @param[out] c
- * @param[in] d
- *
- * @return comment for method return
- *
- * @ingroup Class_Simple
- */
-EAPI char *evas_object_simple_foo(Class_Simple *obj, int a, char *b, double *c, int *d);
 EAPI int *evas_object_simple_bar(Class_Simple *obj, int x);
 
 #endif
