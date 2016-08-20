52295607e5282f67067dbc64685eb0d15dfd8c91
Jean Guyomarc'h <jean@guyomarch.bzh>
2016-08-20 14:03:15 +0200

    evas-engines: fix compiling of the fb engine
    Build was broken after commit 0c21a926a322f810352a654b82455a41e85f06b2
    which changed an internal API.

--- src/modules/evas/engines/fb/evas_engine.c
+++ src/modules/evas/engines/fb/evas_engine.c
@@ -47,6 +47,7 @@ _output_setup(int w, int h, int rot, int vt, int dev, int refresh)
                                                  evas_fb_outbuf_fb_get_rot,
                                                  evas_fb_outbuf_fb_reconfigure,
                                                  NULL,
+                                                 NULL,
                                                  evas_fb_outbuf_fb_new_region_for_update,
                                                  evas_fb_outbuf_fb_push_updated_region,
                                                  evas_fb_outbuf_fb_free_region_for_update,
