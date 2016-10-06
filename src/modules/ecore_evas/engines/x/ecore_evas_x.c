690319b2f3238b0d9889ecefc4dbd46f70d66d4c
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2016-10-06 10:58:04 +0900

    ecore-evas - x fix intial iconified state so terminology -I works
    this fixes intitial iconic state for x11 as demonstrated by
    
    terminology -I
    
    but enlightenment is broken though... xterm -iconic also shows the
    same break with a black window.
    
    @fix

--- src/modules/ecore_evas/engines/x/ecore_evas_x.c
+++ src/modules/ecore_evas/engines/x/ecore_evas_x.c
@@ -3363,8 +3363,7 @@ _ecore_evas_x_iconified_set(Ecore_Evas *ee, Eina_Bool on)
    Ecore_Evas_Engine_Data_X11 *edata = ee->engine.data;
 
    if (ee->prop.iconified == on) return;
-   if (((ee->should_be_visible) && (!ee->visible)) || (ee->visible))
-     ee->prop.iconified = on;
+   ee->prop.iconified = on;
    _ecore_evas_x_hints_update(ee);
    if (on)
      ecore_x_icccm_iconic_request_send(ee->prop.window, edata->win_root);
