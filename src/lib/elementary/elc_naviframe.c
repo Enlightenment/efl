db0378911a3a5bce6c2a288044b0964e29327813
Jaehyun Cho <jae_hyun.cho@samsung.com>
2016-10-07 17:54:55 +0900

    naviframe: Fix to add SIG_ITEM_ACTIVATED in smart callbacks.
    This commit fixes commit 3a0eb75bda1bd9274e4c9de1b6ce9143b24cfc80.

--- src/lib/elementary/elc_naviframe.c
+++ src/lib/elementary/elc_naviframe.c
@@ -40,6 +40,7 @@ static const Evas_Smart_Cb_Description _smart_callbacks[] = {
    {SIG_WIDGET_ACCESS_CHANGED, ""}, /**< handled by elm_widget */
    {SIG_LAYOUT_FOCUSED, ""}, /**< handled by elm_layout */
    {SIG_LAYOUT_UNFOCUSED, ""}, /**< handled by elm_layout */
+   {SIG_ITEM_ACTIVATED, ""},
    {NULL, NULL}
 };
 
