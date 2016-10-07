9553fd96401e2391ddc3143a64ea9d629c73fb37
Daniel Kolesa <d.kolesa@osg.samsung.com>
2016-10-07 13:34:47 +0200

    eolian gen2: move to eolian gen (as the old one is gone)

--- src/tests/eolian/eolian_generation.c
+++ src/tests/eolian/eolian_generation.c
@@ -89,7 +89,7 @@ _eolian_gen_execute(const char *eo_filename, const char *options, const char *ou
    if (getenv("EFL_RUN_IN_TREE"))
      {
         snprintf(eolian_gen_path, sizeof(eolian_gen_path),
-              "%s/src/bin/eolian2/eolian_gen2%s",
+              "%s/src/bin/eolian/eolian_gen%s",
               PACKAGE_BUILD_DIR, EXEEXT);
      }
    if (eolian_gen_path[0] == '\0')
