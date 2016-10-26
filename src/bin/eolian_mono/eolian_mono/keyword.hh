5e5a6884c03277cca120166688c2a32fa87ff7bb
Felipe Magno de Almeida <felipe@expertisesolutions.com.br>
2016-10-26 17:32:59 -0200

    mono: Type changes

--- src/bin/eolian_mono/eolian_mono/keyword.hh
+++ src/bin/eolian_mono/eolian_mono/keyword.hh
@@ -5,8 +5,8 @@ namespace eolian_mono {
 
 inline std::string escape_keyword(std::string const& name)
 {
-  if(name == "delete" || name == "register")
-    return "cxx_" + name;
+  if(name == "delete" || name == "register" || name == "do")
+    return "mono_" + name;
   return name;
 }
       
