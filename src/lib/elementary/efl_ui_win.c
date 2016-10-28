3749ea47f9bdd21409adf6b9e259b42ac377fb2e
Chris Michael <cp.michael@samsung.com>
2016-10-28 14:27:17 -0400

    elementary: Remove debug printing
    NB: No functional changes, just removing left over prints
    
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

--- src/lib/elementary/efl_ui_win.c
+++ src/lib/elementary/efl_ui_win.c
@@ -2972,14 +2972,11 @@ _elm_win_wl_cursor_set(Evas_Object *obj, const char *cursor)
      {
         Evas_Coord mw = 1, mh = 1, hx = 0, hy = 0;
 
-        fprintf(stderr, "Elm Win Cursor Set: %s\n", cursor);
-
         if (cursor)
           {
              if (!_elm_theme_object_set(sd->obj, sd->pointer.obj,
                                         "cursor", cursor, "default"))
                {
-                  fprintf(stderr, "\tCursor %s not found in theme\n", cursor);
                   _elm_theme_object_set(sd->obj, sd->pointer.obj,
                                         "pointer", "base", "default");
                }
@@ -2995,7 +2992,6 @@ _elm_win_wl_cursor_set(Evas_Object *obj, const char *cursor)
              mw = 32;
              mh = 32;
           }
-        fprintf(stderr, "\tMin Size: %d %d\n", mw, mh);
         evas_object_move(sd->pointer.obj, 0, 0);
         evas_object_resize(sd->pointer.obj, mw, mh);
         edje_object_part_geometry_get(sd->pointer.obj,
