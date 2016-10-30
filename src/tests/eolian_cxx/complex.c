c8504352139900ec8f747e96223e57a9a9b13d81
Jean Guyomarc'h <jean@guyomarch.bzh>
2016-10-30 19:12:24 +0100

    tests: fix eolian_cxx tests with clang
    eolian_cxx tests failed to link because of massively inexistant symbols.
    
    I assume eolian_cxx tests have been working at some point. Maybe they
    were gcc-only? I don't get what's going on with gcc and non-existant
    symbols. Is there a sugar-coating of some kind? Because when a symbol
    does not exist, clang throws you away. Is it because we are only
    referring to the eo implementation functions via function pointers?
    That's the second time I'm doing a fix like this. Maybe we should change
    a bit our linking flags (see --unresolved-symbols in ld)??
    
    Anyway, now we have our symbols. Clang is happy, make check can go on...

--- src/tests/eolian_cxx/complex.c
+++ src/tests/eolian_cxx/complex.c
@@ -10,4 +10,243 @@ struct Complex_Data {};
 typedef struct Complex_Data Complex_Data;
 
 #include "complex.eo.h"
+
+EOLIAN static void
+_complex_inptrcont(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_List *l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_inclasscont(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_List *l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_inptrptrcont(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_List *l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_inptrcontown(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_List *l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_inptrptrcontown(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_List *l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_incontcont(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_List *l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_incontcontown(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_List *l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_incontowncontown(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_List *l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_incontowncont(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_List *l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_instringcont(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_List *l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_instringowncont(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_List *l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_instringcontown(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_List *l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_inarray(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_Array *l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_inarrayown(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_Array *l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_inhash(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_Hash *l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_inhashown(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_Hash *l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_initerator(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_Iterator *l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_initeratorown(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_Iterator *l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_inaccessor(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_Accessor *l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_inaccessorown(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_Accessor *l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_outptrcont(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_List **l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_outclasscont(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_List **l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_outcontcont(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_List **l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_outcontcontown(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_List **l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_outcontowncontown(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_List **l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_outcontowncont(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_List **l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_outstringcont(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_List **l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_outstringowncont(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_List **l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_outstringcontown(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_List **l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_outarray(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_Array **l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_outarrayown(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_Array **l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_outhash(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_Hash **l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_outhashown(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_Hash **l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_outiterator(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_Iterator **l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_outiteratorown(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_Iterator **l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_outaccessor(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_Accessor **l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_outaccessorown(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_Accessor **l EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_foo(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_List *l EINA_UNUSED)
+{
+}
+
+EOLIAN static Eina_Array *
+_complex_bar(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED)
+{
+   return NULL;
+}
+
+EOLIAN static Complex *
+_complex_wrapper_r(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED)
+{
+   return NULL;
+}
+
+EOLIAN static void
+_complex_wrapper_in(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Complex *a1 EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_wrapper_inout(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Complex **a1 EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_wrapper_out(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Complex **a1 EINA_UNUSED)
+{
+}
+
+EOLIAN static Eina_Promise *
+_complex_with_promise_r(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED)
+{
+   return NULL;
+}
+
+EOLIAN static void
+_complex_with_promise_in(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_Promise *p EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_with_promise_out(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_Promise **p EINA_UNUSED)
+{
+}
+
+EOLIAN static void
+_complex_with_promise_inout(Eo *obj EINA_UNUSED, Complex_Data *pd EINA_UNUSED, Eina_Promise_Owner *p EINA_UNUSED)
+{
+}
+
 #include "complex.eo.c"
