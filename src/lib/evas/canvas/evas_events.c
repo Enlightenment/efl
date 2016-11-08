Enlightenment/efl

commit 40fec5f608da4c5188a2afc7cc293d4f74a4b68d

evas: Fix legacy events output vs. canvas coords
In case of a mapped object (eg. when applying a map to a window
in wayland compositor), the canvas and output coordinates are
not meant to be the same.

In EO land, applications should instead use the common interface
Efl.Input.Interface.pointer_xy.get (on the canvas).

@fix

src/lib/evas/canvas/evas_events_legacy.c
@@ -33,12 +33,14 @@ _event_alloc(void *old)
 }
 
 void *
-efl_input_pointer_legacy_info_fill(Efl_Input_Key *evt, Evas_Callback_Type type, Evas_Event_Flags **pflags)
+efl_input_pointer_legacy_info_fill(Evas *eo_evas, Efl_Input_Key *eo_ev, Evas_Callback_Type type, Evas_Event_Flags **pflags)
 {
-   Efl_Input_Pointer_Data *ev = efl_data_scope_get(evt, EFL_INPUT_POINTER_CLASS);
-   if (!ev) return NULL;
+   Efl_Input_Pointer_Data *ev = efl_data_scope_get(eo_ev, EFL_INPUT_POINTER_CLASS);
+   Evas_Public_Data *evas = efl_data_scope_get(eo_evas, EVAS_CANVAS_CLASS);
 
-#define COORD_DUP(e) do { (e)->output.x = (e)->canvas.x; (e)->output.y = (e)->canvas.y; } while (0)
+   if (!ev || !evas) return NULL;
+
+#define COORD_DUP(e) do { (e)->output.x = evas->pointer.x; (e)->output.y = evas->pointer.y; } while (0)
 #define TYPE_CHK(typ) do { if (type != EVAS_CALLBACK_ ## typ) return NULL; } while (0)
 
    switch (ev->action)

src/lib/evas/include/evas_private.h
@@ -1916,7 +1916,7 @@ void _evas_device_cleanup(Evas *e);
 Evas_Device *_evas_device_top_get(const Evas *e);
 
 /* legacy/eo events */
-void *efl_input_pointer_legacy_info_fill(Efl_Input_Key *evt, Evas_Callback_Type type, Evas_Event_Flags **pflags);
+void *efl_input_pointer_legacy_info_fill(Evas *eo_evas, Efl_Input_Key *eo_ev, Evas_Callback_Type type, Evas_Event_Flags **pflags);
 void *efl_input_key_legacy_info_fill(Efl_Input_Key *evt, Evas_Event_Flags **pflags);
 void *efl_input_hold_legacy_info_fill(Efl_Input_Hold *evt, Evas_Event_Flags **pflags);
 

src/lib/evas/canvas/evas_callbacks.c
@@ -167,7 +167,7 @@ _eo_evas_object_cb(void *data, const Efl_Event *event)
    switch (info->efl_event_type)
      {
       case EFL_EVENT_TYPE_POINTER:
-        event_info = efl_input_pointer_legacy_info_fill(efl_event_info, info->type, &event_flags);
+        event_info = efl_input_pointer_legacy_info_fill(evas, efl_event_info, info->type, &event_flags);
         break;
 
       case EFL_EVENT_TYPE_KEY:

commit 0c850f67ceb772c92637e7826baf9f39bbc8001a

evas: Fix previous pointer position in output coords
This fixes the test "Window Socket" map resize.
I broke that in 40fec5f608da4c5188a2afc7cc293d4f

Makes me wonder if the previous position should be exposed
in EO as well.

src/lib/evas/canvas/evas_events.c
@@ -1732,6 +1732,8 @@ _canvas_event_feed_mouse_move_internal(Evas_Public_Data *e, Efl_Input_Pointer_Da
    e->last_timestamp = ev->timestamp;
 
    // prev pos
+   e->pointer.prev.x = e->pointer.x;
+   e->pointer.prev.y = e->pointer.y;
    px = ev->prev.x = e->pointer.x;
    py = ev->prev.y = e->pointer.y;
 

src/lib/evas/canvas/evas_events_legacy.c
@@ -41,6 +41,8 @@ efl_input_pointer_legacy_info_fill(Evas *eo_evas, Efl_Input_Key *eo_ev, Evas_Cal
    if (!ev || !evas) return NULL;
 
 #define COORD_DUP(e) do { (e)->output.x = evas->pointer.x; (e)->output.y = evas->pointer.y; } while (0)
+#define COORD_DUP_CUR(e) do { (e)->cur.output.x = evas->pointer.x; (e)->cur.output.y = evas->pointer.y; } while (0)
+#define COORD_DUP_PREV(e) do { (e)->prev.output.x = evas->pointer.prev.x; (e)->prev.output.y = evas->pointer.prev.y; } while (0)
 #define TYPE_CHK(typ) do { if (type != EVAS_CALLBACK_ ## typ) return NULL; } while (0)
 
    switch (ev->action)
@@ -196,10 +198,10 @@ efl_input_pointer_legacy_info_fill(Evas *eo_evas, Efl_Input_Key *eo_ev, Evas_Cal
              e->buttons = ev->pressed_buttons;
              e->cur.canvas.x = ev->cur.x;
              e->cur.canvas.y = ev->cur.y;
-             COORD_DUP(&e->cur);
              e->prev.canvas.x = ev->prev.x;
              e->prev.canvas.y = ev->prev.y;
-             COORD_DUP(&e->prev);
+             COORD_DUP_CUR(e);
+             COORD_DUP_PREV(e);
              e->data = ev->data;
              e->timestamp = ev->timestamp;
              e->event_flags = ev->event_flags;
@@ -225,7 +227,7 @@ efl_input_pointer_legacy_info_fill(Evas *eo_evas, Efl_Input_Key *eo_ev, Evas_Cal
              e->cur.canvas.ysub = ev->cur.y;
              e->cur.canvas.x = ev->cur.x;
              e->cur.canvas.y = ev->cur.y;
-             COORD_DUP(&e->cur);
+             COORD_DUP_CUR(e);
              e->data = ev->data;
              e->timestamp = ev->timestamp;
              e->event_flags = ev->event_flags;

src/lib/evas/include/evas_private.h
@@ -800,6 +800,7 @@ struct _Evas_Public_Data
       int            downs;
       DATA32         button;
       Evas_Coord     x, y;
+      Evas_Point     prev;
       int            nogrep;
       struct {
           Eina_List *in;