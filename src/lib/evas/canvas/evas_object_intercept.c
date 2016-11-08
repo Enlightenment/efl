Enlightenment/efl

commit 11b7cf6b728001dbcd42ce41d5ac2e129a835fd8

evas/elm: Remove function group_move
This is an override of efl_gfx_position_set.
As for the other patches, I hope I didn't break anything.

A problem likely to happen is that the super call was inserted
too early or too late in the call flow. For instance:

  _myclass_position_set(obj, x, y) {
    position_set(super(obj), x, y);
    position_get(obj, &prevx, &prevy);
    do_something_with_delta_xy();
  }

The above code flow is obvisouly wrong, but may have crept in this
patch (such a bug sneaked in inside smart object, breaking
everything at first).

src/lib/evas/canvas/evas_object_intercept.c
@@ -111,10 +111,17 @@ _evas_object_intercept_call(Evas_Object *eo_obj, Evas_Object_Intercept_Cb_Type c
         else return evas_object_intercept_call_hide(eo_obj, obj);
 
       case EVAS_OBJECT_INTERCEPT_CB_MOVE:
-        if (!obj->interceptors) return 0;
+        if (obj->doing.in_move > 0)
+          {
+             WRN("evas_object_move() called on object %p (%s) in the middle "
+                 "of moving the same object", eo_obj, efl_class_name_get(eo_obj));
+             return 1;
+          }
         va_start(args, internal);
         i = va_arg(args, int);
         j = va_arg(args, int);
+        if ((obj->cur->geometry.x == i) && (obj->cur->geometry.y == j)) goto end_block;
+        if (!obj->interceptors) goto end_noblock;
         blocked = evas_object_intercept_call_move(eo_obj, obj, i, j);
         break;
 

src/lib/evas/canvas/evas_object_main.c
@@ -795,19 +795,9 @@ _efl_canvas_object_efl_gfx_position_set(Eo *eo_obj, Evas_Object_Protected_Data *
    Eina_Bool pass = EINA_FALSE, freeze = EINA_FALSE;
    Eina_Bool source_invisible = EINA_FALSE;
 
-   if (obj->delete_me) return;
-   if (!obj->layer) return;
-
    evas_object_async_block(obj);
-   if (_evas_object_intercept_call(eo_obj, EVAS_OBJECT_INTERCEPT_CB_MOVE, 1, x, y)) return;
-
-   if (obj->doing.in_move > 0)
-     {
-        WRN("evas_object_move() called on object %p when in the middle of moving the same object", obj);
-        return;
-     }
-
-   if ((obj->cur->geometry.x == x) && (obj->cur->geometry.y == y)) return;
+   if (_evas_object_intercept_call(eo_obj, EVAS_OBJECT_INTERCEPT_CB_MOVE, 1, x, y))
+     return;
 
    Evas_Map *map;
    map = (Evas_Map *) evas_object_map_get(eo_obj);
@@ -830,10 +820,8 @@ _efl_canvas_object_efl_gfx_position_set(Eo *eo_obj, Evas_Object_Protected_Data *
      }
    obj->doing.in_move++;
 
-   if (obj->is_smart)
-     {
-        efl_canvas_group_move(eo_obj, x, y);
-     }
+   if (obj->is_smart && obj->smart.smart && obj->smart.smart->smart_class->move)
+     obj->smart.smart->smart_class->move(eo_obj, x, y);
 
    EINA_COW_STATE_WRITE_BEGIN(obj, state_write, cur)
      {

src/lib/edje/edje_object.eo
@@ -2067,12 +2067,12 @@ class Edje.Object (Efl.Canvas.Group.Clipped, Efl.File, Efl.Container, Efl.Part)
    }
    implements {
       Efl.Gfx.visible.set;
+      Efl.Gfx.position.set;
       Efl.Object.constructor;
       Efl.Object.destructor;
       Efl.Object.dbg_info_get;
       Efl.Canvas.Object.no_render.set;
       Efl.Canvas.Object.paragraph_direction.set;
-      Efl.Canvas.Group.group_move;
       Efl.Canvas.Group.group_add;
       Efl.Canvas.Group.group_del;
       Efl.Canvas.Group.group_resize;

src/lib/edje/edje_smart.c
@@ -159,9 +159,14 @@ _edje_object_efl_canvas_group_group_del(Eo *obj, Edje *ed)
 }
 
 EOLIAN static void
-_edje_object_efl_canvas_group_group_move(Eo *obj EINA_UNUSED, Edje *ed, Evas_Coord x, Evas_Coord y)
+_edje_object_efl_gfx_position_set(Eo *obj, Edje *ed, Evas_Coord x, Evas_Coord y)
 {
    unsigned short i;
+
+   if (_evas_object_intercept_call(obj, EVAS_OBJECT_INTERCEPT_CB_MOVE, 0, x, y))
+     return;
+
+   efl_gfx_position_set(efl_super(obj, MY_CLASS), x, y);
 
    if ((ed->x == x) && (ed->y == y)) return;
    ed->x = x;

src/lib/elementary/efl_ui_image.c
@@ -561,9 +561,12 @@ _efl_ui_image_efl_canvas_group_group_del(Eo *obj, Efl_Ui_Image_Data *sd)
 }
 
 EOLIAN static void
-_efl_ui_image_efl_canvas_group_group_move(Eo *obj, Efl_Ui_Image_Data *sd, Evas_Coord x, Evas_Coord y)
+_efl_ui_image_efl_gfx_position_set(Eo *obj, Efl_Ui_Image_Data *sd, Evas_Coord x, Evas_Coord y)
 {
-   efl_canvas_group_move(efl_super(obj, MY_CLASS), x, y);
+   if (_evas_object_intercept_call(obj, EVAS_OBJECT_INTERCEPT_CB_MOVE, 0, x, y))
+     return;
+
+   efl_gfx_position_set(efl_super(obj, MY_CLASS), x, y);
 
    if ((sd->img_x == x) && (sd->img_y == y)) return;
    sd->img_x = x;

src/lib/elementary/efl_ui_image.eo
@@ -124,6 +124,7 @@ class Efl.Ui.Image (Elm.Widget, Efl.Ui.Clickable, Efl.Ui.Draggable,
       Efl.File.mmap.set;
       Efl.Gfx.color.set;
       Efl.Gfx.visible.set;
+      Efl.Gfx.position.set;
       Efl.Gfx.View.view_size.get;
       Efl.Image.Load.load_size.set;
       Efl.Image.Load.load_size.get;
@@ -142,7 +143,6 @@ class Efl.Ui.Image (Elm.Widget, Efl.Ui.Clickable, Efl.Ui.Draggable,
       Edje.Object.size_min_calc;
       Edje.Object.calc_force;
       Efl.Canvas.Object.clip.set;
-      Efl.Canvas.Group.group_move;
       Efl.Canvas.Group.group_add;
       Efl.Canvas.Group.group_del;
       Efl.Canvas.Group.group_member_add;

src/lib/elementary/efl_ui_text.c
@@ -3468,9 +3468,12 @@ _efl_ui_text_efl_canvas_group_group_del(Eo *obj, Efl_Ui_Text_Data *sd)
 }
 
 EOLIAN static void
-_efl_ui_text_efl_canvas_group_group_move(Eo *obj, Efl_Ui_Text_Data *sd, Evas_Coord x, Evas_Coord y)
+_efl_ui_text_efl_gfx_position_set(Eo *obj, Efl_Ui_Text_Data *sd, Evas_Coord x, Evas_Coord y)
 {
-   efl_canvas_group_move(efl_super(obj, MY_CLASS), x, y);
+   if (_evas_object_intercept_call(obj, EVAS_OBJECT_INTERCEPT_CB_MOVE, 0, x, y))
+     return;
+
+   efl_gfx_position_set(efl_super(obj, MY_CLASS), x, y);
 
    evas_object_move(sd->hit_rect, x, y);
 

src/lib/elementary/efl_ui_text.eo
@@ -417,7 +417,7 @@ class Efl.Ui.Text (Elm.Layout, Elm.Interface_Scrollable, Efl.Ui.Clickable,
       Efl.Object.constructor;
       Efl.Object.destructor;
       Efl.Gfx.visible.set;
-      Efl.Canvas.Group.group_move;
+      Efl.Gfx.position.set;
       Efl.Canvas.Group.group_member_add;
       Efl.Canvas.Group.group_add;
       Efl.Canvas.Group.group_resize;

commit 04e3d40ec39b52185325429f9fba52b8bf5e26b3

evas: Fix performance issue with move
See T4749, 11b7cf6b728001dbcd42ce4 introduced an issue and
e1e28ce32062d83b95620c854 fixed it but caused a massive
performance impact.

This should fix that. Thanks @zmike for the first patch.

Fixes T4840

src/lib/evas/canvas/evas_object_intercept.c
@@ -120,7 +120,12 @@ _evas_object_intercept_call(Evas_Object *eo_obj, Evas_Object_Intercept_Cb_Type c
           }
         i = va_arg(args, int);
         j = va_arg(args, int);
-        if (!obj->interceptors) goto end_noblock;
+        if (!obj->interceptors)
+          {
+             if ((obj->cur->geometry.x == i) && (obj->cur->geometry.y == j))
+               blocked = EINA_TRUE;
+             break;
+          }
         blocked = evas_object_intercept_call_move(eo_obj, obj, i, j);
         break;
 

src/lib/evas/canvas/evas_object_main.c
@@ -814,8 +814,6 @@ _efl_canvas_object_efl_gfx_position_set(Eo *eo_obj, Evas_Object_Protected_Data *
    if (_evas_object_intercept_call(eo_obj, EVAS_OBJECT_INTERCEPT_CB_MOVE, 1, x, y))
      return;
 
-   if ((obj->cur->geometry.x == x) && (obj->cur->geometry.y == y)) return;
-
    Evas_Map *map;
    map = (Evas_Map *) evas_object_map_get(eo_obj);
    if (map && map->move_sync.enabled)