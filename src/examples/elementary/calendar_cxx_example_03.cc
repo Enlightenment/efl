a0d9354be14722bbf5a4f33759b007cd3b9845fc
Cedric Bail <cedric@osg.samsung.com>
2016-11-21 15:29:47 -0800

    elementary_cxx: fix examples to use the new API.

--- src/examples/elementary/calendar_cxx_example_03.cc
+++ src/examples/elementary/calendar_cxx_example_03.cc
@@ -8,6 +8,8 @@
 EAPI_MAIN int
 elm_main (int argc, char *argv[])
 {
+   Efl_Time min, max;
+
    elm_policy_set(ELM_POLICY_QUIT, ELM_POLICY_QUIT_LAST_WINDOW_HIDDEN);
 
    efl::ui::win::Standard win;
@@ -15,9 +17,14 @@ elm_main (int argc, char *argv[])
    win.autohide_set(true);
 
    elm::Calendar cal(win);
+
+   min.tm_year = 2020 - 1900;
+   max.tm_year = 2022 - 1900;
+
    //cal.size_hint_weight_set(EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
    //win.resize_object_add(cal);
-   cal.min_max_year_set(2020, 2022);
+   cal.date_min_set(min);
+   cal.date_max_set(max);
    cal.visible_set(true);
 
    cal.eo_cxx::efl::Gfx::size_set(140,140);
