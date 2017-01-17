21b00ac5c09fed3692c65453767b90d363a0839a
Vitor Sousa <vitorsousasilva@gmail.com>
2017-01-17 17:21:37 -0200

    eo_mono: organize test code

--- src/tests/eo_mono/Main.cs
+++ src/tests/eo_mono/Main.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Runtime.InteropServices;
 using System.Runtime.CompilerServices;
+using System.Reflection;
+using System.Linq;
 
 public class MyBox : evas.BoxInherit
 {
@@ -12,7 +14,7 @@ public class MyBox : evas.BoxInherit
     [DllImport("evas")] static extern IntPtr evas_object_evas_get(IntPtr obj);
     [DllImport("evas")] static extern void evas_event_freeze(IntPtr obj);
     [DllImport("evas")] static extern void evas_event_thaw(IntPtr obj);
-    
+
     override public void group_calculate()
     {
         IntPtr evas = evas_object_evas_get(raw_handle);
@@ -26,37 +28,35 @@ public class MyBox : evas.BoxInherit
     }
 }
 
-class TestMain
-{
-    [DllImport("eo")] static extern void efl_object_init();
-    [DllImport("ecore")] static extern void ecore_init();
-    [DllImport("evas")] static extern void evas_init();
+namespace testsuite {
 
-    static string ImagePath([CallerFilePath] string folder="")
+class Core
+{
+    public static void return_same_object()
+    {
+        test.Testing testing = new test.TestingConcrete();
+        test.Testing o1 = testing.return_object();
+        System.Diagnostics.Debug.Assert(o1.raw_handle != IntPtr.Zero);
+        System.Diagnostics.Debug.Assert(o1.raw_handle == testing.raw_handle);
+        test.Testing o2 = o1.return_object();
+        System.Diagnostics.Debug.Assert(o2.raw_handle != IntPtr.Zero);
+        System.Diagnostics.Debug.Assert(o2.raw_handle == o1.raw_handle);
+    }
+}
+
+class Evas
+{
+    private static string ImagePath([CallerFilePath] string folder="")
     {
         return System.IO.Path.GetDirectoryName(folder);
     }
 
-    static void Main(string[] args)
+    public static void simple_layout()
     {
-        efl_object_init();
-        ecore_init();
-        evas_init();
-
-        {
-            test.Testing testing = new test.TestingConcrete();
-            test.Testing o1 = testing.return_object();
-            System.Diagnostics.Debug.Assert(o1.raw_handle != IntPtr.Zero);
-            System.Diagnostics.Debug.Assert(o1.raw_handle == testing.raw_handle);
-            test.Testing o2 = o1.return_object();
-            System.Diagnostics.Debug.Assert(o2.raw_handle != IntPtr.Zero);
-            System.Diagnostics.Debug.Assert(o2.raw_handle == o1.raw_handle);
-        }
-        
-        efl.Loop loop = new efl.LoopConcrete();
+//         efl.Loop loop = new efl.LoopConcrete();
 
         EcoreEvas ecore_evas = new EcoreEvas();
-        
+
         System.Console.WriteLine("Hello World");
 
         efl.canvas.Object canvas = ecore_evas.canvas;
@@ -64,7 +64,7 @@ class TestMain
 
         efl.Object parent = canvas.parent_get();
         System.Diagnostics.Debug.Assert(parent.raw_handle != IntPtr.Zero);
-        
+
         efl.canvas.Rectangle rect = new efl.canvas.RectangleConcrete(canvas);
         rect.color_set(255, 255, 255, 255);
         rect.size_set(640, 480);
@@ -73,7 +73,7 @@ class TestMain
         evas.Box box = new MyBox(canvas);
         rect.size_set(320, 240);
         box.visible_set(true);
-        
+
         efl.canvas.Image image1 = new efl.canvas.ImageConcrete(canvas);
         image1.file_set(ImagePath() + "/../../examples/elementary/sphere_hunter/score.jpg", "");
         image1.hint_min_set(160, 240);
@@ -83,10 +83,41 @@ class TestMain
         image2.file_set(ImagePath() + "/../../examples/evas/shooter/assets/images/bricks.jpg", "");
         image2.hint_min_set(160, 120);
         image2.visible_set(true);
-        
+
         box.append(image1);
         box.append(image2);
-        
-        loop.begin();
+
+//         loop.begin();
+    }
+}
+
+}
+
+class TestMain
+{
+    [DllImport("eo")] static extern void efl_object_init();
+    [DllImport("ecore")] static extern void ecore_init();
+    [DllImport("evas")] static extern void evas_init();
+
+    static Type[] GetTestSuites()
+    {
+        return Assembly.GetExecutingAssembly().GetTypes().Where(t => String.Equals(t.Namespace, "testsuite", StringComparison.Ordinal)).ToArray();
+    }
+
+    static void Main(string[] args)
+    {
+        efl_object_init();
+        ecore_init();
+        evas_init();
+
+        var suites = GetTestSuites();
+        foreach(var suite in suites)
+        {
+            var testcases = suite.GetMethods(BindingFlags.Static | BindingFlags.Public);
+            foreach(var testcase in testcases)
+            {
+                testcase.Invoke(null, null);
+            }
+        }
     }
 }
