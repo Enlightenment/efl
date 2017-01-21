6c87f62b90b1edd12087b7bdce78883b5d52c5e9
Marcel Hollerbach <marcel-hollerbach@t-online.de>
2017-01-21 22:12:42 +0100

    build: make eina working
    This brings in the checks for all the headers and function that are
    required in eina. With this commit only a few other checks are missing.
    This is NOT tested on windows or mac os i can just verify that it runs
    here on a arch linux system.

--- cmake/config/common.cmake
+++ cmake/config/common.cmake
@@ -7,8 +7,6 @@ find_package(Threads REQUIRED)
 
 pkg_check_modules(CHECK check)
 
-CHECK_INCLUDE_FILE(sys/mman.h HAVE_SYS_MMAN_H)
-
 set(VMAJ ${PROJECT_VERSION_MAJOR})
 set(VMIN ${PROJECT_VERSION_MINOR})
 set(VMIC ${PROJECT_VERSION_PATCH})
@@ -17,6 +15,13 @@ set(EFL_VERSION_MAJOR ${PROJECT_VERSION_MAJOR})
 set(EFL_VERSION_MINOR ${PROJECT_VERSION_MINOR})
 set(MODULE_ARCH "v-${VMAJ}.${VMIN}")
 
+#change when on windows
+if (WIN32)
+    set(SHARED_LIB_SUFFIX ".dll")
+else()
+    set(SHARED_LIB_SUFFIX ".so")
+endif()
+
 EFL_OPTION(BUILD_PROFILE "How to build EFL" dev CHOICE release;dev;debug)
 EFL_OPTION(EFL_NOLEGACY_API_SUPPORT "Whenever to disable non-Eo (Legacy) APIs" ON)
 EFL_OPTION(EFL_EO_API_SUPPORT "Whenever to enable new Eo APIs" ON)
@@ -24,5 +29,16 @@ if(NOT EFL_EO_API_SUPPORT)
   EFL_OPTION_SET_MESSAGE(EFL_EO_API_SUPPORT "Risky!")
 endif()
 
+
+#define all our header / function checks
+include(./CommonHeaderChecks)
+
+
 #this is needed to build efl
 set(EFL_BETA_API_SUPPORT 1)
+
+#if there are thread init support from cmake then efl has threads
+
+if (CMAKE_THREAD_LIBS_INIT)
+    set(EFL_HAVE_THREADS 1)
+endif()
--- src/lib/efl/config.h.cmake
+++ src/lib/efl/config.h.cmake
@@ -2,7 +2,8 @@
 #define CONFIG_H
 
 #define _GNU_SOURCE
-#cmakedefine HAVE_SYS_MMAN_H 1
+
+#include "config_headers.h"
 
 #define VMAJ @VMAJ@
 #define VMIN @VMIN@
@@ -10,13 +11,24 @@
 #define VREV @VREV@
 
 #cmakedefine NVALGRIND 1
-#cmakedefine HAVE_CLOCK_GETTIME 1
 
 #cmakedefine EINA_STATIC_BUILD_CHAINED_POOL 1
 #cmakedefine EINA_STATIC_BUILD_ONE_BIG 1
 #cmakedefine EINA_STATIC_BUILD_PASS_THROUGH 1
 
 #define MODULE_ARCH "@MODULE_ARCH@"
-#define SHARED_LIB_SUFFIX "bla"
+#define SHARED_LIB_SUFFIX "@SHARED_LIB_SUFFIX@"
+
+#if defined (HAVE_LISTXATTR) && defined (HAVE_SETXATTR) && defined (HAVE_GETXATTR)
+#define HAVE_XATTR
+#endif
+
+//for now statically define that to one
+#define STRERROR_R_CHAR_P 1
+
+#cmakedefine EFL_ALWAYS_INLINE @EFL_ALWAYS_INLINE@
+#cmakedefine EFL_HAVE_OSX_SPINLOCK 1
+#cmakedefine EFL_HAVE_POSIX_THREADS_SPINLOCK 1
+#cmakedefine EFL_HAVE_THREADS 1
 
 #endif
