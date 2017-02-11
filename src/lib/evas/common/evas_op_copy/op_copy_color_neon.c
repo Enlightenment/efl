c1c7ad836064e3cdb9e89aaa4ac6bb1d216e2b6e
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2017-02-11 23:08:28 +0900

    evas color copy neon asm - declare tmp as output not input for warning--
    less warnings - use output not input for tmp var. much better.

--- src/lib/evas/common/evas_op_copy/op_copy_color_neon.c
+++ src/lib/evas/common/evas_op_copy/op_copy_color_neon.c
@@ -12,7 +12,7 @@ _op_copy_c_dp_neon(DATA32 *s EINA_UNUSED, DATA8 *m EINA_UNUSED, DATA32 c, DATA32
                      });
 #else
 #define AP "COPY_C_DP_"
-   uint32_t *e = d + l,*tmp;
+   uint32_t *e = d + l, *tmp;
    asm volatile (
       ".fpu neon					\n\t"
 
@@ -84,10 +84,10 @@ _op_copy_c_dp_neon(DATA32 *s EINA_UNUSED, DATA8 *m EINA_UNUSED, DATA32 c, DATA32
 		"vst1.32	d0[0], [%[d]]		\n\t"
 
 	AP "done:\n\t"
-
-		: // No output regs
+		// Output
+		: [tmp] "=r" (tmp)
 		// Input
-		: [c] "r" (c), [e] "r" (e), [d] "r" (d),[tmp] "r" (tmp)
+		: [c] "r" (c), [e] "r" (e), [d] "r" (d)
 		// Clobbered
 		: "q0","q1","memory"
 
