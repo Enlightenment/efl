b7780814fb748d947d22db973223edffdb541af5
Felipe Magno de Almeida <felipe@expertisesolutions.com.br>
2017-02-22 18:41:32 -0300

    efl-js: Fix deprecated Set feature of setting non-primitive value

--- src/bin/eolian_js/main.cc
+++ src/bin/eolian_js/main.cc
@@ -1026,7 +1026,7 @@ int main(int argc, char** argv)
      }
 
    functions_ss << "\n  prototype->Set(::efl::eina::js::compatibility_new<v8::String>(isolate, \"cast\"),\n"
-                   "      ::efl::eina::js::compatibility_new<v8::FunctionTemplate>(isolate, &efl::eina::js::cast_function)->GetFunction());\n\n";
+                   "      ::efl::eina::js::compatibility_new<v8::FunctionTemplate>(isolate, &efl::eina::js::cast_function));\n\n";
 
    // generate all events
    std::stringstream events_ss;
