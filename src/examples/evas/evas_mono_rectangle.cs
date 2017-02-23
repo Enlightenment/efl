f984f21cb67c69bfa2014a9c873b0c5a2648caa5
Lauro Moura <lauromoura@expertisesolutions.com.br>
2017-02-23 00:52:10 -0300

    mono: Make examples use some events.

--- src/examples/evas/evas_mono_rectangle.cs
+++ src/examples/evas/evas_mono_rectangle.cs
@@ -8,8 +8,16 @@ class TestMain
     [DllImport("ecore")] static extern void ecore_init();
     [DllImport("evas")] static extern void evas_init();
 
+    private static int[,] colors = new int[,] {
+        {255, 0, 0},
+        {0, 255, 0},
+        {0, 0, 255}
+    };
+
     static void Main(string[] args)
     {
+        int color_index = 0;
+
         eina.Log.Init();
         efl_object_init();
         ecore_init();
@@ -24,15 +32,18 @@ class TestMain
         System.Diagnostics.Debug.Assert(parent.raw_handle != IntPtr.Zero);
 
         efl.canvas.Rectangle rect = new efl.canvas.RectangleConcrete(canvas);
-        rect.color_set(255, 0, 0, 255);
+        rect.color_set(colors[0, 0], colors[0, 1], colors[0, 2], 255);
         rect.size_set(640, 480);
         rect.visible_set(true);
 
         canvas.KEY_DOWN += (object sender, efl.input.KEY_DOWN_Args e) => {
+            color_index = (color_index + 1) % 3;
             Console.WriteLine("Key Down");
             Console.WriteLine("Got key obj at {0}", e.arg.raw_handle.ToString("X"));
             Console.WriteLine("Got key_get() == [{0}]", e.arg.key_get());
-            /* Console.WriteLine("Got key code [{0}] at {1}", e.arg.key_code_get(), e.arg.raw_handle); */
+            rect.color_set(colors[color_index, 0],
+                           colors[color_index, 1],
+                           colors[color_index, 2], 255);
         };
 
         loop.begin();
