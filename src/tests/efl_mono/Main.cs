4276711a6f2f1dc55a3dc1331ff35d2af40367b4
Lauro Moura <lauromoura@expertisesolutions.com.br>
2017-03-01 11:02:36 -0300

    mono: Allow basic support for CK_RUN_ vars
    The suite name is "mono" and the test cases are the individual Test*
    classes in the TestSuite namespace.
    
    CK_RUN_CASE will try to match any test case that contains the given
    string instead of exact matches. This will allow quickly testing all Eo
    stuff, for example.
    
    Example: To run all string tests:
    
    CK_RUN_SUITE=mono CK_RUN_CASE=Strings make check

--- src/tests/efl_mono/Main.cs
+++ src/tests/efl_mono/Main.cs
@@ -22,10 +22,11 @@ class TestMain
     [DllImport("ecore")] static extern void ecore_init();
     [DllImport("evas")] static extern void evas_init();
 
-    static Type[] GetTestSuites()
+    static Type[] GetTestCases(String name="")
     {
         return Assembly.GetExecutingAssembly().GetTypes().Where(t => String.Equals(t.Namespace, "TestSuite", StringComparison.Ordinal) &&
-                                                                t.Name.StartsWith("Test")).ToArray();
+                                                                t.Name.StartsWith("Test") &&
+                                                                t.Name.Contains(name)).ToArray();
     }
 
     static int Main(string[] args)
@@ -37,18 +38,28 @@ class TestMain
 
         bool pass = true;
 
-        var suites = GetTestSuites();
-        foreach(var suite in suites)
+        String ckRunSuite = Environment.GetEnvironmentVariable("CK_RUN_SUITE");
+        String ckRunCase = Environment.GetEnvironmentVariable("CK_RUN_CASE");
+
+        if (ckRunSuite != null && !ckRunSuite.Equals("mono"))
+            return 0;
+
+        if (ckRunCase == null)
+            ckRunCase = String.Empty;
+
+        Console.WriteLine("[ START SUITE ] " + ckRunSuite);
+        var cases= GetTestCases(ckRunCase);
+        foreach(var testCase in cases)
         {
-            var testCases = suite.GetMethods(BindingFlags.Static | BindingFlags.Public);
-            Console.WriteLine("[ START SUITE ] " + suite.Name);
-            foreach(var testCase in testCases)
+            var localTestCases = testCase.GetMethods(BindingFlags.Static | BindingFlags.Public);
+            foreach(var localTestCase in localTestCases)
             {
-                Console.WriteLine("[ RUN         ] " + suite.Name + "." + testCase.Name);
+
+                Console.WriteLine("[ RUN         ] " + testCase.Name + "." + localTestCase.Name);
                 bool caseResult = true;
                 try
                 {
-                    testCase.Invoke(null, null);
+                    localTestCase.Invoke(null, null);
                 }
                 catch (Exception e)
                 {
@@ -56,10 +67,10 @@ class TestMain
                     caseResult = false;
                     Console.WriteLine("[ ERROR       ] " + e.InnerException.Message);
                 }
-                Console.WriteLine("[        " + (caseResult ? "PASS" : "FAIL") + " ] " + suite.Name + "." + testCase.Name);
+                Console.WriteLine("[        " + (caseResult ? "PASS" : "FAIL") + " ] " + testCase.Name + "." + localTestCase.Name);
             }
-            Console.WriteLine("[   END SUITE ] " + suite.Name);
         }
+        Console.WriteLine("[   END SUITE ] " + ckRunSuite);
 
         if (!pass)
           return -1;
