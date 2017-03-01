b2fead8aeca2d5677b8d17a716ee77bcfdd08507
Lauro Moura <lauromoura@expertisesolutions.com.br>
2017-02-28 23:57:20 -0300

    mono: Reorganize test code into separate files.

---
+++ src/tests/efl_mono/Eo.cs
@@ -0,0 +1,99 @@
+using System;
+
+namespace TestSuite
+{
+
+class TestEo
+{
+    private class Derived : test.TestingInherit
+    {
+    }
+
+    //
+    // Test cases:
+    //
+    public static void return_same_object()
+    {
+        test.Testing testing = new test.TestingConcrete();
+        test.Testing o1 = testing.return_object();
+        Test.Assert(o1.raw_handle != IntPtr.Zero);
+        Test.Assert(o1.raw_handle == testing.raw_handle);
+        test.Testing o2 = o1.return_object();
+        Test.Assert(o2.raw_handle != IntPtr.Zero);
+        Test.Assert(o2.raw_handle == o1.raw_handle);
+    }
+
+    public static void destructor_really_frees()
+    {
+       bool delEventCalled = false;
+       {
+           test.Testing obj = new test.TestingConcrete();
+           obj.DEL += (object sender, EventArgs e) => { delEventCalled = true; };
+       }
+
+       System.GC.WaitForPendingFinalizers();
+       System.GC.Collect();
+       System.GC.WaitForPendingFinalizers();
+       System.GC.Collect();
+       System.GC.WaitForPendingFinalizers();
+
+       Test.Assert(delEventCalled, "DEL event not called");
+    }
+
+    public static void dispose_really_frees()
+    {
+       bool delEventCalled = false;
+       {
+           test.Testing obj = new test.TestingConcrete();
+           obj.DEL += (object sender, EventArgs e) => { delEventCalled = true; };
+           ((IDisposable)obj).Dispose();
+       }
+
+       Test.Assert(delEventCalled, "DEL event not called");
+    }
+
+    public static void derived_destructor_really_frees()
+    {
+       bool delEventCalled = false;
+       {
+           test.Testing obj = new Derived();
+           obj.DEL += (object sender, EventArgs e) => { delEventCalled = true; };
+       }
+
+       System.GC.WaitForPendingFinalizers();
+       System.GC.Collect();
+       System.GC.WaitForPendingFinalizers();
+       System.GC.Collect();
+       System.GC.WaitForPendingFinalizers();
+
+       Test.Assert(delEventCalled, "DEL event not called");
+    }
+
+    public static void derived_dispose_really_frees()
+    {
+       bool delEventCalled = false;
+       {
+           test.Testing obj = new Derived();
+           obj.DEL += (object sender, EventArgs e) => { delEventCalled = true; };
+           ((IDisposable)obj).Dispose();
+       }
+
+       Test.Assert(delEventCalled, "DEL event not called");
+    }
+}
+
+class MyLoop : efl.LoopInherit
+{
+    public MyLoop() : base(null) { }
+}
+
+class TestEoInherit
+{
+    public static void instantiate_inherited()
+    {
+        efl.Loop loop = new MyLoop();
+        Test.Assert(loop.raw_handle != System.IntPtr.Zero);
+    }
+}
+
+}
