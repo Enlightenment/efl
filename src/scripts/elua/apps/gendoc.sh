0a252c4463c8d3d2e72c7b23ab7429775afd126b
Daniel Kolesa <d.kolesa@osg.samsung.com>
2017-03-03 17:43:19 +0100

    docgen: add a simple script that does doc generation in parallel

---
+++ src/scripts/elua/apps/gendoc.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+# a parallel doc generation script
+
+# exit on failure
+set -e
+
+gendoc() {
+    elua gendoc.lua --pass $@
+}
+
+gendoc rm $@
+gendoc ref $@
+
+# limit jobs otherwise stuff starts complaining in eldbus etc
+MAXJ=192
+
+I=0
+for cl in $(gendoc clist $@); do
+    gendoc "$cl" $@ &
+    I=$(($I + 1))
+    if [ $I -gt $MAXJ ]; then
+        I=0
+        # wait for the batch to finish
+        wait
+    fi
+done
+
+# wait for all remaining stuff to finish
+wait
+
+gendoc types $@ &
+gendoc vars $@ &
+
+# types and vars run in parallel
+wait
+
+# final results
+gendoc stats $@
+
+exit 0
