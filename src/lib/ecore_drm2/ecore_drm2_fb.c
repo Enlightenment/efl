45e173d186cd3db210e974bfb82d7ffa52245026
Derek Foreman <derekf@osg.samsung.com>
2017-03-21 14:21:14 -0500

    ecore_drm2: Use library function instead of ioctl for addfb2
    Minor clean up.

--- src/lib/ecore_drm2/ecore_drm2_fb.c
+++ src/lib/ecore_drm2/ecore_drm2_fb.c
@@ -3,30 +3,18 @@
 static Eina_Bool
 _fb2_create(Ecore_Drm2_Fb *fb)
 {
-   drm_mode_fb_cmd2 cmd;
    uint32_t hdls[4] = { 0 }, pitches[4] = { 0 }, offsets[4] = { 0 };
-   uint64_t modifiers[4] = { 0 };
+   int r;
 
    hdls[0] = fb->hdl;
    pitches[0] = fb->stride;
    offsets[0] = 0;
-   modifiers[0] = 0;
 
-   memset(&cmd, 0, sizeof(drm_mode_fb_cmd2));
-   cmd.fb_id = 0;
-   cmd.width = fb->w;
-   cmd.height = fb->h;
-   cmd.pixel_format = fb->format;
-   cmd.flags = 0;
-   memcpy(cmd.handles, hdls, 4 * sizeof(hdls[0]));
-   memcpy(cmd.pitches, pitches, 4 * sizeof(pitches[0]));
-   memcpy(cmd.offsets, offsets, 4 * sizeof(offsets[0]));
-   memcpy(cmd.modifier, modifiers, 4 * sizeof(modifiers[0]));
+   r = sym_drmModeAddFB2(fb->fd, fb->w, fb->h, fb->format, hdls,
+                         pitches, offsets, &fb->id, 0);
 
-   if (sym_drmIoctl(fb->fd, DRM_IOCTL_MODE_ADDFB2, &cmd))
+   if (r)
      return EINA_FALSE;
-
-   fb->id = cmd.fb_id;
 
    return EINA_TRUE;
 }
