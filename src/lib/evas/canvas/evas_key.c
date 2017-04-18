4e282c79bd293de0291358ec0ebfa01df0e28db3
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2017-04-18 15:38:32 +0900

    evas key modifier state set - remove dead code
    m is checked for NULL twice. this removes the pointless second check.
    @fix

--- src/lib/evas/canvas/evas_key.c
+++ src/lib/evas/canvas/evas_key.c
@@ -66,7 +66,6 @@ evas_seat_key_modifier_is_set(const Evas_Modifier *m, const char *keyname,
    if (!seat)
      seat = m->e->default_seat;
    EINA_SAFETY_ON_NULL_RETURN_VAL(seat, 0);
-   if (!m) return 0;
    if (!keyname) return 0;
    return _key_is_set(evas_key_modifier_number(m, keyname), m->masks, seat);
 }
