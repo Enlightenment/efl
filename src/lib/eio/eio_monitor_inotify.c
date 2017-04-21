24b77d8ed9e3eb2b1c5a7000b96a56aaaecfc438
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2017-04-21 16:39:06 +0900

    eio monitor - remove unused vars and fix warning

--- src/lib/eio/eio_monitor_inotify.c
+++ src/lib/eio/eio_monitor_inotify.c
@@ -174,9 +174,6 @@ _eio_inotify_handler(void *data EINA_UNUSED, Ecore_Fd_Handler *fdh)
 void eio_monitor_backend_init(void)
 {
    int fd;
-#ifdef HAVE_FCNTL
-   int flags, rc;
-#endif
 
    fd = inotify_init();
    if (fd < 0)
