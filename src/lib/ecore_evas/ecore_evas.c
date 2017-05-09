Enlightenment/efl

commit 696ed3e2e8f97de482e98db34997ad1848da985d

ecore_evas: add infrastructure to properly track sub ecore_evas.

src/lib/ecore_evas/ecore_evas.c
@@ -3098,6 +3098,17 @@ _ecore_evas_register(Ecore_Evas *ee)
      evas_event_callback_add(ee->evas, EVAS_CALLBACK_RENDER_POST, _evas_evas_buffer_rendered, ee);
 }
 
+void
+_ecore_evas_subregister(Ecore_Evas *ee_target, Ecore_Evas *ee)
+{
+   _ecore_evas_register_animators(ee);
+
+   ee_target->sub_ecore_evas = eina_list_append(ee_target->sub_ecore_evas, ee);
+
+   if (!ee->engine.func->fn_render)
+     evas_event_callback_add(ee->evas, EVAS_CALLBACK_RENDER_POST, _evas_evas_buffer_rendered, ee);
+}
+
 EAPI void
 _ecore_evas_ref(Ecore_Evas *ee)
 {

src/lib/ecore_evas/ecore_evas_private.h
@@ -390,6 +390,7 @@ EAPI void _ecore_evas_fps_debug_init(void);
 EAPI void _ecore_evas_fps_debug_shutdown(void);
 EAPI void _ecore_evas_fps_debug_rendertime_add(double t);
 EAPI void _ecore_evas_register(Ecore_Evas *ee);
+void _ecore_evas_subregister(Ecore_Evas *ee_target, Ecore_Evas *ee);
 EAPI void _ecore_evas_register_animators(Ecore_Evas *ee);
 EAPI void _ecore_evas_free(Ecore_Evas *ee);
 EAPI void _ecore_evas_idle_timeout_update(Ecore_Evas *ee);

commit d340d85a1f15057999d635d997402ddf0ac50528

ecore_evas: fix link issue on macOS
696ed3e2e8f97de482e98db34997ad1848da985d introduced a build failure on
macOS. _ecore_evas_subregister is being used in a foreign code module,
but it was not exported. Enforcing EAPI gives this symbol enough
visibility to be used outside of ecore_evas.

src/lib/ecore_evas/ecore_evas.c
@@ -3098,7 +3098,7 @@ _ecore_evas_register(Ecore_Evas *ee)
      evas_event_callback_add(ee->evas, EVAS_CALLBACK_RENDER_POST, _evas_evas_buffer_rendered, ee);
 }
 
-void
+EAPI void
 _ecore_evas_subregister(Ecore_Evas *ee_target, Ecore_Evas *ee)
 {
    _ecore_evas_register_animators(ee);

src/lib/ecore_evas/ecore_evas_private.h
@@ -390,7 +390,7 @@ EAPI void _ecore_evas_fps_debug_init(void);
 EAPI void _ecore_evas_fps_debug_shutdown(void);
 EAPI void _ecore_evas_fps_debug_rendertime_add(double t);
 EAPI void _ecore_evas_register(Ecore_Evas *ee);
-void _ecore_evas_subregister(Ecore_Evas *ee_target, Ecore_Evas *ee);
+EAPI void _ecore_evas_subregister(Ecore_Evas *ee_target, Ecore_Evas *ee);
 EAPI void _ecore_evas_register_animators(Ecore_Evas *ee);
 EAPI void _ecore_evas_free(Ecore_Evas *ee);
 EAPI void _ecore_evas_idle_timeout_update(Ecore_Evas *ee);