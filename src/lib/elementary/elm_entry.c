Enlightenment/efl

commit 3eb649b180eb98f7be829de5aed7e8c7f1ceb35a

Elm layout: replace 'text' property with 'efl_part' interface
The expected usage is efl_text_set(efl_part(layout, part), text);
Same for text_get.

Also, added an example how to make API easier with providing
efl_text_set/get for the widget itself, in efl_ui_button. Please see
this example.

src/lib/elementary/elm_entry.c
@@ -3187,7 +3187,7 @@ _entry_text_append(Evas_Object* obj, const char* entry, Eina_Bool set)
 }
 
 EOLIAN static Eina_Bool
-_elm_entry_elm_layout_text_set(Eo *obj, Elm_Entry_Data *sd, const char *part, const char *entry)
+_elm_entry_text_set(Eo *obj, Elm_Entry_Data *sd, const char *part, const char *entry)
 {
    int len = 0;
 
@@ -3238,7 +3238,7 @@ _elm_entry_elm_layout_text_set(Eo *obj, Elm_Entry_Data *sd, const char *part, co
 }
 
 EOLIAN static const char *
-_elm_entry_elm_layout_text_get(Eo *obj, Elm_Entry_Data *sd, const char *item)
+_elm_entry_text_get(Eo *obj, Elm_Entry_Data *sd, const char *item)
 {
    const char *text;
 
@@ -4139,7 +4139,7 @@ elm_entry_entry_set(Evas_Object *obj,
                     const char *entry)
 {
    ELM_ENTRY_CHECK(obj);
-   elm_obj_layout_text_set(obj, NULL, entry);
+   efl_text_set(efl_part(efl_super(obj, MY_CLASS), "elm.text"), entry);
 }
 
 EAPI const char *
@@ -4147,7 +4147,7 @@ elm_entry_entry_get(const Evas_Object *obj)
 {
    ELM_ENTRY_CHECK(obj) NULL;
    const char *text = NULL;
-   text = elm_obj_layout_text_get((Eo *)obj, NULL);
+   text = efl_text_get(efl_part(efl_super(obj, MY_CLASS), "elm.text"));
    return text;
 }
 
@@ -6021,6 +6021,8 @@ _elm_entry_elm_interface_atspi_accessible_name_get(Eo *obj, Elm_Entry_Data *sd)
 ELM_PART_OVERRIDE(elm_entry, ELM_ENTRY, ELM_LAYOUT, Elm_Entry_Data, Elm_Part_Data)
 ELM_PART_OVERRIDE_CONTENT_SET(elm_entry, ELM_ENTRY, ELM_LAYOUT, Elm_Entry_Data, Elm_Part_Data)
 ELM_PART_OVERRIDE_CONTENT_UNSET(elm_entry, ELM_ENTRY, ELM_LAYOUT, Elm_Entry_Data, Elm_Part_Data)
+ELM_PART_OVERRIDE_TEXT_SET(elm_entry, ELM_ENTRY, ELM_LAYOUT, Elm_Entry_Data, Elm_Part_Data)
+ELM_PART_OVERRIDE_TEXT_GET(elm_entry, ELM_ENTRY, ELM_LAYOUT, Elm_Entry_Data, Elm_Part_Data)
 #include "elm_entry_internal_part.eo.c"
 
 /* Efl.Part end */

src/lib/elementary/elm_layout.c
@@ -2315,6 +2315,19 @@ elm_layout_table_clear(Elm_Layout *obj, const char *part, Eina_Bool clear)
      return efl_pack_unpack_all(efl_part(obj, part));
 }
 
+EAPI Eina_Bool
+elm_layout_text_set(Elm_Layout *obj, const char *part, const char *text)
+{
+   efl_text_set(efl_part(obj, part), text);
+   return EINA_TRUE;
+}
+
+EAPI const char *
+elm_layout_text_get(const Elm_Layout *obj, const char *part)
+{
+   return efl_text_get(efl_part(obj, part));
+}
+
 /* Efl.Part implementation */
 
 static EOLIAN Efl_Object *
@@ -2354,6 +2367,8 @@ ELM_PART_IMPLEMENT_DESTRUCTOR(elm_layout, ELM_LAYOUT, Elm_Layout_Smart_Data, Elm
 ELM_PART_IMPLEMENT_CONTENT_SET(elm_layout, ELM_LAYOUT, Elm_Layout_Smart_Data, Elm_Part_Data)
 ELM_PART_IMPLEMENT_CONTENT_GET(elm_layout, ELM_LAYOUT, Elm_Layout_Smart_Data, Elm_Part_Data)
 ELM_PART_IMPLEMENT_CONTENT_UNSET(elm_layout, ELM_LAYOUT, Elm_Layout_Smart_Data, Elm_Part_Data)
+ELM_PART_IMPLEMENT_TEXT_SET(elm_layout, ELM_LAYOUT, Elm_Layout_Smart_Data, Elm_Part_Data)
+ELM_PART_IMPLEMENT_TEXT_GET(elm_layout, ELM_LAYOUT, Elm_Layout_Smart_Data, Elm_Part_Data)
 #include "elm_layout_internal_part.eo.c"
 
 /* Efl.Part end */

src/Makefile_Elementary.am
@@ -110,6 +110,11 @@ elm_public_eolian_files = \
 	lib/elementary/efl_ui_progressbar_internal_part.eo \
 	lib/elementary/elm_popup_internal_part.eo \
 	lib/elementary/elm_scroller_internal_part.eo \
+	lib/elementary/elm_label_internal_part.eo \
+	lib/elementary/elm_actionslider_internal_part.eo \
+	lib/elementary/elm_bubble_internal_part.eo \
+	lib/elementary/elm_fileselector_internal_part.eo \
+	lib/elementary/elm_multibuttonentry_internal_part.eo \
 	lib/elementary/elm_code_widget.eo \
 	lib/elementary/efl_ui_text_interactive.eo \
 	lib/elementary/efl_ui_text.eo \

src/lib/elementary/efl_ui_button.c
@@ -445,7 +445,7 @@ _efl_ui_button_class_constructor(Efl_Class *klass)
    evas_smart_legacy_type_register(MY_CLASS_NAME_LEGACY, klass);
 }
 
-EFL_TEXT_DEFAULT_IMPLEMENT(efl_ui_button, Efl_Ui_Button_Data)
+EFL_TEXT_PART_DEFAULT_IMPLEMENT(efl_ui_button, Efl_Ui_Button_Data)
 
 /* Efl.Part begin */
 

src/lib/elementary/efl_ui_text.c
@@ -2565,99 +2565,6 @@ _markup_filter_cb(void *data,
      }
 }
 
-/* This function is used to insert text by chunks in jobs */
-static Eina_Bool
-_text_append_idler(void *data)
-{
-   int start;
-   char backup;
-   Evas_Object *obj = (Evas_Object *)data;
-
-   EFL_UI_TEXT_DATA_GET(obj, sd);
-
-   evas_event_freeze(evas_object_evas_get(obj));
-   ELM_SAFE_FREE(sd->text, eina_stringshare_del);
-   sd->changed = EINA_TRUE;
-
-   start = sd->append_text_position;
-   if ((start + EFL_UI_TEXT_CHUNK_SIZE) < sd->append_text_len)
-     {
-        int pos = start;
-        int tag_start, esc_start;
-
-        tag_start = esc_start = -1;
-        /* Find proper markup cut place */
-        while (pos - start < EFL_UI_TEXT_CHUNK_SIZE)
-          {
-             int prev_pos = pos;
-             Eina_Unicode tmp =
-               eina_unicode_utf8_next_get(sd->append_text_left, &pos);
-
-             if (esc_start == -1)
-               {
-                  if (tmp == '<')
-                    tag_start = prev_pos;
-                  else if (tmp == '>')
-                    tag_start = -1;
-               }
-             if (tag_start == -1)
-               {
-                  if (tmp == '&')
-                    esc_start = prev_pos;
-                  else if (tmp == ';')
-                    esc_start = -1;
-               }
-          }
-
-        if (tag_start >= 0)
-          {
-             sd->append_text_position = tag_start;
-          }
-        else if (esc_start >= 0)
-          {
-             sd->append_text_position = esc_start;
-          }
-        else
-          {
-             sd->append_text_position = pos;
-          }
-     }
-   else
-     {
-        sd->append_text_position = sd->append_text_len;
-     }
-
-   backup = sd->append_text_left[sd->append_text_position];
-   sd->append_text_left[sd->append_text_position] = '\0';
-
-   edje_object_part_text_append
-     (sd->entry_edje, "elm.text", sd->append_text_left + start);
-
-   sd->append_text_left[sd->append_text_position] = backup;
-
-   evas_event_thaw(evas_object_evas_get(obj));
-   evas_event_thaw_eval(evas_object_evas_get(obj));
-
-   _efl_ui_text_guide_update(obj, EINA_TRUE);
-
-   /* If there's still more to go, renew the idler, else, cleanup */
-   if (sd->append_text_position < sd->append_text_len)
-     {
-        return ECORE_CALLBACK_RENEW;
-     }
-   else
-     {
-        edje_object_part_text_cursor_pos_set(sd->entry_edje, "elm.text",
-              EDJE_CURSOR_MAIN, sd->cursor_pos);
-        free(sd->append_text_left);
-        sd->append_text_left = NULL;
-        sd->append_text_idler = NULL;
-        efl_event_callback_legacy_call
-          (obj, EFL_UI_TEXT_EVENT_TEXT_SET_DONE, NULL);
-        return ECORE_CALLBACK_CANCEL;
-     }
-}
-
 EOLIAN static void
 _efl_ui_text_elm_layout_signal_emit(Eo *obj EINA_UNUSED, Efl_Ui_Text_Data *sd, const char *emission, const char *source)
 {
@@ -2762,171 +2669,6 @@ _efl_ui_text_content_unset(Eo *obj, Efl_Ui_Text_Data *_pd EINA_UNUSED, const cha
    return ret;
 }
 #endif
-
-static void
-_entry_text_append(Evas_Object* obj, const char* entry, Eina_Bool set)
-{
-   int len = 0;
-   if (!entry) return;
-
-   EFL_UI_TEXT_DATA_GET(obj, sd);
-   len = strlen(entry);
-
-   if (sd->append_text_left)
-     {
-        char *tmpbuf;
-
-        tmpbuf = realloc(sd->append_text_left, sd->append_text_len + len + 1);
-        if (!tmpbuf)
-          {
-             /* Do something */
-             return;
-          }
-        sd->append_text_left = tmpbuf;
-        memcpy(sd->append_text_left + sd->append_text_len, entry, len + 1);
-        sd->append_text_len += len;
-     }
-   else
-     {
-        if (len > EFL_UI_TEXT_CHUNK_SIZE)
-          {
-             sd->append_text_left = (char *)malloc(len + 1);
-          }
-
-        if (sd->append_text_left)
-          {
-             memcpy(sd->append_text_left, entry, len + 1);
-             sd->append_text_position = 0;
-             sd->append_text_len = len;
-             sd->append_text_idler = ecore_idler_add(_text_append_idler, obj);
-          }
-        else
-          {
-             if (set)
-               {
-                  edje_object_part_text_set(sd->entry_edje, "elm.text", entry);
-               }
-             else
-               {
-                  edje_object_part_text_append(sd->entry_edje, "elm.text", entry);
-               }
-             edje_object_part_text_cursor_pos_set(sd->entry_edje, "elm.text",
-                   EDJE_CURSOR_MAIN, sd->cursor_pos);
-             efl_event_callback_legacy_call(obj, EFL_UI_TEXT_EVENT_TEXT_SET_DONE, NULL);
-          }
-     }
-}
-
-EOLIAN static Eina_Bool
-_efl_ui_text_elm_layout_text_set(Eo *obj, Efl_Ui_Text_Data *sd, const char *part, const char *entry)
-{
-   int len = 0;
-
-   if (!entry) entry = "";
-   if (part)
-     {
-        if (!strcmp(part, "guide"))
-          edje_object_part_text_set(sd->entry_edje, "elm.guide", entry);
-        else
-          edje_object_part_text_set(sd->entry_edje, part, entry);
-
-        return EINA_TRUE;
-     }
-
-   evas_event_freeze(evas_object_evas_get(obj));
-   ELM_SAFE_FREE(sd->text, eina_stringshare_del);
-   sd->changed = EINA_TRUE;
-
-   /* Clear currently pending job if there is one */
-   if (sd->append_text_idler)
-     {
-        ecore_idler_del(sd->append_text_idler);
-        ELM_SAFE_FREE(sd->append_text_left, free);
-        sd->append_text_idler = NULL;
-     }
-
-   len = strlen(entry);
-   if (sd->append_text_left)
-     {
-        free(sd->append_text_left);
-        sd->append_text_left = NULL;
-     }
-
-   /* Need to clear the entry first */
-   edje_object_part_text_set(sd->entry_edje, "elm.text", "");
-   _entry_text_append(obj, entry, EINA_TRUE);
-
-   if (len > 0)
-     _efl_ui_text_guide_update(obj, EINA_TRUE);
-   else
-     _efl_ui_text_guide_update(obj, EINA_FALSE);
-
-   evas_event_thaw(evas_object_evas_get(obj));
-   evas_event_thaw_eval(evas_object_evas_get(obj));
-   return EINA_TRUE;
-}
-
-EOLIAN static const char *
-_efl_ui_text_elm_layout_text_get(Eo *obj, Efl_Ui_Text_Data *sd, const char *item)
-{
-   const char *text;
-   Eo *text_obj = edje_object_part_swallow_get(sd->entry_edje, "elm.text");
-   sd->text_obj = text_obj;
-
-   if (item)
-     {
-        if (!strcmp(item, "default")) goto proceed;
-        else if (!strcmp(item, "guide"))
-          {
-             return edje_object_part_text_get(sd->entry_edje, "elm.guide");
-          }
-        else
-          {
-             return edje_object_part_text_get(sd->entry_edje, item);
-          }
-     }
-
-proceed:
-
-   text = efl_text_get(text_obj);
-   if (!text)
-     {
-        ERR("text=NULL for edje %p, part 'elm.text'", sd->entry_edje);
-
-        return NULL;
-     }
-
-   if (sd->append_text_len > 0)
-     {
-        char *tmpbuf;
-        size_t len, tlen;
-
-        tlen = strlen(text);
-        len = tlen + sd->append_text_len - sd->append_text_position;
-        /* FIXME: need that or we do copy uninitialised data */
-        tmpbuf = calloc(1, len + 1);
-        if (!tmpbuf)
-          {
-             ERR("Failed to allocate memory for entry's text %p", obj);
-             return NULL;
-          }
-        memcpy(tmpbuf, text, tlen);
-
-        if (sd->append_text_left)
-          memcpy(tmpbuf + tlen, sd->append_text_left
-                 + sd->append_text_position, sd->append_text_len
-                 - sd->append_text_position);
-        tmpbuf[len] = '\0';
-        eina_stringshare_replace(&sd->text, tmpbuf);
-        free(tmpbuf);
-     }
-   else
-     {
-        eina_stringshare_replace(&sd->text, text);
-     }
-
-   return sd->text;
-}
 
 static char *
 _access_info_cb(void *data EINA_UNUSED, Evas_Object *obj)

src/lib/elementary/efl_ui_text.eo
@@ -414,7 +414,6 @@ class Efl.Ui.Text (Elm.Layout, Elm.Interface_Scrollable, Efl.Ui.Clickable,
       Elm.Widget.focus_next_manager_is;
       Elm.Layout.theme_enable;
       Elm.Layout.sizing_eval;
-      Elm.Layout.text { get; set; }
       Elm.Layout.signal_callback_add;
       Elm.Layout.signal_callback_del;
       Elm.Layout.signal_emit;

src/lib/elementary/elc_fileselector.c
@@ -19,6 +19,9 @@
 #include "elm_interface_fileselector.h"
 #include "elm_widget_fileselector.h"
 
+#include "elm_fileselector_internal_part.eo.h"
+#include "elm_part_helper.h"
+
 #define MY_CLASS ELM_FILESELECTOR_CLASS
 
 #define MY_CLASS_NAME "Elm_Fileselector"
@@ -3108,7 +3111,7 @@ _elm_fileselector_elm_widget_focus_direction(Eo *obj EINA_UNUSED, Elm_Fileselect
 }
 
 EOLIAN static Eina_Bool
-_elm_fileselector_elm_layout_text_set(Eo *obj EINA_UNUSED, Elm_Fileselector_Data *sd, const char *part, const char *label)
+_elm_fileselector_text_set(Eo *obj EINA_UNUSED, Elm_Fileselector_Data *sd, const char *part, const char *label)
 {
    if (!part) return EINA_FALSE;
 
@@ -3124,8 +3127,8 @@ _elm_fileselector_elm_layout_text_set(Eo *obj EINA_UNUSED, Elm_Fileselector_Data
      }
    else
      {
-        Eina_Bool int_ret = EINA_FALSE;
-        int_ret = elm_obj_layout_text_set(efl_super(obj, MY_CLASS), part, label);
+        Eina_Bool int_ret = EINA_TRUE;
+        efl_text_set(efl_part(efl_super(obj, MY_CLASS), part), label);
         return int_ret;
      }
 
@@ -3188,6 +3191,14 @@ _elm_fileselector_elm_interface_atspi_widget_action_elm_actions_get(Eo *obj EINA
    return &atspi_actions[0];
 }
 
+
+/* Efl.Part begin */
+
+ELM_PART_OVERRIDE(elm_fileselector, ELM_FILESELECTOR, ELM_LAYOUT, Elm_Fileselector_Data, Elm_Part_Data)
+ELM_PART_OVERRIDE_TEXT_SET(elm_fileselector, ELM_FILESELECTOR, ELM_LAYOUT, Elm_Fileselector_Data, Elm_Part_Data)
+#include "elm_fileselector_internal_part.eo.c"
+
+/* Efl.Part end */
 /* Internal EO APIs and hidden overrides */
 
 #define ELM_FILESELECTOR_EXTRA_OPS \

src/lib/elementary/elc_fileselector_entry.c
@@ -256,11 +256,11 @@ _elm_fileselector_entry_elm_widget_disable(Eo *obj, Elm_Fileselector_Entry_Data 
 }
 
 EOLIAN static Eina_Bool
-_elm_fileselector_entry_elm_layout_text_set(Eo *obj, Elm_Fileselector_Entry_Data *sd, const char *part, const char *label)
+_elm_fileselector_entry_text_set(Eo *obj, Elm_Fileselector_Entry_Data *sd, const char *part, const char *label)
 {
    if (part && strcmp(part, "default"))
      {
-        return elm_obj_layout_text_set(efl_super(obj, MY_CLASS), part, label);
+        efl_text_set(efl_part(efl_super(obj, MY_CLASS), part), label);
      }
 
    elm_object_text_set(sd->button, label);
@@ -268,12 +268,12 @@ _elm_fileselector_entry_elm_layout_text_set(Eo *obj, Elm_Fileselector_Entry_Data
 }
 
 EOLIAN static const char *
-_elm_fileselector_entry_elm_layout_text_get(Eo *obj, Elm_Fileselector_Entry_Data *sd, const char *part)
+_elm_fileselector_entry_text_get(Eo *obj, Elm_Fileselector_Entry_Data *sd, const char *part)
 {
    if (part && strcmp(part, "default"))
      {
         const char *text = NULL;
-        text = elm_obj_layout_text_get(efl_super(obj, MY_CLASS), part);
+        text = efl_text_get(efl_part(efl_super(obj, MY_CLASS), part));
         return text;
      }
 
@@ -681,6 +681,8 @@ ELM_PART_OVERRIDE(elm_fileselector_entry, ELM_FILESELECTOR_ENTRY, ELM_LAYOUT, El
 ELM_PART_OVERRIDE_CONTENT_SET(elm_fileselector_entry, ELM_FILESELECTOR_ENTRY, ELM_LAYOUT, Elm_Fileselector_Entry_Data, Elm_Part_Data)
 ELM_PART_OVERRIDE_CONTENT_GET(elm_fileselector_entry, ELM_FILESELECTOR_ENTRY, ELM_LAYOUT, Elm_Fileselector_Entry_Data, Elm_Part_Data)
 ELM_PART_OVERRIDE_CONTENT_UNSET(elm_fileselector_entry, ELM_FILESELECTOR_ENTRY, ELM_LAYOUT, Elm_Fileselector_Entry_Data, Elm_Part_Data)
+ELM_PART_OVERRIDE_TEXT_SET(elm_fileselector_entry, ELM_FILESELECTOR_ENTRY, ELM_LAYOUT, Elm_Fileselector_Entry_Data, Elm_Part_Data)
+ELM_PART_OVERRIDE_TEXT_GET(elm_fileselector_entry, ELM_FILESELECTOR_ENTRY, ELM_LAYOUT, Elm_Fileselector_Entry_Data, Elm_Part_Data)
 #include "elm_fileselector_entry_internal_part.eo.c"
 
 /* Efl.Part end */

commit ca8a8b712c8f6693700d516e5695561b11df7540

Elm layout: fix a few objects after move to efl_part
Fixes elm_layout_text_set/get bugs that were introduced in
3eb649b180eb98f7be829de5aed7e8c7f1ceb35a.

src/lib/elementary/elm_entry.c
@@ -3192,7 +3192,7 @@ _elm_entry_text_set(Eo *obj, Elm_Entry_Data *sd, const char *part, const char *e
    int len = 0;
 
    if (!entry) entry = "";
-   if (part)
+   if (part && strcmp(part, "elm.text"))
      {
         if (!strcmp(part, "guide"))
           edje_object_part_text_set(sd->entry_edje, "elm.guide", entry);
@@ -3244,7 +3244,7 @@ _elm_entry_text_get(Eo *obj, Elm_Entry_Data *sd, const char *item)
 
    if (item)
      {
-        if (!strcmp(item, "default")) goto proceed;
+        if (!strcmp(item, "default") || strcmp(item, "elm.text")) goto proceed;
         else if (!strcmp(item, "guide"))
           {
              return edje_object_part_text_get(sd->entry_edje, "elm.guide");
@@ -4147,7 +4147,7 @@ elm_entry_entry_get(const Evas_Object *obj)
 {
    ELM_ENTRY_CHECK(obj) NULL;
    const char *text = NULL;
-   text = efl_text_get(efl_part(efl_super(obj, MY_CLASS), "elm.text"));
+   text = efl_text_get(efl_part(obj, "elm.text"));
    return text;
 }
 

src/lib/elementary/elm_layout.c
@@ -2318,6 +2318,10 @@ elm_layout_table_clear(Elm_Layout *obj, const char *part, Eina_Bool clear)
 EAPI Eina_Bool
 elm_layout_text_set(Elm_Layout *obj, const char *part, const char *text)
 {
+   if (!part || (*part == '\0'))
+     {
+        part = "elm.text";
+     }
    efl_text_set(efl_part(obj, part), text);
    return EINA_TRUE;
 }
@@ -2325,6 +2329,10 @@ elm_layout_text_set(Elm_Layout *obj, const char *part, const char *text)
 EAPI const char *
 elm_layout_text_get(const Elm_Layout *obj, const char *part)
 {
+   if (!part || (*part == '\0'))
+     {
+        part = "elm.text";
+     }
    return efl_text_get(efl_part(obj, part));
 }
 