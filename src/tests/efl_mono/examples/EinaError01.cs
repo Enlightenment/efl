7497481edc8b26e133382f4c6f8a70fadf816fa5
Vitor Sousa <vitorsousasilva@gmail.com>
2017-06-07 20:49:50 -0300

    eina_mono: add example for eina.Error

---
+++ src/tests/efl_mono/examples/EinaError01.cs
@@ -0,0 +1,76 @@
+using static System.Console;
+
+namespace TestSuite
+{
+
+class TestExampleEinaError01
+{
+    private static bool RegisterdErrors = false;
+    private static eina.Error MyErrorNegative;
+    private static eina.Error MyErrorNull;
+
+    private static void testFunc(int n, string s)
+    {
+        if (!RegisterdErrors)
+        {
+            MyErrorNegative = eina.Error.Register("Negative number");
+            MyErrorNull = eina.Error.Register("NULL pointer");
+            RegisterdErrors = true;
+        }
+
+        if (n < 0)
+        {
+            eina.Error.Set(MyErrorNegative);
+            return;
+        }
+
+        if (s == null)
+        {
+            eina.Error.Set(MyErrorNull);
+            return;
+        }
+    }
+
+    public static void EinaError01()
+    {
+        // Handling Eina_Error with exception
+        try
+        {
+            testFunc(-1, "abc");
+            eina.Error.RaiseIfOcurred();
+        }
+        catch(efl.EflException e)
+        {
+            WriteLine($"Caught error: {e.Message}");
+        }
+
+        // Handling Eina_Error directly
+        testFunc(42, null);
+        eina.Error err = eina.Error.Get();
+        if (err != 0)
+        {
+            WriteLine($"Error set: {err.Message}");
+        }
+        eina.Error.Clear();
+
+        // No error set
+        try
+        {
+            testFunc(42, "abc");
+
+            eina.Error.RaiseIfOcurred();
+
+            err = eina.Error.Get();
+            WriteLine($"Really no error? {err == eina.Error.NO_ERROR}.");
+        }
+        catch
+        {
+            WriteLine("Unspected error!!!");
+        }
+
+        WriteLine("No error message is empty string: \"{0}\"", eina.Error.NO_ERROR.Message);
+        WriteLine("No error message is empty string: \"{0}\"", eina.Error.MsgGet(0));
+    }
+}
+
+}
