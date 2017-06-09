53bfe003513b7b309b1e2f8b29be07c61801a1b7
Chris Michael <cp.michael@samsung.com>
2017-06-09 13:47:47 -0400

    ecore-evas-drm: Remove borrowed fourcc_code from source file
    As we will now use static_libs/libdrm for building ecore_evas_drm, we
    no longer need to include this bit of borrowed four_cc code from libdrm
    
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

--- src/modules/ecore_evas/engines/drm/ecore_evas_drm.c
+++ src/modules/ecore_evas/engines/drm/ecore_evas_drm.c
@@ -46,37 +46,6 @@
 # endif
 #endif /* ! _WIN32 */
 
-/* DRM_FORMAT_XRGB8888 and fourcc_code borrowed from <drm_fourcc.h>
- *
- * Copyright 2011 Intel Corporation
- *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice (including the next
- * paragraph) shall be included in all copies or substantial portions of the
- * Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
- * VA LINUX SYSTEMS AND/OR ITS SUPPLIERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
- * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- */
-#define fourcc_code(a, b, c, d) \
-   ((uint32_t)(a) | ((uint32_t)(b) << 8) | \
-       ((uint32_t)(c) << 16) | ((uint32_t)(d) << 24))
-#define DRM_FORMAT_XRGB8888 \
-   fourcc_code('X', 'R', '2', '4') /* [31:0] x:R:G:B 8:8:8:8 little endian */
-
-/* end borrowed <drm_fourcc.h> code */
-
 typedef struct _Ecore_Evas_Engine_Drm_Data
 {
    int fd;
