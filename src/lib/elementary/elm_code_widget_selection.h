0e21b4d280cd5b650989a15d064fdb6c7c5a71ed
Sanjeev BA <iamsanjeev@gmail.com>
2017-06-19 13:54:33 -0700

    Documentation for elm_code_widget_selection.
    Reviewers: jpeg, ajwillia.ms
    
    Subscribers: cedric, jpeg
    
    Differential Revision: https://phab.enlightenment.org/D4981
    
    Signed-off-by: Cedric BAIL <cedric@osg.samsung.com>

--- src/lib/elementary/elm_code_widget_selection.h
+++ src/lib/elementary/elm_code_widget_selection.h
@@ -15,26 +15,132 @@ extern "C" {
  *
  */
 
+/**
+ * @brief Start the selection of widget contents.
+ *
+ * @param[in] widget @c The elm_widget object.
+ * @param[in] line @c The line index of the widget.
+ * @param[in] widget @c The column index of the widget.
+ *
+ * @return None
+ *
+ */
 EAPI void elm_code_widget_selection_start(Evas_Object *widget, unsigned int line, unsigned int col);
 
+/**
+ * @brief End the selection of widget contents.
+ *
+ * @param[in] widget @c The elm_widget object.
+ * @param[in] line @c The line index of the widget.
+ * @param[in] widget @c The column index of the widget.
+ *
+ * @return None
+ *
+ */
 EAPI void elm_code_widget_selection_end(Evas_Object *widget, unsigned int line, unsigned int col);
 
+/**
+ * @brief Clear selection of widget contents.
+ *
+ * @param[in] widget @c The elm_widget object.
+ *
+ * @return None
+ *
+ */
 EAPI void elm_code_widget_selection_clear(Evas_Object *widget);
 
+/**
+ * @brief Delete the selected contents of widget contents.
+ *
+ * @param[in] widget @c The elm_widget object.
+ *
+ * @return None
+ *
+ */
 EAPI void elm_code_widget_selection_delete(Evas_Object *widget);
 
+/**
+ * @brief Select the entire line from the widget.
+ *
+ * @param[in] widget @c The elm_widget object.
+ * @param[in] line @c The line index of the widget.
+ *
+ * @return None
+ *
+ */
 EAPI void elm_code_widget_selection_select_line(Evas_Object *widget, unsigned int line);
 
+/**
+ * @brief Select a word from the widget.
+ *
+ * @param[in] widget @c The elm_widget object.
+ * @param[in] line @c The line index of the widget.
+ * @param[in] widget @c The column index of the widget.
+ *
+ * @return None
+ *
+ */
 EAPI void elm_code_widget_selection_select_word(Evas_Object *widget, unsigned int line, unsigned int col);
 
+/**
+ * @brief Get the selected widget content.
+ *
+ * @param[in] widget @c The elm_widget object.
+ *
+ * @return char pointer to text contents.
+ *
+ */
 EAPI char *elm_code_widget_selection_text_get(Evas_Object *widget);
 
+/**
+ * @brief Cut the selected widget content.
+ *
+ * @param[in] widget @c The elm_widget object.
+ *
+ * @return None
+ *
+ */
 EAPI void elm_code_widget_selection_cut(Evas_Object *widget);
+
+/**
+ * @brief Copy the selected widget content to clipboard.
+ *
+ * @param[in] widget @c The elm_widget object.
+ *
+ * @return None
+ *
+ */
 EAPI void elm_code_widget_selection_copy(Evas_Object *widget);
+
+/**
+ * @brief Paste the copied widget content from clipboard.
+ *
+ * @param[in] widget @c The elm_widget object.
+ *
+ * @return None
+ *
+ */
+
 EAPI void elm_code_widget_selection_paste(Evas_Object *widget);
 
+/**
+ * @brief check if the widget selection is empty.
+ *
+ * @param[in] widget @c The elm_widget object.
+ *
+ * @return true is empty, false otherwise
+ *
+ */
 EAPI Eina_Bool elm_code_widget_selection_is_empty(Evas_Object *widget);
 
+/**
+ * @brief Select the entire widget contents.
+ *
+ * @param[in] widget @c The elm_widget object.
+ *
+ * @return None
+ *
+ */
 EAPI void elm_code_widget_selection_select_all(Evas_Object *widget);
 
 
