8c73e94cdf4af2a33afa95eeec31495650863c8e
Lauro Moura <lauromoura@expertisesolutions.com.br>
2017-07-07 17:42:52 -0300

    efl_mono: Add DisposableIntPtr
    It is a wrapper around IntPtr that calls Marshall.FreeHGlobal on
    dispose, so you free them when exiting a block.
    
    This can be useful when dealing with intptrs returned from
    marshalling functions, where you don't know if it should be freed or
    not (e.g. strings vs ints).

--- src/bindings/mono/eina_mono/eina_config.cs
+++ src/bindings/mono/eina_mono/eina_config.cs
@@ -22,4 +22,42 @@ public class Config {
     }
 
 }
+
+/// <summary>
+/// Wrapper class for pointers that need some cleanup afterwards
+/// like strings.
+/// </summary>
+public class DisposableIntPtr : IDisposable {
+
+    public IntPtr Handle { get; set; }
+    private bool ShouldFree;
+    private bool Disposed;
+
+    /// <summary>Wraps a new ptr what will be freed based on the
+    /// value of shouldFree</summary>
+    public DisposableIntPtr(IntPtr ptr, bool shouldFree=false)
+    {
+        Handle = ptr;
+        ShouldFree = shouldFree;
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!Disposed && ShouldFree) {
+            Marshal.FreeHGlobal(this.Handle);
+        }
+        Disposed = true;
+    }
+
+    ~DisposableIntPtr()
+    {
+        Dispose(false);
+    }
+}
 }
