b156f606e4fefb9b1d3ac0ca6d180e0d70e0140c
Jean-Philippe Andre <jp.andre@samsung.com>
2017-07-12 10:24:11 +0900

    colorselector: Avoid API call on NULL object
    Solves this error:
      ERR<2321>:eo ../src/lib/efl/interfaces/efl_pack_layout.eo.c:1
      efl_pack_layout_request() Unable to resolve op for api func
      0x7fffed16dba0 for obj=(nil) ((null))
    
    Fixes T5715

--- src/lib/elementary/elm_colorselector.c
+++ src/lib/elementary/elm_colorselector.c
@@ -1507,7 +1507,7 @@ _elm_colorselector_elm_layout_sizing_eval(Eo *obj, Elm_Colorselector_Data *sd)
         return;
      }
 
-   efl_pack_layout_request(sd->palette_box);
+   if (sd->palette_box) efl_pack_layout_request(sd->palette_box);
    edje_object_size_min_calc(wd->resize_obj, &minw, &minh);
    evas_object_size_hint_min_set(obj, minw, minh);
 }
