05d1175782a25dab253b689e409f10e7dbd85beb
Taehyub Kim <taehyub.kim@samsung.com>
2017-07-28 14:48:48 +0900

    efl_ui_popup: add Efl.Ui.Popup class
    Summary: add initial code for Efl.Ui.Popup class
    
    Test Plan: 1. run elementary_test -to efluipopup
    
    Reviewers: Jaehyun_Cho, Blackmole, thiepha, woohyun, cedric
    
    Reviewed By: Jaehyun_Cho
    
    Subscribers: bu5hm4n, cedric, jpeg
    
    Differential Revision: https://phab.enlightenment.org/D5037

---
+++ src/lib/elementary/efl_ui_popup_private.h
@@ -0,0 +1,11 @@
+#ifndef EFL_UI_WIDGET_POPUP_H
+#define EFL_UI_WIDGET_POPUP_H
+
+typedef struct _Efl_Ui_Popup_Data Efl_Ui_Popup_Data;
+struct _Efl_Ui_Popup_Data
+{
+   Evas_Object *win_parent;
+   Evas_Object *event_bg;
+};
+
+#endif
