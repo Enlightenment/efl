2ab17913c25996cdd22b7d012317af0234e09509
Jean-Philippe Andre <jp.andre@samsung.com>
2017-08-10 14:55:30 +0900

    ctxpopup: Remove use of Orientation API (EO)
    ctxpopup belongs to legacy, some work is ongoing to provide a cleaner EO
    based API for popups, etc... See T5326
    
    Revert "ctxpopup: implement Efl.Orientation interface functions"
    
    This reverts commit 736fb9d4980a7dee83ca0721d8b9b58c6c12a39d.
    
    Ref T5870

--- src/lib/elementary/elc_ctxpopup_legacy.h
+++ src/lib/elementary/elc_ctxpopup_legacy.h
@@ -9,27 +9,6 @@
 EAPI Evas_Object                 *elm_ctxpopup_add(Evas_Object *parent);
 
 /**
- * @brief Change the ctxpopup's orientation to horizontal or vertical.
- *
- * @param obj Elm Ctxpopup object
- * @param horizontal @c EINA_TRUE for horizontal mode, @c EINA_FALSE for vertical.
- *
- * @ingroup Elm_Ctxpopup
- */
-EAPI void                         elm_ctxpopup_horizontal_set(Evas_Object *obj, Eina_Bool horizontal);
-
-/**
- * @brief Get the value of current ctxpopup object's orientation.
- *
- * @param obj Elm Ctxpopup object
- * return @c EINA_TRUE for horizontal mode, @c EINA_FALSE for vertical.
- * See also @ref elm_ctxpopup_horizontal_set
- *
- * @ingroup Elm_Ctxpopup
- */
-EAPI Eina_Bool                    elm_ctxpopup_horizontal_get(const Evas_Object *obj);
-
-/**
  * @brief Get the internal list of items in a given ctxpopup widget.
  *
  * This list is not to be modified in any way and must not be freed. Use the
