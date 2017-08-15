dee65b525c387f482e0f860f87a1b3e110e3f0e3
Vincent Torri <vincent.torri@gmail.com>
2017-08-15 16:42:13 +0200

    ector: fix EAPI on Windows
    Signed-off-by: Cedric BAIL <cedric@osg.samsung.com>

--- src/lib/ector/gl/Ector_GL.h
+++ src/lib/ector/gl/Ector_GL.h
@@ -3,6 +3,32 @@
 
 #include <Ector.h>
 
+#ifdef EAPI
+# undef EAPI
+#endif
+
+#ifdef _WIN32
+# ifdef EFL_ECTOR_BUILD
+#  ifdef DLL_EXPORT
+#   define EAPI __declspec(dllexport)
+#  else
+#   define EAPI
+#  endif /* ! DLL_EXPORT */
+# else
+#  define EAPI __declspec(dllimport)
+# endif /* ! EFL_EO_BUILD */
+#else
+# ifdef __GNUC__
+#  if __GNUC__ >= 4
+#   define EAPI __attribute__ ((visibility("default")))
+#  else
+#   define EAPI
+#  endif
+# else
+#  define EAPI
+# endif
+#endif /* ! _WIN32 */
+
 #ifdef EFL_BETA_API_SUPPORT
 
 #ifndef _ECTOR_GL_SURFACE_EO_CLASS_TYPE
@@ -23,5 +49,8 @@ typedef short        GLshort;
 #include "gl/ector_renderer_gl_gradient_radial.eo.h"
 
 #endif
+
+#undef EAPI
+#define EAPI
 
 #endif
--- src/lib/ector/software/Ector_Software.h
+++ src/lib/ector/software/Ector_Software.h
@@ -3,6 +3,32 @@
 
 #include <Ector.h>
 
+#ifdef EAPI
+# undef EAPI
+#endif
+
+#ifdef _WIN32
+# ifdef EFL_ECTOR_BUILD
+#  ifdef DLL_EXPORT
+#   define EAPI __declspec(dllexport)
+#  else
+#   define EAPI
+#  endif /* ! DLL_EXPORT */
+# else
+#  define EAPI __declspec(dllimport)
+# endif /* ! EFL_EO_BUILD */
+#else
+# ifdef __GNUC__
+#  if __GNUC__ >= 4
+#   define EAPI __attribute__ ((visibility("default")))
+#  else
+#   define EAPI
+#  endif
+# else
+#  define EAPI
+# endif
+#endif /* ! _WIN32 */
+
 #ifdef EFL_BETA_API_SUPPORT
 
 #include "software/ector_software_surface.eo.h"
@@ -14,5 +40,8 @@
 #include "software/ector_renderer_software_gradient_radial.eo.h"
 
 #endif
+
+#undef EAPI
+#define EAPI
 
 #endif
