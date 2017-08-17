840fdeb37581d280304c222d24ed167b7f91b0f9
Taehyub Kim <taehyub.kim@samsung.com>
2017-08-17 17:37:54 +0900

    efl_ui_popup: add align and position properties
    Summary:
    add align feature and position properties (center, left, right, top, bottom)
    efl_ui_popup_position_set should be seperated from evas_object_move
    since evas_object_move can be called internally.
    
    Test Plan: 1. run elementary_test -to efluipopup
    
    Reviewers: Jaehyun_Cho, jpeg, cedric, thiepha, Blackmole, woohyun
    
    Reviewed By: Jaehyun_Cho
    
    Differential Revision: https://phab.enlightenment.org/D5105

--- src/lib/elementary/efl_ui_popup_private.h
+++ src/lib/elementary/efl_ui_popup_private.h
@@ -4,8 +4,10 @@
 typedef struct _Efl_Ui_Popup_Data Efl_Ui_Popup_Data;
 struct _Efl_Ui_Popup_Data
 {
+   Evas_Object *self;
    Evas_Object *win_parent;
    Evas_Object *event_bg;
+   Efl_Ui_Popup_Align align;
    Eina_Bool    bg_repeat_events : 1;
 };
 
