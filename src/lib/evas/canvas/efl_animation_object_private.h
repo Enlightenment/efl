b4d4e2a2cc4f673fc08554b659846b72f8833285
Jaehyun Cho <jae_hyun.cho@samsung.com>
2017-08-24 22:34:13 +0900

    efl_animation: Add animation object
    Efl.Animation.Object is a class which starts or cancels animation.
    Efl.Animation.Object instance is created by Efl.Animation instance.
    So Efl.Animation.Object instance contains animation properties from
    Efl.animation instance.
    
    Animation properties are set by using Efl.Animation instance and the
    animation is started or cancelled by using Efl.Animation.Object.

---
+++ src/lib/evas/canvas/efl_animation_object_private.h
@@ -0,0 +1,53 @@
+#define EFL_ANIMATION_OBJECT_PROTECTED
+
+#include "evas_common_private.h"
+#include <Ecore.h>
+
+#define MY_CLASS EFL_ANIMATION_OBJECT_CLASS
+#define MY_CLASS_NAME efl_class_name_get(MY_CLASS)
+
+typedef struct _Target_State
+{
+   Evas_Coord x, y, w, h;
+   int        r, g, b, a;
+
+   Evas_Map  *map;
+   Eina_Bool  enable_map : 1;
+} Target_State;
+
+typedef struct _Efl_Animation_Object_Data
+{
+   Ecore_Animator    *animator;
+
+   struct {
+        double begin;
+        double current;
+     } time;
+
+   Efl_Canvas_Object *target;
+   Target_State      *target_state;
+
+   double             progress;
+
+   double             duration;
+
+   Eina_Bool          auto_del : 1;
+   Eina_Bool          is_deleted : 1;
+   Eina_Bool          is_cancelled : 1;
+   Eina_Bool          keep_final_state : 1;
+} Efl_Animation_Object_Data;
+
+#define EFL_ANIMATION_OBJECT_CHECK_OR_RETURN(anim_obj, ...) \
+   do { \
+      if (!anim_obj) { \
+         CRI("Efl_Animation_Object " # anim_obj " is NULL!"); \
+         return __VA_ARGS__; \
+      } \
+      if (efl_animation_object_is_deleted(anim_obj)) { \
+         ERR("Efl_Animation_Object " # anim_obj " has already been deleted!"); \
+         return __VA_ARGS__; \
+      } \
+   } while (0)
+
+#define EFL_ANIMATION_OBJECT_DATA_GET(o, pd) \
+   Efl_Animation_Object_Data *pd = efl_data_scope_get(o, EFL_ANIMATION_OBJECT_CLASS)
