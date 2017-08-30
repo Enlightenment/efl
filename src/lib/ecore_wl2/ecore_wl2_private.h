e9d9ee0f4e0c7116a05b8f7fb385f9b76e715912
Chris Michael <cp.michael@samsung.com>
2017-08-30 10:22:33 -0400

    ecore-wl2: Formatting
    NB: No functional changes
    
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

--- src/lib/ecore_wl2/ecore_wl2_private.h
+++ src/lib/ecore_wl2/ecore_wl2_private.h
@@ -4,10 +4,10 @@
 # include <unistd.h>
 # include "Ecore_Wl2.h"
 # include "Ecore_Input.h"
-#include <xkbcommon/xkbcommon-compose.h>
+# include <xkbcommon/xkbcommon-compose.h>
 # include "www-client-protocol.h"
 
-#define EFL_TEAMWORK_VERSION 2
+# define EFL_TEAMWORK_VERSION 2
 # include "teamwork-client-protocol.h"
 
 # include "session-recovery-client-protocol.h"
