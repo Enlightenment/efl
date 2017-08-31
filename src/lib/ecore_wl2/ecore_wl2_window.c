Enlightenment/efl

commit 1bb45f6e6194fa046d3d1409e1aacecc14dc4b4f

ecore_wl2: Use ecore_wl2_window_commit for all wl_surface_commits
This adds an explicit flush to every surface commit.

src/lib/ecore_wl2/ecore_wl2_window.c
@@ -24,6 +24,7 @@ _ecore_wl2_window_semi_free(Ecore_Wl2_Window *window)
    if (window->surface) wl_surface_destroy(window->surface);
    window->surface = NULL;
    window->surface_id = -1;
+   ecore_wl2_display_flush(window->display);
 }
 
 
@@ -234,7 +235,7 @@ _ecore_wl2_window_zxdg_popup_create(Ecore_Wl2_Window *win)
 
    win->pending.configure = EINA_TRUE;
 
-   wl_surface_commit(win->surface);
+   ecore_wl2_window_commit(win, EINA_TRUE);
 }
 
 static void
@@ -352,7 +353,7 @@ _ecore_wl2_window_shell_surface_init(Ecore_Wl2_Window *window)
                  window->aspect.w, window->aspect.h, window->aspect.aspect);
           }
 
-        wl_surface_commit(window->surface);
+        ecore_wl2_window_commit(window, EINA_TRUE);
      }
 
    if (window->display->wl.session_recovery)
@@ -541,7 +542,7 @@ ecore_wl2_window_hide(Ecore_Wl2_Window *window)
    if (window->surface)
      {
         wl_surface_attach(window->surface, NULL, 0, 0);
-        wl_surface_commit(window->surface);
+        ecore_wl2_window_commit(window, EINA_TRUE);
         window->commit_pending = EINA_FALSE;
      }
 

src/lib/ecore_wl2/ecore_wl2_display.c
@@ -700,6 +700,7 @@ _ecore_wl2_display_connect(Ecore_Wl2_Display *ewd, Eina_Bool sync)
    ewd->idle_enterer = ecore_idle_enterer_add(_cb_connect_idle, ewd);
 
    _ecore_wl2_display_event(ewd, ECORE_WL2_EVENT_CONNECT);
+   ecore_wl2_display_flush(ewd);
    return EINA_TRUE;
 }
 

commit d0b8c41f920038f41446f06c3d08dc9c57adf35c

ecore_wl2: Fix session recovery
I broke this in commit 1bb45f6e6194fa046d3d1409e1aacecc14dc4b4f

We intentionally *don't* flush in window_semi_free, as this could be
when there's no compositor left to flush to (in the case of session
recovery).

src/lib/ecore_wl2/ecore_wl2_window.c
@@ -24,7 +24,6 @@ _ecore_wl2_window_semi_free(Ecore_Wl2_Window *window)
    if (window->surface) wl_surface_destroy(window->surface);
    window->surface = NULL;
    window->surface_id = -1;
-   ecore_wl2_display_flush(window->display);
 }
 
 