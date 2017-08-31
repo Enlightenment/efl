f69be30bb6c08a2959cc3f224eac7a44ace0f2a9
Mike Blumenkrantz <zmike@osg.samsung.com>
2017-08-31 15:03:23 -0400

    evas: handle object names with a list hash
    a canvas can have multiple objects with the same name. assuming that name:obj
    is a 1:1 ratio means that name-finding functions are likely to return invalid
    objects
    
    @fix

--- src/lib/evas/canvas/evas_main.c
+++ src/lib/evas/canvas/evas_main.c
@@ -217,7 +217,7 @@ _evas_canvas_efl_object_constructor(Eo *eo_obj, Evas_Public_Data *e)
    e->framespace.h = 0;
    e->hinting = EVAS_FONT_HINTING_BYTECODE;
    e->current_event = EVAS_CALLBACK_LAST;
-   e->name_hash = eina_hash_string_superfast_new(NULL);
+   e->name_hash = eina_hash_string_superfast_new((Eina_Free_Cb)eina_list_free);
    eina_clist_init(&e->calc_list);
    eina_clist_init(&e->calc_done);
 
