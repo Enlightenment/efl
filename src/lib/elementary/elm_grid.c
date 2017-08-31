65d2dfc8925a183bf7f2709677d87edcd759e9c3
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2017-08-31 16:12:57 +0900

    elm focus manager - dont leak child lists when updating focus order
    this should fix T5800
    
    @fix

--- src/lib/elementary/elm_grid.c
+++ src/lib/elementary/elm_grid.c
@@ -20,6 +20,7 @@ _focus_order_flush(Eo *obj)
    Eina_List *order = evas_object_grid_children_get(wpd->resize_obj);
 
    efl_ui_focus_manager_calc_update_order(wpd->focus.manager, obj, order);
+   eina_list_free(order);
 }
 
 
