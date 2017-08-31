9caca134ff682d6162266766a10fe810d89be7ab
Taehyub Kim <taehyub.kim@samsung.com>
2017-08-31 13:18:45 +0900

    efl_ui_popup_alert_scroll: add Efl.Ui.Popup.Alert.Scroll class
    Summary:
    Add initial code for Efl.Ui.Popup.Alert.Scroll Class.
    It supports scrolling content.
    
    Test Plan: 1. run elementary_test -to efluipopupalertscroll
    
    Reviewers: Jaehyun_Cho, jpeg, thiepha, cedric, Blackmole, woohyun
    
    Reviewed By: Jaehyun_Cho
    
    Differential Revision: https://phab.enlightenment.org/D5123

---
+++ src/lib/elementary/efl_ui_popup_alert_scroll_private.h
@@ -0,0 +1,17 @@
+#ifndef EFL_UI_POPUP_ALERT_SCROLL_H
+#define EFL_UI_POPUP_ALERT_SCROLL_H
+
+#include "Elementary.h"
+
+typedef struct _Efl_Ui_Popup_Alert_Scroll_Data Efl_Ui_Popup_Alert_Scroll_Data;
+struct _Efl_Ui_Popup_Alert_Scroll_Data
+{
+   Evas_Object *scroller;
+   Evas_Object *content;
+   Evas_Coord max_scroll_w;
+   Evas_Coord max_scroll_h;
+   Eina_Bool is_expandable_w;
+   Eina_Bool is_expandable_h;
+};
+
+#endif
