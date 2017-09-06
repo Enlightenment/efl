26bacce0fd117896be65ebd75db4fed433529bd1
Jaehyun Cho <jae_hyun.cho@samsung.com>
2017-09-06 20:03:26 +0900

    Revert "naviframe: Fix to finish transition for newly pushed item"
    This reverts commit 8574128b67d336e796ca0f0abc9b97e65f7799d4.

--- src/lib/elementary/elc_naviframe.c
+++ src/lib/elementary/elc_naviframe.c
@@ -627,10 +627,11 @@ end:
          * This case can happen when an item is deleted by elm_object_item_del()
          * right after the item is newly pushed.
          */
-        if (nit == nfo->self)
-          nfo->self = NULL;
-        else if (nit == nfo->related)
-          nfo->related = NULL;
+        if ((nfo->self == nit) || (nfo->related == nit))
+          {
+             nfo->self = NULL;
+             nfo->related = NULL;
+          }
      }
 
    _item_free(nit);
