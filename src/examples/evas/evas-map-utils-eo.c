Enlightenment/efl

commit 13da5e980eb43288b9b9f502cb6a7a000e1f26ea

efl_gfx: Use Eina.Rectangle for geometry (EO)
This saves a few lines already (without even having the proper helpers
for stack rectangles).

src/examples/evas/evas-map-utils-eo.c
@@ -70,7 +70,8 @@ _anim_cb(void *data)
 {
    App_Data *ad = data;
    Evas_Object *o;
-   int r, g, b, a, x, y, w, h, f;
+   Eina_Rectangle r;
+   int r, g, b, a, f;
    int win_w, win_h, mx, my;
 
    f = ad->frame;
@@ -127,12 +128,12 @@ _anim_cb(void *data)
 
 
    o = evas_object_name_find(ad->canvas, "obj4");
-   efl_gfx_geometry_get(o, &x, &y, &w, &h);
+   r = efl_gfx_geometry_get(o);
    efl_gfx_map_reset(o);
    efl_gfx_map_smooth_set(o, ad->smooth);
    efl_gfx_map_alpha_set(o, ad->alpha);
-   efl_gfx_map_coord_absolute_set(o, 0, x, y + h, 0);
-   efl_gfx_map_coord_absolute_set(o, 1, x + w, y + h, 0);
+   efl_gfx_map_coord_absolute_set(o, 0, r.x, r.y + r.h, 0);
+   efl_gfx_map_coord_absolute_set(o, 1, r.x + r.w, r.y + r.h, 0);
    efl_gfx_map_coord_absolute_set(o, 2, win_w - 10, win_h - 30, 0);
    efl_gfx_map_coord_absolute_set(o, 3, (win_w / 2) + 10, win_h - 30, 0);
    efl_gfx_map_uv_set(o, 0, 0, 1);

src/bin/elementary/test_evas_map.c
@@ -41,7 +41,7 @@ _image_resize_cb(void *data EINA_UNUSED, Evas *e EINA_UNUSED, Evas_Object *obj, 
 {
    Evas_Coord w, h;
 
-   efl_gfx_geometry_get(obj, NULL, NULL, &w, &h);
+   efl_gfx_size_get(obj, &w, &h);
    _map_set(obj, w, h);
 }
 

src/bin/elementary/test_photocam.c
@@ -763,12 +763,11 @@ _zoomable_clicked_cb(void *data EINA_UNUSED, const Efl_Event *ev)
 static void
 _zoomable_move_resize_cb(void *data, const Efl_Event *ev)
 {
-   int x, y, w, h;
+   Eina_Rectangle r;
 
-   efl_gfx_geometry_get(ev->object, &x, &y, &w, &h);
-
-   efl_gfx_size_set(data, w, h);
-   efl_gfx_position_set(data, x, y);
+   r = efl_gfx_geometry_get(ev->object);
+   efl_gfx_size_set(data, r.w, r.h);
+   efl_gfx_position_set(data, r.x, r.y);
 }
 
 static void

src/bin/elementary/test_ui_box.c
@@ -170,7 +170,7 @@ _custom_layout_update(Eo *pack, const void *data EINA_UNUSED)
 {
    Eina_Iterator *it = efl_content_iterate(pack);
    int count = efl_content_count(pack), i = 0;
-   int px, py, pw, ph;
+   Eina_Rectangle rp;
    Eo *sobj;
 
    // Note: This is a TERRIBLE layout. Just an example of the API, not showing
@@ -178,16 +178,18 @@ _custom_layout_update(Eo *pack, const void *data EINA_UNUSED)
 
    if (!count) return;
 
-   efl_gfx_geometry_get(pack, &px, &py, &pw, &ph);
+   rp = efl_gfx_geometry_get(pack);
    EINA_ITERATOR_FOREACH(it, sobj)
      {
-        int x, y, h, w, mw, mh;
+        Eina_Rectangle r;
+        int mw, mh;
+
         efl_gfx_size_hint_combined_min_get(sobj, &mw, &mh);
-        x = (pw / count) * i;
-        y = (ph / count) * i;
-        w = mw;
-        h = mh;
-        efl_gfx_geometry_set(sobj, x + px, y + py, w, h);
+        r.x = (rp.w / count) * i;
+        r.y = (rp.h / count) * i;
+        r.w = mw;
+        r.h = mh;
+        efl_gfx_geometry_set(sobj, r);
         i++;
      }
    eina_iterator_free(it);

src/examples/evas/evas-3d-cube-rotate.c
@@ -262,7 +262,7 @@ main(void)
 
    /* Add an image object for 3D scene rendering. */
    image = efl_add(EFL_CANVAS_SCENE3D_CLASS, evas);
-   efl_gfx_geometry_set(image, 0, 0, WIDTH, HEIGHT);
+   efl_gfx_geometry_set(image, (Eina_Rectangle) { 0, 0, WIDTH, HEIGHT });
    evas_object_show(image);
 
    /* Set the image object as render target for 3D scene. */

src/lib/edje/edje_part.c
@@ -100,27 +100,17 @@ _efl_canvas_layout_part_efl_object_finalize(Eo *obj, Efl_Canvas_Layout_Part_Data
    return efl_finalize(efl_super(obj, MY_CLASS));
 }
 
-EOLIAN void
-_efl_canvas_layout_part_efl_gfx_geometry_get(Eo *obj, Efl_Canvas_Layout_Part_Data *pd, int *x, int *y, int *w, int *h)
+EOLIAN Eina_Rectangle
+_efl_canvas_layout_part_efl_gfx_geometry_get(Eo *obj, Efl_Canvas_Layout_Part_Data *pd)
 {
    Edje_Real_Part *rp = pd->rp;
+   Eina_Rectangle r = { 0, };
+
    PROXY_CALL_BEGIN(pd);
-
    _edje_recalc_do(pd->ed);
-   if (!rp)
-     {
-        if (x) *x = 0;
-        if (y) *y = 0;
-        if (w) *w = 0;
-        if (h) *h = 0;
-        RETURN_VOID;
-     }
+   if (!rp) RETURN_VAL(r);
 
-   if (x) *x = rp->x;
-   if (y) *y = rp->y;
-   if (w) *w = rp->w;
-   if (h) *h = rp->h;
-   RETURN_VOID;
+   RETURN_VAL(rp->rect);
 }
 
 EOLIAN static void

src/lib/edje/edje_private.h
@@ -2034,7 +2034,12 @@ struct _Edje_Real_Part
    } typedata; // 4
    FLOAT_T                   description_pos; // 8
    Edje_Rectangle            req; // 16
-   int                       x, y, w, h; // 16
+   union {
+      struct {
+         int                 x, y, w, h; // 16
+      };
+      Eina_Rectangle         rect;
+   };
    Evas_Object_Pointer_Mode  pointer_mode;
 #ifdef EDJE_CALC_CACHE
    unsigned short            state; // 2

src/lib/efl/interfaces/efl_gfx.eo
@@ -1,3 +1,4 @@
+import eina_types;
 import efl_gfx_types;
 
 interface Efl.Gfx {
@@ -45,10 +46,7 @@ interface Efl.Gfx {
       @property geometry {
          [[Rectangular geometry that combines both position and size.]]
          values {
-            x: int; [[X coordinate]]
-            y: int; [[Y coordinate]]
-            w: int; [[Width]]
-            h: int; [[Height]]
+            rect: Eina.Rectangle; [[The X,Y position and W,H size, in pixels.]]
          }
       }
       @property color {

commit b737d4fea7141b8076dd01ee7c7dd8c2c54d3284

examples/evas: fix recent build break from rectangle change
Introduced with commit 13da5e980eb43288b9b9f502cb6a7a000e1f26ea

A compile before pushing would have been great, again.
Having the same name for two variables is something no compiler likes.

evas-map-utils-eo.c:74:8: error: conflicting types for ‘r’
    int r, g, b, a, f;
        ^
evas-map-utils-eo.c:73:19: note: previous declaration of ‘r’ was here
    Eina_Rectangle r;
                   ^
evas-map-utils-eo.c:93:31: error: ‘h’ undeclared (first use in this function)
    efl_gfx_size_get(o, NULL, &h);
                               ^
evas-map-utils-eo.c:93:31: note: each undeclared identifier is reported only once for each function it appears in
evas-map-utils-eo.c:108:25: error: ‘w’ undeclared (first use in this function)
    efl_gfx_size_get(o, &w, &h);

src/examples/evas/evas-map-utils-eo.c
@@ -70,8 +70,8 @@ _anim_cb(void *data)
 {
    App_Data *ad = data;
    Evas_Object *o;
-   Eina_Rectangle r;
-   int r, g, b, a, f;
+   Eina_Rectangle rect;
+   int r, g, b, a, f, h, w;
    int win_w, win_h, mx, my;
 
    f = ad->frame;
@@ -128,12 +128,12 @@ _anim_cb(void *data)
 
 
    o = evas_object_name_find(ad->canvas, "obj4");
-   r = efl_gfx_geometry_get(o);
+   rect = efl_gfx_geometry_get(o);
    efl_gfx_map_reset(o);
    efl_gfx_map_smooth_set(o, ad->smooth);
    efl_gfx_map_alpha_set(o, ad->alpha);
-   efl_gfx_map_coord_absolute_set(o, 0, r.x, r.y + r.h, 0);
-   efl_gfx_map_coord_absolute_set(o, 1, r.x + r.w, r.y + r.h, 0);
+   efl_gfx_map_coord_absolute_set(o, 0, rect.x, rect.y + rect.h, 0);
+   efl_gfx_map_coord_absolute_set(o, 1, rect.x + rect.w, rect.y + rect.h, 0);
    efl_gfx_map_coord_absolute_set(o, 2, win_w - 10, win_h - 30, 0);
    efl_gfx_map_coord_absolute_set(o, 3, (win_w / 2) + 10, win_h - 30, 0);
    efl_gfx_map_uv_set(o, 0, 0, 1);