e2a4a123f0d86ce27ebc8498684ae4fc9e089fe5
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2017-10-07 19:26:10 +0900

    Revert "edje: moved from edje_multisense.c to Ecore_Audio.h for select the type of render."
    This reverts commit c1c424a4075a85375c83f4c137b74c9919c5fc57.
    
    revert edje multisense changes to match ecore audio revert

--- src/lib/edje/edje_multisense.c
+++ src/lib/edje/edje_multisense.c
@@ -202,12 +202,16 @@ _edje_multisense_internal_sound_sample_play(Edje *ed, const char *sample_name, c
              in = efl_add(ECORE_AUDIO_IN_SNDFILE_CLASS, NULL, ecore_audio_obj_name_set(efl_added, snd_id_str), ecore_audio_obj_in_speed_set(efl_added, speed), ecore_audio_obj_vio_set(efl_added, &eet_data->vio, eet_data, _free), efl_event_callback_add(efl_added, ECORE_AUDIO_IN_EVENT_IN_STOPPED, _play_finished, NULL));
              if (!out)
                {
-                  out = efl_add(ECORE_AUDIO_OUT_RENDER_CLASS, NULL, efl_event_callback_add(efl_added, ECORE_AUDIO_OUT_RENDER_EVENT_CONTEXT_FAIL, _out_fail, NULL));
+#if HAVE_PULSE
+                  out = efl_add(ECORE_AUDIO_OUT_PULSE_CLASS, NULL, efl_event_callback_add(efl_added, ECORE_AUDIO_OUT_PULSE_EVENT_CONTEXT_FAIL, _out_fail, NULL));
+#endif
                   if (out) outs++;
                }
              if (!out)
                {
-                  ERR("Could not create multisense audio out");
+#if HAVE_PULSE
+                  ERR("Could not create multisense audio out (pulse)");
+#endif
                   efl_del(in);
                   return EINA_FALSE;
                }
@@ -266,7 +270,9 @@ _edje_multisense_internal_sound_tone_play(Edje *ed, const char *tone_name, const
 
              if (!out)
                {
-                  out = efl_add(ECORE_AUDIO_OUT_RENDER_CLASS, NULL, efl_event_callback_add(efl_added, ECORE_AUDIO_OUT_RENDER_EVENT_CONTEXT_FAIL, _out_fail, NULL));
+#if HAVE_PULSE
+                  out = efl_add(ECORE_AUDIO_OUT_PULSE_CLASS, NULL, efl_event_callback_add(efl_added, ECORE_AUDIO_OUT_PULSE_EVENT_CONTEXT_FAIL, _out_fail, NULL));
+#endif
                   if (out) outs++;
                }
 
