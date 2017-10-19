33d860f60d9246fc91de702844490dee3d9aa41f
Cedric BAIL <cedric@osg.samsung.com>
2017-10-19 16:14:48 -0700

    evas: propagate object data to evas_object_inform_call_changed_size_hints instead of fetching it again.

--- src/lib/evas/canvas/evas_object_inform.c
+++ src/lib/evas/canvas/evas_object_inform.c
@@ -50,9 +50,8 @@ evas_object_inform_call_restack(Evas_Object *eo_obj, Evas_Object_Protected_Data 
 }
 
 void
-evas_object_inform_call_changed_size_hints(Evas_Object *eo_obj)
+evas_object_inform_call_changed_size_hints(Evas_Object *eo_obj, Evas_Object_Protected_Data *obj)
 {
-   Evas_Object_Protected_Data *obj = efl_data_scope_get(eo_obj, EFL_CANVAS_OBJECT_CLASS);
    int event_id = _evas_object_event_new();
 
    evas_object_event_callback_call(eo_obj, obj, EVAS_CALLBACK_CHANGED_SIZE_HINTS, NULL, event_id, EFL_GFX_EVENT_CHANGE_SIZE_HINTS);
