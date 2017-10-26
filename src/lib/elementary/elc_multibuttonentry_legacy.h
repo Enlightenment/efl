de74fd11ba1b8afce81c89777d381016cee454ea
Amitesh Singh <amitesh.sh@samsung.com>
2017-10-26 15:36:07 +0900

    Efl.Ui.Mbe: implement format_cb.set()
    Ref T6204

--- src/lib/elementary/elc_multibuttonentry_legacy.h
+++ src/lib/elementary/elc_multibuttonentry_legacy.h
@@ -9,5 +9,22 @@
  */
 EAPI Evas_Object               *elm_multibuttonentry_add(Evas_Object *parent);
 
+/**
+ * @brief Set a function to format the string that will be used to display the
+ * hidden items counter.
+ *
+ * If @c format_function is @c NULL, the default format will be used, which is
+ * $"... + %d".
+ *
+ * @param[in] obj The object.
+ * @param[in] format_function Format_function The actual format function
+ * @param[in] data Data User data to passed to @c format_function
+ *
+ * @since 1.9
+ *
+ * @ingroup Multibuttonentry
+ */
+ EAPI void elm_multibuttonentry_format_function_set(Eo *obj, Efl_Ui_Multibuttonentry_Format_Cb format_function, const void *data);
+
 #include "elm_multibuttonentry_item.eo.legacy.h"
 #include "efl_ui_multibuttonentry.eo.legacy.h"
