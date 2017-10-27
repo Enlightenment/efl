6d7de2610d6f90a671e41c489a87e214a08e7c18
subhransu mohanty <sub.mohanty@samsung.com>
2017-10-27 09:24:25 +0900

    edje: cleanup old implementation of handling svg file.

--- src/lib/edje/edje_private.h
+++ src/lib/edje/edje_private.h
@@ -385,8 +385,6 @@ typedef struct _Edje_Part_Limit                      Edje_Part_Limit;
 typedef struct _Edje_Part_Description_Vector         Edje_Part_Description_Vector;
 typedef struct _Edje_Part_Description_Spec_Svg       Edje_Part_Description_Spec_Svg;
 typedef struct _Edje_Part_Allowed_Seat               Edje_Part_Allowed_Seat;
-typedef struct _Edje_Real_Part_Vector                Edje_Real_Part_Vector;
-typedef struct _Edje_Vector_Data                     Edje_Vector_Data;
 
 typedef struct _Edje Edje;
 typedef struct _Edje_Real_Part_Text Edje_Real_Part_Text;
@@ -1694,7 +1692,6 @@ struct _Edje
    Eina_List            *subobjs;
    Eina_List            *text_insert_filter_callbacks;
    Eina_List            *markup_filter_callbacks;
-   Eina_List            *vector_cache; /* list of Edje_Vector_Data */
 
    Eina_List            *groups;
 
@@ -1996,19 +1993,6 @@ struct _Edje_Real_Part_Swallow
    } swallow_params; // 28 // FIXME: only if type SWALLOW
 };
 
-struct _Edje_Vector_Data
-{
-   int svg_id;
-   double x, y, w, h;
-   Eina_Bool preserve_aspect;
-   Efl_VG *vg;
-};
-
-struct _Edje_Real_Part_Vector
-{
-   Edje_Vector_Data cur;
-};
-
 struct _Edje_Real_Part
 {
    Edje_Real_Part_State      param1; // 32
@@ -2031,7 +2015,6 @@ struct _Edje_Real_Part
       Edje_Real_Part_Text      *text;
       Edje_Real_Part_Container *container;
       Edje_Real_Part_Swallow   *swallow;
-      Edje_Real_Part_Vector    *vector;
    } typedata; // 4
    FLOAT_T                   description_pos; // 8
    Edje_Rectangle            req; // 16
@@ -2248,13 +2231,13 @@ struct _Edje_Patterns
 
    int             ref;
    Eina_Bool       delete_me : 1;
-   
+
    size_t          patterns_size;
    size_t          max_length;
    unsigned int    finals[];
 };
 
-typedef enum _Edje_User_Defined_Type 
+typedef enum _Edje_User_Defined_Type
 {
    EDJE_USER_SWALLOW,
    EDJE_USER_BOX_PACK,
@@ -3184,278 +3167,6 @@ char * _edje_text_cursor_content_get(Edje_Real_Part *rp, Efl_Text_Cursor_Cursor 
 void _edje_object_part_text_insert(Edje *ed, Edje_Real_Part *rp, const char *text);
 
 void _edje_internal_proxy_shutdown(void);
-
-// Svg loader
-typedef enum _Svg_Node_Type Svg_Node_Type;
-typedef enum _Svg_Length_Type Svg_Length_Type;
-
-typedef struct _Svg_Node Svg_Node;
-typedef struct _Svg_Doc_Node Svg_Doc_Node;
-typedef struct _Svg_G_Node Svg_G_Node;
-typedef struct _Svg_Defs_Node Svg_Defs_Node;
-typedef struct _Svg_Arc_Node Svg_Arc_Node;
-typedef struct _Svg_Circle_Node Svg_Circle_Node;
-typedef struct _Svg_Ellipse_Node Svg_Ellipse_Node;
-typedef struct _Svg_Polygon_Node Svg_Polygon_Node;
-typedef struct _Svg_Rect_Node Svg_Rect_Node;
-typedef struct _Svg_Path_Node Svg_Path_Node;
-typedef struct _Svg_Style_Property Svg_Style_Property;
-
-typedef struct  _Svg_Style_Stroke   Svg_Style_Stroke;
-typedef struct  _Svg_Style_Fill     Svg_Style_Fill;
-typedef enum    _Svg_Fill_Flags     Svg_Fill_Flags;
-typedef enum    _Svg_Stroke_Flags   Svg_Stroke_Flags;
-
-
-typedef enum   _Svg_Gradient_Type   Svg_Gradient_Type;
-typedef struct _Svg_Style_Gradient  Svg_Style_Gradient;
-typedef struct _Svg_Linear_Gradient Svg_Linear_Gradient;
-typedef struct _Svg_Radial_Gradient Svg_Radial_Gradient;
-typedef struct _Svg_Paint           Svg_Paint;
-
-enum _Svg_Node_Type
-{
-   SVG_NODE_DOC,
-   SVG_NODE_G,
-   SVG_NODE_DEFS,
-   SVG_NODE_SWITCH,
-   SVG_NODE_ANIMATION,
-   SVG_NODE_ARC,
-   SVG_NODE_CIRCLE,
-   SVG_NODE_ELLIPSE,
-   SVG_NODE_IMAGE,
-   SVG_NODE_LINE,
-   SVG_NODE_PATH,
-   SVG_NODE_POLYGON,
-   SVG_NODE_POLYLINE,
-   SVG_NODE_RECT,
-   SVG_NODE_TEXT,
-   SVG_NODE_TEXTAREA,
-   SVG_NODE_TSPAN,
-   SVG_NODE_USE,
-   SVG_NODE_VIDEO,
-   SVG_NODE_UNKNOWN
-};
-
-enum _Svg_Length_Type
-{
-   SVG_LT_PERCENT,
-   SVG_LT_PX,
-   SVG_LT_PC,
-   SVG_LT_PT,
-   SVG_LT_MM,
-   SVG_LT_CM,
-   SVG_LT_IN,
-};
-
-struct _Svg_Doc_Node
-{
-   double width;
-   double height;
-   double vx;
-   double vy;
-   double vw;
-   double vh;
-   Svg_Node *defs;
-};
-
-struct _Svg_G_Node
-{
-
-};
-
-struct _Svg_Defs_Node
-{
-   Eina_List   *gradients;
-};
-
-struct _Svg_Arc_Node
-{
-
-};
-
-struct _Svg_Ellipse_Node
-{
-   double cx;
-   double cy;
-   double rx;
-   double ry;
-};
-
-struct _Svg_Circle_Node
-{
-   double cx;
-   double cy;
-   double r;
-};
-
-struct _Svg_Rect_Node
-{
-   double x;
-   double y;
-   double w;
-   double h;
-   double rx;
-   double ry;
-};
-
-struct _Svg_Path_Node
-{
-   Eina_Stringshare *path;
-};
-
-struct _Svg_Polygon_Node
-{
-   int points_count;
-   double *points;
-};
-
-
-enum _Svg_Gradient_Type
-{
-   SVG_LINEAR_GRADIENT,
-   SVG_RADIAL_GRADIENT
-};
-struct _Svg_Linear_Gradient
-{
-   double x1;
-   double y1;
-   double x2;
-   double y2;
-};
-
-struct _Svg_Radial_Gradient
-{
-   double cx;
-   double cy;
-   double fx;
-   double fy;
-   double r;
-};
-
-struct _Svg_Style_Gradient
-{
-   Svg_Gradient_Type type;
-   Eina_Stringshare  *id;
-   Eina_Stringshare  *ref;
-   Efl_Gfx_Gradient_Spread spread;
-   Eina_List   *stops; // Efl_Gfx_Gradient_Stop
-   Svg_Radial_Gradient *radial;
-   Svg_Linear_Gradient *linear;
-};
-
-struct _Svg_Paint
-{
-   int        r;
-   int        g;
-   int        b;
-   Eina_Bool  none;
-   Eina_Bool  cur_color;
-   Svg_Style_Gradient  *gradient;
-   Eina_Stringshare    *url;
-};
-
-enum _Svg_Fill_Flags
-{
-   SVG_FILL_FLAGS_PAINT     = 0x1,
-   SVG_FILL_FLAGS_OPACITY   = 0x2,
-   SVG_FILL_FLAGS_GRADIENT  = 0x4,
-   SVG_FILL_FLAGS_FILL_RULE = 0x8
-};
-
-enum _Svg_Stroke_Flags
-{
-   SVG_STROKE_FLAGS_PAINT    = 0x1,
-   SVG_STROKE_FLAGS_OPACITY  = 0x2,
-   SVG_STROKE_FLAGS_GRADIENT = 0x4,
-   SVG_STROKE_FLAGS_SCALE    = 0x8,
-   SVG_STROKE_FLAGS_WIDTH    = 0x10,
-   SVG_STROKE_FLAGS_CAP      = 0x20,
-   SVG_STROKE_FLAGS_JOIN     = 0x40,
-   SVG_STROKE_FLAGS_DASH     = 0x80,
-};
-
-struct _Svg_Style_Fill
-{
-   Svg_Fill_Flags       flags;
-   Svg_Paint            paint;
-   int                  opacity;
-   Efl_Gfx_Fill_Rule    fill_rule;
-};
-
-struct _Svg_Style_Stroke
-{
-   Svg_Stroke_Flags     flags;
-   Svg_Paint            paint;
-   int                  opacity;
-   double               scale;
-   double               width;
-   double               centered;
-   Efl_Gfx_Cap          cap;
-   Efl_Gfx_Join         join;
-   Efl_Gfx_Dash        *dash;
-   int                  dash_count;
-};
-
-struct _Svg_Style_Property
-{
-   Svg_Style_Fill     fill;
-   Svg_Style_Stroke   stroke;
-   // the color property indirectly 
-   // used by fill and stroke
-   int                r;
-   int                g;
-   int                b;
-};
-
-struct _Svg_Node
-{
-   Svg_Node_Type        type;
-   Svg_Node            *parent;
-   Eina_List           *child;
-   Eina_Stringshare    *id;
-   Svg_Style_Property  *style;
-   Eina_Matrix3        *transform;
-   union
-     {
-        Svg_G_Node   g;
-        Svg_Doc_Node doc;
-        Svg_Defs_Node defs;
-        Svg_Arc_Node arc;
-        Svg_Circle_Node circle;
-        Svg_Ellipse_Node ellipse;
-        Svg_Polygon_Node polygon;
-        Svg_Polygon_Node polyline;
-        Svg_Rect_Node rect;
-        Svg_Path_Node path;
-     }node;
-};
-
-enum _Svg_Style_Type
-{
-   SVG_STYLE_QUALITY,
-   SVG_STYLE_FILL,
-   SVG_STYLE_VIEWPORT_FILL,
-   SVG_STYLE_FONT,
-   SVG_STYLE_STROKE,
-   SVG_STYLE_SOLID_COLOR,
-   SVG_STYLE_GRADIENT,
-   SVG_STYLE_TRANSFORM,
-   SVG_STYLE_OPACITY,
-   SVG_STYLE_COMP_OP
-};
-
-EAPI Eet_Data_Descriptor * _edje_svg_node_eet(void);
-void _edje_svg_node_destroy_eet(void);
-
-void _edje_dupe_vector_data(Edje *ed, int svg_id, double width, double height,
-                            Edje_Vector_Data *data);
-void _apply_transformation(Efl_VG *root, double w, double h,
-                           Edje_Vector_Data *vg_data);
-
-Edje_Vector_Data * _edje_ref_vector_data(Edje *ed, int svg_id);
-
-EAPI void _edje_svg_node_free(Svg_Node *node);
 
 #ifdef HAVE_EPHYSICS
 Eina_Bool _edje_ephysics_load(void);
