302cc259ff27c7a912f8335b1910edfe39ae30ec
Amitesh Singh <amitesh.sh@samsung.com>
2017-11-14 10:51:03 +0900

    Efl.Ui.Format: Add support for "struct tm" in string format.

--- src/lib/efl/interfaces/efl_ui_format.c
+++ src/lib/efl/interfaces/efl_ui_format.c
@@ -24,7 +24,12 @@ _default_format_cb(void *data, Eina_Strbuf *str, const Eina_Value value)
         eina_value_get(&value, &v);
         eina_strbuf_append_printf(str, sd->template, v);
      }
-   //XXX: handle other types e.g. struct tm
+   else if (type == EINA_VALUE_TYPE_TM)
+     {
+        struct tm v;
+        eina_value_get(&value, &v);
+        eina_strbuf_append_strftime(str, sd->template, &v);
+     }
    else
      {
         char *v = eina_value_to_string(&value);
