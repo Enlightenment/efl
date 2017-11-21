b291d25dfcfcf55e09780c5ca46d9dc286b38bcb
Pawel Aksiutowicz <p.aksiutowic@partner.samsung.com>
2017-11-21 11:12:45 +0900

    elementary: removed warning from efl_ui_win
    Reviewers: stanluk, lukasz.stanislawski
    
    Subscribers: cedric, jpeg
    
    Differential Revision: https://phab.enlightenment.org/D5503

--- src/lib/elementary/efl_ui_win.c
+++ src/lib/elementary/efl_ui_win.c
@@ -6066,7 +6066,7 @@ _efl_ui_win_efl_gfx_size_hint_hint_aspect_set(Eo *obj EINA_UNUSED, Efl_Ui_Win_Da
 }
 
 EOLIAN static void
-_efl_ui_win_efl_gfx_size_hint_hint_weight_set(Eo *obj EINA_UNUSED, Efl_Ui_Win_Data *pd,
+_efl_ui_win_efl_gfx_size_hint_hint_weight_set(Eo *obj EINA_UNUSED, Efl_Ui_Win_Data *pd EINA_UNUSED,
                                               double w, double h)
 {
    efl_gfx_size_hint_weight_set(efl_super(obj, MY_CLASS), w, h);
