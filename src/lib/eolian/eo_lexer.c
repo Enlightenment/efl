43a6fddf1f665cb5e936e8b9db51a578b9239d56
Jean-Philippe Andre <jp.andre@samsung.com>
2017-11-22 15:44:06 +0900

    eolian: Add builtin type "strbuf"
    Refers to Eina_Strbuf*

--- src/lib/eolian/eo_lexer.c
+++ src/lib/eolian/eo_lexer.c
@@ -76,7 +76,7 @@ static const char * const ctypes[] =
    "Eina_List *", "Eina_Inarray *", "Eina_Inlist *",
    "Efl_Future *",
    "Eina_Value", "Eina_Value *",
-   "char *", "const char *", "Eina_Stringshare *",
+   "char *", "const char *", "Eina_Stringshare *", "Eina_Strbuf *",
 
    "void *",
 
