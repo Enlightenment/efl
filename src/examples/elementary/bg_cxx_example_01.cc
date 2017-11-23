e23ea711ab1d9a37141cd2a317cf14984f5097fa
Jean-Philippe Andre <jp.andre@samsung.com>
2017-11-23 14:10:24 +0900

    cxx: Fix bg examples
    One uses the Bg widget and the other one uses the part.

--- src/examples/elementary/bg_cxx_example_01.cc
+++ src/examples/elementary/bg_cxx_example_01.cc
@@ -1,17 +1,25 @@
-#define EFL_EO_API_SUPPORT
-
 #include <Elementary.hh>
+
+using efl::eo::instantiate;
 
 EAPI_MAIN int
 elm_main (int argc EINA_UNUSED, char **argv EINA_UNUSED)
 {
-   efl::ui::Win win;
-   win.text_set("Bg Plain");
+   efl::ui::Win win(instantiate);
+   win.text_set("Window Background");
    win.autohide_set(true);
+   win.size_set({320,320});
+   try {
+      auto bg = efl::eo::downcast<efl::ui::win::Part>(win.part("background"));
+      bg.color_set(139, 69, 19, 255);
+   } catch (std::exception const&e) {
+      std::cerr << "Failed to set bg color: " << e.what() << std::endl;
+   }
 
-   win.eo_cxx::efl::Gfx::size_set({320,320});
-   //win.size_set(320,320);
-   win.visible_set(true);
+   // Clean exit
+   elm_policy_set(ELM_POLICY_QUIT, ELM_POLICY_QUIT_LAST_WINDOW_CLOSED);
+   efl::eolian::event_add(efl::ui::Win::delete_request_event, win,
+                          std::bind([&](){ win = nullptr; }));
 
    elm_run();
    return 0;
