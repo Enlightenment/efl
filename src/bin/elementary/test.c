3a79575cdf4d4881c996bfb3c0d7a03155702e89
Dave Andreoli <dave@gurumeditation.it>
2017-12-10 20:35:42 +0100

    Some more focus movement tests
    Those two layouts are quite simple but in real it's difficult to solve both in the best way

--- src/bin/elementary/test.c
+++ src/bin/elementary/test.c
@@ -245,6 +245,7 @@ void test_focus3(void *data, Evas_Object *obj, void *event_info);
 void test_focus_object_style(void *data, Evas_Object *obj, void *event_info);
 void test_focus_object_policy(void *data, Evas_Object *obj, void *event_info);
 void test_focus4(void *data, Evas_Object *obj, void *event_info);
+void test_focus5(void *data, Evas_Object *obj, void *event_info);
 void test_flipselector(void *data, Evas_Object *obj, void *event_info);
 void test_diskselector(void *data, Evas_Object *obj, void *event_info);
 void test_colorselector(void *data, Evas_Object *obj, void *event_info);
@@ -1089,6 +1090,7 @@ add_tests:
    ADD_TEST(NULL, "Focus", "Focus Object Style", test_focus_object_style);
    ADD_TEST(NULL, "Focus", "Focus Object Policy", test_focus_object_policy);
    ADD_TEST(NULL, "Focus", "Focus 4", test_focus4);
+   ADD_TEST(NULL, "Focus", "Focus 5", test_focus5);
 
    //------------------------------//
    ADD_TEST(NULL, "Naviframe", "Naviframe", test_naviframe);
