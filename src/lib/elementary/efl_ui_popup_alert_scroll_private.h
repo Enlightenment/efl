f394db44fc854ff6898dafa72b8dabbc02eff564
Jaehyun Cho <jae_hyun.cho@samsung.com>
2017-12-11 13:55:02 +0900

    efl_ui_popup_alert_scroll: Add expandable property
    Summary:
    The expandable property allows popup to expand its size up to the given
    maximum size.
    So by setting expandable property, popup expands its scrollable content
    size from the minimum size of the content to the given maximum size.
    
    Reviewers: jpeg
    
    Subscribers: cedric, thiepha, woohyun, Blackmole, herb
    
    Differential Revision: https://phab.enlightenment.org/D5630

--- src/lib/elementary/efl_ui_popup_alert_scroll_private.h
+++ src/lib/elementary/efl_ui_popup_alert_scroll_private.h
@@ -1,13 +1,18 @@
 #ifndef EFL_UI_POPUP_ALERT_SCROLL_H
 #define EFL_UI_POPUP_ALERT_SCROLL_H
 
+#define EFL_UI_POPUP_ALERT_SCROLL_BETA
+
 #include "Elementary.h"
 
 typedef struct _Efl_Ui_Popup_Alert_Scroll_Data Efl_Ui_Popup_Alert_Scroll_Data;
 struct _Efl_Ui_Popup_Alert_Scroll_Data
 {
-   Eo *scroller;
-   Eo *content;
+   Eo         *scroller;
+   Eo         *content;
+   Eina_Size2D size;
+   Eina_Size2D max_size;
+   Eina_Bool   needs_size_calc : 1;
 };
 
 #endif
