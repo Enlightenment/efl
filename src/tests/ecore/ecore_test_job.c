2ee19c1d0f2ac6929534b830f89dfba995710d40
Cedric BAIL <cedric@osg.samsung.com>
2017-12-11 11:28:23 -0800

    ecore: remove test of efl_loop_job.

--- src/tests/ecore/ecore_test_job.c
+++ src/tests/ecore/ecore_test_job.c
@@ -6,35 +6,6 @@
 #include "ecore_suite.h"
 
 static void
-_ecore_promise_quit(void *data, const Efl_Event *ev)
-{
-   Efl_Future_Event_Success *success = ev->info;
-   Eina_Bool *bob = data;
-   void *value = success->value;
-
-   fail_if(data != value);
-   *bob = EINA_TRUE;
-   ecore_main_loop_quit();
-}
-
-START_TEST(ecore_test_job_promise)
-{
-   Eina_Bool bob = EINA_FALSE;
-
-   ecore_init();
-
-   Efl_Object *promise = efl_loop_job(ecore_main_loop_get(), &bob);
-   efl_future_then(promise, &_ecore_promise_quit, NULL, NULL, &bob);
-
-   ecore_main_loop_begin();
-
-   fail_if(bob != EINA_TRUE);
-
-   ecore_shutdown();
-}
-END_TEST
-
-static void
 _ecore_quit(void *data)
 {
    Eina_Bool *bob = data;
@@ -64,5 +35,4 @@ END_TEST
 void ecore_test_ecore_job(TCase *tc)
 {
    tcase_add_test(tc, ecore_test_job);
-   tcase_add_test(tc, ecore_test_job_promise);
 }
