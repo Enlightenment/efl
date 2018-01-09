Enlightenment/efl

commit 9d583b3fdbed686642dbc0d96d1e417485ca0077

ecore_drm2: Fix dpms shutdown while disabling outputs
We need to set output->enabled to disabled *after* dpms takes place or set
it to enabled *before* dpms takes place.  We can't just set it at the
start of the function or one of enable/disable will hit the dpms path
with a disabled display.

src/lib/ecore_drm2/ecore_drm2_outputs.c
@@ -1237,16 +1237,19 @@ ecore_drm2_output_enabled_set(Ecore_Drm2_Output *output, Eina_Bool enabled)
 
    if (!output->connected) return;
    if (output->enabled == enabled) return;
-   output->enabled = enabled;
 
    if (output->enabled)
-     ecore_drm2_output_dpms_set(output, DRM_MODE_DPMS_ON);
+     {
+        output->enabled = enabled;
+        ecore_drm2_output_dpms_set(output, DRM_MODE_DPMS_ON);
+     }
    else
      {
         if (_ecore_drm2_use_atomic)
           ecore_drm2_fb_flip(NULL, output);
 
         ecore_drm2_output_dpms_set(output, DRM_MODE_DPMS_OFF);
+        output->enabled = enabled;
 
         if (output->current.fb)
           _ecore_drm2_fb_buffer_release(output, &output->current);

commit 4aeca75cff45748c9ff67e007f408f15ea73709f

ecore-drm2: Fix enabling outputs
Commit 9d583b3fdbed686642dbc0d96d1e417485ca0077 broke
ecore_drm2_output_enabled_set function due changing order of setting
output->enabled value. This patch fixes both issues by checking the
'enabled' variable being passed in.

@fix

Signed-off-by: Chris Michael <cp.michael@samsung.com>

src/lib/ecore_drm2/ecore_drm2_outputs.c
@@ -1175,7 +1175,7 @@ ecore_drm2_output_enabled_set(Ecore_Drm2_Output *output, Eina_Bool enabled)
    if (!output->connected) return;
    if (output->enabled == enabled) return;
 
-   if (output->enabled)
+   if (enabled)
      {
         output->enabled = enabled;
         ecore_drm2_output_dpms_set(output, DRM_MODE_DPMS_ON);