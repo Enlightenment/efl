bd9adec65577de5ce30bc1e4c46e272c371b5f41
Mike Blumenkrantz <zmike@osg.samsung.com>
2018-01-22 13:02:44 -0500

    ecore-wl2: correctly translate spacebar keyname into key events
    fix T6620
    
    @fix

--- src/lib/ecore_wl2/ecore_wl2_input.c
+++ src/lib/ecore_wl2/ecore_wl2_input.c
@@ -598,7 +598,7 @@ _ecore_wl2_input_symbol_rep_find(xkb_keysym_t keysym, char *buffer, int size, un
 
     /* check if we are a control code */
     if (n > 0 && !(
-        (buffer[0] > 0x0 && buffer[0] < 0x20) || /* others 0x0 to 0x1F control codes */
+        (buffer[0] > 0x0 && buffer[0] <= 0x20) || /* others 0x0 to 0x20 control codes */
         buffer[0] == 0x7F)) /*delete control code */
       return;
 
