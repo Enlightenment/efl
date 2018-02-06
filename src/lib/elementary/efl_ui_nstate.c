b1ee35e35ce3132566244abbbd3a804167ef9c00
Shinwoo Kim <cinoo.kim@samsung.com>
2018-02-06 22:38:14 +0900

    elm: do not call "changed" callback when setting style

--- src/lib/elementary/efl_ui_nstate.c
+++ src/lib/elementary/efl_ui_nstate.c
@@ -19,7 +19,6 @@ typedef struct
 } Efl_Ui_Nstate_Data;
 
 static Eina_Bool _key_action_activate(Evas_Object *obj, const char *params);
-static void _state_active(Evas_Object *obj, Efl_Ui_Nstate_Data *sd);
 
 static const Elm_Action key_actions[] = {
    {"activate", _key_action_activate},
@@ -116,14 +115,12 @@ _efl_ui_nstate_value_set(Eo *obj, Efl_Ui_Nstate_Data *pd, int state)
 }
 
 EOLIAN static Efl_Ui_Theme_Apply
-_efl_ui_nstate_efl_ui_widget_theme_apply(Eo *obj, Efl_Ui_Nstate_Data *pd)
+_efl_ui_nstate_efl_ui_widget_theme_apply(Eo *obj, Efl_Ui_Nstate_Data *pd EINA_UNUSED)
 {
    Efl_Ui_Theme_Apply int_ret = EFL_UI_THEME_APPLY_FAILED;
 
    int_ret = efl_ui_widget_theme_apply(efl_super(obj, MY_CLASS));
    if (!int_ret) return EFL_UI_THEME_APPLY_FAILED;
-
-   _state_active(obj, pd);
 
    return int_ret;
 }
