a221bec6d0a44b01b9e771b3c59f6e28ea2fb86f
Derek Foreman <derekf@osg.samsung.com>
2018-02-09 09:49:52 -0600

    ecore_wl2: Destroy offscreen event handler in surface destroy
    Or we can get the event after destroying the surface and crash.

--- src/lib/ecore_wl2/ecore_wl2_surface.c
+++ src/lib/ecore_wl2/ecore_wl2_surface.c
@@ -15,6 +15,7 @@ ecore_wl2_surface_destroy(Ecore_Wl2_Surface *surface)
 {
    EINA_SAFETY_ON_NULL_RETURN(surface);
 
+   ecore_event_handler_del(surface->offscreen_handler);
    surface->funcs->destroy(surface, surface->private_data);
    surface->wl2_win = NULL;
 
