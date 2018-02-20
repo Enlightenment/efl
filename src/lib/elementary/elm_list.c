8555a22972b5936635529075f781ad438287a0ac
Mike Blumenkrantz <zmike@osg.samsung.com>
2018-02-20 10:55:50 -0500

    elm_list: revert some accidental changes
    ref f8e93775c066b193090beeb03c23fbaeaec2486b
    
    fix T6712

--- src/lib/elementary/elm_list.c
+++ src/lib/elementary/elm_list.c
@@ -656,7 +656,9 @@ _elm_list_deletions_process(Elm_List_Data *sd)
      {
         sd->items = eina_list_remove_list(sd->items, it->node);
 
-        efl_del(EO_OBJ(it));
+        /* issuing free because of "locking" item del pre hook */
+        _elm_list_item_free(it);
+        efl_parent_set(EO_OBJ(it), NULL);
      }
 
    sd->walking--;
@@ -2436,6 +2438,9 @@ _elm_list_efl_canvas_group_group_del(Eo *obj, Elm_List_Data *sd)
 
    EINA_LIST_FREE(sd->items, eo_it)
      {
+        ELM_LIST_ITEM_DATA_GET(eo_it, it);
+        /* issuing free because of "locking" item del pre hook */
+        _elm_list_item_free(it);
         efl_del(eo_it);
      }
 
@@ -2697,6 +2702,9 @@ _elm_list_clear(Eo *obj, Elm_List_Data *sd)
 
    EINA_LIST_FREE(sd->items, eo_it)
      {
+        ELM_LIST_ITEM_DATA_GET(eo_it, it);
+        /* issuing free because of "locking" item del pre hook */
+        _elm_list_item_free(it);
         efl_del(eo_it);
      }
 
