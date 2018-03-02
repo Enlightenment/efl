6f31f36aa81cd15c34b9e55f43231e521acd35e0
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2018-03-02 18:16:05 +0900

    put efl app test back with mods to match app as superclass

--- src/tests/ecore/ecore_test_ecore.c
+++ src/tests/ecore/ecore_test_ecore.c
@@ -857,67 +857,6 @@ START_TEST(ecore_test_ecore_main_loop_poller_add_del)
 }
 END_TEST
 
-START_TEST(ecore_test_efl_loop_register)
-{
-   Efl_Object *t, *n;
-
-   ecore_init();
-
-   t = efl_provider_find(efl_main_loop_get(), EFL_LOOP_CLASS);
-   fail_if(!efl_isa(t, EFL_LOOP_CLASS));
-
-   t = efl_provider_find(efl_main_loop_get(), EFL_LOOP_TIMER_CLASS);
-   fail_if(t != NULL);
-
-   n = efl_add(EFL_LOOP_TIMER_CLASS, efl_main_loop_get());
-   fail_if(n != NULL);
-
-   n = efl_add(EFL_LOOP_TIMER_CLASS, efl_main_loop_get(),
-               efl_loop_timer_interval_set(efl_added, 1.0));
-   efl_loop_register(efl_main_loop_get(), EFL_LOOP_TIMER_CLASS, n);
-
-   t = efl_provider_find(efl_main_loop_get(), EFL_LOOP_TIMER_CLASS);
-   fail_if(!efl_isa(t, EFL_LOOP_TIMER_CLASS));
-   fail_if(t != n);
-
-   efl_loop_unregister(efl_main_loop_get(), EFL_LOOP_TIMER_CLASS, n);
-
-   t = efl_provider_find(efl_main_loop_get(), EFL_LOOP_TIMER_CLASS);
-   fail_if(t != NULL);
-
-   ecore_shutdown();
-}
-END_TEST
-
-START_TEST(ecore_test_efl_app_version)
-{
-   const Efl_Version *ver;
-   Eo *loop;
-
-   ecore_init();
-
-   loop = efl_app_loop_main_get(EFL_APP_CLASS);
-   fail_if(!efl_isa(loop, EFL_LOOP_CLASS));
-
-   efl_build_version_set(EFL_VERSION_MAJOR, EFL_VERSION_MINOR, 0, 0, NULL, EFL_BUILD_ID);
-   ver = efl_app_build_efl_version_get(loop);
-   fail_if(!ver);
-   fail_if(ver->major != EFL_VERSION_MAJOR);
-   fail_if(ver->minor != EFL_VERSION_MINOR);
-   fail_if(ver->micro != 0);
-   fail_if(ver->revision != 0);
-   fail_if(ver->flavor);
-   fail_if(!eina_streq(ver->build_id, EFL_BUILD_ID));
-
-   ver = efl_app_efl_version_get(loop);
-   fail_if(!ver);
-   fail_if(ver->major != EFL_VERSION_MAJOR);
-   fail_if(ver->minor != EFL_VERSION_MINOR);
-
-   ecore_shutdown();
-}
-END_TEST
-
 void ecore_test_ecore(TCase *tc)
 {
    tcase_add_test(tc, ecore_test_ecore_init);
@@ -940,6 +879,4 @@ void ecore_test_ecore(TCase *tc)
    tcase_add_test(tc, ecore_test_ecore_main_loop_poller_add_del);
    tcase_add_test(tc, ecore_test_efl_loop_fd);
    tcase_add_test(tc, ecore_test_efl_loop_fd_lifecycle);
-   tcase_add_test(tc, ecore_test_efl_loop_register);
-   tcase_add_test(tc, ecore_test_efl_app_version);
 }
---
+++ src/tests/ecore/efl_app_suite.c
@@ -0,0 +1,103 @@
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include <stdio.h>
+#include <unistd.h>
+#define EFL_NOLEGACY_API_SUPPORT
+#include <Efl_Core.h>
+#include <Efl_Net.h>
+#include "efl_app_suite.h"
+#include "../efl_check.h"
+
+START_TEST(efl_app_test_efl_loop_register)
+{
+   Efl_Object *t, *n;
+
+   ecore_init();
+
+   t = efl_provider_find(efl_app_get(), EFL_LOOP_CLASS);
+   fail_if(!efl_isa(t, EFL_LOOP_CLASS));
+   fail_if(!efl_isa(t, EFL_APP_CLASS));
+
+   t = efl_provider_find(efl_app_get(), EFL_LOOP_TIMER_CLASS);
+   fail_if(t != NULL);
+
+   n = efl_add(EFL_LOOP_TIMER_CLASS, efl_app_get());
+   fail_if(n != NULL);
+
+   n = efl_add(EFL_LOOP_TIMER_CLASS, efl_app_get(),
+               efl_loop_timer_interval_set(efl_added, 1.0));
+   efl_loop_register(efl_app_get(), EFL_LOOP_TIMER_CLASS, n);
+
+   t = efl_provider_find(efl_app_get(), EFL_LOOP_TIMER_CLASS);
+   fail_if(!efl_isa(t, EFL_LOOP_TIMER_CLASS));
+   fail_if(t != n);
+
+   efl_loop_unregister(efl_app_get(), EFL_LOOP_TIMER_CLASS, n);
+
+   t = efl_provider_find(efl_app_get(), EFL_LOOP_TIMER_CLASS);
+   fail_if(t != NULL);
+
+   ecore_shutdown();
+}
+END_TEST
+
+START_TEST(efl_app_test_efl_build_version)
+{
+   const Efl_Version *ver;
+   Eo *app;
+
+   ecore_init();
+
+   app = efl_app_get();
+   fail_if(!efl_isa(app, EFL_APP_CLASS));
+
+   efl_build_version_set(EFL_VERSION_MAJOR, EFL_VERSION_MINOR, 0, 0, NULL, EFL_BUILD_ID);
+   ver = efl_app_build_efl_version_get(app);
+   fail_if(!ver);
+   fail_if(ver->major != EFL_VERSION_MAJOR);
+   fail_if(ver->minor != EFL_VERSION_MINOR);
+   fail_if(ver->micro != 0);
+   fail_if(ver->revision != 0);
+   fail_if(ver->flavor);
+   fail_if(!eina_streq(ver->build_id, EFL_BUILD_ID));
+
+   ver = efl_app_efl_version_get(app);
+   fail_if(!ver);
+   fail_if(ver->major != EFL_VERSION_MAJOR);
+   fail_if(ver->minor != EFL_VERSION_MINOR);
+
+   ecore_shutdown();
+}
+END_TEST
+
+void efl_test_efl_app(TCase *tc)
+{
+   tcase_add_test(tc, efl_app_test_efl_loop_register);
+   tcase_add_test(tc, efl_app_test_efl_build_version);
+}
+
+
+static const Efl_Test_Case etc[] = {
+  { "Efl_App", efl_test_efl_app },
+  { NULL, NULL }
+};
+
+int
+main(int argc, char **argv)
+{
+   int failed_count;
+
+   if (!_efl_test_option_disp(argc, argv, etc))
+     return 0;
+
+#ifdef NEED_RUN_IN_TREE
+   putenv("EFL_RUN_IN_TREE=1");
+#endif
+
+   failed_count = _efl_suite_build_and_run(argc - 1, (const char **)argv + 1,
+                                           "Efl_App", etc);
+
+   return (failed_count == 0) ? 0 : 255;
+}
