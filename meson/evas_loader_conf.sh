1aae2fc03e9b9c93642f84a61ee062fac356aafe
Marcel Hollerbach <marcel@osg.samsung.com>
2018-03-28 14:12:26 +0100

    meson: fix scripts for installing
    they used to depend on .so as extension, not anymore now

--- meson/evas_loader_conf.sh
+++ meson/evas_loader_conf.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 original_loader=$1
-original_name="$(basename $original_loader .so)"
+filename="$(basename $original_loader)"
+ext="${ext##*.}"
+original_name="$(basename $original_loader $ext)"
 loader_dir="$(dirname $original_loader)"
 loader="$(basename $original_loader)"
 loader_name="$(echo $original_name | cut -f 1 -d '.')"
 
-if [ ${original_loader: -3} == ".so" ]
+if [ ${original_loader: -3} == "$ext" ]
   then
     mv "$DESTDIR"/"$original_loader" "$DESTDIR"/"$loader_dir"/"$original_name"
 fi
--- meson/meson_modules.sh
+++ meson/meson_modules.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 for x in "$@" ; do
-	dir="$(dirname $x)"
-	mv "$DESTDIR"/"$x" "$DESTDIR"/"$dir"/module.so
+  dir="$(dirname $x)"
+  filename="$(basename $x)"
+  ext="${filename##*.}"
+  mv "$DESTDIR"/"$x" "$DESTDIR"/"$dir"/module."$ext"
 done
