f2b50ed1a68e921e9f27007f3ed05f82bfb6b71f
Stefan Schmidt <stefan@osg.samsung.com>
2018-04-25 18:17:24 +0200

    ci: travis: enable pulseaudio and openssl in macos build
    These had been disabled due to former problems. They should work, so
    time to re-enable them.

--- .ci/ci-osx-build.sh
+++ .ci/ci-osx-build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-COPTS="--with-crypto=none --disable-pulseaudio --disable-cxx-bindings"
+COPTS="--disable-cxx-bindings"
 PARALLEL_JOBS=10
 
 # Prepare OSX env for build
@@ -10,6 +10,9 @@ mkdir -p ~/Library/LaunchAgents
 ln -sfv /usr/local/opt/d-bus/*.plist ~/Library/LaunchAgents
 launchctl load ~/Library/LaunchAgents/org.freedesktop.dbus-session.plist
 export PATH="$(brew --prefix gettext)/bin:$PATH"
+
+export CFLAGS="-I/usr/local/opt/openssl/include $CFLAGS"
+export LDFLAGS="-L/usr/local/opt/openssl/lib $LDFLAGS"
 
 # Normal build test of all targets
 ./autogen.sh $COPTS $@
