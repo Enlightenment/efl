8696b480cd1e0b9af16868c33be7c9a660f3dd9d
Stefan Schmidt <stefan@osg.samsung.com>
2018-04-26 18:14:19 +0200

    ci: enable gl-drm and egl in wayland build

--- .ci/ci-linux-build.sh
+++ .ci/ci-linux-build.sh
@@ -7,7 +7,8 @@ PARALLEL_JOBS=10
 CI_BUILD_TYPE=$1
 
 DEFAULT_COPTS="--with-tests=regular"
-WAYLAND_COPTS="--with-tests=regular --enable-wayland --enable-elput --enable-drm"
+WAYLAND_COPTS="--with-tests=regular --enable-wayland --enable-elput --enable-drm \
+--enable-wayland-ivi-shell --enable-gl-drm --with-opengl=es --enable-egl"
 MISC_COPTS="--enable-harfbuzz --enable-liblz4 --enable-image-loader-webp --enable-xinput22 --enable-multisense --enable-lua-old --enable-xpresent --enable-hyphen"
 
 # --enable-libvlc --enable-vnc-server --enable-g-main-loop --enable-libuv --enable-fb --enable-eglfs --enable-sdl --enable-gl-drm --enable-egl --enable-pix    man --enable-tile-rotate --enable-ecore-buffer --enable-image-loader-generic --enable-image-loader-jp2k --enable-gesture --enable-v4l2 --enable-xine
