f2ec39c8e6deed89cc9930f80fca197b3797cf36
Stefan Schmidt <stefan@osg.samsung.com>
2018-04-26 18:05:03 +0200

    ci: travis: create misc-disabled build
    This build should cover all kind of disable configure flags we support
    in efl. Making sure we still work with all of these when a commit comes
    in.

--- .ci/ci-linux-build.sh
+++ .ci/ci-linux-build.sh
@@ -13,6 +13,13 @@ MISC_COPTS="--enable-harfbuzz --enable-liblz4 --enable-image-loader-webp --enabl
 # --enable-libvlc --enable-vnc-server --enable-g-main-loop --enable-libuv --enable-fb --enable-eglfs --enable-sdl --enable-gl-drm --enable-egl --enable-pix    man --enable-tile-rotate --enable-ecore-buffer --enable-image-loader-generic --enable-image-loader-jp2k --enable-gesture --enable-v4l2 --enable-xine
 # --with-profile=PROFILE --with-crypto=CRYPTO
 
+MISC_DISABLED_COPTS="--disable-neon --disable-libeeze --disable-systemd --disable-magic-debug \
+--disable-valgrind --disable-cxx-bindings --disable-gstreamer1 \
+--disable-fontconfig --disable-fribidi --disable-poppler --disable-spectre --disable-libraw \
+--disable-librsvg --disable-xcf --disable-libmount --disable-tslib --disable-audio \
+--disable-pulseaudio --disable-avahi --disable-xinput2 --disable-xim --disable-scim \
+--disable-ibus --disable-physics --disable-quick-launch --disable-elua"
+
 if [ "$CI_BUILD_TYPE" = "" ]; then
   # Normal build test of all targets
   ./autogen.sh $DEFAULT_COPTS
@@ -33,3 +40,8 @@ if [ "$CI_BUILD_TYPE" = "misc" ]; then
   make -j $PARALLEL_JOBS examples
 fi
 
+if [ "$CI_BUILD_TYPE" = "misc-disabled" ]; then
+  ./autogen.sh $MISC_DISABLED_COPTS
+  make -j $PARALLEL_JOBS
+  make -j $PARALLEL_JOBS examples
+fi
