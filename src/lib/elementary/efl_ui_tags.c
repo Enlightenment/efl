b8de04d77f63f5f305b0ccd2670f1d908ba4bf7d
Jaehyun Cho <jae_hyun.cho@samsung.com>
2018-05-02 19:35:14 +0900

    efl_ui_tags: Change part names based on proper namespace

--- src/lib/elementary/efl_ui_tags.c
+++ src/lib/elementary/efl_ui_tags.c
@@ -924,7 +924,7 @@ _view_init(Evas_Object *obj, Efl_Ui_Tags_Data *sd)
 
    elm_box_layout_set(sd->box, _box_layout_cb, obj, NULL);
    elm_box_homogeneous_set(sd->box, EINA_FALSE);
-   elm_layout_content_set(obj, "box.swallow", sd->box);
+   elm_layout_content_set(obj, "efl.box", sd->box);
 
    sd->label = edje_object_add(evas_object_evas_get(obj));
    if (!sd->label) return;
