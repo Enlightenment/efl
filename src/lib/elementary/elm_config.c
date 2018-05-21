a5f9ee5ab30d62449502034bd78d8ad75a6aaecf
Cedric BAIL <cedric@osg.samsung.com>
2018-05-21 16:59:51 -0700

    elementary: fix configuration destruction by actually calling the proper way of destruction.

--- src/lib/elementary/elm_config.c
+++ src/lib/elementary/elm_config.c
@@ -1684,7 +1684,7 @@ _config_load(void)
         efl_del_intercept_set(_efl_config_obj, NULL);
         efl_loop_unregister(efl_main_loop_get(), EFL_CONFIG_INTERFACE, _efl_config_obj);
         efl_loop_unregister(efl_main_loop_get(), EFL_CONFIG_GLOBAL_CLASS, _efl_config_obj);
-        ELM_SAFE_FREE(_efl_config_obj, efl_unref);
+        ELM_SAFE_FREE(_efl_config_obj, efl_del);
         ELM_SAFE_FREE(_elm_config, _config_free);
         _elm_font_overlays_del_free();
 
