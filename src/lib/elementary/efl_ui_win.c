Enlightenment/efl

commit 473e657120ffd9626b6ee8832c75f2f66c9a5351

efl_ui_win: remove references to removed functions
Reviewers: ManMower

Reviewed By: ManMower

Subscribers: cedric

Differential Revision: https://phab.enlightenment.org/D6004

src/lib/elementary/efl_ui_win.c
@@ -373,7 +373,6 @@ static void _elm_win_frame_add(Efl_Ui_Win_Data *sd, const char *element, const c
 static void _elm_win_frame_style_update(Efl_Ui_Win_Data *sd, Eina_Bool force_emit, Eina_Bool calc);
 static inline void _elm_win_need_frame_adjust(Efl_Ui_Win_Data *sd, const char *engine);
 static void _elm_win_resize_objects_eval(Evas_Object *obj, Eina_Bool force_resize);
-static void _elm_win_opaque_update(Efl_Ui_Win_Data *sd, Eina_Bool force_alpha);
 static void _elm_win_frame_obj_update(Efl_Ui_Win_Data *sd, Eina_Bool force);
 
 #ifdef HAVE_ELEMENTARY_X
@@ -4471,7 +4470,6 @@ _elm_win_frame_style_update(Efl_Ui_Win_Data *sd, Eina_Bool force_emit, Eina_Bool
         if (calc)
           evas_object_smart_calculate(sd->frame_obj);
         _elm_win_frame_obj_update(sd, 0);
-        _elm_win_opaque_update(sd, EINA_FALSE);
      }
 }
 

commit 060e62061d030d0e3b69b315efb11e61baa6c064

efl ui win - fix legacy theme support correctly
this fixes T6918. during development legacy theme support was b roken.
473e657120ffd9626b6ee8832c75f2f66c9a5351 broke it.

src/lib/elementary/efl_ui_win.c
@@ -4349,6 +4349,30 @@ _elm_object_part_cursor_set(Evas_Object *obj, Evas_Object *edj,
    elm_object_sub_cursor_set(sub, obj, cursor);
 }
 
+static char *
+_efl_system_theme_path_get(void)
+{
+   // Find the default theme from EFL install. Quite ugly.
+   const char *sysdir;
+   char *version;
+   char path[PATH_MAX];
+   int v;
+
+   sysdir = elm_theme_system_dir_get();
+   if (!sysdir) return NULL;
+
+   eina_file_path_join(path, PATH_MAX, sysdir, "default.edj");
+   version = edje_file_data_get(path, "version");
+   v = version ? atoi(version) : 0;
+   free(version);
+   if (v < FRAME_OBJ_THEME_MIN_VERSION)
+     {
+        ERR("Default system theme is too old, something is wrong with your installation of EFL.");
+        return NULL;
+     }
+   return strdup(path);
+}
+
 static void
 _elm_win_frame_add(Efl_Ui_Win_Data *sd, const char *element, const char *style)
 {
@@ -4380,16 +4404,23 @@ _elm_win_frame_add(Efl_Ui_Win_Data *sd, const char *element, const char *style)
 
    if (v < FRAME_OBJ_THEME_MIN_VERSION)
      {
+        // Theme compatibility
+        const char *key  = "elm/border/base/default"; // FIXME?
+        char *sys_theme;
+
         WRN("Selected theme does not support the required border theme API "
             "(version = %d, requires >= %d).",
             v, FRAME_OBJ_THEME_MIN_VERSION);
-
-        if (!elm_widget_theme_object_set(sd->obj, sd->frame_obj, "border", element, style))
+        sys_theme = _efl_system_theme_path_get();
+        if (!sys_theme ||
+            !edje_object_file_set(sd->frame_obj, sys_theme, key))
           {
              ERR("Failed to set main border theme for the window.");
              ELM_SAFE_FREE(sd->frame_obj, evas_object_del);
+             free(sys_theme);
              return;
           }
+        free(sys_theme);
      }
 
    edje_object_freeze(sd->frame_obj);