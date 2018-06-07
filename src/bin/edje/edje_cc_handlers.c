Enlightenment/efl

commit 7fb47fc064b8f429242b4d5cdf57071475087821

edje: code refactoring for readibility.
collection.part is too ambigious, since group has parts as well.
Normally we regard it as "name" (of group) instead of part.

src/bin/edje/edje_cc_handlers.c
@@ -1589,7 +1589,7 @@ New_Object_Handler object_handlers[] =
    {"collections.group.physics.world", NULL},
 #endif
    PROGRAM_OBJECTS("collections.group.parts.part.description")
-   PROGRAM_OBJECTS("collections.group.parts.part")
+   PROGRAM_OBJECTS("collections.group.parts.name")
    PROGRAM_OBJECTS("collections.group.parts")
    PROGRAM_OBJECTS("collections.group")
 };
@@ -4473,7 +4473,7 @@ _group_name(char *name)
      goto double_named_group;
 
    current_de->entry = name;
-   current_pc->part = current_de->entry;
+   current_pc->name = current_de->entry;
 
    older = eina_hash_find(edje_file->collection, current_de->entry);
    if (older) eina_hash_del(edje_file->collection, current_de->entry, older);
@@ -4487,7 +4487,7 @@ _group_name(char *name)
 
           pc = eina_list_nth(edje_collections, older->id);
           INF("overriding alias ('%s' => '%s') by group '%s'",
-              alias->entry, pc->part,
+              alias->entry, pc->name,
               current_de->entry);
           aliases = eina_list_remove_list(aliases, l);
           free(alias);
@@ -5074,7 +5074,7 @@ st_collections_group_inherit(void)
           {
              free(parent_name);
              pc2 = eina_list_nth(edje_collections, alias->id);
-             parent_name = strdup(pc2->part);
+             parent_name = strdup(pc2->name);
              break;
           }
      }
@@ -5083,7 +5083,7 @@ st_collections_group_inherit(void)
      {
         EINA_LIST_FOREACH(edje_collections, l, pc2)
           {
-             if (!strcmp(parent_name, pc2->part))
+             if (!strcmp(parent_name, pc2->name))
                break;
           }
      }
@@ -5293,7 +5293,7 @@ st_collections_group_inherit(void)
    Code_Program *cp, *cp2;
    Edje_Part_Collection_Directory_Entry *de;
 
-   de = eina_hash_find(edje_file->collection, pc2->part);
+   de = eina_hash_find(edje_file->collection, pc2->name);
    cd2 = eina_list_nth(codes, de->id);
    cd = eina_list_data_get(eina_list_last(codes));
 
@@ -5306,7 +5306,7 @@ st_collections_group_inherit(void)
         if (cd->shared)
           {
              WRN("%s:%i. script block in group \"%s\" will be overwritten by inheriting "
-                 "from group \"%s\".", file_in, line - 1, pc->part, pc2->part);
+                 "from group \"%s\".", file_in, line - 1, pc->name, pc2->name);
              free(cd->shared);
           }
         if (cd->original)
@@ -5417,7 +5417,7 @@ st_collections_group_alias(void)
 
           pc = eina_list_nth(edje_collections, tmp->id);
           INF("overriding alias ('%s' => '%s') to ('%s' => '%s')",
-              tmp->entry, pc->part,
+              tmp->entry, pc->name,
               alias->entry, current_de->entry);
           aliases = eina_list_remove_list(aliases, l);
           free(tmp);
@@ -6487,7 +6487,7 @@ _part_type_set(unsigned int type)
         dummy->type = ep->type + 2;
         if (previous)
           {
-             new = _edje_part_description_alloc(type, pc->part, ep->name);
+             new = _edje_part_description_alloc(type, pc->name, ep->name);
              eina_hash_add(desc_hash, &new, ep);
              eina_hash_set(desc_hash, &previous, dummy);
              parent_desc = previous;
@@ -6504,7 +6504,7 @@ _part_type_set(unsigned int type)
         for (i = 0; i < ep->other.desc_count; i++)
           {
              previous = ep->other.desc[i];
-             new = _edje_part_description_alloc(type, pc->part, ep->name);
+             new = _edje_part_description_alloc(type, pc->name, ep->name);
              eina_hash_add(desc_hash, &new, ep);
              eina_hash_set(desc_hash, &previous, dummy);
              parent_desc = previous;
@@ -8513,7 +8513,7 @@ ob_collections_group_parts_part_description(void)
   pc = eina_list_data_get(eina_list_last(edje_collections));
   ep = current_part;
 
-  ed = _edje_part_description_alloc(ep->type, pc->part, ep->name);
+  ed = _edje_part_description_alloc(ep->type, pc->name, ep->name);
   eina_hash_add(desc_hash, &ed, ep);
 
   ed->rel1.id_x = -1;
@@ -9599,7 +9599,7 @@ st_collections_group_parts_part_description_color3(void)
        && current_part->type != EDJE_PART_TYPE_TEXTBLOCK)
      {
         ERR("Setting color3 in part %s from %s not of type TEXT or TEXTBLOCK.",
-            current_part->name, pc->part);
+            current_part->name, pc->name);
         exit(-1);
      }
 

src/bin/edje/edje_cc_out.c
@@ -403,7 +403,7 @@ check_image_part_desc(Edje_Part_Collection *pc, Edje_Part *ep,
    if (epd->image.id == -1 && epd->common.visible)
      WRN("Collection %s(%i): image attributes missing for "
          "part \"%s\", description \"%s\" %f",
-         pc->part, pc->id, ep->name, epd->common.state.name, epd->common.state.value);
+         pc->name, pc->id, ep->name, epd->common.state.name, epd->common.state.value);
 
    for (i = 0; i < epd->image.tweens_count; ++i)
      {
@@ -422,7 +422,7 @@ _source_group_find(const char *source)
    if (!source) return NULL;
    EINA_LIST_FOREACH(edje_collections, l, pc2)
      {
-        if (!strcmp(pc2->part, source))
+        if (!strcmp(pc2->name, source))
           return pc2;
      }
    return NULL;
@@ -469,9 +469,9 @@ check_text_part_desc(Edje_Part_Collection *pc, Edje_Part *ep,
                return;
           }
 
-        error_and_abort(ef, "Collection \"%s\" Part \"%s\" Description \"%s\" [%.3f]: "
+        error_and_abort(ef, "Group \"%s\" Part \"%s\" Description \"%s\" [%.3f]: "
                             "text.source point to a non TEXT part \"%s\"!",
-                        pc->part, ep->name, epd->common.state.name,
+                        pc->name, ep->name, epd->common.state.name,
                         epd->common.state.value, pc->parts[epd->text.id_source]->name);
      }
 
@@ -489,9 +489,9 @@ check_text_part_desc(Edje_Part_Collection *pc, Edje_Part *ep,
                return;
           }
 
-        error_and_abort(ef, "Collection \"%s\" Part \"%s\" Description \"%s\" [%.3f]: "
+        error_and_abort(ef, "Group \"%s\" Part \"%s\" Description \"%s\" [%.3f]: "
                             "text.text_source point to a non TEXT part \"%s\"!",
-                        pc->part, ep->name, epd->common.state.name,
+                        pc->name, ep->name, epd->common.state.name,
                         epd->common.state.value, pc->parts[epd->text.id_text_source]->name);
      }
 }
@@ -513,7 +513,7 @@ check_source_links(Edje_Part_Collection *pc, Edje_Part *ep, Eet_File *ef, Eina_L
    EINA_LIST_FOREACH(edje_collections, l, pc_source)
      {
         /* Find sourced group */
-        if (ep->source && pc_source->part && strcmp(ep->source, pc_source->part) == 0)
+        if (ep->source && pc_source->name && strcmp(ep->source, pc_source->name) == 0)
           {
              /* Go through every part to find parts with type GROUP */
              for (i = 0; i < pc_source->parts_count; ++i)
@@ -530,7 +530,7 @@ check_source_links(Edje_Part_Collection *pc, Edje_Part *ep, Eet_File *ef, Eina_L
                                                      "already included inside "
                                                      "part '%s' of group '%s'",
                                                  data, pc_source->parts[i]->name,
-                                                 pc->part);
+                                                 pc->name);
                               }
                          }
                        group_path = eina_list_append(group_path, ep->source);
@@ -2848,7 +2848,7 @@ reorder_parts(void)
              ep = (Edje_Part_Parser *)pc->parts[i];
              if (ep->reorder.insert_before && ep->reorder.insert_after)
                error_and_abort(NULL, "In group \"%s\": Unable to use together insert_before and insert_after in part \"%s\".",
-                               pc->part, pc->parts[i]->name);
+                               pc->name, pc->parts[i]->name);
 
              if (ep->reorder.done)
                {
@@ -2865,10 +2865,10 @@ reorder_parts(void)
                             ep2 = (Edje_Part_Parser *)pc->parts[j];
                             if (ep2->reorder.after)
                               error_and_abort(NULL, "In group \"%s\": The part \"%s\" is ambiguous ordered part.",
-                                              pc->part, pc->parts[i]->name);
+                                              pc->name, pc->parts[i]->name);
                             if (ep2->reorder.linked_prev)
                               error_and_abort(NULL, "In group \"%s\": Unable to insert two or more parts in same part \"%s\".",
-                                              pc->part, pc->parts[j]->name);
+                                              pc->name, pc->parts[j]->name);
                             /* Need it to be able to insert an element before the first */
                             if (j == 0) k = 0;
                             else k = j - 1;
@@ -2888,10 +2888,10 @@ reorder_parts(void)
                             ep2 = (Edje_Part_Parser *)pc->parts[j];
                             if (ep2->reorder.before)
                               error_and_abort(NULL, "In group \"%s\": The part \"%s\" is ambiguous ordered part.",
-                                              pc->part, pc->parts[i]->name);
+                                              pc->name, pc->parts[i]->name);
                             if (ep2->reorder.linked_next)
                               error_and_abort(NULL, "In group \"%s\": Unable to insert two or more parts in same part \"%s\".",
-                                              pc->part, pc->parts[j]->name);
+                                              pc->name, pc->parts[j]->name);
                             k = j;
                             found = EINA_TRUE;
                             ep2->reorder.linked_next += ep->reorder.linked_next + 1;
@@ -2910,7 +2910,7 @@ reorder_parts(void)
 
                        if (((i > k) && ((i - ep->reorder.linked_prev) <= k))
                            || ((i < k) && ((i + ep->reorder.linked_next) >= k)))
-                         error_and_abort(NULL, "In group \"%s\": The part order is wrong. It has circular dependency.", pc->part);
+                         error_and_abort(NULL, "In group \"%s\": The part order is wrong. It has circular dependency.", pc->name);
 
                        amount = ep->reorder.linked_prev + ep->reorder.linked_next + 1;
                        linked = i - ep->reorder.linked_prev;
@@ -2954,10 +2954,10 @@ reorder_parts(void)
                     {
                        if (ep->reorder.insert_before)
                          error_and_abort(NULL, "In group \"%s\": Unable to find part \"%s\" for insert_before in part \"%s\".",
-                                         pc->part, ep->reorder.insert_before, pc->parts[i]->name);
+                                         pc->name, ep->reorder.insert_before, pc->parts[i]->name);
                        else
                          error_and_abort(NULL, "In group \"%s\": Unable to find part \"%s\" for insert_after in part \"%s\".",
-                                         pc->part, ep->reorder.insert_after, pc->parts[i]->name);
+                                         pc->name, ep->reorder.insert_after, pc->parts[i]->name);
                     }
                }
           }
@@ -3208,7 +3208,7 @@ copied_program_anonymous_lookup_delete(Edje_Part_Collection *pc, int *dest)
         Edje_Part_Collection_Directory_Entry *de;
         Eina_List *l2, *ll2;
 
-        de = eina_hash_find(edje_file->collection, pl->pc->part);
+        de = eina_hash_find(edje_file->collection, pl->pc->name);
         cd = eina_list_nth(codes, de->id);
 
         EINA_LIST_FOREACH_SAFE(cd->programs, l2, ll2, cp)
@@ -3716,13 +3716,13 @@ data_process_lookups(void)
         unsigned int id = 0;
         unsigned int i;
 
-        if (!pc->part)
+        if (!pc->name)
           {
              ERR("A collection without a name was detected, that's not allowed.");
              exit(-1);
           }
 
-        find = eina_hash_find(edje_file->collection, pc->part);
+        find = eina_hash_find(edje_file->collection, pc->name);
         if (find && find->id == pc->id)
           {
              if (((Edje_Part_Collection_Parser *)pc)->inherit_only)
@@ -3756,7 +3756,7 @@ data_process_lookups(void)
                if (pc->id == alias->id)
                  alias->id = id;
 
-             find = eina_hash_find(edje_file->collection, pc->part);
+             find = eina_hash_find(edje_file->collection, pc->name);
              if (pc->id != find->id) find = NULL;
 
              de = eina_hash_find(edje_collections_lookup, &pc->id);
@@ -3832,7 +3832,7 @@ data_process_lookups(void)
              if ((i == part->key.pc->parts_count) && (!((Edje_Part_Collection_Parser *)part->key.pc)->inherit_only))
                {
                   ERR("Unable to find part name \"%s\" needed in group '%s'.",
-                      alias, part->key.pc->part);
+                      alias, part->key.pc->name);
                   exit(-1);
                }
           }

src/bin/edje/edje_cc_script.c
@@ -347,7 +347,7 @@ _push_symbol(Eina_List **total, Code_Symbol *sym, Edje_Part_Collection *pc)
         if (!strcmp(sym2->name, sym->name))
           {
              WRN("Symbols in group \"%s\" have same name \"%s\". Latter defined "
-                 "will shadow former one.", pc->part, sym->name);
+                 "will shadow former one.", pc->name, sym->name);
              list = eina_list_remove(list, sym2);
              break;
           }

src/bin/edje/edje_pick.c
@@ -1589,7 +1589,7 @@ _edje_pick_collection_process(Edje_Part_Collection *edc, Edje_File *edf, Edje_Fi
    /* Update all IDs, NAMES in current collection */
    Edje_Part_Collection_Directory_Entry *ce;
 
-   ce = eina_hash_find(o->collection, edc->part);
+   ce = eina_hash_find(o->collection, edc->name);
    edc->id = ce->id;
 
    _edje_pick_resources_process(edc, edf, o);

src/lib/edje/edje_cache.c
@@ -106,7 +106,7 @@ _edje_programs_patterns_init(Edje_Part_Collection *edc)
 
    if (getenv("EDJE_DUMP_PROGRAMS"))
      {
-        INF("Group '%s' programs:", edc->part);
+        INF("Group '%s' programs:", edc->name);
 #define EDJE_DUMP_PROGRAM(Section)                    \
   for (i = 0; i < edc->programs.Section##_count; i++) \
     INF(#Section " for ('%s', '%s')", edc->programs.Section[i]->signal, edc->programs.Section[i]->source);
@@ -171,7 +171,7 @@ _edje_file_coll_open(Edje_File *edf, const char *coll)
 
    EINA_LIST_FOREACH(edf->collection_cache, l, edc)
      {
-        if (!strcmp(edc->part, coll))
+        if (!strcmp(edc->name, coll))
           {
              edc->references = 1;
              ce->ref = edc;
@@ -190,7 +190,7 @@ _edje_file_coll_open(Edje_File *edf, const char *coll)
    if (!edc) return NULL;
 
    edc->references = 1;
-   edc->part = ce->entry;
+   edc->name = ce->entry;
 
    /* For Edje file build with Edje 1.0 */
    if (edf->version <= 3 && edf->minor <= 1)
@@ -614,7 +614,7 @@ _edje_cache_coll_clean(Edje_File *edf)
         edc = eina_list_data_get(eina_list_last(edf->collection_cache));
         edf->collection_cache = eina_list_remove_list(edf->collection_cache, eina_list_last(edf->collection_cache));
 
-        ce = eina_hash_find(edf->collection, edc->part);
+        ce = eina_hash_find(edf->collection, edc->name);
         _edje_collection_free(edf, edc, ce);
      }
 }
@@ -633,7 +633,7 @@ _edje_cache_coll_flush(Edje_File *edf)
         edf->collection_cache = eina_list_remove_list(edf->collection_cache,
                                                       last);
 
-        ce = eina_hash_find(edf->collection, edc->part);
+        ce = eina_hash_find(edf->collection, edc->name);
         _edje_collection_free(edf, edc, ce);
      }
 }
@@ -646,10 +646,10 @@ _edje_cache_coll_unref(Edje_File *edf, Edje_Part_Collection *edc)
    edc->references--;
    if (edc->references != 0) return;
 
-   ce = eina_hash_find(edf->collection, edc->part);
+   ce = eina_hash_find(edf->collection, edc->name);
    if (!ce)
      {
-        ERR("Something is wrong with reference count of '%s'.", edc->part);
+        ERR("Something is wrong with reference count of '%s'.", edc->name);
      }
    else if (ce->ref)
      {

src/lib/edje/edje_data.c
@@ -1423,7 +1423,7 @@ _edje_edd_init(void)
    EET_DATA_DESCRIPTOR_ADD_BASIC(_edje_edd_edje_part_collection, Edje_Part_Collection, "prop.min.h", prop.min.h, EET_T_INT);
    EET_DATA_DESCRIPTOR_ADD_BASIC(_edje_edd_edje_part_collection, Edje_Part_Collection, "prop.max.w", prop.max.w, EET_T_INT);
    EET_DATA_DESCRIPTOR_ADD_BASIC(_edje_edd_edje_part_collection, Edje_Part_Collection, "prop.max.h", prop.max.h, EET_T_INT);
-   EET_DATA_DESCRIPTOR_ADD_BASIC(_edje_edd_edje_part_collection, Edje_Part_Collection, "part", part, EET_T_STRING);
+   EET_DATA_DESCRIPTOR_ADD_BASIC(_edje_edd_edje_part_collection, Edje_Part_Collection, "name", name, EET_T_STRING);
    EET_DATA_DESCRIPTOR_ADD_BASIC(_edje_edd_edje_part_collection, Edje_Part_Collection, "lua_script_only", lua_script_only, EET_T_UCHAR);
    EET_DATA_DESCRIPTOR_ADD_BASIC(_edje_edd_edje_part_collection, Edje_Part_Collection, "prop.orientation", prop.orientation, EET_T_INT);
    EET_DATA_DESCRIPTOR_ADD_BASIC(_edje_edd_edje_part_collection, Edje_Part_Collection, "broadcast_signal", broadcast_signal, EET_T_UCHAR);

src/lib/edje/edje_edit.c
@@ -1799,7 +1799,7 @@ edje_edit_group_copy(Evas_Object *obj, const char *group_name, const char *copy_
    _mempools_add(de);
 
    epc->id = id;
-   epc->part = eina_stringshare_add(copy_name);
+   epc->name = eina_stringshare_add(copy_name);
    snprintf(buf, sizeof(buf), "edje/collections/%i", epc->id);
    eet_data_write(eetf, _edje_edd_edje_part_collection, buf, epc, 1);
 
@@ -1908,7 +1908,7 @@ edje_edit_group_add(Evas_Object *obj, const char *name)
    pc->parts = NULL;
    pc->data = NULL;
    pc->script = NULL;
-   pc->part = eina_stringshare_add(name);
+   pc->name = eina_stringshare_add(name);
 
    //cd = _alloc(sizeof(Code));
    //codes = eina_list_append(codes, cd);
@@ -1944,7 +1944,7 @@ edje_edit_group_del(Evas_Object *obj, const char *group_name)
 
    EINA_LIST_FOREACH(ed->file->collection_cache, l, g)
      {
-        if (strcmp(g->part, e->entry) == 0)
+        if (strcmp(g->name, e->entry) == 0)
           {
              ed->file->collection_cache =
                eina_list_remove_list(ed->file->collection_cache, l);
@@ -1988,8 +1988,8 @@ edje_edit_group_del(Evas_Object *obj, const char *group_name)
                {
                   ed->file->collection_cache =
                     eina_list_remove_list(ed->file->collection_cache, l);
-                  e_del = eina_hash_find(ed->file->collection, g->part);
-                  eina_hash_del(ed->file->collection, g->part, e_del);
+                  e_del = eina_hash_find(ed->file->collection, g->name);
+                  eina_hash_del(ed->file->collection, g->name, e_del);
                   _edje_collection_free(ed->file, g, e_del);
                }
           }
@@ -2022,22 +2022,22 @@ edje_edit_group_name_set(Evas_Object *obj, const char *new_name)
 
    pc = ed->collection;
 
-   if (!strcmp(pc->part, new_name)) return EINA_TRUE;
+   if (!strcmp(pc->name, new_name)) return EINA_TRUE;
 
    if (edje_edit_group_exist(obj, new_name)) return EINA_FALSE;
 
-   _edje_edit_group_references_update(obj, pc->part, new_name);
+   _edje_edit_group_references_update(obj, pc->name, new_name);
 
    //printf("Set name of current group: %s [id: %d][new name: %s]\n",
-   // pc->part, pc->id, new_name);
+   // pc->name, pc->id, new_name);
 
-   //if (pc->part && ed->file->free_strings) eina_stringshare_del(pc->part); TODO FIXME
-   pce = eina_hash_find(ed->file->collection, pc->part);
+   //if (pc->name && ed->file->free_strings) eina_stringshare_del(pc->name); TODO FIXME
+   pce = eina_hash_find(ed->file->collection, pc->name);
 
    eina_hash_move(ed->file->collection, pce->entry, new_name);
 
    pce->entry = eina_stringshare_add(new_name);
-   pc->part = pce->entry;
+   pc->name = pce->entry;
    eina_stringshare_replace(&ed->group, new_name);
 
    return EINA_TRUE;

src/lib/edje/edje_embryo.c
@@ -4728,7 +4728,7 @@ _edje_embryo_test_run(Edje *ed, Edje_Program *pr, const char *sig, const char *s
                  "SIGNAL: '%s', "
                  "SOURCE: '%s', "
                  "ERROR: '%s'",
-                 ed->collection->part,
+                 ed->collection->name,
                  ed->file->path,
                  fname, pr->name,
                  sig, src,
@@ -4743,7 +4743,7 @@ _edje_embryo_test_run(Edje *ed, Edje_Program *pr, const char *sig, const char *s
                  "SIGNAL: '%s', "
                  "SOURCE: '%s', "
                  "ERROR: 'Script exceeded maximum allowed cycle count of %i'",
-                 ed->collection->part,
+                 ed->collection->name,
                  ed->file->path,
                  fname, pr->name,
                  sig, src,

commit 475c93fd7599badad0c073e5eb025b92481b53ec

edje: fix a regression error by 7fb47fc064b8f429242b4d5cdf57071475087821.
That shouldn't be touched, just confused by its name.

src/bin/edje/edje_cc_handlers.c
@@ -1589,7 +1589,7 @@ New_Object_Handler object_handlers[] =
    {"collections.group.physics.world", NULL},
 #endif
    PROGRAM_OBJECTS("collections.group.parts.part.description")
-   PROGRAM_OBJECTS("collections.group.parts.name")
+   PROGRAM_OBJECTS("collections.group.parts.part")
    PROGRAM_OBJECTS("collections.group.parts")
    PROGRAM_OBJECTS("collections.group")
 };