6d1bb7d2d9ab642e08583d5c9cec8a0482f34c2d
Mike Blumenkrantz <zmike@samsung.com>
2018-06-21 18:44:54 -0400

    eo: remove some erroneous mutex unlocks during base class parent_set errors
    recent changes added unlocks for these mutexes in some other places, so only
    do the unlocks where necessary
    
    fix T7020

--- src/lib/eo/eo_base_class.c
+++ src/lib/eo/eo_base_class.c
@@ -767,7 +767,7 @@ _efl_object_parent_set(Eo *obj, Efl_Object_Data *pd, Eo *parent_id)
              if (prev_parent) efl_unref(obj);
              // unlikely this error happens, so move it out of execution path
              // to improve l1 cache efficiency
-             goto err_parent;
+             goto err_parent_done;
           }
 
         eo_obj->parent = EINA_TRUE;
@@ -783,16 +783,16 @@ _efl_object_parent_set(Eo *obj, Efl_Object_Data *pd, Eo *parent_id)
    EO_OBJ_DONE(obj);
    return;
 
+err_parent_done:
+   EO_OBJ_DONE(obj);
 err_parent:
    ERR("New parent %p for object %p is not a valid Eo object.",
        parent_id, obj);
-   EO_OBJ_DONE(obj);
    return;
 
 err_impossible:
    ERR("CONTACT DEVS!!! SHOULD NEVER HAPPEN!!! Old parent %p for object %p is not a valid Eo object.",
        pd->parent, obj);
-   EO_OBJ_DONE(obj);
 }
 
 EOLIAN static Eo *
