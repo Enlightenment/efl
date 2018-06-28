5a4fe074154f37b8e45e438a092c6fbd4523766a
Mike Blumenkrantz <zmike@samsung.com>
2018-06-28 14:53:10 -0500

    evas/main: move common shutdown to after ecore shutdown
    Summary:
    ecore_shutdown will trigger object deletions which require common
    components to still be active in order to avoid crashes
    
    ref 3433be343779424c5e030ace30e211298cd060f8
    ref T7052
    
    Reviewers: ManMower, devilhorns
    
    Reviewed By: ManMower, devilhorns
    
    Subscribers: cedric, #committers
    
    Tags: #efl
    
    Maniphest Tasks: T7052
    
    Differential Revision: https://phab.enlightenment.org/D6475

--- src/lib/evas/canvas/evas_main.c
+++ src/lib/evas/canvas/evas_main.c
@@ -147,8 +147,6 @@ evas_shutdown(void)
 
    evas_focus_shutdown();
 
-   evas_common_shutdown();
-
 #ifdef EVAS_CSERVE2
    if (evas_cserve2_use_get())
      evas_cserve2_shutdown();
@@ -162,6 +160,7 @@ evas_shutdown(void)
    evas_async_events_shutdown();
 
    ecore_shutdown();
+   evas_common_shutdown();
 
    evas_filter_shutdown();
    evas_module_shutdown();
