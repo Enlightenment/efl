51ccaa91b937512f519ea406431ac2a8ad16e609
Stefan Schmidt <s.schmidt@samsung.com>
2018-07-09 10:22:59 -0400

    eina: remove no longer used function _eina_thread_queue_msg_block_real_free
    Summary:
    It seems this function is no longer used since some rework. Clean it up.
    lib/eina/eina_thread_queue.c:127:1: warning: ‘_eina_thread_queue_msg_block_real_free’ defined but not used [-Wunused-function]
    
    Reviewers: devilhorns
    
    Reviewed By: devilhorns
    
    Subscribers: cedric, #committers, zmike
    
    Tags: #efl
    
    Differential Revision: https://phab.enlightenment.org/D6537

--- src/lib/eina/eina_thread_queue.c
+++ src/lib/eina/eina_thread_queue.c
@@ -124,23 +124,6 @@ _eina_thread_queue_msg_block_new(int size)
 }
 
 static void
-_eina_thread_queue_msg_block_real_free(Eina_Thread_Queue_Msg_Block *blk)
-{
-   eina_lock_take(&(blk->lock_non_0_ref));
-   eina_lock_release(&(blk->lock_non_0_ref));
-   eina_lock_free(&(blk->lock_non_0_ref));
-#ifndef ATOMIC
-   eina_spinlock_take(&(blk->lock_ref));
-   eina_spinlock_release(&(blk->lock_ref));
-   eina_spinlock_free(&(blk->lock_ref));
-   eina_spinlock_take(&(blk->lock_first));
-   eina_spinlock_release(&(blk->lock_first));
-   eina_spinlock_free(&(blk->lock_first));
-#endif
-   free(blk);
-}
-
-static void
 _eina_thread_queue_msg_block_free(Eina_Thread_Queue_Msg_Block *blk)
 {
    Eina_Mempool *mp;
