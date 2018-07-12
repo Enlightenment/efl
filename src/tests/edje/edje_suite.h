b05a5cb4c73d890ab4cd65d8872a470193439bcc
Mike Blumenkrantz <zmike@samsung.com>
2018-07-12 16:25:15 +0900

    tests/edje: split edje tests into separate files
    Summary:
    this will speed up tests and make it slightly less intimidating to
    add new tests
    
    Reviewers: devilhorns
    
    Subscribers: cedric, #committers
    
    Tags: #efl
    
    Differential Revision: https://phab.enlightenment.org/D6554

--- src/tests/edje/edje_suite.h
+++ src/tests/edje/edje_suite.h
@@ -3,7 +3,16 @@
 
 #include <check.h>
 #include "../efl_check.h"
+#include <Edje.h>
+Evas *_setup_evas(void);
+const char *test_layout_get(const char *name);
+
 void edje_test_edje(TCase *tc);
+void edje_test_container(TCase *tc);
+void edje_test_features(TCase *tc);
+void edje_test_signal(TCase *tc);
+void edje_test_swallow(TCase *tc);
+void edje_test_text(TCase *tc);
 
 
 #endif /* _EDJE_SUITE_H */
