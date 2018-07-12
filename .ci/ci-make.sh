0d750f7f581d763f4946516bb6416e924c48a5b2
Mike Blumenkrantz <zmike@samsung.com>
2018-07-12 19:21:15 -0400

    ci: disable make on release builds
    make is no longer required for distcheck, so stop doing 2 full tree builds
    on this build type
    
    fixes release builds on travis
    
    Differential Revision: https://phab.enlightenment.org/D6611

--- .ci/ci-make.sh
+++ .ci/ci-make.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+if [ "$1" = "release-ready" ] ; then
+  exit 0
+fi
+
 if [ "$DISTRO" != "" ] ; then
   docker exec --env MAKEFLAGS="-j5" --env EIO_MONITOR_POLL=1 $(cat $HOME/cid) make
 else
