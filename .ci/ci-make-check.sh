9b6b88c9a237023c3f47b8638e22bf7b21556b71
Mike Blumenkrantz <zmike@samsung.com>
2018-07-14 00:45:05 -0400

    ci: make check -> make check-TESTS
    the tests have all been compiled at this point using the check-build
    target (separately) in order to split out test compile failures from
    runtime failures, so use the check-TESTS target to run the tests without
    checking whether the tests are built first
    
    Differential Revision: https://phab.enlightenment.org/D6619

--- .ci/ci-make-check.sh
+++ .ci/ci-make-check.sh
@@ -1,13 +1,13 @@
-#!/bin/sh -x
+#!/bin/sh
 
 . .ci/travis.sh
 if [ "$1" = "release-ready" ] ; then
   exit 0
 fi
-travis_fold check "make check"
+travis_fold check "make check-TESTS"
 if [ "$DISTRO" != "" ] ; then
   for tries in 1 2 3 ; do
-    (docker exec --env MAKEFLAGS="-j5" --env EIO_MONITOR_POLL=1 $(cat $HOME/cid) make -j2 check) && break
+    (docker exec --env MAKEFLAGS="-j5" --env EIO_MONITOR_POLL=1 $(cat $HOME/cid) make -j2 -C src/ check-TESTS) && break
     docker exec --env MAKEFLAGS="-j5" --env EIO_MONITOR_POLL=1 $(cat $HOME/cid) cat src/test-suite.log
     if [ $tries != 3 ] ; then echo "tests failed, trying again!" ; fi
     false
@@ -15,7 +15,7 @@ if [ "$DISTRO" != "" ] ; then
 else
   export PATH="/usr/local/opt/ccache/libexec:$(brew --prefix gettext)/bin:$PATH"
   for tries in 1 2 3 ; do
-    make -j2 check && break
+    make -j2 -C src/ check-TESTS && break
     cat src/test-suite.log
     if [ $tries != 3 ] ; then echo "tests failed, trying again!" ; fi
     false
