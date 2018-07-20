bc248fdcf2095f5e047b7fc6e0f7273e521470d4
Mike Blumenkrantz <zmike@samsung.com>
2018-07-20 11:40:58 -0400

    ci: attempt to print the test logs if distcheck fails
    distcheck failures from test failures are hard to debug since distcheck
    is more complicated than a regular build, this is a first step in providing
    info for that debugging
    
    Differential Revision: https://phab.enlightenment.org/D6652

--- .ci/ci-make-distcheck.sh
+++ .ci/ci-make-distcheck.sh
@@ -10,7 +10,7 @@ if [ "$DISTRO" != "" ] ; then
   docker exec --env MAKEFLAGS="-j5 -rR" --env EIO_MONITOR_POLL=1 --env CC="ccache gcc" \
     --env CXX="ccache g++" \
     --env CFLAGS="-fdirectives-only" --env CXXFLAGS="-fdirectives-only" \
-    $(cat $HOME/cid) make distcheck
+    $(cat $HOME/cid) make distcheck || (sudo cat efl-*/_build/sub/src/test-suite.log; false)
 else
   export PATH="/usr/local/opt/ccache/libexec:$(brew --prefix gettext)/bin:$PATH"
   make
