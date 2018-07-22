325896630dba82973e64e0c8ed5376ca6f422c18
Mike Blumenkrantz <zmike@samsung.com>
2018-07-22 01:50:15 -0400

    ci: trigger distcheck only if build files changed or keyword present
    'cibuildme' , when placed in a commit log, will force
    a distcheck build on CI if no build files have changed
    
    Differential Revision: https://phab.enlightenment.org/D6664

--- .ci/ci-make-distcheck.sh
+++ .ci/ci-make-distcheck.sh
@@ -1,19 +1,46 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
+
+
 . .ci/travis.sh
+
+run_distcheck() {
+   if [ "$DISTRO" != "" ] ; then
+     docker exec --env MAKEFLAGS="-j5 -rR" --env EIO_MONITOR_POLL=1 --env CC="ccache gcc" \
+       --env CXX="ccache g++" \
+       --env CFLAGS="-fdirectives-only" --env CXXFLAGS="-fdirectives-only" \
+       --env LD="ld.gold" $(cat $HOME/cid) make distcheck || \
+       (sudo cat efl-*/_build/sub/src/test-suite.log; false)
+   else
+     export PATH="/usr/local/opt/ccache/libexec:$(brew --prefix gettext)/bin:$PATH"
+     make distcheck
+   fi
+}
+
 if [ "$1" != "release-ready" ] ; then
   exit 0
 fi
 travis_fold distcheck "make distcheck"
-if [ "$DISTRO" != "" ] ; then
-  docker exec --env MAKEFLAGS="-j5 -rR" --env EIO_MONITOR_POLL=1 --env CC="ccache gcc" \
-    --env CXX="ccache g++" \
-    --env CFLAGS="-fdirectives-only" --env CXXFLAGS="-fdirectives-only" \
-    --env LD="ld.gold" $(cat $HOME/cid) make distcheck || \
-    (sudo cat efl-*/_build/sub/src/test-suite.log; false)
+
+num_buildfiles_changed=0
+if [ -f ~/cachedir/last-distcheck.txt ] ; then
+  prev_distcheck=$(cat ~/cachedir/last-distcheck.txt)
+  if git show --oneline $prev_distcheck 2>&1 > /dev/null ; then
+    num_buildfiles_changed=$(git diff --name-only $prev_distcheck HEAD|grep '\.am\|\.ac\|\.mk'|wc -l)
+    manual_trigger=$(git log --grep 'cibuildme' $prev_distcheck..HEAD|wc -l)
+  else
+    manual_trigger=1
+  fi
+  if [ "$num_buildfiles_changed" != "0" -o "$manual_trigger" != "0" ]; then
+    run_distcheck
+  else
+    echo "Skipping distcheck: not forced and no build files changed"
+  fi
 else
-  export PATH="/usr/local/opt/ccache/libexec:$(brew --prefix gettext)/bin:$PATH"
-  make
+  run_distcheck
 fi
+rm -f ~/cachedir/last-distcheck.txt
+git rev-parse HEAD > ~/cachedir/last-distcheck.txt
+
 travis_endfold distcheck
