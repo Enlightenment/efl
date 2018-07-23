2ea7bc5a81c920afbb9ef197c6c7c5659508c4d4
Hermet Park <hermetpark@gmail.com>
2018-07-23 08:18:00 -0400

    evas clip: more strict comparison.
    Summary: just improvement from D6645
    
    Reviewers: zmike, #committers
    
    Reviewed By: zmike, #committers
    
    Subscribers: cedric, #committers, zmike
    
    Tags: #efl
    
    Differential Revision: https://phab.enlightenment.org/D6650

--- src/lib/evas/canvas/evas_clip.c
+++ src/lib/evas/canvas/evas_clip.c
@@ -394,8 +394,9 @@ _efl_canvas_object_clip_set(Eo *eo_obj, Evas_Object_Protected_Data *obj, Evas_Ob
    evas_object_update_bounding_box(eo_obj, obj, NULL);
    evas_object_clip_dirty(eo_obj, obj);
    evas_object_recalc_clippees(obj);
-   if (obj->cur->visible && (!obj->is_smart) &&
-       (!((obj->map->cur.map) && (obj->map->cur.usemap))))
+   if ((!obj->is_smart) &&
+       (!((obj->map->cur.map) && (obj->map->cur.usemap))) &&
+       evas_object_is_visible(eo_obj, obj))
      {
         _evas_canvas_event_pointer_in_rect_mouse_move_feed(obj->layer->evas,
                                                            eo_obj,
@@ -443,8 +444,9 @@ _clip_unset(Eo *eo_obj, Evas_Object_Protected_Data *obj)
    evas_object_clip_dirty(eo_obj, obj);
    evas_object_recalc_clippees(obj);
 
-   if (obj->cur->visible && (!obj->is_smart) &&
-       (!((obj->map->cur.map) && (obj->map->cur.usemap))))
+   if ((!obj->is_smart) &&
+       (!((obj->map->cur.map) && (obj->map->cur.usemap))) &&
+       evas_object_is_visible(eo_obj, obj))
      {
         _evas_canvas_event_pointer_in_rect_mouse_move_feed(obj->layer->evas,
                                                            eo_obj,
