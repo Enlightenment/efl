124703826f24975a3f45ab26e567bf547b9db25b
Chris Michael <devilhorns@comcast.net>
2018-07-25 14:57:52 -0400

    elementary: Check for valid resize object
    Summary:
    Fix an issue where elm_slider was passing NULL to eo functions due to
    wd->resize_obj being NULL. Discovered via Enlightenment mixer popup
    dialog
    
    ref T7030
    Depends on D6639
    
    Reviewers: zmike
    
    Reviewed By: zmike
    
    Subscribers: Hermet, bu5hm4n, cedric, #committers
    
    Tags: #efl
    
    Maniphest Tasks: T7030
    
    Differential Revision: https://phab.enlightenment.org/D6640

--- src/lib/elementary/elm_slider.c
+++ src/lib/elementary/elm_slider.c
@@ -1045,6 +1045,8 @@ _elm_slider_efl_ui_format_format_cb_set(Eo *obj, Elm_Slider_Data *sd, void *func
    if (sd->format_cb_data && sd->format_free_cb)
      sd->format_free_cb(sd->format_cb_data);
 
+   if (efl_invalidated_get(obj)) return;
+
    sd->format_cb = func;
    sd->format_cb_data = func_data;
    sd->format_free_cb = func_free_cb;
