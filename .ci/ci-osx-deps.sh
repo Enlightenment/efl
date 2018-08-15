92e4d32d04f62c556625ee6b2f8c07e46aa422ee
Mike Blumenkrantz <zmike@samsung.com>
2018-08-15 17:29:00 -0400

    install meson and ninja on osx

--- .ci/ci-osx-deps.sh
+++ .ci/ci-osx-deps.sh
@@ -2,4 +2,4 @@
 
 brew update
 brew unlink python
-brew install gettext check bullet dbus fontconfig freetype fribidi gst-plugins-good gstreamer luajit openssl webp libsndfile glib libspectre libraw librsvg poppler lz4 pulseaudio ccache
+brew install gettext check bullet dbus fontconfig freetype fribidi gst-plugins-good gstreamer luajit openssl webp libsndfile glib libspectre libraw librsvg poppler lz4 pulseaudio ccache meson ninja
