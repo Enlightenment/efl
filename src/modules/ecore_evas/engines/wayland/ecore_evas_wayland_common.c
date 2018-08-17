Enlightenment/efl

commit 5af84afced98956198b9c316facb671e46b381c1

ecore_evas_wayland: Replace transparent_set with alpha_set
transparent_set is a legacy path that should do exactly what alpha_set
does these days, so squash them into the same function.

src/modules/ecore_evas/engines/wayland/ecore_evas_wayland_common.c
@@ -1849,36 +1849,6 @@ _ecore_evas_wayland_alpha_do(Ecore_Evas *ee, int alpha)
 }
 
 static void
-_ecore_evas_wayland_transparent_do(Ecore_Evas *ee, int transparent)
-{
-   Evas_Engine_Info_Wayland *einfo;
-   Ecore_Evas_Engine_Wl_Data *wdata;
-   int fw, fh;
-
-   LOGFN(__FILE__, __LINE__, __FUNCTION__);
-
-   if (!ee) return;
-   if (ee->transparent == transparent) return;
-   ee->transparent = transparent;
-
-   wdata = ee->engine.data;
-   if (!wdata->sync_done) return;
-
-   if (wdata->win)
-     ecore_wl2_window_transparent_set(wdata->win, ee->transparent);
-
-   evas_output_framespace_get(ee->evas, NULL, NULL, &fw, &fh);
-
-   if ((einfo = (Evas_Engine_Info_Wayland *)evas_engine_info_get(ee->evas)))
-     {
-        einfo->info.destination_alpha = EINA_TRUE;
-        if (!evas_engine_info_set(ee->evas, (Evas_Engine_Info *)einfo))
-          ERR("evas_engine_info_set() for engine '%s' failed.", ee->driver);
-        evas_damage_rectangle_add(ee->evas, 0, 0, ee->w + fw, ee->h + fh);
-     }
-}
-
-static void
 _ecore_evas_wl_common_render_updates(void *data, Evas *evas EINA_UNUSED, void *event EINA_UNUSED)
 {
    Ecore_Evas *ee = data;
@@ -1887,11 +1857,6 @@ _ecore_evas_wl_common_render_updates(void *data, Evas *evas EINA_UNUSED, void *e
      {
         _ecore_evas_wayland_alpha_do(ee, ee->delayed.alpha);
         ee->delayed.alpha_changed = EINA_FALSE;
-     }
-   if (ee->delayed.transparent_changed)
-     {
-        _ecore_evas_wayland_transparent_do(ee, ee->delayed.transparent);
-        ee->delayed.transparent_changed = EINA_FALSE;
      }
    if (ee->delayed.rotation_changed)
      {
@@ -2211,19 +2176,6 @@ _ecore_evas_wl_common_alpha_set(Ecore_Evas *ee, int alpha)
 }
 
 static void
-_ecore_evas_wl_common_transparent_set(Ecore_Evas *ee, int transparent)
-{
-   if (ee->in_async_render)
-     {
-        ee->delayed.transparent = transparent;
-        ee->delayed.transparent_changed = EINA_TRUE;
-        return;
-     }
-
-   _ecore_evas_wayland_transparent_do(ee, transparent);
-}
-
-static void
 _ecore_evas_wl_common_rotation_set(Ecore_Evas *ee, int rotation, int resize)
 {
    LOGFN(__FILE__, __LINE__, __FUNCTION__);
@@ -2402,7 +2354,7 @@ static Ecore_Evas_Engine_Func _ecore_wl_engine_func =
    NULL, // func sticky set
    _ecore_evas_wl_common_ignore_events_set,
    _ecore_evas_wl_common_alpha_set,
-   _ecore_evas_wl_common_transparent_set,
+   _ecore_evas_wl_common_alpha_set, // transparent set
    NULL, // func profiles set
    NULL, // func profile set
    NULL, // window group set

commit 6010d3e8c5d1e5b0ec617915ddb2cc9024197633

ecore wayland: fix a regression bug of wayland_transparent_get().
Summary:
wayland transparent func was replaced with alpha's.

By this, transparent common attribute is no more valid,
ecore_evas_transparent_get() doesn't work.

This is a regression bug by 5af84afced98956198b9c316facb671e46b381c1

Reviewers: ManMower, devilhorns

Reviewed By: ManMower, devilhorns

Subscribers: cedric, #reviewers, #committers, zmike

Tags: #efl

Differential Revision: https://phab.enlightenment.org/D6856

src/modules/ecore_evas/engines/wayland/ecore_evas_wayland_common.c
@@ -1899,12 +1899,13 @@ _ecore_evas_wayland_alpha_do(Ecore_Evas *ee, int alpha)
    if (!ee) return;
    if (ee->alpha == alpha) return;
 
+   /* alpha used for transparent as well.
+		ecore_evas_transparent_get() must be valid. */
+   ee->transparent = ee->alpha = alpha;
+
    wdata = ee->engine.data;
-   if (!wdata->sync_done)
-     {
-        ee->alpha = alpha;
-        return;
-     }
+   if (!wdata->sync_done) return;
+
    _ecore_evas_wayland_window_update(ee, wdata, alpha);
 
    _ecore_evas_wl_common_wm_rotation_protocol_set(ee);