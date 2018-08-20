ab3f39e18e00e8466b21a176e86293d7b230f973
Mike Blumenkrantz <zmike@samsung.com>
2018-08-20 17:01:32 -0400

    distcheck script...

--- .ci/ci-make-distcheck.sh
+++ .ci/ci-make-distcheck.sh
@@ -10,7 +10,7 @@ if [ "$DISTRO" != "" ] ; then
   docker exec --env MAKEFLAGS="-j5 -rR" --env EIO_MONITOR_POLL=1 --env CC="ccache gcc" \
     --env CXX="ccache g++" \
     --env CFLAGS="-fdirectives-only" --env CXXFLAGS="-fdirectives-only" \
-    --env LD="ld.gold" $(cat $HOME/cid) make distcheck || \
+    --env LD="ld.gold" $(cat $HOME/cid) bash -c .ci/distcheck.sh || \
     (sudo cat efl-*/_build/sub/src/test-suite.log; false)
 else
   export PATH="/usr/local/opt/ccache/libexec:$(brew --prefix gettext)/bin:$PATH"
