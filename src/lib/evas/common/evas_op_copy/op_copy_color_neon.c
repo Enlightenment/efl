945eeeab330b8f404d6405c0f9a7049ca5ecc8b5
Hermet Park <hermetpark@gmail.com>
2018-09-12 13:35:42 +0900

    evas neon: fix build break.
    there is a grammatic error, just fix it.

--- src/lib/evas/common/evas_op_copy/op_copy_color_neon.c
+++ src/lib/evas/common/evas_op_copy/op_copy_color_neon.c
@@ -6,7 +6,7 @@
 extern void
 pixman_composite_src_n_8888_asm_neon (int32_t   w,
                                       int32_t   h,
-                                      uint32_t *dst
+                                      uint32_t *dst,
                                       int32_t   dst_stride,
                                       uint32_t  src);
 #endif
