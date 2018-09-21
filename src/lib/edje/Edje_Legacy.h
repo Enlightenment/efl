331bd330364a4c7be2aeaddb3bc0629f509737b2
Hermet Park <hermetpark@gmail.com>
2018-09-21 17:59:30 +0900

    edje: add missing eina_deprecated to apis.
    Subscribers: cedric, #reviewers, #committers
    
    Tags: #efl
    
    Differential Revision: https://phab.enlightenment.org/D7090

--- src/lib/edje/Edje_Legacy.h
+++ src/lib/edje/Edje_Legacy.h
@@ -797,7 +797,7 @@ EINA_DEPRECATED EAPI void         edje_extern_object_min_size_set (Evas_Object *
  * This sets the maximum size restriction for the object.
  *
  */
-EAPI void         edje_extern_object_max_size_set (Evas_Object *obj, Evas_Coord maxw, Evas_Coord maxh);
+EINA_DEPRECATED EAPI void         edje_extern_object_max_size_set (Evas_Object *obj, Evas_Coord maxw, Evas_Coord maxh);
 
 /**
  * @deprecated use evas_object_size_hint_aspect_set() instead.
@@ -814,7 +814,7 @@ EAPI void         edje_extern_object_max_size_set (Evas_Object *obj, Evas_Coord 
  * retaining the relative scale of both aspect width and height.
  *
  */
-EAPI void         edje_extern_object_aspect_set   (Evas_Object *obj, Edje_Aspect_Control aspect, Evas_Coord aw, Evas_Coord ah);
+EINA_DEPRECATED EAPI void         edje_extern_object_aspect_set   (Evas_Object *obj, Edje_Aspect_Control aspect, Evas_Coord aw, Evas_Coord ah);
 
 /**
  * @brief Sets the @b EDJ file (and group within it) to load an Edje
