39de8cdc752f4454dc173c42300126c76dc6c2bc
Yeongjong Lee <yj34.lee@samsung.com>
2018-09-27 14:08:39 +0900

    elm_genlist: prevent null pointer access
    Summary: found by coverity
    
    Reviewers: Hermet
    
    Reviewed By: Hermet
    
    Subscribers: cedric, #reviewers, #committers
    
    Tags: #efl
    
    Differential Revision: https://phab.enlightenment.org/D7108

--- src/lib/elementary/elm_genlist.c
+++ src/lib/elementary/elm_genlist.c
@@ -6637,8 +6637,11 @@ _elm_genlist_item_sorted_insert(Eo *obj, Elm_Genlist_Data *sd, const Elm_Genlist
                   if (rel->item->items)
                     {
                        Eina_List *ll = _list_last_recursive(rel->item->items);
-                       eo_rel = ll->data;
-                       rel = efl_data_scope_get(eo_rel, ELM_GENLIST_ITEM_CLASS);
+                       if (ll)
+                         {
+                            eo_rel = ll->data;
+                            rel = efl_data_scope_get(eo_rel, ELM_GENLIST_ITEM_CLASS);
+                         }
                     }
                   sd->items = eina_inlist_append_relative
                       (sd->items, EINA_INLIST_GET(it), EINA_INLIST_GET(rel));
