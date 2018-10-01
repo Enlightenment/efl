Enlightenment/efl

commit 9e01cf2698d5b24f440d696fd57d469cdc5a6b5f

evas image async preload - add option to also make header load async
to date if you use async preload we still load the header
synchronously and this can be horrible especially with generic
loaders. there is no way to farm this off to the preload thread. now
there is. youhave to set it as a skip head load option before doing a
file_set AND you need to issue a preload ... but now it's possible.

@feature

src/modules/evas/engines/gl_common/evas_gl_image.c
@@ -14,8 +14,8 @@ evas_gl_common_image_alloc_ensure(Evas_GL_Image *im)
                                                         im->w, im->h);
    else
 #endif
-   im->im = (RGBA_Image *)evas_cache_image_size_set(&im->im->cache_entry,
-                                                    im->w, im->h);
+     im->im = (RGBA_Image *)evas_cache_image_size_set(&im->im->cache_entry,
+                                                      im->w, im->h);
 }
 
 EAPI void
@@ -148,6 +148,84 @@ _evas_gl_cspace_list_fill(Evas_Engine_GL_Context *gc)
    CS_APPEND(EVAS_COLORSPACE_GRY8);
    CS_APPEND(EVAS_COLORSPACE_AGRY88);
    CS_APPEND(EVAS_COLORSPACE_ARGB8888);
+}
+
+static void
+preload_done(void *data)
+{
+   Evas_GL_Image *im = data;
+
+   if (im->im)
+     {
+        Evas_Colorspace cspace = EVAS_COLORSPACE_ARGB8888;
+
+        if (im->im->cache_entry.cspaces)
+          {
+             Evas_Colorspace cs;
+             unsigned int i;
+             Eina_List *l2;
+             void *ldata;
+
+             cspace = EVAS_COLORSPACE_ARGB8888;
+             for (i = 0;
+                  im->im->cache_entry.cspaces[i] != EVAS_COLORSPACE_ARGB8888;
+                  i++)
+               {
+                  EINA_LIST_FOREACH(im->gc->shared->info.cspaces, l2, ldata)
+                    {
+                       cs = (Evas_Colorspace) (intptr_t) ldata;
+                       if (cs == im->im->cache_entry.cspaces[i])
+                         {
+                            cspace = cs;
+                            goto found_cspace;
+                         }
+                    }
+               }
+found_cspace:
+             if (cspace == EVAS_COLORSPACE_ETC1 && im->gc->shared->info.etc2)
+               cspace = EVAS_COLORSPACE_RGB8_ETC2;
+             im->im->cache_entry.space = cspace;
+          }
+        im->cs.space = cspace;
+        im->orient = EVAS_IMAGE_ORIENT_NONE;
+        im->alpha = im->im->cache_entry.flags.alpha;
+        im->w = im->im->cache_entry.w;
+        im->h = im->im->cache_entry.h;
+     }
+   evas_gl_common_image_preload_unwatch(im);
+}
+
+void
+evas_gl_common_image_preload_watch(Evas_GL_Image *im)
+{
+   Evas_Cache_Target *tg;
+
+   if (!im->im) return;
+   tg = calloc(1, sizeof(Evas_Cache_Target));
+   if (tg)
+     {
+        tg->simple_cb = preload_done;
+        tg->simple_data = im;
+        im->im->cache_entry.targets =  (Evas_Cache_Target *)
+          eina_inlist_append(EINA_INLIST_GET(im->im->cache_entry.targets),
+                             EINA_INLIST_GET(tg));
+     }
+}
+
+void
+evas_gl_common_image_preload_unwatch(Evas_GL_Image *im)
+{
+   Eina_Inlist *l2;
+   Evas_Cache_Target *tg;
+
+   if (!im->im) return;
+   EINA_INLIST_FOREACH_SAFE(im->im->cache_entry.targets, l2, tg)
+     {
+        if ((tg->simple_cb != preload_done) || (tg->simple_data != im))
+          continue;
+        tg->delete_me = EINA_TRUE;
+        break;
+     }
 }
 
 Evas_GL_Image *
@@ -690,6 +768,8 @@ evas_gl_common_image_free(Evas_GL_Image *im)
    if (im->references > 0) return;
    evas_gl_common_context_flush(im->gc);
 
+   evas_gl_common_image_preload_unwatch(im);
+
    if (im->scaled.origin)
      {
         evas_gl_common_image_free(im->scaled.origin);
@@ -791,11 +871,16 @@ evas_gl_common_image_update(Evas_Engine_GL_Context *gc, Evas_GL_Image *im)
 {
    Image_Entry *ie;
    if (!im->im) return;
-   evas_gl_common_image_alloc_ensure(im);
 
    // alloc ensure can change im->im, so only get the local variable later.
    ie = &im->im->cache_entry;
-
+   if (!im->tex)
+     {
+        if (ie->preload) return;
+        im->w = ie->w;
+        im->h = ie->h;
+     }
+   evas_gl_common_image_alloc_ensure(im);
 /*
    if ((im->cs.space == EVAS_COLORSPACE_YCBCR422P601_PL) ||
        (im->cs.space == EVAS_COLORSPACE_YCBCR422P709_PL))

src/bin/evas/evas_cserve2_cache.c
@@ -28,19 +28,22 @@ typedef struct _Font_Source Font_Source;
 typedef struct _Font_Entry Font_Entry;
 
 static const Evas_Image_Load_Opts empty_lo = {
-  { 0, 0, 0, 0 },
-  {
-    0, 0, 0, 0,
-    0, 0,
-    0,
-    0
-  },
-  0.0,
-  0, 0,
-  0,
-  0,
+   {
+        { 0, 0, 0, 0 },
+        {
+           0, 0, 0, 0,
+           0, 0,
+           0,
+           0
+        },
+      0.0,
+      0, 0,
+      0,
+      0,
 
-  EINA_FALSE
+      EINA_FALSE
+   },
+   EINA_FALSE
 };
 
 typedef enum {
@@ -761,13 +764,13 @@ _load_request_build(Image_Entry *ientry, int *bufsize)
    msg.alpha = fd->alpha;
 
    // NOTE: Not passing scale_load options
-   msg.opts.w = idata->opts.w;
-   msg.opts.h = idata->opts.h;
-   msg.opts.region = idata->opts.region;
-   msg.opts.scale_down_by = idata->opts.scale_down_by;
-   msg.opts.dpi = idata->opts.dpi;
-   msg.opts.degree = idata->opts.degree;
-   msg.opts.orientation = idata->opts.orientation;
+   msg.opts.emile.w = idata->opts.emile.w;
+   msg.opts.emile.h = idata->opts.emile.h;
+   msg.opts.emile.region = idata->opts.emile.region;
+   msg.opts.emile.scale_down_by = idata->opts.emile.scale_down_by;
+   msg.opts.emile.dpi = idata->opts.emile.dpi;
+   msg.opts.emile.degree = idata->opts.emile.degree;
+   msg.opts.emile.orientation = idata->opts.emile.orientation;
 
    msg.shm.mmap_offset = cserve2_shm_map_offset_get(ientry->shm);
    msg.shm.image_offset = cserve2_shm_offset_get(ientry->shm);
@@ -798,9 +801,9 @@ _load_request_build(Image_Entry *ientry, int *bufsize)
 static inline Eina_Bool
 _scaling_needed(Image_Data *idata, Slave_Msg_Image_Loaded *resp)
 {
-   return (((idata->opts.scale_load.dst_w) && (idata->opts.scale_load.dst_h)) &&
-           ((idata->opts.scale_load.dst_w != resp->w) ||
-            (idata->opts.scale_load.dst_h != resp->h)));
+   return (((idata->opts.emile.scale_load.dst_w) && (idata->opts.emile.scale_load.dst_h)) &&
+           ((idata->opts.emile.scale_load.dst_w != resp->w) ||
+            (idata->opts.emile.scale_load.dst_h != resp->h)));
 }
 
 static int
@@ -838,20 +841,20 @@ _scaling_do(Shm_Handle *scale_shm, Image_Data *idata, Image_Entry *original)
 
    DBG("Scaling image ([%dx%d]:[%d,%d:%dx%d] --> [%d,%d:%dx%d])",
        orig_idata->w, orig_idata->h,
-       idata->opts.scale_load.src_x, idata->opts.scale_load.src_y,
-       idata->opts.scale_load.src_w, idata->opts.scale_load.src_h,
+       idata->opts.emile.scale_load.src_x, idata->opts.emile.scale_load.src_y,
+       idata->opts.emile.scale_load.src_w, idata->opts.emile.scale_load.src_h,
        0, 0,
-       idata->opts.scale_load.dst_w, idata->opts.scale_load.dst_h);
+       idata->opts.emile.scale_load.dst_w, idata->opts.emile.scale_load.dst_h);
 
    idata->alpha = orig_idata->alpha;
    cserve2_rgba_image_scale_do(
             src_data, orig_idata->w, orig_idata->h,
             dst_data,
-            idata->opts.scale_load.src_x, idata->opts.scale_load.src_y,
-            idata->opts.scale_load.src_w, idata->opts.scale_load.src_h,
+            idata->opts.emile.scale_load.src_x, idata->opts.emile.scale_load.src_y,
+            idata->opts.emile.scale_load.src_w, idata->opts.emile.scale_load.src_h,
             0, 0,
-            idata->opts.scale_load.dst_w, idata->opts.scale_load.dst_h,
-            idata->alpha, idata->opts.scale_load.smooth);
+            idata->opts.emile.scale_load.dst_w, idata->opts.emile.scale_load.dst_h,
+            idata->alpha, idata->opts.emile.scale_load.smooth);
 
    cserve2_shm_unmap(original->shm);
    cserve2_shm_unmap(scale_shm);
@@ -864,8 +867,8 @@ _scaling_prepare_and_do(Image_Entry *ientry, Image_Data *idata)
 {
    Shm_Handle *scale_shm;
 
-   scale_shm = cserve2_shm_request("img", idata->opts.scale_load.dst_w
-                                   * idata->opts.scale_load.dst_h * 4);
+   scale_shm = cserve2_shm_request("img", idata->opts.emile.scale_load.dst_w
+                                   * idata->opts.emile.scale_load.dst_h * 4);
 
    if (!scale_shm)
      return -1;
@@ -881,8 +884,8 @@ _scaling_prepare_and_do(Image_Entry *ientry, Image_Data *idata)
    cserve2_shared_string_del(idata->shm_id);
    ientry->shm = scale_shm;
    idata->shm_id = 0;
-   idata->w = idata->opts.scale_load.dst_w;
-   idata->h = idata->opts.scale_load.dst_h;
+   idata->w = idata->opts.emile.scale_load.dst_w;
+   idata->h = idata->opts.emile.scale_load.dst_h;
 
    return 0;
 }
@@ -938,14 +941,14 @@ _image_key_set(unsigned int file_id, const Evas_Image_Load_Opts *opts,
 
    snprintf(buf, size,
             "%u:%0.3f:%dx%d:%d:%d,%d+%dx%d:!([%d,%d:%dx%d]-[%dx%d:%d]):%d:%d",
-            file_id, opts->dpi, opts->w, opts->h,
-            opts->scale_down_by, opts->region.x, opts->region.y,
-            opts->region.w, opts->region.h,
-            opts->scale_load.src_x, opts->scale_load.src_y,
-            opts->scale_load.src_w, opts->scale_load.src_h,
-            opts->scale_load.dst_w, opts->scale_load.dst_h,
-            opts->scale_load.smooth, opts->degree,
-            opts->orientation);
+            file_id, opts->emile.dpi, opts->emile.w, opts->emile.h,
+            opts->emile.scale_down_by, opts->emile.region.x, opts->emile.region.y,
+            opts->emile.region.w, opts->emile.region.h,
+            opts->emile.scale_load.src_x, opts->emile.scale_load.src_y,
+            opts->emile.scale_load.src_w, opts->emile.scale_load.src_h,
+            opts->emile.scale_load.dst_w, opts->emile.scale_load.dst_h,
+            opts->emile.scale_load.smooth, opts->emile.degree,
+            opts->emile.orientation);
 }
 
 static unsigned int
@@ -975,12 +978,12 @@ _evas_image_load_opts_empty(Evas_Image_Load_Opts *lo)
 {
    if (!lo) return EINA_TRUE;
 
-   return ((lo->scale_down_by == 0)
-           && (lo->dpi == 0.0)
-           && (lo->w == 0) && (lo->h == 0)
-           && (lo->region.x == 0) && (lo->region.y == 0)
-           && (lo->region.w == 0) && (lo->region.h == 0)
-           && (lo->orientation == 0));
+   return ((lo->emile.scale_down_by == 0)
+           && (lo->emile.dpi == 0.0)
+           && (lo->emile.w == 0) && (lo->emile.h == 0)
+           && (lo->emile.region.x == 0) && (lo->emile.region.y == 0)
+           && (lo->emile.region.w == 0) && (lo->emile.region.h == 0)
+           && (lo->emile.orientation == 0));
 }
 
 static void
@@ -995,17 +998,17 @@ _file_hkey_get(char *buf, size_t sz, const char *path, const char *key,
      snprintf(buf, sz, "%s:%s", path, key);
    else
      {
-        if (lo->orientation)
+        if (lo->emile.orientation)
           {
              snprintf(buf, sz, "%s:%s//@/%d/%f/%dx%d/%d+%d.%dx%d",
-                      path, key, lo->scale_down_by, lo->dpi, lo->w, lo->h,
-                      lo->region.x, lo->region.y, lo->region.w, lo->region.h);
+                      path, key, lo->emile.scale_down_by, lo->emile.dpi, lo->emile.w, lo->emile.h,
+                      lo->emile.region.x, lo->emile.region.y, lo->emile.region.w, lo->emile.region.h);
           }
         else
           {
              snprintf(buf, sz, "%s:%s//@/%d/%f/%dx%d/%d+%d.%dx%d/o",
-                      path, key, lo->scale_down_by, lo->dpi, lo->w, lo->h,
-                      lo->region.x, lo->region.y, lo->region.w, lo->region.h);
+                      path, key, lo->emile.scale_down_by, lo->emile.dpi, lo->emile.w, lo->emile.h,
+                      lo->emile.region.x, lo->emile.region.y, lo->emile.region.w, lo->emile.region.h);
           }
      }
 }
@@ -1016,15 +1019,15 @@ _file_id_free(File_Data *fd)
    Evas_Image_Load_Opts lo = empty_lo;
    char buf[4096];
 
-   lo.region.x = fd->lo.region.x;
-   lo.region.y = fd->lo.region.y;
-   lo.region.w = fd->lo.region.w;
-   lo.region.h = fd->lo.region.h;
-   lo.dpi = fd->lo.dpi;
-   lo.w = fd->lo.w;
-   lo.h = fd->lo.h;
-   lo.scale_down_by = fd->lo.scale_down_by;
-   lo.orientation = fd->lo.orientation;
+   lo.emile.region.x = fd->lo.region.x;
+   lo.emile.region.y = fd->lo.region.y;
+   lo.emile.region.w = fd->lo.region.w;
+   lo.emile.region.h = fd->lo.region.h;
+   lo.emile.dpi = fd->lo.dpi;
+   lo.emile.w = fd->lo.w;
+   lo.emile.h = fd->lo.h;
+   lo.emile.scale_down_by = fd->lo.scale_down_by;
+   lo.emile.orientation = fd->lo.orientation;
 
    _file_hkey_get(buf, sizeof(buf), cserve2_shared_string_get(fd->path),
                   cserve2_shared_string_get(fd->key), &lo);
@@ -1629,24 +1632,24 @@ _image_entry_new(Client *client, int rid,
    ientry->base.type = CSERVE2_IMAGE_DATA;
    if (opts)
      {
-        idata->opts.dpi = opts->dpi;
-        idata->opts.w = opts->w;
-        idata->opts.h = opts->h;
-        idata->opts.scale_down_by = opts->scale_down_by;
-        idata->opts.region.x = opts->region.x;
-        idata->opts.region.y = opts->region.y;
-        idata->opts.region.w = opts->region.w;
-        idata->opts.region.h = opts->region.h;
-        idata->opts.scale_load.src_x = opts->scale_load.src_x;
-        idata->opts.scale_load.src_y = opts->scale_load.src_y;
-        idata->opts.scale_load.src_w = opts->scale_load.src_w;
-        idata->opts.scale_load.src_h = opts->scale_load.src_h;
-        idata->opts.scale_load.dst_w = opts->scale_load.dst_w;
-        idata->opts.scale_load.dst_h = opts->scale_load.dst_h;
-        idata->opts.scale_load.smooth = opts->scale_load.smooth;
-        idata->opts.scale_load.scale_hint = opts->scale_load.scale_hint;
-        idata->opts.degree = opts->degree;
-        idata->opts.orientation = opts->orientation;
+        idata->opts.emile.dpi = opts->emile.dpi;
+        idata->opts.emile.w = opts->emile.w;
+        idata->opts.emile.h = opts->emile.h;
+        idata->opts.emile.scale_down_by = opts->emile.scale_down_by;
+        idata->opts.emile.region.x = opts->emile.region.x;
+        idata->opts.emile.region.y = opts->emile.region.y;
+        idata->opts.emile.region.w = opts->emile.region.w;
+        idata->opts.emile.region.h = opts->emile.region.h;
+        idata->opts.emile.scale_load.src_x = opts->emile.scale_load.src_x;
+        idata->opts.emile.scale_load.src_y = opts->emile.scale_load.src_y;
+        idata->opts.emile.scale_load.src_w = opts->emile.scale_load.src_w;
+        idata->opts.emile.scale_load.src_h = opts->emile.scale_load.src_h;
+        idata->opts.emile.scale_load.dst_w = opts->emile.scale_load.dst_w;
+        idata->opts.emile.scale_load.dst_h = opts->emile.scale_load.dst_h;
+        idata->opts.emile.scale_load.smooth = opts->emile.scale_load.smooth;
+        idata->opts.emile.scale_load.scale_hint = opts->emile.scale_load.scale_hint;
+        idata->opts.emile.degree = opts->emile.degree;
+        idata->opts.emile.orientation = opts->emile.orientation;
      }
    idata->valid = EINA_FALSE;
    idata->file_id = ref->entry->id;
@@ -2622,15 +2625,15 @@ cserve2_cache_file_open(Client *client, unsigned int client_file_id,
    fd->path = cserve2_shared_string_add(path);
    fd->key = cserve2_shared_string_add(key);
    if (!lo) lo = (Evas_Image_Load_Opts *) &empty_lo;
-   fd->lo.region.x = lo->region.x;
-   fd->lo.region.y = lo->region.y;
-   fd->lo.region.w = lo->region.w;
-   fd->lo.region.h = lo->region.h;
-   fd->lo.dpi = lo->dpi;
-   fd->lo.w = lo->w;
-   fd->lo.h = lo->h;
-   fd->lo.scale_down_by = lo->scale_down_by;
-   fd->lo.orientation = lo->orientation;
+   fd->lo.region.x = lo->emile.region.x;
+   fd->lo.region.y = lo->emile.region.y;
+   fd->lo.region.w = lo->emile.region.w;
+   fd->lo.region.h = lo->emile.region.h;
+   fd->lo.dpi = lo->emile.dpi;
+   fd->lo.w = lo->emile.w;
+   fd->lo.h = lo->emile.h;
+   fd->lo.scale_down_by = lo->emile.scale_down_by;
+   fd->lo.orientation = lo->emile.orientation;
    fd->refcount = 1;
    fd->id = file_id;
 
@@ -2698,23 +2701,23 @@ _cserve2_cache_fast_scaling_check(Client *client, Image_Entry *ientry,
    idata = _image_data_find(ENTRYID(ientry));
    if (!idata) return -1;
 
-   dst_w = idata->opts.scale_load.dst_w;
-   dst_h = idata->opts.scale_load.dst_h;
+   dst_w = idata->opts.emile.scale_load.dst_w;
+   dst_h = idata->opts.emile.scale_load.dst_h;
 
    // Copy opts w/o scaling
    memset(&unscaled, 0, sizeof(unscaled));
-   unscaled.dpi = idata->opts.dpi;
-   unscaled.w = idata->opts.w;
-   unscaled.h = idata->opts.h;
-   unscaled.scale_down_by = idata->opts.scale_down_by;
-   unscaled.region.x = idata->opts.region.x;
-   unscaled.region.y = idata->opts.region.y;
-   unscaled.region.w = idata->opts.region.w;
-   unscaled.region.h = idata->opts.region.h;
-   unscaled.scale_load.scale_hint = 0;
-   unscaled.degree = idata->opts.degree;
-   unscaled.orientation = idata->opts.orientation;
-   unscaled.scale_load.smooth = idata->opts.scale_load.smooth;
+   unscaled.emile.dpi = idata->opts.emile.dpi;
+   unscaled.emile.w = idata->opts.emile.w;
+   unscaled.emile.h = idata->opts.emile.h;
+   unscaled.emile.scale_down_by = idata->opts.emile.scale_down_by;
+   unscaled.emile.region.x = idata->opts.emile.region.x;
+   unscaled.emile.region.y = idata->opts.emile.region.y;
+   unscaled.emile.region.w = idata->opts.emile.region.w;
+   unscaled.emile.region.h = idata->opts.emile.region.h;
+   unscaled.emile.scale_load.scale_hint = 0;
+   unscaled.emile.degree = idata->opts.emile.degree;
+   unscaled.emile.orientation = idata->opts.emile.orientation;
+   unscaled.emile.scale_load.smooth = idata->opts.emile.scale_load.smooth;
 
 try_again:
    image_id = _image_opts_id_get(idata->file_id, &unscaled, buf, sizeof(buf));
@@ -2725,17 +2728,17 @@ try_again:
         if (!orig_data || !orig_entry) return -1;
 
         DBG("Found original image in hash: %d,%d:%dx%d -> %dx%d shm %p",
-            orig_data->opts.scale_load.src_x, orig_data->opts.scale_load.src_y,
-            orig_data->opts.scale_load.src_w, orig_data->opts.scale_load.src_h,
-            orig_data->opts.scale_load.dst_w, orig_data->opts.scale_load.dst_h,
+            orig_data->opts.emile.scale_load.src_x, orig_data->opts.emile.scale_load.src_y,
+            orig_data->opts.emile.scale_load.src_w, orig_data->opts.emile.scale_load.src_h,
+            orig_data->opts.emile.scale_load.dst_w, orig_data->opts.emile.scale_load.dst_h,
             orig_entry->shm);
         goto do_scaling;
      }
 
-   if (first_attempt && unscaled.scale_load.smooth)
+   if (first_attempt && unscaled.emile.scale_load.smooth)
      {
         first_attempt = EINA_FALSE;
-        unscaled.scale_load.smooth = 0;
+        unscaled.emile.scale_load.smooth = 0;
         goto try_again;
      }
 
@@ -2749,14 +2752,14 @@ try_again:
         id = _image_data_find(ENTRYID(i));
         if (!id) continue;
 
-        if (id->opts.w && id->opts.h &&
-            (!id->opts.scale_load.dst_w &&
-             !id->opts.scale_load.dst_h))
+        if (id->opts.emile.w && id->opts.emile.h &&
+            (!id->opts.emile.scale_load.dst_w &&
+             !id->opts.emile.scale_load.dst_h))
           {
              DBG("Found image in list: %d,%d:%dx%d -> %dx%d shm %p",
-                 id->opts.scale_load.src_x, id->opts.scale_load.src_y,
-                 id->opts.scale_load.src_w, id->opts.scale_load.src_h,
-                 id->opts.scale_load.dst_w, id->opts.scale_load.dst_h,
+                 id->opts.emile.scale_load.src_x, id->opts.emile.scale_load.src_y,
+                 id->opts.emile.scale_load.src_w, id->opts.emile.scale_load.src_h,
+                 id->opts.emile.scale_load.dst_w, id->opts.emile.scale_load.dst_h,
                  i->shm);
              if (i->base.request || !i->shm) continue; // Not loaded yet
              orig_entry = i;
@@ -2905,7 +2908,7 @@ cserve2_cache_image_entry_create(Client *client, int rid,
      return -1;
    fentry->images = eina_list_append(fentry->images, ientry);
 
-   if (opts && opts->scale_load.dst_w && opts->scale_load.dst_h)
+   if (opts && opts->emile.scale_load.dst_w && opts->emile.scale_load.dst_h)
      {
         if (!_cserve2_cache_fast_scaling_check(client, ientry, client_file_id))
           return 0;

src/bin/evas/evas_cserve2_main.c
@@ -165,21 +165,21 @@ _cserve2_client_open(Client *client)
         // FIXME: Check message size first?
 
         DBG("Load Options:");
-        DBG("\tdpi: %03.1f", opts->dpi);
-        DBG("\tsize: %dx%d", opts->w, opts->h);
-        DBG("\tscale down: %d", opts->scale_down_by);
+        DBG("\tdpi: %03.1f", opts->emile.dpi);
+        DBG("\tsize: %dx%d", opts->emile.w, opts->emile.h);
+        DBG("\tscale down: %d", opts->emile.scale_down_by);
         DBG("\tregion: %d,%d + %dx%d",
-            opts->region.x, opts->region.y, opts->region.w, opts->region.h);
+            opts->emile.region.x, opts->emile.region.y, opts->emile.region.w, opts->emile.region.h);
         DBG("\toriginal image's source coord: %d,%d",
-            opts->scale_load.src_x, opts->scale_load.src_y);
+            opts->emile.scale_load.src_x, opts->emile.scale_load.src_y);
         DBG("\toriginal image size: %dx%d",
-            opts->scale_load.src_w, opts->scale_load.src_h);
+            opts->emile.scale_load.src_w, opts->emile.scale_load.src_h);
         DBG("\tscale size: %dx%d",
-            opts->scale_load.dst_w, opts->scale_load.dst_h);
-        DBG("\tscale smooth: %d", opts->scale_load.smooth);
-        DBG("\tscale hint: %d", opts->scale_load.scale_hint);
-        DBG("\tdegree: %d", opts->degree);
-        DBG("\torientation: %d", opts->orientation);
+            opts->emile.scale_load.dst_w, opts->emile.scale_load.dst_h);
+        DBG("\tscale smooth: %d", opts->emile.scale_load.smooth);
+        DBG("\tscale hint: %d", opts->emile.scale_load.scale_hint);
+        DBG("\tdegree: %d", opts->emile.degree);
+        DBG("\torientation: %d", opts->emile.orientation);
 
         cserve2_cache_image_entry_create(client, msg->base.rid,
                                          msg->file_id, msg->image_id,

src/bin/evas/evas_cserve2_shm_debug.c
@@ -515,20 +515,20 @@ _images_all_print_full(void)
                id->valid ? "YES" : "NO");
         printf("Shm Path:       '%s'\n",
                id->shm_id ? _shared_string_get(id->shm_id) : "");
-        printf("LoadOpts: width          %d\n", id->opts.w);
-        printf("          height         %d\n", id->opts.h);
-        printf("          degree         %d\n", id->opts.degree);
-        printf("          scale_down_by  %d\n", id->opts.scale_down_by);
-        if (!EINA_DBL_CMP(id->opts.dpi, 0.0))
-          printf("          dpi            %.2f\n", id->opts.dpi);
+        printf("LoadOpts: width          %d\n", id->opts.emile.w);
+        printf("          height         %d\n", id->opts.emile.h);
+        printf("          degree         %d\n", id->opts.emile.degree);
+        printf("          scale_down_by  %d\n", id->opts.emile.scale_down_by);
+        if (!EINA_DBL_CMP(id->opts.emile.dpi, 0.0))
+          printf("          dpi            %.2f\n", id->opts.emile.dpi);
         else
           printf("          dpi            0\n");
-        printf("          orientation    %s\n", id->opts.orientation ? "YES" : "NO");
+        printf("          orientation    %s\n", id->opts.emile.orientation ? "YES" : "NO");
         printf("          region         (%d,%d) %dx%d\n",
-               id->opts.region.x, id->opts.region.y,
-               id->opts.region.w, id->opts.region.h);
+               id->opts.emile.region.x, id->opts.emile.region.y,
+               id->opts.emile.region.w, id->opts.emile.region.h);
 
-        switch (id->opts.scale_load.scale_hint)
+        switch (id->opts.emile.scale_load.scale_hint)
           {
            case EVAS_IMAGE_SCALE_HINT_NONE:
              scale_hint = "EVAS_IMAGE_SCALE_HINT_NONE"; break;
@@ -541,14 +541,14 @@ _images_all_print_full(void)
           }
 
         printf("          scale src      (%d,%d) %dx%d\n",
-               id->opts.scale_load.src_x, id->opts.scale_load.src_y,
-               id->opts.scale_load.src_w, id->opts.scale_load.src_h);
+               id->opts.emile.scale_load.src_x, id->opts.emile.scale_load.src_y,
+               id->opts.emile.scale_load.src_w, id->opts.emile.scale_load.src_h);
         printf("          scale dst      %dx%d\n",
-               id->opts.scale_load.dst_w, id->opts.scale_load.dst_h);
+               id->opts.emile.scale_load.dst_w, id->opts.emile.scale_load.dst_h);
         printf("          scale smooth   %s\n",
-               id->opts.scale_load.smooth ? "YES" : "NO");
+               id->opts.emile.scale_load.smooth ? "YES" : "NO");
         printf("          scale hint     %s (%d)\n",
-               scale_hint, id->opts.scale_load.scale_hint);
+               scale_hint, id->opts.emile.scale_load.scale_hint);
 
         fd = (File_Data *)
               _shared_index_item_get_by_id(sf_files, sizeof(*fd), id->file_id);

src/bin/evas/evas_cserve2_slave.c
@@ -475,8 +475,8 @@ image_load(const char *file, const char *key, const char *shmfile,
      }
 
    memset(&property, 0, sizeof (property));
-   property.w = params->opts.w; // Should we rather use params->w ?
-   property.h = params->opts.h;
+   property.w = params->opts.emile.w; // Should we rather use params->w ?
+   property.h = params->opts.emile.h;
 
    skey = eina_stringshare_add(key);
    loader_data = _image_file_open(fd, skey, opts, module, &property, &animated, &funcs);
@@ -544,15 +544,15 @@ handle_image_open(int wfd, void *params)
    size_t resp_size;
 
    memset(&load_opts, 0, sizeof(load_opts));
-   load_opts.region.x = msg->lo.region.x;
-   load_opts.region.y = msg->lo.region.y;
-   load_opts.region.w = msg->lo.region.w;
-   load_opts.region.h = msg->lo.region.h;
-   load_opts.dpi = msg->lo.dpi;
-   load_opts.w = msg->lo.w;
-   load_opts.h = msg->lo.h;
-   load_opts.scale_down_by = msg->lo.scale_down_by;
-   load_opts.orientation = msg->lo.orientation;
+   load_opts.emile.region.x = msg->lo.region.x;
+   load_opts.emile.region.y = msg->lo.region.y;
+   load_opts.emile.region.w = msg->lo.region.w;
+   load_opts.emile.region.h = msg->lo.region.h;
+   load_opts.emile.dpi = msg->lo.dpi;
+   load_opts.emile.w = msg->lo.w;
+   load_opts.emile.h = msg->lo.h;
+   load_opts.emile.scale_down_by = msg->lo.scale_down_by;
+   load_opts.emile.orientation = msg->lo.orientation;
 
    file = (const char *) (msg + 1);
    key = file + strlen(file) + 1;

src/lib/efl/interfaces/efl_image_load.eo
@@ -182,6 +182,21 @@ interface Efl.Image.Load ()
             ]]
          }
       }
+      @property load_skip_header {
+         [[Initial load should skip header check and leave it all to data load
+
+           If this is true, then future loads of images will defer header
+           loading to a preload stage and/or data load later on rather than
+           at the start when the load begins (e.g. when file is set).
+         ]]
+         set {
+            [[Set the skip header state for susbsequent loads of a file. ]]
+         }
+         get {}
+         values {
+            skip: bool; [[Will be true if header is to be skipped. ]]
+         }
+      }
    }
    events {
       load,done; [[Called when he image was loaded]]

src/lib/evas/Evas_Legacy.h
@@ -4068,6 +4068,31 @@ EAPI void evas_object_image_load_scale_down_set(Evas_Object *obj, int scale_down
 EAPI int evas_object_image_load_scale_down_get(const Evas_Object *obj);
 
 /**
+ * @brief Set a load option to skip initial header load and defer to preload
+ * 
+ * This is meant to be used in conjunction with evas_object_image_file_set()
+ * and evas_object_image_preload() by deferring any header loading until
+ * a evas_object_image_preload() is issued making the file file set simply
+ * set up the file to refer to without any validation of its type or
+ * file existence or even inspecting the image header to get size or alpha
+ * channel flags etc. All of this will then be done as part of the preload
+ * stage.
+ * 
+ * @since 1.19
+ */
+EAPI void evas_object_image_load_head_skip_set(Evas_Object *obj, Eina_Bool skip);
+
+/**
+ * @breif Get the load option to skip header loads before preload
+ * 
+ * This gets the heade skip value set by evas_object_image_load_head_skip_set()
+ * 
+ * @see evas_object_image_load_head_skip_set
+ * @since 1.19
+ */
+EAPI Eina_Bool evas_object_image_load_head_skip_get(const Evas_Object *obj);
+
+/**
  * @brief Retrieves a number representing any error that occurred during the
  * last loading of the given image object's source image.
  *

commit 431c8cc09046ddb60f10f11d024816c6a1ec517a

evas gl: fix invalid image size.
Summary:
When we reset of texture for a valid object,
this object cache size become -1 x -1 with null texture.

Later, we reset a new texture of the object,
Its texture size could be -1 x -1.
That brings to incorrect result drawing.

Can't see any points of using cache size there.

This bug was introduced by 9e01cf2698d5b24f440d696fd57d469cdc5a6b5f

@fix

Reviewers: #committers, raster

Subscribers: cedric, #reviewers, #committers

Tags: #efl

Differential Revision: https://phab.enlightenment.org/D7077

src/modules/evas/engines/gl_common/evas_gl_image.c
@@ -763,12 +763,8 @@ evas_gl_common_image_update(Evas_Engine_GL_Context *gc, Evas_GL_Image *im)
    if (!im->im) return;
 
    ie = &im->im->cache_entry;
-   if (!im->tex)
-     {
-        if (ie->preload) return;
-        im->w = ie->w;
-        im->h = ie->h;
-     }
+   if (!im->tex && ie->preload) return;
+
    evas_gl_common_image_alloc_ensure(im);
    // alloc ensure can change im->im, so only get the local variable later.
    ie = &im->im->cache_entry;