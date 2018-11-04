be7d76ece63844f1967ee12d042c920a8540ab4a
Daniel Kolesa <d.kolesa@samsung.com>
2018-11-04 18:56:54 +0100

    eolian_aux: add initial eolian_aux APIs
    eolian_aux is a set of auxiliary APIs for eolian that build on top
    of the existing eolian APIs but do not belong in the main library.

--- src/tests/eolian/eolian_suite.h
+++ src/tests/eolian/eolian_suite.h
@@ -4,6 +4,7 @@
 #include <check.h>
 #include "../efl_check.h"
 void eolian_parsing_test(TCase *tc);
+void eolian_aux_test(TCase *tc);
 void eolian_static_test(TCase *tc);
 void eolian_generation_test(TCase *tc);
 
