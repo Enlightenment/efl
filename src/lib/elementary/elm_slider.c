84f82be847e0c56940492ef66efa69a03e4e9a43
Daniel Zaoui <daniel.zaoui@samsung.com>
2018-11-05 14:03:15 +0900

    Slider: fix callback invocation
    Summary:
    efl_event_callback_legacy_call already invokes legacy and non-legacy
    callbacks. Therefore, there is no reason to call
    efl_event_callback_call as well.
    
    Reviewers: eagleeye, Hermet
    
    Reviewed By: Hermet
    
    Subscribers: cedric, #reviewers, #committers
    
    Tags: #efl
    
    Differential Revision: https://phab.enlightenment.org/D7237

--- src/lib/elementary/elm_slider.c
+++ src/lib/elementary/elm_slider.c
@@ -630,7 +630,6 @@ _elm_slider_val_fetch(Evas_Object *obj, Elm_Slider_Data *pd, Eina_Bool user_even
         id->intvl_from = val;
         if (user_event)
           {
-             efl_event_callback_call(obj, EFL_UI_SLIDER_EVENT_CHANGED, NULL);
              efl_event_callback_legacy_call(obj, EFL_UI_SLIDER_EVENT_CHANGED, NULL);
              ecore_timer_del(pd->delay);
              pd->delay = ecore_timer_add(SLIDER_DELAY_CHANGED_INTERVAL, _delay_change, obj);
@@ -642,7 +641,6 @@ _elm_slider_val_fetch(Evas_Object *obj, Elm_Slider_Data *pd, Eina_Bool user_even
         id->intvl_to = val2;
         if (user_event)
           {
-             efl_event_callback_call(obj, EFL_UI_SLIDER_EVENT_CHANGED, NULL);
              efl_event_callback_legacy_call(obj, EFL_UI_SLIDER_EVENT_CHANGED, NULL);
              ecore_timer_del(pd->delay);
              pd->delay = ecore_timer_add(SLIDER_DELAY_CHANGED_INTERVAL, _delay_change, obj);
