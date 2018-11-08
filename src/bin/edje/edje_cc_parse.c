24b345bb53e55171088538c470ce356caf62f3f8
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2018-11-08 14:30:54 +0000

    edje_cc_parse - warn - make buffers bigger to avoid truncation

--- src/bin/edje/edje_cc_parse.c
+++ src/bin/edje/edje_cc_parse.c
@@ -1015,7 +1015,7 @@ get_verbatim_line2(void)
 void
 compile(void)
 {
-   char buf[4096], buf2[4096];
+   char buf[4096 + 4096 + 4096], buf2[4096];
    Eina_Tmpstr *tmpn;
    int fd;
    off_t size;
