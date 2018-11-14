bd81e38434660314249036170f84e3fc7a6904ea
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2018-11-14 16:23:49 +0000

    meson- evas generic loader symlinks - fix to point to right place
    symlinks for loaders and their extensions first pointers to an abs
    pathname that was bad for destdirs with packaging and they also linked
    to the wrong file - they had an extra . at the end due to link script
    not using basename right. this fixes both of these to now point
    relative (locally) and remove the . t the end so they work again.

--- meson/evas_loader_conf.sh
+++ meson/evas_loader_conf.sh
@@ -3,7 +3,7 @@
 original_loader=$1
 filename="$(basename $original_loader)"
 ext="${ext##*.}"
-original_name="$(basename $original_loader $ext)"
+original_name="$(basename $original_loader .$ext)"
 loader_dir="$(dirname $original_loader)"
 loader="$(basename $original_loader)"
 loader_name="$(echo $original_name | cut -f 1 -d '.')"
@@ -14,5 +14,5 @@ if [ ${original_loader: -3} == "$ext" ]
 fi
 
 for x in "${@:2}"; do
-  ln -sf "$DESTDIR"/"$loader_dir"/"$original_name" "$DESTDIR"/"$loader_dir"/"$loader_name"."$x"
+  ln -sf "$original_name" "$DESTDIR"/"$loader_dir"/"$loader_name"."$x"
 done
