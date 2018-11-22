9315c220d55be9cccb05192cf53628d3548884cb
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2018-11-22 16:48:56 +0000

    elm perf tool - bring one back to efl
    expedite is basically dead. because it's out of tree and because it
    was ported to eo too early... but it's also not a great tool given it
    requires manual addition of engines and can't test elm stuff (complex
    widgets).
    
    so this is an elm based performance tool. i've added some tests (taken
    from expedite and modified a bit). to get the ball rolling. the idea
    is to add all the important core tests (rects, images, text, video
    etc.) and then add widget/smart object like tests that go beyond what
    expedite did. the problem with these tests is they are dependent on
    theme. there isn't really any way around that... but i might
    categorize tests in the end.
    
    @feature

---
+++ src/bin/elementary/perf_list.c
@@ -0,0 +1,6 @@
+#include "perf_test_01.c"
+#include "perf_test_02.c"
+#include "perf_test_03.c"
+#include "perf_test_04.c"
+#include "perf_test_05.c"
+#include "perf_test_06.c"
---
+++ src/bin/elementary/perf_test_01.c
@@ -0,0 +1,45 @@
+#ifdef T2
+TPROT(01);
+#endif
+#ifdef T1
+{ TFUN(01), "Rectangles (Few)", 0.2 },
+#endif
+#if !defined(T1) && !defined(T2)
+# include "perf.h"
+static Evas_Object *objs[NUM_FEW];
+
+TST(01, init) (Evas *e) {
+   Evas_Object *o;
+   int i;
+
+   srnd();
+   for (i = 0; i < NUM_FEW; i++)
+     {
+        objs[i] = o = evas_object_rectangle_add(e);
+        cleanup_add(o);
+        evas_object_color_set
+          (o, rnd() & 0x7f, rnd() & 0x7f, rnd() & 0x7f, 0x80);
+        evas_object_pass_events_set(o, EINA_TRUE);
+        evas_object_show(o);
+     }
+}
+
+TST(01, tick) (Evas *e EINA_UNUSED, double f, Evas_Coord win_w, Evas_Coord win_h) {
+   int i;
+   Evas_Coord x, y, w, h, w0, h0;
+
+   for (i = 0; i < NUM_FEW; i++)
+     {
+        Evas_Object *o = objs[i];
+        w0 = 120;
+        h0 = 120;
+        w = 5 + ((1.0 + cos((double)((f * 30.0) + (i * 10)))) * w0 * 2);
+        h = 5 + ((1.0 + sin((double)((f * 40.0) + (i * 19)))) * h0 * 2);
+        x = (win_w / 2) - (w / 2);
+        x += sin((double)((f * 50.0) + (i * 13))) * (w0 / 2);
+        y = (win_h / 2) - (h / 2);
+        y += cos((double)((f * 45.0) + (i * 28))) * (h0 / 2);
+        evas_object_geometry_set(o, x, y, w, h);
+     }
+}
+#endif
