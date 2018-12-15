0d2b624f1e24240a1c4e651aa1cfe9a8dd10a573
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2018-12-15 16:19:01 +0000

    evas gl - make GLintptr etc. also ndefed for GL_VERSION_1_5 fix typedef
    It seems that GL_VERSION_1_5 define == these provided by gl already. At
    least reading the mesa headers I do, so this should fix T7502
    
    @fix

--- src/lib/evas/Evas_GL.h
+++ src/lib/evas/Evas_GL.h
@@ -4272,9 +4272,11 @@ typedef signed int       GLfixed;      // Changed khronos_int32_t
 
 #ifndef GL_ES_VERSION_2_0
 /* GL types for handling large vertex buffer objects */
-#include <stddef.h>
+# ifndef GL_VERSION_1_5
+#  include <stddef.h>
 typedef ptrdiff_t GLintptr;     // Changed khronos_intptr_t
 typedef ptrdiff_t GLsizeiptr;   // Changed khronos_ssize_t
+# endif
 #endif
 
 /* Some definitions from GLES 3.0.
