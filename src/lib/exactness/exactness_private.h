50dbfcf31de264366cc6212f5af73084fa8a7f0e
Daniel Zaoui <daniel.zaoui@yahoo.com>
2018-12-24 22:51:52 +0200

    exactness: import code from external repo into efl.git
    Exactness has been developed in a separate git repo for many years. This
    finally moves it over into efl. Having it in tree allows us for easier
    testing with our current main target elementary_test and integration
    into our CI system (patches for this are work in progress already).
    
    We are only importing the lib and binary for test execution, not the
    full set of test data. This is would be over 500MB and thus it will stay
    in a different repo and only made available during the actual testing.
    
    [The original patch was made by Daniel Zaoui. Over the course of review
    and testing it got extended with build fixes for API changes and mingw
    compilation support from Stefan Schmidt and Michael Blumenkrantz]
    
    Reviewed-by: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
    Differential Revision: https://phab.enlightenment.org/D11285

---
+++ src/lib/exactness/exactness_private.h
@@ -0,0 +1,10 @@
+#ifndef _EXACTNESS_PRIVATE_H
+#define _EXACTNESS_PRIVATE_H
+
+#include <Exactness.h>
+
+/* private header */
+EAPI const char *_exactness_action_type_to_string_get(Exactness_Action_Type type);
+
+#define SHOT_DELIMITER '+'
+#endif
