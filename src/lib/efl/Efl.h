68d1579faad59d2f062b98ae4b4cdbbd1dd4cabf
Marcel Hollerbach <mail@marcel-hollerbach.de>
2019-01-24 12:47:38 +0100

    efl: introduce efl_ui_direction_readonly
    the readonly mixin implement the efl_ui_direction interface and prints a
    error in case it is written.
    
    ref T5719
    
    Reviewed-by: Cedric BAIL <cedric.bail@free.fr>
    Reviewed-by: Xavi Artigas <xavierartigas@yahoo.es>
    Differential Revision: https://phab.enlightenment.org/D7755

--- src/lib/efl/Efl.h
+++ src/lib/efl/Efl.h
@@ -96,6 +96,7 @@ typedef Efl_Gfx_Path_Command_Type Efl_Gfx_Path_Command;
 #include "interfaces/efl_orientation.eo.h"
 #include "interfaces/efl_ui_i18n.eo.h"
 #include "interfaces/efl_ui_direction.eo.h"
+#include "interfaces/efl_ui_direction_readonly.eo.h"
 #include "interfaces/efl_ui_drag.eo.h"
 #include "interfaces/efl_ui_range_display.eo.h"
 #include "interfaces/efl_ui_range_interactive.eo.h"
---
+++ src/lib/efl/interfaces/efl_ui_direction_readonly.c
@@ -0,0 +1,17 @@
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include <Efl.h>
+
+typedef struct {
+
+} Efl_Ui_Direction_Readonly_Data;
+
+EOLIAN static void
+_efl_ui_direction_readonly_efl_ui_direction_direction_set(Eo *obj EINA_UNUSED, Efl_Ui_Direction_Readonly_Data *pd EINA_UNUSED, Efl_Ui_Dir dir EINA_UNUSED)
+{
+   EINA_LOG_ERR("This object does not allow setting of a direction");
+}
+
+#include "efl_ui_direction_readonly.eo.c"
