8659f5979ca0762273ea04df040ae06c035477f7
Christopher Michael <cp.michael@samsung.com>
2019-01-29 10:43:00 -0500

    evas-drm: Add basic Outbuf structure

--- src/modules/evas/engines/drm/evas_engine.h
+++ src/modules/evas/engines/drm/evas_engine.h
@@ -36,4 +36,20 @@ extern int _evas_engine_drm_log_dom;
 # endif
 # define CRI(...) EINA_LOG_DOM_CRIT(_evas_engine_drm_log_dom, __VA_ARGS__)
 
+struct _Outbuf
+{
+   int w, h, bpp, rotation;
+   unsigned int depth, format;
+
+   Evas_Engine_Info_Drm *info;
+
+   Eina_Bool alpha : 1;
+};
+
+Outbuf *_outbuf_setup(Evas_Engine_Info_Drm *einfo, int w, int h);
+void _outbuf_free(Outbuf *ob);
+int _outbuf_get_rotation(Outbuf *ob);
+void _outbuf_reconfigure(Outbuf *ob, int w, int h, int rotation, Outbuf_Depth depth);
+void *_outbuf_new_region_for_update(Outbuf *ob, int x, int y, int w, int h, int *cx, int *cy, int *cw, int *ch);
+
 #endif
