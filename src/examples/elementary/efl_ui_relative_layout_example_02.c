329147a26a29c46577bff070b184cc400e0e65f4
Xavi Artigas <xavierartigas@yahoo.es>
2019-02-22 17:42:31 +0100

    examples: fixes after Efl.Gfx.Hint rename

--- src/examples/elementary/efl_ui_relative_layout_example_02.c
+++ src/examples/elementary/efl_ui_relative_layout_example_02.c
@@ -25,11 +25,11 @@ elm_main(int argc, char **argv)
 
    btn1 = efl_add(EFL_UI_BUTTON_CLASS, layout,
                   efl_text_set(efl_added, "btn1"),
-                  efl_gfx_size_hint_min_set(efl_added, EINA_SIZE2D(100, 100)));
+                  efl_gfx_hint_size_min_set(efl_added, EINA_SIZE2D(100, 100)));
 
    btn2 = efl_add(EFL_UI_BUTTON_CLASS, layout,
                   efl_text_set(efl_added, "btn2"),
-                  efl_gfx_size_hint_min_set(efl_added, EINA_SIZE2D(0, 100)));
+                  efl_gfx_hint_size_min_set(efl_added, EINA_SIZE2D(0, 100)));
 
    efl_ui_relative_layout_relation_right_set(layout, btn1, btn2, 0.0);
    efl_ui_relative_layout_relation_bottom_set(layout, btn1, btn2, 0.0);
