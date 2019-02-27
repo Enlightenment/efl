120e247fa0acb912a3974c5af2c59271ebf1ce40
Marcel Hollerbach <mail@marcel-hollerbach.de>
2019-02-27 14:57:03 -0500

    efl_canvas_layout_part_external: implement missing functions
    Summary:
    this just prints a error, manipulating the content here is not
    permitted. As it is created by edje externals, which are owned by edje.
    
    ref T5719
    Depends on D7757
    
    Reviewers: cedric, zmike, segfaultxavi
    
    Reviewed By: zmike
    
    Subscribers: q66, #reviewers, #committers
    
    Tags: #efl
    
    Maniphest Tasks: T5719
    
    Differential Revision: https://phab.enlightenment.org/D7759

--- src/lib/edje/edje_part_external.c
+++ src/lib/edje/edje_part_external.c
@@ -22,4 +22,18 @@ _efl_canvas_layout_part_external_efl_content_content_get(const Eo *obj, void *_p
    return _edje_object_part_external_object_get(pd->ed, pd->part);
 }
 
+EOLIAN static Eina_Bool
+_efl_canvas_layout_part_external_efl_content_content_set(Eo *obj EINA_UNUSED, void *pd EINA_UNUSED, Efl_Gfx_Entity *content EINA_UNUSED)
+{
+   ERR("Setting of content is not permitted on this part");
+   return EINA_FALSE;
+}
+
+EOLIAN static Efl_Gfx_Entity*
+_efl_canvas_layout_part_external_efl_content_content_unset(Eo *obj EINA_UNUSED, void *pd EINA_UNUSED)
+{
+   ERR("Unsetting of content is not permitted on this part");
+   return NULL;
+}
+
 #include "efl_canvas_layout_part_external.eo.c"
