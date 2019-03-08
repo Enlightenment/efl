c876ac52d91806bfc6440b10387ccb91e6a75abf
Daniel Kolesa <daniel@octaforge.org>
2019-03-08 08:57:51 -0500

    elementary,evas: remove obsolete legacy includes
    Summary:
    Since the removal of legacy interfaces from eo files, these files
    contain nothing useful, and can safely be removed. One exception
    is `efl_ui_layout.eo.legacy.h`, which will require more involved
    work to remove, since a lot of things seem to depend on the
    Efl_Ui_Layout typedef being present, wrongly (i suspect this
    will break everything with `EFL_NOLEGACY_API_SUPPORT`).
    
    Reviewers: cedric, zmike, bu5hm4n
    
    Reviewed By: zmike
    
    Subscribers: #reviewers, #committers
    
    Tags: #efl
    
    Differential Revision: https://phab.enlightenment.org/D8251

--- src/lib/elementary/elm_scroller_legacy.h
+++ src/lib/elementary/elm_scroller_legacy.h
@@ -1,4 +1,42 @@
-#include "elm_interface_scrollable.eo.legacy.h"
+#ifndef _ELM_INTERFACE_SCROLLABLE_EO_TYPES
+#define _ELM_INTERFACE_SCROLLABLE_EO_TYPES
+
+/**
+ * @brief Type that controls when scrollbars should appear.
+ *
+ * See also @ref Elm.Interface_Scrollable.policy.set.
+ *
+ * @ingroup Elm_Scroller
+ */
+typedef enum
+{
+  ELM_SCROLLER_POLICY_AUTO = 0, /**< Show scrollbars as needed */
+  ELM_SCROLLER_POLICY_ON, /**< Always show scrollbars */
+  ELM_SCROLLER_POLICY_OFF, /**< Never show scrollbars */
+  ELM_SCROLLER_POLICY_LAST /**< Sentinel value to indicate last enum field
+                            * during iteration */
+} Elm_Scroller_Policy;
+
+/**
+ * @brief Type that controls how the content is scrolled.
+ *
+ * See also @ref Elm.Interface_Scrollable.single_direction.set.
+ *
+ * @ingroup Elm_Scroller
+ */
+typedef enum
+{
+  ELM_SCROLLER_SINGLE_DIRECTION_NONE = 0, /**< Scroll every direction */
+  ELM_SCROLLER_SINGLE_DIRECTION_SOFT, /**< Scroll single direction if the
+                                       * direction is certain */
+  ELM_SCROLLER_SINGLE_DIRECTION_HARD, /**< Scroll only single direction */
+  ELM_SCROLLER_SINGLE_DIRECTION_LAST /**< Sentinel value to indicate last enum
+                                      * field during iteration */
+} Elm_Scroller_Single_Direction;
+
+
+#endif
+
 #include "elm_scroller_eo.legacy.h"
 
 /**
