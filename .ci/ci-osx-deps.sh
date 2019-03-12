bf26ce4b12a1fdd6e8dbda1db625789a5456bd23
Mike Blumenkrantz <zmike@samsung.com>
2019-03-12 10:33:31 -0400

    ci: force osx libffi dep to be found
    this is really, really stupid.
    
    Reviewed-by: Stefan Schmidt <stefan@datenfreihafen.org>
    Differential Revision: https://phab.enlightenment.org/D8305

--- .ci/ci-osx-deps.sh
+++ .ci/ci-osx-deps.sh
@@ -2,5 +2,5 @@
 
 brew update
 brew unlink python
-brew install gettext check bullet dbus fontconfig freetype fribidi gst-plugins-good gstreamer luajit openssl webp libsndfile glib libspectre libraw librsvg poppler lz4 pulseaudio ccache ninja python3
+brew install gettext check bullet dbus fontconfig freetype fribidi gst-plugins-good gstreamer luajit openssl webp libsndfile glib libspectre libraw librsvg poppler lz4 pulseaudio ccache ninja python3 libffi
 pip3 install meson
