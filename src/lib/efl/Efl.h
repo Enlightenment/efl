3e7017e6a8d24422cbd8bf0e3fb0af69984b4e35
Mike Blumenkrantz <zmike@samsung.com>
2019-04-05 20:17:27 -0400

    add EFL_VERSION_1_23 define

--- src/lib/efl/Efl.h
+++ src/lib/efl/Efl.h
@@ -51,6 +51,7 @@ extern "C" {
 #define EFL_VERSION_1_20 1
 #define EFL_VERSION_1_21 1
 #define EFL_VERSION_1_22 1
+#define EFL_VERSION_1_23 1
 
 /* Add here all the required ifdef for any @protected method */
 #ifdef EFL_BUILD
