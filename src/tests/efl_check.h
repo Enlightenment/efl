1f402946a20a4024310b0b578973b69cf17d8db5
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2019-04-07 14:14:55 +0100

    tests - fix check header to always include eina due to windows
    windows means HAVE_FORK is false... thus missing eina.h and now we
    have macros that use eina calls always... so this fixes nbuild of
    tests on windows
    
    @fix

--- src/tests/efl_check.h
+++ src/tests/efl_check.h
@@ -13,28 +13,29 @@
 #include <sys/time.h>
 
 #ifdef HAVE_FORK
-#ifdef HAVE_SYS_TYPES_H
-# include <sys/types.h>
+# ifdef HAVE_SYS_TYPES_H
+#  include <sys/types.h>
+# endif
+# ifdef HAVE_SYS_WAIT_H
+#  include <sys/wait.h>
+# endif
+# include <signal.h>
 #endif
-#ifdef HAVE_SYS_WAIT_H
-# include <sys/wait.h>
-#endif
-#include <signal.h>
+
 #include <Eina.h>
-#endif
 
 #ifndef EINA_UNUSED
 
-#ifdef __GNUC__
+# ifdef __GNUC__
 
-# if __GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 1)
-#  define EINA_UNUSED __attribute__ ((__unused__))
+#  if __GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 1)
+#   define EINA_UNUSED __attribute__ ((__unused__))
+#  else
+#   define EINA_UNUSED
+#  endif
 # else
 #  define EINA_UNUSED
 # endif
-#else
-#  define EINA_UNUSED
-#endif
 
 #endif
 
