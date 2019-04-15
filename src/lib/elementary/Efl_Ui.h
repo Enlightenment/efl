f218ac741ccd832fc1e6f62bca0207882c293e54
Vincent Torri <vincent.torri@gmail.com>
2019-04-15 11:53:42 +0100

    Windows : fix the definition of EAPI
    Summary:
    with autotools, EFL_BUILD was passed to the preprocessor for libraries and binaries, which was wrong. Only libraries must have EFL_BUILD defined. See T7797 for an explanation
    This also fix EAPI in Ecore_Getopt.g and Efl_UI.h
    
    Also note that all the wayland and drm Makefile's have not been touched
    
    Test Plan: compilation
    
    Reviewers: raster, zmike, cedric
    
    Subscribers: #reviewers, #committers
    
    Tags: #efl
    
    Differential Revision: https://phab.enlightenment.org/D8602

--- src/lib/elementary/Efl_Ui.h
+++ src/lib/elementary/Efl_Ui.h
@@ -53,41 +53,6 @@
 #include <Eo.h>
 #include <Efl.h>
 
-#ifdef EAPI
-# undef EAPI
-#endif
-#ifdef EWAPI
-# undef EWAPI
-#endif
-
-#ifdef _WIN32
-# ifdef EFL_BUILD
-#  ifdef DLL_EXPORT
-#   define EAPI __declspec(dllexport)
-#  else
-#   define EAPI
-#  endif
-# else
-#  define EAPI __declspec(dllimport)
-# endif
-# define EAPI_WEAK
-#else
-# ifdef __GNUC__
-#  if __GNUC__ >= 4
-#   define EAPI __attribute__ ((visibility("default")))
-#   define EAPI_WEAK __attribute__ ((weak))
-#  else
-#   define EAPI
-#   define EAPI_WEAK
-# endif
-# else
-#  define EAPI
-#  define EAPI_WEAK
-# endif
-#endif
-
-#define EWAPI EAPI EAPI_WEAK
-
 #ifdef _WIN32
 # define WIN32_LEAN_AND_MEAN
 # include <winsock2.h>
@@ -132,6 +97,41 @@
 #ifdef ELM_EMAP
 #include <EMap.h>
 #endif
+
+#ifdef EAPI
+# undef EAPI
+#endif
+#ifdef EWAPI
+# undef EWAPI
+#endif
+
+#ifdef _WIN32
+# ifdef EFL_BUILD
+#  ifdef DLL_EXPORT
+#   define EAPI __declspec(dllexport)
+#  else
+#   define EAPI
+#  endif
+# else
+#  define EAPI __declspec(dllimport)
+# endif
+# define EAPI_WEAK
+#else
+# ifdef __GNUC__
+#  if __GNUC__ >= 4
+#   define EAPI __attribute__ ((visibility("default")))
+#   define EAPI_WEAK __attribute__ ((weak))
+#  else
+#   define EAPI
+#   define EAPI_WEAK
+# endif
+# else
+#  define EAPI
+#  define EAPI_WEAK
+# endif
+#endif
+
+#define EWAPI EAPI EAPI_WEAK
 
 #ifdef __cplusplus
 extern "C" {
