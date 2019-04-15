2ac93b5005e0693c5d84df71b4aa1c5a4b6baa80
Hermet Park <hermetpark@gmail.com>
2019-04-15 17:53:22 +0900

    evas gl: move to floating point coordinate system.
    Summary:
    GL engine has used integer coordinates system since it's born though
    OpenGL basically uses floating point vertex coordinates system.
    
    There were many dissatisfaction, complaints about this
    since object's transition is jiggled, not perfectly smooth.
    
    It's obvious because Positioning must be stepping with integer units
    without any subpixel rendering.
    
    Our gl engine currently supports msaa options and evas map allows to
    have double precivion coordinates system, our engine must do handle this over as well,
    to work together.
    
    If you don't like change,
    
    We could switch behaviors optionally (turn on, only when msaa is enabled)
    But I think it's pointless using integer coordinates system in GL thesedays.
    It just make code complex to maintain.
    
    There will be an additional patch coming for SW backend map behavior soon.
    
    Left: before patch
    Right: after patch
    
    {F3694624}
    
    Reviewers: #committers, raster
    
    Subscribers: raster, cedric, #reviewers, #committers
    
    Tags: #efl
    
    Differential Revision: https://phab.enlightenment.org/D8552

--- src/modules/evas/engines/gl_common/evas_gl_common.h
+++ src/modules/evas/engines/gl_common/evas_gl_common.h
@@ -335,7 +335,7 @@ struct _Evas_Engine_GL_Context
       } shader;
       struct {
          int            num, alloc;
-         GLshort       *vertex;
+         GLfloat       *vertex;
          GLubyte       *color;
          GLfloat       *texuv;
          GLfloat       *texuv2;
