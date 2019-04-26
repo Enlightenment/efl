58f5529f704baf4c14afbb8f8ffef9e6c487d2b7
Marcel Hollerbach <mail@marcel-hollerbach.de>
2019-04-26 16:04:03 +0200

    efl_ui_spec: we can add efl_ui_pager now
    Reviewed-by: Cedric BAIL <cedric.bail@free.fr>
    Differential Revision: https://phab.enlightenment.org/D8752

--- src/tests/elementary/spec/efl_test_pack_linear.c
+++ src/tests/elementary/spec/efl_test_pack_linear.c
@@ -9,7 +9,7 @@
 
 /* spec-meta-start
       {"test-interface":"Efl.Pack_Linear",
-       "test-widgets": ["Efl.Ui.Box", "Efl.Ui.Grid"],
+       "test-widgets": ["Efl.Ui.Box", "Efl.Ui.Grid", "Efl.Ui.Pager"],
        "custom-mapping" : {
           "Efl.Ui.Grid" : "EFL_UI_GRID_DEFAULT_ITEM_CLASS"
         }
