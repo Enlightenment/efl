1b6ac14b3f7de1bcb3c275acb34ac1928c09dd26
Marcel Hollerbach <mail@marcel-hollerbach.de>
2019-04-26 15:51:09 +0200

    efl_ui_pager: provide a pack( implementation
    this is needed in order to make efl_ui_pager complete.
    
    Reviewed-by: Cedric BAIL <cedric.bail@free.fr>
    Differential Revision: https://phab.enlightenment.org/D8739

--- src/lib/elementary/efl_ui_pager.c
+++ src/lib/elementary/efl_ui_pager.c
@@ -906,6 +906,10 @@ _efl_ui_pager_efl_pack_linear_pack_unpack_at(Eo *obj,
    return subobj;
 }
 
-
+EOLIAN static Eina_Bool
+_efl_ui_pager_efl_pack_pack(Eo *obj, Efl_Ui_Pager_Data *pd EINA_UNUSED, Efl_Gfx_Entity *subobj)
+{
+   return efl_pack_begin(obj, subobj);
+}
 
 #include "efl_ui_pager.eo.c"
