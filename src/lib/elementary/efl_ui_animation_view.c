Enlightenment/efl

commit ad040eec2e60b092572f8bec5edb6ee73e1eb834

efl_ui_animation_view: Set PLAY state before call "start" callback.
Summary:
In transit_cb state set PLAY.
But before that "Start" callback is called.
So the state in the callback function is STOP or another state.

Test Plan: N/A

Reviewers: Hermet, kimcinoo

Reviewed By: kimcinoo

Subscribers: cedric, #reviewers, #committers

Tags: #efl

Differential Revision: https://phab.enlightenment.org/D8801

src/lib/elementary/efl_ui_animation_view.c
@@ -66,6 +66,10 @@ static void
 _transit_go_facade(Efl_Ui_Animation_View_Data *pd)
 {
    pd->repeat_times = 0;
+   if (pd->play_back)
+     pd->state = EFL_UI_ANIMATION_VIEW_STATE_PLAY_BACK;
+   else
+     pd->state = EFL_UI_ANIMATION_VIEW_STATE_PLAY;
    evas_object_smart_callback_call(pd->obj, SIG_PLAY_START, NULL);
    elm_transit_go(pd->transit);
 }

commit b75cb6ce80963252632f83ddefb2ba20ea363529

animation_view: fix a regression bug by ad040eec2e60b092572f8bec5edb6ee73e1eb834
The initial progress must be reverted in stop status.
otherwise rewind animation progress will be courrpted.

src/lib/elementary/efl_ui_animation_view.c
@@ -503,11 +503,11 @@ _efl_ui_animation_view_play_back(Eo *obj, Efl_Ui_Animation_View_Data *pd)
    if (!efl_file_loaded_get(obj)) return EINA_FALSE;
    if (!pd->transit && !_ready_play(pd)) return EINA_FALSE;
 
-   if (pd->progress == 0)
-     pd->progress = 1.0;
-
    if (pd->state == EFL_UI_ANIMATION_VIEW_STATE_STOP)
-     _transit_go_facade(pd);
+     {
+        if (pd->progress == 0) pd->progress = 1.0;
+        _transit_go_facade(pd);
+     }
    else if (rewind)
      elm_transit_progress_value_set(pd->transit, 1 - pd->progress);
 