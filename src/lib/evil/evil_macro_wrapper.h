a41c5f7a0656f9b8a1b00bee65f03d2455ed6547
Vincent Torri <vincent.torri@gmail.com>
2019-05-08 10:14:45 -0400

    Evil: remove localtime_r as mingw-w64 defines it when _POSIX_C_SOURCE is defined
    Summary:
    localtime_r() is already defined by mingw-w64 when _POSIX_C_SOURCE is defined
    
    edit: also only define _POSIX_C_SOURCE (correctly) on windows
    
    Reviewers: bu5hm4n
    
    Reviewed By: bu5hm4n
    
    Subscribers: cedric, #reviewers, #committers
    
    Tags: #efl
    
    Differential Revision: https://phab.enlightenment.org/D8859

--- src/lib/evil/evil_macro_wrapper.h
+++ src/lib/evil/evil_macro_wrapper.h
@@ -46,20 +46,6 @@
 #define mkdir(dirname, mode) evil_mkdir(dirname, mode)
 
 /*
- * evil_time.h
- */
-
-/**
- * @def localtime_r(t, r)
- *
- * Wrapper around evil_localtime_r().
- */
-#ifdef localtime_r
-# undef localtime_r
-#endif
-#define localtime_r(t, r) evil_localtime_r(t, r)
-
-/*
  * evil_unistd.h
  */
 
