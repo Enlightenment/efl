a4c44e5e744dba15dfe512668127906afd4b6dba
Vincent Torri <vincent.torri@gmail.com>
2019-05-08 12:39:54 +0100

    Evil: remove localtime_r as mingw-w64 defines it when _POSIX_C_SOURCE is defined
    Summary: localtime_r() is already defined by mingw-w64 when _POSIX_C_SOURCE is defined
    
    Test Plan: compilation
    
    Reviewers: raster, zmike, cedric
    
    Subscribers: #reviewers, #committers
    
    Tags: #efl
    
    Maniphest Tasks: T7907
    
    Differential Revision: https://phab.enlightenment.org/D8854

--- src/lib/evil/evil_time.h
+++ src/lib/evil/evil_time.h
@@ -2,6 +2,9 @@
 #define __EVIL_TIME_H__
 
 
+#include <time.h>
+
+
 /**
  * @file evil_time.h
  * @brief The file that provides functions ported from Unix in time.h.
@@ -13,26 +16,6 @@
  * @{
  */
 
-
-/**
- * @brief Convert the calendar time to broken-time representation in a
- * user supplied data.
- *
- * @param timep The calender time.
- * @param result The broken-down time representation.
- * @return The broken-down time representation.
- *
- * This function converts the calendar time @p timep to a broken-time
- * representation. The result is stored in the buffer  @p result
- * supplied by the user. If @p timep or @p result are @c NULL, or if
- * an error occurred, this function returns @c NULL and the values in
- * @p result might be undefined. Otherwise it returns @p result.
- *
- * Conformity: Non applicable.
- *
- * Supported OS: Windows XP.
- */
-EAPI struct tm *evil_localtime_r(const time_t *timep, struct tm *result);
 
 /**
  * @brief Convert a string representation of time to a time tm structure .
