0c4c74b5f7779fbf5c6154b8241291041a8d86b7
Marcel Hollerbach <mail@marcel-hollerbach.de>
2019-06-21 09:40:17 -0400

    efl_ui_win: fix emitting of object focus events
    Summary: the event type was wrong. This is now fixed and works correctly.
    
    Reviewers: stefan_schmidt, zmike, segfaultxavi
    
    Reviewed By: zmike
    
    Subscribers: cedric, #reviewers, #committers
    
    Tags: #efl
    
    Differential Revision: https://phab.enlightenment.org/D9144

--- src/tests/elementary/efl_ui_test_win.c
+++ src/tests/elementary/efl_ui_test_win.c
@@ -51,8 +51,52 @@ EFL_START_TEST(efl_ui_win_test_scene_focus)
    efl_unref(win1);
 }
 EFL_END_TEST
+
+static void
+_check_focus_event(void *data, const Efl_Event *ev)
+{
+   void **tmp = data;
+
+   *tmp = efl_input_focus_object_get(ev->info);
+}
+
+EFL_START_TEST(efl_ui_win_test_object_focus)
+{
+   Efl_Ui_Win *win1;
+
+   void *win1_focus_in = NULL;
+   void *win1_focus_out = NULL;
+
+   win1 = efl_new(EFL_UI_WIN_CLASS);
+
+   efl_event_callback_add(win1, EFL_CANVAS_SCENE_EVENT_OBJECT_FOCUS_IN, _check_focus_event, &win1_focus_in);
+   efl_event_callback_add(win1, EFL_CANVAS_SCENE_EVENT_OBJECT_FOCUS_OUT, _check_focus_event, &win1_focus_out);
+
+   Eo *r1 = efl_add(EFL_CANVAS_RECTANGLE_CLASS, evas_object_evas_get(win1));
+   efl_canvas_object_seat_focus_add(r1, NULL);
+   ck_assert_ptr_eq(win1_focus_in, r1);
+   ck_assert_ptr_eq(win1_focus_out, NULL);
+   win1_focus_in = NULL;
+   win1_focus_out = NULL;
+
+   Eo *r2 = efl_add(EFL_CANVAS_RECTANGLE_CLASS, evas_object_evas_get(win1));
+   efl_canvas_object_seat_focus_add(r2, NULL);
+   ck_assert_ptr_eq(win1_focus_in, r2);
+   ck_assert_ptr_eq(win1_focus_out, r1);
+   win1_focus_in = NULL;
+   win1_focus_out = NULL;
+
+   efl_canvas_object_seat_focus_del(r2, NULL);
+   ck_assert_ptr_eq(win1_focus_in, NULL);
+   ck_assert_ptr_eq(win1_focus_out, r2);
+
+   efl_unref(win1);
+}
+EFL_END_TEST
+
 void
 efl_ui_test_win(TCase *tc)
 {
    tcase_add_test(tc, efl_ui_win_test_scene_focus);
+   tcase_add_test(tc, efl_ui_win_test_object_focus);
 }
