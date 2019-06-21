431b5e817c5652d39560fc2a416f87b948cc8d64
Hermet Park <hermetpark@gmail.com>
2019-06-21 17:36:16 +0900

    evas vector: add a lottie animation example.
    Summary: Depends on {D8941}
    
    Reviewers: #committers, jsuya
    
    Subscribers: cedric, #reviewers, #committers
    
    Tags: #efl
    
    Differential Revision: https://phab.enlightenment.org/D8944

--- src/examples/evas/evas-common.h
+++ src/examples/evas/evas-common.h
@@ -1,5 +1,7 @@
 #define EVAS_IMAGE_FOLDER "/resources/images"
 #define EVAS_MODEL_FOLDER "/resources/models"
+#define EVAS_VG_FOLDER "/resources/vg"
+
 #define EVAS_CONVEX_HULL_FOLDER "/resources/models/convex_hull"
 #define EVAS_SAVED_FILES "/saved_files"
 
