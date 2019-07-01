87cfde51d3e143260a1b431f9c14d30536b549ce
Stefan Schmidt <s.schmidt@samsung.com>
2019-07-01 10:24:04 +0200

    ci: finally enable examples for mingw build
    With the gnutls build problem fixed we can now enable these as well.

--- .ci/ci-configure.sh
+++ .ci/ci-configure.sh
@@ -38,7 +38,7 @@ if [ "$DISTRO" != "" ] ; then
   MINGW_COPTS="--cross-file .ci/cross_toolchain.txt -Davahi=false -Deeze=false -Dsystemd=false \
   -Dpulseaudio=false -Dx11=false -Dopengl=none -Dlibmount=false \
   -Devas-loaders-disabler=json,pdf,ps,raw,svg -Devas-modules=static -Dbindings=luajit \
-  -Dharfbuzz=true -Dpixman=true -Dembedded-lz4=false -Dbuild-examples=false "
+  -Dharfbuzz=true -Dpixman=true -Dembedded-lz4=false "
 
   if [ "$1" = "options-enabled" ]; then
     OPTS="$OPTS $ENABLED_LINUX_COPTS $WAYLAND_LINUX_COPTS"
