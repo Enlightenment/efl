477611f01460f4b9881f2b5dd4b88f463d8f81b0
Cedric BAIL <cedric.bail@free.fr>
2019-07-11 15:53:54 -0700

    elementary: fix potential race condition by using Eina_Future attached to the object.
    I get some random segfault in elementary test suite pointing to this code. Most likely
    we do not properly destroy the timer during destruction. Could be because we initiate
    a delay while destruction is going on or something like that. Anyway, it is easier and
    more robust to get it fixed by linking the lifetime of the timeout to the lifetime of
    the widget as future allow us to do easily.
    
    Reviewed-by: Marcel Hollerbach <mail@marcel-hollerbach.de>
    Differential Revision: https://phab.enlightenment.org/D9298

--- src/lib/elementary/efl_ui_spin_button_private.h
+++ src/lib/elementary/efl_ui_spin_button_private.h
@@ -5,7 +5,7 @@ typedef struct _Efl_Ui_Spin_Button_Data    Efl_Ui_Spin_Button_Data;
 struct _Efl_Ui_Spin_Button_Data
 {
    Evas_Object          *ent, *inc_button, *dec_button, *text_button;
-   Ecore_Timer          *delay_change_timer; /**< a timer for a delay,changed smart callback */
+   Eina_Future          *delay_change_timer; /**< a timer for a delay,changed smart callback */
 
    Efl_Ui_Layout_Orientation dir;
 
