b9f89fabb6f90b5bbd8a0198d2c626f3cbda101a
Christopher Michael <cp.michael@samsung.com>
2019-07-15 10:08:32 -0400

    tests: Add tests for ecore_wl2_display_get and ecore_wl2_display_name_get
    ref T8016

--- src/tests/ecore_wl2/ecore_wl2_test_display.c
+++ src/tests/ecore_wl2/ecore_wl2_test_display.c
@@ -31,9 +31,35 @@ EFL_START_TEST(wl2_display_destroy)
 }
 EFL_END_TEST
 
+EFL_START_TEST(wl2_display_get)
+{
+   Ecore_Wl2_Display *disp;
+   struct wl_display *wdisp;
+
+   disp = ECORE_WL2_TEST_DISPLAY_SETUP();
+   ck_assert(disp != NULL);
+
+   wdisp = ecore_wl2_display_get(disp);
+   ck_assert(wdisp != NULL);
+}
+EFL_END_TEST
+
+EFL_START_TEST(wl2_display_name_get)
+{
+   Ecore_Wl2_Display *disp;
+
+   disp = ECORE_WL2_TEST_DISPLAY_SETUP();
+   ck_assert(disp != NULL);
+
+   ck_assert(ecore_wl2_display_name_get(disp) != NULL);
+}
+EFL_END_TEST
+
 void
 ecore_wl2_test_display(TCase *tc)
 {
    tcase_add_test(tc, wl2_display_create);
    tcase_add_test(tc, wl2_display_destroy);
+   tcase_add_test(tc, wl2_display_get);
+   tcase_add_test(tc, wl2_display_name_get);
 }
