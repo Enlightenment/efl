Enlightenment/efl

commit 1c0a459293981ef9691b9bb5acd468ba84e502b4

efl_gfx_color: fix color_code_set
Summary:
There are two parts for this patch:
1- Fix sigmentation fault when using (efl_gfx_color_color_code_set).  // It try to modify const variable
2- Remove unnecessary code. // why would user pass slash or back slash as color code format.

Test Plan:
```
#define EFL_EO_API_SUPPORT 1
#define EFL_BETA_API_SUPPORT 1

#include <Eina.h>
#include <Elementary.h>
#include <Efl_Ui.h>

static void
_gui_quit_cb(void *data EINA_UNUSED, const Efl_Event *event EINA_UNUSED)
{
   efl_exit(0);
}

static void
_gui_setup()
{
   Eo *win, *rect;

   win = efl_add(EFL_UI_WIN_CLASS, efl_main_loop_get(),
                 efl_ui_win_type_set(efl_added, EFL_UI_WIN_TYPE_BASIC),
                 efl_text_set(efl_added, "Hello World"),
                 efl_ui_win_autodel_set(efl_added, EINA_TRUE));

   // when the user clicks "close" on a window there is a request to delete
   efl_event_callback_add(win, EFL_UI_WIN_EVENT_DELETE_REQUEST, _gui_quit_cb, NULL);
   rect = efl_add(EFL_CANVAS_RECTANGLE_CLASS,win);
   const char *color = "#FF0000FF";
   efl_gfx_color_code_set(rect,color);
   evas_object_resize(rect,250,250);
   evas_object_resize(win,250,250);
   evas_object_move(rect,0,0);
   evas_object_show(rect);
   evas_object_show(win);
}

EAPI_MAIN void
efl_main(void *data EINA_UNUSED, const Efl_Event *ev EINA_UNUSED)
{
   _gui_setup();
}
EFL_MAIN()
```

Reviewers: zmike, cedric, segfaultxavi, woohyun

Reviewed By: segfaultxavi

Subscribers: singh.amitesh, cedric, #reviewers, #committers

Tags: #efl

Differential Revision: https://phab.enlightenment.org/D9422

src/lib/efl/interfaces/efl_gfx_color.c
@@ -2,24 +2,6 @@
 #include "Efl.h"
 
 static int
-_format_clean_param(Eina_Tmpstr *s)
-{
-   Eina_Tmpstr *ss;
-   char *ds;
-   int len = 0;
-
-   ds = (char*) s;
-   for (ss = s; *ss; ss++, ds++, len++)
-     {
-        if ((*ss == '\\') && *(ss + 1)) ss++;
-        if (ds != ss) *ds = *ss;
-     }
-   *ds = 0;
-
-   return len;
-}
-
-static int
 _hex_string_get(char ch, Eina_Bool *ok)
 {
    if ((ch >= '0') && (ch <= '9')) return (ch - '0');
@@ -102,7 +84,7 @@ _efl_gfx_color_color_code_set(Eo *obj, void *_pd EINA_UNUSED, const char *colorc
     int len;
     unsigned char r, g, b, a;
 
-    len = _format_clean_param(colorcode);
+    len = (size_t) strlen(colorcode);
 
     _format_color_parse(colorcode, len, &r, &g, &b, &a);
     efl_gfx_color_set(obj, r, g, b, a);

commit ef235c43c81c7e81a7779e0ed13f671bb426989d

Fix build break
Caused by 1c0a459293981ef9691b9bb5acd468ba84e502b4
I should have been more careful before approving, my bad.

src/lib/efl/interfaces/efl_gfx_color.c
@@ -106,7 +106,7 @@ _efl_gfx_color_class_color_class_code_set(Eo *obj, void *_pd EINA_UNUSED, const 
    int len;
    unsigned char r, g, b, a;
 
-   len = _format_clean_param(colorcode);
+   len = (size_t) strlen(colorcode);
 
    _format_color_parse(colorcode, len, &r, &g, &b, &a);
    return efl_gfx_color_class_set(obj, color_class, layer, r, g, b, a);