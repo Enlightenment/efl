f15ca5e227fc401bae2fa8272ba8cc6953a22150
Christopher Michael <cp.michael@samsung.com>
2019-08-01 08:56:42 -0400

    tests/ecore_wl2: Add test for ecore_wl2_input_display_get function
    ref T8016

--- src/tests/ecore_wl2/ecore_wl2_test_input.c
+++ src/tests/ecore_wl2/ecore_wl2_test_input.c
@@ -67,6 +67,27 @@ EFL_START_TEST(wl2_input_seat_id_get)
 }
 EFL_END_TEST
 
+EFL_START_TEST(wl2_input_display_get)
+{
+   Ecore_Wl2_Display *disp;
+   Ecore_Wl2_Input *input;
+   Eina_Iterator *itr;
+
+   disp = _display_connect();
+   ck_assert(disp != NULL);
+
+   itr = ecore_wl2_display_inputs_get(disp);
+   ck_assert(itr != NULL);
+
+   EINA_ITERATOR_FOREACH(itr, input)
+     {
+        ck_assert(ecore_wl2_input_display_get(input) != NULL);
+     }
+
+   eina_iterator_free(itr);
+}
+EFL_END_TEST
+
 void
 ecore_wl2_test_input(TCase *tc)
 {
@@ -74,5 +95,6 @@ ecore_wl2_test_input(TCase *tc)
      {
         tcase_add_test(tc, wl2_input_seat_get);
         tcase_add_test(tc, wl2_input_seat_id_get);
+        tcase_add_test(tc, wl2_input_display_get);
      }
 }
