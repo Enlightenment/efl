e9e8078cec9100993f8124f9ad7bf1158c8e0cea
Mike Blumenkrantz <zmike@samsung.com>
2019-08-05 13:57:48 -0400

    tests/elm: add helper callback for automatically quitting main loop when triggered
    Reviewed-by: Marcel Hollerbach <mail@marcel-hollerbach.de>
    Differential Revision: https://phab.enlightenment.org/D9506

--- src/tests/elementary/suite_helpers.c
+++ src/tests/elementary/suite_helpers.c
@@ -467,6 +467,12 @@ event_callback_that_is_called_exactly_one_time_and_sets_a_single_int_data_pointe
 }
 
 void
+event_callback_that_quits_the_main_loop_when_called()
+{
+   ecore_main_loop_quit();
+}
+
+void
 click_object_at(Eo *obj, int x, int y)
 {
    Evas *e = evas_object_evas_get(obj);
