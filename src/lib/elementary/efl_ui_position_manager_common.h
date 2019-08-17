18b28ccadfb410f07c196fa8595692495fb499b2
Marcel Hollerbach <mail@marcel-hollerbach.de>
2019-08-17 15:59:54 +0200

    efl_ui_position_manager_common: fix buffer_id
    the buffer was accessed with the wrong id, buffer_id should be used.
    
    Differential Revision: https://phab.enlightenment.org/D9607

--- src/lib/elementary/efl_ui_position_manager_common.h
+++ src/lib/elementary/efl_ui_position_manager_common.h
@@ -44,7 +44,7 @@ vis_change_segment(Api_Callback *cb, int a, int b, Eina_Bool flag)
           {
              EINA_SAFETY_ON_FALSE_RETURN(_fill_buffer(cb, MIN(a,b), len, NULL, data) >= 0);
           }
-        ent = data[i - MIN(a,b)].entity;
+        ent = data[buffer_id].entity;
         if (ent && !flag && (efl_ui_focus_object_focus_get(ent) || efl_ui_focus_object_child_focus_get(ent)))
           {
              //we should not make focused object invisible, rather move it to some parking lot
