f10a3c9ee36cd270045f7e30fd3716ef15e3106d
Hermet Park <hermetpark@gmail.com>
2019-08-19 18:04:38 +0900

    edje: fix compatibility issue.
    Obviously previous edje object file set would try reload file
    even though the file is already loaded.
    
    This brings different result if the file data has been modified or changed.

--- src/lib/edje/edje_smart.c
+++ src/lib/edje/edje_smart.c
@@ -443,6 +443,7 @@ edje_object_mmap_set(Edje_Object *obj, const Eina_File *file, const char *group)
 EAPI Eina_Bool
 edje_object_file_set(Edje_Object *obj, const char *file, const char *group)
 {
+   efl_file_unload(obj);
    return efl_file_simple_load(obj, file, group);
 }
 
