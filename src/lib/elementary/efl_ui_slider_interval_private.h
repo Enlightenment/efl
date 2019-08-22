ebb685aae1fbf44d9600acc7b5b8be099f4bdea7
WooHyun Jung <wh0705.jung@samsung.com>
2019-08-22 08:16:11 +0000

    efl_ui_slider_interval: cut off the dependency with efl_ui_slider
    efl_ui_slider_interval is totally different widget from efl_ui_slider.
    Now, it extends efl_ui_layout_base and implements all efl_ui_slider's
    functionalities by its own.
    This class needs to be marked to @beta till reviewing whole class
    definitions again.
    
    @ref T7893
    
    Reviewed-by: Marcel Hollerbach <mail@marcel-hollerbach.de>
    Differential Revision: https://phab.enlightenment.org/D9696

--- src/lib/elementary/efl_ui_slider_interval_private.h
+++ src/lib/elementary/efl_ui_slider_interval_private.h
@@ -5,9 +5,18 @@
 typedef struct _Efl_Ui_Slider_Interval_Data Efl_Ui_Slider_Interval_Data;
 struct _Efl_Ui_Slider_Interval_Data
 {
-   Ecore_Timer          *delay;
-   double                intvl_from, intvl_to;
-   int                   intvl_flag;
+   Evas_Object                *spacer;
+   Ecore_Timer                *delay;
+
+   double                     intvl_from, intvl_to;
+   double                     val, val_min, val_max, step;
+
+   int                        intvl_flag;
+   Evas_Coord                 downx, downy;
+   Efl_Ui_Layout_Orientation  dir;
+
+   Eina_Bool                  spacer_down : 1;
+   Eina_Bool                  frozen : 1;
 };
 
 /**
