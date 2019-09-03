b3aba63616198175d352f5428b08d1f6e80a8eeb
Marcel Hollerbach <mail@marcel-hollerbach.de>
2019-09-03 10:25:48 +0200

    efl_ui_positon_manager_list: skip viewport size setting if its the same
    this can be skipped.
    
    Co-authored-by: Mike Blumenkrantz <zmike@samsung.com>
    
    Reviewed-by: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
    Differential Revision: https://phab.enlightenment.org/D9824

--- src/lib/elementary/efl_ui_position_manager_list.c
+++ src/lib/elementary/efl_ui_position_manager_list.c
@@ -350,6 +350,8 @@ schedule_recalc_absolut_size(Eo *obj, Efl_Ui_Position_Manager_List_Data *pd)
 EOLIAN static void
 _efl_ui_position_manager_list_efl_ui_position_manager_entity_viewport_set(Eo *obj, Efl_Ui_Position_Manager_List_Data *pd, Eina_Rect size)
 {
+   if (pd->viewport.w == size.w && pd->viewport.h == size.h) return;
+
    pd->viewport = size;
 
    recalc_absolut_size(obj, pd);
