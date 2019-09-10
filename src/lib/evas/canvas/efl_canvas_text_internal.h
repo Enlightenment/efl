6918b52762853b022f4f26e9138fd09a91d4f97b
Tom Hacohen <tom@stosb.com>
2019-09-10 19:37:41 +0100

    Remove per_cluster from coord_set.

--- src/lib/evas/canvas/efl_canvas_text_internal.h
+++ src/lib/evas/canvas/efl_canvas_text_internal.h
@@ -111,6 +111,6 @@ Eina_Bool _canvas_text_cursor_geometry_get(const Efl2_Text_Cursor_Handle *cur, E
 void _canvas_text_cursor_content_geometry_get(const Efl2_Text_Cursor_Handle *cur, Evas_Coord *cx, Evas_Coord *cy, Evas_Coord *cw, Evas_Coord *ch);
 Eina_List *_canvas_text_cursor_range_precise_geometry_get(const Efl2_Text_Cursor_Handle *cur1, const Efl2_Text_Cursor_Handle *cur2);
 Eina_Iterator *_canvas_text_cursor_range_simple_geometry_get(const Efl2_Text_Cursor_Handle *cur1, const Efl2_Text_Cursor_Handle *cur2);
-Eina_Bool _canvas_text_cursor_coord_set(Efl2_Text_Cursor_Handle *cur, Evas_Coord x, Evas_Coord y, Eina_Bool per_cluster);
+Eina_Bool _canvas_text_cursor_coord_set(Efl2_Text_Cursor_Handle *cur, Evas_Coord x, Evas_Coord y);
 Eina_Bool _canvas_text_cursor_eol_get(const Efl2_Text_Cursor_Handle *cur);
 #endif
