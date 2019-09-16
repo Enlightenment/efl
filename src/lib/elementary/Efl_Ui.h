bab9180cf711a3e7b092d70b6593f4942b29fc80
Cedric Bail <cedric.bail@free.fr>
2019-09-15 23:18:50 -0700

    elementary: add Efl.Ui.Grid_View by leveraging MVVM and Collection/Position_Manager infrastructure.
    Differential Revision: https://phab.enlightenment.org/D9960

--- src/lib/elementary/Efl_Ui.h
+++ src/lib/elementary/Efl_Ui.h
@@ -286,6 +286,7 @@ typedef Eo Efl_Ui_Spotlight_Indicator;
 # include <efl_ui_view_model.eo.h>
 # include <efl_ui_collection_view.eo.h>
 # include <efl_ui_list_view.eo.h>
+# include <efl_ui_grid_view.eo.h>
 
 /**
  * Initialize Elementary
