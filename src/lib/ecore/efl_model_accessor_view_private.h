77ab3523cdafe3e128d1fa911e93ede14fac96b6
Cedric Bail <cedric.bail@free.fr>
2019-09-18 10:33:23 -0700

    ecore: remove unused piece of code.
    Reviewed-by: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
    Differential Revision: https://phab.enlightenment.org/D10030

--- src/lib/ecore/efl_model_accessor_view_private.h
+++
@@ -1,5 +0,0 @@
-
-typedef Eo*(*Efl_Model_Accessor_View_Constructor_Cb)(void* data, Eo* child);
-
-Eina_Accessor* efl_model_accessor_view_new(Eina_Accessor* accessor,
-                                           Efl_Model_Accessor_View_Constructor_Cb constructor, void* data);
