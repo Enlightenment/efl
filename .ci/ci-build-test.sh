8b2cae5b07150c641124978f92f0727a579b6b17
Mike Blumenkrantz <zmike@samsung.com>
2019-09-19 09:28:57 -0400

    ci: add back target to do out-of-tree test build
    this is necessary in order to ensure that our headers are usable
    
    ref T8228
    
    Reviewed-by: Stefan Schmidt <stefan@datenfreihafen.org>
    Differential Revision: https://phab.enlightenment.org/D10036

---
+++ .ci/ci-build-test.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+. .ci/travis.sh
+travis_fold compile_test compile_test
+if [ "$DISTRO" != "" ] ; then
+  if [ "$1" = "coverity" ] ; then
+    exit 0
+  fi
+  if [ "$1" = "mingw" ] ; then
+    exit 0
+  fi
+  if [ "$1" = "release-ready" ] ; then
+    exit 0
+  fi
+  docker exec --env EIO_MONITOR_POLL=1 $(cat $HOME/cid) .ci/build-example.sh
+else
+  exit 0
+  #FIXME: we don't install efl_ui.pc on osx?
+  export PATH="$(brew --prefix gettext)/bin:$PATH"
+  .ci/build-example.sh
+fi
+travis_endfold compile_test
