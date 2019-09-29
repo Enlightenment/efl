6b5ee8a3339e3f34eeab49a08b1210a997c1ea55
Ali Alzyod <ali198724@gmail.com>
2019-09-29 18:38:12 +0300

    add raw_editable

---
+++ src/lib/elementary/efl2_text_raw_editable.eo.c
@@ -0,0 +1,326 @@
+EWAPI const Efl_Event_Description _EFL2_TEXT_RAW_EDITABLE_EVENT_CHANGED_USER_PRE =
+   EFL_EVENT_DESCRIPTION("changed,user,pre");
+EWAPI const Efl_Event_Description _EFL2_TEXT_RAW_EDITABLE_EVENT_CHANGED_USER =
+   EFL_EVENT_DESCRIPTION("changed,user");
+EWAPI const Efl_Event_Description _EFL2_TEXT_RAW_EDITABLE_EVENT_REDO_REQUEST =
+   EFL_EVENT_DESCRIPTION("redo,request");
+EWAPI const Efl_Event_Description _EFL2_TEXT_RAW_EDITABLE_EVENT_UNDO_REQUEST =
+   EFL_EVENT_DESCRIPTION("undo,request");
+EWAPI const Efl_Event_Description _EFL2_TEXT_RAW_EDITABLE_EVENT_CURSOR_CHANGED_MANUAL =
+   EFL_EVENT_DESCRIPTION("cursor,changed,manual");
+EWAPI const Efl_Event_Description _EFL2_TEXT_RAW_EDITABLE_EVENT_CONTEXT_OPEN =
+   EFL_EVENT_DESCRIPTION("context,open");
+EWAPI const Efl_Event_Description _EFL2_TEXT_RAW_EDITABLE_EVENT_PREEDIT_CHANGED =
+   EFL_EVENT_DESCRIPTION("preedit,changed");
+EWAPI const Efl_Event_Description _EFL2_TEXT_RAW_EDITABLE_EVENT_SELECTION_START =
+   EFL_EVENT_DESCRIPTION("selection,start");
+EWAPI const Efl_Event_Description _EFL2_TEXT_RAW_EDITABLE_EVENT_SELECTION_CHANGED =
+   EFL_EVENT_DESCRIPTION("selection,changed");
+EWAPI const Efl_Event_Description _EFL2_TEXT_RAW_EDITABLE_EVENT_SELECTION_CLEARED =
+   EFL_EVENT_DESCRIPTION("selection,cleared");
+
+void _efl2_text_raw_editable_efl2_input_text_input_panel_show_on_demand_set(Eo *obj, Efl2_Text_Raw_Editable_Data *pd, Eina_Bool ondemand);
+
+
+Eina_Bool _efl2_text_raw_editable_efl2_input_text_input_panel_show_on_demand_get(const Eo *obj, Efl2_Text_Raw_Editable_Data *pd);
+
+
+void _efl2_text_raw_editable_efl2_input_text_input_panel_language_set(Eo *obj, Efl2_Text_Raw_Editable_Data *pd, Efl2_Input_Text_Panel_Lang lang);
+
+
+Efl2_Input_Text_Panel_Lang _efl2_text_raw_editable_efl2_input_text_input_panel_language_get(const Eo *obj, Efl2_Text_Raw_Editable_Data *pd);
+
+
+void _efl2_text_raw_editable_efl2_input_text_input_panel_layout_variation_set(Eo *obj, Efl2_Text_Raw_Editable_Data *pd, int variation);
+
+
+int _efl2_text_raw_editable_efl2_input_text_input_panel_layout_variation_get(const Eo *obj, Efl2_Text_Raw_Editable_Data *pd);
+
+
+void _efl2_text_raw_editable_efl2_input_text_autocapital_type_set(Eo *obj, Efl2_Text_Raw_Editable_Data *pd, Efl2_Input_Text_Autocapital_Type autocapital_type);
+
+
+Efl2_Input_Text_Autocapital_Type _efl2_text_raw_editable_efl2_input_text_autocapital_type_get(const Eo *obj, Efl2_Text_Raw_Editable_Data *pd);
+
+
+void _efl2_text_raw_editable_efl2_input_text_input_panel_return_key_disabled_set(Eo *obj, Efl2_Text_Raw_Editable_Data *pd, Eina_Bool disabled);
+
+
+Eina_Bool _efl2_text_raw_editable_efl2_input_text_input_panel_return_key_disabled_get(const Eo *obj, Efl2_Text_Raw_Editable_Data *pd);
+
+
+void _efl2_text_raw_editable_efl2_input_text_prediction_allow_set(Eo *obj, Efl2_Text_Raw_Editable_Data *pd, Eina_Bool prediction);
+
+
+Eina_Bool _efl2_text_raw_editable_efl2_input_text_prediction_allow_get(const Eo *obj, Efl2_Text_Raw_Editable_Data *pd);
+
+
+void _efl2_text_raw_editable_efl2_input_text_input_hint_set(Eo *obj, Efl2_Text_Raw_Editable_Data *pd, Efl2_Input_Text_Hints hints);
+
+
+Efl2_Input_Text_Hints _efl2_text_raw_editable_efl2_input_text_input_hint_get(const Eo *obj, Efl2_Text_Raw_Editable_Data *pd);
+
+
+void _efl2_text_raw_editable_efl2_input_text_input_panel_layout_set(Eo *obj, Efl2_Text_Raw_Editable_Data *pd, Efl2_Input_Text_Panel_Layout layout);
+
+
+Efl2_Input_Text_Panel_Layout _efl2_text_raw_editable_efl2_input_text_input_panel_layout_get(const Eo *obj, Efl2_Text_Raw_Editable_Data *pd);
+
+
+void _efl2_text_raw_editable_efl2_input_text_input_panel_return_key_type_set(Eo *obj, Efl2_Text_Raw_Editable_Data *pd, Efl2_Input_Text_Panel_Return_Key_Type return_key_type);
+
+
+Efl2_Input_Text_Panel_Return_Key_Type _efl2_text_raw_editable_efl2_input_text_input_panel_return_key_type_get(const Eo *obj, Efl2_Text_Raw_Editable_Data *pd);
+
+
+void _efl2_text_raw_editable_efl2_input_text_input_panel_enabled_set(Eo *obj, Efl2_Text_Raw_Editable_Data *pd, Eina_Bool enabled);
+
+
+Eina_Bool _efl2_text_raw_editable_efl2_input_text_input_panel_enabled_get(const Eo *obj, Efl2_Text_Raw_Editable_Data *pd);
+
+
+void _efl2_text_raw_editable_efl2_input_text_input_panel_return_key_autoenabled_set(Eo *obj, Efl2_Text_Raw_Editable_Data *pd, Eina_Bool enabled);
+
+
+void _efl2_text_raw_editable_efl2_input_text_input_panel_show(Eo *obj, Efl2_Text_Raw_Editable_Data *pd);
+
+
+void _efl2_text_raw_editable_efl2_input_text_input_panel_imdata_set(Eo *obj, Efl2_Text_Raw_Editable_Data *pd, const void *data, int len);
+
+
+void _efl2_text_raw_editable_efl2_input_text_input_panel_imdata_get(const Eo *obj, Efl2_Text_Raw_Editable_Data *pd, void *data, int *len);
+
+
+void _efl2_text_raw_editable_efl2_input_text_input_panel_hide(Eo *obj, Efl2_Text_Raw_Editable_Data *pd);
+
+
+void _efl2_text_raw_editable_editable_set(Eo *obj, Efl2_Text_Raw_Editable_Data *pd, Eina_Bool editable);
+
+
+static Eina_Error
+__eolian_efl2_text_raw_editable_editable_set_reflect(Eo *obj, Eina_Value val)
+{
+   Eina_Error r = 0;   Eina_Bool cval;
+   if (!eina_value_bool_convert(&val, &cval))
+      {
+         r = EINA_ERROR_VALUE_FAILED;
+         goto end;
+      }
+   efl2_text_raw_editable_set(obj, cval);
+ end:
+   eina_value_flush(&val);
+   return r;
+}
+
+EOAPI EFL_VOID_FUNC_BODYV(efl2_text_raw_editable_set, EFL_FUNC_CALL(editable), Eina_Bool editable);
+
+Eina_Bool _efl2_text_raw_editable_editable_get(const Eo *obj, Efl2_Text_Raw_Editable_Data *pd);
+
+
+static Eina_Value
+__eolian_efl2_text_raw_editable_editable_get_reflect(const Eo *obj)
+{
+   Eina_Bool val = efl2_text_raw_editable_get(obj);
+   return eina_value_bool_init(val);
+}
+
+EOAPI EFL_FUNC_BODY_CONST(efl2_text_raw_editable_get, Eina_Bool, 0);
+
+void _efl2_text_raw_editable_password_mode_set(Eo *obj, Efl2_Text_Raw_Editable_Data *pd, Eina_Bool enabled);
+
+
+static Eina_Error
+__eolian_efl2_text_raw_editable_password_mode_set_reflect(Eo *obj, Eina_Value val)
+{
+   Eina_Error r = 0;   Eina_Bool cval;
+   if (!eina_value_bool_convert(&val, &cval))
+      {
+         r = EINA_ERROR_VALUE_FAILED;
+         goto end;
+      }
+   efl2_text_raw_editable_password_mode_set(obj, cval);
+ end:
+   eina_value_flush(&val);
+   return r;
+}
+
+EOAPI EFL_VOID_FUNC_BODYV(efl2_text_raw_editable_password_mode_set, EFL_FUNC_CALL(enabled), Eina_Bool enabled);
+
+Eina_Bool _efl2_text_raw_editable_password_mode_get(const Eo *obj, Efl2_Text_Raw_Editable_Data *pd);
+
+
+static Eina_Value
+__eolian_efl2_text_raw_editable_password_mode_get_reflect(const Eo *obj)
+{
+   Eina_Bool val = efl2_text_raw_editable_password_mode_get(obj);
+   return eina_value_bool_init(val);
+}
+
+EOAPI EFL_FUNC_BODY_CONST(efl2_text_raw_editable_password_mode_get, Eina_Bool, 0);
+
+void _efl2_text_raw_editable_replacement_char_set(Eo *obj, Efl2_Text_Raw_Editable_Data *pd, const char *repch);
+
+
+static Eina_Error
+__eolian_efl2_text_raw_editable_replacement_char_set_reflect(Eo *obj, Eina_Value val)
+{
+   Eina_Error r = 0;   const char *cval;
+   if (!eina_value_string_convert(&val, &cval))
+      {
+         r = EINA_ERROR_VALUE_FAILED;
+         goto end;
+      }
+   efl2_text_raw_editable_replacement_char_set(obj, cval);
+ end:
+   eina_value_flush(&val);
+   return r;
+}
+
+EOAPI EFL_VOID_FUNC_BODYV(efl2_text_raw_editable_replacement_char_set, EFL_FUNC_CALL(repch), const char *repch);
+
+const char *_efl2_text_raw_editable_replacement_char_get(const Eo *obj, Efl2_Text_Raw_Editable_Data *pd);
+
+
+static Eina_Value
+__eolian_efl2_text_raw_editable_replacement_char_get_reflect(const Eo *obj)
+{
+   const char *val = efl2_text_raw_editable_replacement_char_get(obj);
+   return eina_value_string_init(val);
+}
+
+EOAPI EFL_FUNC_BODY_CONST(efl2_text_raw_editable_replacement_char_get, const char *, NULL);
+
+Efl2_Text_Cursor *_efl2_text_raw_editable_main_cursor_get(const Eo *obj, Efl2_Text_Raw_Editable_Data *pd);
+
+EOAPI EFL_FUNC_BODY_CONST(efl2_text_raw_editable_main_cursor_get, Efl2_Text_Cursor *, NULL);
+
+void _efl2_text_raw_editable_selection_allowed_set(Eo *obj, Efl2_Text_Raw_Editable_Data *pd, Eina_Bool allowed);
+
+
+static Eina_Error
+__eolian_efl2_text_raw_editable_selection_allowed_set_reflect(Eo *obj, Eina_Value val)
+{
+   Eina_Error r = 0;   Eina_Bool cval;
+   if (!eina_value_bool_convert(&val, &cval))
+      {
+         r = EINA_ERROR_VALUE_FAILED;
+         goto end;
+      }
+   efl2_text_raw_editable_selection_allowed_set(obj, cval);
+ end:
+   eina_value_flush(&val);
+   return r;
+}
+
+EOAPI EFL_VOID_FUNC_BODYV(efl2_text_raw_editable_selection_allowed_set, EFL_FUNC_CALL(allowed), Eina_Bool allowed);
+
+Eina_Bool _efl2_text_raw_editable_selection_allowed_get(const Eo *obj, Efl2_Text_Raw_Editable_Data *pd);
+
+
+static Eina_Value
+__eolian_efl2_text_raw_editable_selection_allowed_get_reflect(const Eo *obj)
+{
+   Eina_Bool val = efl2_text_raw_editable_selection_allowed_get(obj);
+   return eina_value_bool_init(val);
+}
+
+EOAPI EFL_FUNC_BODY_CONST(efl2_text_raw_editable_selection_allowed_get, Eina_Bool, 0);
+
+void _efl2_text_raw_editable_selection_cursors_get(const Eo *obj, Efl2_Text_Raw_Editable_Data *pd, Efl2_Text_Cursor **start, Efl2_Text_Cursor **end);
+
+EOAPI EFL_VOID_FUNC_BODYV_CONST(efl2_text_raw_editable_selection_cursors_get, EFL_FUNC_CALL(start, end), Efl2_Text_Cursor **start, Efl2_Text_Cursor **end);
+
+void _efl2_text_raw_editable_selection_start(Eo *obj, Efl2_Text_Raw_Editable_Data *pd);
+
+EOAPI EFL_VOID_FUNC_BODY(efl2_text_raw_editable_selection_start);
+
+void _efl2_text_raw_editable_selection_end(Eo *obj, Efl2_Text_Raw_Editable_Data *pd);
+
+EOAPI EFL_VOID_FUNC_BODY(efl2_text_raw_editable_selection_end);
+
+void _efl2_text_raw_editable_select_none(Eo *obj, Efl2_Text_Raw_Editable_Data *pd);
+
+EOAPI EFL_VOID_FUNC_BODY(efl2_text_raw_editable_select_none);
+
+static Eina_Bool
+_efl2_text_raw_editable_class_initializer(Efl_Class *klass)
+{
+   const Efl_Object_Ops *opsp = NULL;
+
+   const Efl_Object_Property_Reflection_Ops *ropsp = NULL;
+
+#ifndef EFL2_TEXT_RAW_EDITABLE_EXTRA_OPS
+#define EFL2_TEXT_RAW_EDITABLE_EXTRA_OPS
+#endif
+
+   EFL_OPS_DEFINE(ops,
+      EFL_OBJECT_OP_FUNC(efl_constructor, _efl2_text_raw_editable_efl_object_constructor),
+      EFL_OBJECT_OP_FUNC(efl_finalize, _efl2_text_raw_editable_efl_object_finalize),
+      EFL_OBJECT_OP_FUNC(efl_destructor, _efl2_text_raw_editable_efl_object_destructor),
+      EFL_OBJECT_OP_FUNC(efl2_input_text_input_panel_show_on_demand_set, _efl2_text_raw_editable_efl2_input_text_input_panel_show_on_demand_set),
+      EFL_OBJECT_OP_FUNC(efl2_input_text_input_panel_show_on_demand_get, _efl2_text_raw_editable_efl2_input_text_input_panel_show_on_demand_get),
+      EFL_OBJECT_OP_FUNC(efl2_input_text_input_panel_language_set, _efl2_text_raw_editable_efl2_input_text_input_panel_language_set),
+      EFL_OBJECT_OP_FUNC(efl2_input_text_input_panel_language_get, _efl2_text_raw_editable_efl2_input_text_input_panel_language_get),
+      EFL_OBJECT_OP_FUNC(efl2_input_text_input_panel_layout_variation_set, _efl2_text_raw_editable_efl2_input_text_input_panel_layout_variation_set),
+      EFL_OBJECT_OP_FUNC(efl2_input_text_input_panel_layout_variation_get, _efl2_text_raw_editable_efl2_input_text_input_panel_layout_variation_get),
+      EFL_OBJECT_OP_FUNC(efl2_input_text_autocapital_type_set, _efl2_text_raw_editable_efl2_input_text_autocapital_type_set),
+      EFL_OBJECT_OP_FUNC(efl2_input_text_autocapital_type_get, _efl2_text_raw_editable_efl2_input_text_autocapital_type_get),
+      EFL_OBJECT_OP_FUNC(efl2_input_text_input_panel_return_key_disabled_set, _efl2_text_raw_editable_efl2_input_text_input_panel_return_key_disabled_set),
+      EFL_OBJECT_OP_FUNC(efl2_input_text_input_panel_return_key_disabled_get, _efl2_text_raw_editable_efl2_input_text_input_panel_return_key_disabled_get),
+      EFL_OBJECT_OP_FUNC(efl2_input_text_prediction_allow_set, _efl2_text_raw_editable_efl2_input_text_prediction_allow_set),
+      EFL_OBJECT_OP_FUNC(efl2_input_text_prediction_allow_get, _efl2_text_raw_editable_efl2_input_text_prediction_allow_get),
+      EFL_OBJECT_OP_FUNC(efl2_input_text_input_hint_set, _efl2_text_raw_editable_efl2_input_text_input_hint_set),
+      EFL_OBJECT_OP_FUNC(efl2_input_text_input_hint_get, _efl2_text_raw_editable_efl2_input_text_input_hint_get),
+      EFL_OBJECT_OP_FUNC(efl2_input_text_input_panel_layout_set, _efl2_text_raw_editable_efl2_input_text_input_panel_layout_set),
+      EFL_OBJECT_OP_FUNC(efl2_input_text_input_panel_layout_get, _efl2_text_raw_editable_efl2_input_text_input_panel_layout_get),
+      EFL_OBJECT_OP_FUNC(efl2_input_text_input_panel_return_key_type_set, _efl2_text_raw_editable_efl2_input_text_input_panel_return_key_type_set),
+      EFL_OBJECT_OP_FUNC(efl2_input_text_input_panel_return_key_type_get, _efl2_text_raw_editable_efl2_input_text_input_panel_return_key_type_get),
+      EFL_OBJECT_OP_FUNC(efl2_input_text_input_panel_enabled_set, _efl2_text_raw_editable_efl2_input_text_input_panel_enabled_set),
+      EFL_OBJECT_OP_FUNC(efl2_input_text_input_panel_enabled_get, _efl2_text_raw_editable_efl2_input_text_input_panel_enabled_get),
+      EFL_OBJECT_OP_FUNC(efl2_input_text_input_panel_return_key_autoenabled_set, _efl2_text_raw_editable_efl2_input_text_input_panel_return_key_autoenabled_set),
+      EFL_OBJECT_OP_FUNC(efl2_input_text_input_panel_show, _efl2_text_raw_editable_efl2_input_text_input_panel_show),
+      EFL_OBJECT_OP_FUNC(efl2_input_text_input_panel_imdata_set, _efl2_text_raw_editable_efl2_input_text_input_panel_imdata_set),
+      EFL_OBJECT_OP_FUNC(efl2_input_text_input_panel_imdata_get, _efl2_text_raw_editable_efl2_input_text_input_panel_imdata_get),
+      EFL_OBJECT_OP_FUNC(efl2_input_text_input_panel_hide, _efl2_text_raw_editable_efl2_input_text_input_panel_hide),
+      EFL_OBJECT_OP_FUNC(efl2_text_raw_editable_set, _efl2_text_raw_editable_editable_set),
+      EFL_OBJECT_OP_FUNC(efl2_text_raw_editable_get, _efl2_text_raw_editable_editable_get),
+      EFL_OBJECT_OP_FUNC(efl2_text_raw_editable_password_mode_set, _efl2_text_raw_editable_password_mode_set),
+      EFL_OBJECT_OP_FUNC(efl2_text_raw_editable_password_mode_get, _efl2_text_raw_editable_password_mode_get),
+      EFL_OBJECT_OP_FUNC(efl2_text_raw_editable_replacement_char_set, _efl2_text_raw_editable_replacement_char_set),
+      EFL_OBJECT_OP_FUNC(efl2_text_raw_editable_replacement_char_get, _efl2_text_raw_editable_replacement_char_get),
+      EFL_OBJECT_OP_FUNC(efl2_text_raw_editable_main_cursor_get, _efl2_text_raw_editable_main_cursor_get),
+      EFL_OBJECT_OP_FUNC(efl2_text_raw_editable_selection_allowed_set, _efl2_text_raw_editable_selection_allowed_set),
+      EFL_OBJECT_OP_FUNC(efl2_text_raw_editable_selection_allowed_get, _efl2_text_raw_editable_selection_allowed_get),
+      EFL_OBJECT_OP_FUNC(efl2_text_raw_editable_selection_cursors_get, _efl2_text_raw_editable_selection_cursors_get),
+      EFL_OBJECT_OP_FUNC(efl2_text_raw_editable_selection_start, _efl2_text_raw_editable_selection_start),
+      EFL_OBJECT_OP_FUNC(efl2_text_raw_editable_selection_end, _efl2_text_raw_editable_selection_end),
+      EFL_OBJECT_OP_FUNC(efl2_text_raw_editable_select_none, _efl2_text_raw_editable_select_none),
+      EFL2_TEXT_RAW_EDITABLE_EXTRA_OPS
+   );
+   opsp = &ops;
+
+   static const Efl_Object_Property_Reflection refl_table[] = {
+      {"editable", __eolian_efl2_text_raw_editable_editable_set_reflect, __eolian_efl2_text_raw_editable_editable_get_reflect},
+      {"password_mode", __eolian_efl2_text_raw_editable_password_mode_set_reflect, __eolian_efl2_text_raw_editable_password_mode_get_reflect},
+      {"replacement_char", __eolian_efl2_text_raw_editable_replacement_char_set_reflect, __eolian_efl2_text_raw_editable_replacement_char_get_reflect},
+      {"selection_allowed", __eolian_efl2_text_raw_editable_selection_allowed_set_reflect, __eolian_efl2_text_raw_editable_selection_allowed_get_reflect},
+   };
+   static const Efl_Object_Property_Reflection_Ops rops = {
+      refl_table, EINA_C_ARRAY_LENGTH(refl_table)
+   };
+   ropsp = &rops;
+
+   return efl_class_functions_set(klass, opsp, ropsp);
+}
+
+static const Efl_Class_Description _efl2_text_raw_editable_class_desc = {
+   EO_VERSION,
+   "Efl2.Text.Raw_Editable",
+   EFL_CLASS_TYPE_REGULAR,
+   sizeof(Efl2_Text_Raw_Editable_Data),
+   _efl2_text_raw_editable_class_initializer,
+   NULL,
+   NULL
+};
+
+EFL_DEFINE_CLASS(efl2_text_raw_editable_class_get, &_efl2_text_raw_editable_class_desc, EFL2_CANVAS_TEXT_CLASS, EFL2_INPUT_TEXT_INTERFACE, NULL);
