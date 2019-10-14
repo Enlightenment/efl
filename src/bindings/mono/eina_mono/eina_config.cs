8ba2638f0ea0e5542cb93d9880e3b0740b8d325e
Bruno da Silva Belo <brunodasilvabelo@gmail.com>
2019-10-14 12:23:56 -0300

    csharp: updating eina_config docs and hide api.
    Reviewers: felipealmeida, lauromoura, woohyun, segfaultxavi
    
    Reviewed By: lauromoura
    
    Subscribers: cedric, #reviewers, #committers
    
    Tags: #efl
    
    Maniphest Tasks: T8293
    
    Differential Revision: https://phab.enlightenment.org/D10312

--- src/bindings/mono/eina_mono/eina_config.cs
+++ src/bindings/mono/eina_mono/eina_config.cs
@@ -2,20 +2,24 @@
 
 using System;
 using System.Runtime.InteropServices;
+using System.ComponentModel;
 
 namespace Eina
 {
 
 /// <summary>
 /// Manage the initialization and cleanup for eina.
-///
-/// Since EFL 1.23.
+/// <para>Since EFL 1.23.</para>
 /// </summary>
 public class Config
 {
     [DllImport(efl.Libs.Eina)] private static extern int eina_init();
     [DllImport(efl.Libs.Eina)] private static extern int eina_shutdown();
 
+    /// <summary>
+    /// Initialize the Eina library.
+    /// <para>Since EFL 1.23.</para>
+    /// </summary>
     public static void Init()
     {
         if (eina_init() == 0)
@@ -24,6 +28,10 @@ public class Config
         }
     }
 
+    /// <summary>
+    /// Finalize the Eina library.
+    /// <para>Since EFL 1.23.</para>
+    /// </summary>
     public static int Shutdown()
     {
         return eina_shutdown();
@@ -33,29 +41,41 @@ public class Config
 
 /// <summary>
 /// Wrapper class for pointers that need some cleanup afterwards like strings
-///
-/// Since EFL 1.23.
+/// <para>Since EFL 1.23.</para>
 /// </summary>
 public class DisposableIntPtr : IDisposable
 {
+    [EditorBrowsable(EditorBrowsableState.Never)]
     public IntPtr Handle { get; set; }
     private bool ShouldFree;
     private bool Disposed;
 
     /// <summary>Wraps a new ptr what will be freed based on the
-    /// value of shouldFree</summary>
+    /// value of shouldFree
+    /// <para>Since EFL 1.23.</para>
+    /// </summary>
     public DisposableIntPtr(IntPtr ptr, bool shouldFree = false)
     {
         Handle = ptr;
         ShouldFree = shouldFree;
     }
 
+
+    /// <summary>Release the native resources held by this instance.
+    /// <para>Since EFL 1.23.</para>
+    /// </summary>
     public void Dispose()
     {
         Dispose(true);
         GC.SuppressFinalize(this);
     }
 
+    /// <summary>Disposes of this wrapper, releasing the native handle if
+    /// owned.
+    /// <para>Since EFL 1.23.</para>
+    /// </summary>
+    /// <param name="disposing">True if this was called from <see cref="Dispose()"/> public method. False if
+    /// called from the C# finalizer.</param>
     protected virtual void Dispose(bool disposing)
     {
         if (!Disposed && ShouldFree)
@@ -66,6 +86,10 @@ public class DisposableIntPtr : IDisposable
         Disposed = true;
     }
 
+    
+    /// <summary>Release the native resources held by this instance.
+    /// <para>Since EFL 1.23.</para>
+    /// </summary>
     ~DisposableIntPtr()
     {
         Dispose(false);
