92199b3450f294729864b643afc874234ddcab5b
Lauro Moura <lauromoura@expertisesolutions.com.br>
2019-10-24 10:01:50 -0300

    csharp: Add licensing information.
    Summary:
    C# bindings will be lincensed under Apache Sofware License 2.0.
    
    This commit adds the license text to the licenses folder and a copyright
    notice to the binding files.
    
    Fixes T8039
    
    Reviewers: woohyun, felipealmeida, vitor.sousa
    
    Reviewed By: felipealmeida
    
    Subscribers: cedric, #reviewers, #committers
    
    Tags: #efl
    
    Maniphest Tasks: T8039
    
    Differential Revision: https://phab.enlightenment.org/D9414

--- src/bin/eolian_mono/eolian_mono.cc
+++ src/bin/eolian_mono/eolian_mono.cc
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2019 by its authors. See AUTHORS.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #include <iostream>
 #include <fstream>
@@ -128,6 +143,25 @@ run(options_type const& opts)
            return std::ostream_iterator<char>(output_file);
          }
      }()};
+
+   if (!as_generator(
+              "/*\n"
+              " * Copyright 2019 by its authors. See AUTHORS.\n"
+              " *\n"
+              " * Licensed under the Apache License, Version 2.0 (the \"License\");\n"
+              " * you may not use this file except in compliance with the License.\n"
+              " * You may obtain a copy of the License at\n"
+              " *\n"
+              " *     http://www.apache.org/licenses/LICENSE-2.0\n"
+              " *\n"
+              " * Unless required by applicable law or agreed to in writing, software\n"
+              " * distributed under the License is distributed on an \"AS IS\" BASIS,\n"
+              " * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n"
+              " * See the License for the specific language governing permissions and\n"
+              " * limitations under the License.\n"
+              " */\n\n"
+        ).generate(iterator, attributes::unused, efl::eolian::grammar::context_null()))
+     throw std::runtime_error("Failed to generate license notice");
 
    if (!as_generator("#pragma warning disable CS1591\n").generate(iterator, efl::eolian::grammar::attributes::unused, efl::eolian::grammar::context_null()))
      throw std::runtime_error("Failed to generate pragma to disable missing docs");
