91b1033a983e4008e4eed1ea01ac8b76b6a95ee1
Felipe Magno de Almeida <felipe@expertisesolutions.com.br>
2019-10-24 10:36:42 -0300

    cxx: Add license to CXX bindings.
    Summary:
    Like C#, it will also be Apache 2.0
    
    Depends on D9414
    
    Reviewers: vitor.sousa, woohyun, jpeg, lauromoura
    
    Reviewed By: lauromoura
    
    Subscribers: jpeg, cedric, #reviewers, #committers
    
    Tags: #efl
    
    Differential Revision: https://phab.enlightenment.org/D9418

--- src/examples/elementary/bg_cxx_example_02.cc
+++ src/examples/elementary/bg_cxx_example_02.cc
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2019 by its authors. See AUTHORS.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 /* To compile:
  * g++ `pkg-config --cflags --libs elementary-cxx eo-cxx efl-cxx eina-cxx ecore-cxx evas-cxx edje-cxx` ./bg_cxx_example_02.cc -o bg_cxx_example_02
  *
