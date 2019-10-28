1db12b5fb4c0160c0d486a0e6d2a8bd748820881
Lauro Moura <lauromoura@expertisesolutions.com.br>
2019-10-28 19:04:33 -0300

    mono: encapsulate internal FunctionWrapper
    Summary: Depends on D10340
    
    Test Plan: meson setup -Dbindings=mono,cxx -Dmono-beta=true
    
    Reviewers: lauromoura, segfaultxavi, Jaehyun_Cho
    
    Reviewed By: lauromoura
    
    Subscribers: cedric, #reviewers, woohyun, #committers
    
    Tags: #efl
    
    Differential Revision: https://phab.enlightenment.org/D10342

--- src/bindings/mono/eo_mono/FunctionWrapper_Windows.cs
+++ src/bindings/mono/eo_mono/FunctionWrapper_Windows.cs
@@ -19,10 +19,10 @@ using System.Runtime.InteropServices;
 namespace Efl.Eo
 {
 
-public static partial class FunctionInterop
+static partial class FunctionInterop
 {
     [DllImport(efl.Libs.Libdl)]
-    public static extern IntPtr GetProcAddress(IntPtr handle, string symbol);
+    internal static extern IntPtr GetProcAddress(IntPtr handle, string symbol);
 
     private static IntPtr LoadFunctionPointer(IntPtr nativeLibraryHandle, string functionName)
         => FunctionInterop.GetProcAddress(nativeLibraryHandle, functionName);
