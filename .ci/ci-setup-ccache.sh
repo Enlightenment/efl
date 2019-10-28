f8ecd60e2e5c96e2b36d16fee52f5d44268e2afe
Mike Blumenkrantz <zmike@samsung.com>
2019-10-28 08:12:13 -0400

    ci: remove existing base_dir ccache config on osx build before adding new one
    ensure that this file does not balloon to infinite size
    
    Differential Revision: https://phab.enlightenment.org/D10538

--- .ci/ci-setup-ccache.sh
+++ .ci/ci-setup-ccache.sh
@@ -7,6 +7,7 @@ if [ "$DISTRO" != "" ] ; then
   docker exec $(cat $HOME/cid)  ccache -pz
 else
   cp .ci/ccache.conf ~/.ccache
+  sed -iE '/^base_dir/d' ~/.ccache/ccache.conf
   echo "base_dir = $pwd" >> ~/.ccache/ccache.conf
   ccache -pz
 fi
