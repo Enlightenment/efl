ad692d2cbd38f4835050113a2afdaf7bf1cf9e81
Christopher Michael <cp.michael@samsung.com>
2019-11-05 09:13:46 -0500

    ecore-wl2: Move ecore_wl2_buffer_discard function to be internal
    As this function is only used in 1 place and will likely never be used
    outside of the modular dmabuf engine, we can move it to be internal
    only.
    
    ref T8013

--- src/lib/ecore_wl2/ecore_wl2_internal.h
+++ src/lib/ecore_wl2/ecore_wl2_internal.h
@@ -47,6 +47,7 @@ EAPI void ecore_wl2_buffer_destroy(Ecore_Wl2_Buffer *b);
 EAPI struct wl_buffer *ecore_wl2_buffer_wl_buffer_get(Ecore_Wl2_Buffer *buf);
 EAPI void *ecore_wl2_buffer_map(Ecore_Wl2_Buffer *buf, int *w, int *h, int *stride);
 EAPI void ecore_wl2_buffer_unmap(Ecore_Wl2_Buffer *buf);
+EAPI void ecore_wl2_buffer_discard(Ecore_Wl2_Buffer *buf);
 
 # undef EAPI
 # define EAPI
