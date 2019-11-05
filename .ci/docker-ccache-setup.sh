38b011fb2f73d3efd423125aac75502944caeccf
Stefan Schmidt <s.schmidt@samsung.com>
2019-11-05 15:26:36 +0100

    ci: work around ccache segafult during setup
    This is the same fix we applied for the macOS. Somehow ccache segfaults
    during the init. This only shows on the Ubuntu 19.10 update.
    
    Reviewed-by: Marcel Hollerbach <mail@marcel-hollerbach.de>
    Differential Revision: https://phab.enlightenment.org/D10599

--- .ci/docker-ccache-setup.sh
+++ .ci/docker-ccache-setup.sh
@@ -7,5 +7,6 @@ cp .ci/ccache.conf ~/.ccache
 if [ "$1" = "release-ready" ] ; then
   ccache -o base_dir="$(pwd)/$(grep '^PACKAGE_STRING' config.log|cut -d\' -f2|tr ' ' -)"
 else
-  ccache -o base_dir=$(pwd)
+  sed -iE '/^base_dir/d' ~/.ccache/ccache.conf
+  echo "base_dir = $pwd" >> ~/.ccache/ccache.conf
 fi
