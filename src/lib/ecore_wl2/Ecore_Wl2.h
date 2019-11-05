752b541e6c3474c6be4b272c8905c3dd51aef4ea
Christopher Michael <cp.michael@samsung.com>
2019-11-05 09:18:26 -0500

    ecore-wl2: Move ecore_wl2_surface_destroy function to be internal
    As this function is only used in 1 place and will likely never be used
    outside of the modular dmabuf engine, we can move it to be internal
    only.
    
    ref T8013

--- src/lib/ecore_wl2/Ecore_Wl2.h
+++ src/lib/ecore_wl2/Ecore_Wl2.h
@@ -2006,7 +2006,6 @@ EAPI void ecore_wl2_window_update_begin(Ecore_Wl2_Window *window);
 /* TODO: doxygen if we are keeping any of the below functions public */
 
 
-EAPI void ecore_wl2_surface_destroy(Ecore_Wl2_Surface *surface);
 EAPI void ecore_wl2_surface_reconfigure(Ecore_Wl2_Surface *surface, int w, int h, uint32_t flags, Eina_Bool alpha);
 EAPI void *ecore_wl2_surface_data_get(Ecore_Wl2_Surface *surface, int *w, int *h);
 EAPI int  ecore_wl2_surface_assign(Ecore_Wl2_Surface *surface);
--- src/lib/ecore_wl2/ecore_wl2_internal.h
+++ src/lib/ecore_wl2/ecore_wl2_internal.h
@@ -58,6 +58,7 @@ EAPI void ecore_wl2_buffer_age_set(Ecore_Wl2_Buffer *buffer, int age);
 EAPI void ecore_wl2_buffer_age_inc(Ecore_Wl2_Buffer *buffer);
 
 EAPI Ecore_Wl2_Surface *ecore_wl2_surface_create(Ecore_Wl2_Window *win, Eina_Bool alpha);
+EAPI void ecore_wl2_surface_destroy(Ecore_Wl2_Surface *surface);
 
 # undef EAPI
 # define EAPI
