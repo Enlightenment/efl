307837d1991d0c553229fafdc922629e31c27f6f
Christopher Michael <cp.michael@samsung.com>
2019-11-05 09:20:42 -0500

    ecore-wl2: Move ecore_wl2_window_surface_flush function to be internal
    As this function is only used in 1 place and will likely never be used
    outside of the modular dmabuf engine, we can move it to be internal
    only.
    
    ref T8013

--- src/lib/ecore_wl2/Ecore_Wl2.h
+++ src/lib/ecore_wl2/Ecore_Wl2.h
@@ -2006,7 +2006,6 @@ EAPI void ecore_wl2_window_update_begin(Ecore_Wl2_Window *window);
 /* TODO: doxygen if we are keeping any of the below functions public */
 
 
-EAPI void ecore_wl2_window_surface_flush(Ecore_Wl2_Window *window, Eina_Bool purge);
 EAPI Ecore_Wl2_Buffer *ecore_wl2_surface_buffer_create(Ecore_Wl2_Surface *surface);
 EAPI int ecore_wl2_surface_manager_add(Ecore_Wl2_Surface_Interface *intf);
 EAPI void ecore_wl2_surface_manager_del(Ecore_Wl2_Surface_Interface *intf);
--- src/lib/ecore_wl2/ecore_wl2_internal.h
+++ src/lib/ecore_wl2/ecore_wl2_internal.h
@@ -64,6 +64,7 @@ EAPI void *ecore_wl2_surface_data_get(Ecore_Wl2_Surface *surface, int *w, int *h
 EAPI int  ecore_wl2_surface_assign(Ecore_Wl2_Surface *surface);
 EAPI void ecore_wl2_surface_post(Ecore_Wl2_Surface *surface, Eina_Rectangle *rects, unsigned int count);
 EAPI void ecore_wl2_surface_flush(Ecore_Wl2_Surface *surface, Eina_Bool purge);
+EAPI void ecore_wl2_window_surface_flush(Ecore_Wl2_Window *window, Eina_Bool purge);
 
 # undef EAPI
 # define EAPI
