5b6b1b10ee7ec47503a9e8f256434daf6220792a
Stefan Schmidt <s.schmidt@samsung.com>
2019-11-11 09:58:18 +0100

    ci: fix ccache segfault during setup on CI in release-ready build
    Hopefully this is the last place we need to apply this. It has been
    bugging the cron daily CI builds a while now.
    
    Differential Revision: https://phab.enlightenment.org/D10643
    
    Signed-off-by: Stefan Schmidt <s.schmidt@samsung.com>

--- .ci/docker-ccache-setup.sh
+++ .ci/docker-ccache-setup.sh
@@ -5,7 +5,8 @@ CI_BUILD_TYPE="$1"
 cp .ci/ccache.conf ~/.ccache
 
 if [ "$1" = "release-ready" ] ; then
-  ccache -o base_dir="$(pwd)/$(grep '^PACKAGE_STRING' config.log|cut -d\' -f2|tr ' ' -)"
+  sed -iE '/^base_dir/d' ~/.ccache/ccache.conf
+  echo "base_dir = $(pwd)/$(grep '^PACKAGE_STRING' config.log|cut -d\' -f2|tr ' ' -)" >> ~/.ccache/ccache.conf
 else
   sed -iE '/^base_dir/d' ~/.ccache/ccache.conf
   echo "base_dir = $pwd" >> ~/.ccache/ccache.conf
