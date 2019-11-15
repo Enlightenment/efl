9fb1ae0d3c1cd99ed8e81e4a54cae9216a72c7e0
Stefan Schmidt <s.schmidt@samsung.com>
2019-11-15 09:29:24 +0100

    ci: enable benchmark target again
    It only runs after efl is installed and we need to ensure running with
    Elm buffer engine in our docker build for the graphical collection
    benchmark.
    
    Signed-off-by: Stefan Schmidt <s.schmidt@samsung.com>
    Reviewed-by: Marcel Hollerbach <mail@marcel-hollerbach.de>
    Differential Revision: https://phab.enlightenment.org/D10848

--- .ci/ci-make-benchmark.sh
+++ .ci/ci-make-benchmark.sh
@@ -2,20 +2,14 @@
 
 set -e
 . .ci/travis.sh
-if [ "$1" = "release-ready" ] ; then
-  exit 0
-fi
-if [ "$1" = "coverity" ] ; then
+if [ "$1" != "default" ] ; then
   exit 0
 fi
 travis_fold benchmark "ninja benchmark"
-echo "Nothing to do here, the benchmarks don't seem to terminate"
-#else
-  #if [ "$DISTRO" != "" ] ; then
-    #docker exec --env EIO_MONITOR_POLL=1 $(cat $HOME/cid) make benchmark
-  #else
-    #export PATH="/usr/local/opt/ccache/libexec:$(brew --prefix gettext)/bin:$PATH"
-    #make benchmark
-  #fi
-#fi
+if [ "$DISTRO" != "" ] ; then
+  docker exec --env EIO_MONITOR_POLL=1 --env ELM_ENGINE=buffer $(cat $HOME/cid) ninja benchmark -C build
+else
+  export PATH="/usr/local/opt/ccache/libexec:$(brew --prefix gettext)/bin:$PATH"
+  ninja benchmark -C build
+fi
 travis_endfold benchmark
