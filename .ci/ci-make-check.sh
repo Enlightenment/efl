e6ca452832b60fb16043677a9a750f0ec57eec8f
Stefan Schmidt <s.schmidt@samsung.com>
2019-11-15 09:24:48 +0100

    ci: cleanup the check for old meson versions
    We install the newest version via pip on our CI docker images. No longer
    need to check for this.
    
    Differential Revision: https://phab.enlightenment.org/D10681
    
    Signed-off-by: Stefan Schmidt <s.schmidt@samsung.com>

--- .ci/ci-make-check.sh
+++ .ci/ci-make-check.sh
@@ -22,16 +22,12 @@ NUM_TRIES=5
 
 travis_fold check "ninja test"
 if [ "$DISTRO" != "" ] ; then
-   # disable them for this distros, after meson 0.49 is out, this can be removed
-   # https://github.com/mesonbuild/meson/commit/253c581412d7f2b09af353dd83d943454bd555be
-   if [ "$DISTRO" != "Ubuntu1810" ] && [ "$DISTRO" != "Debian96" ]; then
-     for tries in $(seq 1 ${NUM_TRIES}); do
-         (docker exec --env EINA_LOG_BACKTRACE="0" --env EIO_MONITOR_POLL=1 $(cat $HOME/cid) meson test -t 120 -C build --wrapper dbus-launch ) && break
-         docker exec --env EIO_MONITOR_POLL=1 $(cat $HOME/cid) cat build/meson-logs/testlog-dbus-launch.txt
-         if [ $tries != ${NUM_TRIES} ] ; then echo "tests failed, trying again!" ; fi
-         false
-     done
-   fi
+  for tries in $(seq 1 ${NUM_TRIES}); do
+    (docker exec --env EINA_LOG_BACKTRACE="0" --env EIO_MONITOR_POLL=1 $(cat $HOME/cid) meson test -t 120 -C build --wrapper dbus-launch ) && break
+    docker exec --env EIO_MONITOR_POLL=1 $(cat $HOME/cid) cat build/meson-logs/testlog-dbus-launch.txt
+    if [ $tries != ${NUM_TRIES} ] ; then echo "tests failed, trying again!" ; fi
+      false
+  done
 fi
 ret=$?
 travis_endfold check
