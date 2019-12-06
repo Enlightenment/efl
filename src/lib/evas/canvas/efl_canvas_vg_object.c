885967523b3a7d5c42005b4ba69073b9c422c3e6
Hermet Park <hermetpark@gmail.com>
2019-12-06 15:30:50 +0900

    evas vg: --remove log

--- src/lib/evas/canvas/efl_canvas_vg_object.c
+++ src/lib/evas/canvas/efl_canvas_vg_object.c
@@ -447,7 +447,6 @@ _evas_vg_render(Evas_Object_Protected_Data *obj, Efl_Canvas_Vg_Object_Data *pd,
              ector_buffer_pixels_get(ector, &ppixels, &px, &ph, &pstride);
              Efl_Gfx_Colorspace pcspace = ector_buffer_cspace_get(ector);
              ector_surface_reference_point_get(ector, &px, &py);
-				 ERR("buffer = %p, %d %d %d %d, stride = %d, color = %d", ppixels, px,py, pw, ph, pstride, pcspace);
 
              // Buffer change
              ector_buffer_pixels_set(ector, cd->blend.pixels,
