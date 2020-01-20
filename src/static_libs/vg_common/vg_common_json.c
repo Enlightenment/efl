3f1c7492f9b03e09b396474510e16ae88b78fb8a
JunsuChoi <jsuya.choi@samsung.com>
2020-01-20 17:01:38 +0900

    vg_common_json: Supports transform properties
    Summary:
    Supports scale, position, and rotation properties
    related to transform among properties supported by value provider.
    
    Test Plan:
    Remove 'json' in evas-loaders-disabler
    build
    elementary_test -to "value provider"
    (path : layer.box1)
    
    Reviewers: Hermet, smohanty, kimcinoo
    
    Subscribers: cedric, #reviewers, #committers
    
    Tags: #efl
    
    Differential Revision: https://phab.enlightenment.org/D11129

--- src/static_libs/vg_common/vg_common_json.c
+++ src/static_libs/vg_common/vg_common_json.c
@@ -530,8 +530,19 @@ _value_provider_override(Vg_File_Data *vfd)
           }
         if (flag & EFL_GFX_VG_VALUE_PROVIDER_CHANGE_FLAG_TRANSFORM_MATRIX)
           {
-             //TODO: When the lottie animation's transform property is implemented,
-             //      the transform property override function have to added.
+             Eina_Matrix3 *m;
+             double m11, m13, m21, m22, m23;
+
+             m = efl_gfx_vg_value_provider_transform_get(vp);
+             if (!m) continue;
+
+             keypath = efl_gfx_vg_value_provider_keypath_get(vp);
+             eina_matrix3_values_get(m, &m11, NULL, &m13,
+                                        &m21, &m22, &m23,
+                                        NULL, NULL, NULL);
+             lottie_animation_property_override(lot_anim, LOTTIE_ANIMATION_PROPERTY_TR_SCALE, (char*)keypath, 100.0 * m11, 100.0 * m22);
+             lottie_animation_property_override(lot_anim, LOTTIE_ANIMATION_PROPERTY_TR_ROTATION, (char*)keypath, atan2(m21, m11) * (180.0 / M_PI)); // radian to degree
+             lottie_animation_property_override(lot_anim, LOTTIE_ANIMATION_PROPERTY_TR_POSITION, (char*)keypath, m13, m23);
           }
      }
 }
