30668b44a244132a2ca7872f5a867f245e0e2e0d
Mike Blumenkrantz <zmike@samsung.com>
2020-01-24 13:58:40 -0500

    efl/gesture: expose 'continues' as a public property for recognizers
    we still access this using private data in the in-tree recognizers, but
    now it's also accessible normally to custom recognizers
    
    ref T8503
    
    Differential Revision: https://phab.enlightenment.org/D11175

--- src/lib/evas/gesture/efl_canvas_gesture_recognizer.c
+++ src/lib/evas/gesture/efl_canvas_gesture_recognizer.c
@@ -1,6 +1,8 @@
+#define EFL_CANVAS_GESTURE_RECOGNIZER_PROTECTED
 #include "efl_canvas_gesture_private.h"
 
 #define MY_CLASS                                    EFL_CANVAS_GESTURE_RECOGNIZER_CLASS
+#include "efl_canvas_gesture_recognizer.eo.h"
 
 EOLIAN static Eina_Value *
 _efl_canvas_gesture_recognizer_config_get(const Eo *obj EINA_UNUSED, Efl_Canvas_Gesture_Recognizer_Data *pd, const char *name)
@@ -12,6 +14,19 @@ EOLIAN static void
 _efl_canvas_gesture_recognizer_reset(Eo *obj EINA_UNUSED, Efl_Canvas_Gesture_Recognizer_Data *pd EINA_UNUSED,
                                      Efl_Canvas_Gesture *gesture EINA_UNUSED)
 {
+}
+
+
+EOLIAN static Eina_Bool
+_efl_canvas_gesture_recognizer_continues_get(const Eo *obj EINA_UNUSED, Efl_Canvas_Gesture_Recognizer_Data *pd)
+{
+   return pd->continues;
+}
+
+EOLIAN static void
+_efl_canvas_gesture_recognizer_continues_set(Eo *obj EINA_UNUSED, Efl_Canvas_Gesture_Recognizer_Data *pd, Eina_Bool value)
+{
+   pd->continues = !!value;
 }
 
 int
