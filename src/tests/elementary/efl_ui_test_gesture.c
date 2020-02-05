c0453132df662b0568bb450947760276e0c50d1c
Mike Blumenkrantz <zmike@samsung.com>
2020-02-05 09:55:50 -0500

    tests/gesture: add another tap test to check finger size works
    add some move events in to shake up the recognizers and check whether
    taps are still working
    
    Reviewed-by: Marcel Hollerbach <mail@marcel-hollerbach.de>
    Differential Revision: https://phab.enlightenment.org/D11292

--- src/tests/elementary/efl_ui_test_gesture.c
+++ src/tests/elementary/efl_ui_test_gesture.c
@@ -141,6 +141,22 @@ EFL_START_TEST(test_efl_ui_gesture_taps)
    wait_timer(0.4);
    RESET;
 
+   /* verify finger size */
+   click_object_at(rect, 500, 500);
+   click_object_at(rect, 505, 505);
+   CHECK_ALL(TAP, 2, 0, 2, 0);
+   CHECK_ALL(LONG_TAP, 2, 0, 0, 2);
+   /* UPDATE -> FINISH */
+   CHECK_ALL(DOUBLE_TAP, 1, 2, 1, 0);
+   CHECK_ALL(TRIPLE_TAP, 1, 3, 0, 0);
+   CHECK_ZERO(MOMENTUM);
+   CHECK_ZERO(FLICK);
+   CHECK_ZERO(ZOOM);
+
+   /* clear states */
+   wait_timer(0.4);
+   RESET;
+
    /* verify multiple simultaneous presses treated as same press */
    multi_click_object(rect, 2);
    CHECK_ALL(TAP, 1, 0, 1, 0);
