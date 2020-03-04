c872940baefc5fa04a9d902c20d766dd0ffd3648
Marcel Hollerbach <mail@marcel-hollerbach.de>
2020-03-04 15:03:20 +0100

    elm_label: add EFL_ACCESS_WIDGET_ACTION_MIXIN
    we implement the API from it, so we should have that as a type here.
    
    Differential Revision: https://phab.enlightenment.org/D11447

--- src/lib/elementary/elm_label_eo.c
+++ src/lib/elementary/elm_label_eo.c
@@ -216,6 +216,6 @@ static const Efl_Class_Description _elm_label_class_desc = {
    NULL
 };
 
-EFL_DEFINE_CLASS(elm_label_class_get, &_elm_label_class_desc, EFL_UI_LAYOUT_BASE_CLASS, ELM_LAYOUT_MIXIN, EFL_UI_LEGACY_INTERFACE, NULL);
+EFL_DEFINE_CLASS(elm_label_class_get, &_elm_label_class_desc, EFL_UI_LAYOUT_BASE_CLASS, ELM_LAYOUT_MIXIN, EFL_UI_LEGACY_INTERFACE, EFL_ACCESS_WIDGET_ACTION_MIXIN, NULL);
 
 #include "elm_label_eo.legacy.c"
