73082c6489f24b4dc18fd761c0bda867bea8c918
João Paulo Taylor Ienczak Zanette <jpaulotiz@gmail.com>
2020-03-05 09:16:50 -0300

    windows: Use labels as functions.

--- configure.bat
+++ configure.bat
@@ -1,62 +1,79 @@
 @echo off
-@set vcvars64="C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvars64.bat"
+@setlocal
 
-@Rem ---------------------------------
-@Rem Windows terminal specific options
+call :main
 
-@set CC=clang-cl
-@echo C Compiler: %CC%
-@set CXX=clang-cl
-@echo C++ Compiler: %CXX%
+:check_env_vars
+    echo Running the function!
+exit /B 0
 
-@Rem ---------------------------------
-@Rem Windows terminal specific options
-@set CFLAGS=-fansi-escape-codes -fcolor-diagnostics %CFLAGS%
 
-@Rem ---------------------------------
-@Rem Default flags
-@set CFLAGS=-Wno-language-extension-token %CFLAGS%
+:setup_flags
+    @echo ------------------------------
+    @echo Setting up build flags...
 
-@echo Using CFLAGS=%CFLAGS%
+    :: ---------------------------------
+    :: Compilers
+    @set CC=clang-cl
+    @echo C Compiler: %CC%
+    @set CXX=clang-cl
+    @echo C++ Compiler: %CXX%
 
-@Rem ------------------------------------------------------
-@set MESONFLAGS=^
- -Dopenssl_dir="C:/Users/Tiz/source/pkg/openssl/"^
- -Dregex_include_dir="C:/Users/Tiz/source/pkg/pcre-7.0/include/"^
- -Dregex_dir="C:/Users/Tiz/source/pkg/pcre-7.0/lib/"^
-        -Dcrypto=openssl^
-        -Dnls=false^
-        -Dsystemd=false^
-        -Dglib=false^
-        -Dgstreamer=false^
-        -Ddbus=false^
-        -Daudio=false^
-        -Davahi=false^
-        -Dv4l2=false^
-        -Delua=false^
-        -Dx11=false^
-        -Dphysics=false^
-        -Deeze=false^
-        -Dpulseaudio=false^
-        -Dharfbuzz=false^
-        -Dfribidi=false^
-        -Dfontconfig=false^
-        -Dedje-sound-and-video=false^
-        -Dlibmount=false^
-        "-Devas-loaders-disabler=gst,pdf,ps,raw,svg,rsvg,xcf,bmp,dds,eet,generic,gif,ico,jp2k,jpeg,pmaps,png,psd,tga,tgv,tiff,wbmp,webp,xpm,json"^
-        -Dopengl=none^
-        "-Demotion-loaders-disabler=gstreamer1,libvlc,xine"^
-        -Dbuild-tests=false^
-        -Dbuild-examples=false^
-        -Dbindings=^
-        --native-file native-file-windows.txt
+    :: ---------------------------------
+    :: Windows terminal specific options
+    @set CFLAGS=-fansi-escape-codes -fcolor-diagnostics %CFLAGS%
 
-@if exist build (
-    @echo "Build directory ("build") already exists. Old config will be wiped with `--wipe`."
-    @set MESONFLAGS=%MESONFLAGS% --wipe
-) else (
-    @echo No Creating new build directory.
-)
+    :: ------------------------------------
+    :: Default flags for native compilation
+    @set CFLAGS=-Wno-language-extension-token %CFLAGS%
 
-@echo on
-%vcvars64% && meson build %MESONFLAGS%
+    @echo Using CFLAGS=%CFLAGS%
+
+    :: ------------------------------------------------------
+    @set MESONFLAGS=^
+     -Dopenssl_dir="C:/Users/Tiz/source/pkg/openssl/"^
+     -Dregex_include_dir="C:/Users/Tiz/source/pkg/pcre-7.0/include/"^
+     -Dregex_dir="C:/Users/Tiz/source/pkg/pcre-7.0/lib/"^
+            -Dcrypto=openssl^
+            -Dnls=false^
+            -Dsystemd=false^
+            -Dglib=false^
+            -Dgstreamer=false^
+            -Ddbus=false^
+            -Daudio=false^
+            -Davahi=false^
+            -Dv4l2=false^
+            -Delua=false^
+            -Dx11=false^
+            -Dphysics=false^
+            -Deeze=false^
+            -Dpulseaudio=false^
+            -Dharfbuzz=false^
+            -Dfribidi=false^
+            -Dfontconfig=false^
+            -Dedje-sound-and-video=false^
+            -Dlibmount=false^
+            "-Devas-loaders-disabler=gst,pdf,ps,raw,svg,rsvg,xcf,bmp,dds,eet,generic,gif,ico,jp2k,jpeg,pmaps,png,psd,tga,tgv,tiff,wbmp,webp,xpm,json"^
+            -Dopengl=none^
+            "-Demotion-loaders-disabler=gstreamer1,libvlc,xine"^
+            -Dbuild-tests=false^
+            -Dbuild-examples=false^
+            -Dbindings=^
+            --native-file native-file-windows.txt
+
+    @if exist build (
+        @echo "Build directory ("build") already exists. Old config will be wiped with `--wipe`."
+        @set MESONFLAGS=%MESONFLAGS% --wipe
+    ) else (
+        @echo No Creating new build directory.
+    )
+exit /B 0
+
+:main
+    @set vcvars64="C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvars64.bat"
+    call :setup_flags
+
+    @echo on
+    @echo Running meson with flags: %MESONFLAGS%
+    @echo
+    @%vcvars64% && meson build %MESONFLAGS%
