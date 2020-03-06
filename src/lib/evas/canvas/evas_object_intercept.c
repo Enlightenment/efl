Enlightenment/efl

commit 7c99e0a444d81cc545181682f1a4ae00f76d0873

evas_object_intercept: Add check for NULL 'obj' variable inside macros
This patch adds a small check for a valid 'obj' variable inside the
object_intercept macros, which fixes several Coverity reported issues
of NULL pointer dereference.

Fixes CID1420239 through CID1420258

src/lib/evas/canvas/evas_object_intercept.c
@@ -264,7 +264,7 @@ _evas_object_intercept_call_evas(Evas_Object_Protected_Data *obj,
      return;                                                            \
      MAGIC_CHECK_END();                                                 \
      Evas_Object_Protected_Data *obj = efl_data_scope_safe_get(eo_obj, EFL_CANVAS_OBJECT_CLASS); \
-     if (!func) return;                                                 \
+     if ((!obj) || (!func) return;                                      \
      evas_object_intercept_init(obj);                                   \
      if (!obj->interceptors) return;                                    \
      obj->interceptors->Lower_Type.func = func;                         \
@@ -281,7 +281,7 @@ _evas_object_intercept_call_evas(Evas_Object_Protected_Data *obj,
      return NULL;                                                       \
      MAGIC_CHECK_END();                                                 \
      Evas_Object_Protected_Data *obj = efl_data_scope_safe_get(eo_obj, EFL_CANVAS_OBJECT_CLASS); \
-     if (!func) return NULL;                                            \
+     if ((!obj) || (!func)) return NULL;                                \
      if (!obj->interceptors) return NULL;                               \
      obj->interceptors->Lower_Type.func = NULL;                         \
      data = obj->interceptors->Lower_Type.data;                         \

commit f6464b0deb6de2dbd877916413ac1178b0145c78

evas_pbject_intercept: add missing parenthesis
Broken in 7c99e0a444d81cc545181682f1a4ae00f76d0873.

*spanks*

src/lib/evas/canvas/evas_object_intercept.c
@@ -264,7 +264,7 @@ _evas_object_intercept_call_evas(Evas_Object_Protected_Data *obj,
      return;                                                            \
      MAGIC_CHECK_END();                                                 \
      Evas_Object_Protected_Data *obj = efl_data_scope_safe_get(eo_obj, EFL_CANVAS_OBJECT_CLASS); \
-     if ((!obj) || (!func) return;                                      \
+     if ((!obj) || (!func)) return;                                     \
      evas_object_intercept_init(obj);                                   \
      if (!obj->interceptors) return;                                    \
      obj->interceptors->Lower_Type.func = func;                         \