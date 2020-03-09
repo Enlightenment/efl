a491e95dea7bdde97635c292b8c415f5f2320c6e
Marcel Hollerbach <mail@marcel-hollerbach.de>
2020-03-09 16:45:36 +0100

    elm_cnp: free array when going out of scope
    this was missing and indeed a memory leak.

--- src/lib/elementary/elm_cnp.c
+++ src/lib/elementary/elm_cnp.c
@@ -104,6 +104,7 @@ elm_cnp_selection_set(Evas_Object *obj, Elm_Sel_Type selection,
         ERR("You cannot specify more than one format when setting selection");
      }
    mime_type = eina_array_data_get(tmp, 0);
+   eina_array_free(tmp);
    content = eina_content_new(data, mime_type);
    _register_selection_changed(obj);
 
