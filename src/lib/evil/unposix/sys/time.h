82e9fb78a5ee0f4daf3697f2145a596c0d67311a
Lucas Cavalcante de Sousa <lucks.sousa@gmail.com>
2020-03-18 16:46:43 -0300

    sys/time.h: add gettimeofday and localtime_r

--- src/lib/evil/unposix/sys/time.h
+++ src/lib/evil/unposix/sys/time.h
@@ -1,10 +1,10 @@
-#ifndef TIME_H
-#define TIME_H
+#ifndef SYS_TIME_H
+#define SYS_TIME_H
 
-#include "unimplemented.h"
-
+#define WIN32_LEAN_AND_MEAN
 // Windows Kit for Windows 10 already defines `struct timeval` and `time_t`
 #include <winsock2.h>
+#include <time.h>
 
 typedef unsigned short u_short;
 
@@ -12,15 +12,30 @@ typedef long suseconds_t;
 
 typedef struct timeval timeval;
 
-UNIMPLEMENTED inline int gettimeofday(struct timeval* a, void* b) {
-    #warning gettimeofday is not implemented.
+inline int gettimeofday(struct timeval * tp, struct timezone * tzp)
+{
+    // This number is the number of 100 nanosecond intervals since 00:00:00
+    // 01/01/1601 (UTC) (Windows way) until 00:00:00 01/01/1970 (POSIX way)
+    static const uint64_t EPOCH = ((uint64_t) 116444736000000000ULL);
+
+    SYSTEMTIME system_time;
+    FILETIME file_time;
+    uint64_t time;
+
+    GetSystemTime(&system_time);
+    SystemTimeToFileTime( &system_time, &file_time );
+    time =  ((uint64_t)file_time.dwLowDateTime )      ;
+    time += ((uint64_t)file_time.dwHighDateTime) << 32;
+
+    tp->tv_sec  = (long) ((time - EPOCH) / 10000000L);
+    tp->tv_usec = (long) (system_time.wMilliseconds * 1000);
     return 0;
 }
 
-UNIMPLEMENTED inline struct tm *localtime_r(const time_t * time, struct tm * result)
+inline struct tm *localtime_r(const time_t * time, struct tm * result)
 {
-    #warning localtime_r is not implemented.
-    return 0;
+    localtime_s(result, time);
+    return result;
 }
 
 #endif
