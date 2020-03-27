cc8538f53b3517ca6f795531cd21a527e197d7d8
Stefan Schmidt <s.schmidt@samsung.com>
2020-03-27 09:54:43 +0100

    ci: enable ecore_con IPv6 tests
    We have them behind an env variable as this is system specific. Enable
    them on CI to fully test these paths as well.

--- .ci/ci-make-check.sh
+++ .ci/ci-make-check.sh
@@ -11,6 +11,7 @@ NUM_TRIES=5
 
 if [ "$1" = "codecov" ] ; then
   for tries in $(seq 1 ${NUM_TRIES}); do
+    export EFL_TEST_ECORE_CON_IPV6=1
     meson test -t 120 -C build --wrapper dbus-launch && break
     cat build/meson-logs/testlog-dbus-launch.txt
     if [ $tries != ${NUM_TRIES} ] ; then echo "tests failed, trying again!" ; fi
@@ -24,9 +25,9 @@ travis_fold check "ninja test"
 if [ "$DISTRO" != "" ] ; then
   for tries in $(seq 1 ${NUM_TRIES}); do
     if [ "$1" = "asan" ]; then
-      (docker exec --env EINA_LOG_BACKTRACE="0" --env EIO_MONITOR_POLL=1 --env ASAN_OPTIONS=detect_leaks=0:abort_on_error=0 $(cat $HOME/cid) meson test -t 120 -C build --wrapper dbus-launch) && break
+      (docker exec --env EFL_TEST_ECORE_CON_IPV6=1 --env EINA_LOG_BACKTRACE="0" --env EIO_MONITOR_POLL=1 --env ASAN_OPTIONS=detect_leaks=0:abort_on_error=0 $(cat $HOME/cid) meson test -t 120 -C build --wrapper dbus-launch) && break
     else
-      (docker exec --env EINA_LOG_BACKTRACE="0" --env EIO_MONITOR_POLL=1 $(cat $HOME/cid) meson test -t 120 -C build --wrapper dbus-launch ) && break
+      (docker exec --env EFL_TEST_ECORE_CON_IPV6=1 --env EINA_LOG_BACKTRACE="0" --env EIO_MONITOR_POLL=1 $(cat $HOME/cid) meson test -t 120 -C build --wrapper dbus-launch ) && break
     fi
     docker exec --env EIO_MONITOR_POLL=1 $(cat $HOME/cid) cat build/meson-logs/testlog-dbus-launch.txt
     if [ $tries != ${NUM_TRIES} ] ; then echo "tests failed, trying again!" ; fi
