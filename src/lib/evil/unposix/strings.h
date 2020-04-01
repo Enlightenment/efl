cf62ea7bab2e252d2cee5444f218ba56dfe3f380
Joao Antonio Cardoso <joao.antonio@expertisesolutions.com.br>
2020-04-01 19:07:01 -0300

    alternative: using definitions to map ucrt

--- src/lib/evil/unposix/strings.h
+++ src/lib/evil/unposix/strings.h
@@ -1,37 +1,9 @@
 #ifndef UNPOSIX_STRINGS_H
 #define UNPOSIX_STRINGS_H
 
-#include <sys/types.h>
-#include <locale.h>
-
-inline int ffs(int i)
-{
-    #warning ffs is not implemented.
-    return 0;
-}
-
-inline int strcasecmp(const char* s1, const char* s2)
-{
-    #warning strcasecmp is not implemented.
-    return 0;
-}
-
-inline int strcasecmp_l(const char* s1, const char* s2, locale_t locale)
-{
-    #warning strcasecmp_l is not implemented.
-    return 0;
-}
-
-inline int strncasecmp(const char* s1, const char* s2, size_t size)
-{
-    #warning strncasecmp is not implemented.
-    return 0;
-}
-
-inline int strncasecmp_l(const char* s1, const char* s2, size_t size, locale_t locale)
-{
-    #warning strncasecmp_l is not implemented.
-    return 0;
-}
+#define strncasecmp _strnicmp
+#define strcasecmp _stricmp
+#define strncasecmp_l _strnicmp_l
+#define strcasecmp_l _strncmp_l
 
 #endif
