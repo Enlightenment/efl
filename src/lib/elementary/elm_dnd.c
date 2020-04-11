24219081c697a7badf59c460476fdcd66c67f711
Marcel Hollerbach <mail@marcel-hollerbach.de>
2020-04-11 19:09:21 +0200

    elm_dnd: complete the list of text types
    the text types added here were present as "TEXT" in selection manager,
    so add them here again.
    
    Reviewed-by: Christopher Michael <devilhorns@comcast.net>
    Differential Revision: https://phab.enlightenment.org/D11684

--- src/lib/elementary/elm_dnd.c
+++ src/lib/elementary/elm_dnd.c
@@ -133,7 +133,11 @@ _format_to_mime_array(Elm_Sel_Format format)
    Eina_Array *ret = eina_array_new(10);
 
    if (format & ELM_SEL_FORMAT_TEXT)
-     eina_array_push(ret, "text/plain;charset=utf-8");
+     {
+        eina_array_push(ret, "text/plain");
+        eina_array_push(ret, "text/plain;charset=utf-8");
+        eina_array_push(ret, "text/uri-list");
+     }
    if (format & ELM_SEL_FORMAT_MARKUP)
      eina_array_push(ret, "application/x-elementary-markup");
    if (format & ELM_SEL_FORMAT_IMAGE)
