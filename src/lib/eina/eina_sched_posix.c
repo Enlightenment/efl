4e346e2c06c26928420dcf0deb077b01d84839cb
carlos <caiosignor@gmail.com>
2020-04-17 16:02:53 -0300

    eina_sched_posix code convention

--- src/lib/eina/eina_sched_posix.c
+++ src/lib/eina/eina_sched_posix.c
@@ -25,46 +25,45 @@
 
 EAPI void eina_sched_prio_drop(void)
 {
-  struct sched_param param;
-  int pol, ret;
-  Eina_Thread pthread_id;
+   struct sched_param param;
+   int pol, ret;
+   Eina_Thread pthread_id;
 
-  pthread_id = eina_thread_self();
-  ret = pthread_getschedparam(pthread_id, &pol, &param);
-  if (ret)
-    {
-      EINA_LOG_ERR("Unable to query sched parameters");
-      return;
-    }
+   pthread_id = eina_thread_self();
+   ret = pthread_getschedparam(pthread_id, &pol, &param);
+   if (ret)
+     {
+        EINA_LOG_ERR("Unable to query sched parameters");
+        return;
+     }
 
-  if (EINA_UNLIKELY(pol == SCHED_RR || pol == SCHED_FIFO))
-    {
-      param.sched_priority -= RTNICENESS;
+   if (EINA_UNLIKELY(pol == SCHED_RR || pol == SCHED_FIFO))
+     {
+        param.sched_priority -= RTNICENESS;
 
-      /* We don't change the policy */
-      if (param.sched_priority < 1)
-        {
-          EINA_LOG_INFO("RT prio < 1, setting to 1 instead");
-          param.sched_priority = 1;
-        }
+        /* We don't change the policy */
+        if (param.sched_priority < 1)
+          {
+             EINA_LOG_INFO("RT prio < 1, setting to 1 instead");
+             param.sched_priority = 1;
+          }
 
-      pthread_setschedparam(pthread_id, pol, &param);
-    }
-  else
-    {
-      int prio;
-      errno = 0;
-      prio = getpriority(PRIO_PROCESS, 0);
-      if (errno == 0)
-        { 
-          prio += NICENESS;
-          if (prio > 19)
-            {
-              EINA_LOG_INFO("Max niceness reached; keeping max (19)");
-              prio = 19;
-            }
-
-          setpriority(PRIO_PROCESS, 0, prio);
-        }
-    }
+        pthread_setschedparam(pthread_id, pol, &param);
+     }
+   else
+     {
+        int prio;
+        errno = 0;
+        prio = getpriority(PRIO_PROCESS, 0);
+        if (errno == 0)
+          { 
+             prio += NICENESS;
+             if (prio > 19)
+               {
+                  EINA_LOG_INFO("Max niceness reached; keeping max (19)");
+                  prio = 19;
+               }
+             setpriority(PRIO_PROCESS, 0, prio);
+          }
+     }
 }
