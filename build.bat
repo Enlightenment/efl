0bb295fadca485008580e597ae201ca001b036bf
Felipe Magno de Almeida <felipe@expertise.dev>
2020-05-02 22:48:45 -0300

    windows: Add more debug information to build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,4 @@
 @echo off
-setlocal
 
 set __OLD_vcvars64=%vcvars64%
 set vcvars64="C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvars64.bat"
@@ -11,7 +10,9 @@ set vcvars64="C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\V
 set CLICOLOR_FORCE=1
 
 if not defined DevEnvDir (
+    echo DevEnvDir not defined. Running vcvars64...
     call %vcvars64%
+    @set DevEnvDir=%cd%
 )
 
 ninja %NINJAFLAGS% -C build
