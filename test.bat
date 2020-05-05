e84115e1355b3a26d9f7907cbcdc26efafccb303
Felipe Magno de Almeida <felipe@expertise.dev>
2020-05-04 23:49:54 -0300

    windows: Add test batch file

---
+++ test.bat
@@ -0,0 +1,22 @@
+@echo off
+
+set __OLD_vcvars64=%vcvars64%
+set vcvars64="C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvars64.bat"
+set CLICOLOR_FORCE=1
+
+set NINJAFLAGS=%*
+
+set vcvars64="C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvars64.bat"
+set CLICOLOR_FORCE=1
+
+if not defined DevEnvDir (
+    echo DevEnvDir not defined. Running vcvars64...
+    call %vcvars64%
+    @set DevEnvDir=%cd%
+)
+
+ninja %NINJAFLAGS% -C build test
+
+set vcvars64=%__OLD_vcvars64%
+set __OLD_vcvars64=
+exit /B %ERRORLEVEL%
