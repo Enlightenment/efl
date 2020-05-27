450e63b17ca6a2d2eaf5f40a9dd90d8cb86246ca
Stefan Schmidt <s.schmidt@samsung.com>
2020-05-27 10:01:31 +0200

    debug

--- .ci/ci-make.sh
+++ .ci/ci-make.sh
@@ -15,11 +15,9 @@ fi
 
 if [ "$DISTRO" != "" ] ; then
   if [ "$1" = "coverity" ] ; then
-    # Needed on 4.8 or newer Kernel to allow running cov tools
-    docker exec --env EIO_MONITOR_POLL=1 $(cat $HOME/cid) sh -c "cat /proc/cmdline || true"
-    docker exec --env EIO_MONITOR_POLL=1 $(cat $HOME/cid) sh -c "uname -a || true"
     docker exec --env EIO_MONITOR_POLL=1 --env PATH="/src/cov-analysis-linux64-2019.03/bin:$PATH" $(cat $HOME/cid) sh -c "cov-build --dir cov-int ninja -C build"
     docker exec --env EIO_MONITOR_POLL=1 $(cat $HOME/cid) sh -c "tar caf efl-$(git rev-parse --short HEAD).xz cov-int"
+    docker exec --env EIO_MONITOR_POLL=1 $(cat $HOME/cid) sh -c "cat /src/cov-int/build-log.txt"
 #docker exec --env EIO_MONITOR_POLL=1 --env COVERITY_SCAN_TOKEN=$COVERITY_SCAN_TOKEN $(cat $HOME/cid) sh -c ".ci/coverity-upload.sh"
   else
     docker exec --env EIO_MONITOR_POLL=1 $(cat $HOME/cid) ninja -C build
