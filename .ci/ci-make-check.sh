599e6a510d87db60038eed789e1729dea5ab7259
Stefan Schmidt <s.schmidt@samsung.com>
2020-06-02 16:03:00 +0200

    add timing for other parts

--- .ci/ci-make-check.sh
+++ .ci/ci-make-check.sh
@@ -8,9 +8,9 @@ fi
 
 NUM_TRIES=5
 
-travis_fold start "ninja-test"
-travis_time_start "ninja-test"
 if [ "$1" = "codecov" ] ; then
+  travis_fold start "codecov"
+  travis_time_start "codecov"
   for tries in $(seq 1 ${NUM_TRIES}); do
     export EFL_TEST_ECORE_CON_IPV6=1
     meson test -t 120 -C build --wrapper dbus-launch && break
@@ -19,9 +19,13 @@ if [ "$1" = "codecov" ] ; then
       false
   done
   curl -s https://codecov.io/bash | bash -s -
+  travis_time_finish "codecov"
+  travis_fold end "codecov"
   exit 0
 fi
 
+travis_fold start "ninja-test"
+travis_time_start "ninja-test"
 if [ "$DISTRO" != "" ] ; then
   for tries in $(seq 1 ${NUM_TRIES}); do
     (docker exec --env EINA_LOG_BACKTRACE="0" --env EIO_MONITOR_POLL=1 $(cat $HOME/cid) meson test -t 120 -C build --wrapper dbus-launch ) && break
