816b3b36379b2eafc2cd91cd9d63597996f45a2a
Stefan Schmidt <s.schmidt@samsung.com>
2020-06-04 15:19:47 +0200

    ci: simplify ccache setup handling
    We no longer need the special case for release ready. This was needed
    for the way autotools setup directories, which we no longer use. (We are
    not having a config.log anymore)
    
    Differential Revision: https://phab.enlightenment.org/D11937

--- .ci/docker-ccache-setup.sh
+++ .ci/docker-ccache-setup.sh
@@ -4,10 +4,5 @@ CI_BUILD_TYPE="$1"
 
 cp .ci/ccache.conf ~/.ccache
 
-if [ "$1" = "release-ready" ] ; then
-  sed -iE '/^base_dir/d' ~/.ccache/ccache.conf
-  echo "base_dir = $(pwd)/$(grep '^PACKAGE_STRING' config.log|cut -d\' -f2|tr ' ' -)" >> ~/.ccache/ccache.conf
-else
-  sed -iE '/^base_dir/d' ~/.ccache/ccache.conf
-  echo "base_dir = $pwd" >> ~/.ccache/ccache.conf
-fi
+sed -iE '/^base_dir/d' ~/.ccache/ccache.conf
+echo "base_dir = $pwd" >> ~/.ccache/ccache.conf
