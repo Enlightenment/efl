4c2cbecc69c4c08ccacba7d2f6bef072addc3e33
Stefan Schmidt <s.schmidt@samsung.com>
2020-06-09 09:43:57 +0200

    Revert "ci: disable wepb loader until we have a change to update the mingw cross env"
    This reverts commit 63b1334e2c98f0c0f08801ce77d66f372e7abb30.
    
    No longer needed as we have updated the mingw image to have 1.1.0
    available.

--- .ci/ci-configure.sh
+++ .ci/ci-configure.sh
@@ -37,7 +37,7 @@ if [ "$DISTRO" != "" ] ; then
 
   MINGW_COPTS="--cross-file .ci/cross_toolchain.txt -Davahi=false -Deeze=false -Dsystemd=false \
   -Dpulseaudio=false -Dx11=false -Dopengl=none -Dlibmount=false \
-  -Devas-loaders-disabler=json,pdf,ps,raw,svg,rsvg,webp \
+  -Devas-loaders-disabler=json,pdf,ps,raw,svg,rsvg \
   -Dharfbuzz=true -Dpixman=true -Dembedded-lz4=false "
 
   if [ "$1" = "default" ]; then
