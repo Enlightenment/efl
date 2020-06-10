e48ac73bdcd43ffff1177739d2aa2c015da9a3cb
Christopher Michael <devilhorns@comcast.net>
2020-06-10 08:26:29 -0400

    ecore_wl2_tests: Fix formatting

--- src/tests/ecore_wl2/ecore_wl2_tests_helper_egl.h
+++ src/tests/ecore_wl2/ecore_wl2_tests_helper_egl.h
@@ -11,23 +11,28 @@ _init_egl(Test_Data *td)
    eglBindAPI(EGL_OPENGL_API);
    EGLint num_config;
 
-   EGLint attributes[] = {
+   EGLint attributes[] =
+     {
         EGL_RED_SIZE, 8,
         EGL_GREEN_SIZE, 8,
         EGL_BLUE_SIZE, 8,
         EGL_NONE
-   };
+     };
 
-   td->egl_display = eglGetDisplay((EGLNativeDisplayType)ecore_wl2_display_get(td->display));
+   td->egl_display =
+     eglGetDisplay((EGLNativeDisplayType)ecore_wl2_display_get(td->display));
    eglInitialize(td->egl_display, NULL, NULL);
    eglChooseConfig(td->egl_display, attributes, &td->egl_conf, 1, &num_config);
-   td->egl_context = eglCreateContext(td->egl_display, td->egl_conf, EGL_NO_CONTEXT, NULL);
+   td->egl_context =
+     eglCreateContext(td->egl_display, td->egl_conf, EGL_NO_CONTEXT, NULL);
 
    td->egl_window = wl_egl_window_create(td->surface, td->width, td->height);
-   td->egl_surface = eglCreateWindowSurface(td->egl_display,
-                                            td->egl_conf, td->egl_window, NULL);
+   td->egl_surface =
+     eglCreateWindowSurface(td->egl_display,
+                            td->egl_conf, td->egl_window, NULL);
 
-   eglMakeCurrent(td->egl_display, td->egl_surface, td->egl_surface, td->egl_context);
+   eglMakeCurrent(td->egl_display, td->egl_surface,
+                  td->egl_surface, td->egl_context);
 }
 
 static void
