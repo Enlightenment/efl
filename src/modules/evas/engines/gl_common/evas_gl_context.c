f3e6a8c9b38959f98dc0593ddf7954ea8373ee35
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2020-06-17 12:17:13 +0100

    gl common - actually allow vertex limits
    fix non-commented out return that should have been commented out

--- src/modules/evas/engines/gl_common/evas_gl_context.c
+++ src/modules/evas/engines/gl_common/evas_gl_context.c
@@ -1845,7 +1845,7 @@ pipe_region_expand(Evas_Engine_GL_Context *gc, int n,
 static Eina_Bool
 vertex_array_size_check(Evas_Engine_GL_Context *gc, int pn, int n)
 {
-   return 1;
+//   return 1;
 // this fixup breaks for expedite test 32. why?
 // for reference
    if ((gc->pipe[pn].array.num + n) > gc->shared->info.max_vertex_elements)
