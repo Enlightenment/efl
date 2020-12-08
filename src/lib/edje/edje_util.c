cfd17f145ace345f08581cd17b9328eaf56812e2
Woochanlee <wc0917.lee@samsung.com>
2020-12-08 12:16:22 +0000

    edje_util: Fix memory leak
    Summary:
    evas_object_data_set call callc for internal node.
    It's not free before call evas_object_data_del or evas_object_data_set(obj, NULL)
    
    Reviewers: raster, cedric, Hermet
    
    Reviewed By: raster
    
    Subscribers: cedric, #reviewers, #committers
    
    Tags: #efl
    
    Differential Revision: https://phab.enlightenment.org/D12205

--- src/lib/edje/edje_util.c
+++ src/lib/edje/edje_util.c
@@ -6384,6 +6384,7 @@ _edje_real_part_swallow_clear(Edje *ed, Edje_Real_Part *rp)
                                          rp);
    evas_object_clip_unset(rp->typedata.swallow->swallowed_object);
    evas_object_data_del(rp->typedata.swallow->swallowed_object, "\377 edje.swallowing_part");
+   evas_object_data_del(rp->typedata.swallow->swallowed_object, ".edje");
    _edje_callbacks_del(rp->typedata.swallow->swallowed_object, ed);
    _edje_callbacks_focus_del(rp->typedata.swallow->swallowed_object, ed);
    rp->typedata.swallow->swallowed_object = NULL;
