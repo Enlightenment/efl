f6d0bc1b2980098859f6181bf823c6b627ae6a5b
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2021-01-11 11:04:32 +0000

    evas - missing EVAS_API - add.

--- src/lib/evas/include/evas_private.h
+++ src/lib/evas/include/evas_private.h
@@ -1382,7 +1382,7 @@ void evas_render_object_recalc(Evas_Object_Protected_Data *obj);
 void evas_render_proxy_subrender(Evas *eo_e, void *output, Evas_Object *eo_source, Evas_Object *eo_proxy, Evas_Object_Protected_Data *proxy_obj, Eina_Bool source_clip, Eina_Bool do_async);
 
 Eina_Bool evas_map_inside_get(const Evas_Map *m, Evas_Coord x, Evas_Coord y);
-Eina_Bool evas_map_coords_get(const Evas_Map *m, double x, double y, double *mx, double *my, int grab);
+EVAS_API Eina_Bool evas_map_coords_get(const Evas_Map *m, double x, double y, double *mx, double *my, int grab);
 Eina_Bool evas_object_map_update(Evas_Object *obj, int x, int y, int imagew, int imageh, int uvw, int uvh);
 void evas_map_object_move_diff_set(Evas_Map *m, Evas_Coord diff_x, Evas_Coord diff_y);
 
