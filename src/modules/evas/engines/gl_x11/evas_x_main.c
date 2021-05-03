0509fbe16d63a45eb67b139e4bad9759714028cd
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2021-05-03 20:43:56 +0100

    evas gl - dont swap with damages if we are debugging partial

--- src/modules/evas/engines/gl_x11/evas_x_main.c
+++ src/modules/evas/engines/gl_x11/evas_x_main.c
@@ -1629,7 +1629,7 @@ eng_outbuf_flush(Outbuf *ob, Tilebuf_Rect *surface_damage EINA_UNUSED, Tilebuf_R
         ob->vsync = 1;
      }
    if ((glsym_eglSwapBuffersWithDamage) && (rects) &&
-       (ob->swap_mode != MODE_FULL))
+       (ob->swap_mode != MODE_FULL) && (!partial_render_debug))
      {
         EGLint num = 0, *result = NULL, i = 0;
         Tilebuf_Rect *r;
