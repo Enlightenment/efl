da2b8ca35687690bc74cb73b6551ba80f500b314
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2022-08-02 20:39:47 +0100

    build confs - add libinput false to osx conf
    @fix

--- confs/osx.sh
+++ confs/osx.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
 meson -Dsystemd=false -Dv4l2=false -Davahi=false -Deeze=false -Dx11=false \
--Dopengl=full -Dcocoa=true -Dnls=false \
+-Dopengl=full -Dcocoa=true -Dnls=false -Dinput=false \
 -Decore-imf-loaders-disabler=scim,ibus \
  $@ . build
