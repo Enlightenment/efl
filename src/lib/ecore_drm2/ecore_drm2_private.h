fcafe3d9baec4362780ab74048d1953a5c53f488
Christopher Michael <devilhorns@comcast.net>
2022-09-20 08:38:14 -0400

    ecore_drm2: Add public structures for Crtc, Connector, Display, Plane
    This commit adds opaque API structures for Crtc, Connector, Display,
    and Plane. It also adds missing sym_drm functions that we will need
    later

--- src/lib/ecore_drm2/ecore_drm2_private.h
+++ src/lib/ecore_drm2/ecore_drm2_private.h
@@ -31,9 +31,6 @@
 /* define necessary vars/macros for ecore_drm2 log domain */
 extern int _ecore_drm2_log_dom;
 
-/* define external variable for atomic usage */
-extern Eina_Bool _ecore_drm2_atomic_use;
-
 # ifdef ECORE_DRM2_DEFAULT_LOG_COLOR
 #  undef ECORE_DRM2_DEFAULT_LOG_COLOR
 # endif
@@ -63,6 +60,13 @@ extern Eina_Bool _ecore_drm2_atomic_use;
 #  undef CRIT
 # endif
 # define CRIT(...) EINA_LOG_DOM_CRIT(_ecore_drm2_log_dom, __VA_ARGS__)
+
+typedef enum _Ecore_Drm2_Backlight_Type
+{
+   ECORE_DRM2_BACKLIGHT_RAW,
+   ECORE_DRM2_BACKLIGHT_PLATFORM,
+   ECORE_DRM2_BACKLIGHT_FIRMWARE
+} Ecore_Drm2_Backlight_Type;
 
 /* internal structures (not exposed) */
 typedef struct _Ecore_Drm2_Atomic_Blob
@@ -100,13 +104,13 @@ typedef struct _Ecore_Drm2_Connector_State
    /* Ecore_Drm2_Atomic_Property Content Protection; */
    /* Ecore_Drm2_Atomic_Property HDCP Content Type; */
    /* Ecore_Drm2_Atomic_Property subconnector; */
-
+   /* Ecore_Drm2_Atomic_Property panel orientation; */
 } Ecore_Drm2_Connector_State;
 
 typedef struct _Ecore_Drm2_Crtc_State
 {
    uint32_t obj_id;
-   int index;
+   /* int index; */
    Ecore_Drm2_Atomic_Property active;
    Ecore_Drm2_Atomic_Blob mode;
 
@@ -124,55 +128,145 @@ typedef struct _Ecore_Drm2_Crtc_State
 typedef struct _Ecore_Drm2_Plane_State
 {
    uint32_t obj_id, mask;
+   uint32_t num_formats;
+   uint32_t *formats;
+
    Ecore_Drm2_Atomic_Property type;
    Ecore_Drm2_Atomic_Property cid, fid;
    Ecore_Drm2_Atomic_Property sx, sy, sw, sh;
    Ecore_Drm2_Atomic_Property cx, cy, cw, ch;
    Ecore_Drm2_Atomic_Property rotation;
 
-   /* NB: these are not part of an atomic state, but we store these here
-    * so that we do not have to refetch properties when iterating planes */
-   uint32_t rotation_map[6];
-   uint32_t supported_rotations;
-
-   uint32_t num_formats;
-   uint32_t *formats;
-
-   Eina_Bool in_use;
-
    /* TODO ?? */
    /* Ecore_Drm2_Atomic_Property IN_FENCE_FD; */
    /* Ecore_Drm2_Atomic_Property IN_FORMATS; */
    /* Ecore_Drm2_Atomic_Property COLOR_ENCODING; */
    /* Ecore_Drm2_Atomic_Property COLOR_RANGE; */
+   /* Ecore_Drm2_Atomic_Blob FB_DAMAGE_CLIPS; */
+   /* Ecore_Drm2_Atomic_Property zpos; */
+
+   /* NB: these are not part of an atomic state, but we store these here
+    * so that we do not have to refetch properties when iterating planes */
+   uint32_t rotation_map[6];
+   uint32_t supported_rotations;
+
+   Eina_Bool in_use : 1;
 } Ecore_Drm2_Plane_State;
 
-typedef struct _Ecore_Drm2_Atomic_State
-{
-   int crtcs, conns, planes;
-   Ecore_Drm2_Crtc_State *crtc_states;
-   Ecore_Drm2_Connector_State *conn_states;
-   Ecore_Drm2_Plane_State *plane_states;
-} Ecore_Drm2_Atomic_State;
-
 /* opaque API structures */
+struct _Ecore_Drm2_Plane
+{
+   int fd;
+   uint32_t id;
+
+   drmModePlanePtr dplane;
+
+   Ecore_Drm2_Plane_State *state;
+
+   Ecore_Thread *thread;
+};
+
+struct _Ecore_Drm2_Display
+{
+   int fd;
+   int x, y, w, h;
+   int pw, ph; // physical dimensions
+   Eina_Stringshare *name, *make, *model, *serial;
+
+   uint32_t subpixel;
+   uint16_t gamma;
+
+   struct
+     {
+        char eisa[13];
+        char monitor[13];
+        char pnp[5];
+        char serial[13];
+     } edid;
+
+   struct
+     {
+        const char *path;
+        double value, max;
+        Ecore_Drm2_Backlight_Type type;
+     } backlight;
+
+   Ecore_Drm2_Crtc *crtc;
+   Ecore_Drm2_Connector *conn;
+
+   Ecore_Thread *thread;
+
+   Eina_Bool primary : 1;
+   Eina_Bool enabled : 1;
+   Eina_Bool connected : 1;
+};
+
+struct _Ecore_Drm2_Connector
+{
+   uint32_t id;
+   uint32_t type;
+   int fd;
+
+   drmModeConnector *conn;
+
+   Ecore_Drm2_Connector_State *state;
+
+   Ecore_Thread *thread;
+
+   Eina_Bool writeback : 1;
+};
+
+struct _Ecore_Drm2_Crtc
+{
+   uint32_t id;
+   uint32_t pipe;
+   int fd;
+
+   drmModeCrtcPtr dcrtc;
+
+   /* TODO: store FBs */
+
+   Ecore_Drm2_Crtc_State *state;
+
+   Ecore_Thread *thread;
+};
+
 struct _Ecore_Drm2_Device
 {
    Elput_Manager *em;
 
    int fd;
+   int clock_id;
 
-   Ecore_Drm2_Atomic_State *atomic_state;
-};
+   struct
+     {
+        int width, height;
+     } cursor;
 
-struct _Ecore_Drm2_Output
-{
-   Eina_Stringshare *name, *make, *model, *serial;
+   Ecore_Event_Handler *session_hdlr;
+
+   Eina_List *crtcs;
+   Eina_List *conns;
+   Eina_List *displays;
+   Eina_List *planes;
+
+   Eina_Bool atomic : 1;
+   /* Eina_Bool gbm_mods : 1; */
+   Eina_Bool aspect_ratio : 1;
 };
 
 /* internal function prototypes */
-Eina_Bool _ecore_drm2_atomic_state_fill(Ecore_Drm2_Atomic_State *state, int fd);
-void _ecore_drm2_atomic_state_free(Ecore_Drm2_Atomic_State *state);
+Eina_Bool _ecore_drm2_crtcs_create(Ecore_Drm2_Device *dev);
+void _ecore_drm2_crtcs_destroy(Ecore_Drm2_Device *dev);
+
+Eina_Bool _ecore_drm2_connectors_create(Ecore_Drm2_Device *dev);
+void _ecore_drm2_connectors_destroy(Ecore_Drm2_Device *dev);
+
+Eina_Bool _ecore_drm2_displays_create(Ecore_Drm2_Device *dev);
+void _ecore_drm2_displays_destroy(Ecore_Drm2_Device *dev);
+
+Eina_Bool _ecore_drm2_planes_create(Ecore_Drm2_Device *dev);
+void _ecore_drm2_planes_destroy(Ecore_Drm2_Device *dev);
 
 /* external drm function prototypes (for dlopen) */
 extern void *(*sym_drmModeGetResources)(int fd);
@@ -191,5 +285,11 @@ extern void *(*sym_drmModeGetPlaneResources)(int fd);
 extern void (*sym_drmModeFreePlaneResources)(drmModePlaneResPtr ptr);
 extern void *(*sym_drmModeGetPlane)(int fd, uint32_t plane_id);
 extern void (*sym_drmModeFreePlane)(drmModePlanePtr ptr);
+extern void *(*sym_drmModeGetConnector)(int fd, uint32_t connectorId);
+extern void (*sym_drmModeFreeConnector)(drmModeConnectorPtr ptr);
+extern void *(*sym_drmModeGetEncoder)(int fd, uint32_t encoder_id);
+extern void (*sym_drmModeFreeEncoder)(drmModeEncoderPtr ptr);
+extern void *(*sym_drmModeGetCrtc)(int fd, uint32_t crtcId);
+extern void (*sym_drmModeFreeCrtc)(drmModeCrtcPtr ptr);
 
 #endif
