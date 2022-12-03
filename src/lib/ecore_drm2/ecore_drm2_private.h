cd5afa6128c4d3cf5357e4ff5ddd38d8ed156ffd
Christopher Michael <devilhorns@comcast.net>
2022-12-03 06:09:46 -0500

    ecore_drm2: Reorder defines for messages

--- src/lib/ecore_drm2/ecore_drm2_private.h
+++ src/lib/ecore_drm2/ecore_drm2_private.h
@@ -36,25 +36,25 @@ extern int _ecore_drm2_log_dom;
 # endif
 # define ECORE_DRM2_DEFAULT_LOG_COLOR EINA_COLOR_BLUE
 
-# ifdef ERR
-#  undef ERR
+# ifdef INF
+#  undef INF
 # endif
-# define ERR(...) EINA_LOG_DOM_ERR(_ecore_drm2_log_dom, __VA_ARGS__)
+# define INF(...) EINA_LOG_DOM_INFO(_ecore_drm2_log_dom, __VA_ARGS__)
 
 # ifdef DBG
 #  undef DBG
 # endif
 # define DBG(...) EINA_LOG_DOM_DBG(_ecore_drm2_log_dom, __VA_ARGS__)
 
-# ifdef INF
-#  undef INF
-# endif
-# define INF(...) EINA_LOG_DOM_INFO(_ecore_drm2_log_dom, __VA_ARGS__)
-
 # ifdef WRN
 #  undef WRN
 # endif
 # define WRN(...) EINA_LOG_DOM_WARN(_ecore_drm2_log_dom, __VA_ARGS__)
+
+# ifdef ERR
+#  undef ERR
+# endif
+# define ERR(...) EINA_LOG_DOM_ERR(_ecore_drm2_log_dom, __VA_ARGS__)
 
 # ifdef CRIT
 #  undef CRIT
