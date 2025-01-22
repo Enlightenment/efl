Enlightenment/efl

commit 57f0b42b94e19c57e2b5c3e91f87ad711df5af4f

spellign ... xfi smoe esplliing in EFL

src/lib/evil/evil_dlfcn.h
@@ -2,7 +2,7 @@
 #define __EVIL_DLFCN_H__
 
 
-#include "evil_private.h"
+#include "evil_privte.h"
 #include <limits.h>
 
 
@@ -100,7 +100,7 @@ struct Dl_info
  * slash, or backward ones. Mapping a module can map other modules.
  * @p mode is unused.
  *
- * If an error occurred, an error string can be retrived with dlerror().
+ * If an error occurred, an error string can be retrieved with dlerror().
  *
  * According to the OS, the search order of the module can change,
  * according to the value of SafeDllSearchMode.
@@ -159,7 +159,7 @@ EVIL_API void *dlopen(const char* path, int mode);
  * is the value returned by a previous call to dlopen().
  *
  * If no error occurred, the returned value is 0, otherwise the
- * returned value is -1 and an error string can be retrived with
+ * returned value is -1 and an error string can be retrieved with
  * dlerror().
  *
  * Conformity: None.
@@ -184,7 +184,7 @@ EVIL_API int dlclose(void* handle);
  *
  * If no error occurred, the returned value is the code or data
  * location, otherwise the returned value is NULL and an error
- * string can be retrived with dlerror().
+ * string can be retrieved with dlerror().
  *
  * Conformity: None.
  *

src/bin/elementary/config.c
@@ -4213,7 +4213,7 @@ _status_config_rendering(Evas_Object *win,
 
    CHECK_ADD("Aggressive withdrawn",
              "When the application is iconified it will<br>"
-             "drop its ressource and switch to a paused state<br>"
+             "drop its resources and switch to a paused state<br>"
              "if the application handle that lifecycle state.",
              _cb_withdrawn, NULL);
    elm_check_state_set(ck, elm_config_agressive_withdrawn_get());

src/bin/elementary/test.c
@@ -411,7 +411,7 @@ void test_efl_ui_vg_animation(void *data, Evas_Object *obj, void *event_info);
 void test_efl_gfx_vg_value_provider(void *data, Evas_Object *obj, void *event_info);
 void test_ui_separator(void *data EINA_UNUSED, Eo *obj EINA_UNUSED, void *event_info EINA_UNUSED);
 
-static void _list_udpate(void);
+static void _list_update(void);
 
 static Evas_Object *win, *tbx, *entry; // TODO: refactoring
 static void *tt;
@@ -472,14 +472,14 @@ static void
 _legacy_chk_changed(void *data EINA_UNUSED, Evas_Object *obj, void *event_info EINA_UNUSED)
 {
    hide_legacy = elm_check_state_get(obj);
-   _list_udpate();
+   _list_update();
 }
 
 static void
 _beta_chk_changed(void *data EINA_UNUSED, Evas_Object *obj, void *event_info EINA_UNUSED)
 {
    hide_beta = elm_check_state_get(obj);
-   _list_udpate();
+   _list_update();
 }
 
 static void
@@ -603,7 +603,7 @@ _entry_changed_cb(void *data EINA_UNUSED, Evas_Object *obj, void *event_info EIN
 }
 
 static void
-_list_udpate(void)
+_list_update(void)
 {
    const char *str = elm_entry_entry_get(entry);
    if (!str) return;

src/bin/elementary/test_entry.c
@@ -2688,7 +2688,7 @@ _test_regex_bt_cb(void *data,
      {
      case ELM_REG_NOERROR:
           {
-             eina_strbuf_append(tmp, "No error. Regex maches to the Entrys text.");
+             eina_strbuf_append(tmp, "No error. Regex matches Entry text.");
              break;
           }
      case ELM_REG_BADPAT:

src/bin/elementary/test_scroller.c
@@ -1022,7 +1022,7 @@ _btn_clicked_cb(void *data, Evas_Object *obj EINA_UNUSED, void *event_info EINA_
    elm_object_text_set(lbl,
                        "Do you want close the popup?<br>"
                        "Please select cancel button<br><br>"
-                       "Do you wnat close the popup?<br>"
+                       "Do you want close the popup?<br>"
                        "Please select cancel button");
    evas_object_size_hint_weight_set(lbl, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
    evas_object_size_hint_align_set(lbl, EVAS_HINT_FILL, EVAS_HINT_FILL);

src/bin/embryo/embryo_cc_sc2.c
@@ -2270,7 +2270,7 @@ stowlit(cell val)
  *  Return current literal character and increase the pointer to point
  *  just behind this literal character.
  *
- *  Note: standard "escape sequences" are suported, but the backslash may be
+ *  Note: standard "escape sequences" are supported, but the backslash may be
  *        replaced by another character; the syntax '\ddd' is supported,
  *        but ddd must be decimal!
  */

src/bin/eolian_mono/eolian/mono/function_helpers.hh
@@ -195,7 +195,7 @@ inline std::string function_scope_get(attributes::function_def const& f)
        return "protected ";
      case attributes::member_scope::scope_unknown:
        // This should trigger a compilation error
-       return "unkown_scope ";
+       return "unknown_scope ";
     }
   return " ";
 }

src/bindings/mono/eina_mono/eina_container_common.cs
@@ -1085,7 +1085,7 @@ internal static class TraitFunctions
     // Traits functions //
     //                  //
 
-    // Convertion functions //
+    // Conversion functions //
 
     public static IntPtr ManagedToNativeAlloc<T>(T man)
     {

commit ab7c227fe84f792dfad08ac23f1967357ff84c86

build: fix compilation of evil
bug introduced in 57f0b42b94e19c57e2b5c3e91f87ad711df5af4f

src/lib/evil/evil_dlfcn.h
@@ -2,7 +2,7 @@
 #define __EVIL_DLFCN_H__
 
 
-#include "evil_privte.h"
+#include "evil_private.h"
 #include <limits.h>
 
 