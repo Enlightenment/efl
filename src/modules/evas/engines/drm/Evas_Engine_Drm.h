9394d78dff41e0b55516671b6fa82b465941b8cc
Christopher Michael <devilhorns@comcast.net>
2025-08-19 08:48:07 -0500

    evas_engine_drm: Add start of basic evas_engine_drm header files

---
+++ src/modules/evas/engines/drm/Evas_Engine_Drm.h
@@ -0,0 +1,27 @@
+#ifndef _EVAS_ENGINE_DRM_H
+# define _EVAS_ENGINE_DRM_H
+
+# include <Ecore_Drm2.h>
+
+typedef struct _Evas_Engine_Info_Drm
+{
+   /* PRIVATE - don't mess with this baby or evas will poke its tongue out */
+   /* at you and make nasty noises */
+   Evas_Engine_Info magic;
+
+   struct
+     {
+        int depth, bpp;
+        unsigned int format, rotation;
+
+        Ecore_Drm2_Device *dev;
+
+        Eina_Bool alpha : 1;
+        Eina_Bool vsync : 1;
+     } info;
+
+   /* non-blocking or blocking mode */
+   Evas_Engine_Render_Mode render_mode;
+} Evas_Engine_Info_Drm;
+
+#endif
