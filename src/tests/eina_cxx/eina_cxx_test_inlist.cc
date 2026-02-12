4e98eef2d56280cfe1494c0ec41faca14aa7f08b
Swagtoy <me@ow.swag.toys>
2026-02-12 11:18:32 -0500

    tests: Fix std::not1 and std::bind1st deprecation
    I think the preferred solution nowadays would be std::not_fn with
    std::equal_to<int>()
    
    but... the lambda is shorter anyway. I have no idea why it was written
    like this.
    
    Signed-off-by: Swagtoy <me@ow.swag.toys>

--- src/tests/eina_cxx/eina_cxx_test_inlist.cc
+++ src/tests/eina_cxx/eina_cxx_test_inlist.cc
@@ -171,8 +171,8 @@ EFL_START_TEST(eina_cxx_inlist_constructors)
 
   efl::eina::inlist<int> list2(10, 5);
   ck_assert(list2.size() == 10);
-  ck_assert(std::find_if(list2.begin(), list2.end()
-                      , std::not1(std::bind1st(std::equal_to<int>(), 5))) == list2.end());
+  ck_assert(std::find_if(list2.begin(), list2.end(),
+                         [](int n) { return n != 5; }) == list2.end());
 
   efl::eina::inlist<int> list3(list2);
   ck_assert(list2 == list3);
